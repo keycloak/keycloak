0b9a59950a099a480e73c488d8dd33a0da93b44e
mposolda <mposolda@gmail.com>
2013-08-16 13:55:57 +0200

    KEYCLOAK-43 ApplicationAdapter.addScope doesn't add scope into IDM

--- services/src/test/java/org/keycloak/test/ImportTest.java
+++ services/src/test/java/org/keycloak/test/ImportTest.java
@@ -87,6 +87,13 @@ public class ImportTest {
         List<RealmModel> realms = identitySession.getRealms(admin);
         Assert.assertEquals(1, realms.size());
 
+        // Test scope relationship
+        ApplicationModel application = realm.getResourceNameMap().get("Application");
+        UserModel oauthClient = realm.getUser("oauthclient");
+        Assert.assertNotNull(application);
+        Assert.assertNotNull(oauthClient);
+        Set<String> appScopes = application.getScope(oauthClient);
+        Assert.assertTrue(appScopes.contains("user"));
     }
 
     @Test
