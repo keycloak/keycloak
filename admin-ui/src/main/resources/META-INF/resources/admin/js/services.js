36f91ac75c9b79629ef4f4f4d9d77f45c365a328
Stian Thorgersen <stianst@gmail.com>
2013-11-26 13:31:46 +0000

    KEYCLOAK-165 Confusing automatic converstion of time units

--- admin-ui/src/main/resources/META-INF/resources/admin/js/services.js
+++ admin-ui/src/main/resources/META-INF/resources/admin/js/services.js
@@ -226,9 +226,6 @@ module.factory('ApplicationOrigins', function($resource) {
     });
 });
 
-
-
-
 module.factory('Current', function(Realm, $route) {
     var current = {};
 
@@ -253,4 +250,52 @@ module.factory('Current', function(Realm, $route) {
     current.refresh();
 
     return current;
+});
+
+module.factory('TimeUnit', function() {
+    var t = {};
+
+    t.autoUnit = function(time) {
+        var unit = 'Seconds';
+        if (time % 60 == 0) {
+            unit = 'Minutes';
+            time  = time / 60;
+        }
+        if (time % 60 == 0) {
+            unit = 'Hours';
+            time = time / 60;
+        }
+        if (time % 24 == 0) {
+            unit = 'Days'
+            time = time / 24;
+        }
+        return unit;
+    }
+
+    t.toSeconds = function(time, unit) {
+        switch (unit) {
+            case 'Seconds': return time;
+            case 'Minutes': return time * 60;
+            case 'Hours': return time * 360;
+            case 'Days': return time * 86400;
+            default: throw 'invalid unit ' + unit;
+        }
+    }
+
+    t.toUnit = function(time, unit) {
+        switch (unit) {
+            case 'Seconds': return time;
+            case 'Minutes': return Math.ceil(time / 60);
+            case 'Hours': return Math.ceil(time / 360);
+            case 'Days': return Math.ceil(time / 86400);
+            default: throw 'invalid unit ' + unit;
+        }
+    }
+
+    t.convert = function(time, from, to) {
+        var seconds = t.toSeconds(time, from);
+        return t.toUnit(seconds, to);
+    }
+
+    return t;
 });
