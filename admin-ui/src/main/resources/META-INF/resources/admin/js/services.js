cce8814254c42a46786d5ec9a695eae714b8d0d9
Bill Burke <bburke@redhat.com>
2014-02-05 10:28:11 -0500

    composite switch

--- admin-ui/src/main/resources/META-INF/resources/admin/js/services.js
+++ admin-ui/src/main/resources/META-INF/resources/admin/js/services.js
@@ -235,6 +235,8 @@ function roleControl($scope, realm, role, roles, applications,
 
     if (!role.id) return;
 
+    $scope.compositeSwitch = role.composite;
+    $scope.compositeSwitchDisabled = role.composite;
     $scope.realmRoles = angular.copy(roles);
     $scope.selectedRealmRoles = [];
     $scope.selectedRealmMappings = [];
@@ -273,6 +275,7 @@ function roleControl($scope, realm, role, roles, applications,
     });
 
     $scope.addRealmRole = function() {
+        $scope.compositeSwitchDisabled=true;
         $http.post('/auth/rest/admin/realms/' + realm.realm + '/roles-by-id/' + role.id + '/composites',
                 $scope.selectedRealmRoles).success(function() {
                 for (var i = 0; i < $scope.selectedRealmRoles.length; i++) {
@@ -288,6 +291,7 @@ function roleControl($scope, realm, role, roles, applications,
     };
 
     $scope.deleteRealmRole = function() {
+        $scope.compositeSwitchDisabled=true;
         $http.delete('/auth/rest/admin/realms/' + realm.realm + '/roles-by-id/' + role.id + '/composites',
             {data : $scope.selectedRealmMappings, headers : {"content-type" : "application/json"}}).success(function() {
                 for (var i = 0; i < $scope.selectedRealmMappings.length; i++) {
@@ -303,6 +307,7 @@ function roleControl($scope, realm, role, roles, applications,
     };
 
     $scope.addApplicationRole = function() {
+        $scope.compositeSwitchDisabled=true;
         $http.post('/auth/rest/admin/realms/' + realm.realm + '/roles-by-id/' + role.id + '/composites',
                 $scope.selectedApplicationRoles).success(function() {
                 for (var i = 0; i < $scope.selectedApplicationRoles.length; i++) {
@@ -318,6 +323,7 @@ function roleControl($scope, realm, role, roles, applications,
     };
 
     $scope.deleteApplicationRole = function() {
+        $scope.compositeSwitchDisabled=true;
         $http.delete('/auth/rest/admin/realms/' + realm.realm + '/roles-by-id/' + role.id + '/composites',
             {data : $scope.selectedApplicationMappings, headers : {"content-type" : "application/json"}}).success(function() {
                 for (var i = 0; i < $scope.selectedApplicationMappings.length; i++) {
