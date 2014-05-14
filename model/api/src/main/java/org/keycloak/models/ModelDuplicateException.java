d9c8dedcfb62f17abc8b0ed372e362d58b5d0ecf
Matthias Wessendorf <matzew@apache.org>
2014-05-14 17:43:36 +0200

    removing JavaSE6 incompatible ctor

--- model/api/src/main/java/org/keycloak/models/ModelDuplicateException.java
+++ model/api/src/main/java/org/keycloak/models/ModelDuplicateException.java
@@ -20,8 +20,4 @@ public class ModelDuplicateException extends ModelException {
         super(cause);
     }
 
-    public ModelDuplicateException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
-
 }
