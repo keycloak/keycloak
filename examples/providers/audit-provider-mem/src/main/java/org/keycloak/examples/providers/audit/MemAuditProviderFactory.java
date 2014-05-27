6dda7fc3565dfe3e998068b4f84fbc2e4649ff3e
Stian Thorgersen <stianst@gmail.com>
2014-05-27 15:01:35 +0100

    Added example providers for audit listener and audit provider

---
+++ examples/providers/audit-provider-mem/src/main/java/org/keycloak/examples/providers/audit/MemAuditProviderFactory.java
@@ -0,0 +1,53 @@
+package org.keycloak.examples.providers.audit;
+
+import org.keycloak.Config;
+import org.keycloak.audit.AuditProvider;
+import org.keycloak.audit.AuditProviderFactory;
+import org.keycloak.audit.Event;
+import org.keycloak.audit.EventType;
+import org.keycloak.provider.ProviderSession;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author <a href="mailto:sthorger@redhat.com">Stian Thorgersen</a>
+ */
+public class MemAuditProviderFactory implements AuditProviderFactory {
+
+    private List<Event> events;
+
+    private Set<EventType> excludedEvents;
+
+    @Override
+    public AuditProvider create(ProviderSession providerSession) {
+        return new MemAuditProvider(events, excludedEvents);
+    }
+
+    @Override
+    public void init(Config.Scope config) {
+        events = Collections.synchronizedList(new LinkedList<Event>());
+
+        String excludes = config.get("excludes");
+        if (excludes != null) {
+            excludedEvents = new HashSet<EventType>();
+            for (String e : excludes.split(",")) {
+                excludedEvents.add(EventType.valueOf(e));
+            }
+        }
+    }
+
+    @Override
+    public void close() {
+        events = null;
+        excludedEvents = null;
+    }
+
+    @Override
+    public String getId() {
+        return "in-mem";
+    }
+}
