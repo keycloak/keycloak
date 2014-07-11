5f5316fbdcd28ffbf9b2c36e08eedf177188bd43
Bill Burke <bburke@redhat.com>
2014-07-11 19:29:11 -0400

    session transaction

---
+++ model/api/src/main/java/org/keycloak/models/UserProvider.java
@@ -0,0 +1,37 @@
+package org.keycloak.models;
+
+import org.keycloak.provider.Provider;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
+ * @version $Revision: 1 $
+ */
+public interface UserProvider extends Provider {
+    // Note: The reason there are so many query methods here is for layering a cache on top of an persistent KeycloakSession
+
+    KeycloakTransaction getTransaction();
+
+    UserModel addUser(RealmModel realm, String id, String username, boolean addDefaultRoles);
+    UserModel addUser(RealmModel realm, String username);
+    boolean removeUser(RealmModel realm, String name);
+
+    UserModel getUserById(String id, RealmModel realm);
+    UserModel getUserByUsername(String username, RealmModel realm);
+    UserModel getUserByEmail(String email, RealmModel realm);
+    UserModel getUserBySocialLink(SocialLinkModel socialLink, RealmModel realm);
+    List<UserModel> getUsers(RealmModel realm);
+    List<UserModel> searchForUser(String search, RealmModel realm);
+    List<UserModel> searchForUserByAttributes(Map<String, String> attributes, RealmModel realm);
+    Set<SocialLinkModel> getSocialLinks(UserModel user, RealmModel realm);
+    SocialLinkModel getSocialLink(UserModel user, String socialProvider, RealmModel realm);
+
+    void preRemove(RealmModel realm);
+    void preRemove(RoleModel role);
+
+
+    void close();
+}
--- services/src/main/java/org/keycloak/services/DefaultKeycloakSession.java
+++ services/src/main/java/org/keycloak/services/DefaultKeycloakSession.java
@@ -3,13 +3,16 @@ package org.keycloak.services;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakTransaction;
 import org.keycloak.models.ModelProvider;
+import org.keycloak.models.UserProvider;
 import org.keycloak.models.UserSessionProvider;
 import org.keycloak.models.cache.CacheModelProvider;
 import org.keycloak.provider.Provider;
 import org.keycloak.provider.ProviderFactory;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -20,21 +23,82 @@ public class DefaultKeycloakSession implements KeycloakSession {
 
     private final DefaultKeycloakSessionFactory factory;
     private final Map<Integer, Provider> providers = new HashMap<Integer, Provider>();
-    private final ModelProvider model;
+    private ModelProvider model;
+    private UserSessionProvider sessionProvider;
+    private final List<KeycloakTransaction> managedTransactions = new ArrayList<KeycloakTransaction>();
+
+    private final KeycloakTransaction transaction = new KeycloakTransaction() {
+        protected boolean active;
+        protected boolean rollback;
+
+        @Override
+        public void begin() {
+            active = true;
+        }
+
+        @Override
+        public void commit() {
+            if (!active) throw new IllegalStateException("Transaction not active");
+            try {
+                if (rollback) {
+                    rollback();
+                    throw new RuntimeException("Transaction markedfor rollback, so rollback happend");
+                }
+                for (KeycloakTransaction transaction : managedTransactions) {
+                    transaction.commit();
+                }
+            } finally {
+                active = false;
+            }
+
+        }
+
+        @Override
+        public void rollback() {
+            if (!active) throw new IllegalStateException("Transaction not active");
+            try {
+                for (KeycloakTransaction transaction : managedTransactions) {
+                    transaction.rollback();
+                }
+            } finally {
+                active = false;
+            }
+        }
+
+        @Override
+        public void setRollbackOnly() {
+            if (!active) throw new IllegalStateException("Transaction not active");
+            rollback = true;
+        }
+
+        @Override
+        public boolean getRollbackOnly() {
+            if (!active) throw new IllegalStateException("Transaction not active");
+            return rollback;
+        }
+
+        @Override
+        public boolean isActive() {
+            return active;
+        }
+    };
 
     public DefaultKeycloakSession(DefaultKeycloakSessionFactory factory) {
         this.factory = factory;
+    }
 
+    private ModelProvider getModelProvider() {
         if (factory.getDefaultProvider(CacheModelProvider.class) != null) {
-            model = getProvider(CacheModelProvider.class);
+            return getProvider(CacheModelProvider.class);
         } else {
-            model = getProvider(ModelProvider.class);
+            return getProvider(ModelProvider.class);
         }
     }
 
+
     @Override
     public KeycloakTransaction getTransaction() {
-        return model.getTransaction();
+        return transaction;
     }
 
     public <T extends Provider> T getProvider(Class<T> clazz) {
@@ -76,13 +140,26 @@ public class DefaultKeycloakSession implements KeycloakSession {
         return providers;
     }
 
+    @Override
     public ModelProvider model() {
+        if (!transaction.isActive()) throw new IllegalStateException("Transaction is not active");
+        if (model == null) {
+            model = getModelProvider();
+            model.getTransaction().begin();
+            managedTransactions.add(model.getTransaction());
+        }
         return model;
     }
 
     @Override
     public UserSessionProvider sessions() {
-        return getProvider(UserSessionProvider.class);
+        if (!transaction.isActive()) throw new IllegalStateException("Transaction is not active");
+        if (sessionProvider == null) {
+            sessionProvider = getProvider(UserSessionProvider.class);
+            sessionProvider.getTransaction().begin();
+            managedTransactions.add(sessionProvider.getTransaction());
+        }
+        return sessionProvider;
     }
 
     public void close() {
