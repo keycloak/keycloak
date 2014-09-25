f1dd254675598bb13112a076427775abce768242
Stian Thorgersen <stianst@gmail.com>
2014-09-25 14:02:43 +0200

    Add basic config for infinispan user session cache

--- model/sessions-infinispan/src/main/java/org/keycloak/models/sessions/infinispan/InfinispanUserSessionProviderFactory.java
+++ model/sessions-infinispan/src/main/java/org/keycloak/models/sessions/infinispan/InfinispanUserSessionProviderFactory.java
@@ -1,16 +1,13 @@
 package org.keycloak.models.sessions.infinispan;
 
-import org.infinispan.Cache;
-import org.infinispan.configuration.cache.Configuration;
+import org.infinispan.configuration.cache.CacheMode;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.global.GlobalConfigurationBuilder;
 import org.infinispan.manager.DefaultCacheManager;
 import org.keycloak.Config;
 import org.keycloak.models.KeycloakSession;
-import org.keycloak.models.RealmModel;
 import org.keycloak.models.UserSessionProvider;
 import org.keycloak.models.UserSessionProviderFactory;
-import org.keycloak.models.sessions.infinispan.entities.ClientSessionEntity;
-import org.keycloak.models.sessions.infinispan.entities.UserSessionEntity;
 
 /**
  * @author <a href="mailto:sthorger@redhat.com">Stian Thorgersen</a>
@@ -26,8 +23,36 @@ public class InfinispanUserSessionProviderFactory implements UserSessionProvider
 
     @Override
     public void init(Config.Scope config) {
-        Configuration configuration = new ConfigurationBuilder().build();
-        cacheManager = new DefaultCacheManager(configuration);
+        GlobalConfigurationBuilder gcb = new GlobalConfigurationBuilder();
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+
+        CacheMode cacheMode = getCacheMode(config.get("cacheMode", "local"), config.getBoolean("async", false));
+        if (!cacheMode.equals(CacheMode.LOCAL)) {
+            gcb.transport().defaultTransport();
+
+            int owners = config.getInt("owners", 2);
+            int segments = config.getInt("segments", 60);
+
+            cb.clustering().cacheMode(cacheMode).hash().numOwners(owners).numSegments(segments);
+        }
+
+        cacheManager = new DefaultCacheManager(gcb.build(), cb.build());
+    }
+
+    private CacheMode getCacheMode(String cacheModeString, boolean async) {
+        if (cacheModeString.equalsIgnoreCase("local")) {
+            return CacheMode.LOCAL;
+        }
+
+        if (cacheModeString.equalsIgnoreCase("replicated")) {
+            return async ? CacheMode.REPL_ASYNC : CacheMode.REPL_SYNC;
+        }
+
+        if (cacheModeString.equalsIgnoreCase("distributed")) {
+            return async ? CacheMode.DIST_ASYNC : CacheMode.DIST_SYNC;
+        }
+
+        throw new RuntimeException("Invalid cacheMode " + cacheModeString);
     }
 
     @Override
