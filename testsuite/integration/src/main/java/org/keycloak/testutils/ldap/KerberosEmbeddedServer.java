5da05aa62ac088d61bfb78b1c651e2251907746f
mposolda <mposolda@gmail.com>
2015-02-13 21:05:17 +0100

    LDAP testing improvements. Support for embedded Kerberos server in testsuite

---
+++ testsuite/integration/src/main/java/org/keycloak/testutils/ldap/KerberosEmbeddedServer.java
@@ -0,0 +1,183 @@
+package org.keycloak.testutils.ldap;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+
+import javax.security.auth.kerberos.KerberosPrincipal;
+
+import org.apache.directory.api.ldap.model.constants.SupportedSaslMechanisms;
+import org.apache.directory.api.ldap.model.exception.LdapInvalidDnException;
+import org.apache.directory.server.core.api.DirectoryService;
+import org.apache.directory.server.core.kerberos.KeyDerivationInterceptor;
+import org.apache.directory.server.kerberos.KerberosConfig;
+import org.apache.directory.server.kerberos.kdc.KdcServer;
+import org.apache.directory.server.kerberos.shared.replay.ReplayCache;
+import org.apache.directory.server.ldap.LdapServer;
+import org.apache.directory.server.ldap.handlers.sasl.cramMD5.CramMd5MechanismHandler;
+import org.apache.directory.server.ldap.handlers.sasl.digestMD5.DigestMd5MechanismHandler;
+import org.apache.directory.server.ldap.handlers.sasl.gssapi.GssapiMechanismHandler;
+import org.apache.directory.server.ldap.handlers.sasl.ntlm.NtlmMechanismHandler;
+import org.apache.directory.server.ldap.handlers.sasl.plain.PlainMechanismHandler;
+import org.apache.directory.server.protocol.shared.transport.UdpTransport;
+import org.apache.directory.shared.kerberos.KerberosTime;
+import org.jboss.logging.Logger;
+
+/**
+ * @author <a href="mailto:mposolda@redhat.com">Marek Posolda</a>
+ */
+public class KerberosEmbeddedServer extends LDAPEmbeddedServer {
+
+    private static final Logger log = Logger.getLogger(LDAPEmbeddedServer.class);
+
+    private final String kerberosRealm;
+    private final int kdcPort;
+
+    private KdcServer kdcServer;
+
+
+    public static void main(String[] args) throws Exception {
+        EmbeddedServersFactory factory = EmbeddedServersFactory.readConfiguration();
+        KerberosEmbeddedServer kerberosEmbeddedServer = factory.createKerberosServer();
+        kerberosEmbeddedServer.init();
+        kerberosEmbeddedServer.start();
+    }
+
+
+    protected KerberosEmbeddedServer(String baseDN, String bindHost, int bindPort, String ldifFile, String kerberosRealm, int kdcPort) {
+        super(baseDN, bindHost, bindPort, ldifFile);
+        this.kerberosRealm = kerberosRealm;
+        this.kdcPort = kdcPort;
+    }
+
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+
+        log.info("Creating KDC server");
+        createAndStartKdcServer();
+    }
+
+
+    @Override
+    protected DirectoryService createDirectoryService() throws Exception {
+        DirectoryService directoryService = super.createDirectoryService();
+
+        directoryService.addLast(new KeyDerivationInterceptor());
+        return directoryService;
+    }
+
+
+    @Override
+    protected LdapServer createLdapServer() {
+        LdapServer ldapServer = super.createLdapServer();
+
+        ldapServer.setSaslHost( this.bindHost );
+        ldapServer.setSaslPrincipal( "ldap/" + this.bindHost + "@" + this.kerberosRealm);
+
+        ldapServer.addSaslMechanismHandler(SupportedSaslMechanisms.PLAIN, new PlainMechanismHandler());
+        ldapServer.addSaslMechanismHandler(SupportedSaslMechanisms.CRAM_MD5, new CramMd5MechanismHandler());
+        ldapServer.addSaslMechanismHandler(SupportedSaslMechanisms.DIGEST_MD5, new DigestMd5MechanismHandler());
+        ldapServer.addSaslMechanismHandler(SupportedSaslMechanisms.GSSAPI, new GssapiMechanismHandler());
+        ldapServer.addSaslMechanismHandler(SupportedSaslMechanisms.NTLM, new NtlmMechanismHandler());
+        ldapServer.addSaslMechanismHandler(SupportedSaslMechanisms.GSS_SPNEGO, new NtlmMechanismHandler());
+
+        return ldapServer;
+    }
+
+
+    protected KdcServer createAndStartKdcServer() throws Exception {
+        KerberosConfig kdcConfig = new KerberosConfig();
+        kdcConfig.setServicePrincipal("krbtgt/" + this.kerberosRealm + "@" + this.kerberosRealm);
+        kdcConfig.setPrimaryRealm(this.kerberosRealm);
+        kdcConfig.setMaximumTicketLifetime(60000 * 1440);
+        kdcConfig.setMaximumRenewableLifetime(60000 * 10080);
+        kdcConfig.setPaEncTimestampRequired(false);
+
+        kdcServer = new NoReplayKdcServer(kdcConfig);
+        kdcServer.setSearchBaseDn(this.baseDN);
+
+        UdpTransport udp = new UdpTransport(this.bindHost, this.kdcPort);
+        kdcServer.addTransports(udp);
+
+        kdcServer.setDirectoryService(directoryService);
+
+        // Launch the server
+        kdcServer.start();
+
+        return kdcServer;
+    }
+
+
+    public void stop() throws Exception {
+        stopLdapServer();
+        stopKerberosServer();
+        shutdownDirectoryService();
+    }
+
+
+    protected void stopKerberosServer() {
+        log.info("Stoping Kerberos server.");
+        kdcServer.stop();
+    }
+
+
+    /**
+     * Replacement of apacheDS KdcServer class with disabled ticket replay cache.
+     *
+     * @author Dominik Pospisil <dpospisi@redhat.com>
+     */
+    class NoReplayKdcServer extends KdcServer {
+
+        NoReplayKdcServer(KerberosConfig kdcConfig) {
+            super(kdcConfig);
+        }
+
+        /**
+         *
+         * Dummy implementation of the ApacheDS kerberos replay cache. Essentially disables kerbores ticket replay checks.
+         * https://issues.jboss.org/browse/JBPAPP-10974
+         *
+         * @author Dominik Pospisil <dpospisi@redhat.com>
+         */
+        private class DummyReplayCache implements ReplayCache {
+
+            @Override
+            public boolean isReplay(KerberosPrincipal serverPrincipal, KerberosPrincipal clientPrincipal, KerberosTime clientTime,
+                                    int clientMicroSeconds) {
+                return false;
+            }
+
+            @Override
+            public void save(KerberosPrincipal serverPrincipal, KerberosPrincipal clientPrincipal, KerberosTime clientTime,
+                             int clientMicroSeconds) {
+                return;
+            }
+
+            @Override
+            public void clear() {
+                return;
+            }
+
+        }
+
+        /**
+         * @throws java.io.IOException if we cannot bind to the sockets
+         */
+        @Override
+        public void start() throws IOException, LdapInvalidDnException {
+            super.start();
+
+            try {
+
+                // override initialized replay cache with a dummy implementation
+                Field replayCacheField = KdcServer.class.getDeclaredField("replayCache");
+                replayCacheField.setAccessible(true);
+                replayCacheField.set(this, new DummyReplayCache());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
+}
---
+++ testsuite/integration/src/main/java/org/keycloak/testutils/ldap/LDAPEmbeddedServer.java
@@ -0,0 +1,186 @@
+package org.keycloak.testutils.ldap;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.text.StrSubstitutor;
+import org.apache.directory.api.ldap.model.entry.DefaultEntry;
+import org.apache.directory.api.ldap.model.exception.LdapEntryAlreadyExistsException;
+import org.apache.directory.api.ldap.model.ldif.LdifEntry;
+import org.apache.directory.api.ldap.model.ldif.LdifReader;
+import org.apache.directory.api.ldap.model.schema.SchemaManager;
+import org.apache.directory.server.core.api.DirectoryService;
+import org.apache.directory.server.core.api.partition.Partition;
+import org.apache.directory.server.core.factory.DSAnnotationProcessor;
+import org.apache.directory.server.core.factory.PartitionFactory;
+import org.apache.directory.server.ldap.LdapServer;
+import org.apache.directory.server.protocol.shared.transport.TcpTransport;
+import org.apache.directory.server.protocol.shared.transport.Transport;
+import org.jboss.logging.Logger;
+import org.keycloak.util.StreamUtil;
+
+/**
+ * @author <a href="mailto:mposolda@redhat.com">Marek Posolda</a>
+ */
+public class LDAPEmbeddedServer {
+
+    private static final Logger log = Logger.getLogger(LDAPEmbeddedServer.class);
+
+    protected final String baseDN;
+    protected final String bindHost;
+    protected final int bindPort;
+    protected final String ldifFile;
+
+    protected DirectoryService directoryService;
+    protected LdapServer ldapServer;
+
+
+    public static void main(String[] args) throws Exception {
+        EmbeddedServersFactory factory = EmbeddedServersFactory.readConfiguration();
+        LDAPEmbeddedServer ldapEmbeddedServer = factory.createLdapServer();
+        ldapEmbeddedServer.init();
+        ldapEmbeddedServer.start();
+    }
+
+    public LDAPEmbeddedServer(String baseDN, String bindHost, int bindPort, String ldifFile) {
+        this.baseDN = baseDN;
+        this.bindHost = bindHost;
+        this.bindPort = bindPort;
+        this.ldifFile = ldifFile;
+    }
+
+
+    public void init() throws Exception {
+        log.info("Creating LDAP Directory Service. Config: baseDN=" + baseDN + ", bindHost=" + bindHost + ", bindPort=" + bindPort);
+        this.directoryService = createDirectoryService();
+
+        log.info("Importing LDIF: " + ldifFile);
+        importLdif();
+
+        log.info("Creating LDAP Server");
+        this.ldapServer = createLdapServer();
+    }
+
+
+    public void start() throws Exception {
+        log.info("Starting LDAP Server");
+        ldapServer.start();
+        log.info("LDAP Server started");
+    }
+
+
+    protected DirectoryService createDirectoryService() throws Exception {
+        // Parse "keycloak" from "dc=keycloak,dc=org"
+        String dcName = baseDN.split(",")[0].substring(3);
+
+        InMemoryDirectoryServiceFactory dsf = new InMemoryDirectoryServiceFactory();
+
+        DirectoryService service = dsf.getDirectoryService();
+        service.setAccessControlEnabled(false);
+        service.setAllowAnonymousAccess(false);
+        service.getChangeLog().setEnabled(false);
+
+        dsf.init(dcName + "DS");
+
+        SchemaManager schemaManager = service.getSchemaManager();
+
+        PartitionFactory partitionFactory = dsf.getPartitionFactory();
+        Partition partition = partitionFactory.createPartition(
+                schemaManager,
+                service.getDnFactory(),
+                dcName,
+                this.baseDN,
+                1000,
+                new File(service.getInstanceLayout().getPartitionsDirectory(), dcName));
+        partition.setCacheService( service.getCacheService() );
+        partition.initialize();
+
+        partition.setSchemaManager( schemaManager );
+
+        // Inject the partition into the DirectoryService
+        service.addPartition( partition );
+
+        // Last, process the context entry
+        String entryLdif =
+                "dn: " + baseDN + "\n" +
+                        "dc: " + dcName + "\n" +
+                        "objectClass: top\n" +
+                        "objectClass: domain\n\n";
+        DSAnnotationProcessor.injectEntries(service, entryLdif);
+
+        return service;
+    }
+
+
+    protected LdapServer createLdapServer() {
+        LdapServer ldapServer = new LdapServer();
+
+        ldapServer.setServiceName("DefaultLdapServer");
+
+        // Read the transports
+        Transport ldap = new TcpTransport(this.bindHost, this.bindPort, 3, 50);
+        ldapServer.addTransports( ldap );
+
+        // Associate the DS to this LdapServer
+        ldapServer.setDirectoryService( directoryService );
+
+        // Propagate the anonymous flag to the DS
+        directoryService.setAllowAnonymousAccess(false);
+
+        ldapServer.setSaslHost( this.bindHost );
+        ldapServer.setSaslPrincipal( "ldap/" + this.bindHost + "@KEYCLOAK.ORG");
+        ldapServer.setSaslRealms(new ArrayList<String>());
+        return ldapServer;
+    }
+
+
+    private void importLdif() throws Exception {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("hostname", this.bindHost);
+
+        // For now, assume that LDIF file is on classpath
+        InputStream is = getClass().getClassLoader().getResourceAsStream(ldifFile);
+        if (is == null) {
+            throw new IllegalStateException("LDIF file not found on classpath. Location was: " + ldifFile);
+        }
+
+        final String ldifContent = StrSubstitutor.replace(StreamUtil.readString(is), map);
+        log.info("Importing LDIF: " + ldifContent);
+        final SchemaManager schemaManager = directoryService.getSchemaManager();
+
+        for (LdifEntry ldifEntry : new LdifReader(IOUtils.toInputStream(ldifContent))) {
+            try {
+                directoryService.getAdminSession().add(new DefaultEntry(schemaManager, ldifEntry.getEntry()));
+            } catch (LdapEntryAlreadyExistsException ignore) {
+                log.debug("Entry " + ldifEntry.getNewRdn() + " already exists. Ignoring");
+            }
+        }
+    }
+
+
+    public void stop() throws Exception {
+        stopLdapServer();
+        shutdownDirectoryService();
+    }
+
+
+    protected void stopLdapServer() {
+        log.info("Stoping LDAP server.");
+        ldapServer.stop();
+    }
+
+
+    protected void shutdownDirectoryService() throws Exception {
+        log.info("Stoping Directory service.");
+        directoryService.shutdown();
+
+        log.info("Removing Directory service workfiles.");
+        FileUtils.deleteDirectory(directoryService.getInstanceLayout().getInstanceDirectory());
+    }
+
+}
