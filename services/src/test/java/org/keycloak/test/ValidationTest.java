efe28c2eaabb3ab1430f71c4d105e4948cad4b87
Jean Merelis <jean.merelis@gmail.com>
2015-02-14 23:31:24 -0200

    [KEYCLOAK-1039] fix email validation

--- services/src/test/java/org/keycloak/test/ValidationTest.java
+++ services/src/test/java/org/keycloak/test/ValidationTest.java
@@ -12,10 +12,13 @@ public class ValidationTest {
     @Test
     public void testEmailValidation() {
         Assert.assertTrue(Validation.isEmailValid("abc@abc.cz"));
-        Assert.assertTrue(Validation.isEmailValid("435@coco.foo.cz"));
-        Assert.assertTrue(Validation.isEmailValid("A@something"));
+        Assert.assertTrue(Validation.isEmailValid("435@coco.foo.cz"));        
         Assert.assertTrue(Validation.isEmailValid("A@some-thing.foo"));
-        Assert.assertTrue(Validation.isEmailValid("1@A"));
+        Assert.assertTrue(Validation.isEmailValid("A.b@some-thing.foo"));
+        Assert.assertTrue(Validation.isEmailValid("A.b123@some-thing.foo"));
+        Assert.assertTrue(Validation.isEmailValid("A.123@some-thing.foo"));
+        Assert.assertFalse(Validation.isEmailValid("A@something"));
+        Assert.assertFalse(Validation.isEmailValid("1@A"));
         Assert.assertFalse(Validation.isEmailValid("A@some_thing.foo"));
         Assert.assertFalse(Validation.isEmailValid("@some_thing.foo"));
         Assert.assertFalse(Validation.isEmailValid("abc@"));
@@ -23,5 +26,14 @@ public class ValidationTest {
         Assert.assertFalse(Validation.isEmailValid("abc@.foo"));
         Assert.assertFalse(Validation.isEmailValid("abc@foo."));
         Assert.assertFalse(Validation.isEmailValid("abc@foo..bar"));
+        Assert.assertFalse(Validation.isEmailValid(".@foo.bar"));
+        Assert.assertFalse(Validation.isEmailValid(",@foo.bar"));
+        Assert.assertFalse(Validation.isEmailValid("!@foo.bar"));
+        Assert.assertFalse(Validation.isEmailValid("ok!@foo.bar"));
+        Assert.assertFalse(Validation.isEmailValid("ok!a@foo.bar"));
+        Assert.assertFalse(Validation.isEmailValid("someone@foo.bar.2s"));
+        Assert.assertFalse(Validation.isEmailValid(".someone@foo.bar.us"));
+        Assert.assertFalse(Validation.isEmailValid("someone,a@foo.bar.us"));
+        Assert.assertFalse(Validation.isEmailValid("someone..a@foo.bar"));
     }
 }
