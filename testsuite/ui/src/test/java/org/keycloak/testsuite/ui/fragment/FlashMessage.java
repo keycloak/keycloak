79faef80b29f9cb8e404c21b0197545cf1198437
Petr Mensik <pmensik@localhost.localdomain>
2015-02-26 16:30:21 +0100

    Added attribute waits to FlashMessage

--- testsuite/ui/src/test/java/org/keycloak/testsuite/ui/fragment/FlashMessage.java
+++ testsuite/ui/src/test/java/org/keycloak/testsuite/ui/fragment/FlashMessage.java
@@ -1,5 +1,6 @@
 package org.keycloak.testsuite.ui.fragment;
 
+import static org.jboss.arquillian.graphene.Graphene.waitGui;
 import org.jboss.arquillian.graphene.fragment.Root;
 
 import static org.keycloak.testsuite.ui.util.SeleniumUtils.waitGuiForElement;
@@ -11,14 +12,26 @@ public class FlashMessage {
     private WebElement root;
 
     public boolean isSuccess() {
-        return root.getAttribute("class").contains("success");
+		waitGui().until("Flash message should be success")
+			.element(root)
+			.attribute("class")
+			.contains("success");
+		return root.getAttribute("class").contains("success");
     }
 
     public boolean isError() {
+		waitGui().until("Flash message should be error")
+			.element(root)
+			.attribute("class")
+			.contains("error");
         return root.getAttribute("class").contains("error");
     }
 
     public boolean isDanger() {
+		waitGui().until("Flash message should be danger")
+			.element(root)
+			.attribute("class")
+			.contains("danger");
 		return root.getAttribute("class").contains("danger"); 
 	}
 
