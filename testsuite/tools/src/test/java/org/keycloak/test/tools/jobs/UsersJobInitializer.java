bd52908df4dbe5b793a34a2a31bcd122d22f8704
Stian Thorgersen <stianst@gmail.com>
2015-05-05 07:22:50 +0200

    Fix version setting

--- testsuite/tools/src/test/java/org/keycloak/test/tools/jobs/UsersJobInitializer.java
+++
@@ -1,9 +0,0 @@
-package org.keycloak.test.tools.jobs;
-
-/**
- * @author <a href="mailto:mposolda@redhat.com">Marek Posolda</a>
- */
-public interface UsersJobInitializer {
-
-    UsersJob instantiateJob();
-}
