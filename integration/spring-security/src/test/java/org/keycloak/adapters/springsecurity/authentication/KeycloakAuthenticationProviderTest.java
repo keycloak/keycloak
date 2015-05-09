78999537f0cd7013945024456542d32e8c410a39
Thomas Raehalme <thomas.raehalme@codecenter.fi>
2015-05-09 13:42:09 +0300

    Added support for GrantedAuthoritiesMapper in KeycloakAuthenticationProvider.

--- integration/spring-security/src/test/java/org/keycloak/adapters/springsecurity/authentication/KeycloakAuthenticationProviderTest.java
+++ integration/spring-security/src/test/java/org/keycloak/adapters/springsecurity/authentication/KeycloakAuthenticationProviderTest.java
@@ -8,6 +8,8 @@ import org.keycloak.adapters.springsecurity.account.SimpleKeycloakAccount;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.mockito.internal.util.collections.Sets;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 
 import java.security.Principal;
@@ -20,14 +22,12 @@ import static org.mockito.Mockito.*;
  * Keycloak authentication provider tests.
  */
 public class KeycloakAuthenticationProviderTest {
-
     private KeycloakAuthenticationProvider provider = new KeycloakAuthenticationProvider();
     private KeycloakAuthenticationToken token;
     private Set<String> roles = Sets.newSet("user", "admin");
 
     @Before
     public void setUp() throws Exception {
-
         Principal principal = mock(Principal.class);
         RefreshableKeycloakSecurityContext securityContext = mock(RefreshableKeycloakSecurityContext.class);
         KeycloakAccount account = new SimpleKeycloakAccount(principal, roles, securityContext);
@@ -39,7 +39,7 @@ public class KeycloakAuthenticationProviderTest {
     public void testAuthenticate() throws Exception {
         Authentication result = provider.authenticate(token);
         assertNotNull(result);
-        assertEquals(roles.size(), result.getAuthorities().size());
+        assertEquals(roles, AuthorityUtils.authorityListToSet(result.getAuthorities()));
         assertTrue(result.isAuthenticated());
         assertNotNull(result.getPrincipal());
         assertNotNull(result.getCredentials());
@@ -50,5 +50,17 @@ public class KeycloakAuthenticationProviderTest {
     public void testSupports() throws Exception {
         assertTrue(provider.supports(KeycloakAuthenticationToken.class));
         assertFalse(provider.supports(PreAuthenticatedAuthenticationToken.class));
+    }
+
+    @Test
+    public void testGrantedAuthoritiesMapper() throws Exception {
+        SimpleAuthorityMapper grantedAuthorityMapper = new SimpleAuthorityMapper();
+        grantedAuthorityMapper.setPrefix("ROLE_");
+        grantedAuthorityMapper.setConvertToUpperCase(true);
+        provider.setGrantedAuthoritiesMapper(grantedAuthorityMapper);
+
+        Authentication result = provider.authenticate(token);
+        assertEquals(Sets.newSet("ROLE_USER", "ROLE_ADMIN"),
+            AuthorityUtils.authorityListToSet(result.getAuthorities()));
     }
 }
