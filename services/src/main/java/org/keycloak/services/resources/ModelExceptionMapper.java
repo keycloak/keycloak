bcd607a1ee692cf82679817fc77a41dd76aaeb8d
mposolda <mposolda@gmail.com>
2015-05-28 19:19:30 +0200

    KEYCLOAK-1360 ModelExceptionMapper logges the exception

--- services/src/main/java/org/keycloak/services/resources/ModelExceptionMapper.java
+++ services/src/main/java/org/keycloak/services/resources/ModelExceptionMapper.java
@@ -4,6 +4,8 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
+
+import org.jboss.logging.Logger;
 import org.keycloak.messages.MessagesProvider;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.ModelException;
@@ -15,6 +17,8 @@ import org.keycloak.services.ErrorResponse;
 @Provider
 public class ModelExceptionMapper implements ExceptionMapper<ModelException> {
 
+    private static final Logger logger = Logger.getLogger(ModelExceptionMapper.class);
+
     @Context
     private KeycloakSession session;
 
@@ -22,6 +26,8 @@ public class ModelExceptionMapper implements ExceptionMapper<ModelException> {
     public Response toResponse(ModelException ex) {
         String message = session.getProvider(MessagesProvider.class, "admin")
                 .getMessage(ex.getMessage(), ex.getParameters());
+
+        logger.error(message, ex);
         return ErrorResponse.error(message, Response.Status.BAD_REQUEST);
     }
 }
