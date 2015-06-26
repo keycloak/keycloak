c2f2fbb811c5d717e9b78b7dfbe7b2eea47dd244
Tomas Kyjovsky <tkyjovsk@redhat.com>
2015-06-26 07:57:43 +0200

    removed unused code

--- testsuite/integration-arquillian/src/test/java/org/keycloak/testsuite/console/page/AdminConsole.java
+++ testsuite/integration-arquillian/src/test/java/org/keycloak/testsuite/console/page/AdminConsole.java
@@ -1,20 +1,12 @@
 package org.keycloak.testsuite.console.page;
 
 import javax.ws.rs.core.UriBuilder;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 
 /**
  *
  * @author tkyjovsk
  */
 public class AdminConsole extends AdminRoot {
-
-    @FindBy(xpath = "//ul[contains(@class,'navbar-utility')]//a[contains(@class,'dropdown-toggle')]")
-    private WebElement utilityNavbarDropdown;
-
-    @FindBy(linkText = "Sign Out")
-    private WebElement signOutLink;
 
     public AdminConsole() {
         setTemplateValue("consoleRealm", Realm.MASTER);
@@ -23,12 +15,6 @@ public class AdminConsole extends AdminRoot {
     @Override
     public UriBuilder createUriBuilder() {
         return super.createUriBuilder().path("{consoleRealm}/console");
-    }
-
-    public void logOut() {
-        System.out.println("Logging out.");
-        utilityNavbarDropdown.click();
-        signOutLink.click();
     }
 
 }
