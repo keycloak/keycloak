25a48a9143dde7f9feebecdba8787ca552e3835d
Tomas Kyjovsky <tkyjovsk@redhat.com>
2015-07-03 03:05:24 +0200

    Added profile app-server-as7 (doesn't fully work yet).

---
+++ testsuite/integration-arquillian/src/test/java/org/keycloak/testsuite/adapter/AS7ServletsAdapterTest.java
@@ -0,0 +1,12 @@
+package org.keycloak.testsuite.adapter;
+
+import org.keycloak.testsuite.arquillian.annotation.AppServerContainer;
+
+/**
+ *
+ * @author tkyjovsk
+ */
+@AppServerContainer("app-server-as7")
+public class AS7ServletsAdapterTest extends AbstractServletsAdapterTest {
+
+}
