ae242f84804b3dfb2c898fef29dc7ceb7daff0da
nick.grange <nicolas.grange@retrievercommunications.com>
2015-07-04 22:38:34 +1000

    KEYCLOAK-1486 fixed User link in Clients' Active Sessions page
    Changed User link in Clients' Active Sessions page to use userId
    instead of username to show User details instead of Resource not found.
    Renamed UserSessionRepresentation’s user attribute to username as it
    was ambiguous and only used in 1 place (i.e. client-sessions.html).

--- model/api/src/main/java/org/keycloak/models/utils/ModelToRepresentation.java
+++ model/api/src/main/java/org/keycloak/models/utils/ModelToRepresentation.java
@@ -230,7 +230,8 @@ public class ModelToRepresentation {
         rep.setId(session.getId());
         rep.setStart(Time.toMillis(session.getStarted()));
         rep.setLastAccess(Time.toMillis(session.getLastSessionRefresh()));
-        rep.setUser(session.getUser().getUsername());
+        rep.setUsername(session.getUser().getUsername());
+        rep.setUserId(session.getUser().getId());
         rep.setIpAddress(session.getIpAddress());
         for (ClientSessionModel clientSession : session.getClientSessions()) {
             ClientModel client = clientSession.getClient();
