97213dd20450509afd1a88df019605df70078550
Scott Rossillo <srossillo@smartling.com>
2015-07-06 15:40:55 -0400

    Log time required to remove expired user sessions

--- services/src/main/java/org/keycloak/services/scheduled/ClearExpiredUserSessions.java
+++ services/src/main/java/org/keycloak/services/scheduled/ClearExpiredUserSessions.java
@@ -1,5 +1,6 @@
 package org.keycloak.services.scheduled;
 
+import org.jboss.logging.Logger;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.UserSessionProvider;
@@ -9,12 +10,16 @@ import org.keycloak.models.UserSessionProvider;
  */
 public class ClearExpiredUserSessions implements ScheduledTask {
 
+    private static final Logger log = Logger.getLogger(ClearExpiredUserSessions.class);
+
     @Override
     public void run(KeycloakSession session) {
         UserSessionProvider sessions = session.sessions();
+        long start = System.currentTimeMillis();
         for (RealmModel realm : session.realms().getRealms()) {
             sessions.removeExpiredUserSessions(realm);
         }
+        log.warnf("Removed expired user sessions in %d ms", System.currentTimeMillis() - start);
     }
 
 }
