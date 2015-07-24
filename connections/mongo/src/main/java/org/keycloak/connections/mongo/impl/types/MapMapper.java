772ef3cd10205fb52be42bf8ee81ddb3d5316335
mposolda <mposolda@gmail.com>
2015-07-24 12:46:47 +0200

    Mongo model fix

--- connections/mongo/src/main/java/org/keycloak/connections/mongo/impl/types/MapMapper.java
+++ connections/mongo/src/main/java/org/keycloak/connections/mongo/impl/types/MapMapper.java
@@ -3,6 +3,7 @@ package org.keycloak.connections.mongo.impl.types;
 import com.mongodb.BasicDBObject;
 import org.keycloak.connections.mongo.api.types.Mapper;
 import org.keycloak.connections.mongo.api.types.MapperContext;
+import org.keycloak.connections.mongo.api.types.MapperRegistry;
 
 import java.util.Map;
 import java.util.Set;
@@ -17,30 +18,34 @@ public class MapMapper<T extends Map> implements Mapper<T, BasicDBObject> {
     // Just some dummy way of encoding . character as it's not allowed by mongo in key fields
     static final String DOT_PLACEHOLDER = "###";
 
+    private final MapperRegistry mapperRegistry;
     private final Class<T> mapType;
 
-    public MapMapper(Class<T> mapType) {
+    public MapMapper(MapperRegistry mapperRegistry, Class<T> mapType) {
+        this.mapperRegistry = mapperRegistry;
         this.mapType = mapType;
     }
 
     @Override
     public BasicDBObject convertObject(MapperContext<T, BasicDBObject> context) {
         T mapToConvert = context.getObjectToConvert();
-        return convertMap(mapToConvert);
+        return convertMap(mapToConvert, mapperRegistry);
     }
 
-    public static BasicDBObject convertMap(Map mapToConvert) {
+    public static BasicDBObject convertMap(Map mapToConvert, MapperRegistry mapperRegistry) {
         BasicDBObject dbObject = new BasicDBObject();
         Set<Map.Entry> entries = mapToConvert.entrySet();
         for (Map.Entry entry : entries) {
             String key = (String)entry.getKey();
             Object value = entry.getValue();
 
+            Object dbValue = mapperRegistry==null ? entry.getValue() : mapperRegistry.convertApplicationObjectToDBObject(value, Object.class);
+
             if (key.contains(".")) {
                 key = key.replaceAll("\\.", DOT_PLACEHOLDER);
             }
 
-            dbObject.put(key, value);
+            dbObject.put(key, dbValue);
         }
         return dbObject;
     }
