84db71d7f7c8e075d3a7e57a657bb27ea7486822
fkiss <fkiss@redhat.com>
2015-08-13 15:33:02 +0200

    fixed missing web.xml file in wars examples

---
+++ testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/adapter/example/AbstractCorsExampleAdapterTest.java
@@ -0,0 +1,77 @@
+package org.keycloak.testsuite.adapter.example;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.graphene.page.Page;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Before;
+import org.junit.Test;
+import org.keycloak.representations.idm.RealmRepresentation;
+import org.keycloak.testsuite.adapter.AbstractExampleAdapterTest;
+import org.keycloak.testsuite.adapter.page.AngularCorsProductExample;
+import org.keycloak.testsuite.auth.page.account.Account;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import static org.keycloak.testsuite.util.IOUtil.loadRealm;
+import static org.keycloak.testsuite.util.PageAssert.assertCurrentUrlStartsWith;
+
+/**
+ * Created by fkiss.
+ */
+public class AbstractCorsExampleAdapterTest extends AbstractExampleAdapterTest {
+
+    public static final String CORS = "cors";
+
+    @Page
+    private AngularCorsProductExample angularCorsProductExample;
+
+    @Page
+    private Account testRealmAccount;
+
+    @Deployment(name = AngularCorsProductExample.DEPLOYMENT_NAME)
+    private static WebArchive angularCorsProductExample() throws IOException {
+        return exampleDeployment(AngularCorsProductExample.DEPLOYMENT_NAME, "angular-cors-product");
+    }
+
+    /*@Deployment(name = CorsDatabaseServiceExample.DEPLOYMENT_NAME)
+    private static WebArchive corsDatabaseServiceExample() throws IOException {
+        return exampleDeployment(CorsDatabaseServiceExample.DEPLOYMENT_NAME);
+    }*/
+
+    @Override
+    public void addAdapterTestRealms(List<RealmRepresentation> testRealms) {
+        testRealms.add(
+                loadRealm(new File(EXAMPLES_HOME_DIR + "/cors/cors-realm.json")));
+    }
+
+    @Override
+    public void setDefaultPageUriParameters() {
+        super.setDefaultPageUriParameters();
+        testRealm.setAuthRealm(CORS);
+        testRealmLogin.setAuthRealm(CORS);
+        testRealmAccount.setAuthRealm(CORS);
+    }
+
+    @Before
+    public void beforeDemoExampleTest() {
+        angularCorsProductExample.navigateTo();
+        //driver.navigate().to("http://localhost:8180/angular-cors-product/index.html");
+        driver.manage().deleteAllCookies();
+    }
+
+    @Test
+    public void angularCorsProductTest() {
+
+        try {
+            Thread.sleep(500000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        angularCorsProductExample.navigateTo();
+        testRealmLogin.form().login("bburke@redhat.com", "password");
+
+        assertCurrentUrlStartsWith(angularCorsProductExample);
+    }
+}
