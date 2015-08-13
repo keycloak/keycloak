523bdd5ec9a802aed3a6c54aae3598ba316e37ce
Bill Burke <bburke@redhat.com>
2015-08-12 20:37:51 -0400

    doco

--- examples/providers/authenticator/src/main/java/org/keycloak/examples/authenticator/SecretQuestionAuthenticator.java
+++ examples/providers/authenticator/src/main/java/org/keycloak/examples/authenticator/SecretQuestionAuthenticator.java
@@ -3,12 +3,15 @@ package org.keycloak.examples.authenticator;
 import org.keycloak.authentication.AuthenticationFlowContext;
 import org.keycloak.authentication.AuthenticationFlowError;
 import org.keycloak.authentication.AbstractFormAuthenticator;
+import org.keycloak.models.AuthenticatorConfigModel;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.UserCredentialValueModel;
 import org.keycloak.models.UserModel;
 import org.keycloak.models.utils.CredentialValidation;
+import org.keycloak.services.util.CookieHelper;
 
+import javax.ws.rs.core.Cookie;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 
@@ -20,21 +23,52 @@ public class SecretQuestionAuthenticator extends AbstractFormAuthenticator {
 
     public static final String CREDENTIAL_TYPE = "secret_question";
 
+    protected boolean hasCookie(AuthenticationFlowContext context) {
+        Cookie cookie = context.getHttpRequest().getHttpHeaders().getCookies().get("SECRET_QUESTION_ANSWERED");
+        return cookie != null;
+    }
+
     @Override
     public void authenticate(AuthenticationFlowContext context) {
+        if (hasCookie(context)) {
+            context.success();
+            return;
+        }
         Response challenge = loginForm(context).createForm("secret_question.ftl");
         context.challenge(challenge);
     }
 
     @Override
     public void action(AuthenticationFlowContext context) {
-        MultivaluedMap<String, String> formData = context.getHttpRequest().getDecodedFormParameters();
-        String secret = formData.getFirst("secret");
-        if (secret == null || secret.trim().equals("")) {
-            badSecret(context);
+        boolean validated = validateAnswer(context);
+        if (!validated) {
+            Response challenge =  loginForm(context)
+                    .setError("badSecret")
+                    .createForm("secret_question.ftl");
+            context.failureChallenge(AuthenticationFlowError.INVALID_CREDENTIALS, challenge);
             return;
         }
+        setCookie(context);
+        context.success();
+    }
 
+    protected void setCookie(AuthenticationFlowContext context) {
+        AuthenticatorConfigModel config = context.getAuthenticatorConfig();
+        int maxCookieAge = 60 * 60 * 24 * 30; // 30 days
+        if (config != null) {
+            maxCookieAge = Integer.valueOf(config.getConfig().get("cookie.max.age"));
+
+        }
+        CookieHelper.addCookie("SECRET_QUESTION_ANSWERED", "true",
+                context.getUriInfo().getBaseUri().getPath() + "/realms/" + context.getRealm().getName(),
+                null, null,
+                maxCookieAge,
+                true, true);
+    }
+
+    protected boolean validateAnswer(AuthenticationFlowContext context) {
+        MultivaluedMap<String, String> formData = context.getHttpRequest().getDecodedFormParameters();
+        String secret = formData.getFirst("secret");
         UserCredentialValueModel cred = null;
         for (UserCredentialValueModel model : context.getUser().getCredentialsDirectly()) {
             if (model.getType().equals(CREDENTIAL_TYPE)) {
@@ -42,25 +76,8 @@ public class SecretQuestionAuthenticator extends AbstractFormAuthenticator {
                 break;
             }
         }
-        if (cred == null) {
-            badSecret(context);
-            return;
-        }
 
-        boolean validated = CredentialValidation.validateHashedCredential(context.getRealm(), context.getUser(), secret, cred);
-        if (!validated) {
-            badSecret(context);
-            return;
-        }
-
-        context.success();
-    }
-
-    private void badSecret(AuthenticationFlowContext context) {
-        Response challenge =  loginForm(context)
-                .setError("badSecret")
-                .createForm("secret_question.ftl");
-        context.failureChallenge(AuthenticationFlowError.INVALID_CREDENTIALS, challenge);
+        return CredentialValidation.validateHashedCredential(context.getRealm(), context.getUser(), secret, cred);
     }
 
     @Override
