253197a170b365e3fdbdb1b0d82e48b3850595d5
Lukas Kubik <lkubik@redhat.com>
2015-08-25 17:18:45 +0200

    Add RCUE and PatternFly components

---
+++ forms/common-themes/src/main/resources/theme/keycloak/common/resources/lib/components/patternfly/components/bootstrap-datepicker/js/locales/bootstrap-datepicker.nl.js
@@ -0,0 +1,17 @@
+/**
+ * Dutch translation for bootstrap-datepicker
+ * Reinier Goltstein <mrgoltstein@gmail.com>
+ */
+;(function($){
+	$.fn.datepicker.dates['nl'] = {
+		days: ["zondag", "maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag"],
+		daysShort: ["zo", "ma", "di", "wo", "do", "vr", "za", "zo"],
+		daysMin: ["zo", "ma", "di", "wo", "do", "vr", "za", "zo"],
+		months: ["januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober", "november", "december"],
+		monthsShort: ["jan", "feb", "mrt", "apr", "mei", "jun", "jul", "aug", "sep", "okt", "nov", "dec"],
+		today: "Vandaag",
+		clear: "Wissen",
+		weekStart: 1,
+		format: "dd-mm-yyyy"
+	};
+}(jQuery));
