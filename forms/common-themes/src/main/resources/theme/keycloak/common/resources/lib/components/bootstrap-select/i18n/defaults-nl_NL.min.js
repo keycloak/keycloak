2938759db437678e39027ceea32c74a96cef0762
Stian Thorgersen <stianst@gmail.com>
2015-09-24 08:37:01 +0200

    Removed unused files from common/lib theme

--- forms/common-themes/src/main/resources/theme/keycloak/common/resources/lib/components/bootstrap-select/i18n/defaults-nl_NL.min.js
+++
@@ -1,7 +0,0 @@
-/*
- * Translated default messages for bootstrap-select.
- * Locale: NL (Dutch; Nederlands)
- * Region: EU (Europe)
- * Author: Daan Rosbergen (Badmuts)
- */
- (function(a){a.fn.selectpicker.defaults={style:"btn-default",size:"auto",title:null,selectedTextFormat:"values",noneSelectedText:"Niets geselecteerd",noneResultsText:"Geen resultaten gevonden voor",countSelectedText:"{0} van {1} geselecteerd",maxOptionsText:["Limiet bereikt ({n} {var} max)","Groep limiet bereikt ({n} {var} max)",["items","item"]],width:false,container:false,hideDisabled:false,showSubtext:false,showIcon:true,showContent:true,dropupAuto:true,header:false,liveSearch:false,actionsBox:false,multipleSeparator:", ",iconBase:"glyphicon",tickIcon:"glyphicon-ok",maxOptions:false}}(jQuery));
--- forms/common-themes/src/main/resources/theme/keycloak/common/resources/lib/components/bootstrap-select/i18n/defaults-pl_PL.min.js
+++
@@ -1,6 +0,0 @@
-/*
- * Translated default messages for bootstrap-select.
- * Locale: PL (Polish)
- * Region: EU (Europe)
- */
-(function(e){e.fn.selectpicker.defaults={style:"btn-default",size:"auto",title:null,selectedTextFormat:"values",noneSelectedText:"Nic nie zaznaczono",noneResultsText:"Brak wyników wyszukiwania",countSelectedText:"Zaznaczono {0} z {1}",maxOptionsText:["Osiągnięto limit ({n} {var} max)","Limit grupy osiągnięty ({n} {var} max)",["elementy","element"]],width:false,container:false,hideDisabled:false,showSubtext:false,showIcon:true,showContent:true,dropupAuto:true,header:false,liveSearch:false,multipleSeparator:", ",iconBase:"glyphicon",tickIcon:"glyphicon-ok"}})(jQuery)
