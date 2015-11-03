267be838639241c907fe1f58f1afbd3dcc11b33f
vramik <vramik@localhost.localdomain>
2015-11-03 14:27:16 +0100

    arquillian-testsuite: add UI test for OTP Policy
    Conflicts:
    	testsuite/integration-arquillian/tests/base/src/main/java/org/keycloak/testsuite/console/page/authentication/flows/CreateFlow.java

--- testsuite/integration-arquillian/tests/base/src/main/java/org/keycloak/testsuite/console/page/authentication/Authentication.java
+++ testsuite/integration-arquillian/tests/base/src/main/java/org/keycloak/testsuite/console/page/authentication/Authentication.java
@@ -3,6 +3,7 @@ package org.keycloak.testsuite.console.page.authentication;
 import org.keycloak.testsuite.console.page.AdminConsoleRealm;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import static org.keycloak.testsuite.util.WaitUtils.waitAjaxForElement;
 
 /**
  * @author tkyjovsk
@@ -13,6 +14,22 @@ public class Authentication extends AdminConsoleRealm {
     @FindBy(xpath = "//h1[text()='Authentication']/..")
     private AuthenticationTabs authenticationTabs;
 
+    @FindBy(xpath = "//div[contains(@class, 'alert-danger')]")
+    private WebElement error;
+    
+    @FindBy(xpath = "//div[contains(@class, 'alert-success')]")
+    private WebElement success;
+    
+    public String getSuccessMessage() {
+        waitAjaxForElement(success);
+        return success.getText();
+    }
+    
+    public String getErrorMessage() {
+        waitAjaxForElement(error);
+        return error.getText();
+    }
+    
     public AuthenticationTabs tabs() {
         return authenticationTabs;
     }
