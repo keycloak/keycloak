49e9965125763c3b78b2d10fc9dccff9681eb3d0
mhajas <mhajas@redhat.com>
2015-12-10 15:26:33 +0100

    Fix timeouts after logout from servlet

--- testsuite/integration-arquillian/tests/base/src/main/java/org/keycloak/testsuite/adapter/page/SAMLServletWithLogout.java
+++ testsuite/integration-arquillian/tests/base/src/main/java/org/keycloak/testsuite/adapter/page/SAMLServletWithLogout.java
@@ -2,6 +2,8 @@ package org.keycloak.testsuite.adapter.page;
 
 import org.keycloak.testsuite.page.AbstractPageWithInjectedUrl;
 
+import static org.keycloak.testsuite.util.WaitUtils.pause;
+
 /**
  * @author mhajas
  */
@@ -9,6 +11,7 @@ public abstract class SAMLServletWithLogout extends AbstractPageWithInjectedUrl 
 
     public void logout() {
         driver.navigate().to(getUriBuilder().queryParam("GLO", "true").build().toASCIIString());
-        getUriBuilder().replaceQueryParam("GLO",null);
+        getUriBuilder().replaceQueryParam("GLO", null);
+        pause(300);
     }
 }
