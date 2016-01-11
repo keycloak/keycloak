3ccc97db7462b53657c6ea09e85a3fdebb89a438
Lukas Kubik <lkubik@redhat.com>
2016-01-11 15:01:33 +0100

    Apply branding

---
+++ forms/common-themes/src/main/resources/theme/rh-sso/common/resources/lib/components/patternfly/components/bootstrap-datepicker/js/locales/bootstrap-datepicker.eu.js
@@ -0,0 +1,15 @@
+/**
+ * Basque translation for bootstrap-datepicker
+ * Arkaitz Etxeberria <kondi80@gmail.com>
+ */
+;(function($){
+    $.fn.datepicker.dates['eu'] = {
+        days: ['Igandea','Astelehena','Asteartea','Asteazkena','Osteguna','Ostirala','Larunbata','Igandea'],
+        daysShort: ['Ig','Al','Ar','Az','Og','Ol','Lr', 'Ig'],
+        daysMin: ['Ig','Al','Ar','Az','Og','Ol','Lr', 'Ig'],
+        months: ['Urtarrila','Otsaila','Martxoa','Apirila','Maiatza','Ekaina','Uztaila','Abuztua','Iraila','Urria','Azaroa','Abendua'],
+        monthsShort: ['Urt','Ots','Mar','Api','Mai','Eka','Uzt','Abu','Ira','Urr','Aza','Abe'],
+        today: "Gaur"
+    };
+}(jQuery));
+
