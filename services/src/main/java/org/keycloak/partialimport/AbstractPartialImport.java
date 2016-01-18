d2ad1808a53b8480e3a3af099d6fc3ae8446da74
Stan Silvert <ssilvert@redhat.com>
2016-01-18 16:18:59 -0500

    KEYCLOAK-1280: i18n logging for org.keycloak.partialimport

--- services/src/main/java/org/keycloak/partialimport/AbstractPartialImport.java
+++ services/src/main/java/org/keycloak/partialimport/AbstractPartialImport.java
@@ -21,11 +21,11 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import javax.ws.rs.core.Response;
-import org.jboss.logging.Logger;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.representations.idm.PartialImportRepresentation;
 import org.keycloak.services.ErrorResponse;
+import org.keycloak.services.ServicesLogger;
 
 /**
  * Base PartialImport for most resource types.
@@ -33,7 +33,7 @@ import org.keycloak.services.ErrorResponse;
  * @author Stan Silvert ssilvert@redhat.com (C) 2016 Red Hat Inc.
  */
 public abstract class AbstractPartialImport<T> implements PartialImport<T> {
-    protected static Logger logger = Logger.getLogger(AbstractPartialImport.class);
+    protected static ServicesLogger logger = ServicesLogger.ROOT_LOGGER;
 
     protected final Set<T> toOverwrite = new HashSet<>();
     protected final Set<T> toSkip = new HashSet<>();
@@ -99,7 +99,7 @@ public abstract class AbstractPartialImport<T> implements PartialImport<T> {
             try {
                 create(realm, session, resourceRep);
             } catch (Exception e) {
-                logger.error("Error overwriting " + getName(resourceRep), e);
+                logger.overwriteError(e, getName(resourceRep));
                 throw new ErrorResponseException(ErrorResponse.error(e.getMessage(), Response.Status.INTERNAL_SERVER_ERROR));
             }
 
@@ -121,7 +121,7 @@ public abstract class AbstractPartialImport<T> implements PartialImport<T> {
                 String modelId = getModelId(realm, session, resourceRep);
                 results.addResult(added(modelId, resourceRep));
             } catch (Exception e) {
-                logger.error("Error creating " + getName(resourceRep), e);
+                logger.creationError(e, getName(resourceRep));
                 throw new ErrorResponseException(ErrorResponse.error(e.getMessage(), Response.Status.INTERNAL_SERVER_ERROR));
             }
         }
