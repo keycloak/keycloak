ef8bc9a4f638f96b4ebf11c8329f537fcfac7568
Stan Silvert <ssilvert@redhat.com>
2016-01-19 08:39:53 -0500

    KEYCLOAK-1280: i18n logging for org.keycloak.services

--- services/src/main/java/org/keycloak/services/DefaultKeycloakSession.java
+++ services/src/main/java/org/keycloak/services/DefaultKeycloakSession.java
@@ -1,6 +1,21 @@
+/*
+ * Copyright 2016 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @author tags. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.keycloak.services;
 
-import org.jboss.logging.Logger;
 import org.jboss.resteasy.spi.HttpRequest;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
 import org.keycloak.models.KeycloakContext;
@@ -39,8 +54,6 @@ public class DefaultKeycloakSession implements KeycloakSession {
     private UserSessionProvider sessionProvider;
     private UserFederationManager federationManager;
     private KeycloakContext context;
-    
-    private static final Logger logger = Logger.getLogger(DefaultKeycloakSession.class);
 
     public DefaultKeycloakSession(DefaultKeycloakSessionFactory factory) {
         this.factory = factory;
@@ -112,7 +125,7 @@ public class DefaultKeycloakSession implements KeycloakSession {
         T provider = (T) providers.get(hash);
         if (provider == null) {
             ProviderFactory<T> providerFactory = factory.getProviderFactory(clazz, id);
-          
+
             if (providerFactory != null) {
                 provider = providerFactory.create(this);
                 providers.put(hash, provider);
