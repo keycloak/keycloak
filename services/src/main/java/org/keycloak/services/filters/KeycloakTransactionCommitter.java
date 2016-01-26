7a9c8a7daa346a7eea6e798ea7b8f93c44c95df5
Stian Thorgersen <stianst@gmail.com>
2016-01-26 13:55:39 +0100

    KEYCLOAK-2327 Make sure transaction is only committed for success

--- services/src/main/java/org/keycloak/services/filters/KeycloakTransactionCommitter.java
+++ services/src/main/java/org/keycloak/services/filters/KeycloakTransactionCommitter.java
@@ -3,6 +3,7 @@
 
 package org.keycloak.services.filters;
 
+import org.jboss.logging.Logger;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
 import org.keycloak.models.KeycloakTransaction;
 
@@ -16,12 +17,20 @@ import java.io.IOException;
  */
 public class KeycloakTransactionCommitter implements ContainerResponseFilter {
 
+    private static final Logger log = Logger.getLogger(KeycloakTransactionCommitter.class);
+
     @Override
     public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext containerResponseContext) throws IOException {
         KeycloakTransaction tx = ResteasyProviderFactory.getContextData(KeycloakTransaction.class);
+
         if (tx != null && tx.isActive()) {
-            if (tx.getRollbackOnly()) tx.rollback();
-            else tx.commit();
+            if (tx.getRollbackOnly() || containerResponseContext.getStatus() >= 400) {
+                log.debugv("Rollback transaction, rollback = {0}, status = {1}", tx.getRollbackOnly(), containerResponseContext.getStatus());
+                tx.rollback();
+            } else {
+                log.debugv("Committing transaction, status = {0}", containerResponseContext.getStatus());
+                tx.commit();
+            }
         }
     }
 
