fa3c0bb0aaac622015a54bb84a9690cbc30ac4c5
Tomas Kyjovsky <tkyjovsk@redhat.com>
2016-02-15 23:58:50 +0100

    KEYCLOAK-1678 generalized cluster tests for any number of backend nodes

--- testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/cluster/EntityInvalidationClusterTest.java
+++ testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/cluster/EntityInvalidationClusterTest.java
@@ -5,79 +5,17 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import org.junit.Test;
 import org.keycloak.representations.idm.RealmRepresentation;
+import org.keycloak.testsuite.arquillian.ContainerInfo;
 import static org.keycloak.testsuite.auth.page.AuthRealm.TEST;
 
 /**
  *
  * @author tkyjovsk
  */
-public class EntityInvalidationClusterTest extends AbstractTwoNodeClusterTest {
+public class EntityInvalidationClusterTest extends AbstractClusterTest {
 
     @Override
     public void addTestRealms(List<RealmRepresentation> testRealms) {
-    }
-
-    @Test
-    public void realmCRUDWithoutFailover() {
-        realmCRUD(TEST + "_wofo", false);
-    }
-
-    @Test
-    public void realmCRUDWithFailover() {
-        realmCRUD(TEST + "_wfo", true);
-    }
-
-    public void realmCRUD(String realm, boolean containerFailover) {
-        RealmRepresentation testRealm = new RealmRepresentation();
-        testRealm.setRealm(realm);
-        testRealm.setEnabled(true);
-
-        // CREATE on node1
-        backend1AdminClient().realms().create(testRealm);
-
-        // check if created on node1
-        RealmRepresentation testRealmOnBackend1 = backend1AdminClient().realms().realm(realm).toRepresentation();
-        assertEquals(testRealmOnBackend1.getRealm(), testRealm.getRealm());
-        if (containerFailover) {
-            killBackend1();
-        }
-
-        // check if created on node2
-        RealmRepresentation testRealmOnBackend2 = backend2AdminClient().realms().realm(realm).toRepresentation();
-        assertEquals(testRealmOnBackend1.getId(), testRealmOnBackend2.getId());
-        assertEquals(testRealmOnBackend1.getRealm(), testRealmOnBackend2.getRealm());
-
-        failback();
-
-        // UPDATE on node2
-        String realmUpdated = realm + "_updated";
-        testRealmOnBackend2.setRealm(realmUpdated);
-        backend2AdminClient().realms().realm(realm).update(testRealmOnBackend2);
-        if (containerFailover) {
-            killBackend2();
-        }
-        // check if updated on node1
-        testRealmOnBackend1 = backend1AdminClient().realms().realm(realmUpdated).toRepresentation();
-        assertEquals(testRealmOnBackend1.getId(), testRealmOnBackend2.getId());
-        assertEquals(testRealmOnBackend1.getRealm(), testRealmOnBackend2.getRealm());
-
-        failback();
-
-        // DELETE on node1
-        backend1AdminClient().realms().realm(realmUpdated).remove();
-        if (containerFailover) {
-            killBackend1();
-        }
-        // check if deleted on node2
-        boolean testRealmOnBackend2Exists = false;
-        for (RealmRepresentation realmOnBackend2 : backend2AdminClient().realms().findAll()) {
-            if (realmUpdated.equals(realmOnBackend2.getRealm())
-                    || testRealmOnBackend1.getId().equals(realmOnBackend2.getId())) {
-                testRealmOnBackend2Exists = true;
-                break;
-            }
-        }
-        assertFalse(testRealmOnBackend2Exists);
     }
 
     @Test
@@ -95,15 +33,102 @@ public class EntityInvalidationClusterTest extends AbstractTwoNodeClusterTest {
         RealmRepresentation testRealmOnFrontend = adminClient.realms().realm(realm).toRepresentation();
         assertEquals(testRealmOnFrontend.getRealm(), testRealm.getRealm());
 
-        // check if created on node1
-        RealmRepresentation testRealmOnBackend1 = backend1AdminClient().realms().realm(realm).toRepresentation();
-        assertEquals(testRealmOnBackend1.getId(), testRealmOnFrontend.getId());
-        assertEquals(testRealmOnBackend1.getRealm(), testRealmOnFrontend.getRealm());
-
-        // check if created on node2
-        RealmRepresentation testRealmOnBackend2 = backend2AdminClient().realms().realm(realm).toRepresentation();
-        assertEquals(testRealmOnBackend2.getId(), testRealmOnFrontend.getId());
-        assertEquals(testRealmOnBackend2.getRealm(), testRealmOnFrontend.getRealm());
+        // check if created on backend nodes
+        for (ContainerInfo backend : suiteContext.getAuthServerBackendsInfo()) {
+            RealmRepresentation testRealmOnBackend = getAdminClientFor(backend).realms().realm(realm).toRepresentation();
+            assertEquals(testRealmOnBackend.getId(), testRealmOnFrontend.getId());
+            assertEquals(testRealmOnBackend.getRealm(), testRealmOnFrontend.getRealm());
+        }
     }
-    
+
+    @Test
+    public void realmCRUDWithoutFailover() {
+        realmCRUD(TEST + "_wofo", false);
+    }
+
+    @Test
+    public void realmCRUDWithFailover() {
+        realmCRUD(TEST + "_wfo", true);
+    }
+
+    public void realmCRUD(String realmName, boolean backendFailover) {
+
+        // CREATE on current fail node
+        log.info("Creating realm on : " + getCurrentFailNode());
+        RealmRepresentation testRealm = new RealmRepresentation();
+        testRealm.setRealm(realmName);
+        testRealm.setEnabled(true);
+        getAdminClientFor(getCurrentFailNode()).realms().create(testRealm);
+
+        // check if created on fail node
+        RealmRepresentation testRealmOnFailNode = getAdminClientFor(getCurrentFailNode()).realms().realm(realmName).toRepresentation();
+        assertEquals(testRealmOnFailNode.getRealm(), testRealm.getRealm());
+
+        if (backendFailover) {
+            failure();
+        }
+
+        // check if created on survivor nodes
+        for (ContainerInfo survivorNode : getCurrentSurvivorNodes()) {
+            RealmRepresentation testRealmOnSurvivorNode = getAdminClientFor(survivorNode).realms().realm(realmName).toRepresentation();
+            assertEquals(testRealmOnFailNode.getId(), testRealmOnSurvivorNode.getId());
+            assertEquals(testRealmOnFailNode.getRealm(), testRealmOnSurvivorNode.getRealm());
+            log.info("Creation on survivor: " + survivorNode + " verified");
+        }
+
+        failback();
+        iterateCurrentFailNode();
+
+        // UPDATE on current fail node
+        log.info("Updating realm on: " + getCurrentFailNode());
+        String realmBeforeUpdate = realmName;
+        realmName += "_updated";
+        testRealm = testRealmOnFailNode;
+        testRealm.setRealm(realmName);
+        getAdminClientFor(getCurrentFailNode()).realms().realm(realmBeforeUpdate).update(testRealm);
+
+        // check if updated on fail node
+        testRealmOnFailNode = getAdminClientFor(getCurrentFailNode()).realms().realm(realmName).toRepresentation();
+        assertEquals(testRealmOnFailNode.getRealm(), testRealm.getRealm());
+
+        if (backendFailover) {
+            failure();
+        }
+
+        // check if updated on survivor nodes
+        for (ContainerInfo survivorNode : getCurrentSurvivorNodes()) {
+            RealmRepresentation testRealmOnSurvivorNode = getAdminClientFor(survivorNode).realms().realm(realmName).toRepresentation();
+            assertEquals(testRealmOnFailNode.getId(), testRealmOnSurvivorNode.getId());
+            assertEquals(testRealmOnFailNode.getRealm(), testRealmOnSurvivorNode.getRealm());
+            log.info("Update on survivor: " + survivorNode + " verified");
+        }
+
+        failback();
+        iterateCurrentFailNode();
+
+        // DELETE on current fail node
+        log.info("Deleting realm on: " + getCurrentFailNode());
+        getAdminClientFor(getCurrentFailNode()).realms().realm(realmName).remove();
+
+        if (backendFailover) {
+            failure();
+        }
+
+        // check if deleted from all survivor nodes
+        boolean realmStillExists = false;
+        for (ContainerInfo survivorNode : getCurrentSurvivorNodes()) {
+            boolean realmStillExistsOnSurvivor = false;
+            for (RealmRepresentation realmOnSurvivorNode : getAdminClientFor(survivorNode).realms().findAll()) {
+                if (realmName.equals(realmOnSurvivorNode.getRealm())
+                        || testRealmOnFailNode.getId().equals(realmOnSurvivorNode.getId())) {
+                    realmStillExistsOnSurvivor = true;
+                    realmStillExists = true;
+                    break;
+                }
+            }
+            log.error("Deletion on survivor: " + survivorNode + (realmStillExistsOnSurvivor ? " FAILED" : " verified"));
+        }
+        assertFalse(realmStillExists);
+    }
+
 }
