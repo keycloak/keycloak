8bf6f1d22dd4a9478e64b0fcbde12aeab896fec7
Tomas Kyjovsky <tkyjovsk@redhat.com>
2016-03-11 21:29:08 +0100

    KEYCLOAK-2641 updated install-adapters.bat

--- testsuite/integration-arquillian/servers/app-server/jboss/common/install-adapters.bat
+++ testsuite/integration-arquillian/servers/app-server/jboss/common/install-adapters.bat
@@ -7,11 +7,16 @@ timeout /t 10
 set ERROR=0
 
 call %JBOSS_HOME%\bin\jboss-cli.bat -c --command=":read-attribute(name=server-state)" | findstr "running"
-if %ERRORLEVEL% neq 0 set ERROR=%ERRORLEVEL%
-call %JBOSS_HOME%\bin\jboss-cli.bat -c --file="%JBOSS_HOME%\bin\adapter-install.cli"
-if %ERRORLEVEL% neq 0 set ERROR=%ERRORLEVEL%
-call %JBOSS_HOME%\bin\jboss-cli.bat -c --file="%JBOSS_HOME%\bin\adapter-install-saml.cli"
-if %ERRORLEVEL% neq 0 set ERROR=%ERRORLEVEL%
+if %ERRORLEVEL% neq 0 (
+    set ERROR=%ERRORLEVEL%
+) else (
+    call %JBOSS_HOME%\bin\jboss-cli.bat -c --file="%JBOSS_HOME%\bin\adapter-install.cli"
+    if %ERRORLEVEL% neq 0 set ERROR=%ERRORLEVEL%
+    if "%SAML_SUPPORTED" == "true" (
+        call %JBOSS_HOME%\bin\jboss-cli.bat -c --file="%JBOSS_HOME%\bin\adapter-install-saml.cli"
+        if %ERRORLEVEL% neq 0 set ERROR=%ERRORLEVEL%
+    )
+)
 call %JBOSS_HOME%\bin\jboss-cli.bat -c --command=":shutdown"
 
 if %ERROR% neq 0 exit /b %ERROR%
