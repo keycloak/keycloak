32e911250b9bd20e1e8ab62c35b7b7acb28a29dc
Tomas Kyjovsky <tkyjovsk@redhat.com>
2016-03-11 21:04:22 +0100

    KEYCLOAK-2641 App server configuration in 'servers' module.

---
+++ testsuite/integration-arquillian/servers/app-server/jboss/common/install-adapters.bat
@@ -0,0 +1,19 @@
+set NOPAUSE=true
+
+start /b cmd /c %JBOSS_HOME%\bin\standalone.bat
+
+timeout /t 10
+
+set ERROR=0
+
+call %JBOSS_HOME%\bin\jboss-cli.bat -c --command=":read-attribute(name=server-state)" | findstr "running"
+if %ERRORLEVEL% neq 0 set ERROR=%ERRORLEVEL%
+call %JBOSS_HOME%\bin\jboss-cli.bat -c --file="%JBOSS_HOME%\bin\adapter-install.cli"
+if %ERRORLEVEL% neq 0 set ERROR=%ERRORLEVEL%
+call %JBOSS_HOME%\bin\jboss-cli.bat -c --file="%JBOSS_HOME%\bin\adapter-install-saml.cli"
+if %ERRORLEVEL% neq 0 set ERROR=%ERRORLEVEL%
+call %JBOSS_HOME%\bin\jboss-cli.bat -c --command=":shutdown"
+
+if %ERROR% neq 0 exit /b %ERROR%
+
+goto:eof
