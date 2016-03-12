5778c582da8bf9ca79da20bac7eb4c73748f620c
Tomas Kyjovsky <tkyjovsk@redhat.com>
2016-03-12 19:20:57 +0100

    KEYCLOAK-2641 attempt fix install-features.bat

--- testsuite/integration-arquillian/servers/app-server/karaf/common/install-features.bat
+++ testsuite/integration-arquillian/servers/app-server/karaf/common/install-features.bat
@@ -1,9 +1,9 @@
 set NOPAUSE=true
 echo "JAVA_HOME=%JAVA_HOME%"
 
-start.bat
+start "Karaf" /b cmd /c start.bat
 echo "Karaf container starting"
-timout /t 5
+ping 127.0.0.1 -n 5 > nul
 
 set ERROR=0
 set TIMEOUT=10
@@ -13,7 +13,7 @@ set I=0
 call client.bat %CLIENT_AUTH% info
 if %ERRORLEVEL% equ 0 goto install_features
 echo "Server is not reachable. Waiting."
-timeout /t 1
+ping 127.0.0.1 -n 2 > nul
 set /a I=%I%+1
 if %I% gtr %TIMEOUT% (
     set ERROR=1
@@ -34,5 +34,5 @@ if %ERRORLEVEL% neq 0 set ERROR=%ERRORLEVEL%
 
 :shutdown_karaf
 call stop.bat
-timeout /t 5
+ping 127.0.0.1 -n 5 > nul
 exit /b %ERROR%
