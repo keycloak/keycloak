8beff63dc0ab8880fecc2ef761a29f5f4f84b9b5
Bill Burke <bburke@redhat.com>
2016-04-04 21:12:55 -0400

    KEYCLOAK-2669

--- model/infinispan/src/main/java/org/keycloak/models/cache/infinispan/stream/HasRolePredicate.java
+++ model/infinispan/src/main/java/org/keycloak/models/cache/infinispan/stream/HasRolePredicate.java
@@ -1,5 +1,6 @@
 package org.keycloak.models.cache.infinispan.stream;
 
+import org.keycloak.models.cache.infinispan.entities.CachedClient;
 import org.keycloak.models.cache.infinispan.entities.CachedGroup;
 import org.keycloak.models.cache.infinispan.entities.CachedRole;
 import org.keycloak.models.cache.infinispan.entities.Revisioned;
@@ -40,6 +41,11 @@ public class HasRolePredicate implements Predicate<Map.Entry<String, Revisioned>
         if (value instanceof RoleQuery) {
             RoleQuery roleQuery = (RoleQuery)value;
             if (roleQuery.getRoles().contains(role)) return true;
+        }
+        if (value instanceof CachedClient) {
+            CachedClient cachedClient = (CachedClient)value;
+            if (cachedClient.getScope().contains(role)) return true;
+
         }
         return false;
     }
