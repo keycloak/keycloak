4a285bae9bf1fa43f5534f2905539367c479b191
Stian Thorgersen <stianst@gmail.com>
2016-04-06 07:33:43 +0200

    KEYCLOAK-2748 Mark modules as private

--- model/jpa/src/main/java/org/keycloak/connections/jpa/util/JpaUtils.java
+++ model/jpa/src/main/java/org/keycloak/connections/jpa/util/JpaUtils.java
@@ -17,7 +17,16 @@
 
 package org.keycloak.connections.jpa.util;
 
+import org.hibernate.boot.registry.classloading.internal.ClassLoaderServiceImpl;
+import org.hibernate.jpa.boot.internal.ParsedPersistenceXmlDescriptor;
+import org.hibernate.jpa.boot.internal.PersistenceXmlParser;
+import org.hibernate.jpa.boot.spi.Bootstrap;
+
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.spi.PersistenceUnitTransactionType;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author <a href="mailto:mposolda@redhat.com">Marek Posolda</a>
@@ -29,5 +38,16 @@ public class JpaUtils {
     public static String getTableNameForNativeQuery(String tableName, EntityManager em) {
         String schema = (String) em.getEntityManagerFactory().getProperties().get(HIBERNATE_DEFAULT_SCHEMA);
         return (schema==null) ? tableName : schema + "." + tableName;
+    }
+
+    public static EntityManagerFactory createEntityManagerFactory(String unitName, Map<String, Object> properties, ClassLoader classLoader) {
+        PersistenceXmlParser parser = new PersistenceXmlParser(new ClassLoaderServiceImpl(classLoader), PersistenceUnitTransactionType.RESOURCE_LOCAL);
+        List<ParsedPersistenceXmlDescriptor> persistenceUnits = parser.doResolve(properties);
+        for (ParsedPersistenceXmlDescriptor persistenceUnit : persistenceUnits) {
+            if (persistenceUnit.getName().equals(unitName)) {
+                return Bootstrap.getEntityManagerFactoryBuilder(persistenceUnit, properties, classLoader).build();
+            }
+        }
+        throw new RuntimeException("Persistence unit '" + unitName + "' not found");
     }
 }
