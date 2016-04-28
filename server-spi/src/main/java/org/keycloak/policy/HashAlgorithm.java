b7025eb9cb5853e26c37d349ec8b4a530ecac2cb
Roelof Naude <roelof.naude@gmail.com>
2016-04-28 16:04:37 +0200

    - created the PasswordPolicy spi framework - add all built-in policies as providers

---
+++ server-spi/src/main/java/org/keycloak/policy/HashAlgorithm.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2016 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.keycloak.policy;
+
+import org.keycloak.models.Constants;
+import org.keycloak.models.KeycloakSession;
+
+/**
+ * @author <a href="mailto:sthorger@redhat.com">Stian Thorgersen</a>
+ */
+public class HashAlgorithm extends BasePasswordPolicy {
+    private static final String NAME = "hashAlgorithm";
+    private String algorithm;
+
+    public HashAlgorithm(String arg) {
+        algorithm = stringArg(NAME, Constants.DEFAULT_HASH_ALGORITHM, arg);
+    }
+
+    @Override
+    public String description() {
+        return null;
+    }
+
+    @Override
+    public PasswordPolicyProvider create(KeycloakSession session) {
+        return this;
+    }
+
+    @Override
+    public String getId() {
+        return NAME;
+    }
+}
---
+++ server-spi/src/main/java/org/keycloak/policy/Length.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2016 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.keycloak.policy;
+
+import org.keycloak.models.Constants;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.UserModel;
+
+/**
+ * @author <a href="mailto:sthorger@redhat.com">Stian Thorgersen</a>
+ */
+public class Length extends BasePasswordPolicy {
+    private static final String NAME = "length";
+    private static final String INVALID_PASSWORD_MIN_LENGTH_MESSAGE = "invalidPasswordMinLengthMessage";
+    private int min;
+
+    public Length(String arg) {
+        min = intArg(NAME, 8, arg);
+    }
+
+    @Override
+    public Error validate(KeycloakSession session, String username, String password) {
+        return password.length() < min ? new Error(INVALID_PASSWORD_MIN_LENGTH_MESSAGE, min) : null;
+    }
+
+    @Override
+    public Error validate(KeycloakSession session, UserModel user, String password) {
+        return validate(session, user.getUsername(), password);
+    }
+
+    @Override
+    public String description() {
+        return null;
+    }
+
+    @Override
+    public PasswordPolicyProvider create(KeycloakSession session) {
+        return this;
+    }
+
+    @Override
+    public String getId() {
+        return NAME;
+    }
+}
