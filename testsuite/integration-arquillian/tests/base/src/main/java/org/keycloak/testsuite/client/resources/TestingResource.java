a5687e4660c06ad9e05e4de58d9ea72617cb4f05
Bruno Oliveira <bruno@abstractj.org>
2016-05-05 11:53:43 -0300

    Migration of RefreshTokenTest

--- testsuite/integration-arquillian/tests/base/src/main/java/org/keycloak/testsuite/client/resources/TestingResource.java
+++ testsuite/integration-arquillian/tests/base/src/main/java/org/keycloak/testsuite/client/resources/TestingResource.java
@@ -65,6 +65,11 @@ public interface TestingResource {
     @Produces(MediaType.APPLICATION_JSON)
     Response removeUserSession(@QueryParam("realm") final String realm, @QueryParam("session") final String sessionId);
 
+    @GET
+    @Path("/get-user-session")
+    @Produces(MediaType.APPLICATION_JSON)
+    Integer getLastSessionRefresh(@QueryParam("realm") final String realm, @QueryParam("session") final String sessionId);
+
     @POST
     @Path("/remove-expired")
     @Produces(MediaType.APPLICATION_JSON)
