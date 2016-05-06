30f4f920c1fc0f8083117706aa2692a5b1b88837
wyvie <elijahrum@gmail.com>
2016-05-06 15:01:11 +0200

    KEYCLOAK-2971: saml provider without signature in broker test

--- testsuite/integration-arquillian/tests/base/src/main/java/org/keycloak/testsuite/broker/BrokerTestConstants.java
+++ testsuite/integration-arquillian/tests/base/src/main/java/org/keycloak/testsuite/broker/BrokerTestConstants.java
@@ -1,12 +1,15 @@
 package org.keycloak.testsuite.broker;
 
-class KcOidcBrokerConstants {
+class BrokerTestConstants {
 
     final static String REALM_PROV_NAME = "provider";
     final static String REALM_CONS_NAME = "consumer";
 
-    final static String IDP_ALIAS = "kc-oidc-idp";
-    final static String IDP_PROVIDER_ID = "keycloak-oidc";
+    final static String IDP_OIDC_ALIAS = "kc-oidc-idp";
+    final static String IDP_OIDC_PROVIDER_ID = "keycloak-oidc";
+
+    final static String IDP_SAML_ALIAS = "kc-saml-idp";
+    final static String IDP_SAML_PROVIDER_ID = "saml";
 
     final static String CLIENT_ID = "brokerapp";
     final static String CLIENT_SECRET = "secret";
