8b0bf503c35fdfaeeee225cf3bbae7f295175ecb
Pedro Igor <pigor.craveiro@gmail.com>
2016-06-29 12:07:49 -0300

    [KEYCLOAK-3172] - Migrating older versions with authorization services.

--- server-spi/src/main/java/org/keycloak/migration/MigrationModel.java
+++ server-spi/src/main/java/org/keycloak/migration/MigrationModel.java
@@ -26,7 +26,7 @@ public interface MigrationModel {
     /**
      * Must have the form of major.minor.micro as the version is parsed and numbers are compared
      */
-    String LATEST_VERSION = "1.9.2";
+    String LATEST_VERSION = "2.0.0";
 
     String getStoredVersion();
     void setStoredVersion(String version);
