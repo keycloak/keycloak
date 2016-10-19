584d2e700d2910ffb8c879f12811b6e5725f084a
Marko Strukelj <marko.strukelj@gmail.com>
2016-10-19 23:17:30 +0200

    KEYCLOAK-3740 kcreg.sh doesn't support symlinking

--- integration/client-cli/client-registration-cli/src/main/bin/kcreg.sh
+++ integration/client-cli/client-registration-cli/src/main/bin/kcreg.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
-DIRNAME=`dirname "$0"`
+RESOLVED_NAME="$0"
+case "`uname`" in
+    CYGWIN*)
+        CFILE = `cygpath "$0"`
+        RESOLVED_NAME=`readlink -f "$CFILE"`
+        ;;
+    Darwin*)
+        RESOLVED_NAME=`readlink "$0"`
+        ;;
+    FreeBSD)
+        RESOLVED_NAME=`readlink -f "$0"`
+        ;;
+    Linux)
+        RESOLVED_NAME=`readlink -f "$0"`
+        ;;
+esac
+
+DIRNAME=`dirname "$RESOLVED_NAME"`
+
 java $KC_OPTS -cp $DIRNAME/client/keycloak-client-registration-cli-${project.version}.jar org.keycloak.client.registration.cli.KcRegMain "$@"
