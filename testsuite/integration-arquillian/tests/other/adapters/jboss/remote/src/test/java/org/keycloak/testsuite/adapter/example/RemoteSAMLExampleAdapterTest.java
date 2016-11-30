3f32cb154b188b6e936a0ad7b0d1326fe661d639
Tomas Kyjovsky <tkyjovsk@redhat.com>
2016-11-30 19:58:54 +0100

    Added remote jboss adapter tests.

---
+++ testsuite/integration-arquillian/tests/other/adapters/jboss/remote/src/test/java/org/keycloak/testsuite/adapter/example/RemoteSAMLExampleAdapterTest.java
@@ -0,0 +1,11 @@
+package org.keycloak.testsuite.adapter.example;
+
+import org.keycloak.testsuite.arquillian.annotation.AppServerContainer;
+
+/**
+ * @author mhajas
+ */
+@AppServerContainer("app-server-remote")
+public class RemoteSAMLExampleAdapterTest extends AbstractSAMLExampleAdapterTest {
+
+}
