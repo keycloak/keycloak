642de06fb58a4ff36bbc8c0ae84fd11fa71fad83
Hynek Mlnarik <hmlnarik@redhat.com>
2016-12-13 16:07:11 +0100

    KEYCLOAK-4040 Support a letter-case variant of md:OrganizationURL

--- saml-core/src/test/java/org/keycloak/saml/processing/core/parsers/saml/SAMLParserTest.java
+++ saml-core/src/test/java/org/keycloak/saml/processing/core/parsers/saml/SAMLParserTest.java
@@ -16,12 +16,20 @@
  */
 package org.keycloak.saml.processing.core.parsers.saml;
 
+import org.keycloak.dom.saml.v2.metadata.EntityDescriptorType;
+
 import java.io.InputStream;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+
 import org.keycloak.dom.saml.v2.protocol.LogoutRequestType;
 import org.keycloak.dom.saml.v2.protocol.ResponseType;
+
+import java.io.IOException;
+import java.util.Scanner;
+import org.junit.Before;
 import org.w3c.dom.Element;
 
 /**
@@ -33,75 +41,86 @@ import org.w3c.dom.Element;
  */
 public class SAMLParserTest {
 
+    private SAMLParser parser;
+
+    @Before
+    public void initParser() {
+        this.parser = new SAMLParser();
+    }
+
     @Test
     public void testSaml20EncryptedAssertionsSignedReceivedWithRedirectBinding() throws Exception {
-        InputStream st = SAMLParserTest.class.getResourceAsStream("saml20-encrypted-signed-redirect-response.xml");
-        SAMLParser parser = new SAMLParser();
+        try (InputStream st = SAMLParserTest.class.getResourceAsStream("saml20-encrypted-signed-redirect-response.xml")) {
+            Object parsedObject = parser.parse(st);
+            assertThat(parsedObject, instanceOf(ResponseType.class));
 
-        Object parsedObject = parser.parse(st);
-        assertThat(parsedObject, instanceOf(ResponseType.class));
-        
-        ResponseType resp = (ResponseType) parsedObject;
-        assertThat(resp.getSignature(), nullValue());
-        assertThat(resp.getConsent(), nullValue());
-        assertThat(resp.getIssuer(), not(nullValue()));
-        assertThat(resp.getIssuer().getValue(), is("http://localhost:8081/auth/realms/saml-demo"));
+            ResponseType resp = (ResponseType) parsedObject;
+            assertThat(resp.getSignature(), nullValue());
+            assertThat(resp.getConsent(), nullValue());
+            assertThat(resp.getIssuer(), not(nullValue()));
+            assertThat(resp.getIssuer().getValue(), is("http://localhost:8081/auth/realms/saml-demo"));
 
-        assertThat(resp.getExtensions(), not(nullValue()));
-        assertThat(resp.getExtensions().getAny().size(), is(1));
-        assertThat(resp.getExtensions().getAny().get(0), instanceOf(Element.class));
-        Element el = (Element) resp.getExtensions().getAny().get(0);
-        assertThat(el.getLocalName(), is("KeyInfo"));
-        assertThat(el.getNamespaceURI(), is("urn:keycloak:ext:key:1.0"));
-        assertThat(el.hasAttribute("MessageSigningKeyId"), is(true));
-        assertThat(el.getAttribute("MessageSigningKeyId"), is("FJ86GcF3jTbNLOco4NvZkUCIUmfYCqoqtOQeMfbhNlE"));
+            assertThat(resp.getExtensions(), not(nullValue()));
+            assertThat(resp.getExtensions().getAny().size(), is(1));
+            assertThat(resp.getExtensions().getAny().get(0), instanceOf(Element.class));
+            Element el = (Element) resp.getExtensions().getAny().get(0);
+            assertThat(el.getLocalName(), is("KeyInfo"));
+            assertThat(el.getNamespaceURI(), is("urn:keycloak:ext:key:1.0"));
+            assertThat(el.hasAttribute("MessageSigningKeyId"), is(true));
+            assertThat(el.getAttribute("MessageSigningKeyId"), is("FJ86GcF3jTbNLOco4NvZkUCIUmfYCqoqtOQeMfbhNlE"));
 
-        assertThat(resp.getAssertions(), not(nullValue()));
-        assertThat(resp.getAssertions().size(), is(1));
+            assertThat(resp.getAssertions(), not(nullValue()));
+            assertThat(resp.getAssertions().size(), is(1));
+        }
     }
 
     @Test
     public void testSaml20EncryptedAssertionsSignedTwoExtensionsReceivedWithRedirectBinding() throws Exception {
         Element el;
 
-        InputStream st = SAMLParserTest.class.getResourceAsStream("saml20-encrypted-signed-redirect-response-two-extensions.xml");
-        SAMLParser parser = new SAMLParser();
+        try (InputStream st = SAMLParserTest.class.getResourceAsStream("saml20-encrypted-signed-redirect-response-two-extensions.xml")) {
+            Object parsedObject = parser.parse(st);
+            assertThat(parsedObject, instanceOf(ResponseType.class));
 
-        Object parsedObject = parser.parse(st);
-        assertThat(parsedObject, instanceOf(ResponseType.class));
+            ResponseType resp = (ResponseType) parsedObject;
+            assertThat(resp.getSignature(), nullValue());
+            assertThat(resp.getConsent(), nullValue());
+            assertThat(resp.getIssuer(), not(nullValue()));
+            assertThat(resp.getIssuer().getValue(), is("http://localhost:8081/auth/realms/saml-demo"));
 
-        ResponseType resp = (ResponseType) parsedObject;
-        assertThat(resp.getSignature(), nullValue());
-        assertThat(resp.getConsent(), nullValue());
-        assertThat(resp.getIssuer(), not(nullValue()));
-        assertThat(resp.getIssuer().getValue(), is("http://localhost:8081/auth/realms/saml-demo"));
+            assertThat(resp.getExtensions(), not(nullValue()));
+            assertThat(resp.getExtensions().getAny().size(), is(2));
+            assertThat(resp.getExtensions().getAny().get(0), instanceOf(Element.class));
+            el = (Element) resp.getExtensions().getAny().get(0);
+            assertThat(el.getLocalName(), is("KeyInfo"));
+            assertThat(el.getNamespaceURI(), is("urn:keycloak:ext:key:1.0"));
+            assertThat(el.hasAttribute("MessageSigningKeyId"), is(true));
+            assertThat(el.getAttribute("MessageSigningKeyId"), is("FJ86GcF3jTbNLOco4NvZkUCIUmfYCqoqtOQeMfbhNlE"));
+            assertThat(resp.getExtensions().getAny().get(1), instanceOf(Element.class));
+            el = (Element) resp.getExtensions().getAny().get(1);
+            assertThat(el.getLocalName(), is("ever"));
+            assertThat(el.getNamespaceURI(), is("urn:keycloak:ext:what:1.0"));
+            assertThat(el.hasAttribute("what"), is(true));
+            assertThat(el.getAttribute("what"), is("ever"));
 
-        assertThat(resp.getExtensions(), not(nullValue()));
-        assertThat(resp.getExtensions().getAny().size(), is(2));
-        assertThat(resp.getExtensions().getAny().get(0), instanceOf(Element.class));
-        el = (Element) resp.getExtensions().getAny().get(0);
-        assertThat(el.getLocalName(), is("KeyInfo"));
-        assertThat(el.getNamespaceURI(), is("urn:keycloak:ext:key:1.0"));
-        assertThat(el.hasAttribute("MessageSigningKeyId"), is(true));
-        assertThat(el.getAttribute("MessageSigningKeyId"), is("FJ86GcF3jTbNLOco4NvZkUCIUmfYCqoqtOQeMfbhNlE"));
-        assertThat(resp.getExtensions().getAny().get(1), instanceOf(Element.class));
-        el = (Element) resp.getExtensions().getAny().get(1);
-        assertThat(el.getLocalName(), is("ever"));
-        assertThat(el.getNamespaceURI(), is("urn:keycloak:ext:what:1.0"));
-        assertThat(el.hasAttribute("what"), is(true));
-        assertThat(el.getAttribute("what"), is("ever"));
-
-        assertThat(resp.getAssertions(), not(nullValue()));
-        assertThat(resp.getAssertions().size(), is(1));
+            assertThat(resp.getAssertions(), not(nullValue()));
+            assertThat(resp.getAssertions().size(), is(1));
+        }
     }
 
     @Test
     public void testSaml20PostLogoutRequest() throws Exception {
-        InputStream st = SAMLParserTest.class.getResourceAsStream("saml20-signed-logout-request.xml");
-        SAMLParser parser = new SAMLParser();
+        try (InputStream st = SAMLParserTest.class.getResourceAsStream("saml20-signed-logout-request.xml")) {
+            Object parsedObject = parser.parse(st);
+            assertThat(parsedObject, instanceOf(LogoutRequestType.class));
+        }
+    }
 
-        Object parsedObject = parser.parse(st);
-        assertThat(parsedObject, instanceOf(LogoutRequestType.class));
-
+    @Test
+    public void testOrganizationDetailsMetadata() throws Exception {
+        try (InputStream st = SAMLParserTest.class.getResourceAsStream("KEYCLOAK-4040-sharefile-metadata.xml")) {
+            Object parsedObject = parser.parse(st);
+            assertThat(parsedObject, instanceOf(EntityDescriptorType.class));
+        }
     }
 }
