2598a562c690e232b88ee0cd628587e060dfb795
dsbenghe <dsbenghe@aconex.com>
2017-01-18 16:54:00 +1100

    mapping of authentication methods to amr strings
    (cherry picked from commit b656fc42e33d5bf6d5624668c7ba74b82c001d4d)

--- services/src/main/java/org/keycloak/protocol/oidc/TokenManager.java
+++ services/src/main/java/org/keycloak/protocol/oidc/TokenManager.java
@@ -46,6 +46,7 @@ import org.keycloak.models.UserSessionModel;
 import org.keycloak.models.UserSessionProvider;
 import org.keycloak.models.utils.KeycloakModelUtils;
 import org.keycloak.protocol.ProtocolMapper;
+import org.keycloak.protocol.oidc.mappers.AmrMapper;
 import org.keycloak.protocol.oidc.mappers.OIDCAccessTokenMapper;
 import org.keycloak.protocol.oidc.mappers.OIDCIDTokenMapper;
 import org.keycloak.protocol.oidc.mappers.UserInfoTokenMapper;
@@ -79,6 +80,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Stateless object that creates tokens and manages oauth access codes
@@ -635,7 +637,8 @@ public class TokenManager {
 
         String authMethods = session.getNote(AuthenticationManager.AUTH_METHODS);
         if (authMethods != null){
-            token.setAmr(Collections.unmodifiableList(Arrays.asList(authMethods.trim().split(" "))));
+            token.setAmr(Collections.unmodifiableList(Arrays.asList(authMethods.trim().split(" "))
+                    .stream().map(AmrMapper::getAmr).filter(amrValue -> amrValue != null).collect(Collectors.toList())));
         }
 
         token.setSessionState(session.getId());
