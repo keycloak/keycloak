4425350c93f10ea54c97c0d0362a93bc03a7ca20
Adam Kaplan <abkaplan07@gmail.com>
2017-03-03 18:59:22 -0500

    KEYCLOAK-4523 SHA384 provider

---
+++ server-spi-private/src/main/java/org/keycloak/credential/hash/Pbkdf2Sha384PasswordHashProvider.java
@@ -0,0 +1,25 @@
+package org.keycloak.credential.hash;
+
+/**
+ * PBKDF2 password hash provider with HMAC using SHA384
+ *
+ * @author <a href"mailto:abkaplan07@gmail.com">Adam Kaplan</a>
+ */
+public class Pbkdf2Sha384PasswordHashProvider extends APbkdf2PasswordHashProvider {
+
+    static final String ALIAS = "pbkdf2-hmac-sha384";
+    @Override
+    protected String getAlgorithmAlias() {
+        return ALIAS;
+    }
+
+    @Override
+    protected String getPbkdf2Algorithm() {
+        return "PBKDF2WithHmacSHA384";
+    }
+
+    @Override
+    protected int getDerivedKeySize() {
+        return 384;
+    }
+}
