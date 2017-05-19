7f70b8448767246f654c5134b5e160f4d5c0c10e
Stian Thorgersen <stianst@gmail.com>
2017-05-19 10:05:38 +0200

    KEYCLOAK-5143 Run auth-server-wildfly profile on Travis

---
+++ testsuite/utils/src/main/java/org/keycloak/testsuite/LogTrimmer.java
@@ -0,0 +1,46 @@
+package org.keycloak.testsuite;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by st on 03/07/17.
+ */
+public class LogTrimmer {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String testRunning = null;
+        StringBuilder sb = new StringBuilder();
+        for(String l = br.readLine(); l != null; l = br.readLine()) {
+            if (testRunning == null) {
+                if (l.startsWith("Running")) {
+                    testRunning = l.split(" ")[1];
+                    System.out.println(l);
+                } else {
+                    System.out.println("-- " + l);
+                }
+            } else {
+                if (l.startsWith("Tests run")) {
+                    if (l.contains("Failures: 1") && l.contains("Errors: 0")) {
+                        System.out.println(l + "Logging dropped");
+                    } else {
+                        System.out.println("--------- " + testRunning + " output start ---------");
+                        System.out.println(sb.toString());
+                    }
+                    System.out.println("--------- " + testRunning + " output end  ---------");
+                    System.out.println(l);
+
+
+                    testRunning = null;
+                    sb = new StringBuilder();
+                } else {
+                    sb.append("++ " + l);
+                    sb.append("\n");
+                }
+            }
+        }
+    }
+
+}
