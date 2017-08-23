010dd5709ce5522636064c61f4a0296e9d0ce121
Stan Silvert <ssilvert@redhat.com>
2017-08-23 09:55:49 -0400

    KEYCLOAK-5286: js error on save in client/mappers/details screen (#4409)

--- themes/src/main/resources/theme/base/admin/resources/js/controllers/clients.js
+++ themes/src/main/resources/theme/base/admin/resources/js/controllers/clients.js
@@ -1803,7 +1803,7 @@ module.controller('ClientProtocolMapperCtrl', function($scope, realm, serverInfo
         ClientProtocolMapper.update({
             realm : realm.realm,
             client: client.id,
-            id : mapper.id
+            id : $scope.model.mapper.id
         }, $scope.model.mapper, function() {
             $scope.model.changed = false;
             mapper = angular.copy($scope.mapper);
