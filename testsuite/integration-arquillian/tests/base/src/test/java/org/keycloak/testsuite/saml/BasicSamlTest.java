a7cf38139b5339ba4413c7b824e1d2d3ea70c889
Hynek Mlnarik <hmlnarik@redhat.com>
2017-08-27 18:53:58 +0200

    KEYCLOAK-5318 Verify signature on raw query parameters
    (cherry picked from commit 001f669a151813be6e249265951e24f78b754d02)
    
    Conflicts:
    	services/src/main/java/org/keycloak/protocol/saml/SamlService.java
    	testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/saml/AbstractSamlTest.java
    	testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/saml/BasicSamlTest.java

--- testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/saml/BasicSamlTest.java
+++ testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/saml/BasicSamlTest.java
@@ -2,12 +2,27 @@ package org.keycloak.testsuite.saml;
 
 import org.junit.Test;
 import org.keycloak.dom.saml.v2.protocol.AuthnRequestType;
+import org.keycloak.saml.SignatureAlgorithm;
+import org.keycloak.saml.common.constants.GeneralConstants;
 import org.keycloak.saml.common.exceptions.ConfigurationException;
 import org.keycloak.saml.common.exceptions.ParsingException;
 import org.keycloak.saml.common.exceptions.ProcessingException;
+import org.keycloak.saml.common.util.Base64;
 import org.keycloak.saml.processing.api.saml.v2.request.SAML2Request;
 import org.keycloak.saml.processing.core.saml.v2.common.SAMLDocumentHolder;
+import org.keycloak.saml.processing.web.util.RedirectBindingUtil;
+import org.keycloak.testsuite.util.KeyUtils;
+import org.keycloak.testsuite.util.Matchers;
 import org.keycloak.testsuite.util.SamlClient;
+import org.keycloak.testsuite.util.SamlClient.Binding;
+import java.net.URI;
+import java.security.Signature;
+import javax.ws.rs.core.Response.Status;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.jboss.resteasy.util.Encode;
 import org.w3c.dom.Document;
 
 import static org.hamcrest.CoreMatchers.containsString;
@@ -35,4 +50,52 @@ public class BasicSamlTest extends AbstractSamlTest {
 
         assertThat(documentToString(document.getSamlDocument()), not(containsString("InResponseTo=\"" + System.getProperty("java.version") + "\"")));
     }
+
+    @Test
+    public void testRedirectUrlSigned() throws Exception {
+        testSpecialCharsInRelayState(null);
+    }
+
+    @Test
+    public void testRedirectUrlUnencodedSpecialChars() throws Exception {
+        testSpecialCharsInRelayState("New%20Document%20(1).doc");
+    }
+
+    @Test
+    public void testRedirectUrlEncodedSpecialChars() throws Exception {
+        testSpecialCharsInRelayState("New%20Document%20%281%29.doc");
+    }
+
+    private void testSpecialCharsInRelayState(String encodedRelayState) throws Exception {
+        AuthnRequestType loginRep = createLoginRequestDocument(SAML_CLIENT_ID_SALES_POST_SIG, SAML_ASSERTION_CONSUMER_URL_SALES_POST_SIG, REALM_NAME);
+
+        Document doc = SAML2Request.convert(loginRep);
+        URI redirect = Binding.REDIRECT.createSamlUnsignedRequest(getAuthServerSamlEndpoint(REALM_NAME), null, doc).getURI();
+        String query = redirect.getRawQuery();
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.RSA_SHA256;
+
+        // now add the relayState
+        String relayStatePart = encodedRelayState == null
+          ? ""
+          : ("&" + GeneralConstants.RELAY_STATE + "=" + encodedRelayState);
+        String sigAlgPart = "&" + GeneralConstants.SAML_SIG_ALG_REQUEST_KEY + "=" + Encode.encodeQueryParamAsIs(signatureAlgorithm.getXmlSignatureMethod());
+
+        Signature signature = signatureAlgorithm.createSignature();
+        byte[] sig;
+
+        signature.initSign(KeyUtils.privateKeyFromString(SAML_CLIENT_SALES_POST_SIG_PRIVATE_KEY));
+        signature.update(query.getBytes(GeneralConstants.SAML_CHARSET));
+        signature.update(relayStatePart.getBytes(GeneralConstants.SAML_CHARSET));
+        signature.update(sigAlgPart.getBytes(GeneralConstants.SAML_CHARSET));
+        sig = signature.sign();
+
+        String encodedSig = Base64.encodeBytes(sig, Base64.DONT_BREAK_LINES);
+        String sigPart = "&" + GeneralConstants.SAML_SIGNATURE_REQUEST_KEY + "=" + Encode.encodeQueryParamAsIs(encodedSig);
+
+        try (CloseableHttpClient c = HttpClientBuilder.create().build();
+          CloseableHttpResponse r = c.execute(new HttpGet(redirect.toString() + relayStatePart + sigAlgPart + sigPart))) {
+            assertThat(r, Matchers.statusCodeIsHC(Status.OK));
+        }
+    }
+
 }
