ebe3a024be83707390523cb421d8895ae9bcf18a
Stian Thorgersen <stianst@gmail.com>
2017-08-30 09:26:56 +0200

    Fix URN of SAML adapter schema

--- build-auto.sh
+++ build-auto.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-DIR=`dirname $0`/src/web
-
 while true; do 
-    CHANGED=`inotifywait -r -e modify,move,create,delete server_installation --format %w`
-    GUIDE=`dirname $CHANGED`
+    CHANGED=`inotifywait -r -e modify,move,create,delete authorization_services getting_started securing_apps server_admin server_development server_installation --format %w`
+    GUIDE=`echo $CHANGED | cut -d '/' -f 1`
     mvn install -f $GUIDE    
 done
