8e762cef8afe4db6634596261cc93ed667abba89
Stian Thorgersen <stianst@gmail.com>
2017-08-31 20:39:42 +0200

    KEYCLOAK-5358 Move upgrade guide

--- build-auto.sh
+++ build-auto.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do 
-    CHANGED=`inotifywait -r -e modify,move,create,delete authorization_services getting_started securing_apps server_admin server_development server_installation --format %w`
+    CHANGED=`inotifywait -r -e modify,move,create,delete authorization_services getting_started securing_apps server_admin server_development server_installation upgrading --format %w`
     GUIDE=`echo $CHANGED | cut -d '/' -f 1`
     mvn install -f $GUIDE    
 done
