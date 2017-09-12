a5e8c09f3e2223075a1c12930db3a4cdc265bd1c
Stian Thorgersen <stianst@gmail.com>
2017-09-12 15:17:41 +0200

    Fix testsuite for external docs

--- tests/src/test/java/org/keycloak/documentation/test/utils/Constants.java
+++ tests/src/test/java/org/keycloak/documentation/test/utils/Constants.java
@@ -5,8 +5,8 @@ import java.util.concurrent.TimeUnit;
 public class Constants {
 
     public static final int HTTP_RETRY = 3;
-    public static final int HTTP_CONNECTION_TIMEOUT = 5000;
-    public static final int HTTP_READ_TIMEOUT = 10000;
+    public static final int HTTP_CONNECTION_TIMEOUT = 30000;
+    public static final int HTTP_READ_TIMEOUT = 300000;
     public static final long LINK_CHECK_EXPIRATION = TimeUnit.DAYS.toMillis(1);
 
 }
