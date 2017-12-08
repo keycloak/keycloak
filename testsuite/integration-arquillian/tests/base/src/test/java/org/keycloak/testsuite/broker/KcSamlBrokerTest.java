74a0c3c5d888313f209aec48b46c49ab47e70545
Hynek Mlnarik <hmlnarik@redhat.com>
2017-12-08 01:19:57 +0100

    KEYCLOAK-4998 Fix NPE in AttributeToRoleMapper

--- testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/broker/KcSamlBrokerTest.java
+++ testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/broker/KcSamlBrokerTest.java
@@ -1,15 +1,28 @@
 package org.keycloak.testsuite.broker;
 
+import org.keycloak.admin.client.resource.UserResource;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import org.keycloak.broker.saml.mappers.AttributeToRoleMapper;
 import org.keycloak.broker.saml.mappers.UserAttributeMapper;
 import org.keycloak.protocol.saml.SamlProtocol;
 import org.keycloak.representations.idm.IdentityProviderMapperRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
 import org.keycloak.services.resources.RealmsResource;
 import java.net.URI;
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
 import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriBuilderException;
+import org.junit.Test;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertThat;
+import static org.keycloak.testsuite.broker.AbstractBrokerTest.ROLE_FRIENDLY_MANAGER;
+import static org.keycloak.testsuite.broker.AbstractBrokerTest.ROLE_MANAGER;
+import static org.keycloak.testsuite.broker.AbstractBrokerTest.ROLE_USER;
 
 public class KcSamlBrokerTest extends AbstractBrokerTest {
 
@@ -25,8 +38,8 @@ public class KcSamlBrokerTest extends AbstractBrokerTest {
         attrMapper1.setIdentityProviderMapper(AttributeToRoleMapper.PROVIDER_ID);
         attrMapper1.setConfig(ImmutableMap.<String,String>builder()
                 .put(UserAttributeMapper.ATTRIBUTE_NAME, "Role")
-                .put(ATTRIBUTE_VALUE, "manager")
-                .put("role", "manager")
+                .put(ATTRIBUTE_VALUE, ROLE_MANAGER)
+                .put("role", ROLE_MANAGER)
                 .build());
 
         IdentityProviderMapperRepresentation attrMapper2 = new IdentityProviderMapperRepresentation();
@@ -34,11 +47,75 @@ public class KcSamlBrokerTest extends AbstractBrokerTest {
         attrMapper2.setIdentityProviderMapper(AttributeToRoleMapper.PROVIDER_ID);
         attrMapper2.setConfig(ImmutableMap.<String,String>builder()
                 .put(UserAttributeMapper.ATTRIBUTE_NAME, "Role")
-                .put(ATTRIBUTE_VALUE, "user")
-                .put("role", "user")
+                .put(ATTRIBUTE_VALUE, ROLE_USER)
+                .put("role", ROLE_USER)
                 .build());
 
-        return Lists.newArrayList(attrMapper1, attrMapper2);
+        IdentityProviderMapperRepresentation attrMapper3 = new IdentityProviderMapperRepresentation();
+        attrMapper3.setName("friendly-mapper");
+        attrMapper3.setIdentityProviderMapper(AttributeToRoleMapper.PROVIDER_ID);
+        attrMapper3.setConfig(ImmutableMap.<String,String>builder()
+                .put(UserAttributeMapper.ATTRIBUTE_FRIENDLY_NAME, AbstractUserAttributeMapperTest.ATTRIBUTE_TO_MAP_FRIENDLY_NAME)
+                .put(ATTRIBUTE_VALUE, ROLE_FRIENDLY_MANAGER)
+                .put("role", ROLE_FRIENDLY_MANAGER)
+                .build());
+
+        return Lists.newArrayList(attrMapper1, attrMapper2, attrMapper3);
+    }
+
+    // KEYCLOAK-3987
+    @Test
+    @Override
+    public void grantNewRoleFromToken() {
+        createRolesForRealm(bc.providerRealmName());
+        createRolesForRealm(bc.consumerRealmName());
+
+        createRoleMappersForConsumerRealm();
+
+        RoleRepresentation managerRole = adminClient.realm(bc.providerRealmName()).roles().get(ROLE_MANAGER).toRepresentation();
+        RoleRepresentation friendlyManagerRole = adminClient.realm(bc.providerRealmName()).roles().get(ROLE_FRIENDLY_MANAGER).toRepresentation();
+        RoleRepresentation userRole = adminClient.realm(bc.providerRealmName()).roles().get(ROLE_USER).toRepresentation();
+
+        UserResource userResource = adminClient.realm(bc.providerRealmName()).users().get(userId);
+        userResource.roles().realmLevel().add(Collections.singletonList(managerRole));
+
+        logInAsUserInIDPForFirstTime();
+
+        Set<String> currentRoles = userResource.roles().realmLevel().listAll().stream()
+          .map(RoleRepresentation::getName)
+          .collect(Collectors.toSet());
+
+        assertThat(currentRoles, hasItems(ROLE_MANAGER));
+        assertThat(currentRoles, not(hasItems(ROLE_USER, ROLE_FRIENDLY_MANAGER)));
+
+        logoutFromRealm(bc.consumerRealmName());
+
+
+        userResource.roles().realmLevel().add(Collections.singletonList(userRole));
+        userResource.roles().realmLevel().add(Collections.singletonList(friendlyManagerRole));
+
+        logInAsUserInIDP();
+
+        currentRoles = userResource.roles().realmLevel().listAll().stream()
+          .map(RoleRepresentation::getName)
+          .collect(Collectors.toSet());
+        assertThat(currentRoles, hasItems(ROLE_MANAGER, ROLE_USER, ROLE_FRIENDLY_MANAGER));
+
+        logoutFromRealm(bc.consumerRealmName());
+
+
+        userResource.roles().realmLevel().remove(Collections.singletonList(friendlyManagerRole));
+
+        logInAsUserInIDP();
+
+        currentRoles = userResource.roles().realmLevel().listAll().stream()
+          .map(RoleRepresentation::getName)
+          .collect(Collectors.toSet());
+        assertThat(currentRoles, hasItems(ROLE_MANAGER, ROLE_USER));
+        assertThat(currentRoles, not(hasItems(ROLE_FRIENDLY_MANAGER)));
+
+        logoutFromRealm(bc.providerRealmName());
+        logoutFromRealm(bc.consumerRealmName());
     }
 
     protected URI getAuthServerSamlEndpoint(String realm) throws IllegalArgumentException, UriBuilderException {
