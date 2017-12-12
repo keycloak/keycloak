ea3417253f2f1cbbba48b94bd0461dba251bda8c
Bill Burke <bburke@redhat.com>
2017-12-12 16:07:09 -0500

    KEYCLOAK-5923

--- themes/src/main/resources/theme/base/admin/resources/js/controllers/groups.js
+++ themes/src/main/resources/theme/base/admin/resources/js/controllers/groups.js
@@ -21,12 +21,12 @@ module.controller('GroupListCtrl', function($scope, $route, $q, realm, Groups, G
         var first = ($scope.currentPage * $scope.pageSize) - $scope.pageSize;
         console.log('first:' + first);
         var queryParams = {
-            realm : realm.id,
+            realm : realm.realm,
             first : first,
             max : $scope.pageSize
         };
         var countParams = {
-            realm : realm.id,
+            realm : realm.realm,
             top : 'true'
         };
 
@@ -54,6 +54,7 @@ module.controller('GroupListCtrl', function($scope, $route, $q, realm, Groups, G
         });
 
         var promiseCount = $q.defer();
+        console.log('countParams: realm[' + countParams.realm);
         GroupsCount.query(countParams, function(entry) {
             promiseCount.resolve(entry);
         }, function() {
