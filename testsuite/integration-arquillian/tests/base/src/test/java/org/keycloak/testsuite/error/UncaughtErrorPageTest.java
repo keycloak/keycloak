b672229efccf5dce6eb7dcb73e4d8ff12ddf9e32
stianst <stianst@gmail.com>
2017-12-14 15:22:13 +0100

    KEYCLOAK-6032 Fix error page when internationalization is enabled

--- testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/error/UncaughtErrorPageTest.java
+++ testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/error/UncaughtErrorPageTest.java
@@ -2,13 +2,16 @@ package org.keycloak.testsuite.error;
 
 import org.jboss.arquillian.graphene.page.Page;
 import org.junit.Test;
+import org.keycloak.admin.client.resource.RealmResource;
 import org.keycloak.representations.idm.RealmRepresentation;
 import org.keycloak.testsuite.AbstractKeycloakTest;
 import org.keycloak.testsuite.pages.ErrorPage;
 
 import javax.ws.rs.core.Response;
+import java.lang.reflect.Array;
 import java.net.MalformedURLException;
 import java.net.URI;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -63,6 +66,24 @@ public class UncaughtErrorPageTest extends AbstractKeycloakTest {
 
         assertTrue(errorPage.isCurrent());
         assertEquals("Client not found.", errorPage.getError());
+    }
+
+    @Test
+    public void internationalisationEnabled() throws MalformedURLException {
+        RealmResource testRealm = realmsResouce().realm("master");
+        RealmRepresentation rep = testRealm.toRepresentation();
+        rep.setInternationalizationEnabled(true);
+        rep.setDefaultLocale("en");
+        rep.setSupportedLocales(Collections.singleton("en"));
+        testRealm.update(rep);
+
+        try {
+            checkPageNotFound("/auth/realms/master/nosuch");
+            checkPageNotFound("/auth/nosuch");
+        } finally {
+            rep.setInternationalizationEnabled(false);
+            testRealm.update(rep);
+        }
     }
 
     private void checkPageNotFound(String path) throws MalformedURLException {
