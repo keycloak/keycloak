bb0e4428e8cae4bb00218e2c049cbdfabd7147e9
pskopek <pskopek@redhat.com>
2017-12-18 13:09:10 +0100

    KEYCLOAK-5666 removed not needed decodeToObject methods

--- saml-core-api/src/main/java/org/keycloak/saml/common/util/Base64.java
+++ saml-core-api/src/main/java/org/keycloak/saml/common/util/Base64.java
@@ -693,48 +693,6 @@ public class Base64 {
         return bytes;
     } // end decode
 
-    /**
-     * Attempts to decode Base64 data and deserialize a Java Object within. Returns <tt>null</tt> if there was an error.
-     *
-     * @param encodedObject The Base64 data to decode
-     * @return The decoded and deserialized object
-     * @since 1.5
-     */
-    public static Object decodeToObject(String encodedObject) {
-        // Decode and gunzip if necessary
-        byte[] objBytes = decode(encodedObject);
-
-        java.io.ByteArrayInputStream bais = null;
-        java.io.ObjectInputStream ois = null;
-        Object obj = null;
-
-        try {
-            bais = new java.io.ByteArrayInputStream(objBytes);
-            ois = new java.io.ObjectInputStream(bais);
-
-            obj = ois.readObject();
-        } // end try
-        catch (java.io.IOException e) {
-            e.printStackTrace();
-            obj = null;
-        } // end catch
-        catch (java.lang.ClassNotFoundException e) {
-            e.printStackTrace();
-            obj = null;
-        } // end catch
-        finally {
-            try {
-                bais.close();
-            } catch (Exception e) {
-            }
-            try {
-                ois.close();
-            } catch (Exception e) {
-            }
-        } // end finally
-
-        return obj;
-    } // end decodeObject
 
     /* ******** I N N E R C L A S S I N P U T S T R E A M ******** */
 
