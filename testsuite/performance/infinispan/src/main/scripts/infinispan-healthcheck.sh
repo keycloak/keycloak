df68ca8dcfd481ee64801fbc2cecaa0a6359285d
Tomas Kyjovsky <tkyjovsk@redhat.com>
2017-12-19 12:42:26 +0100

    KEYCLOAK-5968 Move configuration of Infinispan server from Dockerfile to maven

--- testsuite/performance/infinispan/src/main/scripts/infinispan-healthcheck.sh
+++ testsuite/performance/infinispan/src/main/scripts/infinispan-healthcheck.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#$JBOSS_HOME/bin/jboss-cli.sh -c ":read-attribute(name=server-state)" | grep -q "running"
-
 . get-ips.sh
 
 CODE=`curl -s -o /dev/null -w "%{http_code}" http://$PUBLIC_IP:9990/console/index.html`
