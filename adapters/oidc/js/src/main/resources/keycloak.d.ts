0503b86ada2fc5d16300fc6000198bc80bee0cda
Wojciech Trocki <wtrocki@redhat.com>
2018-03-08 15:58:18 +0000

    Allow to use custom adapter

--- adapters/oidc/js/src/main/resources/keycloak.d.ts
+++ adapters/oidc/js/src/main/resources/keycloak.d.ts
@@ -29,7 +29,7 @@ export = Keycloak;
 declare function Keycloak(config?: string|{}): Keycloak.KeycloakInstance;
 
 declare namespace Keycloak {
-	type KeycloakAdapterName = 'cordova'|'default';
+	type KeycloakAdapterName = 'cordova'|'default' | any;
 	type KeycloakOnLoad = 'login-required'|'check-sso';
 	type KeycloakResponseMode = 'query'|'fragment';
 	type KeycloakResponseType = 'code'|'id_token token'|'code id_token token';
@@ -42,7 +42,7 @@ declare namespace Keycloak {
 		useNonce?: boolean;
 
 		/**
-		 * @private Undocumented.
+		 * Allows to use different adapter (default/cordova or custom adapter)
 		 */
 		adapter?: KeycloakAdapterName;
 		
