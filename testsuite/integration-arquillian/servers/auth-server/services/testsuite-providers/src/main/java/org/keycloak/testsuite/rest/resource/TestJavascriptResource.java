70b3f6bd92af00b7d977da6a5a82239d13c774e1
mhajas <mhajas@redhat.com>
2018-04-06 11:47:10 +0200

    KEYCLOAK-6840 Remove untracked file and unpack js adapter directly to testsuite provider target directory (#5133)

--- testsuite/integration-arquillian/servers/auth-server/services/testsuite-providers/src/main/java/org/keycloak/testsuite/rest/resource/TestJavascriptResource.java
+++ testsuite/integration-arquillian/servers/auth-server/services/testsuite-providers/src/main/java/org/keycloak/testsuite/rest/resource/TestJavascriptResource.java
@@ -2,7 +2,6 @@ package org.keycloak.testsuite.rest.resource;
 
 import org.keycloak.testsuite.rest.TestingResourceProvider;
 
-import javax.print.attribute.standard.Media;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
