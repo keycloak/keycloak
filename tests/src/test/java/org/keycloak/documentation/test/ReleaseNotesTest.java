94bd75aa3e51f8387dd22eef4d5f6a615c30a612
Stian Thorgersen <stian@redhat.com>
2018-06-07 15:18:50 +0200

    KEYCLOAK-7527 Release notes in documentation and additions for CD (#397)

---
+++ tests/src/test/java/org/keycloak/documentation/test/ReleaseNotesTest.java
@@ -0,0 +1,10 @@
+package org.keycloak.documentation.test;
+
+public class ReleaseNotesTest extends AbstractDocsTest {
+
+    @Override
+    public String getGuideDirName() {
+        return "release_notes";
+    }
+
+}
