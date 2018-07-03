1de265fab24b0e695005335501bced4ae73987cb
Torbjørn Skyberg Knutsen <torbjornk@netcompany.com>
2018-07-03 15:47:45 +0200

    KEYCLOAK-7166 Added the possibility of not logging out of remote idp on browser logout, by passing a query param containing the id of the identity provider

--- testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/broker/AbstractBaseBrokerTest.java
+++ testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/broker/AbstractBaseBrokerTest.java
@@ -19,6 +19,7 @@ package org.keycloak.testsuite.broker;
 
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
 import org.jboss.arquillian.graphene.page.Page;
 import org.junit.After;
 import org.keycloak.admin.client.resource.RealmResource;
@@ -142,12 +143,17 @@ public abstract class AbstractBaseBrokerTest extends AbstractKeycloakTest {
         return BrokerTestTools.getAuthRoot(suiteContext) + "/auth/realms/" + realmName + "/account/password";
     }
 
-
     protected void logoutFromRealm(String realm) {
+        logoutFromRealm(realm, null);
+    }
+
+    protected void logoutFromRealm(String realm, String initiatingIdp) {
         driver.navigate().to(BrokerTestTools.getAuthRoot(suiteContext)
                 + "/auth/realms/" + realm
                 + "/protocol/" + "openid-connect"
-                + "/logout?redirect_uri=" + encodeUrl(getAccountUrl(realm)));
+                + "/logout?redirect_uri=" + encodeUrl(getAccountUrl(realm))
+                + (!StringUtils.isBlank(initiatingIdp) ? "&initiating_idp=" + initiatingIdp : "")
+        );
 
         try {
             Retry.execute(() -> {
