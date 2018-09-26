f3c9ce6f16f90111b89fb89cd08df54ffec4690b
vramik <vramik@redhat.com>
2018-09-26 12:04:04 +0200

    KEYCLOAK-8425 fix NPE during adapter cluster tests

--- testsuite/integration-arquillian/tests/base/src/main/java/org/keycloak/testsuite/arquillian/provider/URLProvider.java
+++ testsuite/integration-arquillian/tests/base/src/main/java/org/keycloak/testsuite/arquillian/provider/URLProvider.java
@@ -102,19 +102,29 @@ public class URLProvider extends URLResourceProvider {
                 return suiteContext.get().getAuthServerInfo().getContextRoot();
             }
             if (AppServerContext.class.isAssignableFrom(a.annotationType())) {
+                //standalone
                 ContainerInfo appServerInfo = testContext.get().getAppServerInfo();
                 if (appServerInfo != null) return appServerInfo.getContextRoot();
-                
+
+                //cluster
                 List<ContainerInfo> appServerBackendsInfo = testContext.get().getAppServerBackendsInfo();
                 if (appServerBackendsInfo.isEmpty()) throw new IllegalStateException("Both testContext's appServerInfo and appServerBackendsInfo not set.");
-                
+
                 return appServerBackendsInfo.get(0).getContextRoot();
             }
             if (AuthServerBrowserContext.class.isAssignableFrom(a.annotationType())) {
                 return suiteContext.get().getAuthServerInfo().getBrowserContextRoot();
             }
             if (AppServerBrowserContext.class.isAssignableFrom(a.annotationType())) {
-                return testContext.get().getAppServerInfo().getBrowserContextRoot();
+                //standalone
+                ContainerInfo appServerInfo = testContext.get().getAppServerInfo();
+                if (appServerInfo != null) return appServerInfo.getBrowserContextRoot();
+
+                //cluster
+                List<ContainerInfo> appServerBackendsInfo = testContext.get().getAppServerBackendsInfo();
+                if (appServerBackendsInfo.isEmpty()) throw new IllegalStateException("Both testContext's appServerInfo and appServerBackendsInfo not set.");
+
+                return appServerBackendsInfo.get(0).getBrowserContextRoot();
             }
         }
 
