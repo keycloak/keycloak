2af9d002b690cb982563084776a3438475c950fc
Pedro Igor <pigor.craveiro@gmail.com>
2018-10-23 18:37:15 -0300

    [KEYCLOAK-8172] - Evaluation not considering scopes inherited from parent resources

--- services/src/main/java/org/keycloak/authorization/util/Permissions.java
+++ services/src/main/java/org/keycloak/authorization/util/Permissions.java
@@ -118,7 +118,7 @@ public final class Permissions {
         if (requestedScopes.isEmpty()) {
             scopes = populateTypedScopes(resource, authorization);
         } else {
-            scopes = requestedScopes.stream().filter(scope -> resource.getScopes().contains(scope)).collect(Collectors.toList());
+            scopes = populateTypedScopes(resource, requestedScopes.stream().filter(scope -> resource.getScopes().contains(scope)).collect(Collectors.toList()), authorization);
         }
 
         return new ResourcePermission(resource, scopes, resource.getResourceServer(), request.getClaims());
@@ -135,25 +135,32 @@ public final class Permissions {
     }
 
     private static List<Scope> populateTypedScopes(Resource resource, AuthorizationProvider authorization) {
-        List<Scope> scopes = new LinkedList<>(resource.getScopes());
+        return populateTypedScopes(resource, resource.getScopes(), authorization);
+    }
+
+    private static List<Scope> populateTypedScopes(Resource resource, List<Scope> defaultScopes, AuthorizationProvider authorization) {
         String type = resource.getType();
         ResourceServer resourceServer = resource.getResourceServer();
 
+        if (type == null || resource.getOwner().equals(resourceServer.getId())) {
+            return new ArrayList<>(defaultScopes);
+        }
+
+        List<Scope> scopes = new ArrayList<>(defaultScopes);
+
         // check if there is a typed resource whose scopes are inherited by the resource being requested. In this case, we assume that parent resource
         // is owned by the resource server itself
-        if (type != null && !resource.getOwner().equals(resourceServer.getId())) {
-            StoreFactory storeFactory = authorization.getStoreFactory();
-            ResourceStore resourceStore = storeFactory.getResourceStore();
-            resourceStore.findByType(type, resourceServer.getId(), resource1 -> {
-                if (resource1.getOwner().equals(resourceServer.getId())) {
-                    for (Scope typeScope : resource1.getScopes()) {
-                        if (!scopes.contains(typeScope)) {
-                            scopes.add(typeScope);
-                        }
+        StoreFactory storeFactory = authorization.getStoreFactory();
+        ResourceStore resourceStore = storeFactory.getResourceStore();
+        resourceStore.findByType(type, resourceServer.getId(), resource1 -> {
+            if (resource1.getOwner().equals(resourceServer.getId())) {
+                for (Scope typeScope : resource1.getScopes()) {
+                    if (!scopes.contains(typeScope)) {
+                        scopes.add(typeScope);
                     }
                 }
-            });
-        }
+            }
+        });
 
         return scopes;
     }
