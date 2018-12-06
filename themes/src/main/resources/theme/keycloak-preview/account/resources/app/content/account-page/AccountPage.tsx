40071a95da8b0e19b98370446017e88b79dccf28
Stan Silvert <ssilvert@redhat.com>
2018-12-06 15:22:51 -0500

    KEYCLOAK-9038: Add password page and refactor

--- themes/src/main/resources/theme/keycloak-preview/account/resources/app/content/account-page/AccountPage.tsx
+++ themes/src/main/resources/theme/keycloak-preview/account/resources/app/content/account-page/AccountPage.tsx
@@ -18,91 +18,150 @@ import * as React from 'react';
 import {AxiosResponse} from 'axios';
 
 import {AccountServiceClient} from '../../account-service/account.service';
+import {Features} from '../../page/features';
 import {Msg} from '../../widgets/Msg';
+
+declare const features: Features;
  
 interface AccountPageProps {
 }
 
+interface FormFields {
+    readonly username?: string;
+    readonly firstName?: string;
+    readonly lastName?: string;
+    readonly email?: string;
+    readonly emailVerified?: boolean;
+}
+
 interface AccountPageState {
-    readonly changed: boolean,
-    readonly username: string,
-    readonly firstName?: string,
-    readonly lastName?: string,
-    readonly email?: string,
-    readonly emailVerified?: boolean
+    readonly canSubmit: boolean;
+    readonly formFields: FormFields
 }
 
 /**
  * @author Stan Silvert ssilvert@redhat.com (C) 2018 Red Hat Inc.
  */
 export class AccountPage extends React.Component<AccountPageProps, AccountPageState> {
-    readonly state: AccountPageState = {
-        changed: false,
-        username: '',
-        firstName: '',
-        lastName: '',
-        email: ''
+    private isRegistrationEmailAsUsername: boolean = features.isRegistrationEmailAsUsername;
+    private isEditUserNameAllowed: boolean = features.isEditUserNameAllowed;
+    
+    state: AccountPageState = {
+        canSubmit: false,
+        formFields: {username: '',
+                     firstName: '',
+                     lastName: '',
+                     email: ''}
     };
     
     constructor(props: AccountPageProps) {
         super(props);
         AccountServiceClient.Instance.doGet("/")
-            .then((response: AxiosResponse<AccountPageState>) => {
-                this.setState(response.data);
+            .then((response: AxiosResponse<FormFields>) => {
+                this.setState({formFields: response.data});
                 console.log({response});
             });
-            
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.makeTextInput = this.makeTextInput.bind(this);
     }
 
-    private handleChange(event: React.ChangeEvent<HTMLInputElement>): void  {
+    private handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
         const target: HTMLInputElement = event.target;
         const value: string = target.value;
         const name: string = target.name;
         this.setState({
-            changed: true,
-            username: this.state.username,
-            [name]: value
-        } as AccountPageState);
+            canSubmit: this.requiredFieldsHaveData(name, value),
+            formFields: {...this.state.formFields, [name]:value}
+        });
     }
     
-    private handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
+    private handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
         event.preventDefault();
-        const reqData = {...this.state};
-        delete reqData.changed;
+        const reqData: FormFields = {...this.state.formFields};
         AccountServiceClient.Instance.doPost("/", {data: reqData})
-            .then((response: AxiosResponse<AccountPageState>) => {
-                this.setState({changed: false});
+            .then((response: AxiosResponse<FormFields>) => {
+                this.setState({canSubmit: false});
                 alert('Data posted');
             });
     }
     
-    private makeTextInput(name: string, 
-                          disabled = false): React.ReactElement<any> {
-        return (
-            <label><Msg msgKey={name}/>:
-                <input disabled={disabled} type="text" name={name} value={this.state[name]} onChange={this.handleChange} />
-            </label>
-        );
+    private requiredFieldsHaveData(fieldName: string, newValue: string): boolean { 
+        const fields: FormFields = {...this.state.formFields};
+        fields[fieldName] = newValue;
+        for (const field of Object.keys(fields)) {
+            if (!fields[field]) return false;
+        }
+        
+        return true;
     }
     
     render() {
+        const fields: FormFields = this.state.formFields;
         return (
-            <form onSubmit={this.handleSubmit}>
-                {this.makeTextInput('username', true)}
-                <br/>
-                {this.makeTextInput('firstName')}
-                <br/>
-                {this.makeTextInput('lastName')}
-                <br/>
-                {this.makeTextInput('email')}
-                <br/>
-                <button className="btn btn-primary btn-lg btn-sign" 
-                        disabled={!this.state.changed}
-                        value="Submit"><Msg msgKey="doSave"/></button>
-            </form>
+<span>
+<div className="page-header">
+    <h1 id="pageTitle"><Msg msgKey="personalInfoHtmlTitle"/></h1>
+</div>
+
+<div className="col-sm-12 card-pf">
+  <div className="card-pf-body row">
+      <div className="col-sm-4 col-md-4">
+          <div className="card-pf-subtitle" id="personalSubTitle">
+              <Msg msgKey="personalSubTitle"/>
+          </div>
+          <div className="introMessage" id="personalSubMessage">
+            <p><Msg msgKey="personalSubMessage"/></p>
+          </div>
+          <div className="subtitle" id="requiredFieldMessage"><span className="required">*</span> <Msg msgKey="requiredFields"/></div>
+      </div>
+      
+      <div className="col-sm-6 col-md-6">
+        <form onSubmit={this.handleSubmit} className="form-horizontal">
+
+          { !this.isRegistrationEmailAsUsername &&
+            <div className="form-group ">
+                <label htmlFor="username" className="control-label"><Msg msgKey="username" /></label>{this.isEditUserNameAllowed && <span className="required">*</span>}
+                {this.isEditUserNameAllowed && <this.UsernameInput/>}
+                {!this.isEditUserNameAllowed && <this.RestrictedUsernameInput/>}
+            </div>
+          }
+
+          <div className="form-group ">
+            <label htmlFor="email" className="control-label"><Msg msgKey="email"/></label> <span className="required">*</span>
+            <input type="email" className="form-control" id="email" name="email" required autoFocus onChange={this.handleChange} value={fields.email}/>
+          </div>
+
+          <div className="form-group ">
+            <label htmlFor="firstName" className="control-label"><Msg msgKey="firstName"/></label> <span className="required">*</span>
+            <input className="form-control" id="firstName" required name="firstName" type="text" onChange={this.handleChange} value={fields.firstName}/>
+          </div>
+
+          <div className="form-group ">
+            <label htmlFor="lastName" className="control-label"><Msg msgKey="lastName"/></label> <span className="required">*</span>
+            <input className="form-control" id="lastName" required name="lastName" type="text" onChange={this.handleChange} value={fields.lastName}/>
+          </div>
+
+          <div className="form-group">
+            <div id="kc-form-buttons" className="submit">
+              <div className="">
+                <button disabled={!this.state.canSubmit} 
+                        type="submit" className="btn btn-primary btn-lg" 
+                        name="submitAction"><Msg msgKey="doSave"/></button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</span>
         );
     }
+    
+    UsernameInput = () => (
+        <input type="text" className="form-control" required id="username" name="username" onChange={this.handleChange} value={this.state.formFields.username} />
+    );
+    
+    RestrictedUsernameInput = () => (
+        <div className="non-edit" id="username">{this.state.formFields.username}</div>
+    );
+    
 };
