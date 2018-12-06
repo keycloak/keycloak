94c99caf473d29fa9be1c500607c121d4f9bebc1
Domenico Briganti <domenico.briganti@osys.it>
2018-12-06 17:04:52 +0100

    Not hide exception in email templating

--- server-spi-private/src/main/java/org/keycloak/email/EmailException.java
+++ server-spi-private/src/main/java/org/keycloak/email/EmailException.java
@@ -20,10 +20,15 @@ package org.keycloak.email;
 /**
  * @author <a href="mailto:sthorger@redhat.com">Stian Thorgersen</a>
  */
+@SuppressWarnings("serial")
 public class EmailException extends Exception {
 
     public EmailException(Throwable cause) {
         super(cause);
+	}
+
+	public EmailException(String message) {
+		super(message);
     }
 
     public EmailException(String message, Throwable cause) {
