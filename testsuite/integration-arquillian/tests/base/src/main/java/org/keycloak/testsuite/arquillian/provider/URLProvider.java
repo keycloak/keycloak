61758655c5122d015c505a26551eab06f6596575
mhajas <mhajas@redhat.com>
2019-03-11 11:54:07 +0100

    KEYCLOAK-9893 Use SSL in EAP6, add / to url for EAP6 deployment

--- testsuite/integration-arquillian/tests/base/src/main/java/org/keycloak/testsuite/arquillian/provider/URLProvider.java
+++ testsuite/integration-arquillian/tests/base/src/main/java/org/keycloak/testsuite/arquillian/provider/URLProvider.java
@@ -70,7 +70,7 @@ public class URLProvider extends URLResourceProvider {
             try {
                 for (Annotation a : qualifiers) {
                     if (OperateOnDeployment.class.isAssignableFrom(a.annotationType())) {
-                        return new URL(protocol + "://localhost:" + port + "/" + ((OperateOnDeployment) a).value());
+                        return new URL(protocol + "://localhost:" + port + "/" + ((OperateOnDeployment) a).value() + "/");
                     }
                 }
             } catch (MalformedURLException ex) {
