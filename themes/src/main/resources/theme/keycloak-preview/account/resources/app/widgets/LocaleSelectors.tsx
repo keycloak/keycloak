749052b55bbcd0c780a39d6b1bd8e680ad0c3516
Stan Silvert <ssilvert@redhat.com>
2019-04-12 12:52:30 -0400

    KEYCLOAK-9644: Implement Nav and Headers using PF4 React

---
+++ themes/src/main/resources/theme/keycloak-preview/account/resources/app/widgets/LocaleSelectors.tsx
@@ -0,0 +1,126 @@
+/* 
+ * Copyright 2019 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as React from 'react';
+import {withRouter, RouteComponentProps} from 'react-router-dom';
+
+import {Msg} from './Msg';
+
+import {
+    Dropdown,
+    DropdownItem,
+    DropdownToggle,
+    Nav,
+    NavExpandable,
+    NavItem,
+    NavList
+} from '@patternfly/react-core';
+
+declare const locale: string;
+declare const baseUrl: string;
+declare const referrer: string;
+declare const referrerUri: string
+
+interface AvailableLocale {
+    locale: string; 
+    label: string;
+};
+declare const availableLocales: [AvailableLocale];
+// remove entry for current locale
+const availLocales = availableLocales.filter((availableLocale: AvailableLocale) => availableLocale.locale !== locale);
+
+let referrerFragment = '';
+if ((typeof referrer !== 'undefined') && 
+    (typeof referrerUri !== 'undefined')) {
+    referrerFragment = '&referrer=' + referrer + '&referrer_uri=' + encodeURIComponent(referrerUri);
+}
+    
+interface LocaleKebabItemProps extends RouteComponentProps {}
+interface LocaleKebabItemState {activeGroup: string; activeItem: string}
+class LocaleKebabItem extends React.Component<LocaleKebabItemProps, LocaleKebabItemState> {
+    public constructor(props: LocaleKebabItemProps) {
+        super(props);
+        this.state = {
+            activeGroup: 'locale-group',
+            activeItem: ''
+        };
+    }
+     
+    public render(): React.ReactNode {        
+        const appPath = this.props.location.pathname;
+        const localeNavItems = availLocales.map((availableLocale: AvailableLocale) => {
+            const url = baseUrl + '?kc_locale=' + availableLocale.locale + referrerFragment + '#' + appPath;
+            return (<NavItem key={availableLocale.locale} to={url}>
+                        {availableLocale.label}
+                    </NavItem> );
+        });
+        
+        return (
+            <Nav>
+                <NavList>
+                    <NavExpandable title={Msg.localize('locale_' + locale)} isActive={false} groupId="locale-group">
+                    {localeNavItems}
+                  </NavExpandable>
+                </NavList>
+            </Nav>
+        );
+    }
+};
+
+interface LocaleDropdownComponentProps extends RouteComponentProps {}
+interface LocaleDropdownComponentState {isDropdownOpen: boolean}
+class LocaleDropdownComponent extends React.Component<LocaleDropdownComponentProps, LocaleDropdownComponentState> {
+    public constructor(props: LocaleDropdownComponentProps) {
+        super(props);
+        this.state = {isDropdownOpen: false};
+    }
+     
+    private onDropdownToggle = (isDropdownOpen: boolean) => {
+        this.setState({
+            isDropdownOpen
+        });
+    };
+
+    private onDropdownSelect = () => {
+        this.setState({
+            isDropdownOpen: !this.state.isDropdownOpen
+        });
+    };
+
+    public render(): React.ReactNode {
+        const appPath = this.props.location.pathname;
+        const localeDropdownItems = availLocales.map((availableLocale: AvailableLocale) => {
+            const url = baseUrl + '?kc_locale=' + availableLocale.locale + referrerFragment + '#' + appPath;
+            return (<DropdownItem key={availableLocale.locale} href={url}>
+                        {availableLocale.label}
+                    </DropdownItem> );
+        });
+        
+        return (
+            <Dropdown
+                isPlain
+                position="right"
+                onSelect={this.onDropdownSelect}
+                isOpen={this.state.isDropdownOpen}
+                toggle={<DropdownToggle onToggle={this.onDropdownToggle}><Msg msgKey={'locale_' + locale}/></DropdownToggle>}
+                dropdownItems={localeDropdownItems}
+            />
+        );
+    }
+};
+
+export const LocaleDropdown = withRouter(LocaleDropdownComponent);
+export const LocaleNav = withRouter(LocaleKebabItem);
--- themes/src/main/resources/theme/keycloak-preview/account/resources/systemjs.config.js
+++ themes/src/main/resources/theme/keycloak-preview/account/resources/systemjs.config.js
@@ -18,13 +18,29 @@
       'react-dom': 'npm:react-dom/umd/react-dom.development.js',
       'react-router-dom': 'npm:react-router-dom/umd/react-router-dom.js',
       
-      '@patternfly': 'npm:@patternfly',
-      'react-styles': "react-styles/loader.js",
-      //'patternfly-react-core-button': 'patternflyreactcore:js/components/Button/index.js',
+      //'@patternfly/patternfly': 'npm:@patternfly/patternfly',
+      '@patternfly/patternfly': 'npm:@patternfly/react-core/dist/umd/@patternfly/patternfly',
+      '@patternfly/react-core': 'npm:@patternfly/react-core/dist/umd/index.js',
+      '@patternfly/react-styles': 'npm:@patternfly/react-styles/dist/umd/index.js',
+      '@patternfly/react-icons': 'npm:@patternfly/react-icons/dist/umd/index.js',
+      '@patternfly/react-tokens': 'npm:@patternfly/react-tokens/dist/umd/index.js',
+      'emotion': 'npm:emotion/dist/emotion.umd.min.js',
+      'camel-case': 'npm:camel-case/camel-case.js',
+      'upper-case': 'npm:upper-case/upper-case.js',
+      'no-case': 'npm:no-case/no-case.js',
+      'lower-case': 'npm:lower-case/lower-case.js',
+      'prop-types': 'npm:prop-types/prop-types.min.js',
+      'exenv': 'npm:exenv/index.js',
+      'focus-trap': 'npm:focus-trap/dist/focus-trap.min.js',
+      'focus-trap-react': 'npm:focus-trap-react/dist/focus-trap-react.js',
+      '@tippy.js/react': 'npm:@tippy.js/react/dist/Tippy.min.js',
+      'tippy.js': 'npm:tippy.js/dist/tippy.min.js',
       
       'moment': 'npm:moment/min/moment-with-locales.min.js',
       
       'axios': 'npm:axios/dist/axios.min.js',
+      
+      'history': 'npm:history/umd/history.min.js',
     },
     
     bundles: {
@@ -50,11 +66,99 @@
           }
         }
       },
-      
       rxjs: {
         defaultExtension: false
       },
+      'npm:@patternfly/react-core/dist/umd/components': {
+          main: './index.js',
+          defaultExtension: 'js',
+          map: {
+              './Alert': './Alert/index.js',
+              './AboutModal': './AboutModal/index.js',
+              './ApplicationLauncher': './ApplicationLauncher/index.js',
+              './Avatar': './Avatar/index.js',
+              './Backdrop': './Backdrop/index.js',
+              './BackgroundImage': './BackgroundImage/index.js',
+              './Badge': './Badge/index.js',
+              './Brand': './Brand/index.js',
+              './Breadcrumb': './Breadcrumb/index.js',
+              './Button': './Button/index.js',
+              './Card': './Card/index.js',
+              './Checkbox': './Checkbox/index.js',
+              './ChipGroup': './ChipGroup/index.js',
+              './ContextSelector': './ContextSelector/index.js',
+              './DataList': './DataList/index.js',
+              './Dropdown': './Dropdown/index.js',
+              './EmptyState': './EmptyState/index.js',
+              './Form': './Form/index.js',
+              './FormSelect': './FormSelect/index.js',
+              './InputGroup': './InputGroup/index.js',
+              './Label': './Label/index.js',
+              './List': './List/index.js',
+              './LoginPage': './LoginPage/index.js',
+              './Modal': './Modal/index.js',
+              './Nav': './Nav/index.js',
+              './Page': './Page/index.js',
+              './Popover': './Popover/index.js',
+              './Progress': './Progress/index.js',
+              './Pagination': './Pagination/index.js',
+              './Radio': './Radio/index.js',
+              './Select': './Select/index.js',
+              './Switch': './Switch/index.js',
+              './Tabs': './Tabs/index.js',
+              './Text': './Text/index.js',
+              './TextArea': './TextArea/index.js',
+              './TextInput': './TextInput/index.js',
+              './Title': './Title/index.js',
+              './Tooltip': './Tooltip/index.js',
+              './Wizard': './Wizard/index.js',
+              './Bullseye': './Bullseye/index.js',
+              './Gallery': './Gallery/index.js',
+              './Grid': './Grid/index.js',
+              './Level': './Level/index.js',
+              './Split': './Split/index.js',
+              './Stack': './Stack/index.js',
+              './Toolbar': './Toolbar/index.js',
+          }
+      },
       
+      'npm:@patternfly/react-core/dist/umd/styles': {
+          main: './index.js',
+          defaultExtension: 'js',
+      },
+      'npm:@patternfly/react-core/dist/umd/helpers': {
+          main: './index.js',
+          defaultExtension: 'js',
+      },
+      'npm:@patternfly/react-core/dist/umd/layouts': {
+          main: './index.js',
+          defaultExtension: 'js',
+          map: {
+              './Bullseye': './Bullseye/index.js',
+              './Gallery': './Gallery/index.js',
+              './Level': './Level/index.js',
+              './Grid': './Grid/index.js',
+              './Stack': './Stack/index.js',
+              './Split': './Split/index.js',
+              './Toolbar': './Toolbar/index.js',
+          }
+      },
+      'npm:@patternfly/react-core/dist/umd/@patternfly/patternfly/utilities/Accessibility': {
+          defaultExtension: 'js',
+          map: {
+             './accessibility.css': './accessibility.css.js'
+          }
+      },
+      'npm:@patternfly/react-icons/dist/umd': {
+          main: './index.js',
+          defaultExtension: 'js',
+      },
+      'npm:@patternfly/react-styles/dist/umd': {
+          defaultExtension: 'js',
+      },
+      'npm:no-case/vendor': {
+          defaultExtension: 'js',
+      },
     }
   });
 })(this);
