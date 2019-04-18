57c21bc8318d8c9b7a714051ea20ec0b866af384
Alan Balbo <alan.balbo@gmail.com>
2019-04-18 10:13:39 +0200

    KEYCLOAK-10097 Fix register method in javascript adatper for cordova

--- themes/src/main/resources/theme/keycloak-preview/account/resources/app/keycloak-service/keycloak.js
+++ themes/src/main/resources/theme/keycloak-preview/account/resources/app/keycloak-service/keycloak.js
@@ -1270,14 +1270,20 @@
                         return promise.promise;
                     },
 
-                    register : function() {
+
+                    register : function(options) {
+                        var promise = createPromise(false);
                         var registerUrl = kc.createRegisterUrl();
-                        var ref = cordovaOpenWindowWrapper(registerUrl, '_blank', 'location=no');
+                        var cordovaOptions = createCordovaOptions(options);
+                        var ref = cordovaOpenWindowWrapper(registerUrl, '_blank', cordovaOptions);
                         ref.addEventListener('loadstart', function(event) {
                             if (event.url.indexOf('http://localhost') == 0) {
                                 ref.close();
+                                var oauth = parseCallback(event.url);
+                                processCallback(oauth, promise);
                             }
                         });
+                        return promise.promise;
                     },
 
                     accountManagement : function() {
