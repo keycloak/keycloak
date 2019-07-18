bb816161271e4db505774f019269aeb701e686f3
Gilbert Fernandes <g_fernandes@mousquetaires.com>
2019-07-18 10:56:54 +0200

    When switching to PLAIN in PKCE look for both allowed values
    Instead of checking for S256 in code_challenge_method and switching to PLAIN if S256 was not found, check for S256 then PLAIN and report to console as WARNING when none of the allowed values has been found. This helps detecting client programmer errors when they don't set the code_challenge_method properly, think they are using S256 and get PKCE errors because Keycloak switched to PLAIN and only report this when DEBUG level is set (which we usually do not do in production of very busy servers).

--- services/src/main/java/org/keycloak/protocol/oidc/endpoints/TokenEndpoint.java
+++ services/src/main/java/org/keycloak/protocol/oidc/endpoints/TokenEndpoint.java
@@ -455,12 +455,19 @@ public class TokenEndpoint {
         try {
             // https://tools.ietf.org/html/rfc7636#section-4.2
             // plain or S256
-            if (codeChallengeMethod != null && codeChallengeMethod.equals(OAuth2Constants.PKCE_METHOD_S256)) {
-                logger.debugf("PKCE codeChallengeMethod = %s", codeChallengeMethod);
-                codeVerifierEncoded = generateS256CodeChallenge(codeVerifier);
-            } else {
-                logger.debug("PKCE codeChallengeMethod is plain");
-                codeVerifierEncoded = codeVerifier;
+            if(codeChallengeMethod != null) {
+                if(codeChallengeMethod.equals(OAuth2Constants.PKCE_METHOD_S256))) {
+                    logger.debugf("PKCE codeChallengeMethod = %s", codeChallengeMethod);
+                    codeVerifierEncoded = generateS256CodeChallenge(codeVerifier);
+                }
+                else if(codeChallengeMethod.equals(OAuth2Constants.PKCE_METHOD_PLAIN) {
+                    logger.debug("PKCE codeChallengeMethod is plain");
+                    codeVerifierEncoded = codeVerifier;
+                }
+                else {
+                    logger.warn("PKCE codeChallengeMethod neither S256 nor PLAIN : using PLAIN (missing code_challenge_method in headers ?)");
+                    codeVerifierEncoded = codeVerifier;
+                }
             }
         } catch (Exception nae) {
             logger.infof("PKCE code verification failed, not supported algorithm specified");
