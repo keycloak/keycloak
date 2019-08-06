762efb16e5b5147dd3d37ad0d98a4644b72b20a2
Bruno Oliveira da Silva <bruno@abstractj.org>
2019-08-06 16:10:38 -0300

    Revert

--- travis-run-tests.sh
+++ travis-run-tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
-cd testsuite/integration-arquillian
-mvn install -B -nsu -Pauth-server-wildfly -DskipTests
-
 function run-server-tests() {
+    cd testsuite/integration-arquillian
+    mvn install -B -nsu -Pauth-server-wildfly -DskipTests
+
     cd tests/base
     mvn test -B -nsu -Pauth-server-wildfly -Dtest=$1 $2 2>&1 | java -cp ../../../utils/target/classes org.keycloak.testsuite.LogTrimmer
     exit ${PIPESTATUS[0]}
