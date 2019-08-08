3e064b90ef362b87ba3002c56619c7dcc5d03fe4
Dmitry Telegin <dmitryt@backbase.com>
2019-08-08 12:57:33 +0300

    - Resteasy 4 - obtain ServletContext and Dispatcher from Resteasy - create and bind KeycloakSessionFactory - logging

--- server-quarkus/src/main/java/org/keycloak/services/resources/KeycloakApplication.java
+++ server-quarkus/src/main/java/org/keycloak/services/resources/KeycloakApplication.java
@@ -28,6 +28,13 @@ import javax.ws.rs.core.Application;
 import java.io.IOException;
 import java.net.URL;
 import java.util.*;
+import javax.servlet.ServletContext;
+import org.jboss.logging.Logger;
+import org.jboss.resteasy.core.ResteasyContext;
+import org.jboss.resteasy.spi.Dispatcher;
+import org.jboss.resteasy.spi.ResteasyProviderFactory;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.services.DefaultKeycloakSessionFactory;
 
 /**
  * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
@@ -36,12 +43,30 @@ import java.util.*;
 @ApplicationPath("/auth")
 public class KeycloakApplication extends Application {
 
+    private static final Logger LOGGER = Logger.getLogger(KeycloakApplication.class);
+
     protected Set<Object> singletons = new HashSet<Object>();
     protected Set<Class<?>> classes = new HashSet<Class<?>>();
 
+    protected KeycloakSessionFactory sessionFactory;
+    protected String contextPath;
 
     public KeycloakApplication() {
+
+        LOGGER.info("Keycloak.next starting up...");
         loadConfig();
+
+        // FIXME: injecting @Context constructor params does not work in Quarkus
+        ResteasyProviderFactory resteasy = ResteasyProviderFactory.getInstance();
+        ServletContext context = resteasy.getContextData(ServletContext.class);
+        Dispatcher dispatcher = resteasy.getContextData(Dispatcher.class);
+
+        this.contextPath = context.getContextPath();
+        this.sessionFactory = createSessionFactory();
+
+        dispatcher.getDefaultContextObjects().put(KeycloakApplication.class, this);
+        ResteasyContext.pushContext(KeycloakApplication.class, this); // for injection
+        context.setAttribute(KeycloakSessionFactory.class.getName(), this.sessionFactory);
 
         singletons.add(new RobotsResource());
         singletons.add(new RealmsResource());
@@ -49,7 +74,7 @@ public class KeycloakApplication extends Application {
         classes.add(ThemeResource.class);
         classes.add(JsResource.class);
 
-        System.out.println("Master realm: " + Config.getAdminRealm());
+        LOGGER.infov("Master realm: {0}", Config.getAdminRealm());
     }
 
     @Override
@@ -82,4 +107,11 @@ public class KeycloakApplication extends Application {
             throw new RuntimeException("Failed to load config", e);
         }
     }
+
+    public static KeycloakSessionFactory createSessionFactory() {
+        DefaultKeycloakSessionFactory factory = new DefaultKeycloakSessionFactory();
+        factory.init();
+        return factory;
+    }
+
 }
