a1679bd29d2fe061e5e02f74569a14ba94ed3b99
Peter Skopek <pskopek@redhat.com>
2019-08-15 16:22:12 +0200

    KEYCLOAK-8785 remove k_version endpoint

--- testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/adapter/example/cors/CorsExampleAdapterTest.java
+++ testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/adapter/example/cors/CorsExampleAdapterTest.java
@@ -177,8 +177,6 @@ public class CorsExampleAdapterTest extends AbstractExampleAdapterTest {
         jsDriverAngularCorsProductPage.navigateTo();
         waitForPageToLoad();
 
-        jsDriverAngularCorsProductPage.loadVersion();
-        waitUntilElement(jsDriverAngularCorsProductPage.getOutput()).text().contains("Keycloak version: " + serverVersion);
     }
 
     @Nullable
