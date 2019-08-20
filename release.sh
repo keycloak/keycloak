82b3e36c878b5b33e3a9ce7ed37e35908823cfe2
Stian Thorgersen <stianst@gmail.com>
2019-08-20 15:44:21 +0200

    Update release.sh

--- release.sh
+++ release.sh
@@ -16,7 +16,7 @@ echo "--------------------------------------------------------------------------
 echo "Deploying:"
 echo ""
 
-mvn -Pjboss-release,nexus-staging -DretryFailedDeploymentCount=10 -DskipTests -DskipTestsuite -DskipExamples -DautoReleaseAfterClose=false deploy
+mvn -Pjboss-release,nexus-staging -DretryFailedDeploymentCount=10 -DskipTests -DskipTestsuite -DskipExamples -DautoReleaseAfterClose=true deploy
 
 
 echo "------------------------------------------------------------------------------------------------------------"
