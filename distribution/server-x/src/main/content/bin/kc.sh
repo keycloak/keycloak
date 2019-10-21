14dcd9f41e92d5b6b93f6d9c7c32f354df1d5bb1
MatthewC <matthewc@backbase.com>
2019-10-21 13:06:37 +0100

    KEYCLOAK-11325 - Quarkus distribution

---
+++ distribution/server-x/src/main/content/bin/kc.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+case "`uname`" in
+    CYGWIN*)
+        CFILE = `cygpath "$0"`
+        RESOLVED_NAME=`readlink -f "$CFILE"`
+        ;;
+    Darwin*)
+        RESOLVED_NAME=`readlink "$0"`
+        ;;
+    FreeBSD)
+        RESOLVED_NAME=`readlink -f "$0"`
+        ;;
+    Linux)
+        RESOLVED_NAME=`readlink -f "$0"`
+        ;;
+esac
+
+if [ "x$RESOLVED_NAME" = "x" ]; then
+    RESOLVED_NAME="$0"
+fi
+
+DIRNAME=`dirname "$RESOLVED_NAME"`
+java -Dkeycloak.theme.dir=$DIRNAME/../themes -cp "$DIRNAME/../providers/*:$DIRNAME/../lib/keycloak-runner.jar" io.quarkus.runner.GeneratedMain "$@"
---
+++ distribution/server-x/src/main/content/providers/README.txt
@@ -0,0 +1,1 @@
+Copy custom providers into this directory then restart the Keycloak server.
