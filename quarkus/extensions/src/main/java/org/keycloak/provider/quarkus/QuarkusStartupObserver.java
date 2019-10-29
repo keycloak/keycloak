11f761ec1fb317cf337c465cbfd316df9d458175
Dmitry Telegin <dmitryt@backbase.com>
2019-10-29 15:41:38 +0300

    Deferred initialization - Quarkus impl

---
+++ quarkus/extensions/src/main/java/org/keycloak/provider/quarkus/QuarkusStartupObserver.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2019 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.keycloak.provider.quarkus;
+
+import io.quarkus.runtime.StartupEvent;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+
+@ApplicationScoped
+public class QuarkusStartupObserver {
+
+    private Runnable command;
+
+    public void setCommand(Runnable command) {
+        this.command = command;
+    }
+
+    private void startupEvent(@Observes StartupEvent event) {
+        if (command != null) {
+            command.run();
+        }
+    }
+
+}
