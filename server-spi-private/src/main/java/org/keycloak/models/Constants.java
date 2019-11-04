63abebd9934c1a3aa4803645a6b30d25d73d1035
stianst <stianst@gmail.com>
2019-11-04 12:20:58 +0100

    KEYCLOAK-11627 Require users to re-authenticate before invoking AIA

--- server-spi-private/src/main/java/org/keycloak/models/Constants.java
+++ server-spi-private/src/main/java/org/keycloak/models/Constants.java
@@ -67,7 +67,10 @@ public final class Constants {
     public static final String CLIENT_ID = "client_id";
     public static final String TAB_ID = "tab_id";
     public static final String KEY = "key";
+
     public static final String KC_ACTION = "kc_action";
+    public static final int KC_ACTION_MAX_AGE = 300;
+
     public static final String IS_AIA_REQUEST = "IS_AIA_REQUEST";
     public static final String AIA_SILENT_CANCEL = "silent_cancel";
 
