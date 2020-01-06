a2b3747d0e0305d1a6d60b62ed2f46898b407933
vramik <vramik@redhat.com>
2020-01-06 15:53:40 +0100

    KEYCLOAK-7014 - Correctly handle null-values in UserAttributes

--- model/jpa/src/main/java/org/keycloak/models/jpa/UserAdapter.java
+++ model/jpa/src/main/java/org/keycloak/models/jpa/UserAdapter.java
@@ -51,6 +51,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.Objects;
+import java.util.stream.Stream;
 import javax.persistence.LockModeType;
 
 /**
@@ -71,6 +72,7 @@ public class UserAdapter implements UserModel, JpaModel<UserEntity> {
         this.session = session;
     }
 
+    @Override
     public UserEntity getEntity() {
         return user;
     }
@@ -113,32 +115,35 @@ public class UserAdapter implements UserModel, JpaModel<UserEntity> {
 
     @Override
     public void setSingleAttribute(String name, String value) {
-        String firstExistingAttrId = null;
-        List<UserAttributeEntity> toRemove = new ArrayList<>();
-        for (UserAttributeEntity attr : user.getAttributes()) {
-            if (attr.getName().equals(name)) {
-                if (firstExistingAttrId == null) {
-                    attr.setValue(value);
-                    firstExistingAttrId = attr.getId();
-                } else {
-                    toRemove.add(attr);
+        if (value == null) {
+            user.getAttributes().removeIf(a -> a.getName().equals(name));
+        } else {
+            String firstExistingAttrId = null;
+            List<UserAttributeEntity> toRemove = new ArrayList<>();
+            for (UserAttributeEntity attr : user.getAttributes()) {
+                if (attr.getName().equals(name)) {
+                    if (firstExistingAttrId == null) {
+                        attr.setValue(value);
+                        firstExistingAttrId = attr.getId();
+                    } else {
+                        toRemove.add(attr);
+                    }
                 }
             }
-        }
 
-        if (firstExistingAttrId != null) {
-            // Remove attributes through HQL to avoid StaleUpdateException
-            Query query = em.createNamedQuery("deleteUserAttributesByNameAndUserOtherThan");
-            query.setParameter("name", name);
-            query.setParameter("userId", user.getId());
-            query.setParameter("attrId", firstExistingAttrId);
-            int numUpdated = query.executeUpdate();
+            if (firstExistingAttrId != null) {
+                // Remove attributes through HQL to avoid StaleUpdateException
+                Query query = em.createNamedQuery("deleteUserAttributesByNameAndUserOtherThan");
+                query.setParameter("name", name);
+                query.setParameter("userId", user.getId());
+                query.setParameter("attrId", firstExistingAttrId);
+                int numUpdated = query.executeUpdate();
 
-            // Remove attribute from local entity
-            user.getAttributes().removeAll(toRemove);
-        } else {
-
-            persistAttributeValue(name, value);
+                // Remove attribute from local entity
+                user.getAttributes().removeAll(toRemove);
+            } else {
+                persistAttributeValue(name, value);
+            }
         }
     }
 
@@ -146,10 +151,8 @@ public class UserAdapter implements UserModel, JpaModel<UserEntity> {
     public void setAttribute(String name, List<String> values) {
         // Remove all existing
         removeAttribute(name);
-
-        // Put all new
-        for (String value : values) {
-            persistAttributeValue(name, value);
+        for (Iterator<String> it = values.stream().filter(Objects::nonNull).iterator(); it.hasNext();) {
+            persistAttributeValue(name, it.next());
         }
     }
 
