a767a8d2a261ae04350564f9a3a8fd8bc5da6d00
Clement Cureau <clement.cureau@cdiscount.com>
2020-01-16 15:33:13 +0100

    [KEYCLOAK-11621] Allow user creation via group permissions (Admin API)
    Problem:
    Using fine-grained admin permissions on groups, it is not permitted to create new users
    within a group.
    
    Cause:
    The POST /{realm}/users API does not check permission for each group part of the new
    user representation
    
    Solution:
    - Change access logic for POST /{realm}/users to require MANAGE_MEMBERS and
    MANAGE_MEMBERSHIP permissions on each of the incoming groups
    
    Tests:
    Manual API testing performed:
      1. admin user from master realm:
        - POST /{realm}/users without groups                  => HTTP 201 user created
        - POST /{realm}/users with groups                     => HTTP 201 user created
      2. user with MANAGE_MEMBERS & MANAGE_MEMBERSHIP permissions on group1
        - POST /{realm}/users without groups                  => HTTP 403 user NOT created
        - POST /{realm}/users with group1                     => HTTP 201 user created
        - POST /{realm}/users with group1 & group2            => HTTP 403 user NOT created
        - POST /{realm}/users with group1 & wrong group path  => HTTP 400 user NOT created
      3. user with MANAGE_MEMBERS permission on group1
        - POST /{realm}/users without groups                  => HTTP 403 user NOT created
        - POST /{realm}/users with group1                     => HTTP 403 user NOT created
        - POST /{realm}/users with group1 & group2            => HTTP 403 user NOT created
        - POST /{realm}/users with group1 & wrong group path  => HTTP 400 user NOT created

--- services/src/main/java/org/keycloak/services/resources/admin/permissions/GroupPermissions.java
+++ services/src/main/java/org/keycloak/services/resources/admin/permissions/GroupPermissions.java
@@ -368,6 +368,13 @@ class GroupPermissions implements GroupPermissionEvaluator, GroupPermissionManag
     }
 
     @Override
+    public void requireManageMembers(GroupModel group) {
+        if (!canManageMembers(group)) {
+            throw new ForbiddenException();
+        }
+    }
+
+    @Override
     public Map<String, Boolean> getAccess(GroupModel group) {
         Map<String, Boolean> map = new HashMap<>();
         map.put("view", canView(group));
