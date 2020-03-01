c057b994e7dc9afe5d639bf184ae625f88658d55
Takashi Norimatsu <takashi.norimatsu.ws@hitachi.com>
2020-03-01 21:01:46 +0900

    KEYCLOAK-13104 Signed and Encrypted ID Token Support : AES 192bit and 256bit key support

--- core/src/main/java/org/keycloak/jose/jwe/JWEConstants.java
+++ core/src/main/java/org/keycloak/jose/jwe/JWEConstants.java
@@ -29,6 +29,9 @@ public class JWEConstants {
 
     public static final String A128CBC_HS256 = "A128CBC-HS256";
     public static final String A192CBC_HS384 = "A192CBC-HS384";
-    public static final String A256CBC_HS512  = "A256CBC-HS512";
+    public static final String A256CBC_HS512 = "A256CBC-HS512";
     public static final String A128GCM = "A128GCM";
+    public static final String A192GCM = "A192GCM";
+    public static final String A256GCM = "A256GCM";
+
 }
