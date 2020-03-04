75a772f52b66d9d7deaf3b0226226a3ea139d393
stianst <stianst@gmail.com>
2020-03-04 12:55:38 +0100

    KEYCLOAK-10967 Add JSON body methods for test ldap and smtp connections. Deprecate old form based methods.

---
+++ core/src/main/java/org/keycloak/representations/idm/TestLdapConnectionRepresentation.java
@@ -0,0 +1,90 @@
+package org.keycloak.representations.idm;
+
+public class TestLdapConnectionRepresentation {
+
+    private String action;
+    private String connectionUrl;
+    private String bindDn;
+    private String bindCredential;
+    private String useTruststoreSpi;
+    private String connectionTimeout;
+    private String componentId;
+    private String startTls;
+
+    public TestLdapConnectionRepresentation() {
+    }
+
+    public TestLdapConnectionRepresentation(String action, String connectionUrl, String bindDn, String bindCredential, String useTruststoreSpi, String connectionTimeout) {
+        this.action = action;
+        this.connectionUrl = connectionUrl;
+        this.bindDn = bindDn;
+        this.bindCredential = bindCredential;
+        this.useTruststoreSpi = useTruststoreSpi;
+        this.connectionTimeout = connectionTimeout;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getConnectionUrl() {
+        return connectionUrl;
+    }
+
+    public void setConnectionUrl(String connectionUrl) {
+        this.connectionUrl = connectionUrl;
+    }
+
+    public String getBindDn() {
+        return bindDn;
+    }
+
+    public void setBindDn(String bindDn) {
+        this.bindDn = bindDn;
+    }
+
+    public String getBindCredential() {
+        return bindCredential;
+    }
+
+    public void setBindCredential(String bindCredential) {
+        this.bindCredential = bindCredential;
+    }
+
+    public String getUseTruststoreSpi() {
+        return useTruststoreSpi;
+    }
+
+    public void setUseTruststoreSpi(String useTruststoreSpi) {
+        this.useTruststoreSpi = useTruststoreSpi;
+    }
+
+    public String getConnectionTimeout() {
+        return connectionTimeout;
+    }
+
+    public void setConnectionTimeout(String connectionTimeout) {
+        this.connectionTimeout = connectionTimeout;
+    }
+
+    public String getComponentId() {
+        return componentId;
+    }
+
+    public void setComponentId(String componentId) {
+        this.componentId = componentId;
+    }
+
+    public String getStartTls() {
+        return startTls;
+    }
+
+    public void setStartTls(String startTls) {
+        this.startTls = startTls;
+    }
+
+}
