0d83ce4dd2119ee530d897be011098d4a84eb31b
Martin Bartos <mabartos@redhat.com>
2020-03-27 10:04:23 +0100

    [KEYCLOAK-13572] Doesn't observe After events due to assume check

--- testsuite/integration-arquillian/tests/base/src/main/java/org/keycloak/testsuite/arquillian/annotation/EnableFeature.java
+++ testsuite/integration-arquillian/tests/base/src/main/java/org/keycloak/testsuite/arquillian/annotation/EnableFeature.java
@@ -18,7 +18,26 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Repeatable(EnableFeatures.class)
 @Inherited
 public @interface EnableFeature {
+
+    /**
+     * Feature, which should be enabled.
+     */
     Profile.Feature value();
+
+    /**
+     * The feature will be enabled without restarting of a server.
+     */
     boolean skipRestart() default false;
+
+    /**
+     * The feature will be enabled only if the `product` profile is activated
+     */
     boolean onlyForProduct() default false;
+
+    /**
+     * Feature enable should be the last action in @Before context.
+     * If the test halted, the feature is returned to the previous state.
+     * If it's false, feature will be enabled before @Before method.
+     */
+    boolean executeAsLast() default true;
 }
