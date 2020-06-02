d962a42f561acf3cb9b0734ae56cfbb4a696bdf4
mposolda <mposolda@gmail.com>
2020-06-02 17:35:05 +0200

    KEYCLOAK-10162 Fix OIDCAdapterClusterTest with JDK 1.7.0_79

--- common/src/main/java/org/keycloak/common/util/DelegatingSerializationFilter.java
+++ common/src/main/java/org/keycloak/common/util/DelegatingSerializationFilter.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class DelegatingSerializationFilter {
     private static final Logger LOG = Logger.getLogger(DelegatingSerializationFilter.class.getName());
 
-    private static final SerializationFilterAdapter serializationFilterAdapter = isJava6To8() ? new OnJava6To8() : new OnJavaAfter8();
+    private static final SerializationFilterAdapter serializationFilterAdapter = isJava6To8() ? createOnJava6To8Adapter() : new OnJavaAfter8();
 
     private static boolean isJava6To8() {
         List<String> olderVersions = Arrays.asList("1.6", "1.7", "1.8");
@@ -19,7 +19,6 @@ public class DelegatingSerializationFilter {
     }
 
     public void setFilter(ObjectInputStream ois, String filterPattern) {
-
         LOG.info("Using: " + serializationFilterAdapter.getClass().getSimpleName());
 
         if (serializationFilterAdapter.getObjectInputFilter(ois) == null) {
@@ -34,36 +33,35 @@ public class DelegatingSerializationFilter {
         void setObjectInputFilter(ObjectInputStream ois, String filterPattern);
     }
 
+    private static SerializationFilterAdapter createOnJava6To8Adapter() {
+        try {
+            ClassLoader cl = Thread.currentThread().getContextClassLoader();
+            Class<?> objectInputFilterClass = cl.loadClass("sun.misc.ObjectInputFilter");
+            Class<?> objectInputFilterConfigClass = cl.loadClass("sun.misc.ObjectInputFilter$Config");
+            Method getObjectInputFilter = objectInputFilterConfigClass.getDeclaredMethod("getObjectInputFilter", ObjectInputStream.class);
+            Method setObjectInputFilter = objectInputFilterConfigClass.getDeclaredMethod("setObjectInputFilter", ObjectInputStream.class, objectInputFilterClass);
+            Method createFilter = objectInputFilterConfigClass.getDeclaredMethod("createFilter", String.class);
+            return new OnJava6To8(getObjectInputFilter, setObjectInputFilter, createFilter);
+        } catch (ClassNotFoundException | NoSuchMethodException e) {
+            // This can happen for older JDK updates.
+            LOG.warn("Could not configure SerializationFilterAdapter. For better security, it is highly recommended to upgrade to newer JDK version update!");
+            LOG.warn("For the Java 7, the recommended update is at least 131 (1.7.0_131 or newer). For the Java 8, the recommended update is at least 121 (1.8.0_121 or newer).");
+            LOG.warn("Error details", e);
+            return new EmptyFilterAdapter();
+        }
+    }
+
     // If codebase stays on Java 8 for a while you could use Java 8 classes directly without reflection
     static class OnJava6To8 implements SerializationFilterAdapter {
 
-        private static final Method getObjectInputFilterMethod;
-        private static final Method setObjectInputFilterMethod;
-        private static final Method createFilterMethod;
+        private final Method getObjectInputFilterMethod;
+        private final Method setObjectInputFilterMethod;
+        private final Method createFilterMethod;
 
-        static {
-
-            Method getObjectInputFilter;
-            Method setObjectInputFilter;
-            Method createFilter;
-
-            try {
-                ClassLoader cl = Thread.currentThread().getContextClassLoader();
-                Class<?> objectInputFilterClass = cl.loadClass("sun.misc.ObjectInputFilter");
-                Class<?> objectInputFilterConfigClass = cl.loadClass("sun.misc.ObjectInputFilter$Config");
-                getObjectInputFilter = objectInputFilterConfigClass.getDeclaredMethod("getObjectInputFilter", ObjectInputStream.class);
-                setObjectInputFilter = objectInputFilterConfigClass.getDeclaredMethod("setObjectInputFilter", ObjectInputStream.class, objectInputFilterClass);
-                createFilter = objectInputFilterConfigClass.getDeclaredMethod("createFilter", String.class);
-            } catch (ClassNotFoundException | NoSuchMethodException e) {
-                LOG.warn("Could not configure SerializationFilterAdapter: " + e.getMessage());
-                getObjectInputFilter = null;
-                setObjectInputFilter = null;
-                createFilter = null;
-            }
-
-            getObjectInputFilterMethod = getObjectInputFilter;
-            setObjectInputFilterMethod = setObjectInputFilter;
-            createFilterMethod = createFilter;
+        private OnJava6To8(Method getObjectInputFilterMethod, Method setObjectInputFilterMethod, Method createFilterMethod) {
+            this.getObjectInputFilterMethod = getObjectInputFilterMethod;
+            this.setObjectInputFilterMethod = setObjectInputFilterMethod;
+            this.createFilterMethod = createFilterMethod;
         }
 
         public Object getObjectInputFilter(ObjectInputStream ois) {
@@ -84,6 +82,22 @@ public class DelegatingSerializationFilter {
             }
         }
     }
+
+
+    static class EmptyFilterAdapter implements SerializationFilterAdapter {
+
+        @Override
+        public Object getObjectInputFilter(ObjectInputStream ois) {
+            return null;
+        }
+
+        @Override
+        public void setObjectInputFilter(ObjectInputStream ois, String filterPattern) {
+
+        }
+
+    }
+
 
     // If codebase moves to Java 9+ could use Java 9+ classes directly without reflection and keep the old variant with reflection
     static class OnJavaAfter8 implements SerializationFilterAdapter {
