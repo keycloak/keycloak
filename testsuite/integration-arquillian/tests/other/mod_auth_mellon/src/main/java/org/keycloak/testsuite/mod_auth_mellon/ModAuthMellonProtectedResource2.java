5c2385d0816f11148a6ada61e67ad3860ee226c2
mhajas <mhajas@redhat.com>
2020-06-09 14:12:11 +0200

    KEYCLOAK-14105 Update mod-auth-mellon tests to work with TLS

--- testsuite/integration-arquillian/tests/other/mod_auth_mellon/src/main/java/org/keycloak/testsuite/mod_auth_mellon/ModAuthMellonProtectedResource2.java
+++ testsuite/integration-arquillian/tests/other/mod_auth_mellon/src/main/java/org/keycloak/testsuite/mod_auth_mellon/ModAuthMellonProtectedResource2.java
@@ -18,7 +18,7 @@ public class ModAuthMellonProtectedResource2 extends AbstractPageWithInjectedUrl
     @Override
     public URL getInjectedUrl() {
         try {
-            return new URL(System.getProperty("apache.mod_auth_mellon2.url", "http://localhost:8480") + "/auth2");
+            return new URL(System.getProperty("apache.mod_auth_mellon2.url", "https://app-saml-127-0-0-1.nip.io:8843") + "/auth2");
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
