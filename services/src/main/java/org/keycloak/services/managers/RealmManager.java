43e075afa51d40cf65ead3d8797c877762a35c55
Douglas Palmer <dpalmer@redhat.com>
2020-06-16 10:57:00 -0700

    [KEYCLOAK-14352] JavaScript injection vulnerability of Realm registration REST API

--- services/src/main/java/org/keycloak/services/managers/RealmManager.java
+++ services/src/main/java/org/keycloak/services/managers/RealmManager.java
@@ -96,7 +96,12 @@ public class RealmManager {
     }
 
     public RealmModel createRealm(String id, String name) {
-        if (id == null) id = KeycloakModelUtils.generateId();
+        if (id == null) {
+            id = KeycloakModelUtils.generateId();
+        }
+        else {
+            ReservedCharValidator.validate(id);
+        }
         ReservedCharValidator.validate(name);
         RealmModel realm = model.createRealm(id, name);
         realm.setName(name);
@@ -501,6 +506,9 @@ public class RealmManager {
         String id = rep.getId();
         if (id == null) {
             id = KeycloakModelUtils.generateId();
+        }
+        else {
+            ReservedCharValidator.validate(id);
         }
         RealmModel realm = model.createRealm(id, rep.getRealm());
         ReservedCharValidator.validate(rep.getRealm());
