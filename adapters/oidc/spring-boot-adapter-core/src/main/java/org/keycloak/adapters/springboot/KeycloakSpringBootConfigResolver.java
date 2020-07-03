4f4eec4d1e6a99f292b07cbfe69d7ecff34535ca
Thoralf Rickert-Wendt <thoralf@m84.de>
2020-07-03 16:03:32 +0200

    switching to required=false

--- adapters/oidc/spring-boot-adapter-core/src/main/java/org/keycloak/adapters/springboot/KeycloakSpringBootConfigResolver.java
+++ adapters/oidc/spring-boot-adapter-core/src/main/java/org/keycloak/adapters/springboot/KeycloakSpringBootConfigResolver.java
@@ -29,7 +29,7 @@ public class KeycloakSpringBootConfigResolver implements org.keycloak.adapters.K
 
     private KeycloakDeployment keycloakDeployment;
 
-    @Autowired
+    @Autowired(required=false)
     private AdapterConfig adapterConfig;
 
     @Override
