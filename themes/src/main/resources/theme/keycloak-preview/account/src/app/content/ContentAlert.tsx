162c8a4974cfc00b839beb03188174ab45b2dcb1
Erik Jan de Wit <erikjan.dewit@gmail.com>
2020-07-08 11:01:46 +0200

    KEYCLOAK-14692 added aria labels

--- themes/src/main/resources/theme/keycloak-preview/account/src/app/content/ContentAlert.tsx
+++ themes/src/main/resources/theme/keycloak-preview/account/src/app/content/ContentAlert.tsx
@@ -90,9 +90,10 @@ export class ContentAlert extends React.Component<ContentAlertProps, ContentAler
 
     public render(): React.ReactNode {
         return (
-            <AlertGroup isToast>
+            <AlertGroup isToast aria-live="assertive">
                 {this.state.alerts.map(({ key, variant, message }) => (
                     <Alert
+                        aria-details={message}
                         isLiveRegion
                         variant={variant}
                         title={message}
