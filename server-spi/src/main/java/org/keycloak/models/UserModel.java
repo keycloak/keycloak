e1fd6474fa595f7a54dcd826f3619b2ba02bb35b
Daniel Fesenmeyer <daniel.fesenmeyer@bosch.io>
2020-07-30 03:30:40 -0700

    KEYCLOAK-14781 Extend Admin REST API with search by federated identity
    - Add parameters idpAlias and idpUserId to the resource /{realm}/users and allow it to be combined with the other search parameters like username, email and so on
    - Add attribute "federatedIdentities" to UserEntity to allow joining on this field
    - extend integration test "UserTest"

--- server-spi/src/main/java/org/keycloak/models/UserModel.java
+++ server-spi/src/main/java/org/keycloak/models/UserModel.java
@@ -38,6 +38,8 @@ public interface UserModel extends RoleMapperModel {
     String EMAIL_VERIFIED = "emailVerified";
     String LOCALE = "locale";
     String ENABLED = "enabled";
+    String IDP_ALIAS = "keycloak.session.realm.users.query.idp_alias";
+    String IDP_USER_ID = "keycloak.session.realm.users.query.idp_user_id";
     String INCLUDE_SERVICE_ACCOUNT = "keycloak.session.realm.users.query.include_service_account";
     String GROUPS = "keycloak.session.realm.users.query.groups";
     String SEARCH = "keycloak.session.realm.users.query.search";
