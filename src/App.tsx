0b460662c2e436f13fc63207753a6cdccbee5d0f
Erik Jan de Wit <erikjan.dewit@gmail.com>
2020-08-07 15:44:34 +0200

    initial version of the alert system

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import React, { useContext } from 'react';
+import {
+  Page,
+  PageSection,
+  Button,
+  AlertVariant,
+} from '@patternfly/react-core';
 
 import { ClientList } from './clients/ClientList';
 import { DataLoader } from './components/data-loader/DataLoader';
 import { HttpClientContext } from './http-service/HttpClientContext';
 import { Client } from './clients/client-model';
-import { Page, PageSection } from '@patternfly/react-core';
 import { Header } from './PageHeader';
 import { PageNav } from './PageNav';
+import { AlertPanel } from './components/alert/AlertPanel';
+import { useAlerts, withAlerts } from './components/alert/Alerts';
 
-export const App = () => {
+const AppComponent = () => {
+  const [alerts, add, hide] = useAlerts();
   const httpClient = useContext(HttpClientContext);
 
   const loader = async () => {
@@ -19,10 +27,18 @@ export const App = () => {
   return (
     <Page header={<Header />} sidebar={<PageNav />}>
       <PageSection>
+        <AlertPanel alerts={alerts} onCloseAlert={hide} />
         <DataLoader loader={loader}>
           {(clients) => <ClientList clients={clients} />}
         </DataLoader>
+        <Button
+          onClick={() => add('Crazy stuff happened', AlertVariant.danger)}
+        >
+          Click
+        </Button>
       </PageSection>
     </Page>
   );
 };
+
+export const App = withAlerts(AppComponent);
