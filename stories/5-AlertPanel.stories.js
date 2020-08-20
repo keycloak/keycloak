9b6e389da1b4686fb36f08264e941b663f751569
Erik Jan de Wit <erikjan.dewit@gmail.com>
2020-08-20 15:46:21 +0200

    update story

--- stories/5-AlertPanel.stories.js
+++ stories/5-AlertPanel.stories.js
@@ -3,22 +3,16 @@ import { AlertVariant, Button } from '@patternfly/react-core';
 import { storiesOf } from '@storybook/react';
 
 import { AlertPanel } from '../src/components/alert/AlertPanel';
-import { withAlerts, useAlerts } from '../src/components/alert/Alerts';
+import { useAlerts } from '../src/components/alert/Alerts';
 
-storiesOf('Alert Panel', module)
+storiesOf('Alert Panel')
   .add('api', () => <AlertPanel alerts={[{ key: 1, message: 'Hello', variant: AlertVariant.default }]} onCloseAlert={() => { }} />)
   .add('add alert', () => {
-    const Comp = () => {
-      const [alerts, add, hide] = useAlerts();
-      return (
-        <>
-          <AlertPanel alerts={alerts} onCloseAlert={hide} />
-          <Button onClick={() => add('Hello', AlertVariant.default)}>Add</Button>
-        </>
-      );
-    }
-    const WrapperComponent = withAlerts(Comp);
+    const [add, alerts, hide] = useAlerts();
     return (
-      <WrapperComponent/>
+      <>
+        <AlertPanel alerts={alerts} onCloseAlert={hide} />
+        <Button onClick={() => add('Hello', AlertVariant.default)}>Add</Button>
+      </>
     );
   });
