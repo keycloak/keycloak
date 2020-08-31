a1e637c5da3723bdf443a04a014d51d61732da25
Erik Jan de Wit <erikjan.dewit@gmail.com>
2020-08-31 10:23:57 +0200

    Added tests for alert component. (#43)
    * added enzyme
    
    * alert test

--- jest.setup.js
+++ jest.setup.js
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
+
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
---
+++ src/components/alert/Alerts.test.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Button, AlertVariant } from '@patternfly/react-core';
+import { mount } from 'enzyme';
+import EnzymeToJson from 'enzyme-to-json';
+import { act } from 'react-dom/test-utils';
+
+import { AlertPanel } from './AlertPanel';
+import { useAlerts } from './Alerts';
+
+jest.useFakeTimers();
+
+const WithButton = () => {
+  const [add, alerts, hide] = useAlerts();
+  return (
+    <>
+      <AlertPanel alerts={alerts} onCloseAlert={hide} />
+      <Button onClick={() => add('Hello', AlertVariant.default)}>Add</Button>
+    </>
+  );
+};
+
+it('renders global alerts', () => {
+  const empty = EnzymeToJson(
+    mount(<AlertPanel alerts={[]} onCloseAlert={() => {}} />)
+  );
+  expect(empty).toMatchSnapshot();
+
+  const tree = mount(<WithButton />);
+  const button = tree.find('button');
+  expect(button).not.toBeNull();
+
+  act(() => {
+    button!.simulate('click');
+  });
+  expect(EnzymeToJson(tree)).toMatchSnapshot();
+
+  act(() => {
+    jest.runAllTimers();
+  });
+  expect(EnzymeToJson(tree)).toMatchSnapshot();
+});
