050f6aaed50c2dceb51c907ba576fc54cbcee907
Erik Jan de Wit <erikjan.dewit@gmail.com>
2020-09-03 19:25:35 +0200

    added more tests (#59)

--- jest.setup.js
+++ jest.setup.js
@@ -4,6 +4,20 @@
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
 
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+i18n.use(initReactI18next).init({
+  lng: 'en',
+  fallbackLng: 'en',
+
+  // have a common namespace used around the full app
+  ns: ['translations'],
+  defaultNS: 'translations',
+
+  resources: { en: { translations: {} } },
+});
+
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
