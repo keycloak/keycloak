4541a1b250186d9ffe1a7ac3e4dbae80c0a04bdc
Hynek Mlnarik <hmlnarik@redhat.com>
2020-10-09 14:59:03 +0200

    KEYCLOAK-15907 Fix new host in SAML adapter cannot restore session

--- adapters/saml/wildfly/wildfly-adapter/src/main/java/org/keycloak/adapters/saml/wildfly/infinispan/SsoSessionCacheListener.java
+++ adapters/saml/wildfly/wildfly-adapter/src/main/java/org/keycloak/adapters/saml/wildfly/infinispan/SsoSessionCacheListener.java
@@ -26,6 +26,7 @@ import org.infinispan.client.hotrod.annotation.ClientCacheEntryRemoved;
 import org.infinispan.client.hotrod.annotation.ClientListener;
 import org.infinispan.client.hotrod.event.ClientCacheEntryCreatedEvent;
 import org.infinispan.client.hotrod.event.ClientCacheEntryRemovedEvent;
+import org.infinispan.context.Flag;
 import org.infinispan.notifications.Listener;
 import org.infinispan.notifications.cachelistener.annotation.*;
 import org.infinispan.notifications.cachelistener.event.*;
@@ -197,5 +198,6 @@ public class SsoSessionCacheListener {
         LOG.tracev("remoteCacheEntryRemoved {0}", event.getKey());
 
         this.idMapper.removeSession((String) event.getKey());
+        ssoCache.getAdvancedCache().withFlags(Flag.SKIP_CACHE_STORE).remove((String) event.getKey());
     }
 }
