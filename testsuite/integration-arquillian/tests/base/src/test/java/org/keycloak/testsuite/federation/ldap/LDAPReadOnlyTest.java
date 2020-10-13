c40b82dee08237351e7f2d595c88406806d9eb0e
Sven-Torben Janus <sven-torben.janus@conciso.de>
2020-10-13 22:29:30 +0200

    KEYCLOAK-3365 Add test case

--- testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/federation/ldap/LDAPReadOnlyTest.java
+++ testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/federation/ldap/LDAPReadOnlyTest.java
@@ -46,6 +46,8 @@ import org.keycloak.testsuite.util.LDAPRule;
 import org.keycloak.testsuite.util.LDAPTestUtils;
 import org.openqa.selenium.By;
 
+import javax.ws.rs.ClientErrorException;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -123,6 +125,23 @@ public class LDAPReadOnlyTest extends AbstractLDAPTest  {
                 .filter(credentialRep -> credentialRep.getType().equals(OTPCredentialModel.TYPE))
                 .findFirst().get().getId();
         user.removeCredential(totpCredentialId);
+    }
+
+    // KEYCLOAK-3365
+    @Test
+    public void testReadOnlyUserDoesNotThrowIfUnchanged() {
+        UserResource user = ApiUtil.findUserByUsernameId(testRealm(), "johnkeycloak");
+        UserRepresentation userRepresentation = user.toRepresentation();
+        user.update(userRepresentation);
+    }
+
+    // KEYCLOAK-3365
+    @Test(expected = ClientErrorException.class)
+    public void testReadOnlyUserThrowsIfChanged() {
+        UserResource user = ApiUtil.findUserByUsernameId(testRealm(), "johnkeycloak");
+        UserRepresentation userRepresentation = user.toRepresentation();
+        userRepresentation.setFirstName("Jane");
+        user.update(userRepresentation);
     }
 
     private void setTotpRequirementExecutionForRealm(AuthenticationExecutionModel.Requirement requirement) {
