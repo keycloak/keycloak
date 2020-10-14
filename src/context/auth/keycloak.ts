90779b6da49e72487532e5e2ac0b1e992a9be160
Erik Jan de Wit <erikjan.dewit@gmail.com>
2020-10-14 14:53:51 +0200

    introduced "/" for keycloak.json (#170)
    fixes: #165

--- src/context/auth/keycloak.ts
+++ src/context/auth/keycloak.ts
@@ -1,5 +1,5 @@
 import Keycloak, { KeycloakInstance } from "keycloak-js";
-const keycloak: KeycloakInstance = Keycloak();
+const keycloak: KeycloakInstance = Keycloak("/keycloak.json");
 
 export default async function (): Promise<KeycloakInstance> {
   await keycloak.init({ onLoad: "check-sso", pkceMethod: "S256" }).catch(() => {
