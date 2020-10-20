98907091fc8750338423560c6559c3fd1bf9ffc5
Pedro Igor <pigor.craveiro@gmail.com>
2020-10-20 20:19:51 -0300

    Fixing passing system properties and adding http.host option

--- distribution/server-x/src/main/content/bin/kc.sh
+++ distribution/server-x/src/main/content/bin/kc.sh
@@ -46,7 +46,7 @@ do
           break
           ;;
       *)
-          if [[ $1 = -* || ! $1 =~ ^-.* ]]; then
+          if [[ $1 = --* || ! $1 =~ ^-.* ]]; then
             CONFIG_ARGS="$CONFIG_ARGS $1"
           else
             SERVER_OPTS="$SERVER_OPTS $1"
