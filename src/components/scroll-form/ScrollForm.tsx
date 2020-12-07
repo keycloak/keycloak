2be549a60e6eb8f4cf48b258af012b3a75b5c87c
Stan Silvert <ssilvert@redhat.com>
2020-12-07 07:51:38 -0500

    Use new patternfly jumplinks. (#246)
    * Use new patternfly jumplinks.
    
    * Update snapshots

--- src/components/scroll-form/ScrollForm.tsx
+++ src/components/scroll-form/ScrollForm.tsx
@@ -1,5 +1,12 @@
 import React, { Children, useEffect, useState } from "react";
-import { Grid, GridItem, Title } from "@patternfly/react-core";
+import { useTranslation } from "react-i18next";
+import {
+  Grid,
+  GridItem,
+  JumpLinks,
+  JumpLinksItem,
+  Title,
+} from "@patternfly/react-core";
 
 import { FormPanel } from "./FormPanel";
 import style from "./scroll-form.module.css";
@@ -10,76 +17,43 @@ type ScrollFormProps = {
 };
 
 export const ScrollForm = ({ sections, children }: ScrollFormProps) => {
+  const { t } = useTranslation("common");
   const [active, setActive] = useState(sections[0]);
-  useEffect(() => {
-    const getCurrentSection = () => {
-      for (let sectionName of sections) {
-        const section = document.getElementById(sectionName)!;
-        const startAt = section.offsetTop;
-        const endAt = startAt + section.offsetHeight;
-        const currentPosition =
-          document.documentElement.scrollTop || document.body.scrollTop;
-        const isInView = currentPosition >= startAt && currentPosition < endAt;
-        if (isInView) {
-          return sectionName;
-        }
-      }
-    };
-
-    window.addEventListener("scroll", () => {
-      const active = getCurrentSection();
-      if (active) {
-        setActive(active);
-      }
-    });
-  }, [active, sections]);
 
   const Nav = () => (
     <div className={style.sticky}>
       <Title headingLevel="h5" size="lg">
-        Jump to Section
+        {t("jumpToSection")}
       </Title>
-      <div className="pf-c-tabs pf-m-vertical">
-        <ul className="pf-c-tabs__list">
-          {sections.map((cat) => (
-            <li
-              className={
-                "pf-c-tabs__item" + (active === cat ? " pf-m-current" : "")
-              }
-              key={cat}
-            >
-              <button
-                className="pf-c-tabs__link"
-                id={`link-${cat}`}
-                onClick={() =>
-                  document
-                    .getElementById(cat)
-                    ?.scrollIntoView({ behavior: "smooth" })
-                }
-              >
-                <span className="pf-c-tabs__item-text">{cat}</span>
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
+
+      <JumpLinks isVertical>
+        {sections.map((cat) => (
+          <JumpLinksItem
+            isActive={active === cat}
+            key={cat}
+            href={`#${cat}`}
+            onClick={() => setActive(cat)}
+          >
+            {cat}
+          </JumpLinksItem>
+        ))}
+      </JumpLinks>
     </div>
   );
+
   const nodes = Children.toArray(children);
   return (
-    <>
-      <Grid hasGutter>
-        <GridItem span={8}>
-          {sections.map((cat, index) => (
-            <FormPanel id={cat} key={cat} title={cat}>
-              {nodes[index]}
-            </FormPanel>
-          ))}
-        </GridItem>
-        <GridItem span={4}>
-          <Nav />
-        </GridItem>
-      </Grid>
-    </>
+    <Grid hasGutter>
+      <GridItem span={8}>
+        {sections.map((cat, index) => (
+          <FormPanel id={cat} key={cat} title={cat}>
+            {nodes[index]}
+          </FormPanel>
+        ))}
+      </GridItem>
+      <GridItem span={4}>
+        <Nav />
+      </GridItem>
+    </Grid>
   );
 };
