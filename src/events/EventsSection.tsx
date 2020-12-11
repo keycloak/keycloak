7d20fc01a7a99e106579ab154ef3a0e54d27a85c
Erik Jan de Wit <erikjan.dewit@gmail.com>
2020-12-11 14:05:41 +0100

    initial version of the events tab

--- src/events/EventsSection.tsx
+++ src/events/EventsSection.tsx
@@ -1,10 +1,92 @@
-import { PageSection } from "@patternfly/react-core";
-import React from "react";
+import React, { useContext, useState } from "react";
+import { useTranslation } from "react-i18next";
+import {
+  Button,
+  Label,
+  PageSection,
+  ToolbarItem,
+} from "@patternfly/react-core";
+import moment from "moment";
+import EventRepresentation from "keycloak-admin/lib/defs/eventRepresentation";
+
+import { useAdminClient } from "../context/auth/AdminClient";
+import { ViewHeader } from "../components/view-header/ViewHeader";
+import { DataList } from "../components/table-toolbar/DataList";
+import { RealmContext } from "../context/realm-context/RealmContext";
+import { InfoCircleIcon } from "@patternfly/react-icons";
 
 export const EventsSection = () => {
+  const { t } = useTranslation("events");
+  const adminClient = useAdminClient();
+  const { realm } = useContext(RealmContext);
+  const [key, setKey] = useState("");
+  const refresh = () => setKey(`${new Date().getTime()}`);
+
+  const loader = async (first?: number, max?: number, search?: string) => {
+    const params = {
+      first: first!,
+      max: max!,
+      realm,
+    };
+    if (search) {
+      console.log("how to search?", search);
+    }
+    return await adminClient.realms.findEvents({ ...params });
+  };
+
+  const StatusRow = (event: EventRepresentation) => {
+    return (
+      <>
+        <Label color="red" icon={<InfoCircleIcon />}>
+          {event.type}
+        </Label>
+      </>
+    );
+  };
+
   return (
     <>
-      <PageSection variant="light">The Events Page</PageSection>
+      <ViewHeader titleKey="events:title" subKey="events:eventExplain" />
+      <PageSection variant="light">
+        <DataList
+          key={key}
+          loader={loader}
+          isPaginated
+          ariaLabelKey="events:title"
+          searchPlaceholderKey="events:searchForEvent"
+          toolbarItem={
+            <>
+              <ToolbarItem>
+                <Button onClick={refresh}>{t("refresh")}</Button>
+              </ToolbarItem>
+            </>
+          }
+          columns={[
+            {
+              name: "time",
+              displayKey: "events:time",
+              cellRenderer: (row) => moment(row.time).fromNow(),
+            },
+            {
+              name: "userId",
+              displayKey: "events:user",
+            },
+            {
+              name: "type",
+              displayKey: "events:eventType",
+              cellRenderer: StatusRow,
+            },
+            {
+              name: "ipAddress",
+              displayKey: "events:ipAddress",
+            },
+            {
+              name: "clientId",
+              displayKey: "events:client",
+            },
+          ]}
+        />
+      </PageSection>
     </>
   );
 };
