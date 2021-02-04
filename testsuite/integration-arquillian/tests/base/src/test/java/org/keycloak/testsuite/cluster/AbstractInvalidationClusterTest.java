eb37a1ed691381e60342bf31560fbd4d6582ba32
Pedro Igor <pigor.craveiro@gmail.com>
2021-02-04 19:52:02 -0300

    [KEYCLOAK-17031] - ClientInvalidationClusterTest failing on Quarkus due to unreliable comparison

--- testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/cluster/AbstractInvalidationClusterTest.java
+++ testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/cluster/AbstractInvalidationClusterTest.java
@@ -9,10 +9,15 @@ import org.junit.Test;
 import org.keycloak.representations.idm.RealmRepresentation;
 import org.keycloak.testsuite.arquillian.ContainerInfo;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertFalse;
+import static org.keycloak.common.util.reflections.Reflections.resolveListType;
+import static org.keycloak.common.util.reflections.Reflections.setAccessible;
+import static org.keycloak.common.util.reflections.Reflections.unsetAccessible;
 
 /**
  *
@@ -132,7 +137,8 @@ public abstract class AbstractInvalidationClusterTest<T, TR> extends AbstractClu
         boolean entityDiffers = false;
         for (ContainerInfo survivorNode : getCurrentSurvivorNodes()) {
             T testEntityOnSurvivorNode = readEntity(testEntityOnFailNode, survivorNode);
-            if (EqualsBuilder.reflectionEquals(testEntityOnSurvivorNode, testEntityOnFailNode, excludedComparisonFields)) {
+
+            if (EqualsBuilder.reflectionEquals(sortFields(testEntityOnSurvivorNode), sortFields(testEntityOnFailNode), excludedComparisonFields)) {
                 log.info(String.format("Verification of %s on survivor %s PASSED", getEntityType(testEntityOnFailNode), survivorNode));
             } else {
                 entityDiffers = true;
@@ -147,6 +153,29 @@ public abstract class AbstractInvalidationClusterTest<T, TR> extends AbstractClu
             }
         }
         assertFalse(entityDiffers);
+    }
+
+    private T sortFields(T entity) {
+        for (Field field : entity.getClass().getDeclaredFields()) {
+            try {
+                Class<?> type = resolveListType(field, entity);
+
+                if (type != null && Comparable.class.isAssignableFrom(type)) {
+                    setAccessible(field);
+                    Object value = field.get(entity);
+
+                    if (value != null) {
+                        Collections.sort((List) value);
+                    }
+                }
+            } catch (IllegalAccessException cause) {
+                throw new RuntimeException("Failed to sort field [" + field + "]", cause);
+            } finally {
+                unsetAccessible(field);
+            }
+        }
+
+        return entity;
     }
 
     private void assertEntityOnSurvivorNodesIsDeleted(T testEntityOnFailNode) {
