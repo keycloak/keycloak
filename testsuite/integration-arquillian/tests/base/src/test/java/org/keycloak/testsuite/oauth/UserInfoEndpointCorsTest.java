9e6e9dd566c0c8743af8e8c383c26a892d921511
mposolda <mposolda@gmail.com>
2021-02-11 09:13:27 +0100

    KEYCLOAK-15719 CORS headers missing on userinfo error response

--- testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/oauth/UserInfoEndpointCorsTest.java
+++ testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/oauth/UserInfoEndpointCorsTest.java
@@ -1,5 +1,6 @@
 package org.keycloak.testsuite.oauth;
 
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.junit.Test;
 import org.keycloak.representations.idm.RealmRepresentation;
 import org.keycloak.testsuite.AbstractKeycloakTest;
@@ -30,7 +31,7 @@ public class UserInfoEndpointCorsTest extends AbstractKeycloakTest {
     }
 
     @Test
-    public void userInfoCorsRequestWithValidUrl() throws Exception {
+    public void userInfoCorsValidRequestWithValidUrl() throws Exception {
 
         oauth.realm("test");
         oauth.clientId("test-app2");
@@ -38,19 +39,54 @@ public class UserInfoEndpointCorsTest extends AbstractKeycloakTest {
 
         OAuthClient.AccessTokenResponse accessTokenResponse = oauth.doGrantAccessTokenRequest(null, "test-user@localhost", "password");
 
-        WebTarget userInfoTarget =  UserInfoClientUtil.getUserInfoWebTarget(AdminClientUtil.createResteasyClient());
-        Response userInfoResponse = userInfoTarget.request()
-                .header(HttpHeaders.AUTHORIZATION, "bearer " + accessTokenResponse.getAccessToken())
-                .header("Origin", VALID_CORS_URL) // manually trigger CORS handling
-                .get();
+        ResteasyClient resteasyClient = AdminClientUtil.createResteasyClient();
+        try {
+            WebTarget userInfoTarget = UserInfoClientUtil.getUserInfoWebTarget(resteasyClient);
+            Response userInfoResponse = userInfoTarget.request()
+                    .header(HttpHeaders.AUTHORIZATION, "bearer " + accessTokenResponse.getAccessToken())
+                    .header("Origin", VALID_CORS_URL) // manually trigger CORS handling
+                    .get();
 
-        UserInfoClientUtil.testSuccessfulUserInfoResponse(userInfoResponse, "test-user@localhost", "test-user@localhost");
+            UserInfoClientUtil.testSuccessfulUserInfoResponse(userInfoResponse, "test-user@localhost", "test-user@localhost");
 
-        assertCors(userInfoResponse);
+            assertCors(userInfoResponse);
+        } finally {
+            resteasyClient.close();
+        }
+    }
+
+    // KEYCLOAK-15719 error response should still contain CORS headers
+    @Test
+    public void userInfoCorsInvalidRequestWithValidUrl() throws Exception {
+
+        oauth.realm("test");
+        oauth.clientId("test-app2");
+        oauth.redirectUri(VALID_CORS_URL + "/realms/master/app");
+
+        OAuthClient.AccessTokenResponse accessTokenResponse = oauth.doGrantAccessTokenRequest(null, "test-user@localhost", "password");
+
+        // Set time offset to make sure that userInfo request will be invalid due the expired token
+        setTimeOffset(600);
+
+        ResteasyClient resteasyClient = AdminClientUtil.createResteasyClient();
+        try {
+            WebTarget userInfoTarget = UserInfoClientUtil.getUserInfoWebTarget(resteasyClient);
+            Response userInfoResponse = userInfoTarget.request()
+                    .header(HttpHeaders.AUTHORIZATION, "bearer " + accessTokenResponse.getAccessToken())
+                    .header("Origin", VALID_CORS_URL) // manually trigger CORS handling
+                    .get();
+
+            // We should have errorResponse, but CORS headers should be there as origin was valid
+            assertEquals(Response.Status.UNAUTHORIZED.getStatusCode(), userInfoResponse.getStatus());
+
+            assertCors(userInfoResponse);
+        } finally {
+            resteasyClient.close();
+        }
     }
 
     @Test
-    public void userInfoCorsRequestWithInvalidUrlShouldFail() throws Exception {
+    public void userInfoCorsValidRequestWithInvalidUrlShouldFail() throws Exception {
 
         oauth.realm("test");
         oauth.clientId("test-app2");
@@ -58,23 +94,32 @@ public class UserInfoEndpointCorsTest extends AbstractKeycloakTest {
 
         OAuthClient.AccessTokenResponse accessTokenResponse = oauth.doGrantAccessTokenRequest(null, "test-user@localhost", "password");
 
-        WebTarget userInfoTarget =  UserInfoClientUtil.getUserInfoWebTarget(AdminClientUtil.createResteasyClient());
-        Response userInfoResponse = userInfoTarget.request()
-                .header(HttpHeaders.AUTHORIZATION, "bearer " + accessTokenResponse.getAccessToken())
-                .header("Origin", INVALID_CORS_URL) // manually trigger CORS handling
-                .get();
+        ResteasyClient resteasyClient = AdminClientUtil.createResteasyClient();
+        try {
+            WebTarget userInfoTarget =  UserInfoClientUtil.getUserInfoWebTarget(resteasyClient);
+            Response userInfoResponse = userInfoTarget.request()
+                    .header(HttpHeaders.AUTHORIZATION, "bearer " + accessTokenResponse.getAccessToken())
+                    .header("Origin", INVALID_CORS_URL) // manually trigger CORS handling
+                    .get();
 
-        assertNotCors(userInfoResponse);
+            UserInfoClientUtil.testSuccessfulUserInfoResponse(userInfoResponse, "test-user@localhost", "test-user@localhost");
+
+            assertNotCors(userInfoResponse);
+        } finally {
+            resteasyClient.close();
+        }
     }
 
     private static void assertCors(Response response) {
         assertEquals("true", response.getHeaders().getFirst("Access-Control-Allow-Credentials"));
         assertEquals(VALID_CORS_URL, response.getHeaders().getFirst("Access-Control-Allow-Origin"));
+        response.close();
     }
 
     private static void assertNotCors(Response response) {
         assertNull(response.getHeaders().get("Access-Control-Allow-Credentials"));
         assertNull(response.getHeaders().get("Access-Control-Allow-Origin"));
         assertNull(response.getHeaders().get("Access-Control-Expose-Headers"));
+        response.close();
     }
 }
