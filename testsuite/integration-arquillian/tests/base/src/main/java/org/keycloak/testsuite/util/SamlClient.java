9c7f4b62e5976eaec71dad004dc14530f6d57bf7
Michal Hajas <mhajas@redhat.com>
2021-02-16 15:03:52 +0100

    KEYCLOAK-16592 Do not require destination with SOAP binding

--- testsuite/integration-arquillian/tests/base/src/main/java/org/keycloak/testsuite/util/SamlClient.java
+++ testsuite/integration-arquillian/tests/base/src/main/java/org/keycloak/testsuite/util/SamlClient.java
@@ -24,6 +24,8 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.LaxRedirectStrategy;
@@ -32,20 +34,28 @@ import org.apache.http.util.EntityUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.keycloak.adapters.saml.SamlDeployment;
 import org.keycloak.common.util.KeyUtils;
+import org.keycloak.dom.saml.v2.SAML2Object;
 import org.keycloak.dom.saml.v2.protocol.AuthnRequestType;
 import org.keycloak.saml.BaseSAML2BindingBuilder;
 import org.keycloak.saml.SAMLRequestParser;
 import org.keycloak.saml.SignatureAlgorithm;
 import org.keycloak.saml.common.constants.GeneralConstants;
+import org.keycloak.saml.common.constants.JBossSAMLConstants;
 import org.keycloak.saml.common.constants.JBossSAMLURIConstants;
 import org.keycloak.saml.common.exceptions.ConfigurationException;
+import org.keycloak.saml.common.exceptions.ParsingException;
 import org.keycloak.saml.common.exceptions.ProcessingException;
 import org.keycloak.saml.processing.api.saml.v2.request.SAML2Request;
+import org.keycloak.saml.processing.core.parsers.saml.SAMLParser;
 import org.keycloak.saml.processing.core.saml.v2.common.SAMLDocumentHolder;
+import org.keycloak.saml.processing.core.saml.v2.util.DocumentUtil;
+import org.keycloak.saml.processing.core.util.JAXPValidationUtil;
 import org.w3c.dom.Document;
 
 import javax.ws.rs.core.Response;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
@@ -60,6 +70,13 @@ import java.util.List;
 import java.util.UUID;
 import javax.ws.rs.core.MultivaluedHashMap;
 import javax.ws.rs.core.MultivaluedMap;
+import javax.xml.soap.MessageFactory;
+import javax.xml.soap.SOAPBody;
+import javax.xml.soap.SOAPEnvelope;
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPHeader;
+import javax.xml.soap.SOAPHeaderElement;
+import javax.xml.soap.SOAPMessage;
 
 import org.jboss.logging.Logger;
 import static org.hamcrest.Matchers.*;
@@ -68,8 +85,11 @@ import static org.junit.Assert.assertTrue;
 import org.keycloak.common.VerificationException;
 import org.keycloak.protocol.saml.SamlProtocolUtils;
 import org.keycloak.rotation.KeyLocator;
+
 import static org.keycloak.saml.common.constants.GeneralConstants.RELAY_STATE;
 import org.keycloak.saml.processing.web.util.RedirectBindingUtil;
+import org.w3c.dom.Node;
+
 import static org.keycloak.testsuite.util.Matchers.statusCodeIsHC;
 
 /**
@@ -311,7 +331,167 @@ public class SamlClient {
                     throw new RuntimeException(ex);
                 }
             }
-        };
+        },
+        /**
+         * SOAP binding is currently usable only with http://localhost:8280/ecp-sp/ client, see to-do comment within
+         * {@link #createSamlSignedRequest} for more details. After resolving that to-do it should be usable with any
+         * client.
+         */
+        SOAP {
+            @Override
+            public SAMLDocumentHolder extractResponse(CloseableHttpResponse response, String realmPublicKey) throws IOException {
+
+                assertThat(response, statusCodeIsHC(200));
+
+                MessageFactory messageFactory = null;
+                try {
+                    messageFactory = MessageFactory.newInstance();
+                    SOAPMessage soapMessage = messageFactory.createMessage(null, response.getEntity().getContent());
+                    SOAPBody soapBody = soapMessage.getSOAPBody();
+                    Node authnRequestNode = soapBody.getFirstChild();
+                    Document document = DocumentUtil.createDocument();
+                    document.appendChild(document.importNode(authnRequestNode, true));
+
+                    SAMLParser samlParser = SAMLParser.getInstance();
+                    JAXPValidationUtil.checkSchemaValidation(document);
+
+                    SAML2Object responseType = (SAML2Object) samlParser.parse(document);
+
+                    return new SAMLDocumentHolder(responseType, document);
+                } catch (SOAPException | ConfigurationException | ProcessingException | ParsingException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            private static final String NS_PREFIX_PROFILE_ECP = "ecp";
+            private static final String NS_PREFIX_SAML_PROTOCOL = "samlp";
+            private static final String NS_PREFIX_SAML_ASSERTION = "saml";
+            private static final String NS_PREFIX_PAOS_BINDING = "paos";
+
+            private void createEcpRequestHeader(SOAPEnvelope envelope, SamlDeployment deployment) throws SOAPException {
+                SOAPHeader headers = envelope.getHeader();
+                SOAPHeaderElement ecpRequestHeader = headers.addHeaderElement(envelope.createQName(JBossSAMLConstants.REQUEST.get(), NS_PREFIX_PROFILE_ECP));
+
+                ecpRequestHeader.setMustUnderstand(true);
+                ecpRequestHeader.setActor("http://schemas.xmlsoap.org/soap/actor/next");
+                ecpRequestHeader.addAttribute(envelope.createName("ProviderName"), deployment.getEntityID());
+                ecpRequestHeader.addAttribute(envelope.createName("IsPassive"), "0");
+                ecpRequestHeader.addChildElement(envelope.createQName("Issuer", "saml")).setValue(deployment.getEntityID());
+                ecpRequestHeader.addChildElement(envelope.createQName("IDPList", "samlp"))
+                        .addChildElement(envelope.createQName("IDPEntry", "samlp"))
+                        .addAttribute(envelope.createName("ProviderID"), deployment.getIDP().getEntityID())
+                        .addAttribute(envelope.createName("Name"), deployment.getIDP().getEntityID())
+                        .addAttribute(envelope.createName("Loc"), deployment.getIDP().getSingleSignOnService().getRequestBindingUrl());
+            }
+
+            private void createPaosRequestHeader(SOAPEnvelope envelope, SamlDeployment deployment) throws SOAPException {
+                SOAPHeader headers = envelope.getHeader();
+                SOAPHeaderElement paosRequestHeader = headers.addHeaderElement(envelope.createQName(JBossSAMLConstants.REQUEST.get(), NS_PREFIX_PAOS_BINDING));
+
+                paosRequestHeader.setMustUnderstand(true);
+                paosRequestHeader.setActor("http://schemas.xmlsoap.org/soap/actor/next");
+                paosRequestHeader.addAttribute(envelope.createName("service"), JBossSAMLURIConstants.ECP_PROFILE.get());
+                paosRequestHeader.addAttribute(envelope.createName("responseConsumerURL"), getResponseConsumerUrl(deployment));
+            }
+
+            private String getResponseConsumerUrl(SamlDeployment deployment) {
+                return (deployment.getIDP() == null
+                        || deployment.getIDP().getSingleSignOnService() == null
+                        || deployment.getIDP().getSingleSignOnService().getAssertionConsumerServiceUrl() == null
+                ) ? null
+                        : deployment.getIDP().getSingleSignOnService().getAssertionConsumerServiceUrl().toString();
+            }
+
+            @Override
+            public HttpUriRequest createSamlUnsignedRequest(URI samlEndpoint, String relayState, Document samlRequest) {
+                return createSamlSignedRequest(samlEndpoint, relayState, samlRequest, null, null, null);
+            }
+
+            @Override
+            public HttpUriRequest createSamlSignedRequest(URI samlEndpoint, String relayState, Document samlRequest, String realmPrivateKey, String realmPublicKey) {
+                return createSamlSignedRequest(samlEndpoint, relayState, samlRequest, realmPrivateKey, realmPublicKey, null);
+            }
+
+            @Override
+            public HttpUriRequest createSamlSignedRequest(URI samlEndpoint, String relayState, Document samlRequest, String realmPrivateKey, String realmPublicKey, String certificateStr) {
+                BaseSAML2BindingBuilder binding = new BaseSAML2BindingBuilder();
+
+                if (realmPrivateKey != null && realmPublicKey != null) {
+                    PrivateKey privateKey = org.keycloak.testsuite.util.KeyUtils.privateKeyFromString(realmPrivateKey);
+                    PublicKey publicKey = org.keycloak.testsuite.util.KeyUtils.publicKeyFromString(realmPublicKey);
+                    X509Certificate cert = org.keycloak.common.util.PemUtils.decodeCertificate(certificateStr);
+                    binding
+                            .signatureAlgorithm(SignatureAlgorithm.RSA_SHA256)
+                            .signWith(KeyUtils.createKeyId(privateKey), privateKey, publicKey, cert)
+                            .signDocument();
+
+                    try {
+                        samlRequest = binding.postBinding(samlRequest).getDocument();
+                    } catch (ProcessingException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+
+                MessageFactory messageFactory = null;
+                try {
+
+                    messageFactory = MessageFactory.newInstance();
+
+                    SOAPMessage message = messageFactory.createMessage();
+
+                    SOAPEnvelope envelope = message.getSOAPPart().getEnvelope();
+
+                    envelope.addNamespaceDeclaration(NS_PREFIX_SAML_ASSERTION, JBossSAMLURIConstants.ASSERTION_NSURI.get());
+                    envelope.addNamespaceDeclaration(NS_PREFIX_SAML_PROTOCOL, JBossSAMLURIConstants.PROTOCOL_NSURI.get());
+                    envelope.addNamespaceDeclaration(NS_PREFIX_PAOS_BINDING, JBossSAMLURIConstants.PAOS_BINDING.get());
+                    envelope.addNamespaceDeclaration(NS_PREFIX_PROFILE_ECP, JBossSAMLURIConstants.ECP_PROFILE.get());
+
+                    SamlDeployment deployment = SamlUtils.getSamlDeploymentForClient("ecp-sp"); // TODO: Make more general for any client, currently SOAP is usable only with http://localhost:8280/ecp-sp/ client
+
+                    createPaosRequestHeader(envelope, deployment);
+                    createEcpRequestHeader(envelope, deployment);
+
+                    SOAPBody body = envelope.getBody();
+
+                    body.addDocument(samlRequest);
+
+                    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                    message.writeTo(outputStream);
+
+                    HttpPost post = new HttpPost(samlEndpoint);
+                    post.setEntity(new ByteArrayEntity(outputStream.toByteArray(), ContentType.TEXT_XML));
+                    return post;
+                } catch (SOAPException | IOException | ParsingException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            @Override
+            public URI getBindingUri() {
+                return null;
+            }
+
+            @Override
+            public HttpUriRequest createSamlUnsignedResponse(URI samlEndpoint, String relayState, Document samlRequest) {
+                return null;
+            }
+
+            @Override
+            public HttpUriRequest createSamlSignedResponse(URI samlEndpoint, String relayState, Document samlRequest, String realmPrivateKey, String realmPublicKey) {
+                return null;
+            }
+
+            @Override
+            public HttpUriRequest createSamlSignedResponse(URI samlEndpoint, String relayState, Document samlRequest, String realmPrivateKey, String realmPublicKey, String certificateStr) {
+                return null;
+            }
+
+            @Override
+            public String extractRelayState(CloseableHttpResponse response) throws IOException {
+                return null;
+            }
+        }
+        ;
 
         public abstract SAMLDocumentHolder extractResponse(CloseableHttpResponse response, String realmPublicKey) throws IOException;
 
