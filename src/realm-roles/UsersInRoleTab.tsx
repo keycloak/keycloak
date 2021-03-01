65348bd125174579cd17b906284e0942e12cad3c
jenny-s51 <jshandel@redhat.com>
2021-03-01 12:20:58 -0500

    add popover

--- src/realm-roles/UsersInRoleTab.tsx
+++ src/realm-roles/UsersInRoleTab.tsx
@@ -1,13 +1,18 @@
 import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import { PageSection } from "@patternfly/react-core";
+import { Button, PageSection, Popover } from "@patternfly/react-core";
 import { ListEmptyState } from "../components/list-empty-state/ListEmptyState";
 import { KeycloakDataTable } from "../components/table-toolbar/KeycloakDataTable";
 import { boolFormatter, emptyFormatter } from "../util";
 import { useAdminClient } from "../context/auth/AdminClient";
+import { QuestionCircleIcon } from "@patternfly/react-icons";
+import { useRealm } from "../context/realm-context/RealmContext";
 
 export const UsersInRoleTab = () => {
+  const history = useHistory();
+  const { realm } = useRealm();
+
   const { t } = useTranslation("roles");
 
   const { id } = useParams<{ id: string }>();
@@ -33,6 +38,42 @@ export const UsersInRoleTab = () => {
           loader={loader}
           ariaLabelKey="roles:roleList"
           searchPlaceholderKey=""
+          toolbarItem={
+            <Popover
+              aria-label="Basic popover"
+              position="bottom"
+              bodyContent={
+                <div>
+                  {t("roles:whoWillAppearPopoverText")}
+                  <Button
+                    className="kc-groups-link"
+                    variant="link"
+                    onClick={() => history.push(`/${realm}/groups`)}
+                  >
+                    {t("groups")}
+                  </Button>
+                  {t("or")}
+                  <Button
+                    className="kc-users-link"
+                    variant="link"
+                    onClick={() => history.push(`/${realm}/users`)}
+                  >
+                    {t("users")}.
+                  </Button>
+                </div>
+              }
+              footerContent={t("roles:whoWillAppearPopoverFooterText")}
+            >
+              <Button
+                variant="link"
+                className="kc-who-will-appear-button"
+                key="who-will-appear-button"
+                icon={<QuestionCircleIcon />}
+              >
+                {t("roles:whoWillAppearLinkText")}
+              </Button>
+            </Popover>
+          }
           emptyState={
             <ListEmptyState
               hasIcon={true}
