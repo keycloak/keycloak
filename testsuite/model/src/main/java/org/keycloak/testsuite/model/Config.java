631eac09cdb1619eb8efd819a145da9e83c8558a
Hynek Mlnarik <hmlnarik@redhat.com>
2021-03-11 16:55:13 +0100

    KEYCLOAK-17412 Improve control of model tests

---
+++ testsuite/model/src/main/java/org/keycloak/testsuite/model/Config.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.keycloak.testsuite.model;
+
+import org.keycloak.Config.ConfigProvider;
+import org.keycloak.Config.Scope;
+import org.keycloak.Config.SystemPropertiesScope;
+import org.keycloak.common.util.StringPropertyReplacer;
+import org.keycloak.common.util.SystemEnvProperties;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author hmlnarik
+ */
+public class Config implements ConfigProvider {
+
+    private final Properties systemProperties = new SystemEnvProperties();
+
+    private final Map<String, String> properties = new HashMap<>();
+
+    public class SpiConfig {
+
+        private final String prefix;
+
+        public SpiConfig(String prefix) {
+            this.prefix = prefix;
+        }
+
+        public ProviderConfig provider(String provider) {
+            return new ProviderConfig(this, prefix + provider + ".");
+        }
+
+        public SpiConfig defaultProvider(String defaultProviderId) {
+            return config("provider", defaultProviderId);
+        }
+
+        public SpiConfig config(String key, String value) {
+            if (value == null) {
+                properties.remove(prefix + key);
+            } else {
+                properties.put(prefix + key, value);
+            }
+            return this;
+        }
+
+        public SpiConfig spi(String spiName) {
+            return new SpiConfig(spiName + ".");
+        }
+    }
+
+    public class ProviderConfig {
+
+        private final SpiConfig spiConfig;
+        private final String prefix;
+
+        public ProviderConfig(SpiConfig spiConfig, String prefix) {
+            this.spiConfig = spiConfig;
+            this.prefix = prefix;
+        }
+
+        public ProviderConfig config(String key, String value) {
+            if (value == null) {
+                properties.remove(prefix + key);
+            } else {
+                properties.put(prefix + key, value);
+            }
+            return this;
+        }
+
+        public ProviderConfig provider(String provider) {
+            return spiConfig.provider(provider);
+        }
+
+        public SpiConfig spi(String spiName) {
+            return new SpiConfig(spiName + ".");
+        }
+
+    }
+
+    private class MapConfigScope extends SystemPropertiesScope {
+
+        public MapConfigScope(String prefix) {
+            super(prefix);
+        }
+
+        @Override
+        public String get(String key, String defaultValue) {
+            String v = replaceProperties(properties.get(prefix + key));
+            if (v == null || v.isEmpty()) {
+                v = System.getProperty("keycloak." + prefix + key, defaultValue);
+            }
+            return v != null && ! v.isEmpty() ? v : null;
+        }
+
+        @Override
+        public Scope scope(String... scope) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(prefix).append(".");
+            for (String s : scope) {
+                sb.append(s);
+                sb.append(".");
+            }
+            return new MapConfigScope(sb.toString());
+        }
+    }
+
+    @Override
+    public String getProvider(String spiName) {
+        return properties.get(spiName + ".provider");
+    }
+
+    private String replaceProperties(String value) {
+        return StringPropertyReplacer.replaceProperties(value, systemProperties);
+    }
+
+    @Override
+    public Scope scope(String... scope) {
+        StringBuilder sb = new StringBuilder();
+        for (String s : scope) {
+            sb.append(s);
+            sb.append(".");
+        }
+        return new MapConfigScope(sb.toString());
+    }
+
+    public SpiConfig spi(String spiName) {
+        return new SpiConfig(spiName + ".");
+    }
+
+    @Override
+    public String toString() {
+        return properties.entrySet().stream()
+          .sorted((e1, e2) -> e1.getKey().compareTo(e2.getKey()))
+          .map(e -> e.getKey() + " = " + e.getValue())
+          .collect(Collectors.joining("\n    "));
+    }
+}
--- testsuite/model/src/test/java/org/keycloak/testsuite/model/parameters/Map.java
+++ testsuite/model/src/test/java/org/keycloak/testsuite/model/parameters/Map.java
@@ -18,12 +18,15 @@ package org.keycloak.testsuite.model.parameters;
 
 import org.keycloak.testsuite.model.KeycloakModelParameters;
 import org.keycloak.models.map.client.MapClientProviderFactory;
+import org.keycloak.models.map.clientscope.MapClientScopeProviderFactory;
 import org.keycloak.models.map.group.MapGroupProviderFactory;
 import org.keycloak.models.map.role.MapRoleProviderFactory;
 import org.keycloak.models.map.storage.MapStorageProvider;
 import org.keycloak.models.map.storage.MapStorageSpi;
+import org.keycloak.models.map.user.MapUserProviderFactory;
 import org.keycloak.provider.ProviderFactory;
 import org.keycloak.provider.Spi;
+import org.keycloak.testsuite.model.Config;
 import com.google.common.collect.ImmutableSet;
 import java.util.Set;
 
@@ -40,12 +43,24 @@ public class Map extends KeycloakModelParameters {
 
     static final Set<Class<? extends ProviderFactory>> ALLOWED_FACTORIES = ImmutableSet.<Class<? extends ProviderFactory>>builder()
       .add(MapClientProviderFactory.class)
+      .add(MapClientScopeProviderFactory.class)
       .add(MapGroupProviderFactory.class)
       .add(MapRoleProviderFactory.class)
+      .add(MapUserProviderFactory.class)
       .add(MapStorageProvider.class)
       .build();
 
     public Map() {
         super(ALLOWED_SPIS, ALLOWED_FACTORIES);
+    }
+
+    @Override
+    public void updateConfig(Config cf) {
+        cf.spi("client").defaultProvider(MapClientProviderFactory.PROVIDER_ID)
+          .spi("clientScope").defaultProvider(MapClientScopeProviderFactory.PROVIDER_ID)
+          .spi("group").defaultProvider(MapGroupProviderFactory.PROVIDER_ID)
+          .spi("role").defaultProvider(MapRoleProviderFactory.PROVIDER_ID)
+          .spi("user").defaultProvider(MapUserProviderFactory.PROVIDER_ID)
+        ;
     }
 }
