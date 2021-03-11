80c2bbd5772b6cacd9d98d316261927f59d3e45a
Hynek Mlnarik <hmlnarik@redhat.com>
2021-03-11 09:35:37 +0100

    KEYCLOAK-17377 Fix invalidation cluster tests (do not hide failures)

--- testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/cluster/ClientInvalidationClusterTest.java
+++ testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/cluster/ClientInvalidationClusterTest.java
@@ -4,7 +4,6 @@ import org.apache.commons.lang.RandomStringUtils;
 import org.junit.Before;
 import org.keycloak.admin.client.resource.ClientResource;
 import org.keycloak.admin.client.resource.ClientsResource;
-import org.keycloak.common.util.Retry;
 import org.keycloak.representations.idm.ClientRepresentation;
 import org.keycloak.testsuite.admin.ApiUtil;
 import org.keycloak.testsuite.arquillian.ContainerInfo;
@@ -59,38 +58,24 @@ public class ClientInvalidationClusterTest extends AbstractInvalidationClusterTe
 
     @Override
     protected ClientRepresentation readEntity(ClientRepresentation client, ContainerInfo node) {
-        ClientRepresentation u = Retry.call(new Retry.Supplier<ClientRepresentation>() {
-            @Override
-            public ClientRepresentation get(int iteration) {
-                try {
-                    return entityResource(client, node).toRepresentation();
-                } catch (NotFoundException nfe) {
-                    return null;
-                }
-            }
-        }, 3, 5000);
+        ClientRepresentation u = null;
+        try {
+            u = entityResource(client, node).toRepresentation();
+        } catch (NotFoundException nfe) {
+            // expected when client doesn't exist
+        }
         return u;
     }
 
     @Override
     protected ClientRepresentation updateEntity(ClientRepresentation client, ContainerInfo node) {
-        Retry.execute(new Runnable() {
-            @Override
-            public void run() {
-                entityResource(client, node).update(client);
-            }
-        }, 3, 5000);
+        entityResource(client, node).update(client);
         return readEntity(client, node);
     }
 
     @Override
     protected void deleteEntity(ClientRepresentation client, ContainerInfo node) {
-        Retry.execute(new Runnable() {
-            @Override
-            public void run() {
-                entityResource(client, node).remove();
-            }
-        }, 3, 5000);
+        entityResource(client, node).remove();
         assertNull(readEntity(client, node));
     }
 
