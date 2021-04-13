43febe928b69e566e339028aac004d6aef702f78
vramik <vramik@redhat.com>
2021-04-13 22:23:48 +0200

    KEYCLOAK-17615 Move database initialization from KeycloakApplication to JpaConnectionProviderFactory

--- testsuite/model/src/test/java/org/keycloak/testsuite/model/MigrationModelTest.java
+++ testsuite/model/src/test/java/org/keycloak/testsuite/model/MigrationModelTest.java
@@ -1,33 +1,62 @@
+/*
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.keycloak.testsuite.model;
 
+import java.util.List;
+import javax.persistence.EntityManager;
+import org.jboss.logging.Logger;
 import org.junit.Assert;
 import org.junit.Test;
 import org.keycloak.common.Version;
 import org.keycloak.common.util.Time;
 import org.keycloak.connections.jpa.JpaConnectionProvider;
 import org.keycloak.migration.MigrationModel;
-import org.keycloak.models.jpa.entities.MigrationModelEntity;
-import org.keycloak.representations.idm.RealmRepresentation;
-import org.keycloak.testsuite.AbstractKeycloakTest;
-import org.keycloak.testsuite.arquillian.annotation.AuthServerContainerExclude;
-
-import javax.persistence.EntityManager;
-import java.util.List;
-import org.jboss.logging.Logger;
+import org.keycloak.models.ClientProvider;
+import org.keycloak.models.ClientScopeProvider;
+import org.keycloak.models.Constants;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.RealmProvider;
 import org.keycloak.models.ServerInfoProvider;
+import org.keycloak.models.jpa.entities.MigrationModelEntity;
 
-import static org.keycloak.testsuite.arquillian.annotation.AuthServerContainerExclude.AuthServer.REMOTE;
+@RequireProvider(value=RealmProvider.class, only="jpa")
+@RequireProvider(value=ClientProvider.class, only="jpa")
+@RequireProvider(value=ClientScopeProvider.class, only="jpa")
+public class MigrationModelTest extends KeycloakModelTest {
 
-@AuthServerContainerExclude(REMOTE)
-public class MigrationModelTest extends AbstractKeycloakTest {
+    private String realmId;
 
     @Override
-    public void addTestRealms(List<RealmRepresentation> testRealms) {
+    public void createEnvironment(KeycloakSession s) {
+        RealmModel realm = s.realms().createRealm("realm");
+        realm.setDefaultRole(s.roles().addRealmRole(realm, Constants.DEFAULT_ROLES_ROLE_PREFIX + "-" + realm.getName()));
+        this.realmId = realm.getId();
+    }
+
+    @Override
+    public void cleanEnvironment(KeycloakSession s) {
+        s.realms().removeRealm(realmId);
     }
 
     @Test
     public void test() {
-        testingClient.server().run(session -> {
+        inComittedTransaction(1, (session , i) -> {
+
             String currentVersion = Version.VERSION_KEYCLOAK.replaceAll("^(\\d+(?:\\.\\d+){0,2}).*$", "$1");
 
             JpaConnectionProvider p = session.getProvider(JpaConnectionProvider.class);
@@ -64,7 +93,8 @@ public class MigrationModelTest extends AbstractKeycloakTest {
             Assert.assertEquals(currentVersion, m.getStoredVersion());
 
             em.remove(l.get(1));
+
+            return null;
         });
     }
-
 }
