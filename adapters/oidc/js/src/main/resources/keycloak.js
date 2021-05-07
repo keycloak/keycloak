9db5d4b7d774f88d932c83ee84d22787e9ff68c1
Sanket Bhalerao <ketu.bhalerao@gmail.com>
2021-05-07 12:32:23 +0530

    update keycloak js for KEYCLOAK-15595
    while working on cordova+angular+ios the keycloak logout is not working. as the user clicks logout the user can again see the app instead of the inappbrowser page for login.
    with clearcache=yes in the inappbrowser open the issue appears no more.

--- adapters/oidc/js/src/main/resources/keycloak.js
+++ adapters/oidc/js/src/main/resources/keycloak.js
@@ -1475,7 +1475,7 @@
                         var promise = createPromise();
 
                         var logoutUrl = kc.createLogoutUrl(options);
-                        var ref = cordovaOpenWindowWrapper(logoutUrl, '_blank', 'location=no,hidden=yes');
+                        var ref = cordovaOpenWindowWrapper(logoutUrl, '_blank', 'location=no,hidden=yes,clearcache=yes');
 
                         var error;
 
