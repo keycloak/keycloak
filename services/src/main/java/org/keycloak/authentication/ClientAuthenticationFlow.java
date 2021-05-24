4df5932029bf383ca609113fd1c1e1b9a3bee0ec
mposolda <mposolda@gmail.com>
2021-05-24 09:24:22 +0200

    KEYCLOAK-18258 Not possible to login with public client, which was confidential with custom client authenticator set

--- services/src/main/java/org/keycloak/authentication/ClientAuthenticationFlow.java
+++ services/src/main/java/org/keycloak/authentication/ClientAuthenticationFlow.java
@@ -75,8 +75,9 @@ public class ClientAuthenticationFlow implements AuthenticationFlow {
             if (client != null) {
                 String expectedClientAuthType = client.getClientAuthenticatorType();
 
-                // Fallback to secret just in case (for backwards compatibility)
-                if (expectedClientAuthType == null) {
+                // Fallback to secret just in case (for backwards compatibility). Also for public clients, ignore the "clientAuthenticatorType", which is set to them and stick to the
+                // default, which set the client just based on "client_id" parameter
+                if (expectedClientAuthType == null || client.isPublicClient()) {
                     expectedClientAuthType = KeycloakModelUtils.getDefaultClientAuthenticatorType();
                     ServicesLogger.LOGGER.authMethodFallback(client.getClientId(), expectedClientAuthType);
                 }
