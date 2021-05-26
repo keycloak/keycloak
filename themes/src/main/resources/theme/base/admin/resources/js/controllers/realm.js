6e139b8fda5f78b2814d424f1c2cc7d10e167148
mposolda <mposolda@gmail.com>
2021-05-26 09:25:35 +0200

    KEYCLOAK-18280 Issues with boolean properties of executors

--- themes/src/main/resources/theme/base/admin/resources/js/controllers/realm.js
+++ themes/src/main/resources/theme/base/admin/resources/js/controllers/realm.js
@@ -3218,6 +3218,14 @@ module.controller('ClientPoliciesProfilesEditExecutorCtrl', function($scope, rea
                     break;
                 }
             }
+
+            // Convert boolean properties from the configuration to strings as expected by the kc-provider-config directive
+            for (var j=0 ; j < $scope.executorType.properties.length ; j++) {
+                var currentProperty = $scope.executorType.properties[j];
+                if (currentProperty.type === 'boolean') {
+                    $scope.executor.config[currentProperty.name] = ($scope.executor.config[currentProperty.name]) ? "true" : "false";
+                }
+            }
         }
 
     }
@@ -3229,6 +3237,10 @@ module.controller('ClientPoliciesProfilesEditExecutorCtrl', function($scope, rea
             } else {
                 return [];
             }
+        }
+
+        if (configProperty.type === 'boolean') {
+            return (configProperty.defaultValue) ? "true" : "false";
         }
 
         if (configProperty.defaultValue !== undefined) {
@@ -3259,6 +3271,14 @@ module.controller('ClientPoliciesProfilesEditExecutorCtrl', function($scope, rea
         }
 
         ComponentUtils.removeLastEmptyValue($scope.executor.config);
+
+        // Convert String properties required by the kc-provider-config directive back to booleans
+        for (var j=0 ; j < $scope.executorType.properties.length ; j++) {
+            var currentProperty = $scope.executorType.properties[j];
+            if (currentProperty.type === 'boolean') {
+                $scope.executor.config[currentProperty.name] = ($scope.executor.config[currentProperty.name] === "true") ? true : false;
+            }
+        }
 
         if ($scope.createNew) {
             var selectedExecutor = {
@@ -3580,6 +3600,14 @@ module.controller('ClientPoliciesEditConditionCtrl', function($scope, realm, ser
                     break;
                 }
             }
+
+            // Convert boolean properties from the configuration to strings as expected by the kc-provider-config directive
+            for (var j=0 ; j < $scope.conditionType.properties.length ; j++) {
+                var currentProperty = $scope.conditionType.properties[j];
+                if (currentProperty.type === 'boolean') {
+                    $scope.condition.config[currentProperty.name] = ($scope.condition.config[currentProperty.name]) ? "true" : "false";
+                }
+            }
         }
 
     }
@@ -3591,6 +3619,10 @@ module.controller('ClientPoliciesEditConditionCtrl', function($scope, realm, ser
             } else {
                 return [];
             }
+        }
+
+        if (configProperty.type === 'boolean') {
+            return (configProperty.defaultValue) ? "true" : "false";
         }
 
         if (configProperty.defaultValue !== undefined) {
@@ -3622,6 +3654,14 @@ module.controller('ClientPoliciesEditConditionCtrl', function($scope, realm, ser
         }
 
         ComponentUtils.removeLastEmptyValue($scope.condition.config);
+
+        // Convert String properties required by the kc-provider-config directive back to booleans
+        for (var j=0 ; j < $scope.conditionType.properties.length ; j++) {
+            var currentProperty = $scope.conditionType.properties[j];
+            if (currentProperty.type === 'boolean') {
+                $scope.condition.config[currentProperty.name] = ($scope.condition.config[currentProperty.name] === "true") ? true : false;
+            }
+        }
 
         var selectedCondition;
         if ($scope.createNew) {
