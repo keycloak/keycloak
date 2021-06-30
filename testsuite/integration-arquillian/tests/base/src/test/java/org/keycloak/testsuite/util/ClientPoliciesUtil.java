db81bda303af0502ea5a5a354af6972408aaef45
Patrick Weiner <patrick.weiner@prime-sign.com>
2021-06-30 14:33:01 +0200

    Add test for RegexRedirectUri-Policy.

--- testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/util/ClientPoliciesUtil.java
+++ testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/util/ClientPoliciesUtil.java
@@ -29,6 +29,7 @@ import org.keycloak.representations.idm.ClientPolicyRepresentation;
 import org.keycloak.representations.idm.ClientProfileRepresentation;
 import org.keycloak.representations.idm.ClientProfilesRepresentation;
 import org.keycloak.services.clientpolicy.condition.ClientAccessTypeCondition;
+import org.keycloak.services.clientpolicy.condition.ClientIdsCondition;
 import org.keycloak.services.clientpolicy.condition.ClientRolesCondition;
 import org.keycloak.services.clientpolicy.condition.ClientScopesCondition;
 import org.keycloak.services.clientpolicy.condition.ClientUpdaterContextCondition;
@@ -38,6 +39,8 @@ import org.keycloak.services.clientpolicy.condition.ClientUpdaterSourceRolesCond
 import org.keycloak.services.clientpolicy.executor.FullScopeDisabledExecutor;
 import org.keycloak.services.clientpolicy.executor.HolderOfKeyEnforcerExecutor;
 import org.keycloak.services.clientpolicy.executor.PKCEEnforcerExecutor;
+import org.keycloak.services.clientpolicy.executor.RegexRedirectUriExecutor;
+import org.keycloak.services.clientpolicy.executor.RegexRedirectUriExecutorFactory;
 import org.keycloak.services.clientpolicy.executor.SecureClientAuthenticatorExecutor;
 import org.keycloak.services.clientpolicy.executor.SecureRequestObjectExecutor;
 import org.keycloak.services.clientpolicy.executor.SecureResponseTypeExecutor;
@@ -190,6 +193,12 @@ public final class ClientPoliciesUtil {
         config.setDefaultAlgorithm(defaultAlgorithm);
         return config;
     }
+    
+    public static RegexRedirectUriExecutor.Configuration createRegexRedirectUriExecutorConfig(List<String> regexPatterns){
+        RegexRedirectUriExecutor.Configuration config = new RegexRedirectUriExecutor.Configuration();
+        config.setRedirectUriRegexPatterns(regexPatterns);
+        return config;
+    }
 
     public static class ClientPoliciesBuilder {
         private final ClientPoliciesRepresentation policiesRep;
@@ -329,6 +338,12 @@ public final class ClientPoliciesUtil {
     public static ClientUpdaterSourceRolesCondition.Configuration createClientUpdateSourceRolesConditionConfig(List<String> roles) {
         ClientUpdaterSourceRolesCondition.Configuration config = new ClientUpdaterSourceRolesCondition.Configuration();
         config.setRoles(roles);
+        return config;
+    }
+    
+    public static ClientIdsCondition.Configuration createClientIdsConditionConfig(List<String> clientIds){
+        ClientIdsCondition.Configuration config = new ClientIdsCondition.Configuration();
+        config.setClientIds(clientIds);
         return config;
     }
 }
