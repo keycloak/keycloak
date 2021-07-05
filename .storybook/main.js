2bc533b9da0502888e20fba81a70b614afc9f2ce
Jon Koops <jkoops@redhat.com>
2021-07-05 14:52:55 +0200

    Fix Storybook server and update to latest version
    - Fix build for Storybook
    - Update Storybook and dependencies to latest version
    - Update Storybook files from React template
    - Add task to CI to verify Storybook build

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,19 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        // Explicitly enable PostCSS 8+ (see: https://storybook.js.org/addons/@storybook/addon-postcss)
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
   ]
 }
