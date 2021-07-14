b758a1f8b66148ad97b194647c4226fbaf5fc1ee
Agnieszka Gancarczyk <agancarc@redhat.com>
2021-07-14 11:41:04 +0100

    sessions: added cypress tests review changes

--- cypress/support/pages/admin_console/manage/sessions/SessionsPage.ts
+++ cypress/support/pages/admin_console/manage/sessions/SessionsPage.ts
@@ -11,11 +11,11 @@ export default class SessionsPage {
   constructor() {
     this.sessionTypeDrpDwn = ".pf-c-select__toggle";
     this.sessionTypeList = ".pf-c-select__toggle + ul";
-    this.allSessionTypesOption = '[data-testid="all-sessions-option"]';
-    this.regularSSOOption = '[data-testid="regular-sso-option"';
-    this.offlineOption = '[data-testid="offline-option"';
-    this.directGrantOption = '[data-testid="direct-grant-option"';
-    this.serviceAccountOption = '[data-testid="service-account-option"';
+    this.allSessionTypesOption = "all-sessions-option";
+    this.regularSSOOption = "regular-sso-option";
+    this.offlineOption = "offline-option";
+    this.directGrantOption = "direct-grant-option";
+    this.serviceAccountOption = "service-account-option";
     this.selectedType = ".pf-c-select__toggle-text";
   }
 
@@ -32,31 +32,31 @@ export default class SessionsPage {
 
   selectAllSessionsType() {
     cy.get(this.sessionTypeDrpDwn).click();
-    cy.get(this.allSessionTypesOption).click();
+    cy.getId(this.allSessionTypesOption).click();
     cy.get(this.selectedType).should('have.text', 'All session types');
   }
 
   selectRegularSSO() {
     cy.get(this.sessionTypeDrpDwn).click();
-    cy.get(this.regularSSOOption).click();
+    cy.getId(this.regularSSOOption).click();
     cy.get(this.selectedType).should('have.text', 'Regular SSO');
   }
 
   selectOffline() {
     cy.get(this.sessionTypeDrpDwn).click();
-    cy.get(this.offlineOption).click();
+    cy.getId(this.offlineOption).click();
     cy.get(this.selectedType).should('have.text', 'Offline');
   }
 
   selectDirectGrant() {
     cy.get(this.sessionTypeDrpDwn).click();
-    cy.get(this.directGrantOption).click();
+    cy.getId(this.directGrantOption).click();
     cy.get(this.selectedType).should('have.text', 'Direct grant');
   }
 
   selectServiceAccount() {
     cy.get(this.sessionTypeDrpDwn).click();
-    cy.get(this.serviceAccountOption).click();
+    cy.getId(this.serviceAccountOption).click();
     cy.get(this.selectedType).should('have.text', 'Service account');
   }
 }
