a0cd879857e1412a2a9f3c80801dabcadbecf4a1
Jon Koops <jonkoops@gmail.com>
2021-07-20 16:50:37 +0200

    Clean up and modernize ESlint configuration (#866)

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+/** @type {import("eslint").Linter.Config } */
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/base",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:prettier/recommended",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["error"],
+  },
+};
