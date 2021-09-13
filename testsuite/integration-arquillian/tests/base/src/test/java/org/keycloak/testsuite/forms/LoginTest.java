a6cd80c9332be7f26e47d2fa131156f95f7ac146
David Hellwig <hellwig.david@gmail.com>
2021-09-13 11:30:11 +0200

    KEYCLOAK-16076 added new warining when cookies are disabled -with new branch- (#7632)
    * KEYCLOAK-16076 added new warining when cookies are disabled
    
    Co-authored-by: David Hellwig <david.hellwig@bosch.com>
    Co-authored-by: Christoph Leistert <christoph.leistert@bosch-si.com>

--- testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/forms/LoginTest.java
+++ testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/forms/LoginTest.java
@@ -840,7 +840,26 @@ public class LoginTest extends AbstractTestRealmKeycloakTest {
         Assert.assertNotNull(link, thirdParty.getBaseUrl());
     }
 
+    @Test
+    public void loginWithDisabledCookies() {
+        String userId = adminClient.realm("test").users().search("test-user@localhost").get(0).getId();
+        oauth.clientId("test-app");
+        oauth.openLoginForm();
 
+        driver.manage().deleteAllCookies();
+
+
+        // Cookie has been deleted or disabled, the error shown in the UI should be Errors.COOKIE_NOT_FOUND
+        loginPage.login("login@test.com", "password");
+
+        events.expect(EventType.LOGIN_ERROR)
+                .user(new UserRepresentation())
+                .client(new ClientRepresentation())
+                .error(Errors.COOKIE_NOT_FOUND)
+                .assertEvent();
+
+        errorPage.assertCurrent();
+    }
 
     @Test
     public void openLoginFormWithDifferentApplication() throws Exception {
