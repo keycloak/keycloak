ac369f6b30ec3deaef4ce16ca4fb250155e66b7a
Bruno Oliveira da Silva <bruno@abstractj.org>
2021-09-15 19:20:20 -0300

    [KEYCLOAK-19321] Remove the remaining Travis files

--- .travis/docker-hub-master-trigger.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$DOCKER_HUB_TOKEN" != "" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_REPO_SLUG" == "keycloak/keycloak" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-    curl -H "Content-Type: application/json" --data '{"docker_tag": "master"}' -X POST https://registry.hub.docker.com/u/jboss/keycloak/trigger/$DOCKER_HUB_TOKEN/
-    echo "Triggered Docker hub build"
-elif [ "$DOCKER_HUB_TOKEN" != "" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_REPO_SLUG" == "keycloak/keycloak" ] && [ "$TRAVIS_BRANCH" == "openshift-integration2" ]; then
-    curl -H "Content-Type: application/json" --data '{"docker_tag": "nightly-openshift-integration2"}' -X POST https://registry.hub.docker.com/u/jboss/keycloak/trigger/$DOCKER_HUB_TOKEN/
-    echo "Triggered Docker hub build"
-fi
