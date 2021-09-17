db95cb79ace1f027953a4c6f8a13e96c140b9ed3
stianst <stianst@gmail.com>
2021-09-17 13:16:20 +0200

    Temporary fix to set keycloak-admin-ui version

--- set-version.sh
+++ set-version.sh
@@ -3,3 +3,4 @@
 NEW_VERSION=$1
 
 mvn versions:set -DnewVersion=$NEW_VERSION -DgenerateBackupPoms=false -DgroupId=org.keycloak* -DartifactId=*
+mvn versions:use-dep-version -Dincludes=org.keycloak:keycloak-admin-ui -DdepVersion=$NEW_VERSION -DforceVersion=true -pl org.keycloak:keycloak-parent
