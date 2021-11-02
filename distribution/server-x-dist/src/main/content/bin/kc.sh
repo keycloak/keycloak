5ffcd5ec527d2c6183ef211e4cba6b44dba134df
Dominik Guhr <dguhr@redhat.com>
2021-11-02 20:21:21 +0100

    KEYCLOAK-19396_remove_logmsg

--- distribution/server-x-dist/src/main/content/bin/kc.sh
+++ distribution/server-x-dist/src/main/content/bin/kc.sh
@@ -62,7 +62,7 @@ done
 # Specify options to pass to the Java VM.
 #
 if [ "x$JAVA_OPTS" = "x" ]; then
-   JAVA_OPTS="-Xms64m -Xmx512m -XX:MetaspaceSize=96M -XX:MaxMetaspaceSize=256m -Djava.net.preferIPv4Stack=true"
+   JAVA_OPTS="-Xms64m -Xmx512m -XX:MetaspaceSize=96M -XX:MaxMetaspaceSize=256m -Djava.net.preferIPv4Stack=true -Dquarkus-log-max-startup-records=10000"
 else
    echo "JAVA_OPTS already set in environment; overriding default settings with values: $JAVA_OPTS"
 fi
