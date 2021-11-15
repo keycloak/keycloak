4428a4e088f41483dbe465e6fcd874f0b4477ee8
Erik Jan de Wit <erikjan.dewit@gmail.com>
2021-11-15 08:28:55 +0100

    Added progress bar and option to pass flags to standalone script (#1550)
    * add the ability to add parameters to standalone
    
    * added progress bars

--- start.mjs
+++ start.mjs
@@ -5,10 +5,13 @@ import path from "node:path";
 import { spawn } from "node:child_process";
 
 import decompress from "decompress";
-import decompressTargz from "decompress-targz";
+
+import ProgressPromise from "progress-promise";
+import cliProgress from "cli-progress";
+import colors from "colors";
 
 const args = process.argv.slice(2);
-const version = args[0] || "15.0.2";
+const version = args[0] && !args[0].startsWith("-") ? args[0] : "15.0.2";
 
 const folder = "server";
 const fileName = path.join(folder, `keycloak-${version}.tar.gz`);
@@ -19,13 +22,36 @@ if (!fs.existsSync(folder)) {
   fs.mkdirSync(folder);
 }
 
+const progressTick = () => {
+  return new ProgressPromise((resolve, _, progress) => {
+    for (let i = 0; i < 10; i++) {
+      setTimeout(() => progress(i * 10), i * 1500);
+    }
+    setTimeout(resolve, 15000);
+  });
+};
+
 async function decompressKeycloak() {
   try {
-    await decompress(fileName, folder, {
-      plugins: [decompressTargz()],
+    const progressBar = new cliProgress.Bar({
+      format:
+        "Decompress |" +
+        colors.cyan("{bar}") +
+        "| {percentage}% || ETA: {eta}s ",
+      barCompleteChar: "\u2588",
+      barIncompleteChar: "\u2591",
     });
-
-    console.log("Files decompressed");
+    progressBar.start(100);
+    await Promise.all([
+      decompress(fileName, folder),
+      progressTick()
+        .progress((value) => progressBar.update(value))
+        .then(() => {
+          progressBar.update(100);
+          progressBar.stop();
+          console.log("\nFiles decompressed");
+        }),
+    ]);
   } catch (error) {
     console.error(error);
   }
@@ -41,6 +67,7 @@ const run = () => {
     const proc = spawn(path.join(serverPath, "bin", `standalone${extension}`), [
       "-Djboss.socket.binding.port-offset=100",
       "-Dprofile.feature.newadmin=enabled",
+      ...args,
     ]);
     proc.stdout.on("data", (data) => {
       console.log(data.toString());
@@ -48,14 +75,22 @@ const run = () => {
   });
 };
 
-const request = (url, file) => {
+const request = (url, file, progressBar) => {
   http.get(url, (response) => {
-    if (response.statusCode == 302) {
-      request(response.headers.location, file);
+    if (response.statusCode === 302) {
+      request(response.headers.location, file, progressBar);
+    } else if (response.statusCode === 404) {
+      throw new Error(`version not found '${version}'`);
     } else {
+      let data = 0;
+      progressBar.start(parseInt(response.headers["content-length"]), 0);
       response.pipe(file);
+      response.on("data", (chunk) => {
+        progressBar.update(data);
+        data += chunk.length;
+      });
       response.on("end", () => {
-        console.log("Downloaded keycloak");
+        console.log("\nDownloaded keycloak");
         decompressKeycloak().then(() => run());
       });
     }
@@ -64,9 +99,19 @@ const request = (url, file) => {
 
 if (!fs.existsSync(fileName)) {
   const file = fs.createWriteStream(fileName);
+  const progressBar = new cliProgress.Bar({
+    format:
+      "Download |" +
+      colors.cyan("{bar}") +
+      "| {percentage}% || {value}/{total} Chunks",
+    barCompleteChar: "\u2588",
+    barIncompleteChar: "\u2591",
+  });
+
   request(
     `https://github.com/keycloak/keycloak/releases/download/${version}/keycloak-${version}.tar.gz`,
-    file
+    file,
+    progressBar
   );
 } else if (!fs.existsSync(serverPath)) {
   decompressKeycloak().then(() => run());
