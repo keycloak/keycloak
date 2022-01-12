8ea09d38168c22937363cf77a07f9de5dc7b48b0
Daniel Gozalo <48915630+dgozalo@users.noreply.github.com>
2022-01-12 14:27:24 +0100

    [fixes #9222] - Let users configure Dynamic Client Scopes (#9327)

--- services/src/main/java/org/keycloak/services/resources/admin/ClientScopeResource.java
+++ services/src/main/java/org/keycloak/services/resources/admin/ClientScopeResource.java
@@ -19,6 +19,8 @@ package org.keycloak.services.resources.admin;
 import org.jboss.logging.Logger;
 import org.jboss.resteasy.annotations.cache.NoCache;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
+import org.keycloak.common.Profile;
+import org.keycloak.events.Errors;
 import org.keycloak.events.admin.OperationType;
 import org.keycloak.events.admin.ResourceType;
 import org.keycloak.models.ClientScopeModel;
@@ -29,7 +31,9 @@ import org.keycloak.models.RealmModel;
 import org.keycloak.models.utils.ModelToRepresentation;
 import org.keycloak.models.utils.RepresentationToModel;
 import org.keycloak.representations.idm.ClientScopeRepresentation;
+import org.keycloak.saml.common.util.StringUtil;
 import org.keycloak.services.ErrorResponse;
+import org.keycloak.services.ErrorResponseException;
 import org.keycloak.services.resources.admin.permissions.AdminPermissionEvaluator;
 
 import javax.ws.rs.Consumes;
@@ -40,6 +44,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.regex.Pattern;
 
 
 /**
@@ -56,6 +64,7 @@ public class ClientScopeResource {
     private AdminEventBuilder adminEvent;
     protected ClientScopeModel clientScope;
     protected KeycloakSession session;
+    protected static Pattern dynamicScreenPattern = Pattern.compile("[^\\s\\*]*\\*{1}[^\\s\\*]*");
 
     public ClientScopeResource(RealmModel realm, AdminPermissionEvaluator auth, ClientScopeModel clientScope, KeycloakSession session, AdminEventBuilder adminEvent) {
         this.realm = realm;
@@ -96,7 +105,7 @@ public class ClientScopeResource {
     @Consumes(MediaType.APPLICATION_JSON)
     public Response update(final ClientScopeRepresentation rep) {
         auth.clients().requireManageClientScopes();
-
+        validateDynamicClientScope(rep);
         try {
             RepresentationToModel.updateClientScope(rep, clientScope);
             adminEvent.operation(OperationType.UPDATE).resourcePath(session.getContext().getUri()).representation(rep).success();
@@ -141,6 +150,43 @@ public class ClientScopeResource {
             return Response.noContent().build();
         } catch (ModelException me) {
             return ErrorResponse.error(me.getMessage(), Response.Status.BAD_REQUEST);
+        }
+    }
+
+    /**
+     * Performs some validation based on attributes combinations and format.
+     * Validations differ based on whether the DYNAMIC_SCOPES feature is enabled or not
+     * @param clientScope
+     * @throws ErrorResponseException
+     */
+    public static void validateDynamicClientScope(ClientScopeRepresentation clientScope) throws ErrorResponseException {
+        if(clientScope.getAttributes() == null) {
+            return;
+        }
+        boolean isDynamic = Boolean.parseBoolean(clientScope.getAttributes().get(ClientScopeModel.IS_DYNAMIC_SCOPE));
+        String regexp = clientScope.getAttributes().get(ClientScopeModel.DYNAMIC_SCOPE_REGEXP);
+        if(Profile.isFeatureEnabled(Profile.Feature.DYNAMIC_SCOPES)) {
+            // if the scope is dynamic but the regexp is empty, it's not considered valid
+            if(isDynamic && StringUtil.isNullOrEmpty(regexp)) {
+                throw new ErrorResponseException(ErrorResponse.error("Dynamic scope regexp must not be null or empty", Response.Status.BAD_REQUEST));
+            }
+            // Always validate the dynamic scope regexp to avoid inserting a wrong value even when the feature is disabled
+            if(!StringUtil.isNullOrEmpty(regexp) && !dynamicScreenPattern.matcher(regexp).matches()) {
+                throw new ErrorResponseException(ErrorResponse.error(String.format("Invalid format for the Dynamic Scope regexp %1s", regexp), Response.Status.BAD_REQUEST));
+            }
+        } else {
+            // if the value is not null or empty we won't accept the request as the feature is disabled
+            Optional.ofNullable(regexp).ifPresent(s -> {
+                if(!s.isEmpty()) {
+                    throw new ErrorResponseException(ErrorResponse.error(String.format("Unexpected value \"%1s\" for attribute %2s in ClientScope",
+                            regexp, ClientScopeModel.DYNAMIC_SCOPE_REGEXP), Response.Status.BAD_REQUEST));
+                }
+            });
+            // If isDynamic is true, we won't accept the request as the feature is disabled
+            if(isDynamic) {
+                throw new ErrorResponseException(ErrorResponse.error(String.format("Unexpected value \"%1s\" for attribute %2s in ClientScope",
+                        isDynamic, ClientScopeModel.IS_DYNAMIC_SCOPE), Response.Status.BAD_REQUEST));
+            }
         }
     }
 }
