3f1a03824d2cf24901a06eecc4b2fa9670d43364
Jon Koops <jonkoops@gmail.com>
2022-01-20 18:11:27 +0100

    Modernize TypeScript definitions of JS adapter

--- adapters/oidc/js/dist/keycloak-authz.d.ts
+++ adapters/oidc/js/dist/keycloak-authz.d.ts
@@ -18,105 +18,99 @@
  * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-import * as Keycloak from './keycloak';
+import Keycloak from './keycloak';
 
-export as namespace KeycloakAuthorization;
+export interface KeycloakAuthorizationPromise {
+	then(onGrant: (rpt: string) => void, onDeny: () => void, onError: () => void): void;
+}
 
-export = KeycloakAuthorization;
+export interface AuthorizationRequest {
+	/**
+	 * An array of objects representing the resource and scopes.
+	 */
+	permissions?:ResourcePermission[],
 
-/**
- * Creates a new Keycloak client instance.
- * @param config Path to a JSON config file or a plain config object.
- */
-declare function KeycloakAuthorization(keycloak: Keycloak.KeycloakInstance): KeycloakAuthorization.KeycloakAuthorizationInstance;
+	/**
+	 * A permission ticket obtained from a resource server when using UMA authorization protocol.
+	 */
+	ticket?:string,
 
-declare namespace KeycloakAuthorization {
-	interface KeycloakAuthorizationPromise {
-		then(onGrant: (rpt: string) => void, onDeny: () => void, onError: () => void): void;
-	}
+	/**
+	 * A boolean value indicating whether the server should create permission requests to the resources
+	 * and scopes referenced by a permission ticket. This parameter will only take effect when used together
+	 * with the ticket parameter as part of a UMA authorization process.
+	 */
+	submitRequest?:boolean,
 
-    interface AuthorizationRequest {
-        /**
-         * An array of objects representing the resource and scopes.
-         */
-        permissions?:ResourcePermission[],
+	/**
+	 * Defines additional information about this authorization request in order to specify how it should be processed
+	 * by the server.
+	 */
+	metadata?:AuthorizationRequestMetadata,
 
-        /**
-         * A permission ticket obtained from a resource server when using UMA authorization protocol.
-         */
-        ticket?:string,
+	/**
+	 * Defines whether or not this authorization request should include the current RPT. If set to true, the RPT will
+	 * be sent and permissions in the current RPT will be included in the new RPT. Otherwise, only the permissions referenced in this
+	 * authorization request will be granted in the new RPT.
+	 */
+	incrementalAuthorization?:boolean
+}
 
-        /**
-         * A boolean value indicating whether the server should create permission requests to the resources
-         * and scopes referenced by a permission ticket. This parameter will only take effect when used together
-         * with the ticket parameter as part of a UMA authorization process.
-         */
-        submitRequest?:boolean,
+export interface AuthorizationRequestMetadata {
+	/**
+	 * A boolean value indicating to the server if resource names should be included in the RPT’s permissions.
+	 * If false, only the resource identifier is included.
+	 */
+	responseIncludeResourceName?:any,
 
-        /**
-         * Defines additional information about this authorization request in order to specify how it should be processed
-         * by the server.
-         */
-        metadata?:AuthorizationRequestMetadata,
+	/**
+	 * An integer N that defines a limit for the amount of permissions an RPT can have. When used together with
+	 * rpt parameter, only the last N requested permissions will be kept in the RPT.
+	 */
+	response_permissions_limit?:number
+}
 
-        /**
-         * Defines whether or not this authorization request should include the current RPT. If set to true, the RPT will
-         * be sent and permissions in the current RPT will be included in the new RPT. Otherwise, only the permissions referenced in this
-         * authorization request will be granted in the new RPT.
-         */
-        incrementalAuthorization?:boolean
-    }
+export interface ResourcePermission {
+	/**
+	 * The id or name of a resource.
+	 */
+	id:string,
 
-    interface AuthorizationRequestMetadata {
-        /**
-         * A boolean value indicating to the server if resource names should be included in the RPT’s permissions.
-         * If false, only the resource identifier is included.
-         */
-        responseIncludeResourceName?:any,
+	/**
+	 * An array of strings where each value is the name of a scope associated with the resource.
+	 */
+	scopes?:string[]
+}
 
-        /**
-         * An integer N that defines a limit for the amount of permissions an RPT can have. When used together with
-         * rpt parameter, only the last N requested permissions will be kept in the RPT.
-         */
-        response_permissions_limit?:number
-    }
+export default class KeycloakAuthorization {
+	/**
+	 * Creates a new Keycloak client instance.
+	 * @param config Path to a JSON config file or a plain config object.
+	 */
+	constructor(keycloak: Keycloak)
 
-    interface ResourcePermission {
-        /**
-         * The id or name of a resource.
-         */
-        id:string,
+	rpt: any;
+	config: { rpt_endpoint: string };
 
-        /**
-         * An array of strings where each value is the name of a scope associated with the resource.
-         */
-        scopes?:string[]
-    }
+	init(): void;
 
-	interface KeycloakAuthorizationInstance {
-		rpt: any;
-		config: { rpt_endpoint: string };
+	/**
+	 * This method enables client applications to better integrate with resource servers protected by a Keycloak
+	 * policy enforcer using UMA protocol.
+	 *
+	 * The authorization request must be provided with a ticket.
+	 *
+	 * @param authorizationRequest An AuthorizationRequest instance with a valid permission ticket set.
+	 * @returns A promise to set functions to be invoked on grant, deny or error.
+	 */
+	authorize(authorizationRequest: AuthorizationRequest): KeycloakAuthorizationPromise;
 
-		init(): void;
-
-		/**
-         * This method enables client applications to better integrate with resource servers protected by a Keycloak
-         * policy enforcer using UMA protocol.
-         *
-         * The authorization request must be provided with a ticket.
-         *
-         * @param authorizationRequest An AuthorizationRequest instance with a valid permission ticket set.
-         * @returns A promise to set functions to be invoked on grant, deny or error.
-         */
-		authorize(authorizationRequest: AuthorizationRequest): KeycloakAuthorizationPromise;
-
-		/**
-		 * Obtains all entitlements from a Keycloak server based on a given resourceServerId.
-         *
-         * @param resourceServerId The id (client id) of the resource server to obtain permissions from.
-         * @param authorizationRequest An AuthorizationRequest instance.
-         * @returns A promise to set functions to be invoked on grant, deny or error.
-		 */
-		entitlement(resourceServerId: string, authorizationRequest?: AuthorizationRequest): KeycloakAuthorizationPromise;
-	}
+	/**
+	 * Obtains all entitlements from a Keycloak server based on a given resourceServerId.
+	 *
+	 * @param resourceServerId The id (client id) of the resource server to obtain permissions from.
+	 * @param authorizationRequest An AuthorizationRequest instance.
+	 * @returns A promise to set functions to be invoked on grant, deny or error.
+	 */
+	entitlement(resourceServerId: string, authorizationRequest?: AuthorizationRequest): KeycloakAuthorizationPromise;
 }
