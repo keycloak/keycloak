623aaf1e8b08547cbc54d0b6f821a2f6faa19fb0
Francis PEROT <francis.perot@elca.ch>
2022-02-02 12:33:10 +0100

    Fixes collection comparison ignoring order
    Use of containsAll() does not permit to compare if 2 lists are equals
    (ignoring order)
    Previous implementation of CollectionUtil.collectionEquals(...) was not taking care of specific cases where you can have [ A, A, B ] and [ A, B, B ] and complexity was O(n²)
    Using Map, complexity is now O(n)
    
    Closes #9920

--- common/src/test/java/org/keycloak/common/util/CollectionUtilTest.java
+++ common/src/test/java/org/keycloak/common/util/CollectionUtilTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -13,29 +14,29 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class CollectionUtilTest {
 
-  @Test
-  public void joinInputNoneOutputEmpty() {
-    final ArrayList<String> strings = new ArrayList<>();
-    final String retval = CollectionUtil.join(strings, ",");
-    Assert.assertEquals("", retval);
-  }
+    @Test
+    public void joinInputNoneOutputEmpty() {
+        final ArrayList<String> strings = new ArrayList<>();
+        final String retval = CollectionUtil.join(strings, ",");
+        Assert.assertEquals("", retval);
+    }
 
-  @Test
-  public void joinInput2SeparatorNull() {
-    final ArrayList<String> strings = new ArrayList<>();
-    strings.add("foo");
-    strings.add("bar");
-    final String retval = CollectionUtil.join(strings, null);
-    Assert.assertEquals("foonullbar", retval);
-  }
+    @Test
+    public void joinInput2SeparatorNull() {
+        final ArrayList<String> strings = new ArrayList<>();
+        strings.add("foo");
+        strings.add("bar");
+        final String retval = CollectionUtil.join(strings, null);
+        Assert.assertEquals("foonullbar", retval);
+    }
 
-  @Test
-  public void joinInput1SeparatorNotNull() {
-    final ArrayList<String> strings = new ArrayList<>();
-    strings.add("foo");
-    final String retval = CollectionUtil.join(strings, ",");
-    Assert.assertEquals("foo", retval);
-  }
+    @Test
+    public void joinInput1SeparatorNotNull() {
+        final ArrayList<String> strings = new ArrayList<>();
+        strings.add("foo");
+        final String retval = CollectionUtil.join(strings, ",");
+        Assert.assertEquals("foo", retval);
+    }
 
   @Test
   public void joinInput2SeparatorNotNull() {
@@ -68,4 +69,12 @@ public class CollectionUtilTest {
     assertThat(CollectionUtil.isEmpty(set), is(false));
     assertThat(CollectionUtil.isNotEmpty(set), is(true));
   }
+
+    @Test
+    public void equalsCollectionTest() {
+        Assert.assertFalse(CollectionUtil.collectionEquals(Arrays.asList(1, 3, 2), Arrays.asList(1, 3)));
+        Assert.assertFalse(CollectionUtil.collectionEquals(Arrays.asList("A", "C"), Arrays.asList("A", "C", "B")));
+        Assert.assertFalse(CollectionUtil.collectionEquals(Arrays.asList(1, 3, 2, 3), Arrays.asList(1, 2, 3, 2)));
+        Assert.assertTrue(CollectionUtil.collectionEquals(Arrays.asList(1, 3, 3), Arrays.asList(3, 1, 3)));
+    }
 }
