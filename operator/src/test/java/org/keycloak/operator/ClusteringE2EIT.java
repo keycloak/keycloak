8e774d57cf1d5027d71a465943e2bf5a6b1b6ca2
Jonathan Vila <jvilalop@redhat.com>
2022-02-16 15:38:17 +0100

    Service and discovery service Closes #9730
    Signed-off-by: Jonathan Vila <jvilalop@redhat.com>

--- operator/src/test/java/org/keycloak/operator/ClusteringE2EIT.java
+++ operator/src/test/java/org/keycloak/operator/ClusteringE2EIT.java
@@ -2,20 +2,21 @@ package org.keycloak.operator;
 
 import io.fabric8.kubernetes.api.model.Service;
 import io.fabric8.kubernetes.api.model.ServiceBuilder;
-import io.quarkus.logging.Log;
 import io.quarkus.test.junit.QuarkusTest;
 import org.awaitility.Awaitility;
-import org.awaitility.core.ConditionTimeoutException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.keycloak.operator.v2alpha1.crds.Keycloak;
 
-import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.keycloak.operator.utils.K8sUtils.deployKeycloak;
+import static org.keycloak.operator.utils.K8sUtils.getDefaultKeycloakDeployment;
 
 @QuarkusTest
 public class ClusteringE2EIT extends ClusterOperatorTest {
@@ -25,65 +26,11 @@ public class ClusteringE2EIT extends ClusterOperatorTest {
     }
 
     @Test
-    public void given_ClusterAndOperatorRunning_when_KeycloakCRCreated_Then_KeycloakStructureIsDeployedAndStatusIsOK() throws IOException {
-        Log.info(((operatorDeployment == OperatorDeployment.remote) ? "Remote " : "Local ") + "Run Test :" + namespace);
-
-        deployAndCheckKeycloak();
-
-        // Check Keycloak has status ready
-        StringBuffer podlog = new StringBuffer();
-        try {
-            Log.info("Checking Keycloak pod has ready replicas == 1");
-            Awaitility.await()
-                    .atMost(Duration.ofSeconds(180))
-                    .pollDelay(Duration.ofSeconds(5))
-                    .untilAsserted(() -> {
-                        podlog.delete(0, podlog.length());
-                        try {
-                            k8sclient.pods().inNamespace(namespace).list().getItems().stream()
-                                    .filter(a -> a.getMetadata().getName().startsWith("example-kc"))
-                                    .forEach(a -> podlog.append(a.getMetadata().getName()).append(" : ")
-                                            .append(k8sclient.pods().inNamespace(namespace).withName(a.getMetadata().getName()).getLog(true)));
-                        } catch (Exception e) {
-                            // swallowing exception bc the pod is not ready to give logs yet
-                        }
-                        assertThat(k8sclient.apps().deployments().inNamespace(namespace).withName("example-kc").get().getStatus().getReadyReplicas()).isEqualTo(1);
-                    });
-        } catch (ConditionTimeoutException e) {
-            Log.error("On error POD LOG " + podlog, e);
-            throw e;
-        }
-
-
-    }
-
-    private void deployAndCheckKeycloak() {
-        // DB
-        Log.info("Creating new PostgreSQL deployment");
-        k8sclient.load(ClusteringE2EIT.class.getResourceAsStream("/example-postgres.yaml")).inNamespace(namespace).createOrReplace();
-
-        // Check DB has deployed and ready
-        Log.info("Checking Postgres is running");
-        Awaitility.await()
-                .atMost(Duration.ofSeconds(60))
-                .pollDelay(Duration.ofSeconds(2))
-                .untilAsserted(() -> assertThat(k8sclient.apps().statefulSets().inNamespace(namespace).withName("postgresql-db").get().getStatus().getReadyReplicas()).isEqualTo(1));
-        // CR
-        Log.info("Creating new Keycloak CR example");
-        k8sclient.load(ClusteringE2EIT.class.getResourceAsStream("/example-keycloak.yml")).inNamespace(namespace).createOrReplace();
-
-        // Check Operator has deployed Keycloak
-        Log.info("Checking Operator has deployed Keycloak deployment");
-        Awaitility.await()
-                .atMost(Duration.ofSeconds(60))
-                .pollDelay(Duration.ofSeconds(2))
-                .untilAsserted(() -> assertThat(k8sclient.apps().deployments().inNamespace(namespace).withName("example-kc").get()).isNotNull());
-    }
-
-    @Test
-    public void given_ClusterAndOperatorRunning_when_KeycloakCRScaled_Then_PodsAreReady() {
+    public void given_ClusterAndOperatorRunning_when_KeycloakCRScaled_Then_PodsAreReady() throws URISyntaxException {
         // given
-        deployAndCheckKeycloak();
+        var kc = getDefaultKeycloakDeployment();
+        var deploymentName = kc.getMetadata().getName();
+        deployKeycloak(k8sclient, kc, true);
 
         Keycloak keycloak = k8sclient.resources(Keycloak.class)
                 .inNamespace(namespace)
@@ -116,12 +63,14 @@ public class ClusteringE2EIT extends ClusterOperatorTest {
         k8sclient.services().inNamespace(namespace).createOrReplace(nodeportTestService);
 
         // check we can reach Keycloak
-        try {
-            URI uri = new URI(k8sclient.getConfiguration().getMasterUrl().replace("8443", "31999") + "realms/master") ;
-            given().when().relaxedHTTPSValidation().get(uri).then().statusCode(200);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        URI uri = new URI(k8sclient.getConfiguration()
+                    .getMasterUrl()
+                    .replace("8443", "31999")
+                    .replace("https", "http") + "realms/master")
+                    ;
+        Awaitility.await()
+                .atMost(10, TimeUnit.SECONDS)
+                .until(() -> given().when().relaxedHTTPSValidation().get(uri).then().extract().statusCode() == 200);
 
         // can we check infinispan ?
     }
