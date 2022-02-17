6bce8b80b99ff9a0d3a13212d1fdb813f4c84390
andreaTP <andrea.peruffo1982@gmail.com>
2022-02-17 12:43:01 +0000

    Add extensions as init container

--- operator/src/test/java/org/keycloak/operator/KeycloakDeploymentE2EIT.java
+++ operator/src/test/java/org/keycloak/operator/KeycloakDeploymentE2EIT.java
@@ -9,10 +9,13 @@ import org.junit.jupiter.api.Test;
 import org.keycloak.operator.v2alpha1.crds.Keycloak;
 
 import java.time.Duration;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.keycloak.operator.Constants.DEFAULT_LABELS;
 import static org.keycloak.operator.utils.K8sUtils.deployKeycloak;
 import static org.keycloak.operator.utils.K8sUtils.getDefaultKeycloakDeployment;
 import static org.keycloak.operator.utils.K8sUtils.waitForKeycloakToBeReady;
@@ -105,6 +108,36 @@ public class KeycloakDeploymentE2EIT extends ClusterOperatorTest {
                         var d = k8sclient.apps().deployments().withName(deploymentName).get();
                         assertThat(d.getMetadata().getLabels().entrySet().containsAll(labels.entrySet())).isTrue(); // additional labels should not be overwritten
                         assertThat(d.getSpec()).isEqualTo(origSpecs); // specs should be reconciled back to original values
+                    });
+        } catch (Exception e) {
+            savePodLogs();
+            throw e;
+        }
+    }
+
+    @Test
+    public void testExtensions() {
+        try {
+            var kc = getDefaultKeycloakDeployment();
+            kc.getSpec().setExtensions(
+                    Collections.singletonList(
+                            "https://github.com/aerogear/keycloak-metrics-spi/releases/download/2.5.3/keycloak-metrics-spi-2.5.3.jar"));
+            deployKeycloak(k8sclient, kc, true);
+
+            var kcPod = k8sclient
+                    .pods()
+                    .inNamespace(namespace)
+                    .withLabels(DEFAULT_LABELS)
+                    .list()
+                    .getItems()
+                    .get(0);
+
+            Awaitility.await()
+                    .ignoreExceptions()
+                    .untilAsserted(() -> {
+                        var logs = k8sclient.pods().inNamespace(namespace).withName(kcPod.getMetadata().getName()).getLog();
+
+                        assertTrue(logs.contains("metrics-listener (org.jboss.aerogear.keycloak.metrics.MetricsEventListenerFactory) is implementing the internal SPI"));
                     });
         } catch (Exception e) {
             savePodLogs();
