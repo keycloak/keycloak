c81da537a51a7aa0e651d5d0e61f70098cc384cd
Trey Dockendorf <tdockendorf@osc.edu>
2022-02-21 10:28:31 -0500

    Add log warning

--- services/src/main/java/org/keycloak/services/resources/admin/AuthenticationManagementResource.java
+++ services/src/main/java/org/keycloak/services/resources/admin/AuthenticationManagementResource.java
@@ -567,6 +567,7 @@ public class AuthenticationManagementResource {
                 String providerId = execution.getAuthenticator();
                 ConfigurableAuthenticatorFactory factory = CredentialHelper.getConfigurableAuthenticatorFactory(session, providerId);
                 if (factory == null) {
+                    logger.warnf("Cannot find authentication provider implementation with provider ID '%s'", providerId);
                     throw new NotFoundException("Could not find authenticator provider");
                 }
                 rep.setDisplayName(factory.getDisplayType());
