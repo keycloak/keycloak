7fa2e53bb7d2e5a236fd1135357ef380c923db1b
Pedro Igor <pigor.craveiro@gmail.com>
2022-04-07 19:01:17 -0300

    NPE when caching policies based on scopes without a resource
    Closes #11180

--- testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/admin/client/authorization/ResourceManagementTest.java
+++ testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/admin/client/authorization/ResourceManagementTest.java
@@ -24,6 +24,7 @@ import org.keycloak.admin.client.resource.ResourcesResource;
 import org.keycloak.authorization.client.util.HttpResponseException;
 import org.keycloak.representations.idm.authorization.ResourceOwnerRepresentation;
 import org.keycloak.representations.idm.authorization.ResourceRepresentation;
+import org.keycloak.representations.idm.authorization.ScopePermissionRepresentation;
 import org.keycloak.representations.idm.authorization.ScopeRepresentation;
 
 import javax.ws.rs.NotFoundException;
@@ -83,6 +84,30 @@ public class ResourceManagementTest extends AbstractAuthorizationTest {
         ResourceResource resource = getClientResource().authorization().resources().resource(newResource.getId());
 
         assertTrue(resource.permissions().isEmpty());
+    }
+
+    @Test
+    public void testQueryAssociatedPermissions() {
+        ResourceRepresentation newResource = new ResourceRepresentation();
+
+        newResource.setName("test");
+        newResource.setDisplayName("display");
+        newResource.setType("some-type");
+        newResource.addScope("GET");
+
+        newResource = doCreateResource(newResource);
+
+        ResourceResource resource = getClientResource().authorization().resources().resource(newResource.getId());
+
+        ScopePermissionRepresentation permission = new ScopePermissionRepresentation();
+
+        permission.setName(newResource.getName());
+        permission.addResource(newResource.getName());
+        permission.addScope("GET");
+
+        getClientResource().authorization().permissions().scope().create(permission);
+
+        assertFalse(resource.permissions().isEmpty());
     }
 
     @Test
