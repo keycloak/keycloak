ac68c3785a972b27c41cf18fa14109481626de65
Václav Muzikář <vmuzikar@redhat.com>
2022-04-11 10:53:55 +0200

    Revert "Build the Olm bundle and test it in CI (#10949)"
    This reverts commit 7816e69e38d314b1c38d74b06c64b53723dcfb0e.

--- operator/scripts/build-testing-docker-images.sh
+++ operator/scripts/build-testing-docker-images.sh
@@ -1,9 +1,7 @@
-#! /bin/bash
-set -euxo pipefail
+#!/usr/bin/env bash
 
 VERSION="${1:-latest}"
 KEYCLOAK_IMAGE="${2:-quay.io/keycloak/keycloak}"
-KEYCLOAK_CUSTOM_IMAGE="${3:-quay.io/keycloak/custom-keycloak}"
 
 echo "Using version: $VERSION"
 
@@ -12,4 +10,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 docker build -f ${SCRIPT_DIR}/Dockerfile-custom-image \
   --build-arg IMAGE=${KEYCLOAK_IMAGE} \
   --build-arg VERSION=${VERSION} \
-  -t ${KEYCLOAK_CUSTOM_IMAGE}:${VERSION} ${SCRIPT_DIR}
+  -t custom-keycloak:${VERSION} ${SCRIPT_DIR}
