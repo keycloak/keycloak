5f4f28c341e1a48c144d18a2d549809b189b7cd3
mposolda <mposolda@gmail.com>
2022-04-11 15:08:18 +0200

    Simple script authenticator working on Wildfly and Quarkus

--- server-spi-private/src/main/java/org/keycloak/utils/ProxyClassLoader.java
+++ server-spi-private/src/main/java/org/keycloak/utils/ProxyClassLoader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Red Hat, Inc. and/or its affiliates
+ * Copyright 2022 Red Hat, Inc. and/or its affiliates
  * and other contributors as indicated by the @author tags.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,13 +13,18 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 
-package org.keycloak.connections.jpa.entityprovider;
+package org.keycloak.utils;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashSet;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 /**
@@ -29,7 +34,8 @@ import java.util.Set;
  * Effectively it forms a proxy to one or more other classloaders.
  * 
  * The way it works:
- * - Get all (unique) classloaders from all provided classes
+ * - Get list of classloaders, which will be used as "delegates" when loaded classes or resources.
+ *   - Can be retrived from provided classloaders or alternatively from the provided classes where the "delegate classloaders" will be determined from the classloaders of given classes
  * - For each class or resource that is 'requested':
  *   - First try all provided classloaders and if we have a match, return that
  *   - If no match was found: proceed with 'normal' classloading in 'current classpath' scope
@@ -41,17 +47,28 @@ public class ProxyClassLoader extends ClassLoader {
 
     private Set<ClassLoader> classloaders;
 
-    public ProxyClassLoader(Collection<Class<?>> classes, ClassLoader parentClassLoader) {
-    	super(parentClassLoader);
-    	init(classes);
+    /**
+     * Init classloader with the list of given delegates
+     * @param delegateClassLoaders
+     */
+    public ProxyClassLoader(ClassLoader... delegateClassLoaders) {
+        if (delegateClassLoaders == null || delegateClassLoaders.length == 0) {
+            throw new IllegalStateException("At least one classloader to delegate must be provided");
+        }
+        classloaders = new LinkedHashSet<>();
+        classloaders.addAll(Arrays.asList(delegateClassLoaders));
     }
-    
+
+    /**
+     * Get all unique classloaders from the provided classes to be used as "Delegate classloaders"
+     * @param classes
+     */
     public ProxyClassLoader(Collection<Class<?>> classes) {
     	init(classes);
     }
 
     private void init(Collection<Class<?>> classes) {
-        classloaders = new HashSet<>();
+        classloaders = new LinkedHashSet<>();
         for (Class<?> clazz : classes) {
             classloaders.add(clazz.getClassLoader());
         }
@@ -84,4 +101,33 @@ public class ProxyClassLoader extends ClassLoader {
         return super.getResource(name);
     }
 
+    @Override
+    public Enumeration<URL> getResources(String name) throws IOException {
+        final LinkedHashSet<URL> resourceUrls = new LinkedHashSet();
+
+        for (ClassLoader classloader : classloaders) {
+            Enumeration<URL> child = classloader.getResources(name);
+
+            while (child.hasMoreElements()) {
+                resourceUrls.add(child.nextElement());
+            }
+        }
+
+        return new Enumeration<URL>() {
+            final Iterator<URL> resourceUrlIterator = resourceUrls.iterator();
+
+            public boolean hasMoreElements() {
+                return this.resourceUrlIterator.hasNext();
+            }
+
+            public URL nextElement() {
+                return (URL)this.resourceUrlIterator.next();
+            }
+        };
+    }
+
+    @Override
+    public String toString() {
+        return "ProxyClassLoader: Delegates: " + classloaders;
+    }
 }
