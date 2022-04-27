a479066bf5856b40144997dc640b606b2dd9dd92
Pedro Igor <pigor.craveiro@gmail.com>
2022-04-27 11:33:29 -0300

    Loading message bundles using the flat-classpath theme provider
    Closes #11186

--- quarkus/runtime/src/main/java/org/keycloak/quarkus/runtime/themes/FlatClasspathThemeResourceProviderFactory.java
+++ quarkus/runtime/src/main/java/org/keycloak/quarkus/runtime/themes/FlatClasspathThemeResourceProviderFactory.java
@@ -21,6 +21,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.Enumeration;
+import java.util.Locale;
+import java.util.Properties;
 import org.keycloak.theme.ClasspathThemeResourceProviderFactory;
 
 public class FlatClasspathThemeResourceProviderFactory extends ClasspathThemeResourceProviderFactory {
@@ -40,6 +42,18 @@ public class FlatClasspathThemeResourceProviderFactory extends ClasspathThemeRes
         }
 
         return null;
+    }
+
+    @Override
+    public Properties getMessages(String baseBundlename, Locale locale) throws IOException {
+        Properties messages = new Properties();
+        Enumeration<URL> resources = classLoader.getResources(THEME_RESOURCES_MESSAGES + baseBundlename + "_" + locale.toString() + ".properties");
+
+        while (resources.hasMoreElements()) {
+            loadMessages(messages, resources.nextElement());
+        }
+
+        return messages;
     }
 
     @Override
