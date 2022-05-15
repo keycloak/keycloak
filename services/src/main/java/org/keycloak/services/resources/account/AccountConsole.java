24c99ac1cd3cc1711f63c4c6e76d30bd19301822
Steve Lewis <stelewis@redhat.com>
2022-05-14 23:23:19 -0500

    Use JsonSerialization utility instead of javax.json library.
    Closes #11554

--- services/src/main/java/org/keycloak/services/resources/account/AccountConsole.java
+++ services/src/main/java/org/keycloak/services/resources/account/AccountConsole.java
@@ -12,8 +12,6 @@ import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import javax.json.Json;
-import javax.json.JsonObjectBuilder;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
@@ -47,6 +45,7 @@ import org.keycloak.theme.FreeMarkerUtil;
 import org.keycloak.theme.Theme;
 import org.keycloak.theme.beans.MessageFormatterMethod;
 import org.keycloak.urls.UrlType;
+import org.keycloak.util.JsonSerialization;
 import org.keycloak.utils.MediaType;
 
 /**
@@ -162,13 +161,12 @@ public class AccountConsole {
     
     private String messagesToJsonString(Properties props) {
         if (props == null) return "";
-        
-        JsonObjectBuilder json = Json.createObjectBuilder();
-        for (String prop : props.stringPropertyNames()) {
-            json.add(prop, convertPropValue(props.getProperty(prop)));
+
+        try {
+            return JsonSerialization.writeValueAsString(props);
+        } catch (IOException e) {
+            throw new RuntimeException("could not write value", e);
         }
-        
-        return json.build().toString();
     }
     
     private String convertPropValue(String propertyValue) {
