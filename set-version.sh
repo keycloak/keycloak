850fd35c92d65bdb7d64cfe0dcba778d4b362ffd
Jon Koops <jonkoops@gmail.com>
2022-08-01 18:37:10 +0200

    Add development version for Keycloak JS

--- set-version.sh
+++ set-version.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -e
 
+DIR="$PWD"
 NEW_VERSION=$1
 
 mvn versions:set -DnewVersion=$NEW_VERSION -DgenerateBackupPoms=false -DgroupId=org.keycloak* -DartifactId=*
 
 sed -i "s/ENV KEYCLOAK_VERSION .*/ENV KEYCLOAK_VERSION $NEW_VERSION/" quarkus/container/Dockerfile
+
+cd adapters/oidc/js
+npm version $NEW_VERSION
+cd $DIR
