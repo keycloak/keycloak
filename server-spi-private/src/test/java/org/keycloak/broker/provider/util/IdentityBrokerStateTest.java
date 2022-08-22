ae4b7c24c2d4568b5743218d6c780780f8e55f7b
eva <eva@kvalitetsit.dk>
2022-08-22 09:19:01 +0200

    Implementing tests without mockito

--- server-spi-private/src/test/java/org/keycloak/broker/provider/util/IdentityBrokerStateTest.java
+++ server-spi-private/src/test/java/org/keycloak/broker/provider/util/IdentityBrokerStateTest.java
@@ -1,14 +1,11 @@
 package org.keycloak.broker.provider.util;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
-import org.keycloak.models.ClientModel;
-import org.keycloak.models.RealmModel;
-import org.mockito.Mockito;
+import org.keycloak.models.*;
+
 
 public class IdentityBrokerStateTest {
-
 
     @Test
     public void testDecodedWithClientIdNotUuid() {
@@ -52,25 +49,26 @@ public class IdentityBrokerStateTest {
     public void testEncodedWithClientIdUUid() {
         // Given
         String encoded = "gNrGamIDGKpKSI9yOrcFzYTKoFGH779_WNCacAelkhk.vpISZLVDAc0.7UlEjBTPRx6oOgY9DcO8jA";
-        ClientModel clientModel = Mockito.mock(ClientModel.class);
-        String myClientId = "my-client-id";
-        Mockito.when(clientModel.getClientId()).thenReturn(myClientId);
-        RealmModel realmModel = Mockito.mock(RealmModel.class);
-        Mockito.when(realmModel.getClientById(Mockito.eq("ed49448c-14cf-471e-a83a-063d0dc3bc8c"))).thenReturn(clientModel);
+        String clientId = "ed49448c-14cf-471e-a83a-063d0dc3bc8c";
+        String clientClientId = "my-client-id";
+        ClientModel clientModel = new IdentityBrokerStateTestHelpers.TestClientModel(clientId, clientClientId);
+        RealmModel realmModel = new IdentityBrokerStateTestHelpers.TestRealmModel(clientId, clientClientId, clientModel);
 
         // When
         IdentityBrokerState decodedState = IdentityBrokerState.encoded(encoded, realmModel);
 
         // Then
         Assert.assertNotNull(decodedState);
-        Assert.assertEquals(myClientId, decodedState.getClientId());
+        Assert.assertEquals(clientClientId, decodedState.getClientId());
     }
 
     @Test
     public void testEncodedWithClientIdNotUUid() {
         // Given
         String encoded = "gNrGamIDGKpKSI9yOrcFzYTKoFGH779_WNCacAelkhk.vpISZLVDAc0.http://i.am.an.url";
-        RealmModel realmModel = Mockito.mock(RealmModel.class);
+        String clientId = "http://i.am.an.url";
+        ClientModel clientModel = new IdentityBrokerStateTestHelpers.TestClientModel(clientId, clientId);
+        RealmModel realmModel = new IdentityBrokerStateTestHelpers.TestRealmModel(clientId, clientId, clientModel);
 
         // When
         IdentityBrokerState decodedState = IdentityBrokerState.encoded(encoded, realmModel);
@@ -78,7 +76,6 @@ public class IdentityBrokerStateTest {
         // Then
         Assert.assertNotNull(decodedState);
         Assert.assertEquals("http://i.am.an.url", decodedState.getClientId());
-//        Mockito.verify(realmModel).getClientById(tim)
     }
 
 }
