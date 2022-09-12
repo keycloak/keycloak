243cec90df189507c4dfaa1993c0a3046062cacd
Jon Koops <jonkoops@gmail.com>
2022-09-12 11:52:11 +0200

    Add Keycloak JS to the project (#3189)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,15 @@
 /** @type {import("eslint").Linter.Config } */
 module.exports = {
   root: true,
-  ignorePatterns: ["node_modules", "dist", "keycloak-theme", "server"],
+  ignorePatterns: [
+    "node_modules",
+    "dist",
+    "keycloak-theme",
+    "server",
+    // Keycloak JS follows a completely different and outdated style, so we'll exclude it for now.
+    // TODO: Eventually align the code-style for Keycloak JS.
+    "libs/keycloak-js",
+  ],
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: "./tsconfig.eslint.json",
