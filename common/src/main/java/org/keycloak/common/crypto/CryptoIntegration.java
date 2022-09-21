c84fd9094fcfb0b68c08a93481e369f8a3eb056a
David Anderson <david.anderson@redhat.com>
2022-09-21 12:02:33 -0500

    adding elytron provider

--- common/src/main/java/org/keycloak/common/crypto/CryptoIntegration.java
+++ common/src/main/java/org/keycloak/common/crypto/CryptoIntegration.java
@@ -25,7 +25,7 @@ public class CryptoIntegration {
             synchronized (lock) {
                 if (cryptoProvider == null) {
                     cryptoProvider = detectProvider(classLoader);
-                    logger.infov("BouncyCastle provider: {0}", BouncyIntegration.PROVIDER);
+                    logger.debugv("java security provider: {0}", BouncyIntegration.PROVIDER);
 
                     if (logger.isTraceEnabled()) {
                         logger.tracef(dumpJavaSecurityProviders());
@@ -54,7 +54,7 @@ public class CryptoIntegration {
             throw new IllegalStateException("Multiple crypto providers loaded with the classLoader: " + classLoader +
                     ". Make sure only one cryptoProvider available on the classpath. Available providers: " +foundProviders);
         } else {
-            logger.infof("Detected crypto provider: %s", foundProviders.get(0).getClass().getName());
+            logger.debugf("Detected crypto provider: %s", foundProviders.get(0).getClass().getName());
             return foundProviders.get(0);
         }
     }
