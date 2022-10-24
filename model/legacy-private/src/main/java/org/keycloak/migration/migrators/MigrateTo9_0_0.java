45a7418510823566544fc123bbb03a8415fd6b0f
Paulo Henrique Alves <paulohenriqueas13@gmail.com>
2022-10-24 17:28:10 -0300

    Recreate client 'account' if not exists during migration. #15020

--- model/legacy-private/src/main/java/org/keycloak/migration/migrators/MigrateTo9_0_0.java
+++ model/legacy-private/src/main/java/org/keycloak/migration/migrators/MigrateTo9_0_0.java
@@ -56,6 +56,7 @@ public class MigrateTo9_0_0 implements Migration {
     }
 
     protected void migrateRealmCommon(RealmModel realm) {
+        addAccountClient(realm);
         addAccountConsoleClient(realm);
         addAccountApiRoles(realm);
         enablePkceAdminAccountClients(realm);
@@ -65,21 +66,17 @@ public class MigrateTo9_0_0 implements Migration {
     private void addAccountApiRoles(RealmModel realm) {
         LOG.debugf("Searching for client '%s' in realm '%s'", Constants.ACCOUNT_MANAGEMENT_CLIENT_ID, realm.getId());
         ClientModel accountClient = realm.getClientByClientId(Constants.ACCOUNT_MANAGEMENT_CLIENT_ID);
-        if(accountClient != null) {
-            RoleModel viewAppRole = accountClient.addRole(AccountRoles.VIEW_APPLICATIONS);
-            viewAppRole.setDescription("${role_" + AccountRoles.VIEW_APPLICATIONS + "}");
-            LOG.debugf("Added the role %s to the '%s' client.", AccountRoles.VIEW_APPLICATIONS, Constants.ACCOUNT_MANAGEMENT_CLIENT_ID);
-            RoleModel viewConsentRole = accountClient.addRole(AccountRoles.VIEW_CONSENT);
-            viewConsentRole.setDescription("${role_" + AccountRoles.VIEW_CONSENT + "}");
-            LOG.debugf("Added the role %s to the '%s' client.", AccountRoles.VIEW_CONSENT, Constants.ACCOUNT_MANAGEMENT_CLIENT_ID);
-            RoleModel manageConsentRole = accountClient.addRole(AccountRoles.MANAGE_CONSENT);
-            manageConsentRole.setDescription("${role_" + AccountRoles.MANAGE_CONSENT + "}");
-            LOG.debugf("Added the role %s to the '%s' client.", AccountRoles.MANAGE_CONSENT, Constants.ACCOUNT_MANAGEMENT_CLIENT_ID);
-            manageConsentRole.addCompositeRole(viewConsentRole);
-            LOG.debugf("Added the %s role as a composite role to %s", AccountRoles.VIEW_CONSENT, AccountRoles.MANAGE_CONSENT);
-        } else {
-            LOG.warnf("Client '%s' do not exists at realm '%s'.", Constants.ACCOUNT_MANAGEMENT_CLIENT_ID, realm.getId());
-        }
+        RoleModel viewAppRole = accountClient.addRole(AccountRoles.VIEW_APPLICATIONS);
+        viewAppRole.setDescription("${role_" + AccountRoles.VIEW_APPLICATIONS + "}");
+        LOG.debugf("Added the role %s to the '%s' client.", AccountRoles.VIEW_APPLICATIONS, Constants.ACCOUNT_MANAGEMENT_CLIENT_ID);
+        RoleModel viewConsentRole = accountClient.addRole(AccountRoles.VIEW_CONSENT);
+        viewConsentRole.setDescription("${role_" + AccountRoles.VIEW_CONSENT + "}");
+        LOG.debugf("Added the role %s to the '%s' client.", AccountRoles.VIEW_CONSENT, Constants.ACCOUNT_MANAGEMENT_CLIENT_ID);
+        RoleModel manageConsentRole = accountClient.addRole(AccountRoles.MANAGE_CONSENT);
+        manageConsentRole.setDescription("${role_" + AccountRoles.MANAGE_CONSENT + "}");
+        LOG.debugf("Added the role %s to the '%s' client.", AccountRoles.MANAGE_CONSENT, Constants.ACCOUNT_MANAGEMENT_CLIENT_ID);
+        manageConsentRole.addCompositeRole(viewConsentRole);
+        LOG.debugf("Added the %s role as a composite role to %s", AccountRoles.VIEW_CONSENT, AccountRoles.MANAGE_CONSENT);
     }
 
     protected void addAccountConsoleClient(RealmModel realm) {
@@ -98,11 +95,9 @@ public class MigrateTo9_0_0 implements Migration {
             client.setProtocol("openid-connect");
 
             ClientModel accountClient = realm.getClientByClientId(Constants.ACCOUNT_MANAGEMENT_CLIENT_ID);
-            if (accountClient != null) {
-                RoleModel role = accountClient.getRole(AccountRoles.MANAGE_ACCOUNT);
-                if (role != null) {
-                    client.addScopeMapping(role);
-                }
+            RoleModel role = accountClient.getRole(AccountRoles.MANAGE_ACCOUNT);
+            if (role != null) {
+                client.addScopeMapping(role);
             }
 
             ProtocolMapperModel audienceMapper = new ProtocolMapperModel();
@@ -123,6 +118,30 @@ public class MigrateTo9_0_0 implements Migration {
         ClientModel accountConsole = realm.getClientByClientId(Constants.ACCOUNT_CONSOLE_CLIENT_ID);
         if (accountConsole != null) {
             accountConsole.setAttribute("pkce.code.challenge.method", "S256");
+        }
+    }
+
+    protected void addAccountClient(RealmModel realm) {
+        if (realm.getClientByClientId(Constants.ACCOUNT_MANAGEMENT_CLIENT_ID) == null) {
+            ClientModel client = KeycloakModelUtils.createPublicClient(realm, Constants.ACCOUNT_MANAGEMENT_CLIENT_ID);
+            client.setName("${client_" + Constants.ACCOUNT_MANAGEMENT_CLIENT_ID + "}");
+            client.setEnabled(true);
+            client.setFullScopeAllowed(false);
+            client.setDirectAccessGrantsEnabled(false);
+
+            client.setRootUrl(Constants.AUTH_BASE_URL_PROP);
+            String baseUrl = "/realms/" + realm.getName() + "/account/";
+            client.setBaseUrl(baseUrl);
+            client.addRedirectUri(baseUrl + "*");
+
+            client.setProtocol("openid-connect");
+
+            ProtocolMapperModel audienceMapper = new ProtocolMapperModel();
+            audienceMapper.setName("audience resolve");
+            audienceMapper.setProtocol("openid-connect");
+            audienceMapper.setProtocolMapper("oidc-audience-resolve-mapper");
+
+            client.addProtocolMapper(audienceMapper);
         }
     }
 
