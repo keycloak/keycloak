49acf404d315c872fd6cfc10b0c617589d915e6f
Jon Koops <jonkoops@gmail.com>
2022-10-25 11:45:28 +0200

    Set Keycloak JS version number in `set-version.sh`

--- set-version.sh
+++ set-version.sh
@@ -2,6 +2,13 @@
 
 NEW_VERSION=$1
 
+# Maven
 mvn versions:set -DnewVersion=$NEW_VERSION -DgenerateBackupPoms=false -DgroupId=org.keycloak* -DartifactId=*
 
+# Docker
 sed -i "s/ENV KEYCLOAK_VERSION .*/ENV KEYCLOAK_VERSION $NEW_VERSION/" quarkus/container/Dockerfile
+
+# NPM
+cd adapters/oidc/js
+npm version --no-git-tag-version $NEW_VERSION
+cd $OLDPWD
