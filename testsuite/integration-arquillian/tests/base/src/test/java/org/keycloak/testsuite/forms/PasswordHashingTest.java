3e1b275bab1889f8e964dda1d0323b931c3c9c55
Marek Posolda <mposolda@gmail.com>
2022-11-06 14:49:50 +0100

    Short passwords with PBKDF2 mode working (#14437)
    * Short passwords with PBKDF2 mode working
    Closes #14314
    
    * Add config option to Pbkdf2 provider to control max padding
    
    * Update according to PR review - more testing for padding and for non-fips mode

--- testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/forms/PasswordHashingTest.java
+++ testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/forms/PasswordHashingTest.java
@@ -21,6 +21,7 @@ import org.junit.Test;
 import org.keycloak.common.Profile;
 import org.keycloak.common.util.Base64;
 import org.keycloak.credential.CredentialModel;
+import org.keycloak.credential.hash.PasswordHashProvider;
 import org.keycloak.credential.hash.Pbkdf2PasswordHashProvider;
 import org.keycloak.credential.hash.Pbkdf2PasswordHashProviderFactory;
 import org.keycloak.credential.hash.Pbkdf2Sha256PasswordHashProviderFactory;
@@ -46,6 +47,7 @@ import java.security.spec.KeySpec;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.fail;
 
 /**
@@ -168,6 +170,7 @@ public class PasswordHashingTest extends AbstractTestRealmKeycloakTest {
         Pbkdf2PasswordHashProvider specificKeySizeHashProvider = new Pbkdf2PasswordHashProvider(Pbkdf2Sha512PasswordHashProviderFactory.ID,
                 Pbkdf2Sha512PasswordHashProviderFactory.PBKDF2_ALGORITHM,
                 Pbkdf2Sha512PasswordHashProviderFactory.DEFAULT_ITERATIONS,
+                0,
                 256);
         String encodedPassword = specificKeySizeHashProvider.encode(password, -1);
 
@@ -224,6 +227,32 @@ public class PasswordHashingTest extends AbstractTestRealmKeycloakTest {
         assertEncoded(credential, "password", credential.getPasswordSecretData().getSalt(), "PBKDF2WithHmacSHA512", 30000);
     }
 
+    @Test
+    public void testPbkdf2Sha256WithPadding() throws Exception {
+        setPasswordPolicy("hashAlgorithm(" + Pbkdf2Sha256PasswordHashProviderFactory.ID + ")");
+
+        int originalPaddingLength = configurePaddingForKeycloak(14);
+        try {
+            // Assert password created with padding enabled can be verified
+            String username1 = "test1-Pbkdf2Sha2562";
+            createUser(username1);
+
+            PasswordCredentialModel credential = PasswordCredentialModel.createFromCredentialModel(fetchCredentials(username1));
+            assertEncoded(credential, "password", credential.getPasswordSecretData().getSalt(), "PBKDF2WithHmacSHA256", 27500);
+
+            // Now configure padding to bigger than 64. The verification without padding would fail as for longer padding than 64 characters, the hashes of the padded password and unpadded password would be different
+            configurePaddingForKeycloak(65);
+            String username2 = "test2-Pbkdf2Sha2562";
+            createUser(username2);
+
+            credential = PasswordCredentialModel.createFromCredentialModel(fetchCredentials(username2));
+            assertEncoded(credential, "password", credential.getPasswordSecretData().getSalt(), "PBKDF2WithHmacSHA256", 27500, false);
+
+        } finally {
+            configurePaddingForKeycloak(originalPaddingLength);
+        }
+    }
+
 
     private void createUser(String username) {
         ApiUtil.createUserAndResetPasswordWithAdminClient(adminClient.realm("test"), UserBuilder.create().username(username).build(), "password");
@@ -245,9 +274,26 @@ public class PasswordHashingTest extends AbstractTestRealmKeycloakTest {
     }
 
     private void assertEncoded(PasswordCredentialModel credential, String password, byte[] salt, String algorithm, int iterations) throws Exception {
+        assertEncoded(credential, password, salt, algorithm, iterations, true);
+    }
+
+    private void assertEncoded(PasswordCredentialModel credential, String password, byte[] salt, String algorithm, int iterations, boolean expectedSuccess) throws Exception {
         KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, iterations, 512);
         byte[] key = SecretKeyFactory.getInstance(algorithm).generateSecret(spec).getEncoded();
-        assertEquals(Base64.encodeBytes(key), credential.getPasswordSecretData().getValue());
+        if (expectedSuccess) {
+            assertEquals(Base64.encodeBytes(key), credential.getPasswordSecretData().getValue());
+        } else {
+            assertNotEquals(Base64.encodeBytes(key), credential.getPasswordSecretData().getValue());
+        }
+    }
+
+    private int configurePaddingForKeycloak(int paddingLength) {
+        return testingClient.server("test").fetch(session -> {
+            Pbkdf2Sha256PasswordHashProviderFactory factory = (Pbkdf2Sha256PasswordHashProviderFactory) session.getKeycloakSessionFactory().getProviderFactory(PasswordHashProvider.class, Pbkdf2Sha256PasswordHashProviderFactory.ID);
+            int origPaddingLength = factory.getMaxPaddingLength();
+            factory.setMaxPaddingLength(paddingLength);
+            return origPaddingLength;
+        }, Integer.class);
     }
 
 }
