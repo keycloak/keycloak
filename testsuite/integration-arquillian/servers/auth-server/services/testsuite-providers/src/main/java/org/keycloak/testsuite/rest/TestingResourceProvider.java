669086acd618cce2ef61403249f1105b04154992
Stian Thorgersen <stianst@gmail.com>
2022-12-21 15:12:57 +0100

    Suspend scheduled tasks if time offset is set to a large value (#16144)

--- testsuite/integration-arquillian/servers/auth-server/services/testsuite-providers/src/main/java/org/keycloak/testsuite/rest/TestingResourceProvider.java
+++ testsuite/integration-arquillian/servers/auth-server/services/testsuite-providers/src/main/java/org/keycloak/testsuite/rest/TestingResourceProvider.java
@@ -237,11 +237,17 @@ public class TestingResourceProvider implements RealmResourceProvider {
     @Produces(MediaType.APPLICATION_JSON)
     public Map<String, String> setTimeOffset(Map<String, String> time) {
         int offset = Integer.parseInt(time.get("offset"));
+
+        if (offset > 60) {
+            suspendTask(ClearExpiredUserSessions.TASK_NAME);
+        }
+
         Time.setOffset(offset);
 
         // Time offset was restarted
         if (offset == 0) {
             session.getKeycloakSessionFactory().publish(new ResetTimeOffsetEvent());
+            restorePeriodicTasks();
         }
 
         return getTimeOffset();
