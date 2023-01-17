e1ff155752c7d617a6f3c92df2653be448294b33
Alexander Schwartz <aschwart@redhat.com>
2023-01-17 19:43:55 +0100

    Creating the downstream master adoc now complete
    Closes: #16475

---
+++ docs/antora/ccutil.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -ex
+rm -rf build/ccutil
+mkdir -p build/ccutil
+cp -r build/assembler/guides-server build/ccutil
+cp -r ccutil/guides-server/* build/ccutil/guides-server
+
+sed -i '2 a\
+Keycloak Project' build/ccutil/guides-server/20.0/guides-keycloak-server.adoc
+
+# sed -i 's|xref:{page-version}@guides-server::\([^.]*\)|https://ahus1.github.io/keycloak-antora/guides-server/latest/\1|g' guides-operator/guides-operator/20.0/guides-keycloak-operator.adoc
+
+IMAGENAME='quay.io/ivanhorvath/ccutil:amazing'
+
+if which podman &>/dev/null;
+then
+    CRT=$(which podman)
+else
+    CRT=$(which docker)
+fi
+
+if ! ${CRT} inspect ccutil &>/dev/null;
+then
+    ${CRT} run --privileged --ulimit host -d -v $(pwd):/docs:rw --name ccutil ${IMAGENAME}
+fi
+
+${CRT} exec -w /docs/build/ccutil/guides-server ccutil ccutil compile --format html --lang en-US
+${CRT} exec -w /docs/build/ccutil/guides-server ccutil ccutil compile --format html-single --lang en-US
+
+rm -rf _site/guides-server-single
+ln -s ../build/ccutil/guides-server/build/tmp/en-US/html-single _site/guides-server-single
+
+rm -rf _site/guides-server-multi
+ln -s ../build/ccutil/guides-server/build/tmp/en-US/html _site/guides-server-multi
+
---
+++ docs/antora/lib/index.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const convertDocumentToPdf = require('./convert-document-to-pdf')
+const { assembleContent } = (() => {
+  try {
+    return require('@antora/assembler')
+  } catch {
+    return require('../../assembler')
+  }
+})()
+
+module.exports.register = function () {
+  //this.on('beforeProcess', ({ asciidocConfig }) => {
+  //  asciidocConfig.keepSource = true
+  //})
+  this.on('contentClassified', ({ contentCatalog }) => {
+    contentCatalog.getPages((page) => {
+      if (!page.out) return
+      page.src.contents = page.contents
+      page.src = new Proxy(page.src, { deleteProperty: (o, p) => (p === 'contents' ? true : delete o[p]) })
+    })
+  })
+  this.on('beforePublish', ({ playbook, contentCatalog, siteCatalog }) =>
+    assembleContent.call(this, playbook, contentCatalog, convertDocumentToPdf, { siteCatalog })
+  )
+}
