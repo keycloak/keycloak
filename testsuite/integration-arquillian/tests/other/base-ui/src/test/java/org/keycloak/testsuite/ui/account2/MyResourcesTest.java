8fc94266ba18b91df25bd6670eb5f23a8dc77e7e
Martin Bartoš <mabartos@redhat.com>
2023-02-02 14:07:12 +0100

    Fix account console tests

--- testsuite/integration-arquillian/tests/other/base-ui/src/test/java/org/keycloak/testsuite/ui/account2/MyResourcesTest.java
+++ testsuite/integration-arquillian/tests/other/base-ui/src/test/java/org/keycloak/testsuite/ui/account2/MyResourcesTest.java
@@ -28,7 +28,7 @@ import org.keycloak.testsuite.util.UserBuilder;
 import org.keycloak.util.JsonSerialization;
 import org.openqa.selenium.NoSuchElementException;
 
-import javax.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
