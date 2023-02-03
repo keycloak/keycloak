989d35fe0efc342d9cbf0fed252577859fb9564a
Raphael Suter <raphi.suter@gmail.com>
2023-02-03 12:26:35 +0100

    Added `stringify` in `MultiValuedStringComponent` (#4306)

--- apps/admin-ui/src/components/dynamic/MultivaluedStringComponent.tsx
+++ apps/admin-ui/src/components/dynamic/MultivaluedStringComponent.tsx
@@ -32,6 +32,7 @@ export const MultiValuedStringComponent = ({
         addButtonLabel={t("addMultivaluedLabel", {
           fieldLabel: t(label!).toLowerCase(),
         })}
+        stringify
       />
     </FormGroup>
   );
