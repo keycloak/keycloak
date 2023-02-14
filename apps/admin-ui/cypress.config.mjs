a517cc70db820b73c571055a7b61e447a019f8b2
Jon Koops <jonkoops@gmail.com>
2023-02-14 06:56:04 +0100

    Remove WebPack pre-proccessor from Cypress config (#4389)

--- apps/admin-ui/cypress.config.mjs
+++ apps/admin-ui/cypress.config.mjs
@@ -1,9 +1,4 @@
-import webpackPreprocessor from "@cypress/webpack-batteries-included-preprocessor";
 import { defineConfig } from "cypress";
-import { deleteAsync } from "del";
-import { createRequire } from "node:module";
-
-const require = createRequire(import.meta.url);
 
 export default defineConfig({
   projectId: "j4yhox",
@@ -23,30 +18,6 @@ export default defineConfig({
   },
 
   e2e: {
-    setupNodeEvents(on) {
-      on(
-        "file:preprocessor",
-        webpackPreprocessor({
-          typescript: require.resolve("typescript"),
-        })
-      );
-
-      on("after:spec", async (spec, results) => {
-        if (!results.video) {
-          return;
-        }
-
-        // Do we have failures for any retry attempts?
-        const failures = results.tests.some(({ attempts }) =>
-          attempts.some(({ state }) => state === "failed")
-        );
-
-        // Delete the video if the spec passed and no tests were retried.
-        if (!failures) {
-          await deleteAsync(results.video);
-        }
-      });
-    },
     baseUrl: "http://localhost:8080",
     slowTestThreshold: 30000,
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
