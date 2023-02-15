a0089ae47f1852cabed4e850b3e6e88046881a59
Jon Koops <jonkoops@gmail.com>
2023-02-15 14:45:17 +0100

    Bump NPM versions in `set-version.sh`

--- set-version.sh
+++ set-version.sh
@@ -2,6 +2,13 @@
 
 NEW_VERSION=$1
 
+# Maven
 mvn versions:set -DnewVersion=$NEW_VERSION -DgenerateBackupPoms=false -DgroupId=org.keycloak* -DartifactId=*
 
+# Docker
 sed -i "s/ENV KEYCLOAK_VERSION .*/ENV KEYCLOAK_VERSION $NEW_VERSION/" quarkus/container/Dockerfile
+
+# NPM
+cd js
+npm version $NEW_VERSION --allow-same-version --no-git-tag-version --workspaces
+cd -
