174b3af6fc8d2cea36b5bd9c3c58cc426f935e73
Erik Jan de Wit <erikjan.dewit@gmail.com>
2023-02-16 15:49:39 +0100

    Always use pagination for role mapping modal (#4414)

--- apps/admin-ui/src/components/role-mapping/AddRoleMappingModal.tsx
+++ apps/admin-ui/src/components/role-mapping/AddRoleMappingModal.tsx
@@ -138,7 +138,7 @@ export const AddRoleMappingModal = ({
         key={key}
         onSelect={(rows) => setSelectedRows([...rows])}
         searchPlaceholderKey="clients:searchByRoleName"
-        isPaginated={filterType === "clients"}
+        isPaginated
         searchTypeComponent={
           <ToolbarItem>
             <Dropdown
