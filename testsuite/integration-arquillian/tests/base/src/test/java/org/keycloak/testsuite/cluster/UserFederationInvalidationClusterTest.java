517f00b4da2d87bd52fdc18bbeda862f303fe9dc
Peter Zaoral <pzaoral@redhat.com>
2023-02-17 11:08:49 +0100

    Quarkus3 branch sync no. 6
    17.2.2023:
    * renamed imports from javax to jakarta as a part of the migration from JavaEE to JakartaEE
    
    Signed-off-by: Peter Zaoral <pzaoral@redhat.com>

--- testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/cluster/UserFederationInvalidationClusterTest.java
+++ testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/cluster/UserFederationInvalidationClusterTest.java
@@ -10,8 +10,8 @@ import org.keycloak.testsuite.admin.ApiUtil;
 import org.keycloak.testsuite.arquillian.ContainerInfo;
 import org.keycloak.testsuite.federation.DummyUserFederationProviderFactory;
 
-import javax.ws.rs.NotFoundException;
-import javax.ws.rs.core.Response;
+import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.core.Response;
 
 import static org.junit.Assert.assertNull;
 
