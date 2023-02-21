d10c7593507cd657fb9e7357c4d64c8651c4fb97
Martin Bartoš <mabartos@redhat.com>
2023-02-21 11:21:12 +0100

    Fix issues with JakartaEE Mail dependencies
    This reverts commit da4644844ed88818c05d777460624403326ab01c

--- testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/forms/BruteForceTest.java
+++ testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/forms/BruteForceTest.java
@@ -49,7 +49,7 @@ import org.keycloak.testsuite.util.OAuthClient;
 import org.keycloak.testsuite.util.RealmRepUtil;
 import org.keycloak.testsuite.util.UserBuilder;
 
-import javax.mail.internet.MimeMessage;
+import jakarta.mail.internet.MimeMessage;
 import java.net.MalformedURLException;
 import java.util.Calendar;
 import java.util.Collections;
