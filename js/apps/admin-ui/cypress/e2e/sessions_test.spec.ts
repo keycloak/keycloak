2ed1a9b72d403944184844e9271ba4bc0849663b
agagancarczyk <agagancarczyk@gmail.com>
2023-03-30 09:44:36 +0100

    Add more a11y tests (#19379)

--- js/apps/admin-ui/cypress/e2e/sessions_test.spec.ts
+++ js/apps/admin-ui/cypress/e2e/sessions_test.spec.ts
@@ -77,4 +77,31 @@ describe("Sessions test", () => {
         );
     });
   });
+
+  describe("Accessibility tests for sessions", () => {
+    beforeEach(() => {
+      loginPage.logIn();
+      keycloakBefore();
+      sidebarPage.goToSessions();
+      cy.injectAxe();
+    });
+
+    it("Check a11y violations on load/ sessions", () => {
+      cy.checkA11y();
+    });
+
+    it("Check a11y violations on revocation dialog", () => {
+      cy.findByTestId("action-dropdown").click();
+      cy.findByTestId("revocation").click();
+      cy.checkA11y();
+      cy.findByTestId("cancel").click();
+    });
+
+    it("Check a11y violations on sign out all active sessions dialog", () => {
+      cy.findByTestId("action-dropdown").click();
+      cy.findByTestId("logout-all").click();
+      cy.checkA11y();
+      cy.findByTestId("cancel").click();
+    });
+  });
 });
