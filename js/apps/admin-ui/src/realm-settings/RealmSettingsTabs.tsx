6dbc6e6c72c577dac2c381a5c6db793bb0b3c54a
Stan Silvert <ssilvert@redhat.com>
2023-04-05 15:15:18 -0400

    Hide tab when client policies are disabled. Fixes #19460

--- js/apps/admin-ui/src/realm-settings/RealmSettingsTabs.tsx
+++ js/apps/admin-ui/src/realm-settings/RealmSettingsTabs.tsx
@@ -48,6 +48,7 @@ import { RealmSettingsThemesTab } from "./ThemesTab";
 import { RealmSettingsTokensTab } from "./TokensTab";
 import { UserProfileTab } from "./user-profile/UserProfileTab";
 import { UserRegistration } from "./UserRegistration";
+import { useServerInfo } from "../context/server-info/ServerInfoProvider";
 
 type RealmSettingsHeaderProps = {
   onChange: (value: boolean) => void;
@@ -350,50 +351,61 @@ export const RealmSettingsTabs = ({
           >
             <RealmSettingsTokensTab save={save} realm={realm} />
           </Tab>
-          <Tab
-            title={
-              <TabTitleText>{t("realm-settings:clientPolicies")}</TabTitleText>
-            }
-            data-testid="rs-clientPolicies-tab"
-            {...clientPoliciesTab}
-          >
-            <RoutableTabs
-              mountOnEnter
-              defaultLocation={toClientPolicies({
-                realm: realmName,
-                tab: "profiles",
-              })}
+
+          {!useServerInfo().profileInfo?.disabledFeatures?.includes(
+            "CLIENT_POLICIES"
+          ) && (
+            <Tab
+              title={
+                <TabTitleText>
+                  {t("realm-settings:clientPolicies")}
+                </TabTitleText>
+              }
+              data-testid="rs-clientPolicies-tab"
+              {...clientPoliciesTab}
             >
-              <Tab
-                id="profiles"
-                data-testid="rs-policies-clientProfiles-tab"
-                aria-label={t("clientProfilesSubTab")}
-                title={<TabTitleText>{t("profiles")}</TabTitleText>}
-                tooltip={
-                  <Tooltip
-                    content={t("realm-settings:clientPoliciesProfilesHelpText")}
-                  />
-                }
-                {...clientPoliciesProfilesTab}
+              <RoutableTabs
+                mountOnEnter
+                defaultLocation={toClientPolicies({
+                  realm: realmName,
+                  tab: "profiles",
+                })}
               >
-                <ProfilesTab />
-              </Tab>
-              <Tab
-                id="policies"
-                data-testid="rs-policies-clientPolicies-tab"
-                aria-label={t("clientPoliciesSubTab")}
-                {...clientPoliciesPoliciesTab}
-                title={<TabTitleText>{t("policies")}</TabTitleText>}
-                tooltip={
-                  <Tooltip
-                    content={t("realm-settings:clientPoliciesPoliciesHelpText")}
-                  />
-                }
-              >
-                <PoliciesTab />
-              </Tab>
-            </RoutableTabs>
-          </Tab>
+                <Tab
+                  id="profiles"
+                  data-testid="rs-policies-clientProfiles-tab"
+                  aria-label={t("clientProfilesSubTab")}
+                  title={<TabTitleText>{t("profiles")}</TabTitleText>}
+                  tooltip={
+                    <Tooltip
+                      content={t(
+                        "realm-settings:clientPoliciesProfilesHelpText"
+                      )}
+                    />
+                  }
+                  {...clientPoliciesProfilesTab}
+                >
+                  <ProfilesTab />
+                </Tab>
+                <Tab
+                  id="policies"
+                  data-testid="rs-policies-clientPolicies-tab"
+                  aria-label={t("clientPoliciesSubTab")}
+                  {...clientPoliciesPoliciesTab}
+                  title={<TabTitleText>{t("policies")}</TabTitleText>}
+                  tooltip={
+                    <Tooltip
+                      content={t(
+                        "realm-settings:clientPoliciesPoliciesHelpText"
+                      )}
+                    />
+                  }
+                >
+                  <PoliciesTab />
+                </Tab>
+              </RoutableTabs>
+            </Tab>
+          )}
           {isFeatureEnabled(Feature.DeclarativeUserProfile) &&
             realm.attributes?.userProfileEnabled === "true" && (
               <Tab
