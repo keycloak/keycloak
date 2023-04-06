863d28e232bddc1f1623bb8e214bfb8e2fface7a
mposolda <mposolda@gmail.com>
2023-04-06 13:14:48 +0200

    Promote FIPS 140-2 to supported in Keycloak 22 closes #17234

--- common/src/test/java/org/keycloak/common/ProfileTest.java
+++ common/src/test/java/org/keycloak/common/ProfileTest.java
@@ -76,7 +76,7 @@ public class ProfileTest {
             disabledFeatutes.add(Profile.Feature.KERBEROS);
         }
         assertEquals(profile.getDisabledFeatures(), disabledFeatutes);
-        assertEquals(profile.getPreviewFeatures(), Profile.Feature.FIPS, Profile.Feature.ADMIN_FINE_GRAINED_AUTHZ, Profile.Feature.RECOVERY_CODES, Profile.Feature.SCRIPTS, Profile.Feature.TOKEN_EXCHANGE, Profile.Feature.OPENSHIFT_INTEGRATION, Profile.Feature.DECLARATIVE_USER_PROFILE, Profile.Feature.CLIENT_SECRET_ROTATION, Profile.Feature.UPDATE_EMAIL);
+        assertEquals(profile.getPreviewFeatures(), Profile.Feature.ADMIN_FINE_GRAINED_AUTHZ, Profile.Feature.RECOVERY_CODES, Profile.Feature.SCRIPTS, Profile.Feature.TOKEN_EXCHANGE, Profile.Feature.OPENSHIFT_INTEGRATION, Profile.Feature.DECLARATIVE_USER_PROFILE, Profile.Feature.CLIENT_SECRET_ROTATION, Profile.Feature.UPDATE_EMAIL);
     }
 
     @Test
--- quarkus/config-api/src/main/java/org/keycloak/config/SecurityOptions.java
+++ quarkus/config-api/src/main/java/org/keycloak/config/SecurityOptions.java
@@ -2,7 +2,8 @@ package org.keycloak.config;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
+
+import org.keycloak.common.Profile;
 import org.keycloak.common.crypto.FipsMode;
 
 public class SecurityOptions {
@@ -11,7 +12,9 @@ public class SecurityOptions {
             .category(OptionCategory.SECURITY)
             .expectedValues(SecurityOptions::getFipsModeValues)
             .buildTime(true)
-            .description("Sets the FIPS mode. If '" + FipsMode.NON_STRICT + "' is set, FIPS is enabled but on non-approved mode. For full FIPS compliance, set '" + FipsMode.STRICT + "' to run on approved mode.")
+            .description("Sets the FIPS mode. If '" + FipsMode.NON_STRICT + "' is set, FIPS is enabled but on non-approved mode. For full FIPS compliance, set '" + FipsMode.STRICT + "' to run on approved mode. "
+                    + "This option defaults to '" + FipsMode.DISABLED + "' when '" + Profile.Feature.FIPS.getKey() + "' feature is disabled, which is by default. "
+                    + "This option defaults to '" + FipsMode.NON_STRICT + "' when '" + Profile.Feature.FIPS.getKey() + "' feature is enabled.")
             .defaultValue(FipsMode.DISABLED)
             .build();
 
