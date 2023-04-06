a6821369b6df754a9a9898d6fa91308278ff534f
Jon Koops <jonkoops@gmail.com>
2023-04-06 14:08:55 +0200

    Use new Root API to render Admin UI

--- js/apps/admin-ui/src/main.tsx
+++ js/apps/admin-ui/src/main.tsx
@@ -2,7 +2,7 @@ import "@patternfly/react-core/dist/styles/base.css";
 import "@patternfly/patternfly/patternfly-addons.css";
 
 import { StrictMode } from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import { createHashRouter, RouterProvider } from "react-router-dom";
 
 import { RootRoute } from "./routes";
@@ -11,10 +11,10 @@ import "./index.css";
 
 const router = createHashRouter([RootRoute]);
 const container = document.getElementById("app");
+const root = createRoot(container!);
 
-render(
+root.render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-  container
+  </StrictMode>
 );
