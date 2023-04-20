6b3eb463144763ade3afb4b4d29378eecafffc2b
Stian Thorgersen <stianst@gmail.com>
2023-04-20 09:07:05 +0200

    Fix setting versions for docs (#19826)
    Closes #19801

--- set-version.sh
+++ set-version.sh
@@ -17,10 +17,10 @@ cd -
 
 # Documentation
 cd docs/documentation
-SHORT_VERSION=`echo $VERSION | awk -F '.' '{ print $1"."$2 }'`
-sed -i 's/:project_version: .*/:project_version: '$VERSION'/' topics/templates/document-attributes.adoc
-sed -i 's/:project_versionMvn: .*/:project_versionMvn: '$VERSION'/' topics/templates/document-attributes.adoc
-sed -i 's/:project_versionNpm: .*/:project_versionNpm: '$VERSION'/' topics/templates/document-attributes.adoc
+SHORT_VERSION=`echo $NEW_VERSION | awk -F '.' '{ print $1"."$2 }'`
+sed -i 's/:project_version: .*/:project_version: '$NEW_VERSION'/' topics/templates/document-attributes.adoc
+sed -i 's/:project_versionMvn: .*/:project_versionMvn: '$NEW_VERSION'/' topics/templates/document-attributes.adoc
+sed -i 's/:project_versionNpm: .*/:project_versionNpm: '$NEW_VERSION'/' topics/templates/document-attributes.adoc
 sed -i 's/:project_versionDoc: .*/:project_versionDoc: '$SHORT_VERSION'/' topics/templates/document-attributes.adoc
 cd -
 
