787bf17d6ba19ef5a99da83c7152184016adb1a6
Pavel Drozd <pdrozd@redhat.com>
2023-05-22 13:26:17 +0200

    Add tests and profiles for testing EAP6, SpringBoot and Fuse adapters

--- testsuite/integration-arquillian/tests/other/springboot-tests/src/test/java/org/keycloak/testsuite/springboot/OfflineTokenSpringBootTest.java
+++ testsuite/integration-arquillian/tests/other/springboot-tests/src/test/java/org/keycloak/testsuite/springboot/OfflineTokenSpringBootTest.java
@@ -1,7 +1,6 @@
 package org.keycloak.testsuite.springboot;
 
 import org.eclipse.persistence.annotations.BatchFetch;
-import org.hibernate.annotations.SelectBeforeUpdate;
 import org.jboss.arquillian.graphene.page.Page;
 import org.junit.Assert;
 import org.junit.Before;
