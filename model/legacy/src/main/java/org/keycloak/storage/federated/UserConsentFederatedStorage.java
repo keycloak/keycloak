cfbc70ffb99f9d429509cd1be9f41c37f25742a9
vramik <vramik@redhat.com>
2023-06-04 15:44:25 +0200

    Deprecate Streams interfaces in federated storage provider classes
    Fixes #20877

--- model/legacy/src/main/java/org/keycloak/storage/federated/UserConsentFederatedStorage.java
+++ model/legacy/src/main/java/org/keycloak/storage/federated/UserConsentFederatedStorage.java
@@ -19,8 +19,6 @@ package org.keycloak.storage.federated;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.UserConsentModel;
 
-import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -32,41 +30,22 @@ public interface UserConsentFederatedStorage {
     UserConsentModel getConsentByClient(RealmModel realm, String userId, String clientInternalId);
 
     /**
-     * @deprecated Use {@link #getConsentsStream(RealmModel, String) getConsentsStream} instead.
-     */
-    @Deprecated
-    List<UserConsentModel> getConsents(RealmModel realm, String userId);
-
-    /**
      * Obtains the consents associated with the federated user identified by {@code userId}.
      *
      * @param realm a reference to the realm.
      * @param userId the user identifier.
      * @return a non-null {@link Stream} of consents associated with the user.
      */
-    default Stream<UserConsentModel> getConsentsStream(RealmModel realm, String userId) {
-        List<UserConsentModel> value = this.getConsents(realm, userId);
-        return value != null ? value.stream() : Stream.empty();
-    }
+    Stream<UserConsentModel> getConsentsStream(RealmModel realm, String userId);
 
     void updateConsent(RealmModel realm, String userId, UserConsentModel consent);
     boolean revokeConsentForClient(RealmModel realm, String userId, String clientInternalId);
 
     /**
-     * The {@link Streams} interface makes all collection-based methods in {@link UserConsentFederatedStorage}
-     * default by providing implementations that delegate to the {@link Stream}-based variants instead of the other way
-     * around.
-     * <p/>
-     * It allows for implementations to focus on the {@link Stream}-based approach for processing sets of data and benefit
-     * from the potential memory and performance optimizations of that approach.
+     * @deprecated This interface is no longer necessary, collection-based methods were removed from the parent interface
+     * and therefore the parent interface can be used directly
      */
+    @Deprecated
     interface Streams extends UserConsentFederatedStorage {
-        @Override
-        default List<UserConsentModel> getConsents(RealmModel realm, String userId) {
-            return this.getConsentsStream(realm, userId).collect(Collectors.toList());
-        }
-
-        @Override
-        Stream<UserConsentModel> getConsentsStream(RealmModel realm, String userId);
     }
 }
