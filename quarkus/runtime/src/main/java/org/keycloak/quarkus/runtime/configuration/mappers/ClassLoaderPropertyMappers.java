bde57ca839c35116c6b6e730c4b187b37cba95f8
Pedro Igor <pigor.craveiro@gmail.com>
2023-07-03 11:27:26 -0300

    Ignoring artifacts when running re-aug to isolate the current and new stores
    Closes #20974

--- quarkus/runtime/src/main/java/org/keycloak/quarkus/runtime/configuration/mappers/ClassLoaderPropertyMappers.java
+++ quarkus/runtime/src/main/java/org/keycloak/quarkus/runtime/configuration/mappers/ClassLoaderPropertyMappers.java
@@ -4,12 +4,19 @@ import static org.keycloak.quarkus.runtime.Environment.getCurrentOrCreateFeature
 import static org.keycloak.quarkus.runtime.configuration.mappers.PropertyMapper.fromOption;
 
 import io.smallrye.config.ConfigSourceInterceptorContext;
+
+import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.keycloak.common.Profile;
 import org.keycloak.common.Profile.Feature;
 import org.keycloak.config.ClassLoaderOptions;
+import org.keycloak.config.StorageOptions;
 import org.keycloak.quarkus.runtime.Environment;
+import org.keycloak.quarkus.runtime.configuration.Configuration;
+import org.keycloak.quarkus.runtime.configuration.MicroProfileConfigProvider;
 
 final class ClassLoaderPropertyMappers {
 
@@ -27,14 +34,27 @@ final class ClassLoaderPropertyMappers {
     private static Optional<String> resolveIgnoredArtifacts(Optional<String> value, ConfigSourceInterceptorContext context) {
         if (Environment.isRebuildCheck() || Environment.isRebuild()) {
             Profile profile = getCurrentOrCreateFeatureProfile();
+            Set<String> ignoredArtifacts = new HashSet<>();
 
             if (profile.getFeatures().get(Feature.FIPS)) {
-                return Optional.of(
-                        "org.bouncycastle:bcprov-jdk15on,org.bouncycastle:bcpkix-jdk15on,org.bouncycastle:bcutil-jdk15on,org.keycloak:keycloak-crypto-default");
+                ignoredArtifacts.addAll(List.of(
+                        "org.bouncycastle:bcprov-jdk15on", "org.bouncycastle:bcpkix-jdk15on", "org.bouncycastle:bcutil-jdk15on", "org.keycloak:keycloak-crypto-default"));
+            } else {
+                ignoredArtifacts.addAll(List.of(
+                        "org.keycloak:keycloak-crypto-fips1402", "org.bouncycastle:bc-fips", "org.bouncycastle:bctls-fips", "org.bouncycastle:bcpkix-fips"));
             }
 
-            return Optional.of(
-                    "org.keycloak:keycloak-crypto-fips1402,org.bouncycastle:bc-fips,org.bouncycastle:bctls-fips,org.bouncycastle:bcpkix-fips");
+            Optional<String> storage = Configuration.getOptionalValue(
+                    MicroProfileConfigProvider.NS_KEYCLOAK_PREFIX + StorageOptions.STORAGE.getKey());
+
+            if (storage.isEmpty()) {
+                ignoredArtifacts.add("org.keycloak:keycloak-model-map-jpa");
+                ignoredArtifacts.add("org.keycloak:keycloak-model-map-hot-rod");
+                ignoredArtifacts.add("org.keycloak:keycloak-model-map");
+                ignoredArtifacts.add("org.keycloak:keycloak-model-map-file");
+            }
+
+            return Optional.of(String.join(",", ignoredArtifacts));
         }
 
         return value;
