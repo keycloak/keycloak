97d81a34e5909ac1e4eb0d2cbafc84d2062e52b1
Erik Jan de Wit <erikjan.dewit@gmail.com>
2023-07-13 14:29:49 +0200

    removed braces from arrow function

--- js/apps/admin-ui/src/components/realm-selector/RealmSelector.tsx
+++ js/apps/admin-ui/src/components/realm-selector/RealmSelector.tsx
@@ -97,9 +97,7 @@ export const RealmSelector = () => {
         .concat(
           realms
             .filter((name) => !recentRealms.includes(name) || name === realm)
-            .map((name) => {
-              return { name, used: false };
-            }),
+            .map((name) => ({ name, used: false })),
         ),
     [recentRealms, realm, realms],
   );
