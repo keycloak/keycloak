c17b021d8211850686b29679f126581929503f6b
Erik Jan de Wit <erikjan.dewit@gmail.com>
2023-07-19 13:33:47 +0200

    now uses save from edit user
    fixes: #21719

--- js/apps/admin-ui/src/user/UserAttributes.tsx
+++ js/apps/admin-ui/src/user/UserAttributes.tsx
@@ -1,33 +1,21 @@
 import type UserRepresentation from "@keycloak/keycloak-admin-client/lib/defs/userRepresentation";
-import {
-  AlertVariant,
-  PageSection,
-  PageSectionVariants,
-} from "@patternfly/react-core";
-import { useEffect, useState } from "react";
+import { PageSection, PageSectionVariants } from "@patternfly/react-core";
+import { useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { useTranslation } from "react-i18next";
 
-import { adminClient } from "../admin-client";
-import { useAlerts } from "../components/alert/Alerts";
 import {
   AttributeForm,
   AttributesForm,
 } from "../components/key-value-form/AttributeForm";
-import {
-  arrayToKeyValue,
-  keyValueToArray,
-} from "../components/key-value-form/key-value-convert";
+import { arrayToKeyValue } from "../components/key-value-form/key-value-convert";
 import { useUserProfile } from "../realm-settings/user-profile/UserProfileContext";
 
 type UserAttributesProps = {
   user: UserRepresentation;
+  save: (user: UserRepresentation) => void;
 };
 
-export const UserAttributes = ({ user: defaultUser }: UserAttributesProps) => {
-  const { t } = useTranslation("users");
-  const { addAlert, addError } = useAlerts();
-  const [user, setUser] = useState<UserRepresentation>(defaultUser);
+export const UserAttributes = ({ user, save }: UserAttributesProps) => {
   const form = useForm<AttributeForm>({ mode: "onChange" });
   const { config } = useUserProfile();
 
@@ -38,18 +26,6 @@ export const UserAttributes = ({ user: defaultUser }: UserAttributesProps) => {
   useEffect(() => {
     form.setValue("attributes", convertAttributes());
   }, [user, config]);
-
-  const save = async (attributeForm: AttributeForm) => {
-    try {
-      const attributes = keyValueToArray(attributeForm.attributes!);
-      await adminClient.users.update({ id: user.id! }, { ...user, attributes });
-
-      setUser({ ...user, attributes });
-      addAlert(t("userSaved"), AlertVariant.success);
-    } catch (error) {
-      addError("groups:groupUpdateError", error);
-    }
-  };
 
   return (
     <PageSection variant={PageSectionVariants.light}>
