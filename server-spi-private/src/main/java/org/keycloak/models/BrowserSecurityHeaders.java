b4714f62a9ac30af8bf7c2cac9fd217098af4c0b
Klaus Niedermair <klaus.niedermair@quickbirdmedical.com>
2023-07-21 08:16:42 +0200

    Mark X-XSS-Protection header as deprecated

--- server-spi-private/src/main/java/org/keycloak/models/BrowserSecurityHeaders.java
+++ server-spi-private/src/main/java/org/keycloak/models/BrowserSecurityHeaders.java
@@ -28,6 +28,7 @@ public enum BrowserSecurityHeaders {
     CONTENT_SECURITY_POLICY_REPORT_ONLY("contentSecurityPolicyReportOnly", "Content-Security-Policy-Report-Only", ""),
     X_CONTENT_TYPE_OPTIONS("xContentTypeOptions", "X-Content-Type-Options", "nosniff"),
     X_ROBOTS_TAG("xRobotsTag", "X-Robots-Tag", "none"),
+    @Deprecated // header and value will be removed in future version for security reasons. See: https://github.com/keycloak/keycloak/issues/21728
     X_XSS_PROTECTION("xXSSProtection", "X-XSS-Protection", "1; mode=block"),
     STRICT_TRANSPORT_SECURITY("strictTransportSecurity", "Strict-Transport-Security", "max-age=31536000; includeSubDomains"),
     REFERRER_POLICY("referrerPolicy", "Referrer-Policy", "no-referrer");
