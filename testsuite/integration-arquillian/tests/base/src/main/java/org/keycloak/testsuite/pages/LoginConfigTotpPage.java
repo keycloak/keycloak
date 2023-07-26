75305269d150c9884fa068210f1f55ef1873ac24
Ricardo Martin <rmartinc@redhat.com>
2023-07-26 11:34:19 +0200

    Add logout other sessions checkbox to TOTP, webauthn and recovery authn codes setup pages (#21897)
    * Add logout other sessions checkbox to TOTP, webauthn, recovery authn codes setup pages and to update-email page
    Closes #10232

--- testsuite/integration-arquillian/tests/base/src/main/java/org/keycloak/testsuite/pages/LoginConfigTotpPage.java
+++ testsuite/integration-arquillian/tests/base/src/main/java/org/keycloak/testsuite/pages/LoginConfigTotpPage.java
@@ -25,7 +25,7 @@ import org.openqa.selenium.support.FindBy;
 /**
  * @author <a href="mailto:sthorger@redhat.com">Stian Thorgersen</a>
  */
-public class LoginConfigTotpPage extends AbstractPage {
+public class LoginConfigTotpPage extends LogoutSessionsPage {
 
     @FindBy(id = "totpSecret")
     private WebElement totpSecret;
