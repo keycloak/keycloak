93bd46557f3be3b81c78d8b31b0de542ab111641
Erik Jan de Wit <edewit@redhat.com>
2023-08-05 10:16:57 +0200

    added create option to multiselect

--- js/apps/admin-ui/src/user/components/SelectComponent.tsx
+++ js/apps/admin-ui/src/user/components/SelectComponent.tsx
@@ -1,6 +1,11 @@
 import { Select, SelectOption } from "@patternfly/react-core";
 import { useState } from "react";
-import { Controller, useFormContext } from "react-hook-form";
+import {
+  Controller,
+  useFormContext,
+  ControllerRenderProps,
+  FieldValues,
+} from "react-hook-form";
 import { useTranslation } from "react-i18next";
 
 import { Options } from "../UserProfileFields";
@@ -15,8 +20,31 @@ export const SelectComponent = ({
   const { control } = useFormContext();
   const [open, setOpen] = useState(false);
 
+  const isMultiValue = (field: ControllerRenderProps<FieldValues, string>) => {
+    return (
+      attribute.annotations?.["inputType"] === "multiselect" ||
+      Array.isArray(field.value)
+    );
+  };
+
+  const setValue = (
+    value: string,
+    field: ControllerRenderProps<FieldValues, string>,
+  ) => {
+    if (isMultiValue(field)) {
+      if (field.value.includes(value)) {
+        field.onChange(field.value.filter((item: string) => item !== value));
+      } else {
+        field.onChange([...field.value, value]);
+      }
+    } else {
+      field.onChange(value);
+    }
+  };
+
   const options =
     (attribute.validations?.options as Options | undefined)?.options || [];
+
   return (
     <UserProfileGroup {...attribute}>
       <Controller
@@ -27,23 +55,23 @@ export const SelectComponent = ({
           <Select
             toggleId={attribute.name}
             onToggle={(b) => setOpen(b)}
+            isCreatable
+            onCreateOption={(value) => setValue(value, field)}
             onSelect={(_, value) => {
               const option = value.toString();
-              if (Array.isArray(field.value)) {
-                if (field.value.includes(option)) {
-                  field.onChange(
-                    field.value.filter((item: string) => item !== option),
-                  );
-                } else {
-                  field.onChange([...field.value, option]);
-                }
-              } else {
-                field.onChange(option);
+              setValue(option, field);
+              if (!Array.isArray(field.value)) {
                 setOpen(false);
               }
             }}
-            selections={field.value ? field.value : t("common:choose")}
-            variant={Array.isArray(field.value) ? "typeaheadmulti" : "single"}
+            selections={
+              field.value
+                ? field.value
+                : isMultiValue(field)
+                ? []
+                : t("common:choose")
+            }
+            variant={isMultiValue(field) ? "typeaheadmulti" : "single"}
             aria-label={t("common:selectOne")}
             isOpen={open}
             isDisabled={
