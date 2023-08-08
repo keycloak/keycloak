baac060eb1036f0702309854de9896f11ea3cfeb
Pedro Igor <pigor.craveiro@gmail.com>
2023-08-08 16:16:34 -0300

    Fixing how e-mail attribute permissions are set for both USER_API and ACCOUNT contexts
    Closes #21751

--- services/src/main/java/org/keycloak/userprofile/LegacyAttributes.java
+++ services/src/main/java/org/keycloak/userprofile/LegacyAttributes.java
@@ -48,7 +48,12 @@ public class LegacyAttributes extends DefaultAttributes {
 
     @Override
     protected boolean isIncludeAttributeIfNotProvided(AttributeMetadata metadata) {
-        // user api expects that built-in attributes are not updated if not provided when in legacy mode
-        return UserProfileContext.USER_API.equals(context) && !isRootAttribute(metadata.getName());
+        if (UserModel.LOCALE.equals(metadata.getName())) {
+            // locale is an internal attribute and should be updated as a regular attribute
+            return false;
+        }
+
+        // user api expects that attributes are not updated if not provided when in legacy mode
+        return UserProfileContext.USER_API.equals(context);
     }
 }
