81da383552812861aeafd80201c2ca5da2ea81a0
Garth <244253+xgp@users.noreply.github.com>
2023-08-08 22:12:57 +0200

    updated useWithTheme to load from accountResourceProvider properties key.

--- server-spi-private/src/main/java/org/keycloak/services/resource/AccountResourceProvider.java
+++ server-spi-private/src/main/java/org/keycloak/services/resource/AccountResourceProvider.java
@@ -3,13 +3,24 @@ package org.keycloak.services.resource;
 import org.keycloak.provider.Provider;
 import org.keycloak.theme.Theme;
 
+import java.io.IOException;
+
 /**
  * <p>A {@link AccountResourceProvider} creates JAX-RS resource instances for the Account endpoints, allowing
  * an implementor to override the behavior of the entire Account console.
  */
 public interface AccountResourceProvider extends Provider {
+  public static final String PROVIDER_CLASS_KEY = "accountResourceProvider";
+
   /** Return true if this should be used with the given theme. */
-  boolean useWithTheme(Theme theme);
+  default boolean useWithTheme(Theme theme) {
+    try {
+      String providerClass = theme.getProperties().getProperty(PROVIDER_CLASS_KEY);
+      return (providerClass != null && providerClass.equals(getClass().getName()));
+    } catch (IOException ignore) {
+      return false;
+    }
+  }
 
   /** Returns a JAX-RS resource instance. */
   Object getResource();
