33aab79d9d9b0dd639e52eda2b3004b0d7f2dcd4
Erik Jan de Wit <erikjan.dewit@gmail.com>
2023-08-09 13:26:04 +0200

    now uses save from edit user (#21809)
    fixes: #21719

--- js/apps/admin-ui/src/user/EditUser.tsx
+++ js/apps/admin-ui/src/user/EditUser.tsx
@@ -15,6 +15,10 @@ import { useNavigate } from "react-router-dom";
 import { adminClient } from "../admin-client";
 import { useAlerts } from "../components/alert/Alerts";
 import { useConfirmDialog } from "../components/confirm-dialog/ConfirmDialog";
+import {
+  KeyValueType,
+  keyValueToArray,
+} from "../components/key-value-form/key-value-convert";
 import { KeycloakSpinner } from "../components/keycloak-spinner/KeycloakSpinner";
 import {
   RoutableTabs,
@@ -145,13 +149,18 @@ const EditUserForm = ({ user, bruteForced, refresh }: EditUserFormProps) => {
   useUpdateEffect(() => userForm.reset(user), [user]);
 
   const save = async (formUser: UserRepresentation) => {
+    const attributes =
+      "key" in (formUser.attributes?.[0] || [])
+        ? keyValueToArray(formUser.attributes as KeyValueType[])
+        : user.attributes;
     try {
       await adminClient.users.update(
         { id: user.id! },
         {
+          ...user,
           ...formUser,
           username: formUser.username?.trim(),
-          attributes: { ...user.attributes, ...formUser.attributes },
+          attributes,
         },
       );
       addAlert(t("userSaved"), AlertVariant.success);
@@ -258,7 +267,7 @@ const EditUserForm = ({ user, bruteForced, refresh }: EditUserFormProps) => {
                   title={<TabTitleText>{t("common:attributes")}</TabTitleText>}
                   {...attributesTab}
                 >
-                  <UserAttributes user={user} />
+                  <UserAttributes user={user} save={save} />
                 </Tab>
               )}
               <Tab
