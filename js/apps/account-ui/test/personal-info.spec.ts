ba09d1d164fe5aafa8257925de86c652424673bd
Erik Jan de Wit <erikjan.dewit@gmail.com>
2023-08-14 12:44:36 +0200

    removed email check for now

--- js/apps/account-ui/test/personal-info.spec.ts
+++ js/apps/account-ui/test/personal-info.spec.ts
@@ -3,7 +3,6 @@ import { test, expect } from "@playwright/test";
 test.describe("Personal info page", () => {
   test("sets basic information", async ({ page }) => {
     await page.goto("./");
-    await page.getByTestId("email").fill("edewit@example.com");
     await page.getByTestId("firstName").fill("Erik");
     await page.getByTestId("lastName").fill("de Wit");
     await page.getByTestId("save").click();
