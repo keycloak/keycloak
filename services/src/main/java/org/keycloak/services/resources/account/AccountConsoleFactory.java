3a6c82ad3381c0250c247a0274c25e7a2e64ff26
Garth <244253+xgp@users.noreply.github.com>
2023-09-06 15:14:09 +0200

    added services config for AccountConsoleFactory

---
+++ services/src/main/java/org/keycloak/services/resources/account/AccountConsoleFactory.java
@@ -0,0 +1,44 @@
+package org.keycloak.services.resources.account;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import java.util.Map;
+import org.keycloak.Config.Scope;
+import org.keycloak.events.EventBuilder;
+import org.keycloak.models.ClientModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.models.ProtocolMapperModel;
+import org.keycloak.models.RealmModel;
+import org.keycloak.provider.ProviderEvent;
+import org.keycloak.services.resource.AccountResourceProvider;
+import org.keycloak.services.resource.AccountResourceProviderFactory;
+import org.keycloak.theme.Theme;
+import jakarta.ws.rs.NotFoundException;
+import org.keycloak.models.Constants;
+
+public class AccountConsoleFactory implements AccountResourceProviderFactory {
+  public static final String ID = "account-console";
+
+  @Override
+  public String getId() {
+    return ID;
+  }
+
+  @Override
+  public AccountResourceProvider create(KeycloakSession session) {
+    RealmModel realm = session.getContext().getRealm();
+    ClientModel client = getAccountManagementClient(realm);
+    Theme theme = getTheme(session);
+    return new AccountConsole(session, client, theme);
+  }
+
+  @Override
+  public void init(Scope config) {}
+
+  @Override
+  public void postInit(KeycloakSessionFactory factory) {}
+
+  @Override
+  public void close() {}
+}
