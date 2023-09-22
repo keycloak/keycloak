a70cf34a6e3a3108b01c956d815ac15b0c34b81a
osorionicolas <osorionicolas95@gmail.com>
2023-09-22 16:35:11 +0200

    Fix issue with groups search not sending the search input in the request
    Backports #23341

--- js/apps/admin-ui/src/components/group/GroupPickerDialog.tsx
+++ js/apps/admin-ui/src/components/group/GroupPickerDialog.tsx
@@ -80,7 +80,7 @@ export const GroupPickerDialog = ({
               first: `${first}`,
               max: `${max + 1}`,
             },
-            isSearching ? null : { search: filter },
+            isSearching ? { search: filter } : null,
           ),
         );
       } else if (!navigation.map(({ id }) => id).includes(groupId)) {
