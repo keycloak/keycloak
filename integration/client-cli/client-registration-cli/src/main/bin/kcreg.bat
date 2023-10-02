01099e60b1712b9b298a50109092e6fe8ba941f6
Peter Zaoral <pzaoral@redhat.com>
2023-10-02 14:32:51 +0200

    kcadm.bat, kcreg.bat do not respect environment variables JAVA, JAVA_HOME to select JVM
    * edited kcadm.bat and kcreg.bat scripts
    
    Closes #23214
    
    Signed-off-by: Peter Zaoral <pzaoral@redhat.com>

--- integration/client-cli/client-registration-cli/src/main/bin/kcreg.bat
+++ integration/client-cli/client-registration-cli/src/main/bin/kcreg.bat
@@ -5,4 +5,24 @@ if "%OS%" == "Windows_NT" (
 ) else (
   set DIRNAME=.\
 )
-java %KC_OPTS% -cp "%DIRNAME%\client\keycloak-client-registration-cli-${project.version}.jar" --add-opens=java.base/java.security=ALL-UNNAMED -Dkc.lib.dir="%DIRNAME%\client\lib" org.keycloak.client.registration.cli.KcRegMain %*
+
+if "x%JAVA_HOME%" == "x" (
+  set  JAVA=java
+  echo JAVA_HOME is not set. Unexpected results may occur.
+  echo Set JAVA_HOME to the directory of your local JDK to avoid this message.
+) else (
+  if not exist "%JAVA_HOME%" (
+    echo JAVA_HOME "%JAVA_HOME%" path doesn't exist
+    goto END
+   ) else (
+     if not exist "%JAVA_HOME%\bin\java.exe" (
+       echo "%JAVA_HOME%\bin\java.exe" does not exist
+       goto END
+     )
+    set "JAVA=%JAVA_HOME%\bin\java"
+  )
+)
+
+"%JAVA%" %KC_OPTS% -cp "%DIRNAME%\client\keycloak-client-registration-cli-${project.version}.jar" --add-opens=java.base/java.security=ALL-UNNAMED -Dkc.lib.dir="%DIRNAME%\client\lib" org.keycloak.client.registration.cli.KcRegMain %*
+
+:END
