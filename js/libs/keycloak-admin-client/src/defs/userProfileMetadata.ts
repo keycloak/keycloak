7ad7c922d3b650a844a1d82b0861e50353a35428
Jon Koops <jonkoops@gmail.com>
2023-10-04 17:31:03 +0200

    Add correct type for groups

--- js/libs/keycloak-admin-client/src/defs/userProfileMetadata.ts
+++ js/libs/keycloak-admin-client/src/defs/userProfileMetadata.ts
@@ -1,9 +1,10 @@
 // See: https://github.com/keycloak/keycloak/blob/main/core/src/main/java/org/keycloak/representations/idm/UserProfileMetadata.java
 export interface UserProfileMetadata {
   attributes?: UserProfileAttributeMetadata[];
+  groups?: UserProfileAttributeGroupMetadata[];
 }
 
-// See: https://github.com/keycloak/keycloak/blob/main/services/src/main/java/org/keycloak/userprofile/config/UPAttribute.java
+// See: https://github.com/keycloak/keycloak/blob/main/core/src/main/java/org/keycloak/representations/idm/UserProfileAttributeMetadata.java
 export interface UserProfileAttributeMetadata {
   name?: string;
   displayName?: string;
@@ -11,4 +12,12 @@ export interface UserProfileAttributeMetadata {
   readOnly?: boolean;
   annotations?: Record<string, unknown>;
   validators?: Record<string, Record<string, unknown>>;
+}
+
+// See: https://github.com/keycloak/keycloak/blob/main/core/src/main/java/org/keycloak/representations/idm/UserProfileAttributeGroupMetadata.java
+export interface UserProfileAttributeGroupMetadata {
+  name?: string;
+  displayHeader?: string;
+  displayDescription?: string;
+  annotations?: Record<string, unknown>;
 }
