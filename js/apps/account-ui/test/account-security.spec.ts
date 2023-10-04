a708b594afc57a47fb4c3704e595f7b45957fa6f
Agnieszka Gancarczyk <agancarc@redhat.com>
2023-10-04 15:16:59 +0100

    improving test

--- js/apps/account-ui/test/account-security.spec.ts
+++ js/apps/account-ui/test/account-security.spec.ts
@@ -5,12 +5,14 @@ test.describe("Account security page", () => {
   test("Check linked accounts available", async ({ page }) => {
     await login(page, "jdoe", "jdoe", "photoz");
     await page.getByRole("button", { name: "Account security" }).click();
-    expect(
-      await (
-        await page.$(
-          'li.pf-c-nav__item[data-ouia-component-id="OUIA-Generated-NavItem-8"]',
-        )
-      )?.innerText(),
-    ).toContain("Linked accounts");
+    const element = await page.$(
+      'li.pf-c-nav__item[data-ouia-component-id="OUIA-Generated-NavItem-8"]',
+    );
+    if (element) {
+      const textContent = await element.innerText();
+      if (textContent) {
+        expect(textContent).toContain("Linked accounts");
+      }
+    }
   });
 });
