b47c8fed965947a764b119b2f218f7dd2dec5789
Jon Koops <jonkoops@gmail.com>
2023-10-05 00:13:04 +0200

    fix cypress admin client

--- js/apps/admin-ui/cypress/support/util/AdminClient.ts
+++ js/apps/admin-ui/cypress/support/util/AdminClient.ts
@@ -1,11 +1,11 @@
 import KeycloakAdminClient from "@keycloak/keycloak-admin-client";
-import type UserRepresentation from "@keycloak/keycloak-admin-client/lib/defs/userRepresentation";
 import type ClientRepresentation from "@keycloak/keycloak-admin-client/lib/defs/clientRepresentation";
 import type ClientScopeRepresentation from "@keycloak/keycloak-admin-client/lib/defs/clientScopeRepresentation";
 import type RealmRepresentation from "@keycloak/keycloak-admin-client/lib/defs/realmRepresentation";
-import type UserProfileConfig from "@keycloak/keycloak-admin-client/lib/defs/userProfileConfig";
 import type RoleRepresentation from "@keycloak/keycloak-admin-client/lib/defs/roleRepresentation";
 import type { RoleMappingPayload } from "@keycloak/keycloak-admin-client/lib/defs/roleRepresentation";
+import type { UserProfileConfig } from "@keycloak/keycloak-admin-client/lib/defs/userProfileConfig";
+import type UserRepresentation from "@keycloak/keycloak-admin-client/lib/defs/userRepresentation";
 import { merge } from "lodash-es";
 
 class AdminClient {
