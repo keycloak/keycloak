127b661eae70ec75d5c73348f2db71af90344759
Peter Zaoral <pzaoral@redhat.com>
2023-10-12 12:04:43 +0200

    kcadm.bat, kcreg.bat do not respect environment variables JAVA, JAVA_HOME to select JVM
    * incorporated suggestions
    
    Closes #23214
    
    Signed-off-by: Peter Zaoral <pzaoral@redhat.com>

--- integration/client-cli/client-registration-cli/src/main/bin/kcreg.bat
+++ integration/client-cli/client-registration-cli/src/main/bin/kcreg.bat
@@ -6,20 +6,22 @@ if "%OS%" == "Windows_NT" (
   set DIRNAME=.\
 )
 
-if "x%JAVA_HOME%" == "x" (
-  set  JAVA=java
-  echo JAVA_HOME is not set. Unexpected results may occur.
-  echo Set JAVA_HOME to the directory of your local JDK to avoid this message.
-) else (
-  if not exist "%JAVA_HOME%" (
-    echo JAVA_HOME "%JAVA_HOME%" path doesn't exist
-    goto END
-   ) else (
-     if not exist "%JAVA_HOME%\bin\java.exe" (
-       echo "%JAVA_HOME%\bin\java.exe" does not exist
-       goto END
-     )
-    set "JAVA=%JAVA_HOME%\bin\java"
+if "x%JAVA%" == "x" (
+  if "x%JAVA_HOME%" == "x" (
+    set JAVA=java
+    echo JAVA_HOME is not set. Unexpected results may occur. 1>&2
+    echo Set JAVA_HOME to the directory of your local JDK to avoid this message. 1>&2
+  ) else (
+    if not exist "%JAVA_HOME%" (
+      echo JAVA_HOME "%JAVA_HOME%" path doesn't exist 1>&2
+      goto END
+    ) else (
+      if not exist "%JAVA_HOME%\bin\java.exe" (
+        echo "%JAVA_HOME%\bin\java.exe" does not exist 1>&2
+        goto END
+      )
+      set "JAVA=%JAVA_HOME%\bin\java"
+    )
   )
 )
 
