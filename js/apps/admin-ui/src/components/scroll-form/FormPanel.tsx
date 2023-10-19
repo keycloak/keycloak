46ad1b8f7dc19bab4704a1ec3b488eb5e47c630e
Erik Jan de Wit <erikjan.dewit@gmail.com>
2023-10-19 16:46:10 +0200

    small refactor on the scroll form
    after this refactor it can be moved into ui-shared

--- js/apps/admin-ui/src/components/scroll-form/FormPanel.tsx
+++ js/apps/admin-ui/src/components/scroll-form/FormPanel.tsx
@@ -1,13 +1,6 @@
-import {
-  Card,
-  CardBody,
-  CardHeader,
-  CardTitle,
-  Title,
-} from "@patternfly/react-core";
+import { Card, CardBody, CardHeader, CardTitle } from "@patternfly/react-core";
 import { PropsWithChildren, useId } from "react";
-
-import "./form-panel.css";
+import { FormTitle } from "./FormTitle";
 
 type FormPanelProps = {
   title: string;
@@ -27,15 +20,7 @@ export const FormPanel = ({
     <Card id={id} className={className} isFlat>
       <CardHeader className="kc-form-panel__header">
         <CardTitle tabIndex={0}>
-          <Title
-            headingLevel="h1"
-            size="xl"
-            className="kc-form-panel__title"
-            id={scrollId}
-            tabIndex={0} // so that jumpLink sends focus to the section for a11y
-          >
-            {title}
-          </Title>
+          <FormTitle id={scrollId} title={title} />
         </CardTitle>
       </CardHeader>
       <CardBody className="kc-form-panel__body">{children}</CardBody>
