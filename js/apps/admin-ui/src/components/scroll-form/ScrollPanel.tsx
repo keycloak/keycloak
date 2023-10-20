29de8418834acb8c6ccfc698068b27d8f15eb3d4
Erik Jan de Wit <erikjan.dewit@gmail.com>
2023-10-20 13:40:16 +0200

    small refactor on the scroll form (#24152)
    after this refactor it can be moved into ui-shared

--- js/apps/admin-ui/src/components/scroll-form/ScrollPanel.tsx
+++ js/apps/admin-ui/src/components/scroll-form/ScrollPanel.tsx
@@ -1,9 +1,7 @@
 /* eslint-disable react/jsx-no-useless-fragment */
 // See: https://github.com/i18next/react-i18next/issues/1543
-import { Title } from "@patternfly/react-core";
 import { HTMLProps } from "react";
-
-import "./form-panel.css";
+import { FormTitle } from "./FormTitle";
 
 type ScrollPanelProps = HTMLProps<HTMLFormElement> & {
   title: string;
@@ -13,17 +11,9 @@ type ScrollPanelProps = HTMLProps<HTMLFormElement> & {
 export const ScrollPanel = (props: ScrollPanelProps) => {
   const { title, children, scrollId, ...rest } = props;
   return (
-    <section {...rest} className="kc-form-panel__panel">
+    <section {...rest} style={{ marginTop: "var(--pf-global--spacer--lg)" }}>
       <>
-        <Title
-          headingLevel="h1"
-          size="xl"
-          className="kc-form-panel__title"
-          id={scrollId}
-          tabIndex={0} // so that jumpLink sends focus to the section for a11y
-        >
-          {title}
-        </Title>
+        <FormTitle id={scrollId} title={title} />
         {children}
       </>
     </section>
