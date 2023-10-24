e05effe62d5ed887eec905eccf223365f2bffa2b
Martin Kanis <mkanis@redhat.com>
2023-10-24 15:13:05 +0200

    Map Store Removal: Delete map profiles and scopes from model tests
    Closes #24093

--- testsuite/model/src/test/java/org/keycloak/testsuite/model/session/UserSessionProviderModelTest.java
+++ testsuite/model/src/test/java/org/keycloak/testsuite/model/session/UserSessionProviderModelTest.java
@@ -17,7 +17,6 @@
 package org.keycloak.testsuite.model.session;
 
 import org.hamcrest.Matchers;
-import org.infinispan.client.hotrod.RemoteCache;
 import org.junit.Assert;
 import org.junit.Test;
 import org.keycloak.models.AuthenticatedClientSessionModel;
@@ -26,19 +25,10 @@ import org.keycloak.models.Constants;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.RealmProvider;
-import org.keycloak.models.UserManager;
 import org.keycloak.models.UserModel;
 import org.keycloak.models.UserProvider;
 import org.keycloak.models.UserSessionModel;
 import org.keycloak.models.UserSessionProvider;
-import org.keycloak.models.map.storage.ModelEntityUtil;
-import org.keycloak.models.map.storage.chm.ConcurrentHashMapStorageProviderFactory;
-import org.keycloak.models.map.storage.hotRod.connections.DefaultHotRodConnectionProviderFactory;
-import org.keycloak.models.map.storage.hotRod.connections.HotRodConnectionProvider;
-import org.keycloak.models.map.storage.hotRod.userSession.HotRodUserSessionEntity;
-import org.keycloak.models.map.userSession.MapUserSessionProvider;
-import org.keycloak.models.map.userSession.MapUserSessionProviderFactory;
-import org.keycloak.models.sessions.infinispan.InfinispanUserSessionProviderFactory;
 import org.keycloak.models.sessions.infinispan.changes.sessions.PersisterLastSessionRefreshStoreFactory;
 import org.keycloak.models.utils.KeycloakModelUtils;
 import org.keycloak.models.utils.ResetTimeOffsetEvent;
@@ -50,7 +40,6 @@ import org.keycloak.timer.TimerProvider;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
-import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicReference;
@@ -59,7 +48,6 @@ import java.util.stream.Collectors;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assume.assumeFalse;
 import static org.keycloak.testsuite.model.session.UserSessionPersisterProviderTest.createClients;
 import static org.keycloak.testsuite.model.session.UserSessionPersisterProviderTest.createSessions;
 
@@ -164,22 +152,8 @@ public class UserSessionProviderModelTest extends KeycloakModelTest {
                 Assert.assertEquals(origSessions[1], userSession);
             });
 
-
-            // not possible to expire client session without expiring user sessions with time offset in map storage because
-            // expiration in map storage takes min of (clientSessionIdleExpiration, ssoSessionIdleTimeout)
             inComittedTransaction(session -> {
-                if (session.getProvider(UserSessionProvider.class) instanceof MapUserSessionProvider) {
-                    RealmModel realm = session.realms().getRealm(realmId);
-
-                    UserSessionModel userSession = session.sessions().getUserSession(realm, origSessions[0].getId());
-
-                    userSession.getAuthenticatedClientSessions().values().stream().forEach(clientSession -> {
-                        // expire client sessions
-                        clientSession.setTimestamp(1);
-                    });
-                } else {
-                    setTimeOffset(1000);
-                }
+                setTimeOffset(1000);
             });
 
             inComittedTransaction(session -> {
@@ -230,7 +204,6 @@ public class UserSessionProviderModelTest extends KeycloakModelTest {
     }
 
     @Test
-    @RequireProvider(value = UserSessionProvider.class, only = InfinispanUserSessionProviderFactory.PROVIDER_ID)
     public void testClientSessionIsNotPersistedForTransientUserSession() {
         Object[] transientUserSessionWithClientSessionId = inComittedTransaction(session -> {
             RealmModel realm = session.realms().getRealm(realmId);
@@ -256,32 +229,7 @@ public class UserSessionProviderModelTest extends KeycloakModelTest {
     }
 
     @Test
-    @RequireProvider(value = HotRodConnectionProvider.class, only = DefaultHotRodConnectionProviderFactory.PROVIDER_ID)
-    public void testRemoteCachesParallel() throws InterruptedException {
-        inIndependentFactories(4, 30, () -> inComittedTransaction(session -> {
-            HotRodConnectionProvider provider = session.getProvider(HotRodConnectionProvider.class);
-            RemoteCache<String, HotRodUserSessionEntity> remoteCache = provider.getRemoteCache(ModelEntityUtil.getModelName(UserSessionModel.class));
-            HotRodUserSessionEntity userSessionEntity = new HotRodUserSessionEntity();
-            userSessionEntity.id = UUID.randomUUID().toString();
-            userSessionEntity.realmId = realmId;
-            remoteCache.put(userSessionEntity.id, userSessionEntity);
-        }));
-
-        inComittedTransaction(session -> {
-            HotRodConnectionProvider provider = session.getProvider(HotRodConnectionProvider.class);
-            RemoteCache<String, HotRodUserSessionEntity> remoteCache = provider.getRemoteCache(ModelEntityUtil.getModelName(UserSessionModel.class));
-            assertThat(remoteCache.size(), Matchers.is(4));
-        });
-    }
-
-    @Test
     public void testCreateUserSessionsParallel() throws InterruptedException {
-        // Skip the test if MapUserSessionProvider == CHM
-        String usProvider = CONFIG.getConfig().get("userSessions.provider");
-        String usMapStorageProvider = CONFIG.getConfig().get("userSessions.map.storage.provider");
-        assumeFalse(MapUserSessionProviderFactory.PROVIDER_ID.equals(usProvider) &&
-                (usMapStorageProvider == null || ConcurrentHashMapStorageProviderFactory.PROVIDER_ID.equals(usMapStorageProvider)));
-
         Set<String> userSessionIds = Collections.newSetFromMap(new ConcurrentHashMap<>());
         CountDownLatch latch = new CountDownLatch(4);
 
