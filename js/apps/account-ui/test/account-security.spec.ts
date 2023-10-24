498ee0690428c3ebe9535e54d948a5ca2dd0661b
Agnieszka Gancarczyk <agancarc@redhat.com>
2023-10-24 15:34:14 +0100

    trying to improve test

--- js/apps/account-ui/test/account-security.spec.ts
+++ js/apps/account-ui/test/account-security.spec.ts
@@ -3,22 +3,19 @@ import { login } from "./login";
 
 test("Check page heading", async ({ page }) => {
   await login(page, "jdoe", "jdoe", "photoz");
-  const securityButton = await page.getByRole("button", {
-    name: "Account security",
-  });
-  await securityButton.click();
+  await page
+    .getByRole("button", {
+      name: "Account security",
+    })
+    .click();
 
   await page.waitForSelector(
     '[data-testid="account-security/linked-accounts"]',
   );
-  const linkedAccountsElement = await page.getByTestId(
-    "account-security/linked-accounts",
-  );
-  await linkedAccountsElement.click();
+  await page.getByTestId("account-security/linked-accounts").click();
 
   // Check the page heading
   await page.waitForSelector('[data-testid="page-heading"]');
   const pageHeadingElement = await page.getByTestId("page-heading");
-  const textContent = await pageHeadingElement.innerText();
-  expect(textContent).toContain("Linked accounts");
+  expect(pageHeadingElement).toHaveText("Linked accounts");
 });
