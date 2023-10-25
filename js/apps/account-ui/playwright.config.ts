ae48d0c447ed37a72a284d46f8a2798247b38fa7
Jon Koops <jonkoops@gmail.com>
2023-10-25 17:24:26 +0200

    Enable `github` reporter for Playwright (#24290)

--- js/apps/account-ui/playwright.config.ts
+++ js/apps/account-ui/playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: 1,
-  reporter: "html",
+  reporter: process.env.CI ? "github" : "list",
   use: {
     baseURL: process.env.CI
       ? "http://localhost:8080/realms/master/account/"
