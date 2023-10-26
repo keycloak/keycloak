5bd4d9d3e1f8d9a43c36a514b4fe26a714637bdb
Erik Jan de Wit <erikjan.dewit@gmail.com>
2023-10-26 11:23:38 +0200

    changed to use resource bundles
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- services/src/main/java/org/keycloak/services/resources/ThemeResource.java
+++ services/src/main/java/org/keycloak/services/resources/ThemeResource.java
@@ -26,7 +26,6 @@ import org.keycloak.encoding.ResourceEncodingProvider;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.services.ServicesLogger;
-import org.keycloak.services.managers.RealmManager;
 import org.keycloak.services.util.CacheControlUtil;
 import org.keycloak.services.util.LocaleUtil;
 import org.keycloak.theme.Theme;
@@ -104,30 +103,35 @@ public class ThemeResource {
     }
 
     @GET
-    @Path("{theme}/{locale}")
+    @Path("/{realm}/{theme}/{locale}")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<KeySource> getLocalizationTexts(@PathParam("theme") String theme, @PathParam("locale") String localeString,
-                                                @QueryParam("source") boolean showSource) throws IOException {
-        final RealmModel realm = session.getContext().getRealm();
+    public Response getLocalizationTexts(@PathParam("realm") String realmName, @PathParam("theme") String theme,
+                                         @PathParam("locale") String localeString, @QueryParam("source") boolean showSource) throws IOException {
+        final RealmModel realm = session.realms().getRealmByName(realmName);
+        session.getContext().setRealm(realm);
+        List<KeySource> result;
 
         Theme theTheme = session.theme().getTheme(Theme.Type.valueOf(theme.toUpperCase()));
         final Locale locale = Locale.forLanguageTag(localeString);
         if (showSource) {
             Properties messagesByLocale = theTheme.getMessages("messages", locale);
-            Set<KeySource> result = messagesByLocale.entrySet().stream().map(e ->
+            Set<KeySource> resultSet = messagesByLocale.entrySet().stream().map(e ->
                     new KeySource((String) e.getKey(), (String) e.getValue(), Source.THEME)).collect(toSet());
 
             Map<Locale, Properties> realmLocalizationMessages = LocaleUtil.getRealmLocalizationTexts(realm, locale);
             for (Locale currentLocale = locale; currentLocale != null; currentLocale = LocaleUtil.getParentLocale(currentLocale)) {
                 final List<KeySource> realmOverride = realmLocalizationMessages.get(currentLocale).entrySet().stream().map(e ->
                         new KeySource((String) e.getKey(), (String) e.getValue(), Source.REALM)).collect(toList());
-                result.addAll(realmOverride);
+                resultSet.addAll(realmOverride);
             }
-
-            return new ArrayList<>(result);
+            result = new ArrayList<>(resultSet);
+        } else {
+            result = theTheme.getEnhancedMessages(realm, locale).entrySet().stream().map(e ->
+                    new KeySource((String) e.getKey(), (String) e.getValue())).collect(toList());
         }
-        return theTheme.getEnhancedMessages(realm, locale).entrySet().stream().map(e ->
-                new KeySource((String) e.getKey(), (String) e.getValue())).collect(toList());
+
+        Response.ResponseBuilder responseBuilder = Response.ok(result);
+        return Cors.add(session.getContext().getHttpRequest(), responseBuilder).allowedOrigins("*").auth().build();
     }
 }
 
