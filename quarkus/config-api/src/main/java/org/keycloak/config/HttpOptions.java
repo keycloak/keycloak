465110cf8845722a8c6b4f900471810b2775d45b
Václav Muzikář <vmuzikar@redhat.com>
2023-11-10 15:29:16 +0100

    Implement load shedding
    Closes #23340
    
    Co-authored-by: Alexander Schwartz <aschwart@redhat.com>
    Signed-off-by: Václav Muzikář <vmuzikar@redhat.com>
    Signed-off-by: ShefeeqPM <86718986+ShefeeqPM@users.noreply.github.com>

--- quarkus/config-api/src/main/java/org/keycloak/config/HttpOptions.java
+++ quarkus/config-api/src/main/java/org/keycloak/config/HttpOptions.java
@@ -110,4 +110,11 @@ public class HttpOptions {
             .description("Enables or disables the HTTP/s and Socket serving.")
             .defaultValue(Boolean.TRUE)
             .build();
+
+    public static final Option<Integer> HTTP_MAX_QUEUED_REQUESTS = new OptionBuilder<>("http-max-queued-requests", Integer.class)
+            .category(OptionCategory.HTTP)
+            .description("Maximum number of queued HTTP requests. " +
+                         "Use this to shed load in an overload situation. Excess requests will return a \"503 Server not Available\" response.")
+            .build();
+
 }
