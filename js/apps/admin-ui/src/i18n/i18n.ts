fe7833c9573d141259c887686ce2c81ca7f43d29
Erik Jan de Wit <erikjan.dewit@gmail.com>
2023-11-13 18:39:46 +0100

    Load Admin Console localizations from resource bundles (#24316)
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- js/apps/admin-ui/src/i18n/i18n.ts
+++ js/apps/admin-ui/src/i18n/i18n.ts
@@ -1,9 +1,11 @@
 import { createInstance } from "i18next";
 import { initReactI18next } from "react-i18next";
 
+import HttpBackend from "i18next-http-backend";
 import environment from "../environment";
 import { joinPath } from "../utils/joinPath";
-import { OverridesBackend } from "./OverridesBackend";
+
+type KeyValue = { key: string; value: string };
 
 export const DEFAULT_LOCALE = "en";
 export const KEY_SEPARATOR = ".";
@@ -15,9 +17,19 @@ export const i18n = createInstance({
     escapeValue: false,
   },
   backend: {
-    loadPath: joinPath(environment.resourceUrl, `locales/{{lng}}/{{ns}}.json`),
+    loadPath: joinPath(
+      environment.authServerUrl,
+      `resources/${environment.masterRealm}/admin/{{lng}}`,
+    ),
+    parse: (data: string) => {
+      const messages = JSON.parse(data);
+
+      const result: Record<string, string> = {};
+      messages.forEach((v: KeyValue) => (result[v.key] = v.value));
+      return result;
+    },
   },
 });
 
-i18n.use(OverridesBackend);
+i18n.use(HttpBackend);
 i18n.use(initReactI18next);
