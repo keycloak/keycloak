76d00900eab60fcfb596ed6357bcb698f2f54fd9
gauthamnagendran <118388391+gauthamnagendran@users.noreply.github.com>
2023-11-16 16:38:09 +0530

    check 'which javac' & 'which java'

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 
 echo "B"
 
+which javac 
+
+which java
+
 export JAVA_HOME=/usr/local/openjdk-20
 
 ./mvnw -pl quarkus/deployment,quarkus/dist,themes, -am -DskipTests clean install | tee log-$(date +%H-%M-%y-%m-%d).txt
