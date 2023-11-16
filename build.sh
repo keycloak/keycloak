1ac21e7462f0f94dd205909687ae206d4622823a
gauthamnagendran <118388391+gauthamnagendran@users.noreply.github.com>
2023-11-16 17:54:27 +0530

    minor changes

--- build.sh
+++ build.sh
@@ -2,11 +2,13 @@
 
 #curl https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.deb --output java.deb
 #curl https://download.oracle.com/java/20/archive/jdk-20.0.2_linux-x64_bin.deb --output java.deb
-curl https://download.java.net/java/GA/jdk20/GPL/openjdk-20_linux-x64_bin.tar.gz --output java.tar.gz
+#curl https://download.java.net/java/GA/jdk20/GPL/openjdk-20_linux-x64_bin.tar.gz --output java.tar.gz
+
+curl -O https://download.java.net/java/GA/jdk20/GPL/openjdk-20_linux-x64_bin.tar.gz
 
 #apt install -y libasound2 libc6-i386 libc6-x32 libfreetype6 libxi6 libxrender1 libxtst6
 
-tar -xf java.tar.gz
+tar -xzf openjdk-20_linux-x64_bin.tar.gz 
 
 export JAVA_HOME=$(pwd)/jdk-20
 
