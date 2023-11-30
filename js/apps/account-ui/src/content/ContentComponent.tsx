335e88f13aef003f6636eda524d955b84f6acc25
Erik Jan de Wit <erikjan.dewit@gmail.com>
2023-11-30 13:52:16 +0100

    bit more performant
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- js/apps/account-ui/src/content/ContentComponent.tsx
+++ js/apps/account-ui/src/content/ContentComponent.tsx
@@ -1,5 +1,5 @@
 import { Spinner } from "@patternfly/react-core";
-import { Suspense, lazy, useState } from "react";
+import { Suspense, lazy, useMemo, useState } from "react";
 import { useParams } from "react-router-dom";
 import { environment } from "../environment";
 import { joinPath } from "../utils/joinPath";
@@ -8,7 +8,10 @@ import { fetchContentJson } from "./ContentRenderer";
 import { ContentItem, ModulePageDef, isExpansion } from "./content";
 import { ContentComponentParams } from "../routes";
 
-function findComponent(content: ContentItem[], componentId: string): string {
+function findComponent(
+  content: ContentItem[],
+  componentId: string,
+): string | undefined {
   for (const item of content) {
     if ("path" in item && item.path === componentId) {
       return (item as ModulePageDef).modulePath;
@@ -17,27 +20,35 @@ function findComponent(content: ContentItem[], componentId: string): string {
       return findComponent(item.content, componentId);
     }
   }
-  throw Error("Could not find component with id: " + componentId);
+  return undefined;
 }
 
 const ContentComponent = () => {
-  const [modulePath, setModulePath] = useState<string>();
+  const [content, setContent] = useState<ContentItem[]>();
   const { componentId } = useParams<ContentComponentParams>();
 
-  usePromise(
-    (signal) => fetchContentJson({ signal }),
-    (content) => setModulePath(findComponent(content, componentId!)),
+  usePromise((signal) => fetchContentJson({ signal }), setContent);
+  const modulePath = useMemo(
+    () => findComponent(content || [], componentId!),
+    [content, componentId],
   );
 
-  let Element = undefined;
-  if (modulePath) {
-    Element = lazy(() => import(joinPath(environment.resourceUrl, modulePath)));
-  }
   return (
     <Suspense fallback={<Spinner />}>
-      {modulePath && Element && <Element />}
+      {modulePath && <Component modulePath={modulePath} />}
     </Suspense>
   );
+};
+
+type ComponentProps = {
+  modulePath: string;
+};
+
+const Component = ({ modulePath }: ComponentProps) => {
+  const Element = lazy(
+    () => import(joinPath(environment.resourceUrl, modulePath)),
+  );
+  return <Element />;
 };
 
 export default ContentComponent;
