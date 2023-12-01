2fbf7cbfb9ff50a74f2af7553aa655004052e5db
Martin Bartoš <mabartos@redhat.com>
2023-12-01 13:58:36 +0100

    Initialize Infinispan earlier in the build chain (#21171)
    Closes #24860
    
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>
    Signed-off-by: ShefeeqPM <86718986+ShefeeqPM@users.noreply.github.com>

--- quarkus/deployment/src/main/java/org/keycloak/quarkus/deployment/CacheBuildSteps.java
+++ quarkus/deployment/src/main/java/org/keycloak/quarkus/deployment/CacheBuildSteps.java
@@ -43,8 +43,8 @@ import io.quarkus.deployment.builditem.ShutdownContextBuildItem;
 
 public class CacheBuildSteps {
 
-    @Consume(KeycloakSessionFactoryPreInitBuildItem.class)
-    @Record(ExecutionTime.RUNTIME_INIT)
+    @Consume(ConfigBuildItem.class)
+    @Record(ExecutionTime.STATIC_INIT)
     @BuildStep
     void configureInfinispan(KeycloakRecorder recorder, BuildProducer<SyntheticBeanBuildItem> syntheticBeanBuildItems, ShutdownContextBuildItem shutdownContext) {
         String configFile = getConfigValue("kc.spi-connections-infinispan-quarkus-config-file").getValue();
