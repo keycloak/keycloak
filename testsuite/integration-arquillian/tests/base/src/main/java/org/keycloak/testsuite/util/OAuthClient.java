2de6d6c5c41ef5d769871a771bd2a15767488d45
Ricardo Martin <rmartinc@redhat.com>
2023-12-06 13:53:01 +0100

    Escape action in the form_post response mode (#60) Closes keycloak/keycloak-private#31 Closes https://issues.redhat.com/browse/RHBK-652
    Signed-off-by: rmartinc <rmartinc@redhat.com>
    Signed-off-by: ShefeeqPM <86718986+ShefeeqPM@users.noreply.github.com>

--- testsuite/integration-arquillian/tests/base/src/main/java/org/keycloak/testsuite/util/OAuthClient.java
+++ testsuite/integration-arquillian/tests/base/src/main/java/org/keycloak/testsuite/util/OAuthClient.java
@@ -1398,6 +1398,9 @@ public class OAuthClient {
         int index = driver.getCurrentUrl().indexOf('?');
         if (index == -1) {
             index = driver.getCurrentUrl().indexOf('#');
+            if (index == -1) {
+                index = driver.getCurrentUrl().length();
+            }
         }
         return driver.getCurrentUrl().substring(0, index);
     }
