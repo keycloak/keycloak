0c660af0477cac937e807e711089c7698503d053
Niko Köbler <niko@n-k.de>
2023-12-21 23:47:56 +0100

    add the exp claim to the backchannel logout token
    This is now, as of Dec 15th 2023, part of the OIDC Backchannel Logout spec, chapter 2.4.
    
    As of chapter 4, the logout token should have a short expiration time, preferably at most two minutes in the future. So we set the expiration to this time.
    
    resolves #25753
    
    Signed-off-by: Niko Köbler <niko@n-k.de>
    (cherry picked from commit 5e623f42d49d09261b75fe19a4f6e37ab3f7344e)

--- testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/util/LogoutTokenUtil.java
+++ testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/util/LogoutTokenUtil.java
@@ -3,6 +3,7 @@ package org.keycloak.testsuite.util;
 import org.apache.http.entity.ContentType;
 import org.keycloak.OAuth2Constants;
 import org.keycloak.common.util.Base64Url;
+import org.keycloak.common.util.Time;
 import org.keycloak.crypto.JavaAlgorithm;
 import org.keycloak.jose.jws.Algorithm;
 import org.keycloak.jose.jws.JWSHeader;
@@ -16,6 +17,7 @@ import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.Signature;
 import java.security.SignatureException;
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.UUID;
 
@@ -36,6 +38,7 @@ public class LogoutTokenUtil {
         logoutToken.issuer(issuer);
         logoutToken.id(UUID.randomUUID().toString());
         logoutToken.issuedNow();
+        logoutToken.exp(Time.currentTime() + Duration.ofMinutes(2).getSeconds());
         logoutToken.audience(clientId);
 
         String logoutTokenPayloadEncoded = Base64Url.encode(JsonSerialization.writeValueAsBytes(logoutToken));
