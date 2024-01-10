3b31e1e38c9cbdaceacf5a148cc9fbc01e2ef14c
Jon Koops <jonkoops@gmail.com>
2024-01-10 19:37:59 +0100

    Replace `react-error-boundary` with own implementation (#26094)
    Closes #21515
    
    Signed-off-by: Jon Koops <jonkoops@gmail.com>
    Signed-off-by: Stefan Wiedemann <wistefan@googlemail.com>

--- js/apps/admin-ui/src/App.tsx
+++ js/apps/admin-ui/src/App.tsx
@@ -1,6 +1,5 @@
 import { Page } from "@patternfly/react-core";
 import { PropsWithChildren, Suspense } from "react";
-import { ErrorBoundary } from "react-error-boundary";
 import { Outlet } from "react-router-dom";
 import { Help, mainPageContentId } from "ui-shared";
 
@@ -10,6 +9,10 @@ import { AlertProvider } from "./components/alert/Alerts";
 import { PageBreadCrumbs } from "./components/bread-crumb/PageBreadCrumbs";
 import { ErrorRenderer } from "./components/error/ErrorRenderer";
 import { KeycloakSpinner } from "./components/keycloak-spinner/KeycloakSpinner";
+import {
+  ErrorBoundaryFallback,
+  ErrorBoundaryProvider,
+} from "./context/ErrorBoundary";
 import { RealmsProvider } from "./context/RealmsContext";
 import { RecentRealmsProvider } from "./context/RecentRealms";
 import { AccessContextProvider } from "./context/access/Access";
@@ -20,21 +23,23 @@ import { SubGroups } from "./groups/SubGroupsContext";
 import { AuthWall } from "./root/AuthWall";
 
 const AppContexts = ({ children }: PropsWithChildren) => (
-  <RealmsProvider>
-    <RealmContextProvider>
-      <WhoAmIContextProvider>
-        <RecentRealmsProvider>
-          <AccessContextProvider>
-            <Help>
-              <AlertProvider>
-                <SubGroups>{children}</SubGroups>
-              </AlertProvider>
-            </Help>
-          </AccessContextProvider>
-        </RecentRealmsProvider>
-      </WhoAmIContextProvider>
-    </RealmContextProvider>
-  </RealmsProvider>
+  <ErrorBoundaryProvider>
+    <RealmsProvider>
+      <RealmContextProvider>
+        <WhoAmIContextProvider>
+          <RecentRealmsProvider>
+            <AccessContextProvider>
+              <Help>
+                <AlertProvider>
+                  <SubGroups>{children}</SubGroups>
+                </AlertProvider>
+              </Help>
+            </AccessContextProvider>
+          </RecentRealmsProvider>
+        </WhoAmIContextProvider>
+      </RealmContextProvider>
+    </RealmsProvider>
+  </ErrorBoundaryProvider>
 );
 
 export const App = () => {
@@ -47,13 +52,7 @@ export const App = () => {
         breadcrumb={<PageBreadCrumbs />}
         mainContainerId={mainPageContentId}
       >
-        <ErrorBoundary
-          FallbackComponent={ErrorRenderer}
-          onReset={() =>
-            (window.location.href =
-              window.location.origin + window.location.pathname)
-          }
-        >
+        <ErrorBoundaryFallback fallback={ErrorRenderer}>
           <ServerInfoProvider>
             <Suspense fallback={<KeycloakSpinner />}>
               <AuthWall>
@@ -61,7 +60,7 @@ export const App = () => {
               </AuthWall>
             </Suspense>
           </ServerInfoProvider>
-        </ErrorBoundary>
+        </ErrorBoundaryFallback>
       </Page>
     </AppContexts>
   );
