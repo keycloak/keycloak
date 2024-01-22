37790c79561d4a12467f562e383d2cbc89c983d2
Erik Jan de Wit <erikjan.dewit@gmail.com>
2024-01-22 09:19:58 +0100

    Add a way to extend the UI with an Java API (#23772)
    * POC to see how we could extend the UI
    
    This is very crude and there are still open issues that need to be worked out
    
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>
    
    * added saving option
    
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>
    
    * added list and recreate client form
    
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>
    
    * add tab ui
    
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>
    
    * integrate tabs
    
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>
    
    * remove examples
    
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>
    
    * fixed error messages
    
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>
    
    * added Feature for ui customization
    
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>
    
    ---------
    
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

---
+++ server-spi-private/src/main/java/org/keycloak/services/ui/extend/UiPageProvider.java
@@ -0,0 +1,10 @@
+package org.keycloak.services.ui.extend;
+
+import org.keycloak.provider.ConfiguredProvider;
+import org.keycloak.provider.Provider;
+
+import java.util.List;
+
+public interface UiPageProvider extends Provider, ConfiguredProvider {
+
+}
