5bf2d4b6ec4f63c7d554dd620d2996bfb26b01b5
Jon Koops <jonkoops@gmail.com>
2024-01-23 14:04:13 +0100

    Enable PKCE by default for Keycloak JS (#26412)
    Closes #26411
    
    Signed-off-by: Jon Koops <jonkoops@gmail.com>

--- js/libs/keycloak-js/src/keycloak.js
+++ js/libs/keycloak-js/src/keycloak.js
@@ -134,9 +134,11 @@ function Keycloak (config) {
 
             if (initOptions.pkceMethod) {
                 if (initOptions.pkceMethod !== "S256") {
-                    throw 'Invalid value for pkceMethod';
+                    throw new TypeError(`Invalid value for 'pkceMethod', expected 'S256' but got '${initOptions.pkceMethod}'.`);
                 }
                 kc.pkceMethod = initOptions.pkceMethod;
+            } else {
+                kc.pkceMethod = "S256";
             }
 
             if (typeof initOptions.enableLogging === 'boolean') {
@@ -374,19 +376,18 @@ function Keycloak (config) {
     }
 
     function generatePkceChallenge(pkceMethod, codeVerifier) {
-        switch (pkceMethod) {
-            // The use of the "plain" method is considered insecure and therefore not supported.
-            case "S256":
-                // hash codeVerifier, then encode as url-safe base64 without padding
-                var hashBytes = new Uint8Array(sha256.arrayBuffer(codeVerifier));
-                var encodedHash = bytesToBase64(hashBytes)
-                    .replace(/\+/g, '-')
-                    .replace(/\//g, '_')
-                    .replace(/\=/g, '');
-                return encodedHash;
-            default:
-                throw 'Invalid value for pkceMethod';
+        if (pkceMethod !== "S256") {
+            throw new TypeError(`Invalid value for 'pkceMethod', expected 'S256' but got '${pkceMethod}'.`);
         }
+
+        // hash codeVerifier, then encode as url-safe base64 without padding
+        const hashBytes = new Uint8Array(sha256.arrayBuffer(codeVerifier));
+        const encodedHash = bytesToBase64(hashBytes)
+            .replace(/\+/g, '-')
+            .replace(/\//g, '_')
+            .replace(/\=/g, '');
+
+        return encodedHash;
     }
 
     function buildClaimsParameter(requestedAcr){
