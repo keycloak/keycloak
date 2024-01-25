be45f19e7f71ccedc4f0cc233f22a3eca4d9e845
Stefan Wiedemann <stefan.wiedemann@fiware.org>
2024-01-25 07:36:28 +0100

    Create SPI and Provider for Verifiable Credentials Signing #25937 (#26263)
    * implement oid4vci service interfaces
    
    Signed-off-by: Stefan Wiedemann <wistefan@googlemail.com>
    
    * add oid4vc to the disabled features test
    
    Signed-off-by: Stefan Wiedemann <wistefan@googlemail.com>
    
    * fix test and add doc
    
    Signed-off-by: Stefan Wiedemann <wistefan@googlemail.com>
    
    * add the new preview feature
    
    Signed-off-by: Stefan Wiedemann <wistefan@googlemail.com>
    
    * add class-level doc
    
    remove wildcard imports
    
    Signed-off-by: Stefan Wiedemann <wistefan@googlemail.com>
    
    * add license headers
    
    Signed-off-by: Stefan Wiedemann <wistefan@googlemail.com>
    
    * fix year
    
    Signed-off-by: Stefan Wiedemann <wistefan@googlemail.com>
    
    * fix teste
    
    Signed-off-by: Stefan Wiedemann <wistefan@googlemail.com>
    
    * two additional test fixes
    
    Signed-off-by: Stefan Wiedemann <wistefan@googlemail.com>
    
    * make the feature experimental
    
    Signed-off-by: Stefan Wiedemann <wistefan@googlemail.com>
    
    * remove clock
    
    Signed-off-by: Stefan Wiedemann <wistefan@googlemail.com>
    
    * remove usage of var
    
    Signed-off-by: Stefan Wiedemann <wistefan@googlemail.com>
    
    * fix tests
    
    Signed-off-by: Stefan Wiedemann <wistefan@googlemail.com>
    
    ---------
    
    Signed-off-by: Stefan Wiedemann <wistefan@googlemail.com>
    Signed-off-by: ShefeeqPM <86718986+ShefeeqPM@users.noreply.github.com>

---
+++ services/src/main/java/org/keycloak/protocol/oid4vc/issuance/signing/JwtSigningService.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2024 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.keycloak.protocol.oid4vc.issuance.signing;
+
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.protocol.oid4vc.model.VerifiableCredential;
+
+/**
+ * {@link VerifiableCredentialsSigningService} implementing the JWT_VC format. It returns a string, containing the
+ * Signed JWT-Credential
+ * {@see https://identity.foundation/jwt-vc-presentation-profile/}
+ *
+ * @author <a href="https://github.com/wistefan">Stefan Wiedemann</a>
+ */
+public class JwtSigningService extends SigningService<String> {
+
+    public JwtSigningService(KeycloakSession keycloakSession, String keyId, String algorithmType) {
+        super(keycloakSession, keyId, algorithmType);
+    }
+
+    @Override
+    public String signCredential(VerifiableCredential verifiableCredential) {
+
+        throw new UnsupportedOperationException("JWT-VC Credentials Signing is not yet supported.");
+    }
+
+}
