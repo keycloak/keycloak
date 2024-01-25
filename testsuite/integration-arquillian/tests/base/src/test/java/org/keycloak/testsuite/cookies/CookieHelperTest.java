cbfdae5e754bce26311bd9adc392aa495c092235
Stian Thorgersen <stianst@gmail.com>
2024-01-25 06:58:42 +0100

    Remove support for multiple AUTH_SESSION_ID cookies (#26462)
    Closes #26457
    
    Signed-off-by: stianst <stianst@gmail.com>

--- testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/cookies/CookieHelperTest.java
+++ testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/cookies/CookieHelperTest.java
@@ -15,7 +15,6 @@ import org.keycloak.testsuite.client.KeycloakTestingClient;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Set;
 
 public class CookieHelperTest extends AbstractKeycloakTest {
 
@@ -42,8 +41,8 @@ public class CookieHelperTest extends AbstractKeycloakTest {
         filter.setHeader("Cookie", "terms_user=; KC_RESTART=eyJhbGciOiJIUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJhZDUyMjdhMy1iY2ZkLTRjZjAtYTdiNi0zOTk4MzVhMDg1NjYifQ.eyJjaWQiOiJodHRwczovL3Nzby5qYm9zcy5vcmciLCJwdHkiOiJzYW1sIiwicnVyaSI6Imh0dHBzOi8vc3NvLmpib3NzLm9yZy9sb2dpbj9wcm92aWRlcj1SZWRIYXRFeHRlcm5hbFByb3ZpZGVyIiwiYWN0IjoiQVVUSEVOVElDQVRFIiwibm90ZXMiOnsiU0FNTF9SRVFVRVNUX0lEIjoibXBmbXBhYWxkampqa2ZmcG5oYmJoYWdmZmJwam1rbGFqbWVlb2lsaiIsInNhbWxfYmluZGluZyI6InBvc3QifX0.d0QJSOQ6pJGzqcjqDTRwkRpU6fwYeICedL6R9Gqs8CQ; AUTH_SESSION_ID=451ec4be-a0c8-430e-b489-6580f195ccf0; AUTH_SESSION_ID=55000981-8b5e-4c8d-853f-ee4c582c1d0d;AUTH_SESSION_ID=451ec4be-a0c8-430e-b489-6580f195ccf0; AUTH_SESSION_ID=55000981-8b5e-4c8d-853f-ee4c582c1d0d;AUTH_SESSION_ID=451ec4be-a0c8-430e-b489-6580f195ccf0; AUTH_SESSION_ID=55000981-8b5e-4c8d-853f-ee4c582c1d0d4;");
 
         testing.server().run(session -> {
-            Set<String> authSessionIds = CookieHelper.getCookieValues(session, "AUTH_SESSION_ID");
-            Assert.assertEquals(3, authSessionIds.size());
+            String authSessionId = CookieHelper.getCookieValue(session, "AUTH_SESSION_ID");
+            Assert.assertEquals("55000981-8b5e-4c8d-853f-ee4c582c1d0d4", authSessionId);
         });
     }
 
@@ -53,20 +52,12 @@ public class CookieHelperTest extends AbstractKeycloakTest {
 
         testing.server().run(session -> {
             Assert.assertEquals("new", CookieHelper.getCookieValue(session, "MYCOOKIE"));
-
-            Set<String> cookieValues = CookieHelper.getCookieValues(session, "MYCOOKIE");
-            Assert.assertEquals(1, cookieValues.size());
-            Assert.assertEquals("new", cookieValues.iterator().next());
         });
 
         filter.setHeader("Cookie", "MYCOOKIE_LEGACY=legacy");
 
         testing.server().run(session -> {
             Assert.assertEquals("legacy", CookieHelper.getCookieValue(session, "MYCOOKIE"));
-
-            Set<String> cookieValues = CookieHelper.getCookieValues(session, "MYCOOKIE");
-            Assert.assertEquals(1, cookieValues.size());
-            Assert.assertEquals("legacy", cookieValues.iterator().next());
         });
     }
 
