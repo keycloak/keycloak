0fc56f6b02a013dbc5c0707faf872b3d7e798c3c
Jon Koops <jonkoops@gmail.com>
2024-01-25 13:14:34 +0100

    Review feedback
    Signed-off-by: Jon Koops <jonkoops@gmail.com>

--- js/apps/admin-ui/src/components/dynamic/UserProfileAttributeListComponent.tsx
+++ js/apps/admin-ui/src/components/dynamic/UserProfileAttributeListComponent.tsx
@@ -1,57 +1,33 @@
 import type { UserProfileConfig } from "@keycloak/keycloak-admin-client/lib/defs/userProfileMetadata";
 import { FormGroup } from "@patternfly/react-core";
 import { useState } from "react";
-import { Controller, useFormContext } from "react-hook-form";
+import { useFormContext } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 import { HelpItem } from "ui-shared";
-import { convertToName } from "./DynamicComponents";
+
 import { adminClient } from "../../admin-client";
 import { useFetch } from "../../utils/useFetch";
+import { KeySelect } from "../key-value-form/KeySelect";
+import { convertToName } from "./DynamicComponents";
 import type { ComponentProps } from "./components";
-import { KeySelect, FetchCallback } from "../key-value-form/KeySelect";
 
-type FetchCallbackWithValue = FetchCallback & {
-  fieldValue: string;
-};
 export const UserProfileAttributeListComponent = ({
   name,
   label,
   helpText,
-  defaultValue,
   required = false,
 }: ComponentProps) => {
   const { t } = useTranslation();
   const {
-    control,
     formState: { errors },
   } = useFormContext();
 
   const [config, setConfig] = useState<UserProfileConfig>();
   const convertedName = convertToName(name!);
 
-  const [custom, setCustom] = useState(true);
-  const fetchCallback: FetchCallbackWithValue = {
-    fieldValue: "",
-    custom,
-    setCustom,
-  };
-  const updateFetchCallback: (
-    currentValue: string,
-  ) => FetchCallbackWithValue = (currentValue) => {
-    fetchCallback.fieldValue = currentValue;
-    return fetchCallback;
-  };
-
   useFetch(
     () => adminClient.users.getProfile(),
-    (cfg) => {
-      setConfig(cfg);
-
-      const fieldIncluded = cfg.attributes
-        ?.map((option) => option.name!)
-        .includes(fetchCallback.fieldValue);
-      setCustom(!fieldIncluded);
-    },
+    (cfg) => setConfig(cfg),
     [],
   );
 
@@ -64,6 +40,8 @@ export const UserProfileAttributeListComponent = ({
     }));
   };
 
+  if (!config) return null;
+
   return (
     <FormGroup
       label={t(label!)}
@@ -73,19 +51,10 @@ export const UserProfileAttributeListComponent = ({
       validated={errors[convertedName!] ? "error" : "default"}
       helperTextInvalid={t("required")}
     >
-      <Controller
-        name={convertedName!}
-        defaultValue={defaultValue || ""}
-        control={control}
+      <KeySelect
+        name={convertedName}
         rules={required ? { required: true } : {}}
-        render={({ field }) => (
-          <KeySelect
-            name={convertedName}
-            rules={required ? { required: true } : {}}
-            selectItems={convert(config)}
-            fetchCallback={updateFetchCallback(field.value)}
-          />
-        )}
+        selectItems={convert(config)}
       />
     </FormGroup>
   );
--- js/apps/admin-ui/src/components/key-value-form/KeySelect.tsx
+++ js/apps/admin-ui/src/components/key-value-form/KeySelect.tsx
@@ -6,37 +6,18 @@ import { KeycloakTextInput } from "ui-shared";
 import useToggle from "../../utils/useToggle";
 import { DefaultValue } from "./KeyValueInput";
 
-/**
- * Used when the values passed to KeySelect in "selectItems" need to be fetched from the server dynamically. In this case, we might want to call "setCustom"
- * from parent component. The "fieldValue" sent to KeySelect is not supposed to be set by the calling component, as it will be set by KeySelect itself with the value of the field, so
- * it can be consumed from the callback function once the values are fetched from the server
- */
-export type FetchCallback = {
-  custom: boolean;
-  setCustom: any;
-};
-
 type KeySelectProp = UseControllerProps & {
   selectItems: DefaultValue[];
-  fetchCallback?: FetchCallback;
 };
 
-export const KeySelect = ({
-  selectItems,
-  fetchCallback,
-  ...rest
-}: KeySelectProp) => {
+export const KeySelect = ({ selectItems, ...rest }: KeySelectProp) => {
   const { t } = useTranslation();
   const [open, toggle] = useToggle();
   const { field } = useController(rest);
 
-  let [custom, setCustom] = useState(
+  const [custom, setCustom] = useState(
     !selectItems.map(({ key }) => key).includes(field.value),
   );
-
-  if (fetchCallback) {
-    [custom, setCustom] = [fetchCallback.custom, fetchCallback.setCustom];
-  }
 
   return (
     <Grid>
