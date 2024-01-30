fa948f37e079b100f3f1f315e646de3db0d19b25
Stefan Wiedemann <wistefan@googlemail.com>
2024-01-30 18:35:20 +0100

    Issue Verifiable Credentials in jwt_vc format #25941 (#26484)
    closes #25941 
    
    Signed-off-by: Stefan Wiedemann <wistefan@googlemail.com>

--- services/src/main/java/org/keycloak/protocol/oid4vc/issuance/signing/JwtSigningService.java
+++ services/src/main/java/org/keycloak/protocol/oid4vc/issuance/signing/JwtSigningService.java
@@ -17,8 +17,19 @@
 
 package org.keycloak.protocol.oid4vc.issuance.signing;
 
+import org.jboss.logging.Logger;
+import org.keycloak.crypto.KeyWrapper;
+import org.keycloak.crypto.SignatureProvider;
+import org.keycloak.crypto.SignatureSignerContext;
+import org.keycloak.jose.jws.JWSBuilder;
 import org.keycloak.models.KeycloakSession;
+import org.keycloak.protocol.oid4vc.issuance.TimeProvider;
 import org.keycloak.protocol.oid4vc.model.VerifiableCredential;
+import org.keycloak.representations.JsonWebToken;
+
+import java.net.URI;
+import java.util.Optional;
+import java.util.UUID;
 
 /**
  * {@link VerifiableCredentialsSigningService} implementing the JWT_VC format. It returns a string, containing the
@@ -29,14 +40,75 @@ import org.keycloak.protocol.oid4vc.model.VerifiableCredential;
  */
 public class JwtSigningService extends SigningService<String> {
 
-    public JwtSigningService(KeycloakSession keycloakSession, String keyId, String algorithmType) {
+    private static final Logger LOGGER = Logger.getLogger(JwtSigningService.class);
+
+    private static final String ID_TEMPLATE = "urn:uuid:%s";
+    private static final String TOKEN_TYPE = "JWT";
+    private static final String VC_CLAIM_KEY = "vc";
+    private static final String ID_CLAIM_KEY = "id";
+
+
+    private final SignatureSignerContext signatureSignerContext;
+    private final TimeProvider timeProvider;
+    private final String tokenType;
+    protected final String issuerDid;
+
+    public JwtSigningService(KeycloakSession keycloakSession, String keyId, String algorithmType, String tokenType, String issuerDid, TimeProvider timeProvider) {
         super(keycloakSession, keyId, algorithmType);
+        this.issuerDid = issuerDid;
+        this.timeProvider = timeProvider;
+        this.tokenType = tokenType;
+        KeyWrapper signingKey = getKey(keyId, algorithmType);
+        if (signingKey == null) {
+            throw new SigningServiceException(String.format("No key for id %s and algorithm %s available.", keyId, algorithmType));
+        }
+        SignatureProvider signatureProvider = keycloakSession.getProvider(SignatureProvider.class, algorithmType);
+        signatureSignerContext = signatureProvider.signer(signingKey);
+
+        LOGGER.debugf("Successfully initiated the JWT Signing Service with algorithm %s.", algorithmType);
     }
 
     @Override
     public String signCredential(VerifiableCredential verifiableCredential) {
+        LOGGER.debugf("Sign credentials to jwt-vc format.");
 
-        throw new UnsupportedOperationException("JWT-VC Credentials Signing is not yet supported.");
+        // Get the issuance date from the credential. Since nbf is mandatory, we set it to the current time if not
+        // provided
+        long iat = Optional.ofNullable(verifiableCredential.getIssuanceDate())
+                .map(issuanceDate -> issuanceDate.toInstant().getEpochSecond())
+                .orElse((long) timeProvider.currentTimeSeconds());
+
+        // set mandatory fields
+        JsonWebToken jsonWebToken = new JsonWebToken()
+                .issuer(verifiableCredential.getIssuer().toString())
+                .nbf(iat)
+                .id(createCredentialId(verifiableCredential));
+        jsonWebToken.setOtherClaims(VC_CLAIM_KEY, verifiableCredential);
+
+        // expiry is optional
+        Optional.ofNullable(verifiableCredential.getExpirationDate())
+                .ifPresent(d -> jsonWebToken.exp(d.toInstant().getEpochSecond()));
+
+        // subject id should only be set if the credential subject has an id.
+        Optional.ofNullable(
+                        verifiableCredential
+                                .getCredentialSubject()
+                                .getClaims()
+                                .get(ID_CLAIM_KEY))
+                .map(Object::toString)
+                .ifPresent(jsonWebToken::subject);
+
+        return new JWSBuilder()
+                .type(tokenType)
+                .jsonContent(jsonWebToken)
+                .sign(signatureSignerContext);
     }
 
+    // retrieve the credential id from the given VC or generate one.
+    private String createCredentialId(VerifiableCredential verifiableCredential) {
+        return Optional.ofNullable(
+                        verifiableCredential.getId())
+                .orElse(URI.create(String.format(ID_TEMPLATE, UUID.randomUUID())))
+                .toString();
+    }
 }
--- services/src/main/java/org/keycloak/protocol/oid4vc/issuance/signing/JwtSigningServiceProviderFactory.java
+++ services/src/main/java/org/keycloak/protocol/oid4vc/issuance/signing/JwtSigningServiceProviderFactory.java
@@ -21,14 +21,19 @@ import org.keycloak.component.ComponentModel;
 import org.keycloak.component.ComponentValidationException;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
+import org.keycloak.protocol.oid4vc.issuance.OffsetTimeProvider;
+import org.keycloak.protocol.oid4vc.issuance.VCIssuerException;
 import org.keycloak.protocol.oid4vc.model.Format;
 import org.keycloak.provider.ConfigurationValidationHelper;
 import org.keycloak.provider.ProviderConfigProperty;
 
 import java.util.List;
+import java.util.Optional;
+
 
 /**
  * Provider Factory to create {@link  JwtSigningService}s
+ *
  * @author <a href="https://github.com/wistefan">Stefan Wiedemann</a>
  */
 public class JwtSigningServiceProviderFactory implements VCSigningServiceProviderFactory {
@@ -38,10 +43,18 @@ public class JwtSigningServiceProviderFactory implements VCSigningServiceProvide
 
     @Override
     public VerifiableCredentialsSigningService create(KeycloakSession session, ComponentModel model) {
+
         String keyId = model.get(SigningProperties.KEY_ID.getKey());
         String algorithmType = model.get(SigningProperties.ALGORITHM_TYPE.getKey());
+        String tokenType = model.get(SigningProperties.TOKEN_TYPE.getKey());
+        String issuerDid = Optional.ofNullable(
+                        session
+                                .getContext()
+                                .getRealm()
+                                .getAttribute(ISSUER_DID_REALM_ATTRIBUTE_KEY))
+                .orElseThrow(() -> new VCIssuerException("No issuerDid configured."));
 
-        return new JwtSigningService(session, keyId, algorithmType);
+        return new JwtSigningService(session, keyId, algorithmType, tokenType, issuerDid, new OffsetTimeProvider());
     }
 
     @Override
@@ -53,6 +66,7 @@ public class JwtSigningServiceProviderFactory implements VCSigningServiceProvide
     public List<ProviderConfigProperty> getConfigProperties() {
         return VCSigningServiceProviderFactory.configurationBuilder()
                 .property(SigningProperties.ALGORITHM_TYPE.asConfigProperty())
+                .property(SigningProperties.TOKEN_TYPE.asConfigProperty())
                 .build();
     }
 
@@ -64,6 +78,7 @@ public class JwtSigningServiceProviderFactory implements VCSigningServiceProvide
     @Override
     public void validateSpecificConfiguration(KeycloakSession session, RealmModel realm, ComponentModel model) throws ComponentValidationException {
         ConfigurationValidationHelper.check(model)
+                .checkRequired(SigningProperties.TOKEN_TYPE.asConfigProperty())
                 .checkRequired(SigningProperties.ALGORITHM_TYPE.asConfigProperty());
     }
 
