01be4032d84062baa8999098e6a0ae43b7adc586
rmartinc <rmartinc@redhat.com>
2024-01-30 17:51:01 +0100

    Enable verify-profile required action by default Closes #25985
    Signed-off-by: rmartinc <rmartinc@redhat.com>

--- testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/federation/storage/UserStorageTest.java
+++ testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/federation/storage/UserStorageTest.java
@@ -868,7 +868,7 @@ public class UserStorageTest extends AbstractAuthTest {
 
         // Re-create realm
         RealmRepresentation repOrig = testContext.getTestRealmReps().get(0);
-        adminClient.realms().create(repOrig);
+        importRealm(repOrig);
     }
 
     @Test
