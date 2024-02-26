562decde3570aebb9ff7f397859f99da75387ed5
rmartinc <rmartinc@redhat.com>
2024-02-26 08:48:37 +0100

    Perform internal introspect for the access token in the account app Closes #27243
    Signed-off-by: rmartinc <rmartinc@redhat.com>

--- services/src/main/java/org/keycloak/protocol/oidc/AccessTokenIntrospectionProvider.java
+++ services/src/main/java/org/keycloak/protocol/oidc/AccessTokenIntrospectionProvider.java
@@ -61,6 +61,7 @@ public class AccessTokenIntrospectionProvider implements TokenIntrospectionProvi
         this.tokenManager = new TokenManager();
     }
 
+    @Override
     public Response introspect(String token, EventBuilder eventBuilder) {
         AccessToken accessToken = null;
         try {
@@ -123,7 +124,7 @@ public class AccessTokenIntrospectionProvider implements TokenIntrospectionProvi
         }
     }
 
-    private AccessToken transformAccessToken(AccessToken token) {
+    public AccessToken transformAccessToken(AccessToken token) {
         if (token == null) {
             return null;
         }
