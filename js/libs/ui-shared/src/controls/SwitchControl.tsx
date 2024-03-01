baeb5c3efe24b75768da722b4a64ae5068f1f0d8
Jon Koops <jonkoops@gmail.com>
2024-03-01 14:52:20 +0100

    Omit built-in props overwritten on form controls
    Signed-off-by: Jon Koops <jonkoops@gmail.com>

--- js/libs/ui-shared/src/controls/SwitchControl.tsx
+++ js/libs/ui-shared/src/controls/SwitchControl.tsx
@@ -12,7 +12,7 @@ import { FormLabel } from "./FormLabel";
 export type SwitchControlProps<
   T extends FieldValues,
   P extends FieldPath<T> = FieldPath<T>,
-> = SwitchProps &
+> = Omit<SwitchProps, "name"> &
   UseControllerProps<T, P> & {
     name: string;
     label?: string;
