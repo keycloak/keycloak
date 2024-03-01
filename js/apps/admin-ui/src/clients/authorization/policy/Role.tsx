05c977eaa1a700a134093ce0f76fa739024e170d
Pedro Igor <pigor.craveiro@gmail.com>
2024-03-01 11:34:42 -0300

    Allow fetching roles when evaluating role licies
    Closes #20736
    
    Signed-off-by: Pedro Igor <pigor.craveiro@gmail.com>

--- js/apps/admin-ui/src/clients/authorization/policy/Role.tsx
+++ js/apps/admin-ui/src/clients/authorization/policy/Role.tsx
@@ -18,6 +18,7 @@ import { AddRoleMappingModal } from "../../../components/role-mapping/AddRoleMap
 import { Row, ServiceRole } from "../../../components/role-mapping/RoleMapping";
 import { useFetch } from "../../../utils/useFetch";
 import type { RequiredIdValue } from "./ClientScope";
+import { DefaultSwitchControl } from "../../../components/SwitchControl";
 
 export const Role = () => {
   const { t } = useTranslation();
@@ -28,6 +29,7 @@ export const Role = () => {
     formState: { errors },
   } = useFormContext<{
     roles?: RequiredIdValue[];
+    fetchRoles?: boolean;
   }>();
   const values = getValues("roles");
 
@@ -58,109 +60,116 @@ export const Role = () => {
   );
 
   return (
-    <FormGroup
-      label={t("roles")}
-      labelIcon={
-        <HelpItem helpText={t("policyRolesHelp")} fieldLabelId="roles" />
-      }
-      fieldId="roles"
-      helperTextInvalid={t("requiredRoles")}
-      validated={errors.roles ? "error" : "default"}
-      isRequired
-    >
-      <Controller
-        name="roles"
-        control={control}
-        defaultValue={[]}
-        rules={{
-          validate: (value?: RequiredIdValue[]) =>
-            value && value.filter((c) => c.id).length > 0,
-        }}
-        render={({ field }) => (
-          <>
-            {open && (
-              <AddRoleMappingModal
-                id="role"
-                type="roles"
-                onAssign={(rows) => {
-                  field.onChange([
-                    ...(field.value || []),
-                    ...rows.map((row) => ({ id: row.role.id })),
-                  ]);
-                  setSelectedRoles([...selectedRoles, ...rows]);
-                  setOpen(false);
+    <>
+      <FormGroup
+        label={t("roles")}
+        labelIcon={
+          <HelpItem helpText={t("policyRolesHelp")} fieldLabelId="roles" />
+        }
+        fieldId="roles"
+        helperTextInvalid={t("requiredRoles")}
+        validated={errors.roles ? "error" : "default"}
+        isRequired
+      >
+        <Controller
+          name="roles"
+          control={control}
+          defaultValue={[]}
+          rules={{
+            validate: (value?: RequiredIdValue[]) =>
+              value && value.filter((c) => c.id).length > 0,
+          }}
+          render={({ field }) => (
+            <>
+              {open && (
+                <AddRoleMappingModal
+                  id="role"
+                  type="roles"
+                  onAssign={(rows) => {
+                    field.onChange([
+                      ...(field.value || []),
+                      ...rows.map((row) => ({ id: row.role.id })),
+                    ]);
+                    setSelectedRoles([...selectedRoles, ...rows]);
+                    setOpen(false);
+                  }}
+                  onClose={() => {
+                    setOpen(false);
+                  }}
+                  isLDAPmapper
+                />
+              )}
+              <Button
+                data-testid="select-role-button"
+                variant="secondary"
+                onClick={() => {
+                  setOpen(true);
                 }}
-                onClose={() => {
-                  setOpen(false);
-                }}
-                isLDAPmapper
-              />
-            )}
-            <Button
-              data-testid="select-role-button"
-              variant="secondary"
-              onClick={() => {
-                setOpen(true);
-              }}
-            >
-              {t("addRoles")}
-            </Button>
-          </>
-        )}
-      />
-      {selectedRoles.length > 0 && (
-        <TableComposable variant="compact">
-          <Thead>
-            <Tr>
-              <Th>{t("roles")}</Th>
-              <Th>{t("required")}</Th>
-              <Th aria-hidden="true" />
-            </Tr>
-          </Thead>
-          <Tbody>
-            {selectedRoles.map((row, index) => (
-              <Tr key={row.role.id}>
-                <Td>
-                  <ServiceRole role={row.role} client={row.client} />
-                </Td>
-                <Td>
-                  <Controller
-                    name={`roles.${index}.required`}
-                    defaultValue={false}
-                    control={control}
-                    render={({ field }) => (
-                      <Checkbox
-                        id="required"
-                        data-testid="standard"
-                        name="required"
-                        isChecked={field.value}
-                        onChange={field.onChange}
-                      />
-                    )}
-                  />
-                </Td>
-                <Td>
-                  <Button
-                    variant="link"
-                    className="keycloak__client-authorization__policy-row-remove"
-                    icon={<MinusCircleIcon />}
-                    onClick={() => {
-                      setValue("roles", [
-                        ...(values || []).filter((s) => s.id !== row.role.id),
-                      ]);
-                      setSelectedRoles([
-                        ...selectedRoles.filter(
-                          (s) => s.role.id !== row.role.id,
-                        ),
-                      ]);
-                    }}
-                  />
-                </Td>
+              >
+                {t("addRoles")}
+              </Button>
+            </>
+          )}
+        />
+        {selectedRoles.length > 0 && (
+          <TableComposable variant="compact">
+            <Thead>
+              <Tr>
+                <Th>{t("roles")}</Th>
+                <Th>{t("required")}</Th>
+                <Th aria-hidden="true" />
               </Tr>
-            ))}
-          </Tbody>
-        </TableComposable>
-      )}
-    </FormGroup>
+            </Thead>
+            <Tbody>
+              {selectedRoles.map((row, index) => (
+                <Tr key={row.role.id}>
+                  <Td>
+                    <ServiceRole role={row.role} client={row.client} />
+                  </Td>
+                  <Td>
+                    <Controller
+                      name={`roles.${index}.required`}
+                      defaultValue={false}
+                      control={control}
+                      render={({ field }) => (
+                        <Checkbox
+                          id="required"
+                          data-testid="standard"
+                          name="required"
+                          isChecked={field.value}
+                          onChange={field.onChange}
+                        />
+                      )}
+                    />
+                  </Td>
+                  <Td>
+                    <Button
+                      variant="link"
+                      className="keycloak__client-authorization__policy-row-remove"
+                      icon={<MinusCircleIcon />}
+                      onClick={() => {
+                        setValue("roles", [
+                          ...(values || []).filter((s) => s.id !== row.role.id),
+                        ]);
+                        setSelectedRoles([
+                          ...selectedRoles.filter(
+                            (s) => s.role.id !== row.role.id,
+                          ),
+                        ]);
+                      }}
+                    />
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </TableComposable>
+        )}
+      </FormGroup>
+      <DefaultSwitchControl
+        name="fetchRoles"
+        label={t("fetchRoles")}
+        labelIcon={t("fetchRolesHelp")}
+      />
+    </>
   );
 };
