9404b888d1f85184d95110d7019f0b5632222822
Tomas Ondrusko <67582554+tondrusk@users.noreply.github.com>
2024-03-05 10:22:51 +0100

    Update disabled feature status code in social login tests
    Closes #27366
    
    Signed-off-by: Tomas Ondrusko <tondrusk@redhat.com>

--- testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/broker/SocialLoginTest.java
+++ testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/broker/SocialLoginTest.java
@@ -643,7 +643,7 @@ public class SocialLoginTest extends AbstractKeycloakTest {
         Assert.assertEquals(1, users.size());
 
         String username = users.get(0).getUsername();
-        checkFeature(501, username);
+        checkFeature(400, username);
 
         testingClient.enableFeature(Profile.Feature.TOKEN_EXCHANGE);
 
@@ -730,7 +730,7 @@ public class SocialLoginTest extends AbstractKeycloakTest {
         } finally {
             httpClient.close();
             testingClient.disableFeature(Profile.Feature.TOKEN_EXCHANGE);
-            checkFeature(501, username);
+            checkFeature(400, username);
         }
     }
 }
