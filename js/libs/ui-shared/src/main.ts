830e189809b3b57b2b933ba6067c1af85a1561d9
Jon Koops <jonkoops@gmail.com>
2024-03-12 12:44:36 +0100

    Handle errors in Admin and Account through unified utility
    Signed-off-by: Jon Koops <jonkoops@gmail.com>

--- js/libs/ui-shared/src/main.ts
+++ js/libs/ui-shared/src/main.ts
@@ -18,6 +18,7 @@ export { useStoredState } from "./utils/useStoredState";
 export { isDefined } from "./utils/isDefined";
 export { createNamedContext } from "./utils/createNamedContext";
 export { useRequiredContext } from "./utils/useRequiredContext";
+export { getErrorMessage } from "./utils/getErrorMessage";
 export { UserProfileFields } from "./user-profile/UserProfileFields";
 export {
   setUserProfileServerError,
