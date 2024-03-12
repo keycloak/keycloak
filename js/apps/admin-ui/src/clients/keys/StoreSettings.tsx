d2a7e879675a4fe83d92ee4d9713655970f46f1f
Erik Jan de Wit <erikjan.dewit@gmail.com>
2024-03-12 11:32:16 +0100

    changed to use ui-shared (#27776)
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- js/apps/admin-ui/src/clients/keys/StoreSettings.tsx
+++ js/apps/admin-ui/src/clients/keys/StoreSettings.tsx
@@ -1,11 +1,9 @@
-import { useTranslation } from "react-i18next";
-import { useFormContext } from "react-hook-form";
-import { FormGroup } from "@patternfly/react-core";
-
 import type KeyStoreConfig from "@keycloak/keycloak-admin-client/lib/defs/keystoreConfig";
-import { HelpItem } from "ui-shared";
+import { FormGroup } from "@patternfly/react-core";
+import { useFormContext } from "react-hook-form";
+import { useTranslation } from "react-i18next";
+import { HelpItem, TextControl } from "ui-shared";
 import { PasswordInput } from "../../components/password-input/PasswordInput";
-import { KeycloakTextInput } from "../../components/keycloak-text-input/KeycloakTextInput";
 
 export const StoreSettings = ({
   hidePassword = false,
@@ -22,23 +20,14 @@ export const StoreSettings = ({
 
   return (
     <>
-      <FormGroup
+      <TextControl
+        name="keyAlias"
         label={t("keyAlias")}
-        fieldId="keyAlias"
-        isRequired
-        labelIcon={
-          <HelpItem helpText={t("keyAliasHelp")} fieldLabelId="keyAlias" />
-        }
-        helperTextInvalid={t("required")}
-        validated={errors.keyAlias ? "error" : "default"}
-      >
-        <KeycloakTextInput
-          data-testid="keyAlias"
-          id="keyAlias"
-          validated={errors.keyAlias ? "error" : "default"}
-          {...register("keyAlias", { required: true })}
-        />
-      </FormGroup>
+        labelIcon={t("keyAliasHelp")}
+        rules={{
+          required: t("required"),
+        }}
+      />
       {!hidePassword && (
         <FormGroup
           label={t("keyPassword")}
@@ -62,22 +51,11 @@ export const StoreSettings = ({
         </FormGroup>
       )}
       {isSaml && (
-        <FormGroup
+        <TextControl
+          name="realmAlias"
           label={t("realmCertificateAlias")}
-          fieldId="realmCertificateAlias"
-          labelIcon={
-            <HelpItem
-              helpText={t("realmCertificateAliasHelp")}
-              fieldLabelId="realmCertificateAlias"
-            />
-          }
-        >
-          <KeycloakTextInput
-            data-testid="realmCertificateAlias"
-            id="realmCertificateAlias"
-            {...register("realmAlias")}
-          />
-        </FormGroup>
+          labelIcon={t("realmCertificateAliasHelp")}
+        />
       )}
       <FormGroup
         label={t("storePassword")}
