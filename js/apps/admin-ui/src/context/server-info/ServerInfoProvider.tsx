64ed5446b8a77da28bbc438daf8081f7cd774531
Jon Koops <jonkoops@gmail.com>
2024-03-13 13:39:54 +0100

    Load server info before rendering rest of application (#27783)
    Signed-off-by: Jon Koops <jonkoops@gmail.com>
    (cherry picked from commit 7b755e02d3677a444f2597ef236ba382ad98bbe5)

--- js/apps/admin-ui/src/context/server-info/ServerInfoProvider.tsx
+++ js/apps/admin-ui/src/context/server-info/ServerInfoProvider.tsx
@@ -3,6 +3,7 @@ import { PropsWithChildren, useState } from "react";
 import { createNamedContext, useRequiredContext } from "ui-shared";
 
 import { adminClient } from "../../admin-client";
+import { KeycloakSpinner } from "../../components/keycloak-spinner/KeycloakSpinner";
 import { sortProviders } from "../../util";
 import { useFetch } from "../../utils/useFetch";
 
@@ -16,9 +17,13 @@ export const useLoginProviders = () =>
   sortProviders(useServerInfo().providers!["login-protocol"].providers);
 
 export const ServerInfoProvider = ({ children }: PropsWithChildren) => {
-  const [serverInfo, setServerInfo] = useState<ServerInfoRepresentation>({});
+  const [serverInfo, setServerInfo] = useState<ServerInfoRepresentation>();
 
   useFetch(adminClient.serverInfo.find, setServerInfo, []);
+
+  if (!serverInfo) {
+    return <KeycloakSpinner />;
+  }
 
   return (
     <ServerInfoContext.Provider value={serverInfo}>
