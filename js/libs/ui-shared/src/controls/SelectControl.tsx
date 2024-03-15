ea723edbe17816d0f9cd85154f9a0bb04a360eb3
Erik Jan de Wit <erikjan.dewit@gmail.com>
2024-03-15 13:51:08 +0100

    changed to use ui-shared
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- js/libs/ui-shared/src/controls/SelectControl.tsx
+++ js/libs/ui-shared/src/controls/SelectControl.tsx
@@ -1,12 +1,3 @@
-import { useState } from "react";
-import {
-  Controller,
-  ControllerProps,
-  FieldValues,
-  FieldPath,
-  useFormContext,
-  UseControllerProps,
-} from "react-hook-form";
 import {
   Select,
   SelectOption,
@@ -14,12 +5,23 @@ import {
   SelectVariant,
   ValidatedOptions,
 } from "@patternfly/react-core";
+import { useState } from "react";
+import {
+  Controller,
+  ControllerProps,
+  FieldPath,
+  FieldValues,
+  UseControllerProps,
+  useFormContext,
+} from "react-hook-form";
 import { FormLabel } from "./FormLabel";
 
 export type SelectControlOption = {
   key: string;
   value: string;
 };
+
+type OptionType = string[] | SelectControlOption[];
 
 export type SelectControlProps<
   T extends FieldValues,
@@ -37,11 +39,16 @@ export type SelectControlProps<
   UseControllerProps<T, P> & {
     name: string;
     label?: string;
-    options: string[] | SelectControlOption[];
+    options: OptionType;
     labelIcon?: string;
     controller: Omit<ControllerProps, "name" | "render">;
     onFilter?: (value: string) => void;
   };
+
+const isString = (option: SelectControlOption | string): option is string =>
+  typeof option === "string";
+const key = (option: SelectControlOption | string) =>
+  isString(option) ? option : option.key;
 
 export const SelectControl = <
   T extends FieldValues,
@@ -68,19 +75,19 @@ export const SelectControl = <
         option.toString().toLowerCase().startsWith(lowercasePrefix),
       )
       .map((option) => (
-        <SelectOption
-          key={typeof option === "string" ? option : option.key}
-          value={typeof option === "string" ? option : option.key}
-        >
-          {typeof option === "string" ? option : option.value}
+        <SelectOption key={key(option)} value={key(option)}>
+          {isString(option) ? option : option.value}
         </SelectOption>
       ));
   };
+  const isSelectBasedOptions = (
+    options: OptionType,
+  ): options is SelectControlOption[] => typeof options[0] !== "string";
   return (
     <FormLabel
       name={name}
       label={label}
-      isRequired={controller.rules?.required === true}
+      isRequired={!!controller.rules?.required}
       error={errors[name]}
       labelIcon={labelIcon}
     >
@@ -94,8 +101,8 @@ export const SelectControl = <
             toggleId={name.slice(name.lastIndexOf(".") + 1)}
             onToggle={(isOpen) => setOpen(isOpen)}
             selections={
-              typeof options[0] !== "string"
-                ? (options as SelectControlOption[])
+              isSelectBasedOptions(options)
+                ? options
                     .filter((o) =>
                       Array.isArray(value)
                         ? value.includes(o.key)
@@ -104,11 +111,15 @@ export const SelectControl = <
                     .map((o) => o.value)
                 : value
             }
-            onSelect={(_, v) => {
-              if (variant === "typeaheadmulti") {
+            onSelect={(event, v) => {
+              event.stopPropagation();
+              if (Array.isArray(value)) {
                 const option = v.toString();
-                if (value.includes(option)) {
-                  onChange(value.filter((item: string) => item !== option));
+                const key = isSelectBasedOptions(options)
+                  ? options.find((o) => o.value === option)?.key
+                  : option;
+                if (value.includes(key)) {
+                  onChange(value.filter((item: string) => item !== key));
                 } else {
                   onChange([...value, option]);
                 }
