63bdba0c39fdb937f081a2039a174c68d9993db2
Martin Bartoš <mabartos@redhat.com>
2024-03-19 15:48:50 +0100

    Remove deprecated ConfigSourceInterceptor method
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>

--- quarkus/runtime/src/main/java/org/keycloak/quarkus/runtime/configuration/PropertyMappingInterceptor.java
+++ quarkus/runtime/src/main/java/org/keycloak/quarkus/runtime/configuration/PropertyMappingInterceptor.java
@@ -29,7 +29,6 @@ import org.keycloak.quarkus.runtime.configuration.mappers.PropertyMapper;
 import org.keycloak.quarkus.runtime.configuration.mappers.PropertyMappers;
 
 import java.util.Iterator;
-import java.util.function.Function;
 
 import static org.keycloak.quarkus.runtime.Environment.isRebuild;
 
@@ -60,14 +59,14 @@ public class PropertyMappingInterceptor implements ConfigSourceInterceptor {
     public static void enable() {
         disable.remove();
     }
-    
-    <T> Iterator<T> filterRuntime(Iterator<T> iter, Function<T, String> nameFunc) {
+
+    static Iterator<String> filterRuntime(Iterator<String> iter) {
         if (!isRebuild() && !Environment.isRebuildCheck()) {
             return iter;
         }
-        return new FilterIterator<>(iter, item -> !isRuntime(nameFunc.apply(item)));
+        return new FilterIterator<>(iter, item -> !isRuntime(item));
     }
-    
+
     static boolean isRuntime(String name) {
         PropertyMapper<?> mapper = PropertyMappers.getMapper(name);
         return mapper != null && mapper.isRunTime();
@@ -75,12 +74,7 @@ public class PropertyMappingInterceptor implements ConfigSourceInterceptor {
 
     @Override
     public Iterator<String> iterateNames(ConfigSourceInterceptorContext context) {
-        return filterRuntime(context.iterateNames(), Function.identity());
-    }
-
-    @Override
-    public Iterator<ConfigValue> iterateValues(ConfigSourceInterceptorContext context) {
-        return filterRuntime(context.iterateValues(), ConfigValue::getName);
+        return filterRuntime(context.iterateNames());
     }
 
     @Override
