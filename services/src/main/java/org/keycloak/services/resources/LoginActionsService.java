ab3b30f6245c562390fb1d6abc4207232796a37e
Giuseppe Graziano <g.graziano94@gmail.com>
2024-03-23 15:14:10 +0100

    Avoid the same userSessionId after re-authentication (#138)
    Closes #69
    
    Signed-off-by: Giuseppe Graziano <g.graziano94@gmail.com>

--- services/src/main/java/org/keycloak/services/resources/LoginActionsService.java
+++ services/src/main/java/org/keycloak/services/resources/LoginActionsService.java
@@ -240,11 +240,12 @@ public class LoginActionsService {
         if (userSession != null) {
             logger.debugf("Logout of user session %s when restarting flow during re-authentication", userSession.getId());
             AuthenticationManager.backchannelLogout(session, userSession, false);
+            authSession = AuthenticationProcessor.recreate(session, authSession);
         }
 
         AuthenticationProcessor.resetFlow(authSession, flowPath);
 
-        URI redirectUri = getLastExecutionUrl(flowPath, null, authSession.getClient().getClientId(), tabId);
+        URI redirectUri = getLastExecutionUrl(flowPath, null, authSession.getClient().getClientId(), authSession.getTabId());
         logger.debugf("Flow restart requested. Redirecting to %s", redirectUri);
         return Response.status(Response.Status.FOUND).location(redirectUri).build();
     }
