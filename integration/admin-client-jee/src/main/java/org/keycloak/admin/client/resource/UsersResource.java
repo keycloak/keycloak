4a5831851562f161510abb068a7cc986659c3aa5
MNaaz <feminity2001@yahoo.com>
2024-04-02 14:48:56 +0200

    Support for searching users based on search filter, enabled attribute, first, max Closes #27241
    Signed-off-by: MNaaz <feminity2001@yahoo.com>

--- integration/admin-client-jee/src/main/java/org/keycloak/admin/client/resource/UsersResource.java
+++ integration/admin-client-jee/src/main/java/org/keycloak/admin/client/resource/UsersResource.java
@@ -213,6 +213,27 @@ public interface UsersResource {
                                     @QueryParam("max") Integer maxResults,
                                     @QueryParam("briefRepresentation") Boolean briefRepresentation);
 
+    /**
+     * Search for users whose username, first or last name or email matches the value provided by {@code search}. The {@code search}
+     * argument also allows finding users by specific attributes as follows:
+     *
+     * <ul>
+     *     <li><i>id:</i> - Find users by identifier. For instance, <i>id:aa497859-bbf5-44ac-bf1a-74dbffcaf197</i></li>
+     * </ul>
+     *
+     * @param search the value to search. It can be the username, email or any of the supported options to query based on user attributes
+     * @param enabled if true, only users that are enabled are returned
+     * @param firstResult the position of the first result to retrieve
+     * @param maxResults the maximum number of results to retrieve
+     * @return a list of {@link UserRepresentation}
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    List<UserRepresentation> search(@QueryParam("search") String search,
+      @QueryParam("enabled") Boolean enabled,
+      @QueryParam("first") Integer firstResult,
+      @QueryParam("max") Integer maxResults);
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     List<UserRepresentation> list(@QueryParam("first") Integer firstResult,
