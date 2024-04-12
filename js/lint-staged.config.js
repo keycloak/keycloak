13aabf402244e972412e0467cbc168cbda00a8ae
Jon Koops <jonkoops@gmail.com>
2024-04-12 10:34:18 +0200

    Move ESLint configuration to project root (#28639)
    Signed-off-by: Jon Koops <jonkoops@gmail.com>

--- js/lint-staged.config.js
+++
@@ -1,8 +0,0 @@
-import path from "node:path";
-
-const CONFIG_PATH = path.resolve(import.meta.dirname, "eslint.config.js");
-
-export default {
-  "*.{js,jsx,mjs,ts,tsx}": (filenames) =>
-    `eslint --config ${CONFIG_PATH} --cache --fix ${filenames.join(" ")}`,
-};
