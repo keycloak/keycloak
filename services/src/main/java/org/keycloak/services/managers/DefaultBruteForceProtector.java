3500618ee24e0f355711075d55555cbc843583ce
Douglas Palmer <dpalmer@redhat.com>
2024-04-16 12:05:22 -0700

    Failure reset time is applied to Permanent Lockout
    Closes #28821
    
    Signed-off-by: Douglas Palmer <dpalmer@redhat.com>

--- services/src/main/java/org/keycloak/services/managers/DefaultBruteForceProtector.java
+++ services/src/main/java/org/keycloak/services/managers/DefaultBruteForceProtector.java
@@ -168,7 +168,7 @@ public class DefaultBruteForceProtector implements Runnable, BruteForceProtector
         }
         userLoginFailure.setLastFailure(currentTime);
 
-        if (deltaTime > 0) {
+        if (!(realm.isPermanentLockout() && realm.getMaxTemporaryLockouts() == 0) && deltaTime > 0) {
             // if last failure was more than MAX_DELTA clear failures
             if (deltaTime > (long) realm.getMaxDeltaTimeSeconds() * 1000L) {
                 userLoginFailure.clearFailures();
