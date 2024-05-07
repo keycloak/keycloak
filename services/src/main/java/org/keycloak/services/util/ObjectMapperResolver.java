fbb9df41d8ca7df2bd9ab20d49644d23af09545b
Thomas Darimont <thomas.darimont@googlemail.com>
2024-05-07 16:38:33 +0200

    Revise ObjectMapper construction (#16295)
    Previously an ObjectMapper was created multiple times during startup:
    two times during bootstrap and one additional time for the first request sent to Keycloak.
    Additionally jackson modules, e.g. support for JSR310 java.time types
    were not registered event-though they are present on the classpath.
    
    This PR revises the initialization of the ObjectMapper.
    
    - Ensure ObjectMapper is only initialized once
    - Ensure that jackson modules on the classpath are properly
    
    Fixes #16295
    
    Signed-off-by: Thomas Darimont <thomas.darimont@googlemail.com>

--- services/src/main/java/org/keycloak/services/util/ObjectMapperResolver.java
+++ services/src/main/java/org/keycloak/services/util/ObjectMapperResolver.java
@@ -40,7 +40,7 @@ public class ObjectMapperResolver implements ContextResolver<ObjectMapper> {
     protected ObjectMapper mapper;
 
     public ObjectMapperResolver() {
-        mapper = createStreamSerializer();
+        mapper = ObjectMapperInitializer.OBJECT_MAPPER;
     }
 
     public static ObjectMapper createStreamSerializer() {
@@ -57,11 +57,21 @@ public class ObjectMapperResolver implements ContextResolver<ObjectMapper> {
             mapper.enable(SerializationFeature.INDENT_OUTPUT);
         }
 
+        // allow to discover jackson mappers on the classpath
+        if (Boolean.parseBoolean(System.getProperty("keycloak.jsonEnableJacksonModuleDiscovery", "true"))) {
+            mapper.findAndRegisterModules();
+        }
+
         return mapper;
     }
 
     @Override
     public ObjectMapper getContext(Class<?> type) {
         return mapper;
+    }
+
+    private static class ObjectMapperInitializer {
+
+        private static final ObjectMapper OBJECT_MAPPER = createStreamSerializer();
     }
 }
