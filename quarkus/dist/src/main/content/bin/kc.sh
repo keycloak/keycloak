71c545b067e87f17ba9af4027743680738912678
Kamesh Akella <kakella@redhat.com>
2024-05-07 15:35:08 -0400

    set maxheapfreeratio to 40 to be higher than the default minheapfreeratio

--- quarkus/dist/src/main/content/bin/kc.sh
+++ quarkus/dist/src/main/content/bin/kc.sh
@@ -100,7 +100,7 @@ if [ -z "$JAVA_OPTS" ]; then
    JAVA_OPTS="-XX:MetaspaceSize=96M -XX:MaxMetaspaceSize=256m -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.err.encoding=UTF-8 -Dstdout.encoding=UTF-8 -Dstderr.encoding=UTF-8 -XX:+ExitOnOutOfMemoryError -Djava.security.egd=file:/dev/urandom -XX:+UseG1GC -XX:GCTimeRatio=4 -XX:AdaptiveSizePolicyWeight=90 -XX:FlightRecorderOptions=stackdepth=512"
 
    if [ -z "$JAVA_OPTS_KC_HEAP" ]; then
-      JAVA_OPTS_KC_HEAP="-XX:MaxHeapFreeRatio=20"
+      JAVA_OPTS_KC_HEAP="-XX:MaxHeapFreeRatio=40"
       if [ "$KC_RUN_IN_CONTAINER" = "true" ]; then
          # Maximum utilization of the heap is set to 70% of the total container memory
          # Initial heap size is set to 50% of the total container memory in order to reduce GC executions
