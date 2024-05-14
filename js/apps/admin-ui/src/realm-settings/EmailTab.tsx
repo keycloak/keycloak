4b6d0fb65160e02e2c6c3bb44502cec1fcab0562
Erik Jan de Wit <erikjan.dewit@gmail.com>
2024-05-14 16:05:58 +0200

    fix smtp authenticate issue (#29508)
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- js/apps/admin-ui/src/realm-settings/EmailTab.tsx
+++ js/apps/admin-ui/src/realm-settings/EmailTab.tsx
@@ -207,6 +207,7 @@ export const RealmSettingsEmailTab = ({
               defaultValue=""
               labelOn={t("enabled")}
               labelOff={t("disabled")}
+              stringify
             />
             {authenticationEnabled === "true" && (
               <>
