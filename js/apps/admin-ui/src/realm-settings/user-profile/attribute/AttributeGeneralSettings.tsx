118d918c85782d1cabef45f81e39fba98d04126e
Erik Jan de Wit <erikjan.dewit@gmail.com>
2024-05-15 08:26:24 +0200

    fixed lint error
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- js/apps/admin-ui/src/realm-settings/user-profile/attribute/AttributeGeneralSettings.tsx
+++ js/apps/admin-ui/src/realm-settings/user-profile/attribute/AttributeGeneralSettings.tsx
@@ -263,7 +263,7 @@ export const AttributeGeneralSettings = ({
                     aria-label={t("addAttributeTranslationBtn")}
                     isDisabled={!newAttributeName && !editMode}
                     onClick={() => {
-                    setType("displayName");
+                      setType("displayName");
                       toggleModal();
                     }}
                     icon={<GlobeRouteIcon />}
