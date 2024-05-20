8c178515111050bc1edbc3254db55ac79f5e9a6a
Andre F de M <trixpan@users.noreply.github.com>
2024-05-20 20:31:51 +1000

    re-add run-fips-ut.sh with comments
    Signed-off-by: Andre F de M <trixpan@users.noreply.github.com>

--- .github/scripts/run-fips-ut.sh
+++ .github/scripts/run-fips-ut.sh
@@ -16,13 +16,15 @@ export JAVA_HOME=/etc/alternatives/java_sdk_17
 # Build all dependent modules
 ./mvnw install -nsu -B -am -pl crypto/default,crypto/fips1402 -DskipTests
 
+./mvnw test -nsu -B -pl crypto/default,crypto/fips1402 -Dcom.redhat.fips=true
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
 # NOTE the use of  "org.bouncycastle.rsa.allow_pkcs15_enc" as per BCFIPS release notes:
 #
 # End of 2023 transition for RSA PKCS1.5 encryption. The provider blocks RSA with PKCS1.5 encryption.
 # The following property can be used to override the default behavior:
 # org.bouncycastle.rsa.allow_pkcs15_enc (allow use of PKCS1.5)
-./mvnw test -nsu -B -pl crypto/default,crypto/fips1402 -Dcom.redhat.fips=true
-if [ $? -ne 0 ]; then
-  exit 1
-fi
-./mvnw test -nsu -B -pl crypto/default,crypto/fips1402 -Dcom.redhat.fips=true -Dorg.bouncycastle.fips.approved_only=true
+# This is required by crypto/fips1402/src/test/java/org/keycloak/crypto/fips/test/FIPS1402JWETest.java
+./mvnw test -nsu -B -pl crypto/default,crypto/fips1402 -Dcom.redhat.fips=true -Dorg.bouncycastle.fips.approved_only=true -Dorg.bouncycastle.rsa.allow_pkcs15_enc=true
