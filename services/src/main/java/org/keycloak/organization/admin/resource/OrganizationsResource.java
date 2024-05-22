2304fe52aa7fcc69ad712c4f0fb257fdc25cd56d
Pedro Igor <pigor.craveiro@gmail.com>
2024-05-22 15:04:22 -0300

    Handling model validation errors when creating organizations
    Signed-off-by: Pedro Igor <pigor.craveiro@gmail.com>
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- services/src/main/java/org/keycloak/organization/admin/resource/OrganizationsResource.java
+++ services/src/main/java/org/keycloak/organization/admin/resource/OrganizationsResource.java
@@ -41,6 +41,7 @@ import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.jboss.resteasy.reactive.NoCache;
 import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.ModelValidationException;
 import org.keycloak.models.OrganizationModel;
 import org.keycloak.organization.OrganizationProvider;
 import org.keycloak.organization.utils.Organizations;
@@ -95,12 +96,17 @@ public class OrganizationsResource {
         Set<String> domains = ofNullable(organization.getDomains()).orElse(Set.of()).stream()
                 .map(OrganizationDomainRepresentation::getName)
                 .filter(StringUtil::isNotBlank)
+
+        try {
                 .collect(Collectors.toSet());
-        OrganizationModel model = provider.create(organization.getName(), domains);
+            OrganizationModel model = provider.create(organization.getName(), domains);
 
-        Organizations.toModel(organization, model);
+            Organizations.toModel(organization, model);
 
-        return Response.created(session.getContext().getUri().getAbsolutePathBuilder().path(model.getId()).build()).build();
+            return Response.created(session.getContext().getUri().getAbsolutePathBuilder().path(model.getId()).build()).build();
+        } catch (ModelValidationException mve) {
+            throw ErrorResponse.error(mve.getMessage(), Response.Status.BAD_REQUEST);
+        }
     }
 
     /**
