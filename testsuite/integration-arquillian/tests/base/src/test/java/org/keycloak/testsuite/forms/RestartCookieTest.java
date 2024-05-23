54c91e38a89fbe048e7a45cfb5db1885c27530ad
rmartinc <rmartinc@redhat.com>
2024-05-23 09:42:44 +0200

    Generate RESTART_AUTHENTICATION event on success Closes #29385
    Signed-off-by: rmartinc <rmartinc@redhat.com>
    (cherry picked from commit b258b459d72b11411fffb833355b36963b0a47ff)

--- testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/forms/RestartCookieTest.java
+++ testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/forms/RestartCookieTest.java
@@ -192,7 +192,6 @@ public class RestartCookieTest extends AbstractTestRealmKeycloakTest {
 
         events.expectLogin().user((String) null).session((String) null).error(Errors.EXPIRED_CODE).clearDetails()
                 .detail(Details.RESTART_AFTER_TIMEOUT, "true")
-                .client((String) null)
                 .assertEvent();
     }
 
@@ -232,7 +231,6 @@ public class RestartCookieTest extends AbstractTestRealmKeycloakTest {
 
         events.expectLogin().user((String) null).session((String) null).error(Errors.EXPIRED_CODE).clearDetails()
                 .detail(Details.RESTART_AFTER_TIMEOUT, "true")
-                .client((String) null)
                 .assertEvent();
     }
 }
