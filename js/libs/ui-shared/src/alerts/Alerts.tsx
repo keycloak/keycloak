7c6792d9fcb57426099275856753efbfd349cf91
Martin Bartoš <mabartos@redhat.com>
2024-05-31 12:58:59 +0200

    Test ui-shared
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>

--- js/libs/ui-shared/src/alerts/Alerts.tsx
+++ js/libs/ui-shared/src/alerts/Alerts.tsx
@@ -5,7 +5,7 @@ import {
   AlertVariant,
 } from "@patternfly/react-core";
 import { createContext, PropsWithChildren, useContext, useState } from "react";
-
+// test
 export type AddAlertFunction = (
   message: string,
   variant?: AlertVariant,
