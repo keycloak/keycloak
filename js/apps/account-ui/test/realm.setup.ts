d502c9a243ddc6a9590e3f5ffee9a85d28ec9380
Hynek Mlnarik <hmlnarik@redhat.com>
2024-06-03 11:08:10 +0200

    Fix linting issues
    Signed-off-by: Hynek Mlnarik <hmlnarik@redhat.com>

--- js/apps/account-ui/test/realm.setup.ts
+++ js/apps/account-ui/test/realm.setup.ts
@@ -3,9 +3,12 @@ import { test as setup } from "@playwright/test";
 
 import { importRealm } from "./admin-client";
 import groupsRealm from "./realms/groups-realm.json" assert { type: "json" };
-import resourcesRealm from "./realms/resources-realm.json" assert { type: "json" };
-import userProfileRealm from "./realms/user-profile-realm.json" assert { type: "json" };
-import verifiableCredentialsRealm from "./realms/verifiable-credentials-realm.json" assert { type: "json" };
+import resourcesRealm from "./realms/resources-realm.json" assert { type:
+  "json" };
+import userProfileRealm from "./realms/user-profile-realm.json" assert { type:
+  "json" };
+import verifiableCredentialsRealm from "./realms/verifiable-credentials-realm.json" assert { type:
+  "json" };
 
 setup("import realm", async () => {
   await importRealm(groupsRealm as RealmRepresentation);
