51bf3471c56874431505f0d1f282f1cd2967a8f4
Jon Koops <jonkoops@gmail.com>
2024-06-05 15:01:24 +0200

    Enable user when removing temporary lock  (#29618) (#29737)
    Closes #29336
    
    Signed-off-by: Himanshi Gupta <higupta@redhat.com>
    (cherry picked from commit 95d883c76f9eb495cbb4059a3abf72a7fda2aed5)

--- js/apps/admin-ui/src/user/UserForm.tsx
+++ js/apps/admin-ui/src/user/UserForm.tsx
@@ -337,6 +337,9 @@ export const UserForm = ({
             onChange={(value) => {
               unLockUser();
               setLocked(value);
+              save({
+                enabled: !value,
+              });
             }}
             isChecked={locked}
             isDisabled={!locked}
