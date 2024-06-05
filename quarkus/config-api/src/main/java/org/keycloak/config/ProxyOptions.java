338f0c579f5259fadfede21408059253815406f8
Michael Warnecke <WarneckeMichael@web.de>
2024-06-05 16:37:10 +0200

    Enhance documentation regarding edge termination
    Closes #29665
    
    Signed-off-by: Michael Warnecke <WarneckeMichael@web.de>

--- quarkus/config-api/src/main/java/org/keycloak/config/ProxyOptions.java
+++ quarkus/config-api/src/main/java/org/keycloak/config/ProxyOptions.java
@@ -39,7 +39,7 @@ public class ProxyOptions {
             .category(OptionCategory.PROXY)
             .description("The proxy address forwarding mode if the server is behind a reverse proxy.")
             .defaultValue(Mode.none)
-            .deprecated(Set.of(PROXY_HEADERS.getKey()))
+            .deprecated(Set.of(PROXY_HEADERS.getKey(), HttpOptions.HTTP_ENABLED.getKey()))
             .build();
 
     public static final Option<Boolean> PROXY_FORWARDED_HOST = new OptionBuilder<>("proxy-forwarded-host", Boolean.class)
