d784442f632992b4de55af06b3a8b9df4fe30227
Erik Jan de Wit <erikjan.dewit@gmail.com>
2024-06-08 11:06:28 +0200

    make admin-ui build as a library
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- js/apps/admin-ui/vite.config.ts
+++ js/apps/admin-ui/vite.config.ts
@@ -1,34 +1,57 @@
-import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig, loadEnv } from "vite";
 import { checker } from "vite-plugin-checker";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "",
-  server: {
-    origin: "http://localhost:5174",
-    port: 5174,
-  },
-  build: {
-    outDir: "target/classes/theme/keycloak.v2/admin/resources",
-    sourcemap: true,
-    target: "esnext",
-    modulePreload: false,
-    cssMinify: "lightningcss",
-    manifest: true,
-    rollupOptions: {
-      input: "src/main.tsx",
-      external: ["react", "react/jsx-runtime", "react-dom"],
-    },
-  },
-  plugins: [react(), checker({ typescript: true })],
-  test: {
-    watch: false,
-    environment: "jsdom",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const external = ["react", "react/jsx-runtime", "react-dom"];
+  const plugins = [react(), checker({ typescript: true })];
+  if (env.LIB) {
+    external.push("react-router-dom");
+    external.push("react-i18next");
+    plugins.push(dts({ insertTypesEntry: true }));
+  }
+  const lib = env.LIB
+    ? {
+        outDir: "lib",
+        lib: {
+          entry: path.resolve(__dirname, "src/index.ts"),
+          formats: ["es"],
+        },
+      }
+    : {
+        outDir: "target/classes/theme/keycloak.v2/admin/resources",
+      };
+  return {
+    base: "",
     server: {
-      deps: {
-        inline: [/@patternfly\/.*/],
+      origin: "http://localhost:5174",
+      port: 5174,
+    },
+    build: {
+      ...lib,
+      sourcemap: true,
+      target: "esnext",
+      modulePreload: false,
+      cssMinify: "lightningcss",
+      manifest: true,
+      rollupOptions: {
+        input: "src/main.tsx",
+        external,
       },
     },
-  },
+    plugins,
+    test: {
+      watch: false,
+      environment: "jsdom",
+      server: {
+        deps: {
+          inline: [/@patternfly\/.*/],
+        },
+      },
+    },
+  };
 });
