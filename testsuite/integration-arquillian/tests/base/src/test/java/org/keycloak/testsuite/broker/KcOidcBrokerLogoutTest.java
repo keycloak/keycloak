8d6f9ab153942d5cc86dc4682e9cb490b0bc2a63
rmartinc <rmartinc@redhat.com>
2024-06-10 11:20:40 +0200

    Logout from all clients after IdP logout is performed Closes #25234
    Signed-off-by: rmartinc <rmartinc@redhat.com>
    (cherry picked from commit 7d05a7a013495a8c59c3bdc71a04f743d3391b34)

--- testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/broker/KcOidcBrokerLogoutTest.java
+++ testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/broker/KcOidcBrokerLogoutTest.java
@@ -1,22 +1,32 @@
 package org.keycloak.testsuite.broker;
 
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.keycloak.OAuth2Constants;
 import org.keycloak.TokenVerifier;
+import org.keycloak.admin.client.resource.IdentityProviderResource;
+import org.keycloak.admin.client.resource.RealmResource;
 import org.keycloak.common.VerificationException;
+import org.keycloak.protocol.oidc.OIDCConfigAttributes;
 import org.keycloak.representations.IDToken;
+import org.keycloak.representations.idm.IdentityProviderRepresentation;
 import org.keycloak.services.managers.AuthenticationManager;
 import org.keycloak.services.util.CookieHelper;
 import org.keycloak.testsuite.AssertEvents;
+import org.keycloak.testsuite.updaters.ClientAttributeUpdater;
 import org.keycloak.testsuite.util.AccountHelper;
 import org.keycloak.testsuite.util.OAuthClient;
+import org.keycloak.testsuite.util.WaitUtils;
 
 import static org.junit.Assert.assertEquals;
 import static org.keycloak.testsuite.broker.BrokerTestConstants.REALM_CONS_NAME;
 import static org.keycloak.testsuite.broker.BrokerTestConstants.REALM_PROV_NAME;
 import static org.keycloak.testsuite.broker.BrokerTestTools.getConsumerRoot;
 import static org.keycloak.testsuite.broker.BrokerTestTools.waitForPage;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class KcOidcBrokerLogoutTest extends AbstractKcOidcBrokerLogoutTest {
 
@@ -139,5 +149,48 @@ public class KcOidcBrokerLogoutTest extends AbstractKcOidcBrokerLogoutTest {
         loginPage.open(REALM_PROV_NAME);
 
         waitForPage(driver, "sign in to provider", true);
+    }
+
+    @Test
+    public void testFrontChannelLogoutRequestsSendingOnlyClientIdWithFrontChannelLogoutApp() throws Exception {
+        RealmResource realm = adminClient.realm(bc.consumerRealmName());
+        IdentityProviderResource identityProviderResource = realm.identityProviders().get(bc.getIDPAlias());
+        IdentityProviderRepresentation representation = identityProviderResource.toRepresentation();
+        Map<String, String> config = representation.getConfig();
+        Map<String, String> originalConfig = new HashMap<>(config);
+
+        try (ClientAttributeUpdater clientUpdater = ClientAttributeUpdater.forClient(adminClient, bc.consumerRealmName(), "broker-app")
+                .setFrontchannelLogout(true)
+                .setAttribute(OIDCConfigAttributes.FRONT_CHANNEL_LOGOUT_URI, getConsumerRoot() + "/auth/realms/" + bc.consumerRealmName() + "/app/logout")
+                .update()){
+            config.put("backchannelSupported", Boolean.FALSE.toString());
+            identityProviderResource.update(representation);
+            logInAsUserInIDPForFirstTime();
+            appPage.assertCurrent();
+            executeLogoutFromRealm(
+                    getConsumerRoot(),
+                    bc.consumerRealmName(),
+                    "something-else",
+                    null,
+                    "broker-app",
+                    null
+            );
+            logoutConfirmPage.isCurrent();
+            // confirm logout at consumer
+            logoutConfirmPage.confirmLogout();
+
+            WaitUtils.waitForPageToLoad();
+            logoutConfirmPage.isCurrent();
+            Assert.assertTrue(driver.getPageSource().contains("You are logging out from following apps"));
+            Assert.assertTrue(driver.getPageSource().contains("broker-app"));
+
+            oauth.clientId("account");
+            oauth.redirectUri(getConsumerRoot() + "/auth/realms/" + REALM_PROV_NAME + "/account");
+            loginPage.open(REALM_PROV_NAME);
+            waitForPage(driver, "sign in to provider", true);
+        } finally {
+            representation.setConfig(originalConfig);
+            identityProviderResource.update(representation);
+        }
     }
 }
