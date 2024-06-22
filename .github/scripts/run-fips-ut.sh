4f9c3d84a161a0a38b3f2a09b0aa243fbb311a1b
rmartinc <rmartinc@redhat.com>
2024-06-22 11:07:42 +0200

    Upgrade bc-fips to 1.0.2.5 Closes #26568 Closes #27884
    Signed-off-by: rmartinc <rmartinc@redhat.com>

--- .github/scripts/run-fips-ut.sh
+++ .github/scripts/run-fips-ut.sh
@@ -15,11 +15,4 @@ export JAVA_HOME=/etc/alternatives/java_sdk_21
 if [ $? -ne 0 ]; then
   exit 1
 fi
-
-# NOTE the use of  "org.bouncycastle.rsa.allow_pkcs15_enc" as per BCFIPS release notes:
-#
-# End of 2023 transition for RSA PKCS1.5 encryption. The provider blocks RSA with PKCS1.5 encryption.
-# The following property can be used to override the default behavior:
-# org.bouncycastle.rsa.allow_pkcs15_enc (allow use of PKCS1.5)
-# This is required by crypto/fips1402/src/test/java/org/keycloak/crypto/fips/test/FIPS1402JWETest.java
-./mvnw test -nsu -B -pl crypto/default,crypto/fips1402 -Dcom.redhat.fips=true -Dorg.bouncycastle.fips.approved_only=true -Dorg.bouncycastle.rsa.allow_pkcs15_enc=true
+./mvnw test -nsu -B -pl crypto/default,crypto/fips1402 -Dcom.redhat.fips=true -Dorg.bouncycastle.fips.approved_only=true
