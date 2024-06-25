af6abfd0c201d300ec5abd7dc15b3006ce19f114
Francis Pouatcha <francis.pouatcha@adorsys.com>
2024-06-25 09:46:49 +0100

    Fix(test): set code request param grant ty urn:ietf:params:oauth:grant-type:pre-authorized_code to pre-authorized_code instead of code as per spec.
    Signed-off-by: Francis Pouatcha <francis.pouatcha@adorsys.com>

--- testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/oid4vc/issuance/signing/OID4VCIssuerEndpointTest.java
+++ testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/oid4vc/issuance/signing/OID4VCIssuerEndpointTest.java
@@ -432,7 +432,7 @@ public class OID4VCIssuerEndpointTest extends OID4VCTest {
         HttpPost postPreAuthorizedCode = new HttpPost(openidConfig.getTokenEndpoint());
         List<NameValuePair> parameters = new LinkedList<>();
         parameters.add(new BasicNameValuePair(OAuth2Constants.GRANT_TYPE, PreAuthorizedCodeGrantTypeFactory.GRANT_TYPE));
-        parameters.add(new BasicNameValuePair("code", credentialsOffer.getGrants().getPreAuthorizedCode().getPreAuthorizedCode()));
+        parameters.add(new BasicNameValuePair("pre-authorized_code", credentialsOffer.getGrants().getPreAuthorizedCode().getPreAuthorizedCode()));
         UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(parameters, "UTF-8");
         postPreAuthorizedCode.setEntity(formEntity);
         OAuthClient.AccessTokenResponse accessTokenResponse = new OAuthClient.AccessTokenResponse(httpClient.execute(postPreAuthorizedCode));
