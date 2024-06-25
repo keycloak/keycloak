13cde6ebe9e91fbc528114de4dac9e1a472850ad
mposolda <mposolda@gmail.com>
2024-06-25 12:06:56 +0200

    Move some server related logic from info representation classes to server codebase
    Signed-off-by: mposolda <mposolda@gmail.com>

--- core/src/main/java/org/keycloak/representations/info/FeatureRepresentation.java
+++ core/src/main/java/org/keycloak/representations/info/FeatureRepresentation.java
@@ -1,39 +1,15 @@
 package org.keycloak.representations.info;
 
-import org.keycloak.common.Profile;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public class FeatureRepresentation {
     private String name;
     private String label;
-    private Type type;
+    private FeatureType type;
     private boolean isEnabled;
     private Set<String> dependencies;
 
     public FeatureRepresentation() {
-    }
-
-    public FeatureRepresentation(Profile.Feature feature, boolean isEnabled) {
-        this.name = feature.name();
-        this.label = feature.getLabel();
-        this.type = Type.valueOf(feature.getType().name());
-        this.isEnabled = isEnabled;
-        this.dependencies = feature.getDependencies() != null ?
-                feature.getDependencies().stream().map(Enum::name).collect(Collectors.toSet()) : Collections.emptySet();
-    }
-
-    public static List<FeatureRepresentation> create() {
-        List<FeatureRepresentation> featureRepresentationList = new ArrayList<>();
-        Profile profile = Profile.getInstance();
-        final Map<Profile.Feature, Boolean> features = profile.getFeatures();
-        features.forEach((f, enabled) -> featureRepresentationList.add(new FeatureRepresentation(f, enabled)));
-        return featureRepresentationList;
     }
 
     public String getName() {
@@ -52,11 +28,11 @@ public class FeatureRepresentation {
         this.label = label;
     }
 
-    public Type getType() {
+    public FeatureType getType() {
         return type;
     }
 
-    public void setType(Type type) {
+    public void setType(FeatureType type) {
         this.type = type;
     }
 
@@ -75,13 +51,4 @@ public class FeatureRepresentation {
     public void setDependencies(Set<String> dependencies) {
         this.dependencies = dependencies;
     }
-}
-
-enum Type {
-    DEFAULT,
-    DISABLED_BY_DEFAULT,
-    PREVIEW,
-    PREVIEW_DISABLED_BY_DEFAULT,
-    EXPERIMENTAL,
-    DEPRECATED;
 }
