26f610fa54a9b131ce53d2b780056362c5685461
Ghenadii Batalski <ghenadii.batalski@conitas.de>
2024-06-26 11:06:16 +0200

    Map groups to UserRepresentation
    Signed-off-by: Ghenadii Batalski <ghenadii.batalski@conitas.de>

--- server-spi-private/src/main/java/org/keycloak/models/utils/ModelToRepresentation.java
+++ server-spi-private/src/main/java/org/keycloak/models/utils/ModelToRepresentation.java
@@ -251,6 +251,9 @@ public class ModelToRepresentation {
             rep.setAttributes(attrs);
         }
 
+        rep.setGroups(user.getGroupsStream()
+            .map(ModelToRepresentation::buildGroupPath).toList());
+
         return rep;
     }
 
