f010f7df9bc3031a42f3524ff4e3653d24310816
Pedro Igor <pigor.craveiro@gmail.com>
2024-07-02 11:41:03 -0300

    Reverting removal of test assertions and keeping existing logic where only brokers the user is linked to is shown after identity-first login page
    Signed-off-by: Pedro Igor <pigor.craveiro@gmail.com>

--- services/src/main/java/org/keycloak/forms/login/freemarker/LoginFormsUtil.java
+++ services/src/main/java/org/keycloak/forms/login/freemarker/LoginFormsUtil.java
@@ -21,14 +21,15 @@ import org.keycloak.authentication.AuthenticationFlowContext;
 import org.keycloak.authentication.AuthenticationProcessor;
 import org.keycloak.authentication.authenticators.broker.AbstractIdpAuthenticator;
 import org.keycloak.authentication.authenticators.broker.util.SerializedBrokeredIdentityContext;
+import org.keycloak.common.Profile;
+import org.keycloak.common.Profile.Feature;
+import org.keycloak.models.FederatedIdentityModel;
 import org.keycloak.models.IdentityProviderModel;
 import org.keycloak.models.KeycloakSession;
-import org.keycloak.models.OrganizationModel;
 import org.keycloak.models.UserModel;
 import org.keycloak.services.resources.LoginActionsService;
 import org.keycloak.sessions.AuthenticationSessionModel;
 
-import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -41,12 +42,11 @@ import java.util.stream.Stream;
  */
 public class LoginFormsUtil {
 
-    public static List<IdentityProviderModel> filterIdentityProvidersForTheme(Stream<IdentityProviderModel> providers, KeycloakSession session, AuthenticationFlowContext context) {
+    public static Stream<IdentityProviderModel> filterIdentityProvidersForTheme(Stream<IdentityProviderModel> providers, KeycloakSession session, AuthenticationFlowContext context) {
         if (context != null) {
             AuthenticationSessionModel authSession = context.getAuthenticationSession();
             String currentFlowPath = authSession.getAuthNote(AuthenticationProcessor.CURRENT_FLOW_PATH);
             UserModel currentUser = context.getUser();
-            Object organization = session.getAttribute(OrganizationModel.class.getName());
             // Fixing #14173
             // If the current user is not null, then it's a re-auth, and we should filter the possible options with the pre-14173 logic
             // If the current user is null, then it's one of the following cases:
@@ -57,14 +57,14 @@ public class LoginFormsUtil {
 	    //    - in this case the current user is null AND the current flow is NOT the FIRST_BROKER_LOGIN_PATH
 	    //    - so we should show all the possible IdPs to the user trying to log in (this is the bug in #14173)
 	    //    - so we're skipping this branch, and retunring everything at the end of the method
-            if (organization == null && (currentUser != null || Objects.equals(LoginActionsService.FIRST_BROKER_LOGIN_PATH, currentFlowPath))) {
+            if (currentUser != null || Objects.equals(LoginActionsService.FIRST_BROKER_LOGIN_PATH, currentFlowPath)) {
                 return filterIdentityProviders(providers, session, context);
             }
         }
-        return providers.collect(Collectors.toList());
+        return filterOrganizationBrokers(providers);
     }
 
-    public static List<IdentityProviderModel> filterIdentityProviders(Stream<IdentityProviderModel> providers, KeycloakSession session, AuthenticationFlowContext context) {
+    public static Stream<IdentityProviderModel> filterIdentityProviders(Stream<IdentityProviderModel> providers, KeycloakSession session, AuthenticationFlowContext context) {
 
         if (context != null) {
             AuthenticationSessionModel authSession = context.getAuthenticationSession();
@@ -73,25 +73,43 @@ public class LoginFormsUtil {
             final IdentityProviderModel existingIdp = (serializedCtx == null) ? null : serializedCtx.deserialize(session, authSession).getIdpConfig();
 
             final Set<String> federatedIdentities;
-            if (context.getUser() != null) {
-                federatedIdentities = session.users().getFederatedIdentitiesStream(session.getContext().getRealm(), context.getUser())
-                        .map(federatedIdentityModel -> federatedIdentityModel.getIdentityProvider())
+            UserModel user = context.getUser();
+            if (user != null) {
+                federatedIdentities = session.users().getFederatedIdentitiesStream(session.getContext().getRealm(), user)
+                        .map(FederatedIdentityModel::getIdentityProvider)
                         .collect(Collectors.toSet());
             } else {
-                federatedIdentities = null;
+                federatedIdentities = Set.of();
             }
 
-            return providers
+            return filterOrganizationBrokers(providers)
                     .filter(p -> { // Filter current IDP during first-broker-login flow. Re-authentication with the "linked" broker should not be possible
                         if (existingIdp == null) return true;
                         return !Objects.equals(p.getAlias(), existingIdp.getAlias());
                     })
-                    .filter(idp -> { // In case that we already have user established in authentication session, we show just providers already linked to this user
-                        if (federatedIdentities == null) return true;
+                    .filter(idp -> {
+                        // user not established in authentication session, he can choose to authenticate using any broker
+                        if (user == null) {
+                            return true;
+                        }
+
+                        if (federatedIdentities.isEmpty()) {
+                            // user established but not linked to any broker, he can choose to authenticate using any organization broker
+                            return idp.getOrganizationId() != null;
+                        }
+
+                        // user established, we show just providers already linked to this user
                         return federatedIdentities.contains(idp.getAlias());
-                    })
-                    .collect(Collectors.toList());
+                    });
         }
-        return providers.collect(Collectors.toList());
+
+        return filterOrganizationBrokers(providers);
+    }
+
+    private static Stream<IdentityProviderModel> filterOrganizationBrokers(Stream<IdentityProviderModel> providers) {
+        if (!Profile.isFeatureEnabled(Feature.ORGANIZATION)) {
+            providers = providers.filter(identityProviderModel -> identityProviderModel.getOrganizationId() == null);
+        }
+        return providers;
     }
 }
