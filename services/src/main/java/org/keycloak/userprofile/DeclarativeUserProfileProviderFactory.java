d4758333611681f87e4d453e0057bca6f483dcdf
Pedro Igor <pigor.craveiro@gmail.com>
2024-07-09 13:54:30 -0300

    Do not expose kc.org attribute in user representations
    Closes #31143
    
    Signed-off-by: Pedro Igor <pigor.craveiro@gmail.com>

--- services/src/main/java/org/keycloak/userprofile/DeclarativeUserProfileProviderFactory.java
+++ services/src/main/java/org/keycloak/userprofile/DeclarativeUserProfileProviderFactory.java
@@ -358,13 +358,8 @@ public class DeclarativeUserProfileProviderFactory implements UserProfileProvide
             metadata.addAttribute(OrganizationModel.ORGANIZATION_ATTRIBUTE, -1,
                             new AttributeValidatorMetadata(OrganizationMemberValidator.ID),
                             new AttributeValidatorMetadata(ImmutableAttributeValidator.ID))
-                    .addReadCondition(c -> USER_API.equals(c.getContext()))
-                    .addWriteCondition(context -> {
-                        // the attribute can only be managed within the scope of the Organization API
-                        // we assume, for now, that if the organization is set as a session attribute, we are operating within the scope if the Organization API
-                        KeycloakSession session = context.getSession();
-                        return session.getAttribute(OrganizationModel.class.getName()) != null;
-                    });
+                    .addReadCondition((c) -> false)
+                    .addWriteCondition((c) -> false);
         }
     }
 
