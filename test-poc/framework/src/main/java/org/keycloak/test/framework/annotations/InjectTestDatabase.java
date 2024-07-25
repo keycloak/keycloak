88b061018fda606fd18f97bf037c4776da9e7cc7
stianst <stianst@gmail.com>
2024-07-25 12:29:17 +0200

    Rename test database annotation
    Signed-off-by: stianst <stianst@gmail.com>

--- test-poc/framework/src/main/java/org/keycloak/test/framework/annotations/InjectTestDatabase.java
+++ test-poc/framework/src/main/java/org/keycloak/test/framework/annotations/InjectTestDatabase.java
@@ -10,7 +10,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
-public @interface KeycloakTestDatabase {
+public @interface InjectTestDatabase {
 
     Class<? extends DatabaseConfig> config() default DatabaseConfig.class;
 
