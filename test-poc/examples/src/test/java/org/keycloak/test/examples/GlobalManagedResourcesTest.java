ab76260141bca906be77dd06f182e751b5d893d0
stianst <stianst@gmail.com>
2024-07-25 13:50:32 +0200

    Add ManagedRealm
    Signed-off-by: stianst <stianst@gmail.com>

--- test-poc/examples/src/test/java/org/keycloak/test/examples/GlobalManagedResourcesTest.java
+++ test-poc/examples/src/test/java/org/keycloak/test/examples/GlobalManagedResourcesTest.java
@@ -2,40 +2,40 @@ package org.keycloak.test.examples;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.keycloak.admin.client.resource.ClientResource;
-import org.keycloak.admin.client.resource.RealmResource;
-import org.keycloak.admin.client.resource.UserResource;
-import org.keycloak.test.framework.annotations.KeycloakIntegrationTest;
 import org.keycloak.test.framework.annotations.InjectClient;
 import org.keycloak.test.framework.annotations.InjectRealm;
 import org.keycloak.test.framework.annotations.InjectUser;
+import org.keycloak.test.framework.annotations.KeycloakIntegrationTest;
 import org.keycloak.test.framework.injection.LifeCycle;
+import org.keycloak.test.framework.realm.ManagedClient;
+import org.keycloak.test.framework.realm.ManagedRealm;
+import org.keycloak.test.framework.realm.ManagedUser;
 
 @KeycloakIntegrationTest
 public class GlobalManagedResourcesTest {
 
     @InjectRealm(lifecycle = LifeCycle.GLOBAL)
-    RealmResource realmResource;
+    ManagedRealm realm;
 
     @InjectClient(lifecycle = LifeCycle.GLOBAL)
-    ClientResource clientResource;
+    ManagedClient client;
 
     @InjectUser(lifecycle = LifeCycle.GLOBAL)
-    UserResource userResource;
+    ManagedUser user;
 
     @Test
     public void testCreatedRealm() {
-        Assertions.assertEquals("default", realmResource.toRepresentation().getRealm());
+        Assertions.assertEquals("default", realm.getName());
     }
 
     @Test
     public void testCreatedClient() {
-        Assertions.assertEquals("default", clientResource.toRepresentation().getClientId());
+        Assertions.assertEquals("default", client.getClientId());
     }
 
     @Test
     public void testCreatedUser() {
-        Assertions.assertEquals("default", userResource.toRepresentation().getUsername());
+        Assertions.assertEquals("default", user.getUsername());
     }
 
 }
--- test-poc/examples/src/test/java/org/keycloak/test/examples/ManagedResources2Test.java
+++ test-poc/examples/src/test/java/org/keycloak/test/examples/ManagedResources2Test.java
@@ -2,13 +2,13 @@ package org.keycloak.test.examples;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.keycloak.admin.client.resource.ClientResource;
-import org.keycloak.admin.client.resource.RealmResource;
 import org.keycloak.representations.idm.ClientRepresentation;
-import org.keycloak.test.framework.annotations.KeycloakIntegrationTest;
 import org.keycloak.test.framework.annotations.InjectClient;
 import org.keycloak.test.framework.annotations.InjectRealm;
+import org.keycloak.test.framework.annotations.KeycloakIntegrationTest;
 import org.keycloak.test.framework.injection.LifeCycle;
+import org.keycloak.test.framework.realm.ManagedClient;
+import org.keycloak.test.framework.realm.ManagedRealm;
 
 import java.util.List;
 
@@ -16,21 +16,23 @@ import java.util.List;
 public class ManagedResources2Test {
 
     @InjectRealm(lifecycle = LifeCycle.CLASS)
-    RealmResource realmResource;
+    ManagedRealm realm;
 
     @InjectClient
-    ClientResource clientResource;
+    ManagedClient client;
 
     @Test
     public void testCreatedRealm() {
-        Assertions.assertEquals("default", realmResource.toRepresentation().getRealm());
+        Assertions.assertEquals("http://localhost:8080/realms/default", realm.getBaseUrl());
+        Assertions.assertEquals("default", realm.getName());
+        Assertions.assertEquals("default", realm.admin().toRepresentation().getRealm());
     }
 
     @Test
     public void testCreatedClient() {
-        Assertions.assertEquals("default", clientResource.toRepresentation().getClientId());
+        Assertions.assertEquals("default", client.getClientId());
 
-        List<ClientRepresentation> clients = realmResource.clients().findByClientId("default");
+        List<ClientRepresentation> clients = realm.admin().clients().findByClientId("default");
         Assertions.assertEquals(1, clients.size());
     }
 
