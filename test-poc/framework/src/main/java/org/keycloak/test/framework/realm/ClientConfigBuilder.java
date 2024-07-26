34f4eeedd83c954e4c4fd4ef0fe540454ccc1021
Stian Thorgersen <stianst@gmail.com>
2024-07-26 15:01:36 +0200

    Testsuite PoC - Prototype OAuth client (#31663)
    Signed-off-by: stianst <stianst@gmail.com>

--- test-poc/framework/src/main/java/org/keycloak/test/framework/realm/ClientConfigBuilder.java
+++ test-poc/framework/src/main/java/org/keycloak/test/framework/realm/ClientConfigBuilder.java
@@ -16,8 +16,18 @@ public class ClientConfigBuilder {
         return this;
     }
 
+    public ClientConfigBuilder secret(String secret) {
+        representation.setSecret(secret);
+        return this;
+    }
+
     public ClientConfigBuilder redirectUris(String... redirectUris) {
         representation.setRedirectUris(Collections.combine(representation.getRedirectUris(), redirectUris));
+        return this;
+    }
+
+    public ClientConfigBuilder serviceAccount() {
+        representation.setServiceAccountsEnabled(true);
         return this;
     }
 
--- test-poc/framework/src/main/java/org/keycloak/test/framework/realm/UserSupplier.java
+++ test-poc/framework/src/main/java/org/keycloak/test/framework/realm/UserSupplier.java
@@ -2,7 +2,6 @@ package org.keycloak.test.framework.realm;
 
 import jakarta.ws.rs.core.Response;
 import org.keycloak.admin.client.resource.UserResource;
-import org.keycloak.representations.idm.RoleRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.keycloak.test.framework.annotations.InjectUser;
 import org.keycloak.test.framework.injection.InstanceContext;
