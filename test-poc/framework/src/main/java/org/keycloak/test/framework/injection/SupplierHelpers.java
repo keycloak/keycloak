e3eb2e79a78ffac401d60ada321e54c6e90a91b0
stianst <stianst@gmail.com>
2024-07-26 11:00:12 +0200

    Testsuite PoC - Allow inner classes for config to be non-public
    Signed-off-by: stianst <stianst@gmail.com>

--- test-poc/framework/src/main/java/org/keycloak/test/framework/injection/SupplierHelpers.java
+++ test-poc/framework/src/main/java/org/keycloak/test/framework/injection/SupplierHelpers.java
@@ -1,10 +1,14 @@
 package org.keycloak.test.framework.injection;
 
+import java.lang.reflect.Constructor;
+
 public class SupplierHelpers {
 
     public static <T> T getInstance(Class<T> clazz) {
         try {
-            return clazz.getDeclaredConstructor().newInstance();
+            Constructor<T> declaredConstructor = clazz.getDeclaredConstructor();
+            declaredConstructor.setAccessible(true);
+            return declaredConstructor.newInstance();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
