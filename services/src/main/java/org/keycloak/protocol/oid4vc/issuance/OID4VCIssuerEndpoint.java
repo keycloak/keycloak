d76dbdb6ba54923f848c07b0f05edca2c6f355d7
Stefan Wiedemann <wistefan@googlemail.com>
2024-07-31 08:13:47 +0200

    clean import
    Signed-off-by: Stefan Wiedemann <wistefan@googlemail.com>

--- services/src/main/java/org/keycloak/protocol/oid4vc/issuance/OID4VCIssuerEndpoint.java
+++ services/src/main/java/org/keycloak/protocol/oid4vc/issuance/OID4VCIssuerEndpoint.java
@@ -18,7 +18,6 @@
 package org.keycloak.protocol.oid4vc.issuance;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
