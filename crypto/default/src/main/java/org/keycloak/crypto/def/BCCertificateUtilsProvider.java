d03f05736edf56264dcae7ef0f40367ec53a412e
Captain-P-Goldfish <captain.p.goldfish@gmx.de>
2024-07-31 23:31:22 +0200

    Remove unused throws-clause
    Signed-off-by: Captain-P-Goldfish <captain.p.goldfish@gmx.de>

--- crypto/default/src/main/java/org/keycloak/crypto/def/BCCertificateUtilsProvider.java
+++ crypto/default/src/main/java/org/keycloak/crypto/def/BCCertificateUtilsProvider.java
@@ -87,7 +87,7 @@ public class BCCertificateUtilsProvider implements CertificateUtilsProvider {
      * @throws Exception the exception
      */
     public X509Certificate generateV3Certificate(KeyPair keyPair, PrivateKey caPrivateKey, X509Certificate caCert,
-                                                 String subject) throws Exception {
+                                                 String subject) {
         try {
             X500Name subjectDN = new X500Name("CN=" + subject);
 
