4c9b422357492716571f877cfb79698f53724535
Erik Jan de Wit <erikjan.dewit@gmail.com>
2024-08-06 13:47:36 +0200

    use stringify on use meta data descriptor (#31717) (#31906)
    fixes: #31687
    
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>
    (cherry picked from commit 3f6136c6487f715bee2badf3d686efd3520eac21)

--- js/apps/admin-ui/src/identity-providers/add/DescriptorSettings.tsx
+++ js/apps/admin-ui/src/identity-providers/add/DescriptorSettings.tsx
@@ -282,6 +282,7 @@ const Fields = ({ readOnly }: DescriptorSettingsProps) => {
               name="config.useMetadataDescriptorUrl"
               label={t("useMetadataDescriptorUrl")}
               isDisabled={readOnly}
+              stringify
             />
             {useMetadataDescriptorUrl !== "true" && (
               <TextAreaControl
