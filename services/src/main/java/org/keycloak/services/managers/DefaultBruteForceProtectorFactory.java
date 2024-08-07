f5df09a32a5730fe96742d6b1ffdb84b5c4ab906
rmartinc <rmartinc@redhat.com>
2024-08-07 10:49:53 +0200

    Adding upgrading notes for brute force changes Closes #31960
    Signed-off-by: rmartinc <rmartinc@redhat.com>

--- services/src/main/java/org/keycloak/services/managers/DefaultBruteForceProtectorFactory.java
+++ services/src/main/java/org/keycloak/services/managers/DefaultBruteForceProtectorFactory.java
@@ -17,9 +17,12 @@
 
 package org.keycloak.services.managers;
 
+import java.util.List;
 import org.keycloak.Config;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.provider.ProviderConfigProperty;
+import org.keycloak.provider.ProviderConfigurationBuilder;
 
 /**
  * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
@@ -56,5 +59,17 @@ public class DefaultBruteForceProtectorFactory implements BruteForceProtectorFac
     @Override
     public String getId() {
         return "default-brute-force-detector";
+    }
+
+    @Override
+    public List<ProviderConfigProperty> getConfigMetadata() {
+        return ProviderConfigurationBuilder.create()
+                .property()
+                .name("allowConcurrentRequests")
+                .type("boolean")
+                .helpText("If concurrent logins are allowed by the brute force protection.")
+                .defaultValue(false)
+                .add()
+                .build();
     }
 }
