c2c74faec0481c9cd6ee9506b65f42c08bb7442d
Giuseppe Graziano <g.graziano94@gmail.com>
2024-08-13 17:09:16 +0200

    Removing BOM character from SAML entity descriptor
    Closes #30604
    
    Signed-off-by: Giuseppe Graziano <g.graziano94@gmail.com>

--- testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/admin/IdentityProviderTest.java
+++ testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/admin/IdentityProviderTest.java
@@ -743,6 +743,19 @@ public class IdentityProviderTest extends AbstractAdminTest {
         assertSamlExport(body, true);
     }
 
+   @Test
+    public void testSamlImportWithBom() throws URISyntaxException, IOException, ParsingException {
+        testSamlImport("saml-idp-metadata_utf8_bom.xml", true);
+
+        // Perform export, and make sure some of the values are like they're supposed to be
+        Response response = realm.identityProviders().get("saml").export("xml");
+        Assert.assertEquals(200, response.getStatus());
+        String body = response.readEntity(String.class);
+        response.close();
+
+        assertSamlExport(body, true);
+    }
+
     @Test
     public void testSamlImportAndExportDifferentBindings() throws URISyntaxException, IOException, ParsingException {
         testSamlImport("saml-idp-metadata-different-bindings.xml", false);
