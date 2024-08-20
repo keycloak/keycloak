4d983d595dca0b62214dd0f790c77e29674b10b9
Pedro Igor <pigor.craveiro@gmail.com>
2024-08-20 08:55:32 -0300

    Fixing compilation errors due to usages of javax.ws.rs imports
    Signed-off-by: Pedro Igor <pigor.craveiro@gmail.com>

--- integration/admin-client/src/main/java/org/keycloak/admin/client/resource/OrganizationsMembersResource.java
+++ integration/admin-client/src/main/java/org/keycloak/admin/client/resource/OrganizationsMembersResource.java
@@ -19,12 +19,11 @@ package org.keycloak.admin.client.resource;
 
 import java.util.List;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import org.keycloak.representations.idm.OrganizationRepresentation;
 
 public interface OrganizationsMembersResource {
