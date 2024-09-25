319a729aff294366b66bb415349e55c77f14efbe
Erik Jan de Wit <erikjan.dewit@gmail.com>
2024-09-25 14:18:08 +0200

    added preview and grouped vars
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- js/apps/admin-ui/src/realm-settings/themes/ThemeColors.tsx
+++ js/apps/admin-ui/src/realm-settings/themes/ThemeColors.tsx
@@ -1,36 +1,62 @@
 import RealmRepresentation from "@keycloak/keycloak-admin-client/lib/defs/realmRepresentation";
 import { TextControl } from "@keycloak/keycloak-ui-shared";
 import { TextControlProps } from "@keycloak/keycloak-ui-shared/dist/controls/TextControl";
-import { Button, PageSection } from "@patternfly/react-core";
+import {
+  Button,
+  Flex,
+  FlexItem,
+  InputGroup,
+  InputGroupItem,
+  PageSection,
+} from "@patternfly/react-core";
 import { useEffect } from "react";
-import { FormProvider, useForm } from "react-hook-form";
+import {
+  FormProvider,
+  useForm,
+  useFormContext,
+  useWatch,
+} from "react-hook-form";
 import { useTranslation } from "react-i18next";
 import { FixedButtonsGroup } from "../../components/form/FixedButtonGroup";
 import { FormAccess } from "../../components/form/FormAccess";
 import { convertAttributeNameToForm, convertToFormValues } from "../../util";
 import mapping from "./PatternflyVars";
-
-type MappingKey = keyof typeof mapping;
+import { PreviewWindow } from "./PreviewWindow";
 
 type ColorControlProps = TextControlProps<any> & {
-  attribute: MappingKey;
+  color: string;
 };
 
-const ColorControl = ({ attribute, ...props }: ColorControlProps) => (
-  <TextControl
-    {...props}
-    customIcon={
-      <span
-        style={{
-          height: "1em",
-          backgroundColor: mapping[attribute],
-        }}
-      >
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      </span>
-    }
-  />
-);
+const ColorControl = ({ name, color, ...props }: ColorControlProps) => {
+  const { control, setValue } = useFormContext();
+  const currentValue = useWatch({
+    control,
+    name,
+  });
+  return (
+    <InputGroup>
+      <InputGroupItem isFill>
+        <TextControl {...props} name={name} />
+      </InputGroupItem>
+      <input
+        type="color"
+        value={currentValue || color}
+        onChange={(e) => setValue(name, e.target.value)}
+      />
+    </InputGroup>
+  );
+};
+
+const convertToCssVars = (attributes: Record<string, string>) =>
+  Object.entries(attributes)
+    .filter(([key]) => key.startsWith("style"))
+    .reduce(
+      (acc, [key, value]) => ({
+        ...acc,
+        [key.substring("style".length + 2)]: value,
+      }),
+      {},
+    );
 
 type ThemeColorsProps = {
   realm: RealmRepresentation;
@@ -41,46 +67,68 @@ export const ThemeColors = ({ realm, save }: ThemeColorsProps) => {
   const { t } = useTranslation();
   const form = useForm<RealmRepresentation>();
   const { handleSubmit, setValue } = form;
+  const attributes = useWatch({
+    control: form.control,
+    name: "attributes",
+    defaultValue: {},
+  });
 
   const reset = () =>
-    convertToFormValues({ attributes: { style: mapping } }, setValue);
+    convertToFormValues(
+      {
+        attributes: {
+          style: mapping.reduce((acc, m) => ({
+            ...acc,
+            [m.variable]: m.defaultValue,
+          })),
+        },
+      },
+      setValue,
+    );
+
   const setupForm = () => {
-    // if it has one of the default colors, we use that as the base
-    if (realm?.attributes?.["style.palette--black-100"]) {
-      convertToFormValues(realm, setValue);
-    } else {
-      reset();
-    }
+    reset();
+    convertToFormValues(realm, setValue);
   };
+
   useEffect(setupForm, [realm]);
 
   return (
     <PageSection variant="light">
-      <FormAccess
-        isHorizontal
-        role="manage-realm"
-        onSubmit={handleSubmit(save)}
+      <Flex>
+        <FlexItem>
+          <FormAccess isHorizontal role="manage-realm">
+            <FormProvider {...form}>
+              <TextControl
+                name={convertAttributeNameToForm("attributes.style.logo")}
+                label={t("logo")}
+              />
+              {mapping.map((m) => (
+                <ColorControl
+                  key={m.name}
+                  color={m.defaultValue}
+                  name={convertAttributeNameToForm(
+                    `attributes.style.${m.variable}`,
+                  )}
+                  label={m.name}
+                />
+              ))}
+            </FormProvider>
+          </FormAccess>
+        </FlexItem>
+        <FlexItem grow={{ default: "grow" }} style={{ zIndex: 0 }}>
+          <PreviewWindow cssVars={convertToCssVars(attributes!)} />
+        </FlexItem>
+      </Flex>
+      <FixedButtonsGroup
+        name="colors"
+        save={handleSubmit(save)}
+        reset={setupForm}
       >
-        <FormProvider {...form}>
-          <TextControl
-            name={convertAttributeNameToForm("attributes.style.logo")}
-            label={t("logo")}
-          />
-          {Object.keys(mapping).map((key) => (
-            <ColorControl
-              key={key}
-              attribute={key as MappingKey}
-              name={convertAttributeNameToForm(`attributes.style.${key}`)}
-              label={`--pf-v5-global--${key}`}
-            />
-          ))}
-        </FormProvider>
-        <FixedButtonsGroup name="colors" isSubmit reset={setupForm}>
-          <Button type="button" variant="link" onClick={reset}>
-            {t("defaults")}
-          </Button>
-        </FixedButtonsGroup>
-      </FormAccess>
+        <Button type="button" variant="link" onClick={reset}>
+          {t("defaults")}
+        </Button>
+      </FixedButtonsGroup>
     </PageSection>
   );
 };
