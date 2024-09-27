805a92adbf9981587c9a2720b40fd264220ec12b
Agnieszka Gancarczyk <4890675+agagancarczyk@users.noreply.github.com>
2024-09-27 12:25:09 +0100

    Duplicating groups (#32454)
    * Duplicating groups - wip
    
    Signed-off-by: Agnieszka Gancarczyk <agagancarczyk@gmail.com>
    
    * Duplicating groups - wip
    
    Signed-off-by: Agnieszka Gancarczyk <agagancarczyk@gmail.com>
    
    * Duplicating groups - wip
    
    Signed-off-by: Agnieszka Gancarczyk <agagancarczyk@gmail.com>
    
    * Duplicating groups - wip
    
    Signed-off-by: Agnieszka Gancarczyk <agagancarczyk@gmail.com>
    
    * Duplicating groups - wip
    
    Signed-off-by: Agnieszka Gancarczyk <agagancarczyk@gmail.com>
    
    * Duplicating groups - wip
    
    Signed-off-by: Agnieszka Gancarczyk <agagancarczyk@gmail.com>
    
    * Duplicating groups - wip
    
    Signed-off-by: Agnieszka Gancarczyk <agagancarczyk@gmail.com>
    
    * Duplicating groups - enhancement
    
    Signed-off-by: Agnieszka Gancarczyk <agagancarczyk@gmail.com>
    
    * Duplicating groups - enhancement
    
    Signed-off-by: Agnieszka Gancarczyk <agagancarczyk@gmail.com>
    
    * Enhancements
    
    Signed-off-by: Agnieszka Gancarczyk <agagancarczyk@gmail.com>
    
    * Enhancements
    
    Signed-off-by: Agnieszka Gancarczyk <agagancarczyk@gmail.com>
    
    * Enhancements
    
    Signed-off-by: Agnieszka Gancarczyk <agagancarczyk@gmail.com>
    
    * Enhancements
    
    Signed-off-by: Agnieszka Gancarczyk <agagancarczyk@gmail.com>
    
    * improvements
    
    Signed-off-by: Agnieszka Gancarczyk <agagancarczyk@gmail.com>
    
    * improvements
    
    Signed-off-by: Agnieszka Gancarczyk <agagancarczyk@gmail.com>
    
    * improvements
    
    Signed-off-by: Agnieszka Gancarczyk <agagancarczyk@gmail.com>
    
    ---------
    
    Signed-off-by: Agnieszka Gancarczyk <agagancarczyk@gmail.com>

--- js/apps/admin-ui/src/groups/GroupTable.tsx
+++ js/apps/admin-ui/src/groups/GroupTable.tsx
@@ -25,25 +25,19 @@ type GroupTableProps = {
 
 export const GroupTable = ({ refresh: viewRefresh }: GroupTableProps) => {
   const { adminClient } = useAdminClient();
-
   const { t } = useTranslation();
-
   const [selectedRows, setSelectedRows] = useState<GroupRepresentation[]>([]);
-
   const [rename, setRename] = useState<GroupRepresentation>();
   const [isCreateModalOpen, toggleCreateOpen] = useToggle();
+  const [duplicateId, setDuplicateId] = useState<string>();
   const [showDelete, toggleShowDelete] = useToggle();
   const [move, setMove] = useState<GroupRepresentation>();
-
   const { currentGroup } = useSubGroups();
-
   const [key, setKey] = useState(0);
   const refresh = () => setKey(key + 1);
   const [search, setSearch] = useState<string>();
-
   const location = useLocation();
   const id = getLastId(location.pathname);
-
   const { hasAccess } = useAccess();
   const isManager = hasAccess("manage-users") || currentGroup()?.access?.manage;
 
@@ -101,6 +95,17 @@ export const GroupTable = ({ refresh: viewRefresh }: GroupTableProps) => {
             refresh();
             viewRefresh();
           }}
+        />
+      )}
+      {duplicateId && (
+        <GroupsModal
+          id={duplicateId}
+          duplicateId={duplicateId}
+          refresh={() => {
+            refresh();
+            viewRefresh();
+          }}
+          handleModalToggle={() => setDuplicateId(undefined)}
         />
       )}
       {move && (
@@ -172,6 +177,17 @@ export const GroupTable = ({ refresh: viewRefresh }: GroupTableProps) => {
                     return false;
                   },
                 },
+                ...(!id
+                  ? [
+                      {
+                        title: t("duplicate"),
+                        onRowClick: async (group: GroupRepresentation) => {
+                          setDuplicateId(group.id);
+                          return false;
+                        },
+                      },
+                    ]
+                  : []),
                 {
                   isSeparator: true,
                 },
