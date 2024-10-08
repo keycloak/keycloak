af3fe9873aa9f4a4c2550aac70264ae81a5610f0
Steve Hawkins <shawkins@redhat.com>
2024-10-08 13:18:35 -0400

    fix: refining https file type detection
    also making common trustore logic align
    
    closes: #33649
    
    Signed-off-by: Steve Hawkins <shawkins@redhat.com>

--- common/src/test/java/org/keycloak/common/util/KeystoreUtilTest.java
+++ common/src/test/java/org/keycloak/common/util/KeystoreUtilTest.java
@@ -1,8 +1,9 @@
 package org.keycloak.common.util;
 
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
 
 public class KeystoreUtilTest {
 
@@ -11,6 +12,15 @@ public class KeystoreUtilTest {
         assertEquals("x", KeystoreUtil.getKeystoreType("x", "y", "z"));
         assertEquals("z", KeystoreUtil.getKeystoreType(null, "y", "z"));
         assertEquals(KeystoreUtil.KeystoreFormat.PKCS12.name(), KeystoreUtil.getKeystoreType(null, "y.pfx", "z"));
+        assertEquals(KeystoreUtil.KeystoreFormat.PKCS12.name(), KeystoreUtil.getKeystoreType(null, "y.pkcs12", "z"));
+    }
+    
+    @Test
+    public void testGetFormat() {
+        assertFalse(KeystoreUtil.getKeystoreFormat("some.file").isPresent());
+        assertFalse(KeystoreUtil.getKeystoreFormat("somepfx").isPresent());
+        assertEquals(KeystoreUtil.KeystoreFormat.PKCS12, KeystoreUtil.getKeystoreFormat("file.pfx").get());
+        assertEquals(KeystoreUtil.KeystoreFormat.JKS, KeystoreUtil.getKeystoreFormat("file.jks").get());
     }
 
 }
