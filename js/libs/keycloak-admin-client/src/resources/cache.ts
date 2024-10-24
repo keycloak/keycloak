89d7255ff341e734310200dbd428f9cab5544c0a
Christian Janker <christian.janker@gmx.at>
2024-10-24 17:31:47 +0200

    Restore the Cache tab in Realm Settings
    closes keycloak#17727
    
    Signed-off-by: Christian Janker <christian.janker@gmx.at>

--- js/libs/keycloak-admin-client/src/resources/cache.ts
+++ js/libs/keycloak-admin-client/src/resources/cache.ts
@@ -6,6 +6,14 @@ export class Cache extends Resource<{ realm?: string }> {
     method: "POST",
     path: "/clear-user-cache",
   });
+  public clearKeysCache = this.makeRequest<{}, void>({
+    method: "POST",
+    path: "/clear-keys-cache",
+  });
+  public clearRealmCache = this.makeRequest<{}, void>({
+    method: "POST",
+    path: "/clear-realm-cache",
+  });
 
   constructor(client: KeycloakAdminClient) {
     super(client, {
