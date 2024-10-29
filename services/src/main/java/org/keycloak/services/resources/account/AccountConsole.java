f9f9a313b3f9f7199721eb435d855e814a92f59e
Pedro Igor <pigor.craveiro@gmail.com>
2024-10-29 17:34:01 -0300

    make sure error dialog is shown at the account console when declining terms
    Signed-off-by: Pedro Igor <pigor.craveiro@gmail.com>

--- services/src/main/java/org/keycloak/services/resources/account/AccountConsole.java
+++ services/src/main/java/org/keycloak/services/resources/account/AccountConsole.java
@@ -3,7 +3,9 @@ package org.keycloak.services.resources.account;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.ServerErrorException;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
 import jakarta.ws.rs.core.UriBuilder;
 import org.jboss.resteasy.reactive.NoCache;
 import org.keycloak.OAuth2Constants;
@@ -109,6 +111,10 @@ public class AccountConsole implements AccountResourceProvider {
             return redirectToLogin(path);
         }
 
+        return renderAccountConsole();
+    }
+
+    private Response renderAccountConsole() throws IOException, FreeMarkerException {
         final var serverUriInfo = session.getContext().getUri(UrlType.FRONTEND);
         final var serverBaseUri = serverUriInfo.getBaseUri();
         // Strip any trailing slashes from the URL.
@@ -250,11 +256,10 @@ public class AccountConsole implements AccountResourceProvider {
         if (!queryParameters.isEmpty()) {
             String error = queryParameters.getFirst(OAuth2Constants.ERROR);
             if (error != null) {
-                uriBuilder.queryParam(OAuth2Constants.ERROR, error);
-
-                String errorDescription = queryParameters.getFirst(OAuth2Constants.ERROR_DESCRIPTION);
-                if (errorDescription != null) {
-                    uriBuilder.queryParam(OAuth2Constants.ERROR_DESCRIPTION, errorDescription);
+                try {
+                    return renderAccountConsole();
+                } catch (IOException | FreeMarkerException e) {
+                    throw new ServerErrorException(Status.INTERNAL_SERVER_ERROR);
                 }
             }
         }
