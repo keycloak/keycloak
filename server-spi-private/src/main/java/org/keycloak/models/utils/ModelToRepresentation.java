8c61e2e250dd61f7a1f579531114f588d1a874c6
Himanshi Gupta <higupta@redhat.com>
2024-11-11 15:34:25 +0530

    fix for issue <34590>
    Signed-off-by: Himanshi Gupta <higupta@redhat.com>

--- server-spi-private/src/main/java/org/keycloak/models/utils/ModelToRepresentation.java
+++ server-spi-private/src/main/java/org/keycloak/models/utils/ModelToRepresentation.java
@@ -1314,6 +1314,7 @@ public class ModelToRepresentation {
         rep.setName(model.getName());
         rep.setAlias(model.getAlias());
         rep.setEnabled(model.isEnabled());
+        rep.setAttributes(model.getAttributes());
         rep.setRedirectUrl(model.getRedirectUrl());
         rep.setDescription(model.getDescription());
         model.getDomains().filter(Objects::nonNull).map(ModelToRepresentation::toRepresentation)
