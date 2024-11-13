567441c51e63b644363b8c245c6b58eca2af3d07
Himanshi Gupta <higupta@redhat.com>
2024-11-13 18:09:52 +0530

    fix for issue Attributes missing in OrganizationRepresentation when using Admin REST API in Keycloak 26
    Signed-off-by: Himanshi Gupta <higupta@redhat.com>

--- server-spi-private/src/main/java/org/keycloak/models/utils/ModelToRepresentation.java
+++ server-spi-private/src/main/java/org/keycloak/models/utils/ModelToRepresentation.java
@@ -1296,7 +1296,7 @@ public class ModelToRepresentation {
     }
 
     public static OrganizationRepresentation toRepresentation(OrganizationModel model) {
-        OrganizationRepresentation rep = toBriefRepresentation(model);
+        OrganizationRepresentation rep = toBriefRepresentation(model,false);
         if (rep == null) {
             return null;
         }
@@ -1304,7 +1304,7 @@ public class ModelToRepresentation {
         return rep;
     }
 
-    public static OrganizationRepresentation toBriefRepresentation(OrganizationModel model) {
+    public static OrganizationRepresentation toBriefRepresentation(OrganizationModel model, Boolean briefRepresentation) {
         if (model == null) {
             return null;
         }
@@ -1312,8 +1312,10 @@ public class ModelToRepresentation {
         rep.setId(model.getId());
         rep.setName(model.getName());
         rep.setAlias(model.getAlias());
+        if (briefRepresentation) {
+            rep.setAttributes(model.getAttributes());
+        }
         rep.setEnabled(model.isEnabled());
-        rep.setAttributes(model.getAttributes());
         rep.setRedirectUrl(model.getRedirectUrl());
         rep.setDescription(model.getDescription());
         model.getDomains().filter(Objects::nonNull).map(ModelToRepresentation::toRepresentation)
