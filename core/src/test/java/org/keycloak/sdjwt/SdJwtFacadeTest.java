132d4e5b37060cea32ae02e0ec4363250538649e
Awambeng Rodrick <awambengrodrick@gmail.com>
2024-11-14 08:37:07 +0100

    test: refine sdjwt string comparison for accuracy
    Added copyright header and adjusted test to directly compare sdJwt.toString() with sdJwtString for exact match, enhancing test precision
    
    Signed-off-by: Awambeng Rodrick <awambengrodrick@gmail.com>

--- core/src/test/java/org/keycloak/sdjwt/SdJwtFacadeTest.java
+++ core/src/test/java/org/keycloak/sdjwt/SdJwtFacadeTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.keycloak.sdjwt;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -12,6 +28,7 @@ import org.keycloak.rule.CryptoInitRule;
 import java.util.Collections;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
@@ -81,6 +98,7 @@ public abstract class SdJwtFacadeTest {
         String sdJwtString = sdJwtFacade.getSdJwtString(sdJwt);
 
         assertNotNull(sdJwtString);
+        assertEquals(sdJwt.toString(), sdJwtString);
     }
 
     @Test
