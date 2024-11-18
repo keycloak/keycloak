347c947182742887e10e1b897e525d27bf74ab15
Giuseppe Graziano <g.graziano94@gmail.com>
2024-11-18 10:12:30 +0100

    Use clickLink to click back to Application in Info Page
    Closes #34558
    
    Signed-off-by: Giuseppe Graziano <g.graziano94@gmail.com>

--- testsuite/integration-arquillian/tests/base/src/main/java/org/keycloak/testsuite/pages/InfoPage.java
+++ testsuite/integration-arquillian/tests/base/src/main/java/org/keycloak/testsuite/pages/InfoPage.java
@@ -20,6 +20,7 @@ package org.keycloak.testsuite.pages;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.keycloak.testsuite.util.DroneUtils;
 import org.keycloak.testsuite.util.OAuthClient;
+import org.keycloak.testsuite.util.UIUtils;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
@@ -61,7 +62,7 @@ public class InfoPage extends LanguageComboboxAwarePage {
     }
 
     public void clickBackToApplicationLink() {
-        backToApplicationLink.click();
+        UIUtils.clickLink(backToApplicationLink);
     }
 
     public void clickToContinueDe() {
