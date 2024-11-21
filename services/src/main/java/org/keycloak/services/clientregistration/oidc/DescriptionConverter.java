d96ab0360f1edcea8f04de4e7daedefddbf94d46
mposolda <mposolda@gmail.com>
2024-11-21 13:54:37 +0100

    Requested grant_types inconsistent with created grant_types for OpenID Connect Dynamic Client Registration closes #32801
    Signed-off-by: mposolda <mposolda@gmail.com>

--- services/src/main/java/org/keycloak/services/clientregistration/oidc/DescriptionConverter.java
+++ services/src/main/java/org/keycloak/services/clientregistration/oidc/DescriptionConverter.java
@@ -28,6 +28,7 @@ import org.keycloak.jose.jwk.JWKParser;
 import org.keycloak.models.CibaConfig;
 import org.keycloak.models.Constants;
 import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.OAuth2DeviceConfig;
 import org.keycloak.models.ParConfig;
 import org.keycloak.models.utils.KeycloakModelUtils;
 import org.keycloak.protocol.oidc.OIDCAdvancedConfigWrapper;
@@ -55,7 +56,6 @@ import java.net.URI;
 import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -65,8 +65,11 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static org.keycloak.OAuth2Constants.AUTHORIZATION_CODE;
+import static org.keycloak.OAuth2Constants.IMPLICIT;
 import static org.keycloak.models.CibaConfig.OIDC_CIBA_GRANT_ENABLED;
 import static org.keycloak.models.OAuth2DeviceConfig.OAUTH2_DEVICE_AUTHORIZATION_GRANT_ENABLED;
+import static org.keycloak.protocol.oidc.utils.OIDCResponseType.CODE;
 
 /**
  * @author <a href="mailto:sthorger@redhat.com">Stian Thorgersen</a>
@@ -88,20 +91,37 @@ public class DescriptionConverter {
         }
 
         List<String> oidcResponseTypes = clientOIDC.getResponseTypes();
-        if (oidcResponseTypes == null || oidcResponseTypes.isEmpty()) {
-            oidcResponseTypes = Collections.singletonList(OIDCResponseType.CODE);
-        }
         List<String> oidcGrantTypes = clientOIDC.getGrantTypes();
+        if (oidcResponseTypes == null || oidcResponseTypes.isEmpty()) {
+            if (oidcGrantTypes == null || oidcGrantTypes.isEmpty()) {
+                // OIDC Client registration specs - https://openid.net/specs/openid-connect-registration-1_0.html#ClientMetadata - If omitted, the default is that the Client will use only the code Response Type.
+                oidcResponseTypes = Collections.singletonList(CODE);
+            } else {
+                // Try to compute default responseTypes according to given grant types
+                oidcResponseTypes = new ArrayList<>();
+                if (oidcGrantTypes.contains(AUTHORIZATION_CODE)) {
+                    oidcResponseTypes.add(CODE);
+                }
+                if (oidcGrantTypes.contains(IMPLICIT)) {
+                    oidcResponseTypes.add(OIDCResponseType.ID_TOKEN);
+                }
+            }
+        }
+
+        OIDCAdvancedConfigWrapper configWrapper = OIDCAdvancedConfigWrapper.fromClientRepresentation(client);
 
         try {
             OIDCResponseType responseType = OIDCResponseType.parse(oidcResponseTypes);
-            client.setStandardFlowEnabled(responseType.hasResponseType(OIDCResponseType.CODE) || responseType.hasResponseType(OIDCResponseType.NONE));
+            client.setStandardFlowEnabled(responseType.hasResponseType(CODE) || responseType.hasResponseType(OIDCResponseType.NONE));
             client.setImplicitFlowEnabled(responseType.isImplicitOrHybridFlow());
 
+            client.setDirectAccessGrantsEnabled(oidcGrantTypes != null && oidcGrantTypes.contains(OAuth2Constants.PASSWORD));
             if (oidcGrantTypes != null) {
-                client.setDirectAccessGrantsEnabled(oidcGrantTypes.contains(OAuth2Constants.PASSWORD));
                 client.setServiceAccountsEnabled(oidcGrantTypes.contains(OAuth2Constants.CLIENT_CREDENTIALS));
-                setOidcCibaGrantEnabled(client, oidcGrantTypes.contains(OAuth2Constants.CIBA_GRANT_TYPE));
+                setOidcGrantEnabled(client, CibaConfig.OIDC_CIBA_GRANT_ENABLED, oidcGrantTypes.contains(OAuth2Constants.CIBA_GRANT_TYPE));
+                setOidcGrantEnabled(client, OAuth2DeviceConfig.OAUTH2_DEVICE_AUTHORIZATION_GRANT_ENABLED, oidcGrantTypes.contains(OAuth2Constants.DEVICE_CODE_GRANT_TYPE));
+                client.setAuthorizationServicesEnabled(oidcGrantTypes.contains(OAuth2Constants.UMA_GRANT_TYPE));
+                configWrapper.setUseRefreshToken(oidcGrantTypes.contains(OAuth2Constants.REFRESH_TOKEN));
             }
         } catch (IllegalArgumentException iae) {
             throw new ClientRegistrationException(iae.getMessage(), iae);
@@ -131,7 +151,6 @@ public class DescriptionConverter {
             throw new ClientRegistrationException("Didn't find key of supported keyType for use " + JWK.Use.SIG.asString());
         }
 
-        OIDCAdvancedConfigWrapper configWrapper = OIDCAdvancedConfigWrapper.fromClientRepresentation(client);
         if (clientOIDC.getUserinfoSignedResponseAlg() != null) {
             configWrapper.setUserInfoSignedResponseAlg(clientOIDC.getUserinfoSignedResponseAlg());
         }
@@ -263,10 +282,10 @@ public class DescriptionConverter {
         return client;
     }
 
-    private static void setOidcCibaGrantEnabled(ClientRepresentation client, Boolean isEnabled) {
+    private static void setOidcGrantEnabled(ClientRepresentation client, String grantAttributeName, Boolean isEnabled) {
         if (isEnabled == null) return;
         Map<String, String> attributes = Optional.ofNullable(client.getAttributes()).orElse(new HashMap<>());
-        attributes.put(CibaConfig.OIDC_CIBA_GRANT_ENABLED, isEnabled.toString());
+        attributes.put(grantAttributeName, isEnabled.toString());
         client.setAttributes(attributes);
     }
 
@@ -491,10 +510,10 @@ public class DescriptionConverter {
     private static List<String> getOIDCGrantTypes(ClientRepresentation client) {
         List<String> grantTypes = new ArrayList<>();
         if (client.isStandardFlowEnabled()) {
-            grantTypes.add(OAuth2Constants.AUTHORIZATION_CODE);
+            grantTypes.add(AUTHORIZATION_CODE);
         }
         if (client.isImplicitFlowEnabled()) {
-            grantTypes.add(OAuth2Constants.IMPLICIT);
+            grantTypes.add(IMPLICIT);
         }
         if (client.isDirectAccessGrantsEnabled()) {
             grantTypes.add(OAuth2Constants.PASSWORD);
