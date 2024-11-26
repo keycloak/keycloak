f17b0c71c6ef9e016816f6dd95666a57aea71dc3
Erik Jan de Wit <erikjan.dewit@gmail.com>
2024-11-26 15:07:09 +0100

    added RETRY_COUNT environment variable
    fixes: #35225
    
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- js/apps/account-ui/playwright.config.ts
+++ js/apps/account-ui/playwright.config.ts
@@ -2,6 +2,8 @@ import { defineConfig, devices } from "@playwright/test";
 
 import { getAccountUrl } from "./test/utils";
 
+const retryCount = parseInt(process.env.RETRY_COUNT || "0");
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -9,7 +11,7 @@ export default defineConfig({
   testDir: "./test",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: retryCount,
   workers: 1,
   reporter: process.env.CI ? [["github"], ["html"]] : "list",
   expect: {
