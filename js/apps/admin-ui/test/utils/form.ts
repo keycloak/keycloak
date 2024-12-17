3635dfb2574aecd6806779f34836c5e5d02b3168
Erik Jan de Wit <erikjan.dewit@gmail.com>
2024-12-17 07:46:37 +0100

    added policies ciba to playwright
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- js/apps/admin-ui/test/utils/form.ts
+++ js/apps/admin-ui/test/utils/form.ts
@@ -1,5 +1,22 @@
-import { expect, Page } from "@playwright/test";
+import { expect, Locator, Page } from "@playwright/test";
 
 export async function assertRequiredFieldError(page: Page, field: string) {
   await expect(page.getByTestId(field + "-helper")).toHaveText(/required/i);
+}
+
+export async function assertFieldError(
+  page: Page,
+  field: string,
+  text: string,
+) {
+  await expect(page.getByTestId(field + "-helper")).toHaveText(text);
+}
+
+export async function selectItem(page: Page, field: Locator, value: string) {
+  await field.click();
+  await page.getByRole("option", { name: value }).click();
+}
+
+export async function clickSaveButton(page: Page) {
+  await page.getByTestId("save").click();
 }
--- js/apps/admin-ui/test/utils/sidebar.ts
+++ js/apps/admin-ui/test/utils/sidebar.ts
@@ -8,3 +8,7 @@ export async function goToRealm(page: Page, realmName: string) {
 export async function goToClients(page: Page) {
   await page.getByTestId("nav-item-clients").click();
 }
+
+export async function goToAuthentication(page: Page) {
+  await page.getByTestId("nav-item-authentication").click();
+}
