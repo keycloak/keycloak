188db71675de41a738227ac47728bdcdd75c82cf
Erik Jan de Wit <erikjan.dewit@gmail.com>
2024-12-19 14:03:15 +0100

    added axe tests for flows
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- js/apps/admin-ui/test/autentication/flow.ts
+++ js/apps/admin-ui/test/autentication/flow.ts
@@ -1,5 +1,7 @@
 import { Page, expect } from "@playwright/test";
 import { confirmModal } from "../utils/modal";
+import AxeBuilder from "@axe-core/playwright";
+import { selectItem } from "../utils/form";
 
 export async function fillDuplicateFlowModal(
   page: Page,
@@ -30,6 +32,29 @@ export async function goToRequiredActions(page: Page) {
   await page.getByTestId("requiredActions").click();
 }
 
+export async function goToPoliciesTab(page: Page) {
+  await page.getByTestId("policies").click();
+}
+
+export async function goToOTPPolicyTab(page: Page) {
+  await goToPoliciesTab(page);
+  await page.getByTestId("otpPolicy").click();
+}
+
+export async function goToWebAuthnTab(page: Page) {
+  await goToPoliciesTab(page);
+  await page.getByTestId("webauthnPolicy").click();
+}
+
+export async function goToCIBAPolicyTab(page: Page) {
+  await goToPoliciesTab(page);
+  await page.getByTestId("tab-ciba-policy").click();
+}
+
+export async function addPolicy(page: Page, value) {
+  await selectItem(page, page.getByTestId("add-policy"), value);
+}
+
 const toKey = (name: string) => {
   return name.replace(/\s/g, "-");
 };
@@ -54,12 +79,33 @@ export async function assertSwitchPolicyChecked(
   page: Page,
   policyName: string,
 ) {
-  expect(getEnabledSwitch(page, policyName)).toBeChecked();
+  await expect(getEnabledSwitch(page, policyName)).toBeChecked();
 }
 
 export async function assertDefaultSwitchPolicyEnabled(
   page: Page,
   policyName: string,
 ) {
-  expect(getDefaultSwitch(page, policyName)).toBeEnabled();
+  await expect(getDefaultSwitch(page, policyName)).toBeEnabled();
+}
+
+export async function goToCreateItem(page: Page) {
+  await page.getByRole("link", { name: "Create flow" }).click();
+}
+
+export async function assertAxeViolations(page: Page) {
+  const { violations } = await new AxeBuilder({ page }).analyze();
+  expect(violations.length, violations.map((v) => v.help).join("\n")).toBe(0);
+}
+
+export async function fillCreateForm(
+  page: Page,
+  name: string,
+  description: string,
+  type: string,
+) {
+  await page.getByTestId("alias").fill(name);
+  await page.getByTestId("description").fill(description);
+  await selectItem(page, page.getByLabel("Flow type"), type);
+  await page.getByTestId("create").click();
 }
