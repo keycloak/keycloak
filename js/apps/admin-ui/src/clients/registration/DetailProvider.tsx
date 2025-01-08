0a546bdf126d6d6e666e3ca6bdb08004d208586d
Erik Jan de Wit <erikjan.dewit@gmail.com>
2025-01-08 19:45:14 +0100

    added registration policies test
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- js/apps/admin-ui/src/clients/registration/DetailProvider.tsx
+++ js/apps/admin-ui/src/clients/registration/DetailProvider.tsx
@@ -148,6 +148,7 @@ export default function DetailProvider() {
                 {t("save")}
               </Button>
               <Button
+                data-testid="cancel"
                 variant="link"
                 component={(props) => (
                   <Link
