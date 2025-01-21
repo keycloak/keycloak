2abf30f5c7be0432e0274d72df39f71a08000975
Agnieszka Gancarczyk <agagancarczyk@gmail.com>
2025-01-21 11:48:30 +0000

    improved permissions list table
    Signed-off-by: Agnieszka Gancarczyk <agagancarczyk@gmail.com>

---
+++ js/apps/admin-ui/src/permissions-configuration/permission-configuration/AuthorizationScopesDetails.tsx
@@ -0,0 +1,61 @@
+import { useTranslation } from "react-i18next";
+import { MoreLabel } from "../../clients/authorization/MoreLabel";
+import { capitalize } from "lodash-es";
+import { Label, Popover } from "@patternfly/react-core";
+
+type AuthorizationScopesDetailsProps = {
+  row: {
+    associatedScopes?: { name: string }[];
+  };
+};
+
+export const AuthorizationScopesDetails = ({
+  row,
+}: AuthorizationScopesDetailsProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <Popover
+        aria-label="Authorization scopes popover"
+        position="top"
+        hasAutoWidth
+        bodyContent={
+          <div>
+            <p style={{ fontSize: "16px" }}>
+              <strong>{t("authorizationScopeDetailsTitle")}</strong>
+            </p>
+            <br />
+            <p>{t("authorizationScopeDetailsSubtitle")}</p>
+            <br />
+            <table>
+              <tbody>
+                <tr>
+                  <td style={{ paddingRight: "15px", paddingBottom: "10px" }}>
+                    <strong>{t("authorizationScopeDetailsName")}</strong>
+                  </td>
+                  <td style={{ paddingBottom: "10px" }}>
+                    {capitalize(row.associatedScopes?.[0]?.name)}
+                  </td>
+                </tr>
+                <tr>
+                  <td style={{ paddingRight: "15px", paddingBottom: "10px" }}>
+                    <strong>{t("authorizationScopeDetailsDescription")}</strong>
+                  </td>
+                  <td style={{ paddingBottom: "10px" }}>
+                    {t("authorizationScopeDetailsDescriptionText")}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        }
+      >
+        <Label color="blue">
+          {capitalize(row.associatedScopes?.[0]?.name)}{" "}
+        </Label>
+      </Popover>
+      <MoreLabel array={row.associatedScopes} />
+    </>
+  );
+};
