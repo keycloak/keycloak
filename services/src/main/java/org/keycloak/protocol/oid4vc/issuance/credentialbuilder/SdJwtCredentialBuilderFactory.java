b1e3d500453cd19b04a38946d449d8cef97c1321
Enrico Costanzi <enrico.costanzi@staff.aruba.it>
2025-01-27 10:20:41 +0100

    Return empty configProperties for CredentialBuilderFactory implementations
    Closes #36826
    
    Signed-off-by: Enrico Costanzi <enrico.costanzi@staff.aruba.it>

--- services/src/main/java/org/keycloak/protocol/oid4vc/issuance/credentialbuilder/SdJwtCredentialBuilderFactory.java
+++ services/src/main/java/org/keycloak/protocol/oid4vc/issuance/credentialbuilder/SdJwtCredentialBuilderFactory.java
@@ -17,6 +17,7 @@
 
 package org.keycloak.protocol.oid4vc.issuance.credentialbuilder;
 
+import java.util.ArrayList;
 import org.keycloak.component.ComponentModel;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.protocol.oid4vc.issuance.OID4VCIssuerWellKnownProvider;
@@ -30,6 +31,8 @@ import java.util.List;
  */
 public class SdJwtCredentialBuilderFactory implements CredentialBuilderFactory {
 
+    protected static final List<ProviderConfigProperty> configProperties = new ArrayList<>();
+
     @Override
     public String getSupportedFormat() {
         return Format.SD_JWT_VC;
@@ -42,7 +45,7 @@ public class SdJwtCredentialBuilderFactory implements CredentialBuilderFactory {
 
     @Override
     public List<ProviderConfigProperty> getConfigProperties() {
-        return null;
+        return configProperties;
     }
 
     @Override
