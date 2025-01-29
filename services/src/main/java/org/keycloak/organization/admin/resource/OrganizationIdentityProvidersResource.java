6a7eed36d1b817eef9886fa40670cc7651013ab6
Borja Domínguez <iam.n3uro@gmail.com>
2025-01-29 12:35:15 +0100

    Update OrganizationIdentityProvidersResource.java
    Signed-off-by: Borja Domínguez <iam.n3uro@gmail.com>

--- services/src/main/java/org/keycloak/organization/admin/resource/OrganizationIdentityProvidersResource.java
+++ services/src/main/java/org/keycloak/organization/admin/resource/OrganizationIdentityProvidersResource.java
@@ -32,7 +32,10 @@ import jakarta.ws.rs.ext.Provider;
 import java.util.stream.Stream;
 
 import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
 import org.eclipse.microprofile.openapi.annotations.extensions.Extension;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
