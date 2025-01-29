025de01a2c93f7aedb70b6949b87fde42f2f5251
akbar1214 <43771058+akbar1214@users.noreply.github.com>
2025-01-29 18:06:08 +0530

    Update metadata for  BrowserSecurityHeaders.java
    Co-authored-by: Jon Koops <jonkoops@gmail.com>
    Signed-off-by: akbar1214 <43771058+akbar1214@users.noreply.github.com>

--- server-spi-private/src/main/java/org/keycloak/models/BrowserSecurityHeaders.java
+++ server-spi-private/src/main/java/org/keycloak/models/BrowserSecurityHeaders.java
@@ -28,7 +28,8 @@ public enum BrowserSecurityHeaders {
     CONTENT_SECURITY_POLICY_REPORT_ONLY("contentSecurityPolicyReportOnly", "Content-Security-Policy-Report-Only", ""),
     X_CONTENT_TYPE_OPTIONS("xContentTypeOptions", "X-Content-Type-Options", "nosniff"),
     X_ROBOTS_TAG("xRobotsTag", "X-Robots-Tag", "none"),
-    @Deprecated // This header is deprecated and marked for removal. See: https://github.com/keycloak/keycloak/issues/21728
+    /** This header is deprecated and marked for removal (see <a href="https://github.com/keycloak/keycloak/issues/21728">#21728</a>) */
+    @Deprecated(since = "26.2.0", forRemoval = true)
     X_XSS_PROTECTION("xXSSProtection", "X-XSS-Protection", "1; mode=block"),
     STRICT_TRANSPORT_SECURITY("strictTransportSecurity", "Strict-Transport-Security", "max-age=31536000; includeSubDomains"),
     REFERRER_POLICY("referrerPolicy", "Referrer-Policy", "no-referrer");
