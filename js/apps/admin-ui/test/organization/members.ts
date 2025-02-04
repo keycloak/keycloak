fc5f119aff5ec2c3b332b8f8d0fdf05de140d51f
Erik Jan de Wit <erikjan.dewit@gmail.com>
2025-02-04 11:29:24 +0100

    added organizations test
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

---
+++ js/apps/admin-ui/test/organization/members.ts
@@ -0,0 +1,9 @@
+import { Page } from "@playwright/test";
+
+export async function goToMembersTab(page: Page) {
+  await page.getByTestId("membersTab").click();
+}
+
+export async function clickAddRealmUser(page: Page) {
+  await page.getByTestId("add-realm-user-empty-action").click();
+}
