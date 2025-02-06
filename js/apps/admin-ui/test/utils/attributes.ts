424cb0eec3c17352587196c2e14b59dad14711e2
Erik Jan de Wit <erikjan.dewit@gmail.com>
2025-02-06 11:06:41 +0100

    added realm roles
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- js/apps/admin-ui/test/utils/attributes.ts
+++ js/apps/admin-ui/test/utils/attributes.ts
@@ -4,14 +4,20 @@ export async function goToAttributesTab(page: Page) {
   await page.getByTestId("attributesTab").click();
 }
 
+function getAttribute(page: Page, index: number, key: string) {
+  return page.locator(`input[name="attributes\\.${index}\\.${key}"]`);
+}
+
 export async function fillAttributeData(
   page: Page,
   key: string,
   value: string,
+  index: number = 0,
 ) {
   await page.getByTestId("attributes-add-row").click();
-  await page.getByTestId("attributes-key").fill(key);
-  await page.getByTestId("attributes-value").fill(value);
+
+  await getAttribute(page, index, "key").fill(key);
+  await getAttribute(page, index, "value").fill(value);
 }
 
 export async function deleteAttribute(page: Page, row: number) {
@@ -25,4 +31,14 @@ export async function clickAttributeSaveButton(page: Page) {
 export async function assertAttributeLength(page: Page, length: number) {
   const rows = await page.getByTestId("attributes-key").all();
   expect(rows.length).toBe(length);
+}
+
+export async function assertAttribute(
+  page: Page,
+  key: string,
+  value: string,
+  index: number = 0,
+) {
+  await expect(getAttribute(page, index, "key")).toHaveValue(key);
+  await expect(getAttribute(page, index, "value")).toHaveValue(value);
 }
