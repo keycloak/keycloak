01f954914999f2a262865a73e7ccb6e72e48a652
Agnieszka Gancarczyk <agagancarczyk@gmail.com>
2025-02-12 17:49:35 +0000

    Adding support for Clients and Groups resource types
    Signed-off-by: Agnieszka Gancarczyk <agagancarczyk@gmail.com>

--- js/apps/admin-ui/src/permissions-configuration/permission-configuration/ResourceType.tsx
+++ js/apps/admin-ui/src/permissions-configuration/permission-configuration/ResourceType.tsx
@@ -3,9 +3,13 @@ import { FormGroup, Radio } from "@patternfly/react-core";
 import { HelpItem } from "@keycloak/keycloak-ui-shared";
 import { useFormContext } from "react-hook-form";
 import { useState } from "react";
-import { UserSelect } from "../../../components/users/UserSelect";
+import { UserSelect } from "../../components/users/UserSelect";
 
-export const Users = () => {
+type PermissionTypeProps = {
+  resourceType: string;
+};
+
+export const ResourceType = ({ resourceType }: PermissionTypeProps) => {
   const { t } = useTranslation();
   const form = useFormContext();
   const resourceIds: string[] = form.getValues("resources");
@@ -18,23 +22,23 @@ export const Users = () => {
   return (
     <>
       <FormGroup
-        label={t("applyPermissionTo")}
+        label={t("EnforceAccessTo")}
         labelIcon={
           <HelpItem
-            helpText={t("applyPermissionToHelpText")}
+            helpText={t("enforceAccessToHelpText")}
             fieldLabelId="apply-permission-to"
           />
         }
-        fieldId="applyPermissionTo"
+        fieldId="EnforceAccessTo"
         hasNoPaddingTop
         isRequired
       >
         <Radio
-          id="allUsers"
-          data-testid="allUsers"
+          id="allResources"
+          data-testid="allResources"
           isChecked={!isSpecificUsers}
-          name="applyPermissionTo"
-          label={t("allUsers")}
+          name="EnforceAccessTo"
+          label={t(`allResourceType`, { resourceType })}
           onChange={() => {
             setIsSpecificUsers(false);
             form.setValue("resources", []);
@@ -42,11 +46,11 @@ export const Users = () => {
           className="pf-v5-u-mb-md"
         />
         <Radio
-          id="specificUsers"
-          data-testid="specificUsers"
+          id="specificResources"
+          data-testid="specificResources"
           isChecked={isSpecificUsers}
-          name="applyPermissionTo"
-          label={t("specificUsers")}
+          name="EnforceAccessTo"
+          label={t(`specificResourceType`, { resourceType })}
           onChange={() => {
             setIsSpecificUsers(true);
             form.setValue("resources", []);
