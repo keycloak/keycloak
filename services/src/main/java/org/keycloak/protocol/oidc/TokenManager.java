44f18467d5afc1db775572e8103cc8a1c300a5f0
Pedro Igor <pigor.craveiro@gmail.com>
2025-02-14 12:38:22 -0300

    Only set organization to client session when re-authenticating if user is member of the mapped organization
    Closes #37169
    
    Signed-off-by: Pedro Igor <pigor.craveiro@gmail.com>

--- services/src/main/java/org/keycloak/protocol/oidc/TokenManager.java
+++ services/src/main/java/org/keycloak/protocol/oidc/TokenManager.java
@@ -731,6 +731,10 @@ public class TokenManager {
                 return null;
             }
 
+            if (user != null && orgScope.resolveOrganizations(user, scopeParam, session).findAny().isEmpty()) {
+                return null;
+            }
+
             return orgScope.toClientScope(name, user, session);
         }
 
