4bcd975d888aed24e6707a2be5b79d0e0633b7f1
Agnieszka Gancarczyk <agagancarczyk@gmail.com>
2025-02-27 13:39:49 +0000

    Cleanup
    Signed-off-by: Agnieszka Gancarczyk <agagancarczyk@gmail.com>

--- js/apps/admin-ui/src/permissions-configuration/permission-evaluation/PermissionEvaluationResult.tsx
+++ js/apps/admin-ui/src/permissions-configuration/permission-evaluation/PermissionEvaluationResult.tsx
@@ -1,7 +1,7 @@
 import PolicyEvaluationResponse from "@keycloak/keycloak-admin-client/lib/defs/policyEvaluationResponse";
+import { useMemo } from "react";
 import { Alert, List, ListItem, Text } from "@patternfly/react-core";
 import { useTranslation } from "react-i18next";
-import { useMemo } from "react";
 import { sortBy } from "lodash-es";
 
 type PermissionEvaluationResultProps = {
@@ -12,36 +12,43 @@ export const PermissionEvaluationResult = ({
   evaluateResult,
 }: PermissionEvaluationResultProps) => {
   const { t } = useTranslation();
-  const alertTitle = evaluateResult?.results?.[0]?.resource?.name!;
+  const evaluatedResults = evaluateResult?.results || [];
+  const evaluatedResult = evaluatedResults[0] || {};
+  const alertTitle =
+    evaluatedResult?.resource?.name ?? t("permissionEvaluationAlertTitle");
   const alertVariant =
     evaluateResult?.status === "PERMIT" ? "success" : "warning";
   const alertMessage =
-    evaluateResult?.status === "PERMIT" ? t("grantedScope") : t("deniedScope");
+    evaluateResult?.status === "PERMIT"
+      ? t("grantedScope")
+      : evaluateResult?.status === "DENY" && evaluatedResults.length === 0
+        ? ""
+        : t("deniedScope");
 
-  const evaluatedAllowedScopes = useMemo(() => {
-    return sortBy(evaluateResult?.results?.[0]?.allowedScopes || [], "name");
-  }, [evaluateResult?.results]);
-
-  const evaluatedDeniedScopes = useMemo(() => {
-    return sortBy(evaluateResult?.results?.[0]?.deniedScopes || [], "name");
-  }, [evaluateResult?.results]);
-
-  const evaluatedPolicies = useMemo(() => {
-    return sortBy(evaluateResult?.results?.[0]?.policies || [], "name");
-  }, [evaluateResult?.results]);
+  const evaluatedAllowedScopes = useMemo(
+    () => sortBy(evaluatedResult?.allowedScopes || [], "name"),
+    [evaluatedResult],
+  );
+  const evaluatedDeniedScopes = useMemo(
+    () => sortBy(evaluatedResult?.deniedScopes || [], "name"),
+    [evaluatedResult],
+  );
+  const evaluatedPolicies = useMemo(
+    () => sortBy(evaluatedResult?.policies || [], "name"),
+    [evaluatedResult],
+  );
 
   return (
     <Alert isInline variant={alertVariant} title={alertTitle} component="h6">
       <Text>{alertMessage}</Text>
 
-      {evaluateResult?.status === "PERMIT" &&
-        evaluatedAllowedScopes.length > 0 && (
-          <List className="pf-v5-u-mt-sm">
-            {evaluatedAllowedScopes.map((scope) => (
-              <ListItem key={scope.id}>{scope.name}</ListItem>
-            ))}
-          </List>
-        )}
+      {evaluatedAllowedScopes.length > 0 && (
+        <List className="pf-v5-u-mt-sm">
+          {evaluatedAllowedScopes.map((scope) => (
+            <ListItem key={scope.id}>{scope.name}</ListItem>
+          ))}
+        </List>
+      )}
 
       {evaluatedDeniedScopes.length > 0 && (
         <List className="pf-v5-u-mt-sm">
