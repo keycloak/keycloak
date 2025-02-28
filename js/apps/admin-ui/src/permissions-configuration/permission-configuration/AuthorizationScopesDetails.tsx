8c84526c0800cc7099ad759f13ea53cadf1da74a
Agnieszka Gancarczyk <agagancarczyk@gmail.com>
2025-02-28 11:24:27 +0000

    Fixed show more/show less in permissions table
    Signed-off-by: Agnieszka Gancarczyk <agagancarczyk@gmail.com>

--- js/apps/admin-ui/src/permissions-configuration/permission-configuration/AuthorizationScopesDetails.tsx
+++ js/apps/admin-ui/src/permissions-configuration/permission-configuration/AuthorizationScopesDetails.tsx
@@ -1,7 +1,6 @@
 import { useTranslation } from "react-i18next";
-import { MoreLabel } from "../../clients/authorization/MoreLabel";
 import { capitalize } from "lodash-es";
-import { Label, Popover } from "@patternfly/react-core";
+import { Label, LabelGroup, Popover } from "@patternfly/react-core";
 
 type AuthorizationScopesDetailsProps = {
   row: {
@@ -15,50 +14,54 @@ export const AuthorizationScopesDetails = ({
 }: AuthorizationScopesDetailsProps) => {
   const { t } = useTranslation();
 
+  const associatedScopes = row.associatedScopes || [];
+
   return (
-    <>
-      <Popover
-        aria-label="Authorization scopes popover"
-        position="top"
-        hasAutoWidth
-        bodyContent={
-          <>
-            <p style={{ fontSize: "16px" }}>
-              <strong>{t("authorizationScopeDetailsTitle")}</strong>
-            </p>
-            <br />
-            <p>{t("authorizationScopeDetailsSubtitle")}</p>
-            <br />
-            <table>
-              <tbody>
-                <tr>
-                  <td style={{ paddingRight: "15px", paddingBottom: "10px" }}>
-                    <strong>{t("authorizationScopeDetailsName")}</strong>
-                  </td>
-                  <td style={{ paddingBottom: "10px" }}>
-                    {capitalize(row.associatedScopes?.[0]?.name)}
-                  </td>
-                </tr>
-                <tr>
-                  <td style={{ paddingRight: "15px", paddingBottom: "10px" }}>
-                    <strong>{t("authorizationScopeDetailsDescription")}</strong>
-                  </td>
-                  <td style={{ paddingBottom: "10px" }}>
-                    {t(
-                      `authorizationScope.${row.resourceType}.${row.associatedScopes?.[0]?.name}`,
-                    )}
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </>
-        }
-      >
-        <Label color="blue">
-          {capitalize(row.associatedScopes?.[0]?.name)}{" "}
-        </Label>
-      </Popover>
-      <MoreLabel array={row.associatedScopes} />
-    </>
+    <LabelGroup>
+      {associatedScopes.map((scope, index) => (
+        <Popover
+          key={index}
+          aria-label={`Authorization scope popover for ${scope.name}`}
+          position="top"
+          hasAutoWidth
+          bodyContent={
+            <>
+              <p style={{ fontSize: "16px" }}>
+                <strong>{t("authorizationScopeDetailsTitle")}</strong>
+              </p>
+              <br />
+              <p>{t("authorizationScopeDetailsSubtitle")}</p>
+              <br />
+              <table>
+                <tbody>
+                  <tr>
+                    <td style={{ paddingRight: "15px", paddingBottom: "10px" }}>
+                      <strong>{t("authorizationScopeDetailsName")}</strong>
+                    </td>
+                    <td style={{ paddingBottom: "10px" }}>
+                      {capitalize(scope.name)}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td style={{ paddingRight: "15px", paddingBottom: "10px" }}>
+                      <strong>
+                        {t("authorizationScopeDetailsDescription")}
+                      </strong>
+                    </td>
+                    <td style={{ paddingBottom: "10px" }}>
+                      {t(
+                        `authorizationScope.${row.resourceType}.${scope.name}`,
+                      )}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </>
+          }
+        >
+          <Label color="blue">{capitalize(scope.name)}</Label>
+        </Popover>
+      ))}
+    </LabelGroup>
   );
 };
