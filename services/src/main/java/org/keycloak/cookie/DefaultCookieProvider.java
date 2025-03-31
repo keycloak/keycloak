32c63ac9da936004e55846fa3001bf991dfc4e28
Steve Hawkins <shawkins@redhat.com>
2025-03-31 09:42:55 -0400

    fix: adds validation over proxy and hostname settings
    also improves the handling and message around insecure contexts
    
    closes: #38091
    
    Signed-off-by: Steve Hawkins <shawkins@redhat.com>

--- services/src/main/java/org/keycloak/cookie/DefaultCookieProvider.java
+++ services/src/main/java/org/keycloak/cookie/DefaultCookieProvider.java
@@ -18,6 +18,7 @@ public class DefaultCookieProvider implements CookieProvider {
     private final CookiePathResolver pathResolver;
 
     private final boolean secure;
+    private boolean warned;
 
     private final Map<String, Cookie> cookies;
 
@@ -31,10 +32,6 @@ public class DefaultCookieProvider implements CookieProvider {
 
         if (logger.isTraceEnabled()) {
             logger.tracef("Received cookies: %s, path: %s", String.join(", ", this.cookies.keySet()), context.getUri().getRequestUri().getRawPath());
-        }
-
-        if (!secure) {
-            logger.warnf("Non-secure context detected; cookies are not secured, and will not be available in cross-origin POST requests");
         }
 
         expireOldUnusedCookies();
@@ -73,6 +70,32 @@ public class DefaultCookieProvider implements CookieProvider {
         session.getContext().getHttpResponse().setCookieIfAbsent(newCookie);
 
         logger.tracef("Setting cookie: name: %s, path: %s, same-site: %s, secure: %s, http-only: %s, max-age: %d", name, path, sameSite, secure, httpOnly, maxAge);
+
+        if (!secure && !warned) {
+            warned = true;
+
+            StringBuilder warning = new StringBuilder("Non-secure context detected; cookies are not secured, and will not be available in cross-origin POST requests.");
+
+            String forwarded = session.getContext().getRequestHeaders().getHeaderString("Forwarded");
+            String xForwarded = session.getContext().getRequestHeaders().getHeaderString("X-Forwarded");
+
+            // if we are getting here then:
+            //   if passthrough or reencrypt, proxy-headers may be misconfigured
+            //   if edge, then proxy-headers should be set (there's hostname validation for this) but they are not set by the proxy server
+            //   not proxied, and not https (there's hostname validation for this)
+
+            if (forwarded != null || xForwarded != null) {
+                if (session.getContext().getHttpRequest().isProxyTrusted()) {
+                    warning.append(" Please review your proxy settings as the request appears to have originated from a proxy.");
+                } else {
+                    warning.append(" This is likely due to the proxy not being trusted.");
+                }
+            } else {
+                warning.append(" Please review whether this direct HTTP usage is expected.");
+            }
+
+            logger.warnf(warning.toString());
+        }
     }
 
     @Override
