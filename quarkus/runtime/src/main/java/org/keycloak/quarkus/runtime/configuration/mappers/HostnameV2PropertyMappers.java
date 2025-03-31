3c9a744c045759a3668bc132d1f5ea506db27266
Steve Hawkins <shawkins@redhat.com>
2025-03-31 09:42:55 -0400

    fix: adds validation over proxy and hostname settings
    also improves the handling and message around insecure contexts
    
    closes: #38091
    
    Signed-off-by: Steve Hawkins <shawkins@redhat.com>

--- quarkus/runtime/src/main/java/org/keycloak/quarkus/runtime/configuration/mappers/HostnameV2PropertyMappers.java
+++ quarkus/runtime/src/main/java/org/keycloak/quarkus/runtime/configuration/mappers/HostnameV2PropertyMappers.java
@@ -2,14 +2,19 @@ package org.keycloak.quarkus.runtime.configuration.mappers;
 
 import static org.keycloak.quarkus.runtime.configuration.mappers.PropertyMapper.fromOption;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
 import org.keycloak.common.Profile;
 import org.keycloak.config.HostnameV2Options;
+import org.keycloak.quarkus.runtime.Environment;
 import org.keycloak.quarkus.runtime.cli.Picocli;
+import org.keycloak.config.ProxyOptions;
 import org.keycloak.quarkus.runtime.configuration.Configuration;
+import org.keycloak.utils.SecureContextResolver;
 
 public final class HostnameV2PropertyMappers {
 
@@ -40,6 +45,52 @@ public final class HostnameV2PropertyMappers {
 
         if (!inUse.isEmpty()) {
             picocli.warn("Hostname v1 options %s are still in use, please review your configuration".formatted(inUse));
+        }
+        boolean httpsEnabled = HttpPropertyMappers.isHttpsEnabled();
+        String host = Configuration.getConfigValue(HostnameV2Options.HOSTNAME).getValue();
+        if (host != null && validateFullHostname(httpsEnabled, host, picocli)) {
+            return;
+        }
+        if (httpsEnabled) {
+            // must be tls passthrough, but there's no way of knowing the intention
+            return;
+        }
+        String proxyHeaders = Configuration.getConfigValue(ProxyOptions.PROXY_HEADERS).getValue();
+        if (proxyHeaders != null) {
+            // must not be tls passthrough, but there's no way of knowing the intention
+            return;
+        }
+        if (host == null) {
+            String message = "With HTTPS not enabled and hostname-strict=false, cross-origin cookies will not be allowed.";
+            if (Environment.isDevProfile()) {
+                picocli.info(message);
+            } else {
+                picocli.warn(message);
+            }
+        } else if (!SecureContextResolver.isLocal(host)) {
+            picocli.warn("HTTPS is not enabled on the server, either the `hostname` should be set to a full URL or `proxy-headers` should be used. This is likely a misconfiguration.");
+        } // else warn on prod
+    }
+
+    static boolean validateFullHostname(boolean httpsEnabled, String host, Picocli picocli) {
+        try {
+            URL url = new URL(host);
+
+            if (!url.getProtocol().toUpperCase().equals("HTTPS")) {
+                if (!SecureContextResolver.isLocal(url.getHost())) {
+                    picocli.warn("`hostname` is set to an HTTP hostname, cross-origin cookies will not be allowed. This is likely a misconfiguration.");
+
+                    // TODO: any hostname-admin specific validation?
+
+                } // else warn on prod?
+                if (httpsEnabled) {
+                    picocli.warn("HTTPS is enabled on the server, but `hostname` specifies HTTP. This is likely a misconfiguration.");
+                }
+            }
+
+            return true;
+        } catch (MalformedURLException e) {
+            return false;
         }
     }
 
