1c9d3a258c0a83ac33db16b17e12c6616a4798e0
Alexander Schwartz <aschwart@redhat.com>
2025-04-02 11:42:43 +0200

    Review - adding additional comments and curly braces, plus a rollback
    Closes #38550
    
    Signed-off-by: Alexander Schwartz <aschwart@redhat.com>

--- quarkus/runtime/src/main/java/org/keycloak/quarkus/runtime/storage/infinispan/jgroups/impl/KEYCLOAK_JDBC_PING2.java
+++ quarkus/runtime/src/main/java/org/keycloak/quarkus/runtime/storage/infinispan/jgroups/impl/KEYCLOAK_JDBC_PING2.java
@@ -6,22 +6,35 @@ import org.jgroups.protocols.PingData;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+/**
+ * Enhanced JDBC_PING2 to handle entries transactionally.
+ * <p>
+ * Workaround for issue <a href="https://issues.redhat.com/browse/JGRP-2870">JGRP-2870</a>
+ */
 public class KEYCLOAK_JDBC_PING2 extends JDBC_PING2 {
     @Override
     protected void writeToDB(PingData data, String clustername) throws SQLException {
         lock.lock();
-        try(Connection connection=getConnection()) {
+        try (Connection connection = getConnection()) {
             if(call_insert_sp != null && insert_sp != null)
                 callInsertStoredProcedure(connection, data, clustername);
             else {
                 boolean isAutocommit = connection.getAutoCommit();
                 try {
-                    if (isAutocommit) connection.setAutoCommit(false);
+                    if (isAutocommit) {
+                        // Always use a transaction for the delete+insert to make it atomic
+                        // to avoid the short moment where there is no entry in the table.
+                        connection.setAutoCommit(false);
+                    }
                     delete(connection, clustername, data.getAddress());
                     insert(connection, data, clustername);
-                    if (isAutocommit) connection.commit();
+                    connection.commit();
+                } catch (SQLException e) {
+                    connection.rollback();
                 } finally {
-                    connection.setAutoCommit(isAutocommit);
+                    if (isAutocommit) {
+                        connection.setAutoCommit(isAutocommit);
+                    }
                 }
             }
         } finally {
