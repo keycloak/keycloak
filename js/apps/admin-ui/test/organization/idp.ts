a4d551778665f70490852f5890e66f5705466325
Erik Jan de Wit <erikjan.dewit@gmail.com>
2025-04-15 15:10:31 +0200

    fix tests
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- js/apps/admin-ui/test/organization/idp.ts
+++ js/apps/admin-ui/test/organization/idp.ts
@@ -19,5 +19,9 @@ export async function fillForm(
   },
 ) {
   await selectItem(page, page.getByTestId("alias"), data.name);
-  await selectItem(page, page.locator("#kc🍺org🍺domain"), data.domain);
+  await selectItem(
+    page,
+    page.getByRole("button", { name: "Domain" }),
+    data.domain,
+  );
 }
