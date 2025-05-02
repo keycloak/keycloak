10416f28d414f96d3aa2602d6f39bffee285432b
Erik Jan de Wit <erikjan.dewit@gmail.com>
2025-05-02 08:16:31 +0200

    in development mode use the message.properties as file
    fixes: #39405
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- js/apps/admin-ui/src/i18n/i18n.ts
+++ js/apps/admin-ui/src/i18n/i18n.ts
@@ -6,6 +6,9 @@ import { initReactI18next } from "react-i18next";
 import { environment } from "../environment";
 import { joinPath } from "../utils/joinPath";
 
+// @ts-ignore imported by rollup plugin
+import code from "message-bundle";
+
 type KeyValue = { key: string; value: string };
 
 export const DEFAULT_LOCALE = "en";
@@ -26,8 +29,10 @@ export const i18n: i18nType = createInstance({
       `resources/{{ns}}/admin/{{lng}}`,
     ),
     parse: (data: string) => {
+      if (process.env.NODE_ENV === "development") {
+        return code;
+      }
       const messages: KeyValue[] = JSON.parse(data);
-
       return Object.fromEntries(messages.map(({ key, value }) => [key, value]));
     },
   },
