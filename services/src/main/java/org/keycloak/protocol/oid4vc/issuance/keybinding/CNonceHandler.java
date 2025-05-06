27ae9e58d8df4b66819b26d34fe3683b8565def9
Pascal Knüppel <pascal.knueppel@governikus.de>
2025-05-06 09:48:19 +0200

    Add nonce endpoint
    fixes #39272
    
    Signed-off-by: Pascal Knüppel <pascal.knueppel@governikus.de>

---
+++ services/src/main/java/org/keycloak/protocol/oid4vc/issuance/keybinding/CNonceHandler.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2025 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.keycloak.protocol.oid4vc.issuance.keybinding;
+
+import jakarta.annotation.Nullable;
+import org.keycloak.provider.Provider;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Pascal Knüppel
+ */
+public interface CNonceHandler extends Provider {
+
+
+    public String buildCNonce(List<String> audiences, @Nullable Map<String, Object> additionalDetails);
+
+    public void verifyCNonce(String cNonce, List<String> audiences, @Nullable Map<String, Object> additionalDetails);
+
+    @Override
+    default void close() {
+        // do nothing
+    }
+}
