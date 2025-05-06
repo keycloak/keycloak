d676836bcecf6e5288e12fa292e4308cc7f560d6
Martin Bartoš <mabartos@redhat.com>
2025-05-06 17:43:42 +0200

    [Admin API v2] Skeleton prototype (#39322)
    * Add new ClientRepresentation
    
    Co-authored-by: Peter Zaoral <pzaoral@redhat.com>
    Co-authored-by: Václav Muzikář <vmuzikar@redhat.com>
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>
    
    * Add APIs
    
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>
    
    * Add ApiModelMapper SPI
    
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>
    
    * Add MapStruct as default ApiModelMapper
    
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>
    
    * Add default APIs implementations
    
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>
    
    * Provide Service SPI and ClientService
    
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>
    
    * Add default Keycloak services and Client service
    
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>
    
    * Add ModelMapper to shared modules
    
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>
    
    * Implement Client service, add ServiceException class
    
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>
    
    * Use ClientService in Client REST API
    
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>
    
    * Update rest/admin-api/src/main/java/org/keycloak/admin/api/client/ClientsApi.java
    
    Co-authored-by: Václav Muzikář <vaclav@muzikari.cz>
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>
    
    * Fix ModelMapperSpi
    
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>
    
    * Use /admin/api/v2 as a root path
    
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>
    
    * Support latest API version by default
    
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>
    
    * Rename path param to comply with API spec
    
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>
    
    ---------
    
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>
    Co-authored-by: Peter Zaoral <pzaoral@redhat.com>
    Co-authored-by: Václav Muzikář <vmuzikar@redhat.com>
    Co-authored-by: Václav Muzikář <vaclav@muzikari.cz>

---
+++ core/src/main/java/org/keycloak/representations/admin/v2/ClientRepresentation.java
@@ -0,0 +1,225 @@
+package org.keycloak.representations.admin.v2;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+
+import java.util.Set;
+
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class ClientRepresentation {
+
+    @JsonProperty(required = true)
+    @JsonPropertyDescription("ID uniquely identifying this client")
+    private String clientId;
+
+    @JsonPropertyDescription("Human readable name of the client")
+    private String displayName;
+
+    @JsonPropertyDescription("Human readable description of the client")
+    private String description;
+
+    @JsonPropertyDescription("The protocol used to communicate with the client")
+    private String protocol;
+
+    @JsonPropertyDescription("Whether this client is enabled")
+    private Boolean enabled;
+
+    @JsonPropertyDescription("URL to the application's homepage that is represented by this client")
+    private String appUrl;
+
+    @JsonPropertyDescription("URLs that the browser can redirect to after login")
+    private Set<String> appRedirectUrls;
+
+    @JsonPropertyDescription("Login flows that are enabled for this client")
+    private Set<String> loginFlows;
+
+    @JsonPropertyDescription("Authentication configuration for this client")
+    private Auth auth;
+
+    @JsonPropertyDescription("Web origins that are allowed to make requests to this client")
+    private Set<String> webOrigins;
+
+    @JsonPropertyDescription("Roles associated with this client")
+    private Set<String> roles;
+
+    @JsonPropertyDescription("Service account configuration for this client")
+    private ServiceAccount serviceAccount;
+
+    public ClientRepresentation() {}
+
+    public ClientRepresentation(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public void setProtocol(String protocol) {
+        this.protocol = protocol;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getAppUrl() {
+        return appUrl;
+    }
+
+    public void setAppUrl(String appUrl) {
+        this.appUrl = appUrl;
+    }
+
+    public Set<String> getAppRedirectUrls() {
+        return appRedirectUrls;
+    }
+
+    public void setAppRedirectUrls(Set<String> appRedirectUrls) {
+        this.appRedirectUrls = appRedirectUrls;
+    }
+
+    public Set<String> getLoginFlows() {
+        return loginFlows;
+    }
+
+    public void setLoginFlows(Set<String> loginFlows) {
+        this.loginFlows = loginFlows;
+    }
+
+    public Auth getAuth() {
+        return auth;
+    }
+
+    public void setAuth(Auth auth) {
+        this.auth = auth;
+    }
+
+    public Set<String> getWebOrigins() {
+        return webOrigins;
+    }
+
+    public void setWebOrigins(Set<String> webOrigins) {
+        this.webOrigins = webOrigins;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
+
+    public ServiceAccount getServiceAccount() {
+        return serviceAccount;
+    }
+
+    public void setServiceAccount(ServiceAccount serviceAccount) {
+        this.serviceAccount = serviceAccount;
+    }
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    public static class Auth {
+
+        @JsonPropertyDescription("Whether authentication is enabled for this client")
+        private Boolean enabled;
+
+        @JsonPropertyDescription("Which authentication method is used for this client")
+        private String method;
+
+        @JsonPropertyDescription("Secret used to authenticate this client with Secret authentication")
+        private String secret;
+
+        @JsonPropertyDescription("Public key used to authenticate this client with Signed JWT authentication")
+        private String certificate;
+
+        public Boolean getEnabled() {
+            return enabled;
+        }
+
+        public void setEnabled(Boolean enabled) {
+            this.enabled = enabled;
+        }
+
+        public String getMethod() {
+            return method;
+        }
+
+        public void setMethod(String method) {
+            this.method = method;
+        }
+
+        public String getSecret() {
+            return secret;
+        }
+
+        public void setSecret(String secret) {
+            this.secret = secret;
+        }
+
+        public String getCertificate() {
+            return certificate;
+        }
+
+        public void setCertificate(String certificate) {
+            this.certificate = certificate;
+        }
+    }
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    public static class ServiceAccount {
+
+        @JsonPropertyDescription("Whether the service account is enabled")
+        private Boolean enabled;
+
+        @JsonPropertyDescription("Roles assigned to the service account")
+        private Set<String> roles;
+
+        public Boolean getEnabled() {
+            return enabled;
+        }
+
+        public void setEnabled(Boolean enabled) {
+            this.enabled = enabled;
+        }
+
+        public Set<String> getRoles() {
+            return roles;
+        }
+
+        public void setRoles(Set<String> roles) {
+            this.roles = roles;
+        }
+    }
+}
+
