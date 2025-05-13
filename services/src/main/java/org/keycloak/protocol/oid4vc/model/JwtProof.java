910513f106886e53bff30688d226ab6a4c328bce
forkimenjeckayang <104195313+forkimenjeckayang@users.noreply.github.com>
2025-05-13 09:16:48 +0100

    Refactored Proof into a base interface with JwtProof and LdpVpProof implementations to support mutually exclusive proof types per OID4VCI spec, and removed CwtProof support as per Draft 14. (#39466)
    Closes #31363
    Closes #39271
    
    Signed-off-by: forkimenjeckayang <forkimenjeckayang@gmail.com>

--- services/src/main/java/org/keycloak/protocol/oid4vc/model/JwtProof.java
+++ services/src/main/java/org/keycloak/protocol/oid4vc/model/JwtProof.java
@@ -17,14 +17,43 @@
 
 package org.keycloak.protocol.oid4vc.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
- * Interface for proof types in OID4VCI Credential Request (Section 8.2.1.1).
+ * JWT Proof for Credential Request in OID4VCI (Section 8.2.1.1).
+ * Represents a signed JWT for holder binding.
  *
+ * @author <a href="https://github.com/wistefan">Stefan Wiedemann</a>
  * @see <a href="https://openid.net/specs/openid-4-verifiable-credential-issuance-1_0.html#name-credential-request">OID4VCI Credential Request</a>
  */
-public interface Proof {
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class JwtProof implements Proof {
+
     @JsonProperty("proof_type")
-    String getProofType();
+    private final String proofType = ProofType.JWT;
+
+    @JsonProperty("jwt")
+    private String jwt;
+
+    public JwtProof() {
+    }
+
+    public JwtProof(String jwt) {
+        this.jwt = jwt;
+    }
+
+    @Override
+    public String getProofType() {
+        return proofType;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public JwtProof setJwt(String jwt) {
+        this.jwt = jwt;
+        return this;
+    }
 }
