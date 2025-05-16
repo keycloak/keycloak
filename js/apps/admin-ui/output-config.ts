bc02d3942a600e0bdb6b0dcfed2267fa27013a76
Erik Jan de Wit <erikjan.dewit@gmail.com>
2025-05-16 15:29:18 +0200

    introduce sharding
    fixes: #38059
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

---
+++ js/apps/admin-ui/output-config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from "@playwright/test";
+
+// used by GHA to generate the report
+export default defineConfig({
+  reporter: [["html", { outputFile: "playwright-report" }]],
+});
