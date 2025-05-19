c2487ed88a75d56b64d7bd843a740c9d75abd1bd
Erik Jan de Wit <erikjan.dewit@gmail.com>
2025-05-19 10:00:57 +0200

    fixed report folder and open command
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- js/apps/admin-ui/output-config.ts
+++ js/apps/admin-ui/output-config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from "@playwright/test";
 
 // used by GHA to generate the report
 export default defineConfig({
-  reporter: [["html", { outputFile: "playwright-report" }]],
+  reporter: [["html", { outputFile: "playwright-report", open: "never" }]],
 });
