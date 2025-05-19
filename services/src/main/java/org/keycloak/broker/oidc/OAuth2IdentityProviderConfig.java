44d53f2b195cd9b0afdb802468f8dd2bb743fdc7
Johannes Knutsen <johannes@kodet.no>
2025-05-19 18:51:18 +0200

    Add IdP configuration to disable forwarding of the acr_values parameter
    Closes #39813
    
    Signed-off-by: Johannes Knutsen <johannes@kodet.no>

--- services/src/main/java/org/keycloak/broker/oidc/OAuth2IdentityProviderConfig.java
+++ services/src/main/java/org/keycloak/broker/oidc/OAuth2IdentityProviderConfig.java
@@ -34,6 +34,7 @@ public class OAuth2IdentityProviderConfig extends IdentityProviderModel {
 
     public static final String PKCE_ENABLED = "pkceEnabled";
     public static final String PKCE_METHOD = "pkceMethod";
+    public static final String FORWARD_ACR_VALUES = "forwardAcrValues";
 
     public static final String JWT_X509_HEADERS_ENABLED = "jwtX509HeadersEnabled";
 
@@ -131,6 +132,14 @@ public class OAuth2IdentityProviderConfig extends IdentityProviderModel {
 
     public void setForwardParameters(String forwardParameters) {
        getConfig().put("forwardParameters", forwardParameters);
+    }
+
+    public boolean isForwardAcrValues() {
+        return Boolean.parseBoolean(getConfig().getOrDefault(FORWARD_ACR_VALUES, "false"));
+    }
+
+    public void setForwardAcrValues(boolean forwardAcrValues) {
+        getConfig().put(FORWARD_ACR_VALUES, String.valueOf(forwardAcrValues));
     }
 
     public boolean isPkceEnabled() {
