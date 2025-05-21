2ffabbf7a7eca17ce7804631e7003ac3a1839ae4
Erik Jan de Wit <edewit@redhat.com>
2025-05-21 16:04:10 +0200

    Revert "introduce sharding (#39774)"
    This reverts commit cf47bef66b391ecf16556d22dbb8368bb0d284b8.
    
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- js/apps/admin-ui/output-config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from "@playwright/test";
-
-// used by GHA to generate the report
-export default defineConfig({
-  reporter: [["html", { outputFile: "playwright-report", open: "never" }]],
-});
