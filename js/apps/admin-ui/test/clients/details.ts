59e99ed5d52880bf86c1d252fa6514e1225bdf19
Erik Jan de Wit <erikjan.dewit@gmail.com>
2025-05-22 13:56:50 +0200

    Moved PKCE Code Challenge Method to more prominent section (#39631)
    fixes: #30227
    
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

---
+++ js/apps/admin-ui/test/clients/details.ts
@@ -0,0 +1,14 @@
+import { Page } from "@playwright/test";
+import { selectItem, assertSelectValue } from "../utils/form";
+
+function getKeyForCodeExchangeInput(page: Page) {
+  return page.locator("#keyForCodeExchange");
+}
+
+export async function selectKeyForCodeExchangeInput(page: Page, value: string) {
+  await selectItem(page, getKeyForCodeExchangeInput(page), value);
+}
+
+export async function assertKeyForCodeExchangeInput(page: Page, value: string) {
+  await assertSelectValue(getKeyForCodeExchangeInput(page), value);
+}
