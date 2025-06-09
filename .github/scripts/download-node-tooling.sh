e1d1f72afee89273393f4e843f344faa41cf2107
rmartinc <rmartinc@redhat.com>
2025-06-09 12:27:41 +0200

    Use direct path in windows for cache (link is problematic) Closes #40339
    Signed-off-by: rmartinc <rmartinc@redhat.com>

--- .github/scripts/download-node-tooling.sh
+++ .github/scripts/download-node-tooling.sh
@@ -37,16 +37,24 @@ pnpm_url() {
 main() {
     local node_version=$1
     local pnpm_version=$2
+    local platform=$3
 
     if [ "$node_version" == "" ] || [ "$pnpm_version" == "" ]; then
         abort "Usage: download-node-tooling.sh <NODE_VERSION> <PNPM_VERSION>"
     fi
 
-    local target_directory=~/.m2/repository/com/github/eirslett
+    local target_directory
+    if [ "$platform" == "Windows" ]; then
+        target_directory=/d/.m2/repository/com/github/eirslett
+    elif [ "$platform" == "Linux" ]; then
+        target_directory=~/.m2/repository/com/github/eirslett
+    else
+        abort "Unsupported platform: $platform"
+    fi
 
     download_file "$(node_url "$node_version" "linux")" "$target_directory/node/$node_version/node-$node_version-linux-x64.tar.gz"
     download_file "$(node_url "$node_version" "windows")" "$target_directory/node/$node_version/node-$node_version-win-x64.exe"
     download_file "$(pnpm_url "$pnpm_version")" "$target_directory/pnpm/$pnpm_version/pnpm-$pnpm_version.tar.gz"
 }
 
-main "$1" "$2"
+main "$1" "$2" "$3"
