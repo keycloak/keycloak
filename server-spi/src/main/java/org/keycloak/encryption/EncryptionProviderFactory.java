6200c52fee6b35debae2d88e36d08ed7f7cbc3c3
Abigail Cortis <abigail.cortis@rs2.com>
2025-06-10 10:26:07 +0200

    reverted custom SPI changes

--- server-spi/src/main/java/org/keycloak/encryption/EncryptionProviderFactory.java
+++
@@ -1,9 +0,0 @@
-package org.keycloak.encryption;
-
-import org.keycloak.models.KeycloakSession;
-import org.keycloak.provider.ProviderFactory;
-
-public interface EncryptionProviderFactory extends ProviderFactory<EncryptionProvider> {
-    @Override
-    EncryptionProvider create(KeycloakSession session);
-}
