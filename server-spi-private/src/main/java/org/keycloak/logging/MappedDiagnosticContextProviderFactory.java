c3f173523c927e8d655f10a639dc013aadacb964
Bjoern Eickvonder <bjoern.eickvonder@inform-software.com>
2025-06-12 17:46:49 +0200

    Reworked implementation.
    - renamed MdcDefinitionSpi -> MappedDiagnosticContextSpi
    - MDC values now set on the fly once information is available in KeycloakContext
    - Filter only clears MDC values as the very last response filter
    - Added clear method to ScheduledTaskRunner
    - Added "org" key = alias of current organization
    
    Signed-off-by: Bjoern Eickvonder <bjoern.eickvonder@inform-software.com>

--- server-spi-private/src/main/java/org/keycloak/logging/MappedDiagnosticContextProviderFactory.java
+++ server-spi-private/src/main/java/org/keycloak/logging/MappedDiagnosticContextProviderFactory.java
@@ -5,5 +5,5 @@ import org.keycloak.provider.ProviderFactory;
 /**
  * @author <a href="mailto:b.eicki@gmx.net">Björn Eickvonder</a>
  */
-public interface MdcDefinitionProviderFactory extends ProviderFactory<MdcDefinitionProvider> {
+public interface MappedDiagnosticContextProviderFactory extends ProviderFactory<MappedDiagnosticContextProvider> {
 }
