df0d13458f790376b1fb776d632922db8454ae45
Alexander Schwartz <aschwart@redhat.com>
2025-06-20 15:28:23 +0200

    Avoid logging when people don't have the feature enabled
    Signed-off-by: Alexander Schwartz <aschwart@redhat.com>

--- server-spi-private/src/main/java/org/keycloak/logging/MappedDiagnosticContextUtil.java
+++ server-spi-private/src/main/java/org/keycloak/logging/MappedDiagnosticContextUtil.java
@@ -2,6 +2,7 @@ package org.keycloak.logging;
 
 import org.jboss.logging.Logger;
 import org.jboss.logging.MDC;
+import org.keycloak.common.Profile;
 import org.keycloak.models.KeycloakSession;
 
 import java.util.ArrayList;
@@ -12,6 +13,9 @@ public final class MappedDiagnosticContextUtil {
     private static final MappedDiagnosticContextProvider NOOP_PROVIDER = new NoopMappedDiagnosticContextProvider();
 
     public static MappedDiagnosticContextProvider getMappedDiagnosticContextProvider(KeycloakSession session) {
+        if (Profile.isFeatureEnabled(Profile.Feature.LOG_MDC)) {
+            return NOOP_PROVIDER;
+        }
         if (session == null) {
             log.warn("Cannot obtain session from thread to init MappedDiagnosticContextProvider. Return Noop provider.");
             return NOOP_PROVIDER;
