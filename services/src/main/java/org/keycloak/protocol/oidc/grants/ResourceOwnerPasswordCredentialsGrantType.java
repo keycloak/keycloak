689eb54cfac495e55ff28726a54337a82d520315
Keshav Deshpande <keshavprashantdeshpande@gmail.com>
2025-07-04 19:29:00 +0200

    Change error to 400 for unknown user
    Closes #39079
    
    Signed-off-by: Keshav Deshpande <keshavprashantdeshpande@gmail.com>

--- services/src/main/java/org/keycloak/protocol/oidc/grants/ResourceOwnerPasswordCredentialsGrantType.java
+++ services/src/main/java/org/keycloak/protocol/oidc/grants/ResourceOwnerPasswordCredentialsGrantType.java
@@ -24,6 +24,7 @@ import org.jboss.logging.Logger;
 
 import org.keycloak.OAuth2Constants;
 import org.keycloak.OAuthErrorException;
+import org.keycloak.authentication.AuthenticationFlowException;
 import org.keycloak.authentication.AuthenticationProcessor;
 import org.keycloak.events.Details;
 import org.keycloak.events.Errors;
@@ -111,7 +112,16 @@ public class ResourceOwnerPasswordCredentialsGrantType extends OAuth2GrantTypeBa
                 .setSession(session)
                 .setUriInfo(session.getContext().getUri())
                 .setRequest(request);
-        Response challenge = processor.authenticateOnly();
+        Response challenge = null;
+        try {
+            challenge = processor.authenticateOnly();
+        } catch (AuthenticationFlowException afe) {
+            new AuthenticationSessionManager(session).removeAuthenticationSession(realm, authSession, false);
+            String errorMessage = "Cannot find user (Unknown user)";
+            event.detail(Details.REASON, errorMessage);
+            event.error(Errors.USER_NOT_FOUND);
+            throw new CorsErrorResponseException(cors, afe.getError().name(), errorMessage, Response.Status.BAD_REQUEST);
+        }
         if (challenge != null) {
             // Remove authentication session as "Resource Owner Password Credentials Grant" is single-request scoped authentication
             new AuthenticationSessionManager(session).removeAuthenticationSession(realm, authSession, false);
