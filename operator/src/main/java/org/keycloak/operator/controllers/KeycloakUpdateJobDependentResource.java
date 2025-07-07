bed45424e4a6514dce0043b8bcf6497654bd28e4
Steve Hawkins <shawkins@redhat.com>
2025-07-07 16:21:44 -0400

    fix: restricting lookup of existing resources to current owner
    closes: #40932
    
    Signed-off-by: Steve Hawkins <shawkins@redhat.com>

--- operator/src/main/java/org/keycloak/operator/controllers/KeycloakUpdateJobDependentResource.java
+++ operator/src/main/java/org/keycloak/operator/controllers/KeycloakUpdateJobDependentResource.java
@@ -102,7 +102,7 @@ public class KeycloakUpdateJobDependentResource extends CRUDKubernetesDependentR
     public static boolean isJobFromCurrentKeycloakCr(Job job, Keycloak keycloak) {
         var annotations = job.getMetadata().getAnnotations();
         var hash = annotations.get(KEYCLOAK_CR_HASH_ANNOTATION);
-        return Objects.equals(hash, keycloakHash(keycloak));
+        return job.hasOwnerReferenceFor(keycloak) && Objects.equals(hash, keycloakHash(keycloak));
     }
 
     public static String jobName(Keycloak keycloak) {
