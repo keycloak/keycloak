9007ef0ec163889bda8a23d1eb4875e85b4597c4
Martin Bartoš <mabartos@redhat.com>
2025-07-11 14:41:56 +0200

    Provide simple validation with Jakarta/Hibernate Validation
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>

--- rest/admin-api/src/main/java/org/keycloak/admin/api/client/ClientApi.java
+++ rest/admin-api/src/main/java/org/keycloak/admin/api/client/ClientApi.java
@@ -1,11 +1,13 @@
 package org.keycloak.admin.api.client;
 
+import jakarta.validation.Valid;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.PATCH;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 
 import org.keycloak.admin.api.FieldValidation;
@@ -22,7 +24,8 @@ public interface ClientApi {
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    ClientRepresentation createOrUpdateClient(ClientRepresentation client, @PathParam("fieldValidation") FieldValidation fieldValidation);
+    ClientRepresentation createOrUpdateClient(@Valid ClientRepresentation client,
+                                              @QueryParam("fieldValidation") FieldValidation fieldValidation);
 
     @PATCH
     @Consumes("application/merge-patch+json")
