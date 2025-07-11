2f36276ff02946eb71f9cb6b88f86d2a45d05a6a
Giuseppe Graziano <g.graziano94@gmail.com>
2025-07-11 17:42:47 +0200

    Remove FGAP:v1 from external-internal token exchange (#40938)
    Closes #40855
    
    Signed-off-by: Giuseppe Graziano <g.graziano94@gmail.com>

--- services/src/main/java/org/keycloak/protocol/oidc/tokenexchange/ExternalToInternalTokenExchangeProvider.java
+++ services/src/main/java/org/keycloak/protocol/oidc/tokenexchange/ExternalToInternalTokenExchangeProvider.java
@@ -20,16 +20,27 @@
 package org.keycloak.protocol.oidc.tokenexchange;
 
 import jakarta.ws.rs.core.Response;
+import org.keycloak.OAuth2Constants;
+import org.keycloak.OAuthErrorException;
+import org.keycloak.broker.provider.BrokeredIdentityContext;
+import org.keycloak.broker.provider.IdentityProvider;
+import org.keycloak.events.Details;
+import org.keycloak.events.Errors;
+import org.keycloak.models.UserModel;
+import org.keycloak.models.UserSessionModel;
 import org.keycloak.protocol.oidc.TokenExchangeContext;
+import org.keycloak.services.CorsErrorResponseException;
+import org.keycloak.services.managers.UserSessionManager;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Provider for external-internal token exchange
  *
- * TODO Should not extend from V1TokenExchangeProvider, but rather AbstractTokenExchangeProvider or from StandardTokenExchangeProvider (as issuing internal tokens might be done in a same/similar way like for standard V2 provider)
- *
  * @author <a href="mailto:mposolda@redhat.com">Marek Posolda</a>
  */
-public class ExternalToInternalTokenExchangeProvider extends V1TokenExchangeProvider {
+public class ExternalToInternalTokenExchangeProvider extends StandardTokenExchangeProvider {
 
     @Override
     public boolean supports(TokenExchangeContext context) {
@@ -47,6 +58,55 @@ public class ExternalToInternalTokenExchangeProvider extends V1TokenExchangeProv
         String subjectTokenType = context.getParams().getSubjectTokenType();
         String subjectIssuer = getSubjectIssuer(this.context, subjectToken, subjectTokenType);
         return exchangeExternalToken(subjectIssuer, subjectToken);
+    }
+
+    @Override
+    protected List<String> getSupportedOAuthResponseTokenTypes() {
+        return Arrays.asList(OAuth2Constants.ACCESS_TOKEN_TYPE, OAuth2Constants.ID_TOKEN_TYPE);
+    }
+
+    @Override
+    protected String getRequestedTokenType() {
+        String requestedTokenType = params.getRequestedTokenType();
+        if (requestedTokenType == null) {
+            requestedTokenType = OAuth2Constants.ACCESS_TOKEN_TYPE;
+            return requestedTokenType;
+        }
+        if (getSupportedOAuthResponseTokenTypes().contains(requestedTokenType)) {
+            return requestedTokenType;
+        }
+
+        event.detail(Details.REASON, "requested_token_type unsupported");
+        event.error(Errors.INVALID_REQUEST);
+        throw new CorsErrorResponseException(cors, OAuthErrorException.INVALID_REQUEST, "requested_token_type unsupported", Response.Status.BAD_REQUEST);
+    }
+
+    protected Response exchangeExternalToken(String subjectIssuer, String subjectToken) {
+        // try to find the IDP whose alias matches the issuer or the subject issuer in the form params.
+        ExternalExchangeContext externalExchangeContext = this.locateExchangeExternalTokenByAlias(subjectIssuer);
+
+        if (externalExchangeContext == null) {
+            event.error(Errors.INVALID_ISSUER);
+            throw new CorsErrorResponseException(cors, Errors.INVALID_ISSUER, "Invalid " + OAuth2Constants.SUBJECT_ISSUER + " parameter", Response.Status.BAD_REQUEST);
+        }
+        BrokeredIdentityContext context = externalExchangeContext.provider().exchangeExternal(this, this.context);
+        if (context == null) {
+            event.error(Errors.INVALID_ISSUER);
+            throw new CorsErrorResponseException(cors, Errors.INVALID_ISSUER, "Invalid " + OAuth2Constants.SUBJECT_ISSUER + " parameter", Response.Status.BAD_REQUEST);
+        }
+
+        UserModel user = importUserFromExternalIdentity(context);
+
+        UserSessionModel userSession = new UserSessionManager(session).createUserSession(realm, user, user.getUsername(), clientConnection.getRemoteHost(), "external-exchange", false, null, null);
+        externalExchangeContext.provider().exchangeExternalComplete(userSession, context, formParams);
+
+        // this must exist so that we can obtain access token from user session if idp's store tokens is off
+        userSession.setNote(IdentityProvider.EXTERNAL_IDENTITY_PROVIDER, externalExchangeContext.idpModel().getAlias());
+        userSession.setNote(IdentityProvider.FEDERATED_ACCESS_TOKEN, subjectToken);
+
+        context.addSessionNotesToUserSession(userSession);
+
+        return exchangeClientToClient(user, userSession, null, false);
     }
 
 }
