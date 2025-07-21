0e7c96f458755c6c19654e8bc081b526aa2d0396
Alexander Schwartz <aschwart@redhat.com>
2025-07-21 14:59:48 +0200

    Ensure to find all members even on concurrent starts
    Closes #41290
    
    Signed-off-by: Alexander Schwartz <aschwart@redhat.com>

--- model/infinispan/src/main/java/org/keycloak/jgroups/protocol/KEYCLOAK_JDBC_PING2.java
+++ model/infinispan/src/main/java/org/keycloak/jgroups/protocol/KEYCLOAK_JDBC_PING2.java
@@ -17,12 +17,20 @@
 
 package org.keycloak.jgroups.protocol;
 
+import org.jgroups.Address;
+import org.jgroups.Event;
+import org.jgroups.PhysicalAddress;
+import org.jgroups.View;
 import org.jgroups.protocols.JDBC_PING2;
 import org.jgroups.protocols.PingData;
+import org.jgroups.util.NameCache;
+import org.jgroups.util.Responses;
 import org.keycloak.connections.jpa.JpaConnectionProviderFactory;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -33,6 +41,39 @@ import java.util.Objects;
 public class KEYCLOAK_JDBC_PING2 extends JDBC_PING2 {
 
     private JpaConnectionProviderFactory factory;
+
+    @Override
+    protected void handleView(View new_view, View old_view, boolean coord_changed) {
+        // If we are the coordinator, it is good to learn about new entries that have been added before we delete them.
+        // If we are not the coordinator, it is good to learn the new entries added by the coordinator.
+        // This avoids a "JGRP000032: %s: no physical address for %s, dropping message" that leads to split clusters at concurrent startup.
+        learnExistingAddresses();
+        super.handleView(new_view, old_view, coord_changed);
+    }
+
+    protected void learnExistingAddresses() {
+        try {
+            List<PingData> list = readFromDB(getClusterName());
+            for (PingData data : list) {
+                Address addr = data.getAddress();
+                if (local_addr != null && !local_addr.equals(addr)) {
+                    addDiscoveryResponseToCaches(addr, data.getLogicalName(), data.getPhysicalAddr());
+                }
+            }
+        } catch (Exception e) {
+            log.error(String.format("%s: failed reading from the DB", local_addr), e);
+        }
+    }
+
+    @Override
+    public void findMembers(List<Address> members, boolean initial_discovery, Responses responses) {
+        // Insert ourselves before reading, to ensure that concurrently starting nodes see each other.
+        // Also re-add ourselves in case the coordinator removed us.
+         PhysicalAddress physical_addr = (PhysicalAddress) down(new Event(Event.GET_PHYSICAL_ADDRESS, local_addr));
+        PingData coord_data = new PingData(local_addr, true, NameCache.get(local_addr), physical_addr).coord(is_coord);
+        write(Collections.singletonList(coord_data), cluster_name);
+        super.findMembers(members, initial_discovery, responses);
+    }
 
     @Override
     protected void writeToDB(PingData data, String clustername) throws SQLException {
