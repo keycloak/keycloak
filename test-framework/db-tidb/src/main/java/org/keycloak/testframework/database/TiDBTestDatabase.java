588f3c123ee9b9d8fb2de75d1581637677a8f7f4
Dennis Kniep <dennis.kniep@merckgroup.com>
2025-07-24 22:40:49 +0200

    tidb ci cd

---
+++ test-framework/db-tidb/src/main/java/org/keycloak/testframework/database/TiDBTestDatabase.java
@@ -0,0 +1,44 @@
+package org.keycloak.testframework.database;
+
+import org.jboss.logging.Logger;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.tidb.TiDBContainer;
+import org.testcontainers.utility.DockerImageName;
+
+class TiDBTestDatabase extends AbstractContainerTestDatabase {
+
+    private static final Logger LOGGER = Logger.getLogger(TiDBTestDatabase.class);
+
+    public static final String NAME = "tidb";
+
+    @Override
+    public JdbcDatabaseContainer<?> createContainer() {
+        return new TiDBContainer(DockerImageName.parse(DatabaseProperties.getContainerImageName(NAME)).asCompatibleSubstituteFor(NAME));
+    }
+
+    @Override
+    public String getDatabaseVendor() {
+        return "mysql";
+    }
+
+    @Override
+    public Logger getLogger() {
+        return LOGGER;
+    }
+
+    @Override
+    public String getDatabase() {
+        return "test";
+    }
+
+    @Override
+    public String getUsername() {
+        return "root";
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+}
