4328baaf3d16819ca65b4b2aac6bca44a1b4d116
Alexander Schwartz <aschwart@redhat.com>
2025-07-30 14:40:07 +0200

    Make import realm idempotent
    Signed-off-by: Alexander Schwartz <aschwart@redhat.com>

--- js/apps/account-ui/test/realm.setup.ts
+++ js/apps/account-ui/test/realm.setup.ts
@@ -1,13 +1,17 @@
 import RealmRepresentation from "@keycloak/keycloak-admin-client/lib/defs/realmRepresentation";
 import { test as setup } from "@playwright/test";
 
-import { importRealm } from "./admin-client";
+import { deleteRealm, importRealm } from "./admin-client";
 import groupsRealm from "./realms/groups-realm.json" assert { type: "json" };
 import resourcesRealm from "./realms/resources-realm.json" assert { type: "json" };
 import userProfileRealm from "./realms/user-profile-realm.json" assert { type: "json" };
 import verifiableCredentialsRealm from "./realms/verifiable-credentials-realm.json" assert { type: "json" };
 
 setup("import realm", async () => {
+  await deleteRealm("photoz");
+  await deleteRealm("groups");
+  await deleteRealm("user-profile");
+  await deleteRealm("verifiable-credentials");
   await importRealm(groupsRealm as RealmRepresentation);
   await importRealm(resourcesRealm as RealmRepresentation);
   await importRealm(userProfileRealm as RealmRepresentation);
