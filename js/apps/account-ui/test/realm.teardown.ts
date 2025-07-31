cfd8525b6b548f8d2a722c0f20745d694a8bebd3
Alexander Schwartz <aschwart@redhat.com>
2025-07-31 17:07:48 +0200

    Rework
    Signed-off-by: Alexander Schwartz <aschwart@redhat.com>

--- js/apps/account-ui/test/realm.teardown.ts
+++ js/apps/account-ui/test/realm.teardown.ts
@@ -1,9 +1,9 @@
 import { test as setup } from "@playwright/test";
 import { deleteRealm } from "./admin-client";
-import groupsRealm from "./realms/groups-realm.json";
-import resourcesRealm from "./realms/resources-realm.json";
-import userProfileRealm from "./realms/user-profile-realm.json";
-import verifiableCredentialsRealm from "./realms/verifiable-credentials-realm.json";
+import groupsRealm from "./realms/groups-realm.json" assert { type: "json" };
+import resourcesRealm from "./realms/resources-realm.json" assert { type: "json" };
+import userProfileRealm from "./realms/user-profile-realm.json" assert { type: "json" };
+import verifiableCredentialsRealm from "./realms/verifiable-credentials-realm.json" assert { type: "json" };
 
 setup("delete realm", async () => {
   await Promise.all([
