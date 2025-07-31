1b03a81e66f8c16027a96f30acbe9f2ec6eed62b
Alexander Schwartz <aschwart@redhat.com>
2025-07-31 17:30:02 +0200

    A username already specified in the event should take precedence
    Signed-off-by: Alexander Schwartz <aschwart@redhat.com>

--- server-spi-private/src/main/java/org/keycloak/events/EventBuilder.java
+++ server-spi-private/src/main/java/org/keycloak/events/EventBuilder.java
@@ -128,11 +128,11 @@ public class EventBuilder {
 
     public EventBuilder user(UserModel user) {
         event.setUserId(user == null ? null : user.getId());
-        if (event.getDetails() == null) {
-            event.setDetails(new HashMap<>());
-        }
         if (user != null && user.getUsername() != null) {
-            event.getDetails().put(Details.USERNAME, user.getUsername()); //capture username in details only if available
+            // If there is already a set username, prefer that one as it will be the one as entered by the user
+            if (event.getDetails() != null && event.getDetails().get(Details.USERNAME) == null) {
+                detail(Details.USERNAME, user.getUsername()); //capture username in details only if available
+            }
         }
         return this;
     }
