d33f3d999f7768d256f0928039960f9f9db96f28
AvivGuiser <avivguiser@gmail.com>
2025-08-04 19:55:56 +0300

    operator: add support for custom labels in ingress
    Signed-off-by: AvivGuiser <avivguiser@gmail.com>

--- operator/src/main/java/org/keycloak/operator/controllers/KeycloakIngressDependentResource.java
+++ operator/src/main/java/org/keycloak/operator/controllers/KeycloakIngressDependentResource.java
@@ -116,6 +116,7 @@ public class KeycloakIngressDependentResource extends CRUDKubernetesDependentRes
                     .withName(getName(keycloak))
                     .withNamespace(keycloak.getMetadata().getNamespace())
                     .addToLabels(Utils.allInstanceLabels(keycloak))
+                    .addToLabels(keycloak.getSpec().getIngressSpec().getLabels())
                     .addToAnnotations(annotations)
                 .endMetadata()
                 .withNewSpec()
