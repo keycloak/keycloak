ac632d609e819dfce4850abf7d44603130100a5f
Pedro Igor <pigor.craveiro@gmail.com>
2025-08-06 08:45:22 -0300

    Do not allow setting default values for root attributes
    Signed-off-by: Pedro Igor <pigor.craveiro@gmail.com>

--- services/src/main/java/org/keycloak/userprofile/config/UPConfigUtils.java
+++ services/src/main/java/org/keycloak/userprofile/config/UPConfigUtils.java
@@ -17,6 +17,7 @@
 package org.keycloak.userprofile.config;
 
 import static org.keycloak.common.util.ObjectUtil.isBlank;
+import static org.keycloak.userprofile.UserProfileUtil.isRootAttribute;
 
 import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
@@ -149,7 +150,7 @@ public class UPConfigUtils {
         }
 
         List<String> errors = new ArrayList<>();
-        List<String> attributeNames = attributes.stream().map(UPAttribute::getName).collect(Collectors.toList());
+        List<String> attributeNames = attributes.stream().map(UPAttribute::getName).toList();
 
         for (String name : Arrays.asList(UserModel.USERNAME, UserModel.EMAIL)) {
             if (!attributeNames.contains(name)) {
@@ -185,15 +186,7 @@ public class UPConfigUtils {
         }
         if (attributeConfig.getValidations() != null) {
             attributeConfig.getValidations().forEach((validator, validatorConfig) -> validateValidationConfig(session, validator, validatorConfig, attributeName, errors));
-
-            if (attributeConfig.getDefaultValue() != null) {
-                attributeConfig.getValidations().forEach((validator, validatorConfig) -> {
-                    ValidationContext context = Validators.validator(session, validator).validate(attributeConfig.getDefaultValue(), attributeName, ValidatorConfig.configFromMap(validatorConfig));
-                    if (!context.isValid()) {
-                        errors.add("Default value is invalid");
-                    }
-                });
-            }
+            validateDefaultValue(session, attributeConfig, errors);
         }
         if (attributeConfig.getPermissions() != null) {
             if (attributeConfig.getPermissions().getView() != null) {
@@ -219,6 +212,27 @@ public class UPConfigUtils {
 
         if (attributeConfig.getAnnotations()!=null) {
             validateAnnotations(attributeConfig.getAnnotations(), errors, attributeName);
+        }
+    }
+
+    private static void validateDefaultValue(KeycloakSession session, UPAttribute attributeConfig, List<String> errors) {
+        String defaultValue = attributeConfig.getDefaultValue();
+
+        if (defaultValue == null) {
+            return;
+        }
+
+        String attributeName = attributeConfig.getName();
+
+        if (isRootAttribute(attributeName)) {
+            errors.add("Default value not supported for attribute '" + attributeName + "'");
+        } else {
+            attributeConfig.getValidations().forEach((validator, validatorConfig) -> {
+                ValidationContext context = Validators.validator(session, validator).validate(defaultValue, attributeName, ValidatorConfig.configFromMap(validatorConfig));
+                if (!context.isValid()) {
+                    errors.add("Default value for attribute '" + attributeName + "' is invalid");
+                }
+            });
         }
     }
 
