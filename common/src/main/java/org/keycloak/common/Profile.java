87003385a4ed94cbec8195fa460b075d170106d8
Martin Bartoš <mabartos@redhat.com>
2025-08-12 11:12:09 +0200

    [OTel] Introduce preview support for OpenTelemetry Logs
    Closes #41264
    
    Co-authored-by: Ryan Emerson <remerson@redhat.com>
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>

--- common/src/main/java/org/keycloak/common/Profile.java
+++ common/src/main/java/org/keycloak/common/Profile.java
@@ -130,6 +130,7 @@ public class Profile {
         OID4VC_VCI("Support for the OID4VCI protocol as part of OID4VC.", Type.EXPERIMENTAL),
 
         OPENTELEMETRY("OpenTelemetry support", Type.DEFAULT),
+        OPENTELEMETRY_LOGS("OpenTelemetry Logs support", Type.PREVIEW, OPENTELEMETRY),
 
         DECLARATIVE_UI("declarative ui spi", Type.EXPERIMENTAL),
 
