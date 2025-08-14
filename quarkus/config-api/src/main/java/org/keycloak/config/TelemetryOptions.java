a22c40f55ea4c0c8ff4e70b365f5d7553fb707e8
Martin Bartoš <mabartos@redhat.com>
2025-08-14 14:05:25 +0200

    Address Ryan's feedback
    Co-authored-by: Ryan Emerson <remerson@redhat.com>
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>

--- quarkus/config-api/src/main/java/org/keycloak/config/TelemetryOptions.java
+++ quarkus/config-api/src/main/java/org/keycloak/config/TelemetryOptions.java
@@ -12,25 +12,25 @@ public class TelemetryOptions {
 
     public static final Option<String> TELEMETRY_ENDPOINT = new OptionBuilder<>("telemetry-endpoint", String.class)
             .category(OptionCategory.TELEMETRY)
-            .description("Telemetry (OpenTelemetry) endpoint to connect to.")
+            .description("OpenTelemetry endpoint to connect to.")
             .defaultValue("http://localhost:4317")
             .build();
 
     public static final Option<String> TELEMETRY_SERVICE_NAME = new OptionBuilder<>("telemetry-service-name", String.class)
             .category(OptionCategory.TELEMETRY)
-            .description("Telemetry (OpenTelemetry) service name. Takes precedence over 'service.name' defined in the 'telemetry-resource-attributes' property.")
+            .description("OpenTelemetry service name. Takes precedence over 'service.name' defined in the 'telemetry-resource-attributes' property.")
             .defaultValue("keycloak")
             .build();
 
     public static final Option<String> TELEMETRY_PROTOCOL = new OptionBuilder<>("telemetry-protocol", String.class)
             .category(OptionCategory.TELEMETRY)
-            .description("Telemetry (OpenTelemetry) protocol used for the telemetry data.")
+            .description("OpenTelemetry protocol used for the communication between server and OpenTelemetry collector.")
             .defaultValue("grpc")
             .expectedValues("grpc", "http/protobuf")
             .build();
 
     public static final Option<List<String>> TELEMETRY_RESOURCE_ATTRIBUTES = OptionBuilder.listOptionBuilder("telemetry-resource-attributes", String.class)
             .category(OptionCategory.TELEMETRY)
-            .description("Telemetry (OpenTelemetry) resource attributes characterize the telemetry producer. Values in format 'key1=val1,key2=val2'.")
+            .description("OpenTelemetry resource attributes characterize the telemetry producer. Values in format 'key1=val1,key2=val2'.")
             .build();
 }
