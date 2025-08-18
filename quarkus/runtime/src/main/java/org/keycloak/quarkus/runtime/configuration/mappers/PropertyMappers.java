4f4ed315d31488812396d0dbe6ffeab9f0fe39f8
Robin Meese <39960884+robson90@users.noreply.github.com>
2025-08-18 17:53:07 +0200

    Add OpenAPI and OpenAPI-UI to management interface (#41853)
    Signed-off-by: Robin Meese <39960884+robson90@users.noreply.github.com>

--- quarkus/runtime/src/main/java/org/keycloak/quarkus/runtime/configuration/mappers/PropertyMappers.java
+++ quarkus/runtime/src/main/java/org/keycloak/quarkus/runtime/configuration/mappers/PropertyMappers.java
@@ -62,6 +62,7 @@ public final class PropertyMappers {
         MAPPERS.addAll(ConfigKeystorePropertyMappers.getConfigKeystorePropertyMappers());
         MAPPERS.addAll(ManagementPropertyMappers.getManagementPropertyMappers());
         MAPPERS.addAll(MetricsPropertyMappers.getMetricsPropertyMappers());
+        MAPPERS.addAll(OpenApiPropertyMappers.getOpenApiPropertyMappers());
         MAPPERS.addAll(EventPropertyMappers.getMetricsPropertyMappers());
         MAPPERS.addAll(ProxyPropertyMappers.getProxyPropertyMappers());
         MAPPERS.addAll(VaultPropertyMappers.getVaultPropertyMappers());
