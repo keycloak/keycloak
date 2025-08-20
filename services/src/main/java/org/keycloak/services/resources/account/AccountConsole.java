3a471782dc9ef492b6d7aa51e201f178702e8d02
rmartinc <rmartinc@redhat.com>
2025-08-20 16:17:06 +0200

    Use back keycloak-js instead of initiate login in the backend for account Closes #40463
    Signed-off-by: rmartinc <rmartinc@redhat.com>

--- services/src/main/java/org/keycloak/services/resources/account/AccountConsole.java
+++ services/src/main/java/org/keycloak/services/resources/account/AccountConsole.java
@@ -3,13 +3,9 @@ package org.keycloak.services.resources.account;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.ServerErrorException;
 import jakarta.ws.rs.core.MultivaluedMap;
 import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.Response.Status;
-import jakarta.ws.rs.core.UriBuilder;
 import org.jboss.resteasy.reactive.NoCache;
-import org.keycloak.OAuth2Constants;
 import org.keycloak.authentication.requiredactions.DeleteAccount;
 import org.keycloak.authentication.requiredactions.UpdateEmail;
 import org.keycloak.common.Profile;
@@ -19,7 +15,6 @@ import org.keycloak.models.FederatedIdentityModel;
 import org.keycloak.models.IdentityProviderModel;
 import org.keycloak.models.IdentityProviderStorageProvider;
 import org.keycloak.protocol.oidc.OIDCLoginProtocol;
-import org.keycloak.protocol.oidc.utils.PkceUtils;
 import org.keycloak.utils.SecureContextResolver;
 import org.keycloak.models.AccountRoles;
 import org.keycloak.models.ClientModel;
@@ -28,7 +23,6 @@ import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.RoleModel;
 import org.keycloak.models.UserModel;
-import org.keycloak.protocol.oidc.OIDCLoginProtocolService;
 import org.keycloak.protocol.oidc.utils.RedirectUtils;
 import org.keycloak.services.Urls;
 import org.keycloak.services.managers.AppAuthManager;
@@ -46,18 +40,15 @@ import org.keycloak.theme.freemarker.FreeMarkerProvider;
 import org.keycloak.urls.UrlType;
 import org.keycloak.util.JsonSerialization;
 import org.keycloak.utils.MediaType;
-import org.keycloak.utils.StringUtil;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Scanner;
-import java.util.UUID;
 import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -109,10 +100,6 @@ public class AccountConsole implements AccountResourceProvider {
     @NoCache
     @Path("{path:.*}")
     public Response getMainPage(@PathParam("path") String path) throws IOException, FreeMarkerException {
-
-        if (auth == null) {
-            return redirectToLogin(path);
-        }
 
         return renderAccountConsole();
     }
@@ -229,77 +216,6 @@ public class AccountConsole implements AccountResourceProvider {
         String result = freeMarkerUtil.processTemplate(map, "index.ftl", theme);
         Response.ResponseBuilder builder = Response.status(Response.Status.OK).type(MediaType.TEXT_HTML_UTF_8).language(Locale.ENGLISH).entity(result);
         return builder.build();
-    }
-
-    private Response redirectToLogin(String path) {
-        UriBuilder consoleUriBuilder = Urls.accountBase(session.getContext().getUri().getBaseUri());
-        if (!StringUtil.isNullOrEmpty(path)) {
-            consoleUriBuilder.path(path);
-        }
-        var queryParameters = session.getContext().getUri().getQueryParameters();
-        if (!queryParameters.isEmpty()) {
-            String referrer = queryParameters.getFirst("referrer");
-            if (referrer != null) {
-                consoleUriBuilder.queryParam("referrer", referrer);
-            }
-
-            String referrerUri = queryParameters.getFirst("referrer_uri");
-            if (referrerUri != null) {
-                consoleUriBuilder.queryParam("referrer_uri", referrerUri);
-            }
-        }
-        URI targetUri = consoleUriBuilder.build(realm.getName());
-
-        String pkceChallenge;
-        try {
-            // Add PKCE parameters as it is required for the account-console client.
-            // Because the account console configuration requires PKCE, we need to send this with the redirect in order to not fail validations.
-            // The real PKCE challenge will be sent by the account-console OIDC client JavaScript integration.
-            String codeVerifier = UUID.randomUUID().toString();
-            pkceChallenge = PkceUtils.generateS256CodeChallenge(codeVerifier);
-        } catch (Exception e) {
-            // this should never happen
-            throw new RuntimeException(e);
-        }
-
-        UriBuilder uriBuilder = UriBuilder.fromUri(OIDCLoginProtocolService.authUrl(session.getContext().getUri()).build(realm.getName()).toString())
-                .queryParam(OAuth2Constants.CLIENT_ID, Constants.ACCOUNT_CONSOLE_CLIENT_ID)
-                .queryParam(OAuth2Constants.REDIRECT_URI, targetUri)
-                // dummy state param to make it usable with secure-session client policy.
-                // Once bootstrapped the account-console frontend will send the actual state with the authorize request.
-                .queryParam(OAuth2Constants.STATE, UUID.randomUUID().toString())
-                .queryParam(OAuth2Constants.RESPONSE_TYPE, OAuth2Constants.CODE)
-                .queryParam(OAuth2Constants.CODE_CHALLENGE, pkceChallenge)
-                .queryParam(OAuth2Constants.CODE_CHALLENGE_METHOD, OAuth2Constants.PKCE_METHOD_S256);
-
-        if (!queryParameters.isEmpty()) {
-            String error = queryParameters.getFirst(OAuth2Constants.ERROR);
-            if (error != null) {
-                String state = queryParameters.getFirst(OAuth2Constants.STATE);
-                if (state != null) {
-                    // Omit the "state" parameter to make sure that account console displays the error (it may not be shown due the keycloak.js, which will not be able to find the "callback data" in the browser callbackStorage)
-                    URI url = session.getContext().getUri(UrlType.FRONTEND)
-                            .getRequestUriBuilder()
-                            .replaceQueryParam(OAuth2Constants.STATE, null)
-                            .build();
-                    return Response.status(302).location(url).build();
-                } else {
-                    try {
-                        return renderAccountConsole();
-                    } catch (IOException | FreeMarkerException e) {
-                        throw new ServerErrorException(Status.INTERNAL_SERVER_ERROR);
-                    }
-                }
-            }
-            String scope = queryParameters.getFirst(OIDCLoginProtocol.SCOPE_PARAM);
-            if (StringUtil.isNotBlank(scope)) {
-                uriBuilder.queryParam(OIDCLoginProtocol.SCOPE_PARAM, scope);
-            }
-        }
-
-        URI url = uriBuilder.build();
-
-        return Response.status(302).location(url).build();
     }
 
     private Map<String, String> supportedLocales(Properties messages) {
