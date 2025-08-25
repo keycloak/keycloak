17e84072307006cbd8737ea0bbf84a5196263052
Martin Bartoš <mabartos@redhat.com>
2025-08-25 13:32:07 +0200

    [admin-api-v2] SPIs for Admin APIs v2 (#41943)
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>

--- rest/admin-api/src/main/java/org/keycloak/admin/api/realm/DefaultRealmApi.java
+++ rest/admin-api/src/main/java/org/keycloak/admin/api/realm/DefaultRealmApi.java
@@ -1,27 +1,27 @@
 package org.keycloak.admin.api.realm;
 
-import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.Path;
 import org.keycloak.admin.api.client.ClientsApi;
-import org.keycloak.admin.api.client.DefaultClientsApi;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 
-import java.util.Optional;
+import java.util.Objects;
 
 public class DefaultRealmApi implements RealmApi {
     private final KeycloakSession session;
     private final RealmModel realm;
 
-    public DefaultRealmApi(KeycloakSession session, String name) {
+    public DefaultRealmApi(KeycloakSession session) {
         this.session = session;
-        this.realm = Optional.ofNullable(session.realms().getRealmByName(name)).orElseThrow(() -> new NotFoundException("Realm cannot be found"));
-        session.getContext().setRealm(realm);
+        this.realm = Objects.requireNonNull(session.getContext().getRealm());
     }
 
     @Path("clients")
     @Override
     public ClientsApi clients() {
-        return new DefaultClientsApi(session, realm);
+        return session.getProvider(ClientsApi.class);
     }
+
+    @Override
+    public void close() {}
 }
