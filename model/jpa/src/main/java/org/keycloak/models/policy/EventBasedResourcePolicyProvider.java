8dd23e63330c209ed786bcde2788b48591e65f0f
Pedro Igor <pigor.craveiro@gmail.com>
2025-08-29 18:20:25 -0300

    Add support for generic event-based policies and conditions
    Signed-off-by: Pedro Igor <pigor.craveiro@gmail.com>

---
+++ model/jpa/src/main/java/org/keycloak/models/policy/EventBasedResourcePolicyProvider.java
@@ -0,0 +1,87 @@
+package org.keycloak.models.policy;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.keycloak.component.ComponentModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+
+public class EventBasedResourcePolicyProvider implements ResourcePolicyProvider {
+
+    private final KeycloakSession session;
+    private final ComponentModel model;
+
+    public EventBasedResourcePolicyProvider(KeycloakSession session, ComponentModel model) {
+        this.session = session;
+        this.model = model;
+    }
+
+    @Override
+    public List<String> getEligibleResourcesForInitialAction() {
+        return List.of();
+    }
+
+    @Override
+    public boolean supports(ResourceType type) {
+        return ResourceType.USERS.equals(type);
+    }
+
+    @Override
+    public boolean scheduleOnEvent(ResourcePolicyEvent event) {
+        List<String> events = model.getConfig().getOrDefault("events", List.of());
+        ResourceOperationType operation = event.getOperation();
+
+        if (!events.contains(operation.name())) {
+            return false;
+        }
+
+        List<String> conditions = model.getConfig().getOrDefault("conditions", List.of());
+
+        for (String providerId : conditions) {
+            ResourcePolicyConditionProvider condition = resolveCondition(providerId);
+
+            if (!condition.evaluate(event)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean resetOnEvent(ResourcePolicyEvent event) {
+        return false;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    private ResourcePolicyConditionProvider resolveCondition(String providerId) {
+        KeycloakSessionFactory sessionFactory = session.getKeycloakSessionFactory();
+        ResourcePolicyConditionProviderFactory<ResourcePolicyConditionProvider> providerFactory = (ResourcePolicyConditionProviderFactory<ResourcePolicyConditionProvider>) sessionFactory.getProviderFactory(ResourcePolicyConditionProvider.class, providerId);
+
+        if (providerFactory == null) {
+            throw new IllegalStateException("Could not find condition provider: " + providerId);
+        }
+
+        Map<String, List<String>> config = new HashMap<>();
+
+        for (Entry<String, List<String>> configEntry : model.getConfig().entrySet()) {
+            if (configEntry.getKey().startsWith(providerId)) {
+                config.put(configEntry.getKey().substring(providerId.length() + 1), configEntry.getValue());
+            }
+        }
+
+        ResourcePolicyConditionProvider condition = providerFactory.create(session, config);
+
+        if (condition == null) {
+            throw new IllegalStateException("Factory " + providerFactory.getClass() + " returned a null provider");
+        }
+        return condition;
+    }
+}
---
+++ tests/base/src/test/java/org/keycloak/tests/admin/model/policy/GroupMembershipJoinPolicyTest.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright 2025 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.keycloak.tests.admin.model.policy;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.time.Duration;
+import java.util.List;
+
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
+import org.junit.jupiter.api.Test;
+import org.keycloak.admin.client.resource.RealmResourcePolicies;
+import org.keycloak.common.util.Time;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserModel;
+import org.keycloak.models.policy.EventBasedResourcePolicyProviderFactory;
+import org.keycloak.models.policy.GroupMembershipPolicyConditionFactory;
+import org.keycloak.models.policy.NotifyUserActionProviderFactory;
+import org.keycloak.models.policy.ResourceOperationType;
+import org.keycloak.models.policy.ResourcePolicyManager;
+import org.keycloak.representations.resources.policies.ResourcePolicyActionRepresentation;
+import org.keycloak.representations.resources.policies.ResourcePolicyConditionRepresentation;
+import org.keycloak.representations.resources.policies.ResourcePolicyRepresentation;
+import org.keycloak.testframework.annotations.InjectRealm;
+import org.keycloak.testframework.annotations.KeycloakIntegrationTest;
+import org.keycloak.testframework.injection.LifeCycle;
+import org.keycloak.testframework.realm.GroupConfigBuilder;
+import org.keycloak.testframework.realm.ManagedRealm;
+import org.keycloak.testframework.realm.UserConfigBuilder;
+import org.keycloak.testframework.remote.runonserver.InjectRunOnServer;
+import org.keycloak.testframework.remote.runonserver.RunOnServerClient;
+import org.keycloak.testframework.util.ApiUtil;
+
+@KeycloakIntegrationTest(config = RLMServerConfig.class)
+public class GroupMembershipJoinPolicyTest {
+
+    private static final String REALM_NAME = "default";
+
+    @InjectRunOnServer(permittedPackages = "org.keycloak.tests")
+    RunOnServerClient runOnServer;
+
+    @InjectRealm(lifecycle = LifeCycle.METHOD)
+    ManagedRealm managedRealm;
+
+    @Test
+    public void testEventsOnGroupMembershipJoin() {
+        String groupId;
+
+        try (Response response = managedRealm.admin().groups().add(GroupConfigBuilder.create()
+                .name("generic-group").build())) {
+            groupId = ApiUtil.getCreatedId(response);
+        }
+
+        List<ResourcePolicyRepresentation> expectedPolicies = ResourcePolicyRepresentation.create()
+                .of(EventBasedResourcePolicyProviderFactory.ID)
+                .onEvent(ResourceOperationType.GROUP_MEMBERSHIP_JOIN.name())
+                .onCoditions(ResourcePolicyConditionRepresentation.create()
+                        .of(GroupMembershipPolicyConditionFactory.ID)
+                        .withConfig(GroupMembershipPolicyConditionFactory.EXPECTED_GROUPS, groupId)
+                        .build())
+                .withActions(
+                        ResourcePolicyActionRepresentation.create()
+                                .of(NotifyUserActionProviderFactory.ID)
+                                .after(Duration.ofDays(5))
+                                .build()
+                ).build();
+
+        RealmResourcePolicies policies = managedRealm.admin().resources().policies();
+
+        try (Response response = policies.create(expectedPolicies)) {
+            assertThat(response.getStatus(), is(Status.CREATED.getStatusCode()));
+        }
+
+        String userId;
+
+        try (Response response = managedRealm.admin().users().create(UserConfigBuilder.create()
+                .username("generic-user").build())) {
+            userId = ApiUtil.getCreatedId(response);
+        }
+
+        managedRealm.admin().users().get(userId).joinGroup(groupId);
+
+        runOnServer.run((session -> {
+            RealmModel realm = configureSessionContext(session);
+            ResourcePolicyManager manager = new ResourcePolicyManager(session);
+
+            UserModel user = session.users().getUserById(realm, userId);
+            assertNull(user.getAttributes().get("message"));
+
+            try {
+                // set offset to 7 days - notify action should run now
+                Time.setOffset(Math.toIntExact(Duration.ofDays(6).toSeconds()));
+                manager.runScheduledTasks();
+                user = session.users().getUserById(realm, userId);
+                assertNotNull(user.getAttributes().get("message"));
+            } finally {
+                Time.setOffset(0);
+            }
+        }));
+    }
+
+    private static RealmModel configureSessionContext(KeycloakSession session) {
+        RealmModel realm = session.realms().getRealmByName(REALM_NAME);
+        session.getContext().setRealm(realm);
+        return realm;
+    }
+}
