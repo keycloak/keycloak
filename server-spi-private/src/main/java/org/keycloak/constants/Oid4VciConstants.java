6e767a30b879645a45b3c4124a574f77fcff4d44
forkimenjeckayang <104195313+forkimenjeckayang@users.noreply.github.com>
2025-09-04 12:12:10 +0100

    Centralize OID4VCI Protocol Constants in Oid4VciConstants and Refactor Usages (#41481)
    Closes #40083
    
    Signed-off-by: forkimenjeckayang <forkimenjeckayang@gmail.com>

--- server-spi-private/src/main/java/org/keycloak/constants/Oid4VciConstants.java
+++ server-spi-private/src/main/java/org/keycloak/constants/Oid4VciConstants.java
@@ -29,6 +29,13 @@ public final class Oid4VciConstants {
 
     public static final String CREDENTIAL_SUBJECT = "credentialSubject";
 
+    // --- Endpoints/Well-Known ---
+    public static final String WELL_KNOWN_OPENID_CREDENTIAL_ISSUER = "openid-credential-issuer";
+    public static final String RESPONSE_TYPE_IMG_PNG = "image/png";
+    public static final String CREDENTIAL_OFFER_URI_CODE_SCOPE = "credential-offer";
+
+    // --- Keybinding/Credential Builder ---
+    public static final String SOURCE_ENDPOINT = "source_endpoint";
     public static final String BATCH_CREDENTIAL_ISSUANCE_BATCH_SIZE = "batch_credential_issuance.batch_size";
 
     private Oid4VciConstants() {
