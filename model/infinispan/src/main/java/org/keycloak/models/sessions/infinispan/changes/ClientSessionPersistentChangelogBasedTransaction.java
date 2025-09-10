eeed4af61cd842250fa49d258ff9c552261db046
Pedro Ruivo <1492066+pruivo@users.noreply.github.com>
2025-09-10 15:31:19 +0100

    ClientSession timestamp not updated in the database
    Closes #42012
    
    Signed-off-by: Pedro Ruivo <1492066+pruivo@users.noreply.github.com>

--- model/infinispan/src/main/java/org/keycloak/models/sessions/infinispan/changes/ClientSessionPersistentChangelogBasedTransaction.java
+++ model/infinispan/src/main/java/org/keycloak/models/sessions/infinispan/changes/ClientSessionPersistentChangelogBasedTransaction.java
@@ -86,6 +86,7 @@ public class ClientSessionPersistentChangelogBasedTransaction extends Persistent
 
             // Cache does not contain the offline flag value so adding it
             wrappedEntity.getEntity().setOffline(offline);
+            wrappedEntity.getEntity().setUserSessionId(userSession.getId());
 
             RealmModel realmFromSession = kcSession.realms().getRealm(wrappedEntity.getEntity().getRealmId());
             if (!realmFromSession.getId().equals(realm.getId())) {
@@ -147,6 +148,7 @@ public class ClientSessionPersistentChangelogBasedTransaction extends Persistent
         entity.setRedirectUri(clientSession.getRedirectUri());
         entity.setTimestamp(clientSession.getTimestamp());
         entity.setOffline(clientSession.getUserSession().isOffline());
+        entity.setUserSessionId(userSessionId);
 
         return entity;
     }
