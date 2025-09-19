3af0959d7c38bc5713550749cf3839cf21f86a32
Stefan Wiedemann <wistefan@googlemail.com>
2025-09-19 10:32:47 +0200

    filter for asymmetric keys
    Signed-off-by: Stefan Wiedemann <wistefan@googlemail.com>

--- services/src/main/java/org/keycloak/crypto/CryptoUtils.java
+++ services/src/main/java/org/keycloak/crypto/CryptoUtils.java
@@ -20,6 +20,9 @@ package org.keycloak.crypto;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.provider.ProviderFactory;
 
+import java.security.Key;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.util.AbstractMap;
 import java.util.List;
 import java.util.Map;
@@ -27,26 +30,49 @@ import java.util.stream.Collectors;
 
 /**
  * Utility class for common cryptographic operations and algorithm discovery.
- * 
+ *
  * @author <a href="https://github.com/forkimenjeckayang">Forkim Akwichek</a>
  */
 public class CryptoUtils {
 
-    /**
-     * Returns the supported asymmetric signature algorithms.
-     * This method discovers all available SignatureProvider implementations and filters
-     * for those that support asymmetric algorithms (RSA, EC, EdDSA, etc.).
-     * 
-     * @param session The Keycloak session
-     * @return List of asymmetric signature algorithm names
-     */
-    public static List<String> getSupportedAsymmetricSignatureAlgorithms(KeycloakSession session) {
-        return session.getKeycloakSessionFactory().getProviderFactoriesStream(SignatureProvider.class)
-                .map(ProviderFactory::getId)
-                .map(algorithm -> new AbstractMap.SimpleEntry<>(algorithm, session.getProvider(SignatureProvider.class, algorithm)))
-                .filter(entry -> entry.getValue() != null)
-                .filter(entry -> entry.getValue().isAsymmetricAlgorithm())
-                .map(Map.Entry::getKey)
-                .collect(Collectors.toList());
-    }
+	/**
+	 * Returns the supported asymmetric signature algorithms.
+	 * This method discovers all available SignatureProvider implementations and filters
+	 * for those that support asymmetric algorithms (RSA, EC, EdDSA, etc.).
+	 *
+	 * @param session The Keycloak session
+	 * @return List of asymmetric signature algorithm names
+	 */
+	public static List<String> getSupportedAsymmetricSignatureAlgorithms(KeycloakSession session) {
+		return session.getKeycloakSessionFactory().getProviderFactoriesStream(SignatureProvider.class)
+				.map(ProviderFactory::getId)
+				.map(algorithm -> new AbstractMap.SimpleEntry<>(algorithm, session.getProvider(SignatureProvider.class, algorithm)))
+				.filter(entry -> entry.getValue() != null)
+				.filter(entry -> entry.getValue().isAsymmetricAlgorithm())
+				.map(Map.Entry::getKey)
+				.collect(Collectors.toList());
+	}
+
+	/**
+	 * Returns the supported asymmetric encryption algorithms.
+	 * This method discovers all available Keys and filters
+	 * for those that use asymmetric algorithms (RSA, EC, EdDSA, etc.).
+	 *
+	 * @param session The Keycloak session
+	 * @return List of asymmetric signature algorithm names
+	 */
+	public static List<String> getSupportedAsymmetricEncryptionAlgorithms(KeycloakSession session) {
+		return session.keys()
+				.getKeysStream(session.getContext().getRealm())
+				.filter(key -> KeyUse.ENC.equals(key.getUse()))
+				.filter(key -> {
+					Key k = key.getPublicKey();
+					// asymmetric keys will have PublicKey/PrivateKey instead of SecretKey
+					return k instanceof PublicKey || key.getPrivateKey() instanceof PrivateKey;
+				})
+				.map(KeyWrapper::getAlgorithm)
+				.filter(algorithm -> algorithm != null && !algorithm.isEmpty())
+				.distinct()
+				.toList();
+	}
 }
