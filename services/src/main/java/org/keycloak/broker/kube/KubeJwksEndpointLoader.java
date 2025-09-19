8a2edb0335fef5b8b73f6338fe836c35532fae6b
stianst <stianst@gmail.com>
2025-09-19 13:08:02 +0200

    Hacked Kube identity provider
    Signed-off-by: stianst <stianst@gmail.com>

---
+++ services/src/main/java/org/keycloak/broker/kube/KubeJwksEndpointLoader.java
@@ -0,0 +1,46 @@
+package org.keycloak.broker.kube;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.keycloak.connections.httpclient.HttpClientProvider;
+import org.keycloak.crypto.PublicKeysWrapper;
+import org.keycloak.jose.jwk.JSONWebKeySet;
+import org.keycloak.jose.jwk.JWK;
+import org.keycloak.keys.PublicKeyLoader;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.util.JWKSUtils;
+import org.keycloak.util.JsonSerialization;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+
+public class KubeJwksEndpointLoader implements PublicKeyLoader {
+
+    private final KeycloakSession session;
+    private final String endpoint;
+
+    public KubeJwksEndpointLoader(KeycloakSession session, String endpoint) {
+        this.session = session;
+        this.endpoint = endpoint;
+    }
+
+    @Override
+    public PublicKeysWrapper loadKeys() throws Exception {
+        try (CloseableHttpClient httpClient = session.getProvider(HttpClientProvider.class).getHttpClient()) {
+            HttpGet httpGet = new HttpGet(endpoint);
+
+            String token = FileUtils.readFileToString(new File("/var/run/secrets/kubernetes.io/serviceaccount/token"), StandardCharsets.UTF_8);
+
+            httpGet.setHeader(HttpHeaders.ACCEPT, "application/json");
+            httpGet.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+
+            CloseableHttpResponse response = httpClient.execute(httpGet);
+            JSONWebKeySet jwks = JsonSerialization.readValue(response.getEntity().getContent(), JSONWebKeySet.class);
+            return JWKSUtils.getKeyWrappersForUse(jwks, JWK.Use.JWT_SVID);
+        }
+    }
+
+}
