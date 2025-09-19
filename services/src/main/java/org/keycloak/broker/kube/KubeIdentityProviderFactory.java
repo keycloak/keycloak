d90a26f46ca0d492824bda5a354f14d39133736e
stianst <stianst@gmail.com>
2025-09-19 13:08:02 +0200

    Experimental Kube service accounts identity provider
    Co-authored-by: Ryan Emerson <remerson@ibm.com>
    
    Signed-off-by: stianst <stianst@gmail.com>
    
    Closes #37600

---
+++ services/src/main/java/org/keycloak/broker/kube/KubeIdentityProviderFactory.java
@@ -0,0 +1,54 @@
+package org.keycloak.broker.kube;
+
+import org.keycloak.Config;
+import org.keycloak.broker.oidc.OIDCIdentityProviderConfig;
+import org.keycloak.broker.provider.AbstractIdentityProviderFactory;
+import org.keycloak.common.Profile;
+import org.keycloak.models.IdentityProviderModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.provider.EnvironmentDependentProviderFactory;
+
+import java.util.Map;
+
+public class KubeIdentityProviderFactory extends AbstractIdentityProviderFactory<KubeIdentityProvider> implements EnvironmentDependentProviderFactory {
+
+    public static final String PROVIDER_ID = "kube";
+
+    private String globalJwksUrl;
+
+    @Override
+    public String getName() {
+        return "Kubernetes";
+    }
+
+    @Override
+    public KubeIdentityProvider create(KeycloakSession session, IdentityProviderModel model) {
+        return new KubeIdentityProvider(session, new OIDCIdentityProviderConfig(model), globalJwksUrl);
+    }
+
+    @Override
+    public void init(Config.Scope config) {
+        globalJwksUrl = config.get("jwksUrl", "https://kubernetes.default.svc/openid/v1/jwks");
+    }
+
+    @Override
+    public Map<String, String> parseConfig(KeycloakSession session, String configString) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public IdentityProviderModel createConfig() {
+        return new OIDCIdentityProviderConfig();
+    }
+
+    @Override
+    public String getId() {
+        return PROVIDER_ID;
+    }
+
+    @Override
+    public boolean isSupported(Config.Scope config) {
+        return Profile.isFeatureEnabled(Profile.Feature.KUBERNETES_SERVICE_ACCOUNTS);
+    }
+
+}
---
+++ services/src/main/java/org/keycloak/broker/kube/KubeJwksEndpointLoader.java
@@ -0,0 +1,56 @@
+package org.keycloak.broker.kube;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.keycloak.connections.httpclient.HttpClientProvider;
+import org.keycloak.crypto.PublicKeysWrapper;
+import org.keycloak.jose.jwk.JSONWebKeySet;
+import org.keycloak.jose.jwk.JWK;
+import org.keycloak.keys.PublicKeyLoader;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.util.JWKSUtils;
+import org.keycloak.util.JsonSerialization;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+
+public class KubeJwksEndpointLoader implements PublicKeyLoader {
+
+    private final KeycloakSession session;
+
+    private final boolean authenticate;
+    private final String endpoint;
+
+    public KubeJwksEndpointLoader(KeycloakSession session, String globalEndpoint, String providerEndpoint) {
+        this.session = session;
+
+        if (providerEndpoint == null || providerEndpoint.isEmpty() || globalEndpoint.equals(providerEndpoint)) {
+            this.endpoint = globalEndpoint;
+            authenticate = true;
+        } else {
+            this.endpoint = providerEndpoint;
+            authenticate = false;
+        }
+    }
+
+    @Override
+    public PublicKeysWrapper loadKeys() throws Exception {
+        CloseableHttpClient httpClient = session.getProvider(HttpClientProvider.class).getHttpClient();
+        HttpGet httpGet = new HttpGet(endpoint);
+
+        httpGet.setHeader(HttpHeaders.ACCEPT, "application/jwk-set+json");
+
+        if (authenticate) {
+            String token = FileUtils.readFileToString(new File("/var/run/secrets/kubernetes.io/serviceaccount/token"), StandardCharsets.UTF_8);
+            httpGet.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+        }
+
+        try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
+            JSONWebKeySet jwks = JsonSerialization.readValue(response.getEntity().getContent(), JSONWebKeySet.class);
+            return JWKSUtils.getKeyWrappersForUse(jwks, JWK.Use.SIG);
+        }
+    }
+}
