a0e14ca28cb53bd53bfd0827d5567d322be0cb46
Sebastian Łaskawiec <sebastian.laskawiec@defenseunicorns.com>
2025-09-24 12:40:58 +0200

    Optimized imports
    Signed-off-by: Sebastian Łaskawiec <sebastian.laskawiec@defenseunicorns.com>

--- services/src/main/java/org/keycloak/broker/kubernetes/KubernetesIdentityProvider.java
+++ services/src/main/java/org/keycloak/broker/kubernetes/KubernetesIdentityProvider.java
@@ -3,7 +3,6 @@ package org.keycloak.broker.kubernetes;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.UriInfo;
 import org.keycloak.broker.oidc.OIDCIdentityProvider;
-import org.keycloak.broker.oidc.OIDCIdentityProviderConfig;
 import org.keycloak.broker.provider.AuthenticationRequest;
 import org.keycloak.broker.provider.BrokeredIdentityContext;
 import org.keycloak.broker.provider.IdentityProviderDataMarshaller;
--- services/src/main/java/org/keycloak/broker/kubernetes/KubernetesIdentityProviderConfig.java
+++ services/src/main/java/org/keycloak/broker/kubernetes/KubernetesIdentityProviderConfig.java
@@ -3,11 +3,6 @@ package org.keycloak.broker.kubernetes;
 import org.keycloak.broker.oidc.OIDCIdentityProviderConfig;
 import org.keycloak.models.IdentityProviderModel;
 import org.keycloak.models.IdentityProviderShowInAccountConsole;
-import org.keycloak.models.RealmModel;
-
-import java.util.regex.Pattern;
-
-import static org.keycloak.common.util.UriUtils.checkUrl;
 
 public class KubernetesIdentityProviderConfig extends OIDCIdentityProviderConfig {
 
