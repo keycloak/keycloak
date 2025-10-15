618504243ef8bb903c21f4017ff9d26f09f1a5ae
Steven Hawkins <shawkins@redhat.com>
2025-10-15 07:44:39 -0400

    fix: refining activation condition error handling (#43197)
    closes: #43096
    
    Signed-off-by: Steve Hawkins <shawkins@redhat.com>
    (cherry picked from commit 43ee41e8a82fc7f326cc74df462fe44a5f95de61)

--- operator/src/main/java/org/keycloak/operator/controllers/KeycloakServiceMonitorDependentResource.java
+++ operator/src/main/java/org/keycloak/operator/controllers/KeycloakServiceMonitorDependentResource.java
@@ -1,16 +1,19 @@
 package org.keycloak.operator.controllers;
 
 import static org.keycloak.operator.controllers.KeycloakDeploymentDependentResource.managementEndpoint;
-import static org.keycloak.operator.crds.v2alpha1.CRDUtils.LEGACY_MANAGEMENT_ENABLED;
 import static org.keycloak.operator.crds.v2alpha1.CRDUtils.METRICS_ENABLED;
 import static org.keycloak.operator.crds.v2alpha1.CRDUtils.configuredOptions;
+
+import java.net.HttpURLConnection;
 
 import org.keycloak.operator.Constants;
 import org.keycloak.operator.Utils;
 import org.keycloak.operator.crds.v2alpha1.deployment.Keycloak;
 import org.keycloak.operator.crds.v2alpha1.deployment.spec.ServiceMonitorSpec;
 
-import io.fabric8.kubernetes.api.model.networking.v1.Ingress;
+import io.fabric8.kubernetes.client.KubernetesClientException;
+import io.fabric8.kubernetes.client.Watcher;
+import io.fabric8.kubernetes.client.WatcherException;
 import io.fabric8.openshift.api.model.monitoring.v1.ServiceMonitor;
 import io.fabric8.openshift.api.model.monitoring.v1.ServiceMonitorBuilder;
 import io.javaoperatorsdk.operator.api.config.informer.Informer;
@@ -19,28 +22,75 @@ import io.javaoperatorsdk.operator.api.reconciler.dependent.DependentResource;
 import io.javaoperatorsdk.operator.processing.dependent.kubernetes.CRUDKubernetesDependentResource;
 import io.javaoperatorsdk.operator.processing.dependent.kubernetes.KubernetesDependent;
 import io.javaoperatorsdk.operator.processing.dependent.workflow.Condition;
+import io.quarkus.logging.Log;
 
 @KubernetesDependent(
       informer = @Informer(labelSelector = Constants.DEFAULT_LABELS_AS_STRING)
 )
 public class KeycloakServiceMonitorDependentResource extends CRUDKubernetesDependentResource<ServiceMonitor, Keycloak> {
 
-    public static class ReconcilePrecondition implements Condition<Ingress, Keycloak> {
+    public static final String WARN_METRICS_NOT_ENABLED = "A ServiceMonitor will not be created because `metrics-enabled` is not true.";
+    public static final String WARN_CRD_NOT_INSTALLED = "A ServiceMonitor will not be created because the ServiceMonitor CRD is not installed.";
+
+    static String SERVICE_MONITOR_WARNING = "ServiceMonitorWarning";
+
+    volatile Boolean crdInstalled;
+
+    public static class ActivationCondition implements Condition<ServiceMonitor, Keycloak> {
+
         @Override
-        public boolean isMet(DependentResource<Ingress, Keycloak> dependentResource, Keycloak primary,
-                             Context<Keycloak> context) {
-            var opts = configuredOptions(primary);
-            if (Boolean.parseBoolean(opts.get(LEGACY_MANAGEMENT_ENABLED)) || !Boolean.parseBoolean(opts.getOrDefault(METRICS_ENABLED, "false"))) {
+        public boolean isMet(DependentResource<ServiceMonitor, Keycloak> dependentResource, Keycloak primary, Context<Keycloak> context) {
+            if (!ServiceMonitorSpec.get(primary).isEnabled()) {
                 return false;
             }
 
-            return ServiceMonitorSpec.get(primary).isEnabled();
+            var opts = configuredOptions(primary);
+
+            if (!Boolean.parseBoolean(opts.getOrDefault(METRICS_ENABLED, "false"))) {
+                context.managedWorkflowAndDependentResourceContext().put(SERVICE_MONITOR_WARNING, WARN_METRICS_NOT_ENABLED);
+                return false;
+            }
+
+            if (!isCRDInstalled(dependentResource, context, (KeycloakServiceMonitorDependentResource)dependentResource)) {
+                context.managedWorkflowAndDependentResourceContext().put(SERVICE_MONITOR_WARNING, WARN_CRD_NOT_INSTALLED);
+                return false;
+            }
+
+            return true;
+        }
+
+        private boolean isCRDInstalled(DependentResource<ServiceMonitor, Keycloak> dependentResource,
+                Context<Keycloak> context, KeycloakServiceMonitorDependentResource serviceMonitorDependentResource) {
+            if (serviceMonitorDependentResource.crdInstalled != null) {
+                return serviceMonitorDependentResource.crdInstalled;
+            }
+            Watcher<ServiceMonitor> dummyWatcher = new Watcher<ServiceMonitor>() {
+
+                @Override
+                public void eventReceived(Action action, ServiceMonitor resource) {
+                }
+
+                @Override
+                public void onClose(WatcherException cause) {
+                }
+            };
+            try (var watch = context.getClient().resources(dependentResource.resourceType()).watch(dummyWatcher)) {
+                serviceMonitorDependentResource.crdInstalled = true;
+                return true;
+            } catch (KubernetesClientException e) {
+                if (e.getCode() == HttpURLConnection.HTTP_NOT_FOUND) {
+                    Log.warn("ServiceMonitors will not be managed by the operator as the CRD for ServiceMonitors not installed. If the CRD is installed later, the operator will need to be restarted.");
+                    serviceMonitorDependentResource.crdInstalled = false;
+                    return false;
+                }
+                throw e;
+            }
         }
     }
 
     @Override
     protected ServiceMonitor desired(Keycloak primary, Context<Keycloak> context) {
-        var endpoint = managementEndpoint(primary, context, true);
+        var endpoint = managementEndpoint(primary, context, false);
         var meta = primary.getMetadata();
         var spec = ServiceMonitorSpec.get(primary);
         return new ServiceMonitorBuilder()
@@ -60,7 +110,7 @@ public class KeycloakServiceMonitorDependentResource extends CRUDKubernetesDepen
                 .addNewEndpoint()
                   .withInterval(spec.getInterval())
                   .withPath(endpoint.relativePath() + "metrics")
-                  .withPort(Constants.KEYCLOAK_MANAGEMENT_PORT_NAME)
+                  .withPort(endpoint.portName())
                   .withScheme(endpoint.protocol().toLowerCase())
                   .withScrapeTimeout(spec.getScrapeTimeout())
                   .withNewTlsConfig()
