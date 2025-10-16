419140b8f84cd68b542b9f797e83557fe84498b6
stianst <stianst@gmail.com>
2025-10-16 15:14:12 +0200

    Extract related methods from IdentityProvider to UserIdentityProvider
    Closes #43534
    
    Signed-off-by: stianst <stianst@gmail.com>

--- server-spi-private/src/main/java/org/keycloak/broker/provider/BrokeredIdentityContext.java
+++ server-spi-private/src/main/java/org/keycloak/broker/provider/BrokeredIdentityContext.java
@@ -48,7 +48,7 @@ public class BrokeredIdentityContext {
     private String brokerUserId;
     private String token;
     private IdentityProviderModel idpConfig;
-    private IdentityProvider idp;
+    private UserAuthenticationIdentityProvider<?> idp;
     private Map<String, Object> contextData = new HashMap<>();
     private AuthenticationSessionModel authenticationSession;
 
@@ -152,11 +152,11 @@ public class BrokeredIdentityContext {
         return idpConfig;
     }
 
-    public IdentityProvider getIdp() {
+    public UserAuthenticationIdentityProvider<?> getIdp() {
         return idp;
     }
 
-    public void setIdp(IdentityProvider idp) {
+    public void setIdp(UserAuthenticationIdentityProvider<?> idp) {
         this.idp = idp;
     }
 
