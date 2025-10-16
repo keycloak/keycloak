1f0ae82029cc9ed16d315c900a7baf2068eae31d
joeknowsdev <jwstanl3@gmail.com>
2025-10-16 12:20:26 -0400

    Export routes array from Account Console and Admin Console UI libraries
    Additionally exports the Organizations page component from the Account Console UI
    
    Closes #43512
    
    Signed-off-by: Joe Stanley <joe@joeknows.dev>
    Co-authored-by: Joe Stanley <joe@joeknows.dev>

--- js/apps/admin-ui/src/index.ts
+++ js/apps/admin-ui/src/index.ts
@@ -315,3 +315,4 @@ export { KeycloakProvider, useEnvironment } from "@keycloak/keycloak-ui-shared";
 export { AdminClientContext, initAdminClient } from "./admin-client";
 export { AppContexts } from "./App";
 export * as PermissionsConfigurationSection from "./permissions-configuration/PermissionsConfigurationSection";
+export { routes } from "./routes";
