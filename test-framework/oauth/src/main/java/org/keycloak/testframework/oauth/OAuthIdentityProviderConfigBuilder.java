dc6d59ab1130a38b026e11ed942cd3c2b2be90d5
Stian Thorgersen <stianst@gmail.com>
2025-10-22 15:19:56 +0200

    SPIFFE should support OIDC JWK endpoint (#43651)
    Closes #43650
    
    Signed-off-by: stianst <stianst@gmail.com>
    (cherry picked from commit f6ac64907d300025c70bbb0ab42a6cacb2730e51)

--- test-framework/oauth/src/main/java/org/keycloak/testframework/oauth/OAuthIdentityProviderConfigBuilder.java
+++ test-framework/oauth/src/main/java/org/keycloak/testframework/oauth/OAuthIdentityProviderConfigBuilder.java
@@ -3,17 +3,23 @@ package org.keycloak.testframework.oauth;
 public class OAuthIdentityProviderConfigBuilder {
 
     private boolean spiffe;
+    private boolean jwkUse = true;
 
     public OAuthIdentityProviderConfigBuilder spiffe() {
         spiffe = true;
         return this;
     }
 
-    public OAuthIdentityProviderConfiguration build() {
-        return new OAuthIdentityProviderConfiguration(spiffe);
+    public OAuthIdentityProviderConfigBuilder jwkUse(boolean jwkUse) {
+        this.jwkUse = jwkUse;
+        return this;
     }
 
-    public record OAuthIdentityProviderConfiguration(boolean spiffe) {
+    public OAuthIdentityProviderConfiguration build() {
+        return new OAuthIdentityProviderConfiguration(spiffe, jwkUse);
+    }
+
+    public record OAuthIdentityProviderConfiguration(boolean spiffe, boolean jwkUse) {
     }
 
 }
