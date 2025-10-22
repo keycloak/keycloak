a635f345171dadf20a9898525c20c1fa155cd1f6
Martin Bartoš <mabartos@redhat.com>
2025-10-22 11:29:02 +0200

    Ability to enable/disable feature via single property
    Closes #43541
    
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>

--- quarkus/runtime/src/main/java/org/keycloak/quarkus/runtime/QuarkusProfileConfigResolver.java
+++ quarkus/runtime/src/main/java/org/keycloak/quarkus/runtime/QuarkusProfileConfigResolver.java
@@ -1,12 +1,26 @@
 package org.keycloak.quarkus.runtime;
 
+import org.keycloak.common.Profile;
 import org.keycloak.common.profile.CommaSeparatedListProfileConfigResolver;
+import org.keycloak.common.profile.ProfileConfigResolver;
+import org.keycloak.common.profile.SingleProfileConfigResolver;
+import org.keycloak.config.FeatureOptions;
+import org.keycloak.quarkus.runtime.cli.PropertyException;
 import org.keycloak.quarkus.runtime.configuration.Configuration;
+import org.keycloak.quarkus.runtime.configuration.mappers.WildcardPropertyMapper;
 
-public class QuarkusProfileConfigResolver extends CommaSeparatedListProfileConfigResolver {
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.keycloak.quarkus.runtime.configuration.MicroProfileConfigProvider.NS_KEYCLOAK_PREFIX;
+
+public class QuarkusProfileConfigResolver implements ProfileConfigResolver {
+    private final CommaSeparatedListProfileConfigResolver commaSeparatedResolver;
+    private final SingleProfileConfigResolver singleResolver;
 
     public QuarkusProfileConfigResolver() {
-        super(getConfig("kc.features"), getConfig("kc.features-disabled"));
+        this.commaSeparatedResolver = new CommaSeparatedListProfileConfigResolver(getConfig("kc.features"), getConfig("kc.features-disabled"));
+        this.singleResolver = new SingleProfileConfigResolver(getQuarkusFeatureState());
     }
 
     static String getConfig(String key) {
@@ -14,4 +28,43 @@ public class QuarkusProfileConfigResolver extends CommaSeparatedListProfileConfi
                 .orElse(Configuration.getConfigValue(key).getValue());
     }
 
+    protected Map<String, Boolean> getQuarkusFeatureState() {
+        var map = new HashMap<String, Boolean>();
+        var index = FeatureOptions.FEATURE.getKey().indexOf(WildcardPropertyMapper.WILDCARD_FROM_START);
+        var featureEnabledOptionPrefix = NS_KEYCLOAK_PREFIX + FeatureOptions.FEATURE.getKey().substring(0, index);
+
+        Configuration.getPropertyNames().forEach(property -> {
+            if (property.startsWith(NS_KEYCLOAK_PREFIX) && property.startsWith(featureEnabledOptionPrefix)) {
+                var feature = property.substring(featureEnabledOptionPrefix.length());
+                var value = Configuration.getOptionalValue(property).orElseThrow(
+                        () -> new PropertyException("Missing value for feature '%s'".formatted(feature)));
+
+                if (value.startsWith("v")) {
+                    map.put(feature + ":" + value, true);
+                } else {
+                    map.put(feature, switch (value) {
+                        case "enabled" -> Boolean.TRUE;
+                        case "disabled" -> Boolean.FALSE;
+                        default -> null;
+                    });
+                }
+            }
+        });
+
+        return map;
+    }
+
+    @Override
+    public Profile.ProfileName getProfileName() {
+        return commaSeparatedResolver.getProfileName();
+    }
+
+    @Override
+    public FeatureConfig getFeatureConfig(String feature) {
+        var singleConfig = singleResolver.getFeatureConfig(feature);
+        if (singleConfig != FeatureConfig.UNCONFIGURED) {
+            return singleConfig;
+        }
+        return commaSeparatedResolver.getFeatureConfig(feature);
+    }
 }
