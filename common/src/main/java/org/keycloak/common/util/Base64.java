6bb3373b30d6f8d01220a04bb3683a0ee7e83728
Pedro Ruivo <1492066+pruivo@users.noreply.github.com>
2025-10-22 13:45:01 +0100

    wip...

--- common/src/main/java/org/keycloak/common/util/Base64.java
+++ common/src/main/java/org/keycloak/common/util/Base64.java
@@ -720,14 +720,7 @@ public class Base64
         // Since we're not going to have the GZIP encoding turned on,
         // we're not going to have an java.io.IOException thrown, so
         // we should not force the user to have to catch it.
-        String encoded = null;
-        try {
-            encoded = encodeBytes(source, 0, source.length, NO_OPTIONS);
-        } catch (java.io.IOException ex) {
-            assert false : ex.getMessage();
-        }   // end catch
-        assert encoded != null;
-        return encoded;
+        return java.util.Base64.getEncoder().encodeToString(source);
     }   // end encodeBytes
 
 
