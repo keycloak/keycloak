74b2a17c42f4a639fe83140f51eb0700693c97a5
Václav Muzikář <vmuzikar@redhat.com>
2025-10-24 14:38:15 +0200

    Remove unused import
    Signed-off-by: Václav Muzikář <vmuzikar@redhat.com>

--- rest/admin-v2/providers/src/main/java/org/keycloak/services/error/ValidationExceptionHandler.java
+++ rest/admin-v2/providers/src/main/java/org/keycloak/services/error/ValidationExceptionHandler.java
@@ -6,7 +6,6 @@ import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
 
-import java.util.Set;
 import java.util.stream.Collectors;
 
 @Provider
