b1f47df44c5c783fcc2d0ab892e91a74c145e2c4
Alexander Schwartz <aschwart@redhat.com>
2025-10-28 17:10:24 +0100

    Avoid holding on to the realm in cached configurations
    Closes #43744
    
    Signed-off-by: Alexander Schwartz <alexander.schwartz@ibm.com>

--- model/infinispan/src/main/java/org/keycloak/models/cache/infinispan/RealmAdapter.java
+++ model/infinispan/src/main/java/org/keycloak/models/cache/infinispan/RealmAdapter.java
@@ -727,13 +727,13 @@ public class RealmAdapter implements CachedRealmModel {
     @Override
     public CibaConfig getCibaPolicy() {
         if (isUpdated()) return updated.getCibaPolicy();
-        return cached.getCibaConfig(session, modelSupplier);
+        return cached.getCibaConfig(modelSupplier);
     }
 
     @Override
     public ParConfig getParPolicy() {
         if (isUpdated()) return updated.getParPolicy();
-        return cached.getParConfig(session, modelSupplier);
+        return cached.getParConfig(modelSupplier);
     }
 
     @Override
