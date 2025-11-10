ad12a6e4fd9689660d090e69b1790d028341e378
Steven Hawkins <shawkins@redhat.com>
2025-11-10 12:49:18 -0500

    Update quarkus/runtime/src/main/java/org/keycloak/quarkus/runtime/cli/command/AbstractAutoBuildCommand.java
    Co-authored-by: Václav Muzikář <vaclav@muzikari.cz>
    Signed-off-by: Steven Hawkins <shawkins@redhat.com>

--- quarkus/runtime/src/main/java/org/keycloak/quarkus/runtime/cli/command/AbstractAutoBuildCommand.java
+++ quarkus/runtime/src/main/java/org/keycloak/quarkus/runtime/cli/command/AbstractAutoBuildCommand.java
@@ -66,7 +66,7 @@ public abstract class AbstractAutoBuildCommand extends AbstractCommand {
             Environment.setRebuildCheck(false);
             String runtimeProfile = getInitProfile();
             if (!Objects.equals(profile, runtimeProfile)) {
-                Environment.setProfile(getInitProfile());
+                Environment.setProfile(runtimeProfile);
                 Configuration.resetConfig();
             }
         }
