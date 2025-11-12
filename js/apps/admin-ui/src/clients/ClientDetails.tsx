070cdd799532b83b723703ea472e6252925c636c
Keshav Deshpande <keshavprashantdeshpande@gmail.com>
2025-11-12 11:41:49 +0100

    Default values for request obj
    Closes #43034
    
    Signed-off-by: Keshav Deshpande <keshavprashantdeshpande@gmail.com>

--- js/apps/admin-ui/src/clients/ClientDetails.tsx
+++ js/apps/admin-ui/src/clients/ClientDetails.tsx
@@ -312,6 +312,24 @@ export default function ClientDetails() {
 
   const setupForm = (client: ClientRepresentation) => {
     convertToFormValues(client, form.setValue);
+
+    // Convert empty/null request object attributes to "any" for display
+    if (client.attributes) {
+      const requestObjectAttrs = [
+        "request.object.signature.alg",
+        "request.object.encryption.alg",
+        "request.object.encryption.enc",
+      ];
+      requestObjectAttrs.forEach((attr) => {
+        if (!client.attributes![attr] || client.attributes![attr] === "") {
+          form.setValue(
+            convertAttributeNameToForm<FormFields>(`attributes.${attr}`),
+            "any"
+          );
+        }
+      });
+    }
+
     if (client.attributes?.["acr.loa.map"]) {
       form.setValue(
         convertAttributeNameToForm("attributes.acr.loa.map"),
@@ -369,6 +387,20 @@ export default function ClientDetails() {
             .map(({ key, value }) => [key, value]),
         ),
       );
+    }
+
+    // Convert "any" to empty string for request object attributes
+    if (submittedClient.attributes) {
+      const requestObjectAttrs = [
+        "request.object.signature.alg",
+        "request.object.encryption.alg",
+        "request.object.encryption.enc",
+      ];
+      requestObjectAttrs.forEach((attr) => {
+        if (submittedClient.attributes![attr] === "any") {
+          submittedClient.attributes![attr] = "";
+        }
+      });
     }
 
     try {
