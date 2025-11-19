ca205272ba6360bc808d19b5f8e2af119fa37c5a
rmartinc <rmartinc@redhat.com>
2025-11-19 11:01:16 +0100

    Initial integration of the JWT Authorization Grant in client Policies Using the downscope executor for testing Closes #44201
    Signed-off-by: rmartinc <rmartinc@redhat.com>

--- tests/base/src/test/java/org/keycloak/tests/oauth/AbstractJWTAuthorizationGrantTest.java
+++ tests/base/src/test/java/org/keycloak/tests/oauth/AbstractJWTAuthorizationGrantTest.java
@@ -1,69 +1,22 @@
 package org.keycloak.tests.oauth;
 
 import java.util.List;
-import java.util.UUID;
 
 import org.keycloak.OAuth2Constants;
 import org.keycloak.broker.oidc.OIDCIdentityProviderConfig;
-import org.keycloak.common.Profile;
 import org.keycloak.common.util.Time;
 import org.keycloak.crypto.Algorithm;
-import org.keycloak.events.EventType;
-import org.keycloak.protocol.oidc.OIDCConfigAttributes;
 import org.keycloak.representations.AccessToken;
 import org.keycloak.representations.IDToken;
 import org.keycloak.representations.JsonWebToken;
-import org.keycloak.representations.idm.EventRepresentation;
-import org.keycloak.testframework.annotations.InjectEvents;
-import org.keycloak.testframework.annotations.InjectRealm;
-import org.keycloak.testframework.annotations.InjectUser;
-import org.keycloak.testframework.events.EventAssertion;
-import org.keycloak.testframework.events.Events;
-import org.keycloak.testframework.oauth.OAuthClient;
 import org.keycloak.testframework.oauth.OAuthIdentityProvider;
-import org.keycloak.testframework.oauth.OAuthIdentityProviderConfig;
-import org.keycloak.testframework.oauth.OAuthIdentityProviderConfigBuilder;
-import org.keycloak.testframework.oauth.annotations.InjectOAuthClient;
-import org.keycloak.testframework.oauth.annotations.InjectOAuthIdentityProvider;
-import org.keycloak.testframework.realm.ManagedRealm;
-import org.keycloak.testframework.realm.ManagedUser;
-import org.keycloak.testframework.realm.RealmConfig;
-import org.keycloak.testframework.realm.RealmConfigBuilder;
-import org.keycloak.testframework.realm.UserConfig;
-import org.keycloak.testframework.realm.UserConfigBuilder;
-import org.keycloak.testframework.remote.timeoffset.InjectTimeOffSet;
-import org.keycloak.testframework.remote.timeoffset.TimeOffSet;
-import org.keycloak.testframework.server.KeycloakServerConfigBuilder;
-import org.keycloak.tests.client.authentication.external.ClientAuthIdpServerConfig;
 import org.keycloak.testsuite.util.oauth.AccessTokenResponse;
 
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public abstract class AbstractJWTAuthorizationGrantTest {
-
-    public static String IDP_ALIAS = "authorization-grant-idp-alias";
-    public static final String IDP_ISSUER = "https://authorization-grant-issuer";
-
-    @InjectOAuthIdentityProvider(config = AbstractJWTAuthorizationGrantTest.AGIdpConfig.class)
-    OAuthIdentityProvider identityProvider;
-
-    @InjectRealm(config = JWTAuthorizationGrantRealmConfig.class)
-    protected ManagedRealm realm;
-
-    @InjectUser(config = FederatedUserConfiguration.class)
-    ManagedUser user;
-
-    @InjectOAuthClient
-    OAuthClient oAuthClient;
-
-    @InjectEvents
-    Events events;
-
-    @InjectTimeOffSet
-    TimeOffSet timeOffSet;
+public abstract class AbstractJWTAuthorizationGrantTest extends BaseAbstractJWTAuthorizationGrantTest {
 
     @Test
     public void testPublicClient() {
@@ -115,7 +68,7 @@ public abstract class AbstractJWTAuthorizationGrantTest {
         assertFailure("Token is not active", response, events.poll());
 
         //test max exp default settings
-        jwt = getIdentityProvider().encodeToken(createAuthorizationGrantToken("basic-user-id", oAuthClient.getEndpoints().getIssuer(), IDP_ISSUER, Time.currentTime() + 301L));
+        jwt = getIdentityProvider().encodeToken(createAuthorizationGrantToken("basic-user-id", oAuthClient.getEndpoints().getIssuer(), IDP_ISSUER, Time.currentTime() + 305L));
         response = oAuthClient.jwtAuthorizationGrantRequest(jwt).send();
         assertFailure("Token expiration is too far in the future and iat claim not present in token", response, events.poll());
 
@@ -277,103 +230,5 @@ public abstract class AbstractJWTAuthorizationGrantTest {
         String jwt = getIdentityProvider().encodeToken(createDefaultAuthorizationGrantToken());
         AccessTokenResponse response = oAuthClient.jwtAuthorizationGrantRequest(jwt).send();
         assertSuccess("test-app", "basic-user", response);
-    }
-
-    protected JsonWebToken createDefaultAuthorizationGrantToken() {
-        return createAuthorizationGrantToken("basic-user-id", oAuthClient.getEndpoints().getIssuer(), IDP_ISSUER, Time.currentTime() + 300L);
-    }
-
-    protected JsonWebToken createAuthorizationGrantToken(String subject, String audience, String issuer) {
-        return createAuthorizationGrantToken(subject, audience, issuer, Time.currentTime() + 300L, (long) Time.currentTime());
-    }
-
-    protected JsonWebToken createAuthorizationGrantToken(String subject, String audience, String issuer, Long exp) {
-        return createAuthorizationGrantToken(subject, audience, issuer, exp, null);
-    }
-
-    protected JsonWebToken createAuthorizationGrantToken(String subject, String audience, String issuer, Long exp, Long iat) {
-        JsonWebToken token = new JsonWebToken();
-        token.id(UUID.randomUUID().toString());
-        token.subject(subject);
-        token.audience(audience);
-        token.issuer(issuer);
-        token.exp(exp);
-        token.iat(iat);
-        return token;
-    }
-
-    public OAuthIdentityProvider getIdentityProvider() {
-        return identityProvider;
-    }
-
-    public static class AGIdpConfig implements OAuthIdentityProviderConfig {
-
-        @Override
-        public OAuthIdentityProviderConfigBuilder configure(OAuthIdentityProviderConfigBuilder config) {
-            return config;
-        }
-    }
-
-    public static class JWTAuthorizationGrantServerConfig extends ClientAuthIdpServerConfig {
-
-        @Override
-        public KeycloakServerConfigBuilder configure(KeycloakServerConfigBuilder config) {
-            return super.configure(config).features(Profile.Feature.JWT_AUTHORIZATION_GRANT);
-        }
-    }
-
-    public static class JWTAuthorizationGrantRealmConfig implements RealmConfig {
-
-        @Override
-        public RealmConfigBuilder configure(RealmConfigBuilder realm) {
-            realm.addClient("test-public").publicClient(true);
-            realm.addClient("authorization-grant-disabled-client").publicClient(false).secret("test-secret");
-            realm.addClient("authorization-grant-not-allowed-idp-client").publicClient(false).attribute(OIDCConfigAttributes.JWT_AUTHORIZATION_GRANT_ENABLED, "true").secret("test-secret");
-            return realm;
-        }
-    }
-
-    public static class FederatedUserConfiguration implements UserConfig {
-
-        @Override
-        public UserConfigBuilder configure(UserConfigBuilder user) {
-            return user
-                    .username("basic-user")
-                    .password("password")
-                    .email("basic@localhost")
-                    .name("First", "Last")
-                    .federatedLink(IDP_ALIAS, "basic-user-id", "basic-user");
-        }
-    }
-
-    protected AccessToken assertSuccess(String expectedClientId, String username, AccessTokenResponse response) {
-        Assertions.assertTrue(response.isSuccess());
-        Assertions.assertNull(response.getRefreshToken());
-        AccessToken accessToken = oAuthClient.parseToken(response.getAccessToken(), AccessToken.class);
-        Assertions.assertNull(accessToken.getSessionId());
-        MatcherAssert.assertThat(accessToken.getId(), Matchers.startsWith("trrtag:"));
-        Assertions.assertEquals(expectedClientId, accessToken.getIssuedFor());
-        Assertions.assertEquals(username, accessToken.getPreferredUsername());
-        EventAssertion.assertSuccess(events.poll())
-                .type(EventType.LOGIN)
-                .clientId(expectedClientId)
-                .details("grant_type", OAuth2Constants.JWT_AUTHORIZATION_GRANT)
-                .details("username", username);
-        return accessToken;
-    }
-
-    protected void assertFailure(String expectedErrorDescription, AccessTokenResponse response, EventRepresentation event) {
-        assertFailure("invalid_grant", expectedErrorDescription, response, event);
-    }
-
-    protected void assertFailure(String expectedError, String expectedErrorDescription, AccessTokenResponse response, EventRepresentation event) {
-        Assertions.assertFalse(response.isSuccess());
-        Assertions.assertEquals(expectedError, response.getError());
-        Assertions.assertEquals(expectedErrorDescription, response.getErrorDescription());
-        EventAssertion.assertError(event)
-                .type(EventType.LOGIN_ERROR)
-                .error("invalid_request")
-                .details("grant_type", OAuth2Constants.JWT_AUTHORIZATION_GRANT)
-                .details("reason", expectedErrorDescription);
     }
 }
