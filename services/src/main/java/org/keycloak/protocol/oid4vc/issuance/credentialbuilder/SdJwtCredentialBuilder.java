a2a4adfa287930c3b5674bdd2b79d08a0a40010f
Thomas Diesler <tdiesler@ibm.com>
2025-11-21 11:14:40 +0100

    -- address reviewer feedback
    -- fix duplicate salt in DisclosureRedListTest
    -- move constants to OID4VCConstants
    -- remove var style declarations
    
    Signed-off-by: Thomas Diesler <tdiesler@ibm.com>

--- services/src/main/java/org/keycloak/protocol/oid4vc/issuance/credentialbuilder/SdJwtCredentialBuilder.java
+++ services/src/main/java/org/keycloak/protocol/oid4vc/issuance/credentialbuilder/SdJwtCredentialBuilder.java
@@ -18,25 +18,29 @@
 package org.keycloak.protocol.oid4vc.issuance.credentialbuilder;
 
 import java.time.Duration;
+import java.time.Instant;
 import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
 import org.keycloak.protocol.oid4vc.model.CredentialBuildConfig;
+import org.keycloak.protocol.oid4vc.model.CredentialSubject;
 import org.keycloak.protocol.oid4vc.model.Format;
 import org.keycloak.protocol.oid4vc.model.VerifiableCredential;
 import org.keycloak.sdjwt.DisclosureSpec;
 import org.keycloak.sdjwt.SdJwt;
 import org.keycloak.sdjwt.SdJwtUtils;
 
-public class SdJwtCredentialBuilder implements CredentialBuilder {
+import static org.keycloak.OID4VCConstants.CLAIM_NAME_EXP;
+import static org.keycloak.OID4VCConstants.CLAIM_NAME_IAT;
+import static org.keycloak.OID4VCConstants.CLAIM_NAME_ISSUER;
+import static org.keycloak.OID4VCConstants.CLAIM_NAME_SUB;
+import static org.keycloak.OID4VCConstants.CLAIM_NAME_SUBJECT_ID;
+import static org.keycloak.OID4VCConstants.CLAIM_NAME_VCT;
 
-    public static final String JWT_ISSUER_CLAIM = "iss";
-    public static final String JWT_ISSUANCE_DATE_CLAIM = "iat";
-    public static final String JWT_EXPIRE_DATE_CLAIM = "exp";
-    public static final String JWT_SUBJECT_CLAIM = "sub";
-    public static final String JWT_CREDENTIAL_TYPE_CLAIM = "vct";
-    public static final String SUBJECT_ID_CLAIM = "id";
+public class SdJwtCredentialBuilder implements CredentialBuilder {
 
     public SdJwtCredentialBuilder() {
     }
@@ -52,24 +56,24 @@ public class SdJwtCredentialBuilder implements CredentialBuilder {
             CredentialBuildConfig credentialBuildConfig
     ) throws CredentialBuilderException {
 
-        var maybeIssuanceDate = verifiableCredential.getIssuanceDate();
-        var maybeExpirySeconds = credentialBuildConfig.getExpiryInSeconds();
+        Instant issuanceDate = verifiableCredential.getIssuanceDate();
+        Integer expirySeconds = credentialBuildConfig.getExpiryInSeconds();
 
         // Retrieve subject claims
-        var credentialSubject = verifiableCredential.getCredentialSubject();
-        var claims = new LinkedHashMap<>(credentialSubject.getClaims());
+        CredentialSubject credentialSubject = verifiableCredential.getCredentialSubject();
+        Map<String, Object> claims = new LinkedHashMap<>(credentialSubject.getClaims());
 
         // Add inner (disclosed) claims iat, sub - the latter being derived from Subject.id
-        Optional.ofNullable(maybeIssuanceDate).ifPresent(it -> {
-            claims.put(JWT_ISSUANCE_DATE_CLAIM, it.getEpochSecond());
+        Optional.ofNullable(issuanceDate).ifPresent(it -> {
+            claims.put(CLAIM_NAME_IAT, it.getEpochSecond());
         });
-        Optional.ofNullable(claims.get(SUBJECT_ID_CLAIM)).ifPresent(it -> {
-            claims.put(JWT_SUBJECT_CLAIM, claims.remove(SUBJECT_ID_CLAIM));
+        Optional.ofNullable(claims.get(CLAIM_NAME_SUBJECT_ID)).ifPresent(it -> {
+            claims.put(CLAIM_NAME_SUB, claims.remove(CLAIM_NAME_SUBJECT_ID));
         });
 
         // Put inner claims into the disclosure spec, except the one to be kept visible
-        var disclosureSpecBuilder = DisclosureSpec.builder();
-        var outerClaims = credentialBuildConfig.getSdJwtVisibleClaims();
+        DisclosureSpec.Builder disclosureSpecBuilder = DisclosureSpec.builder();
+        List<String> outerClaims = credentialBuildConfig.getSdJwtVisibleClaims();
         claims.keySet().stream()
                 .filter(it -> !outerClaims.contains(it))
                 .forEach(it -> {
@@ -79,11 +83,11 @@ public class SdJwtCredentialBuilder implements CredentialBuilder {
         // Add outer (always visible) claims: iss, vct, exp
         // https://www.ietf.org/archive/id/draft-ietf-oauth-sd-jwt-vc-11.html#section-3.2.2.2
 
-        claims.put(JWT_ISSUER_CLAIM, credentialBuildConfig.getCredentialIssuer());
-        claims.put(JWT_CREDENTIAL_TYPE_CLAIM, credentialBuildConfig.getCredentialType());
-        if (maybeIssuanceDate != null && maybeExpirySeconds != null) {
-            var exp = maybeIssuanceDate.plus(Duration.ofSeconds(maybeExpirySeconds));
-            claims.put(JWT_EXPIRE_DATE_CLAIM, exp.getEpochSecond());
+        claims.put(CLAIM_NAME_ISSUER, credentialBuildConfig.getCredentialIssuer());
+        claims.put(CLAIM_NAME_VCT, credentialBuildConfig.getCredentialType());
+        if (issuanceDate != null && expirySeconds != null) {
+            Instant exp = issuanceDate.plus(Duration.ofSeconds(expirySeconds));
+            claims.put(CLAIM_NAME_EXP, exp.getEpochSecond());
         }
 
         // Add the configured number of decoys
@@ -92,7 +96,7 @@ public class SdJwtCredentialBuilder implements CredentialBuilder {
                     .forEach(i -> disclosureSpecBuilder.withDecoyClaim(SdJwtUtils.randomSalt()));
         }
 
-        var disclosureSpec = disclosureSpecBuilder.build();
+        DisclosureSpec disclosureSpec = disclosureSpecBuilder.build();
         var sdJwtBuilder = SdJwt.builder()
                 .withDisclosureSpec(disclosureSpec)
                 .withHashAlgorithm(credentialBuildConfig.getHashAlgorithm())
