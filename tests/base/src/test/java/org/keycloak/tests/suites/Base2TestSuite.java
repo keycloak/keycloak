34b1aa5ccef54df6f825d3a38f28d0ad4b13b23e
stianst <stianst@gmail.com>
2025-11-25 10:27:41 +0100

    Migrate transactions package to new testsuite
    Closes #44460
    
    Signed-off-by: stianst <stianst@gmail.com>

--- tests/base/src/test/java/org/keycloak/tests/suites/Base2TestSuite.java
+++ tests/base/src/test/java/org/keycloak/tests/suites/Base2TestSuite.java
@@ -14,6 +14,7 @@ import org.junit.platform.suite.api.Suite;
         "org.keycloak.tests.keys",
         "org.keycloak.tests.oauth",
         "org.keycloak.tests.tracing",
+        "org.keycloak.tests.transactions",
         "org.keycloak.tests.welcomepage"
 })
 public class Base2TestSuite {
