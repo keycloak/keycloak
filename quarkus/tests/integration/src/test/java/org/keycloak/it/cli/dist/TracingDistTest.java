8def69105394f0a63248ff2f269bd4638deeef9f
Martin Bartoš <mabartos@redhat.com>
2025-12-10 13:03:46 +0100

    [OTel] Provide general options for telemetry settings (#41705)
    * [OTel] Provide general options for telemetry settings
    
    Closes #41263
    
    Co-authored-by: Ryan Emerson <remerson@redhat.com>
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>
    
    * Update docs/guides/observability/telemetry.adoc
    
    Co-authored-by: Václav Muzikář <vaclav@muzikari.cz>
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>
    
    * Provide release notes and deprecation note
    
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>
    
    * Ignore link to the telemetry guide for now
    
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>
    
    ---------
    
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>
    Co-authored-by: Ryan Emerson <remerson@redhat.com>
    Co-authored-by: Václav Muzikář <vaclav@muzikari.cz>

--- quarkus/tests/integration/src/test/java/org/keycloak/it/cli/dist/TracingDistTest.java
+++ quarkus/tests/integration/src/test/java/org/keycloak/it/cli/dist/TracingDistTest.java
@@ -33,18 +33,18 @@ import org.junit.jupiter.api.TestMethodOrder;
 @RawDistOnly(reason = "Containers are immutable")
 public class TracingDistTest {
 
-    private void assertTracingEnabled(CLIResult result) {
+    static void assertTracingEnabled(CLIResult result) {
         result.assertMessage("opentelemetry");
         result.assertMessage("service.name=\"keycloak\"");
     }
 
-    private void assertTracingDisabled(CLIResult result) {
+    static void assertTracingDisabled(CLIResult result) {
         result.assertMessage("opentelemetry");
         result.assertNoMessage("service.name=\"keycloak\"");
         assertSamplingDisabled(result);
     }
 
-    private void assertSamplingDisabled(CLIResult result) {
+    private static void assertSamplingDisabled(CLIResult result) {
         result.assertNoMessage("Failed to export spans.");
         result.assertNoMessage("Connection refused: localhost/127.0.0.1:4317");
     }
@@ -130,7 +130,7 @@ public class TracingDistTest {
     void emptyEndpoint(LaunchResult result) {
         CLIResult cliResult = (CLIResult) result;
 
-        cliResult.assertError("URL specified in 'tracing-endpoint' option must not be empty.");
+        cliResult.assertError("Specified Endpoint URL must not be empty.");
     }
 
     @Test
@@ -138,7 +138,7 @@ public class TracingDistTest {
     void invalidUrl(LaunchResult result) {
         CLIResult cliResult = (CLIResult) result;
 
-        cliResult.assertError("URL specified in 'tracing-endpoint' option is invalid.");
+        cliResult.assertError("Specified Endpoint URL is invalid.");
     }
 
     @Test
@@ -188,6 +188,7 @@ public class TracingDistTest {
     void differentServiceName(LaunchResult result) {
         CLIResult cliResult = (CLIResult) result;
 
+        cliResult.assertMessage("- tracing-service-name: Service name is not directly related to Tracing and you should use the Telemetry option which takes precedence. Use telemetry-service-name.");
         cliResult.assertMessage("opentelemetry");
         cliResult.assertMessage("service.name=\"my-service\"");
 
@@ -211,7 +212,7 @@ public class TracingDistTest {
         CLIResult cliResult = (CLIResult) result;
 
         assertTracingEnabled(cliResult);
-
+        cliResult.assertMessage("- tracing-resource-attributes: Resource attributes are not directly related to Tracing and you should use the Telemetry option which takes precedence. Use telemetry-resource-attributes.");
         cliResult.assertMessage("some.key1=\"some.val1\"");
         cliResult.assertMessage("some.key2=\"some.val2\"");
 
