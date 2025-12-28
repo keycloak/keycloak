ff9ec6f0fed3642696242a53caacfc2b000c42e5
Peter Szabo <petersz.szabo@gmail.com>
2025-12-28 15:55:57 +0100

    Added Prefix option to User Attribute mapper
    Signed-off-by: Peter Szabo <petersz.szabo@gmail.com>

--- services/src/main/java/org/keycloak/broker/oidc/mappers/UserAttributeMapper.java
+++ services/src/main/java/org/keycloak/broker/oidc/mappers/UserAttributeMapper.java
@@ -39,6 +39,8 @@ import org.keycloak.models.UserModel;
 import org.keycloak.provider.ProviderConfigProperty;
 import org.keycloak.saml.common.util.StringUtil;
 
+import org.jboss.logging.Logger;
+
 /**
  * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
  * @version $Revision: 1 $
@@ -50,6 +52,7 @@ public class UserAttributeMapper extends AbstractClaimMapper {
     private static final List<ProviderConfigProperty> configProperties = new ArrayList<>();
 
     public static final String USER_ATTRIBUTE = "user.attribute";
+    public static final String ATTRIBUTE_PREFFIX = "attribute.prefix";
     public static final String EMAIL = "email";
     public static final String FIRST_NAME = "firstName";
     public static final String LAST_NAME = "lastName";
@@ -58,21 +61,34 @@ public class UserAttributeMapper extends AbstractClaimMapper {
     static {
         ProviderConfigProperty property;
         ProviderConfigProperty property1;
+        ProviderConfigProperty property2;
+
         property1 = new ProviderConfigProperty();
         property1.setName(CLAIM);
         property1.setLabel("Claim");
         property1.setHelpText("Name of claim to search for in token. You can reference nested claims using a '.', i.e. 'address.locality'. To use dot (.) literally, escape it with backslash (\\.)");
         property1.setType(ProviderConfigProperty.STRING_TYPE);
         configProperties.add(property1);
+
         property = new ProviderConfigProperty();
         property.setName(USER_ATTRIBUTE);
         property.setLabel("User Attribute Name");
         property.setHelpText("User attribute name to store claim.  Use email, lastName, and firstName to map to those predefined user properties.");
         property.setType(ProviderConfigProperty.USER_PROFILE_ATTRIBUTE_LIST_TYPE);
         configProperties.add(property);
+
+        property2 = new ProviderConfigProperty();
+        property2.setName(ATTRIBUTE_PREFFIX);
+        property2.setLabel("Attribute Value Prefix");
+        property2.setHelpText(
+                "Optional prefix to be concatenated in front of every imported attribute value.");
+        property2.setType(ProviderConfigProperty.STRING_TYPE);
+        configProperties.add(property2);
     }
 
     public static final String PROVIDER_ID = "oidc-user-attribute-idp-mapper";
+
+    private static final Logger LOG = Logger.getLogger(UserAttributeMapper.class);
 
     @Override
     public boolean supportsSyncMode(IdentityProviderSyncMode syncMode) {
@@ -106,12 +122,27 @@ public class UserAttributeMapper extends AbstractClaimMapper {
 
     @Override
     public void preprocessFederatedIdentity(KeycloakSession session, RealmModel realm, IdentityProviderMapperModel mapperModel, BrokeredIdentityContext context) {
+        LOG.debug("executing preprocessFederatedIdentity()");
+        
         String attribute = mapperModel.getConfig().get(USER_ATTRIBUTE);
-        if(StringUtil.isNullOrEmpty(attribute)){
+        String prefix = Objects.toString(mapperModel.getConfig().get(ATTRIBUTE_PREFFIX), "");
+
+        if (StringUtil.isNullOrEmpty(attribute)) {
+            LOG.debug("Empty attribute, not processing.");
             return;
         }
+
         Object value = getClaimValue(mapperModel, context);
-        List<String> values = toList(value);
+        List<String> values;
+
+        if (StringUtil.isNullOrEmpty(prefix)) {
+            LOG.debug("No attribute prefix configured.");
+            values = toList(value);
+        }
+        else {
+            LOG.debug("Retrieved prefix: " + prefix);
+            values = toPrefixedList(value, prefix);
+        } 
 
         if (EMAIL.equalsIgnoreCase(attribute)) {
             setIfNotEmpty(context::setEmail, values);
@@ -145,14 +176,41 @@ public class UserAttributeMapper extends AbstractClaimMapper {
                 .collect(Collectors.toList());
     }
 
+    private List<String> toPrefixedList(Object value, String prefix) {
+        List<Object> values = (value instanceof List)
+            ? (List) value
+            : Collections.singletonList(value);
+
+        return values.stream()
+            .filter(Objects::nonNull)
+            .map(item -> prefix.concat(item.toString()))
+            .collect(Collectors.toList());
+    }
+
     @Override
     public void updateBrokeredUser(KeycloakSession session, RealmModel realm, UserModel user, IdentityProviderMapperModel mapperModel, BrokeredIdentityContext context) {
+        LOG.debug("executing updateBrokeredUser()");
+
         String attribute = mapperModel.getConfig().get(USER_ATTRIBUTE);
-        if(StringUtil.isNullOrEmpty(attribute)){
+        String prefix = Objects.toString(mapperModel.getConfig().get(ATTRIBUTE_PREFFIX), "");
+
+        if (StringUtil.isNullOrEmpty(attribute)) {
+            LOG.debug("Empty attribute, not processing.");
             return;
         }
+
         Object value = getClaimValue(mapperModel, context);
-        List<String> values = toList(value);
+        List<String> values;
+
+        if (StringUtil.isNullOrEmpty(prefix)) {
+            LOG.debug("No attribute prefix configured.");
+            values = toList(value);
+        }
+        else {
+            LOG.debug("Retrieved prefix: " + prefix);
+            values = toPrefixedList(value, prefix);
+        } 
+
         if (EMAIL.equalsIgnoreCase(attribute)) {
             setIfNotEmpty(user::setEmail, values);
         } else if (FIRST_NAME.equalsIgnoreCase(attribute)) {
@@ -171,7 +229,6 @@ public class UserAttributeMapper extends AbstractClaimMapper {
 
     @Override
     public String getHelpText() {
-        return "Import declared claim if it exists in ID, access token or the claim set returned by the user profile endpoint into the specified user property or attribute.";
+        return "Import declared claim if it exists in ID, access token or the claim set returned by the user profile endpoint into the specified user property or attribute and optionally prefix each attribute value with the provided prefix.";
     }
-
 }
