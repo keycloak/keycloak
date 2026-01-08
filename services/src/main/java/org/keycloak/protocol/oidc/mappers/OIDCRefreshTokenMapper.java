49ce7530002cda8c6be7709e6296564fa2bcfc26
Markus Dahm <markus.dahm@spree.de>
2026-01-08 11:28:54 +0100

    https://github.com/keycloak/keycloak/issues/45260
    Introduce OIDCRefreshTokenMapper analogous to OIDCIDTokenMapper

---
+++ services/src/main/java/org/keycloak/protocol/oidc/mappers/OIDCRefreshTokenMapper.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2016 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.keycloak.protocol.oidc.mappers;
+
+import org.keycloak.models.ClientSessionContext;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.ProtocolMapperModel;
+import org.keycloak.models.UserSessionModel;
+import org.keycloak.protocol.oidc.TokenManager;
+import org.keycloak.representations.RefreshToken;
+
+/**
+ * Transform given refresh token, analogous to {@link OIDCIDTokenMapper}.
+ * <p>
+ * Used by {@link TokenManager.AccessTokenResponseBuilder#generateRefreshToken()}
+ *
+ * @author <a href="mailto:markus.dahm@spree.de">Markus Dahm</a>
+ */
+public interface OIDCRefreshTokenMapper {
+  RefreshToken transformRefreshToken(
+      RefreshToken token, ProtocolMapperModel mappingModel, KeycloakSession session,
+      UserSessionModel userSession, ClientSessionContext clientSession
+  );
+}
