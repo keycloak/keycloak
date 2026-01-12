03a55b606e733f5158c786ac3a9ee08303d8d7e5
Lukas Hanusovsky <lhanusov@redhat.com>
2026-01-12 15:09:06 +0100

    Removing cross module dependency between tests/base and tests/clustering.
    Signed-off-by: Lukas Hanusovsky <lhanusov@redhat.com>

--- tests/clustering/src/test/java/org/keycloak/tests/clustering/JdbcPingCustomSchemaTest.java
+++ tests/clustering/src/test/java/org/keycloak/tests/clustering/JdbcPingCustomSchemaTest.java
@@ -2,19 +2,40 @@ package org.keycloak.tests.clustering;
 
 import org.keycloak.testframework.annotations.InjectTestDatabase;
 import org.keycloak.testframework.annotations.KeycloakIntegrationTest;
+import org.keycloak.testframework.conditions.DisabledForDatabases;
+import org.keycloak.testframework.database.DatabaseConfig;
+import org.keycloak.testframework.database.DatabaseConfigBuilder;
 import org.keycloak.testframework.database.TestDatabase;
 import org.keycloak.testframework.injection.LifeCycle;
-import org.keycloak.tests.db.CaseSensitiveSchemaTest;
+import org.keycloak.testframework.server.KeycloakServerConfig;
+import org.keycloak.testframework.server.KeycloakServerConfigBuilder;
 
 import org.junit.jupiter.api.Test;
 
-@KeycloakIntegrationTest(config = CaseSensitiveSchemaTest.CaseSensitiveServerConfig.class)
+@KeycloakIntegrationTest(config = JdbcPingCustomSchemaTest.JdbcPingCustomSchemaServerConfig.class)
+@DisabledForDatabases({"mariadb", "mssql", "mysql", "oracle", "tidb"})
 public class JdbcPingCustomSchemaTest {
-    @InjectTestDatabase(config = CaseSensitiveSchemaTest.CaseSensitiveDatabaseConfig.class, lifecycle = LifeCycle.CLASS)
+
+    @InjectTestDatabase(config = JdbcPingCustomSchemaDatabaseConfig.class, lifecycle = LifeCycle.CLASS)
     TestDatabase db;
 
     @Test
     public void testClusterFormed() {
         // no-op ClusteredKeycloakServer will fail if a cluster is not formed
+    }
+
+    public static class JdbcPingCustomSchemaServerConfig implements KeycloakServerConfig {
+        @Override
+        public KeycloakServerConfigBuilder configure(KeycloakServerConfigBuilder config) {
+            return config.option("db-schema", "KEYCLOAK");
+        }
+    }
+
+    public static class JdbcPingCustomSchemaDatabaseConfig implements DatabaseConfig {
+        @Override
+        public DatabaseConfigBuilder configure(DatabaseConfigBuilder database) {
+            database.initScript("org/keycloak/tests/clustering/case-sensitive-schema-postgres.sql");
+            return database;
+        }
     }
 }
