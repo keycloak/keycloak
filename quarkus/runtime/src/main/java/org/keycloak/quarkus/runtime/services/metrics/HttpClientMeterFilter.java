e59d33e3ef519641870813dd5a37f4d35c127b29
Ryan Emerson <remerson@ibm.com>
2026-01-12 15:02:18 +0000

    Add comment detailing the purpose of the HttpClientMeterFilter metric transformation
    Signed-off-by: Ryan Emerson <remerson@ibm.com>

--- quarkus/runtime/src/main/java/org/keycloak/quarkus/runtime/services/metrics/HttpClientMeterFilter.java
+++ quarkus/runtime/src/main/java/org/keycloak/quarkus/runtime/services/metrics/HttpClientMeterFilter.java
@@ -10,6 +10,7 @@ public class HttpClientMeterFilter implements MeterFilter {
     @Override
     public Meter.Id map(Meter.Id id) {
         if (id.getName().startsWith("httpcomponents.httpclient.")) {
+            // Replace the httpcomponents.httpclient prefix with one that is implementation independent
             return id.withName(id.getName().replace("httpcomponents.httpclient.", "http.client."));
         }
         return id;
