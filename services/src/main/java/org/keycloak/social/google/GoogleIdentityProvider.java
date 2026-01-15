cff06e9c893e85287eb0424a53a81d33051a5335
Giuseppe Graziano <g.graziano94@gmail.com>
2026-01-15 09:23:03 +0100

    JWT Authorization Grant for Google idp
    Closes #45179
    
    Signed-off-by: Giuseppe Graziano <g.graziano94@gmail.com>

--- services/src/main/java/org/keycloak/social/google/GoogleIdentityProvider.java
+++ services/src/main/java/org/keycloak/social/google/GoogleIdentityProvider.java
@@ -16,6 +16,7 @@
  */
 package org.keycloak.social.google;
 
+import java.util.Collections;
 import java.util.List;
 
 import jakarta.ws.rs.core.MultivaluedMap;
@@ -43,15 +44,18 @@ import org.keycloak.services.ErrorResponseException;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
+
 /**
  * @author <a href="mailto:sthorger@redhat.com">Stian Thorgersen</a>
  */
 public class GoogleIdentityProvider extends OIDCIdentityProvider implements SocialIdentityProvider<OIDCIdentityProviderConfig> {
 
+    public static final String ISSUER_URL = "https://accounts.google.com";
     public static final String AUTH_URL = "https://accounts.google.com/o/oauth2/v2/auth";
     public static final String TOKEN_URL = "https://oauth2.googleapis.com/token";
     public static final String PROFILE_URL = "https://openidconnect.googleapis.com/v1/userinfo";
     public static final String TOKEN_INFO_URL = "https://oauth2.googleapis.com/tokeninfo";
+    public static final String JWKS_URL = "https://www.googleapis.com/oauth2/v3/certs";
     public static final String DEFAULT_SCOPE = "openid profile email";
 
     private static final String OIDC_PARAMETER_HOSTED_DOMAINS = "hd";
@@ -63,6 +67,8 @@ public class GoogleIdentityProvider extends OIDCIdentityProvider implements Soci
         config.setAuthorizationUrl(AUTH_URL);
         config.setTokenUrl(TOKEN_URL);
         config.setUserInfoUrl(PROFILE_URL);
+        getConfig().setUseJwksUrl(true);
+        getConfig().setJwksUrl(JWKS_URL);
     }
 
     @Override
@@ -164,4 +170,14 @@ public class GoogleIdentityProvider extends OIDCIdentityProvider implements Soci
         throw new IdentityBrokerException("Hosted domain does not match.");
     }
 
+    @Override
+    public boolean isAssertionReuseAllowed() {
+        return true;
+    }
+
+    @Override
+    public List<String> getAllowedAudienceForJWTGrant() {
+        //google IDToken audience can only contain client-id, this is an exception compared to the RFC7523
+        return Collections.singletonList(getConfig().getClientId());
+    }
 }
