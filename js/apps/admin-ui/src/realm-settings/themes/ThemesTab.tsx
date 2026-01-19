412aa1f2c5a5c2896533a9a5d17d5220f5a42a46
Martin Bartoš <mabartos@redhat.com>
2026-01-19 19:57:32 +0100

    [quick-theme] Ability to set the name of the theme
    Closes #45529
    
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>

--- js/apps/admin-ui/src/realm-settings/themes/ThemesTab.tsx
+++ js/apps/admin-ui/src/realm-settings/themes/ThemesTab.tsx
@@ -21,6 +21,7 @@ type ThemesTabProps = {
 };
 
 export type ThemeRealmRepresentation = RealmRepresentation & {
+  themeName?: string;
   fileName?: string;
   favicon?: File;
   logo?: File;
@@ -39,28 +40,29 @@ export default function ThemesTab({ realm, save }: ThemesTabProps) {
     const zip = new JSZip();
 
     const styles = JSON.parse(realm.attributes?.style ?? "{}");
-    const { favicon, logo, bgimage, fileName } = realm;
+    const { favicon, logo, bgimage, fileName, themeName } = realm;
 
+    const name = themeName || "quick-theme";
     const logoName =
       "img/logo" + logo?.name?.substring(logo?.name?.lastIndexOf("."));
     const bgimageName =
       "img/bgimage" + bgimage?.name?.substring(bgimage?.name?.lastIndexOf("."));
 
     if (favicon) {
-      zip.file(`theme/quick-theme/common/resources/img/favicon.ico`, favicon);
+      zip.file(`theme/${name}/common/resources/img/favicon.ico`, favicon);
     }
     if (logo) {
-      zip.file(`theme/quick-theme/common/resources/${logoName}`, logo);
+      zip.file(`theme/${name}/common/resources/${logoName}`, logo);
     }
     if (bgimage) {
-      zip.file(`theme/quick-theme/common/resources/${bgimageName}`, bgimage);
+      zip.file(`theme/${name}/common/resources/${bgimageName}`, bgimage);
     }
 
     zip.file(
-      "theme/quick-theme/admin/theme.properties",
+      `theme/${name}/admin/theme.properties`,
       `
 parent=keycloak.v2
-import=common/quick-theme
+import=common/${name}
 
 ${logo ? "logo=" + logoName : ""}
 ${favicon ? "favIcon=/img/favicon.ico" : ""}
@@ -69,10 +71,10 @@ styles=css/theme-styles.css
     );
 
     zip.file(
-      "theme/quick-theme/account/theme.properties",
+      `theme/${name}/account/theme.properties`,
       `
 parent=keycloak.v3
-import=common/quick-theme
+import=common/${name}
 
 ${logo ? "logo=" + logoName : ""}
 ${favicon ? "favIcon=/img/favicon.ico" : ""}
@@ -81,10 +83,10 @@ styles=css/theme-styles.css
     );
 
     zip.file(
-      "theme/quick-theme/login/theme.properties",
+      `theme/${name}/login/theme.properties`,
       `
 parent=keycloak.v2
-import=common/quick-theme
+import=common/${name}
 
 styles=css/styles.css css/theme-styles.css
 `,
@@ -94,7 +96,7 @@ styles=css/styles.css css/theme-styles.css
       "META-INF/keycloak-themes.json",
       `{
   "themes": [{
-      "name" : "quick-theme",
+      "name" : "${name}",
       "types": [ "login", "account", "admin", "common" ]
   }]
 }`,
@@ -104,12 +106,10 @@ styles=css/styles.css css/theme-styles.css
       "theme-settings.json",
       JSON.stringify({
         ...styles,
-        logo: logo ? `theme/quick-theme/common/resources/${logoName}` : "",
-        bgimage: bgimage
-          ? `theme/quick-theme/common/resources/${bgimageName}`
-          : "",
+        logo: logo ? `theme/${name}/common/resources/${logoName}` : "",
+        bgimage: bgimage ? `theme/${name}/common/resources/${bgimageName}` : "",
         favicon: favicon
-          ? "theme/quick-theme/common/resources/img/favicon.ico"
+          ? `theme/${name}/common/resources/img/favicon.ico`
           : "",
       }),
     );
@@ -124,10 +124,10 @@ styles=css/styles.css css/theme-styles.css
         `/resources/${environment.resourceVersion}/login/keycloak.v2/css/styles.css`,
       )
     ).text();
-    zip.file("theme/quick-theme/common/resources/css/styles.css", loginCss);
+    zip.file(`theme/${name}/common/resources/css/styles.css`, loginCss);
 
     zip.file(
-      "theme/quick-theme/common/resources/css/theme-styles.css",
+      `theme/${name}/common/resources/css/theme-styles.css`,
       `:root {
         ${bgimage ? `--keycloak-bg-logo-url: url('../${bgimageName}');` : ""}
         ${logo ? `--keycloak-logo-url: url('../${logoName}');` : ""}
