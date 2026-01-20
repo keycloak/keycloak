cc05fdfe90447b94c676e51378ab7ab4dfbb6d10
Erik Jan de Wit <erikjan.dewit@gmail.com>
2026-01-20 19:11:51 +0100

    changed back to use kiota as it offers a nicer fluent api
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- js/libs/keycloak-admin-client/src/index.ts
+++ js/libs/keycloak-admin-client/src/index.ts
@@ -9,19 +9,13 @@ export type { NetworkErrorOptions } from "./utils/fetchWithError.js";
 export type { default as OrganizationInvitationRepresentation } from "./defs/organizationInvitationRepresentation.js";
 export { OrganizationInvitationStatus } from "./defs/organizationInvitationRepresentation.js";
 
-// V2 API types and classes
-export {
-  ClientsV2Api,
-  createClientsV2Api,
-} from "./resources/clientsV2.js";
+// V2 API types and classes (Kiota-generated)
+export { createKiotaAdminClient } from "./resources/clientsV2.js";
 export type {
   OIDCClientRepresentation,
   SAMLClientRepresentation,
   ClientRepresentationV2,
-  AdminApiRealmNameClientsVersionGetRequest,
-  AdminApiRealmNameClientsVersionIdDeleteRequest,
-  AdminApiRealmNameClientsVersionIdGetRequest,
-  AdminApiRealmNameClientsVersionIdPatchRequest,
-  AdminApiRealmNameClientsVersionIdPutRequest,
-  AdminApiRealmNameClientsVersionPostRequest,
 } from "./resources/clientsV2.js";
+
+// Re-export Kiota AdminClient type
+export type { AdminClient } from "./generated/adminClient.js";
