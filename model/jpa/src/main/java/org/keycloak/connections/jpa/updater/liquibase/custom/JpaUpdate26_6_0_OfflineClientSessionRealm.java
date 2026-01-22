a28b925df2a9a7a53b7badd350b030110fe61120
Pedro Ruivo <1492066+pruivo@users.noreply.github.com>
2026-01-22 16:31:13 +0000

    Notable Change
    Signed-off-by: Pedro Ruivo <1492066+pruivo@users.noreply.github.com>

--- model/jpa/src/main/java/org/keycloak/connections/jpa/updater/liquibase/custom/JpaUpdate26_6_0_OfflineClientSessionRealm.java
+++ model/jpa/src/main/java/org/keycloak/connections/jpa/updater/liquibase/custom/JpaUpdate26_6_0_OfflineClientSessionRealm.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Red Hat, Inc. and/or its affiliates
+ * Copyright 2026 Red Hat, Inc. and/or its affiliates
  * and other contributors as indicated by the @author tags.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,6 +16,10 @@
  */
 package org.keycloak.connections.jpa.updater.liquibase.custom;
 
+import liquibase.database.core.MSSQLDatabase;
+import liquibase.database.core.MySQLDatabase;
+import liquibase.database.core.OracleDatabase;
+import liquibase.database.core.PostgresDatabase;
 import liquibase.exception.CustomChangeException;
 import liquibase.statement.core.RawParameterizedSqlStatement;
 
@@ -26,11 +30,30 @@ public class JpaUpdate26_6_0_OfflineClientSessionRealm extends CustomKeycloakTas
         String clientSessionTable = getTableName("OFFLINE_CLIENT_SESSION");
         String userSessionTable = getTableName("OFFLINE_USER_SESSION");
 
-        statements.add(new RawParameterizedSqlStatement("""
-                UPDATE %s c SET realm_id = u.realm_id
-                FROM %s u
-                WHERE c.user_session_id = u.user_session_id AND c.offline_flag = u.offline_flag AND c.realm_id is null"""
-                .formatted(clientSessionTable, userSessionTable)));
+        if (database instanceof PostgresDatabase) {
+            generateUpdateQueryForPostgresSQL(clientSessionTable, userSessionTable);
+            return;
+        }
+
+        if (database instanceof MySQLDatabase) {
+            // and MariaDB
+            generateUpdateQueryForMySQL(clientSessionTable, userSessionTable);
+            return;
+        }
+
+        if (database instanceof MSSQLDatabase) {
+            generateUpdateQueryForMSSQL(clientSessionTable, userSessionTable);
+            return;
+        }
+
+        if (database instanceof OracleDatabase) {
+            generateUpdateQueryForOracle(clientSessionTable, userSessionTable);
+            return;
+        }
+
+        // H2 and others, very slow with O(n^2) complexity
+        // It is standard SQL queries, it *must* be compatible with all vendors (fingers crossed)
+        generateUpdateQueryUsingStandardSQL(clientSessionTable, userSessionTable);
     }
 
     @Override
@@ -38,4 +61,49 @@ public class JpaUpdate26_6_0_OfflineClientSessionRealm extends CustomKeycloakTas
         return "Sets the realm column in offline_client_session";
     }
 
+    private void generateUpdateQueryUsingStandardSQL(String clientSessionTable, String userSessionTable) {
+        statements.add(new RawParameterizedSqlStatement("""
+                UPDATE %s cs
+                SET cs.realm_id = (
+                    SELECT us.realm_id
+                    FROM %s us
+                    WHERE us.user_session_id = cs.user_session_id
+                )"""
+                .formatted(clientSessionTable, userSessionTable)));
+    }
+
+    private void generateUpdateQueryForOracle(String clientSessionTable, String userSessionTable) {
+        statements.add(new RawParameterizedSqlStatement("""
+                MERGE INTO %s cs
+                USING %s us
+                ON (cs.user_session_id = us.user_session_id)
+                WHEN MATCHED THEN
+                UPDATE SET cs.realm_id = us.realm_id"""
+                .formatted(clientSessionTable, userSessionTable)));
+    }
+
+    private void generateUpdateQueryForMSSQL(String clientSessionTable, String userSessionTable) {
+        statements.add(new RawParameterizedSqlStatement("""
+                UPDATE %s cs
+                INNER JOIN %s us ON cs.user_session_id = us.user_session_id
+                SET cs.realm_id = us.realm_id"""
+                .formatted(clientSessionTable, userSessionTable)));
+    }
+
+    private void generateUpdateQueryForMySQL(String clientSessionTable, String userSessionTable) {
+        statements.add(new RawParameterizedSqlStatement("""
+                UPDATE %s cs
+                INNER JOIN %s us ON cs.user_session_id = us.user_session_id
+                SET cs.realm_id = us.realm_id"""
+                .formatted(clientSessionTable, userSessionTable)));
+    }
+
+    private void generateUpdateQueryForPostgresSQL(String clientSessionTable, String userSessionTable) {
+        statements.add(new RawParameterizedSqlStatement("""
+                UPDATE %s cs
+                SET realm_id = us.realm_id
+                FROM %s us
+                WHERE cs.user_session_id = us.user_session_id"""
+                .formatted(clientSessionTable, userSessionTable)));
+    }
 }
