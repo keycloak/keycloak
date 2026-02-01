89861e781b823ba894fcb226fe3ec1c1a6e5e9b2
Thomas Darimont <thomas.darimont@googlemail.com>
2026-02-01 11:45:39 +0100

    Revise DefaultSsfEventListener
    Signed-off-by: Thomas Darimont <thomas.darimont@googlemail.com>

--- services/src/main/java/org/keycloak/protocol/ssf/event/listener/DefaultSsfEventListener.java
+++ services/src/main/java/org/keycloak/protocol/ssf/event/listener/DefaultSsfEventListener.java
@@ -1,12 +1,17 @@
 package org.keycloak.protocol.ssf.event.listener;
 
+import java.io.IOException;
 import java.util.List;
 
+import org.keycloak.events.Details;
+import org.keycloak.events.EventBuilder;
+import org.keycloak.events.EventType;
 import org.keycloak.models.KeycloakContext;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.UserModel;
 import org.keycloak.models.UserSessionModel;
+import org.keycloak.protocol.ssf.event.SecurityEventToken;
 import org.keycloak.protocol.ssf.event.processor.SsfEventContext;
 import org.keycloak.protocol.ssf.event.subjects.SubjectId;
 import org.keycloak.protocol.ssf.event.subjects.SubjectUserLookup;
@@ -14,6 +19,8 @@ import org.keycloak.protocol.ssf.event.types.SsfEvent;
 import org.keycloak.protocol.ssf.event.types.caep.SessionRevoked;
 
 import org.jboss.logging.Logger;
+
+import org.keycloak.util.JsonSerialization;
 
 /**
  * Default {@link SsfEventListener} implementation.
@@ -48,23 +55,83 @@ public class DefaultSsfEventListener implements SsfEventListener {
         }
     }
 
-    protected void handleSessionRevokedEvent(SsfEventContext eventContext, RealmModel realm, SubjectId subjectId, SessionRevoked sessionRevoked) {
+    protected void handleSessionRevokedEvent(SsfEventContext eventContext, RealmModel realm, SubjectId subjectId, SessionRevoked ssfEvent) {
 
         // TODO subject is usually refering to a user, but could also be UserSession, an IdentityProvider, Organization etc. so we might need to be more flexible here
-
         List<UserSessionModel> userSessions = getUserSessions(realm, subjectId);
         if (userSessions == null || userSessions.isEmpty()) {
             return;
         }
 
         // TODO should this only affect online sessions or also offline sessions?
+        EventBuilder eventBuilder = new EventBuilder(realm, session);
         UserModel user = userSessions.get(0).getUser();
         for (var userSession : userSessions) {
-            session.sessions().removeUserSession(realm, userSession);
+
+            if (!shouldRemoveUserSession(realm, userSession, eventContext)) {
+                continue;
+            }
+
+            removeUserSession(realm, userSession, eventContext);
+
+            if (isUserEventRecordingEnabled(realm, EventType.USER_SESSION_DELETED)) {
+                fireUserEvent(eventContext, ssfEvent, userSession, eventBuilder, user);
+            }
         }
 
         LOG.debugf("Removed %s sessions for user. realm=%s userId=%s for SessionRevoked event. reasonAdmin=%s reasonUser=%s",
-                userSessions.size(), realm.getName(), user.getId(), sessionRevoked.getReasonAdmin(), sessionRevoked.getReasonUser());
+                userSessions.size(), realm.getName(), user.getId(), ssfEvent.getReasonAdmin(), ssfEvent.getReasonUser());
+    }
+
+    protected void removeUserSession(RealmModel realm, UserSessionModel userSession, SsfEventContext eventContext) {
+        session.sessions().removeUserSession(realm, userSession);
+    }
+
+    protected boolean shouldRemoveUserSession(RealmModel realm, UserSessionModel userSession, SsfEventContext eventContext) {
+        return true;
+    }
+
+    protected void fireUserEvent(SsfEventContext eventContext, SsfEvent ssfEvent, UserSessionModel userSession, EventBuilder eventBuilder, UserModel user) {
+
+        SecurityEventToken securityEventToken = eventContext.getSecurityEventToken();
+        String rawSubject = extractRawSubjectAsString(securityEventToken);
+        String rawSecurityEvent = extractSecurityEventAsString(ssfEvent);
+        eventBuilder.event(EventType.USER_SESSION_DELETED)
+                .user(user)
+                .session(userSession.getId())
+                .detail(Details.REASON, "user_session_revoked")
+                .detail("ssf_set_jti", securityEventToken.getId())
+                .detail("ssf_set_txn", securityEventToken.getTxn())
+                .detail("ssf_set_event_type", ssfEvent.getEventType())
+                .detail("ssf_set_issuer", securityEventToken.getIssuer())
+                .detail("ssf_set_event", rawSecurityEvent)
+                .detail("ssf_set_sub_id", rawSubject)
+                .detail("ssf_receiver_alias", eventContext.getReceiver().getConfig().getAlias())
+                .success();
+    }
+
+    protected String extractSecurityEventAsString(SsfEvent ssfEvent) {
+        String rawSecurityEvent;
+        try {
+            rawSecurityEvent = JsonSerialization.writeValueAsString(ssfEvent);
+        } catch (IOException e) {
+            rawSecurityEvent = "Failed to serialize SecurityEventToken";
+        }
+        return rawSecurityEvent;
+    }
+
+    protected String extractRawSubjectAsString(SecurityEventToken securityEventToken) {
+        String rawSubject;
+        try {
+            rawSubject = JsonSerialization.writeValueAsString(securityEventToken.getSubjectId());
+        } catch (IOException e) {
+            rawSubject = "Failed to serialize SubjectId";
+        }
+        return rawSubject;
+    }
+
+    protected boolean isUserEventRecordingEnabled(RealmModel realm, EventType eventType) {
+        return realm.isEventsEnabled() && realm.getEnabledEventTypesStream().anyMatch(type -> eventType.name().equals(type));
     }
 
     /**
