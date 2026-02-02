41293f69d33487fbfab74d64c95c059c6af58b33
Takashi Norimatsu <takashi.norimatsu.ws@hitachi.com>
2026-02-02 14:43:52 +0900

    Changing the meaning of the configurations: Restrict Same Domain
    Signed-off-by: Takashi Norimatsu <takashi.norimatsu.ws@hitachi.com>

--- services/src/main/java/org/keycloak/protocol/oauth2/cimd/clientpolicy/executor/ClientIdMetadataDocumentExecutor.java
+++ services/src/main/java/org/keycloak/protocol/oauth2/cimd/clientpolicy/executor/ClientIdMetadataDocumentExecutor.java
@@ -1,14 +1,10 @@
 package org.keycloak.protocol.oauth2.cimd.clientpolicy.executor;
 
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Stream;
 
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.protocol.oidc.OIDCLoginProtocol;
@@ -67,8 +63,6 @@ public class ClientIdMetadataDocumentExecutor extends AbstractClientIdMetadataDo
         // Client Metadata Validation
         @JsonProperty(ClientIdMetadataDocumentExecutorFactory.ONLY_ALLOW_CONFIDENTIAL_CLIENT)
         protected boolean onlyAllowConfidentialClient = false;
-        @JsonProperty(ClientIdMetadataDocumentExecutorFactory.ALL_URIS_RESTRICT_SAME_DOMAIN)
-        protected boolean allUrisRestrictSameDomain = false;
 
         public Configuration() {
             super();
@@ -81,20 +75,11 @@ public class ClientIdMetadataDocumentExecutor extends AbstractClientIdMetadataDo
         public void setOnlyAllowConfidentialClient(boolean onlyAllowConfidentialClient) {
             this.onlyAllowConfidentialClient = onlyAllowConfidentialClient;
         }
-
-        public boolean isAllUrisRestrictSameDomain() {
-            return allUrisRestrictSameDomain;
-        }
-
-        public void setAllUrisRestrictSameDomain(boolean allUrisRestrictSameDomain) {
-            this.allUrisRestrictSameDomain = allUrisRestrictSameDomain;
-        }
     }
 
     // Client Metadata Validation Errors
     public static final String ERR_METADATA_NO_CONFIDENTIAL_CLIENT = "Invalid Client Metadata: confidential client is only allowed.";
     public static final String ERR_METADATA_NO_CONFIDENTIAL_CLIENT_JWKS = "Invalid Client Metadata: ether jwks or jwks_uri property is required.";
-    public static final String ERR_METADATA_NO_ALL_URIS_SAMEDOMAIN = "Invalid Client Metadata: some uri property is not under the same permitted domain";
 
     @Override
     protected void validateClientMetadata(final URI clientIdURI, final URI redirectUriURI, final OIDCClientRepresentation clientOIDC) throws ClientPolicyException {
@@ -113,25 +98,6 @@ public class ClientIdMetadataDocumentExecutor extends AbstractClientIdMetadataDo
             if (clientOIDC.getJwksUri() != null && clientOIDC.getJwks() != null) {
                 getLogger().warn("confidential client but both jwks and jwks_uri properties are included");
                 throw invalidClientIdMetadata(ERR_METADATA_NO_CONFIDENTIAL_CLIENT_JWKS);
-            }
-        }
-
-        // same domain : client_id, redirect_uri, client_uri, logo_uri, tos_uri,, policy_uri, jwks_uri
-        List<String> trustedDomains = convertContentFilledList(getConfiguration().getTrustedDomains());
-        if (getConfiguration().isAllUrisRestrictSameDomain() && trustedDomains != null && !trustedDomains.isEmpty()) {
-            List<String> l = Stream.of(clientOIDC.getClientId(), clientOIDC.getClientUri(), clientOIDC.getLogoUri(), clientOIDC.getTosUri(), clientOIDC.getPolicyUri(), clientOIDC.getJwksUri())
-                    .filter(Objects::nonNull).toList();
-            try {
-                for (String s : l) {
-                    URI u = new URI(s);
-                    if (trustedDomains.stream().filter(i->!i.isBlank()).noneMatch(i -> checkTrustedDomain(u.getHost(), i) && checkTrustedDomain(redirectUriURI.getHost(), i))) {
-                        getLogger().warnv("not under the same domain = {0}", u.getHost());
-                        throw invalidClientIdMetadata(ERR_METADATA_NO_ALL_URIS_SAMEDOMAIN);
-                    }
-                }
-            } catch (URISyntaxException e) {
-                getLogger().warnv("URI not resolved {0}}", e);
-                throw invalidClientIdMetadata(ERR_METADATA_NO_ALL_URIS_SAMEDOMAIN);
             }
         }
 
