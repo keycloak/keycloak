94956acbe6fe810583c5809d186b03365768298e
Pedro Ruivo <pruivo@users.noreply.github.com>
2026-02-13 14:59:44 +0000

    Incorrect code used return value
    Fixes #46290
    
    Signed-off-by: Pedro Ruivo <1492066+pruivo@users.noreply.github.com>
    Signed-off-by: Alexander Schwartz <alexander.schwartz@ibm.com>
    Co-authored-by: Pedro Ruivo <1492066+pruivo@users.noreply.github.com>
    Co-authored-by: Alexander Schwartz <alexander.schwartz@ibm.com>

--- model/infinispan/src/main/java/org/keycloak/models/sessions/infinispan/remote/RemoteInfinispanSingleUseObjectProvider.java
+++ model/infinispan/src/main/java/org/keycloak/models/sessions/infinispan/remote/RemoteInfinispanSingleUseObjectProvider.java
@@ -63,7 +63,16 @@ public class RemoteInfinispanSingleUseObjectProvider implements SingleUseObjectP
     @Override
     public Map<String, String> remove(String key) {
         try {
-            return unwrap(withReturnValue().remove(key));
+            // Using a get-before-remove allows us to return the value even in cases when a state transfer happens in Infinispan
+            // where it might not return the value in all cases.
+            // This workaround can be removed once https://github.com/infinispan/infinispan/issues/16703 is implemented.
+            var data = transaction.getCache().getWithMetadata(key);
+            if (data == null) {
+                return null;
+            }
+            return transaction.getCache().removeWithVersion(key, data.getVersion()) ?
+                    unwrap(data.getValue()) :
+                    null;
         } catch (HotRodClientException re) {
             // No need to retry. The hotrod (remoteCache) has some retries in itself in case of some random network error happened.
             // In case of lock conflict, we don't want to retry anyway as there was likely an attempt to remove the code from different place.
