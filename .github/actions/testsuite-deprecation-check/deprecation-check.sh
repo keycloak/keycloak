1e3ae122a554ecf2c3032115027c97bbe335e062
Lukas Hanusovsky <lhanusov@redhat.com>
2026-02-15 23:39:31 +0100

    GH actions for ci.yaml workflow - testsuite deprecation check.
    Signed-off-by: Lukas Hanusovsky <lhanusov@redhat.com>

---
+++ .github/actions/testsuite-deprecation-check/deprecation-check.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+REPOSITORY="$1"
+REF="$2"
+
+CHANGE_ID=$(echo $REF | cut -f 3 -d '/')
+
+echo "========================================================================================"
+echo "Checking testsuite module additions/changes."
+echo "----------------------------------------------------------------------------------------"
+
+ADDED_FILES=$(gh api -X GET --paginate repos/$REPOSITORY/pulls/$CHANGE_ID/files --jq 'map(select(.filename | contains("testsuite/")) | select (.status | contains("added")) | {filename}) | length')
+CHANGED_FILES=$(gh api -X GET --paginate repos/$REPOSITORY/pulls/$CHANGE_ID/files --jq 'map(select(.filename | contains("testsuite/")) | select (.additions >= 50) | {filename}) | length')
+
+# Check if changed files matches regex
+if [[ $ADDED_FILES > 0 || $CHANGED_FILES > 0 ]]  ; then
+  echo "========================================================================================"
+  echo "Deprecated testsuite module: "
+  echo " * Adding new file(s) is forbidden."
+  echo " * Maximum 50 lines can be added to a single file."
+  echo ""
+  echo "Please, migrate the added/changed file(s) and use the new test framework instead."
+  echo "See: https://github.com/keycloak/keycloak/tree/main/testsuite/DEPRECATED.md for more details."
+  echo "----------------------------------------------------------------------------------------"
+  exit 1
+fi
