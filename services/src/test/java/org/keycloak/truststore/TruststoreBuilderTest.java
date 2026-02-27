0aba5d7df47a66837829a0ee2b59df3743bedbc0
Sebastian Łaskawiec <sebastian.laskawiec@gmail.com>
2026-02-27 10:00:44 +0100

    lint errors fix
    Signed-off-by: Sebastian Łaskawiec <sebastian.laskawiec@gmail.com>

--- services/src/test/java/org/keycloak/truststore/TruststoreBuilderTest.java
+++ services/src/test/java/org/keycloak/truststore/TruststoreBuilderTest.java
@@ -27,7 +27,6 @@ import java.util.List;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
