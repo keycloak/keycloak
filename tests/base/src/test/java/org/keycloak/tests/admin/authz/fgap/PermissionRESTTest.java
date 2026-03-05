1fb5b4fb3a5941df2b89f43e4a2ab98759b107b3
vramik <vramik@redhat.com>
2026-03-05 11:44:41 +0100

    Privilege escalation via manage-clients permission
    Closes #46719
    
    Signed-off-by: vramik <vramik@redhat.com>

--- tests/base/src/test/java/org/keycloak/tests/admin/authz/fgap/PermissionRESTTest.java
+++ tests/base/src/test/java/org/keycloak/tests/admin/authz/fgap/PermissionRESTTest.java
@@ -17,21 +17,32 @@
 
 package org.keycloak.tests.admin.authz.fgap;
 
+import java.util.List;
 import java.util.Set;
 
 import jakarta.ws.rs.BadRequestException;
 import jakarta.ws.rs.core.Response;
 
+import org.keycloak.admin.client.Keycloak;
 import org.keycloak.authorization.fgap.AdminPermissionsSchema;
+import org.keycloak.models.AdminRoles;
+import org.keycloak.models.Constants;
 import org.keycloak.representations.idm.ClientRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
 import org.keycloak.representations.idm.authorization.DecisionStrategy;
 import org.keycloak.representations.idm.authorization.PolicyEnforcementMode;
 import org.keycloak.representations.idm.authorization.ResourceRepresentation;
 import org.keycloak.representations.idm.authorization.ResourceServerRepresentation;
+import org.keycloak.representations.idm.authorization.ScopePermissionRepresentation;
 import org.keycloak.representations.idm.authorization.ScopeRepresentation;
+import org.keycloak.testframework.admin.AdminClientFactory;
+import org.keycloak.testframework.annotations.InjectAdminClientFactory;
 import org.keycloak.testframework.annotations.InjectUser;
 import org.keycloak.testframework.annotations.KeycloakIntegrationTest;
+import org.keycloak.testframework.realm.ClientConfigBuilder;
 import org.keycloak.testframework.realm.ManagedUser;
+import org.keycloak.testframework.util.ApiUtil;
 
 import org.junit.jupiter.api.Test;
 
@@ -40,6 +51,7 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
 
 @KeycloakIntegrationTest
@@ -47,6 +59,9 @@ public class PermissionRESTTest extends AbstractPermissionTest {
 
     @InjectUser(ref = "alice")
     private ManagedUser userAlice;
+
+    @InjectAdminClientFactory
+    private AdminClientFactory adminClientFactory;
 
     @Test
     public void testPreventDeletingAdminPermissionsClient() {
@@ -194,5 +209,89 @@ public class PermissionRESTTest extends AbstractPermissionTest {
                 .resourceType(AdminPermissionsSchema.USERS.getType())
                 .scopes(Set.of("edit", "write", "token-exchange"))
                 .build(), Response.Status.BAD_REQUEST);
-    }    
+    }
+
+    @Test
+    public void testAdminPermissionsClientAuthorizationAccess() {
+        // Create test user with manage-clients role
+        UserRepresentation manageClientsUser = createUser("client-manager-test", "password");
+        ClientRepresentation realmManagement = realm.admin().clients().findByClientId(Constants.REALM_MANAGEMENT_CLIENT_ID).get(0);
+        RoleRepresentation manageClientsRole = realm.admin().clients().get(realmManagement.getId())
+                .roles().get(AdminRoles.MANAGE_CLIENTS).toRepresentation();
+        realm.admin().users().get(manageClientsUser.getId()).roles().clientLevel(realmManagement.getId())
+                .add(List.of(manageClientsRole));
+
+        // Create test user with manage-realm role
+        UserRepresentation realmAdmin = createUser("realm-admin-test", "password");
+        RoleRepresentation manageRealmRole = realm.admin().clients().get(realmManagement.getId())
+                .roles().get(AdminRoles.MANAGE_REALM).toRepresentation();
+        realm.admin().users().get(realmAdmin.getId()).roles().clientLevel(realmManagement.getId())
+                .add(List.of(manageRealmRole));
+
+        // Create a regular test client with authorization enabled
+        ClientRepresentation regularClient = ClientConfigBuilder.create()
+                .clientId("regular-authz-client")
+                .secret("secret")
+                .serviceAccountsEnabled(true)
+                .authorizationServicesEnabled(true)
+                .build();
+        try (Response response = realm.admin().clients().create(regularClient)) {
+            regularClient.setId(ApiUtil.getCreatedId(response));
+        }
+
+        // Test with user having manage-clients permission
+        try (Keycloak clientManagerKeycloak = adminClientFactory.create()
+                .realm(realm.getName())
+                .username(manageClientsUser.getUsername())
+                .password("password")
+                .clientId(Constants.ADMIN_CLI_CLIENT_ID)
+                .build()) {
+
+            // User with manage-clients CANNOT create permissions on admin-permissions client (manage-authorization)
+            ScopePermissionRepresentation blockedPermission = new ScopePermissionRepresentation();
+            blockedPermission.setName("test-blocked-permission");
+            blockedPermission.setResourceType(AdminPermissionsSchema.USERS_RESOURCE_TYPE);
+            blockedPermission.setScopes(Set.of(AdminPermissionsSchema.VIEW));
+
+            try (Response response = clientManagerKeycloak.realm(realm.getName())
+                    .clients()
+                    .get(client.getId())
+                    .authorization()
+                    .permissions()
+                    .scope()
+                    .create(blockedPermission)) {
+                assertThat(response.getStatus(), equalTo(Response.Status.FORBIDDEN.getStatusCode()));
+            }
+
+            // User with manage-clients CAN access regular client authorization settings
+            ResourceServerRepresentation regularClientSettings = clientManagerKeycloak.realm(realm.getName())
+                    .clients()
+                    .get(regularClient.getId())
+                    .authorization()
+                    .getSettings();
+
+            assertNotNull(regularClientSettings, "User with manage-clients permission SHOULD be able to access regular client authorization");
+        }
+
+        // User with manage-realm CAN create permissions on admin-permissions client
+        try (Keycloak realmAdminKeycloak = adminClientFactory.create().realm(realm.getName())
+                .username(realmAdmin.getUsername()).password("password")
+                .clientId(Constants.ADMIN_CLI_CLIENT_ID).build()) {
+
+            ScopePermissionRepresentation permission = new ScopePermissionRepresentation();
+            permission.setName("test-realm-admin-permission");
+            permission.setResourceType(AdminPermissionsSchema.USERS_RESOURCE_TYPE);
+            permission.setScopes(Set.of(AdminPermissionsSchema.VIEW));
+
+            try (Response response = realmAdminKeycloak.realm(realm.getName())
+                    .clients()
+                    .get(client.getId())
+                    .authorization()
+                    .permissions()
+                    .scope()
+                    .create(permission)) {
+                assertThat(response.getStatus(), equalTo(Response.Status.CREATED.getStatusCode()));
+            }
+        }
+    }
 }
