3af6e04de87f4c7ab182ca5a9d560fbb8d8935bd
Thomas Diesler <tdiesler@proton.me>
2026-03-05 12:16:37 +0100

    [OID4VCI] Add a client policy to allow preauth offers
    Signed-off-by: Thomas Diesler <tdiesler@proton.me>

--- services/src/main/java/org/keycloak/protocol/oid4vc/clientpolicy/CredentialClientPolicies.java
+++ services/src/main/java/org/keycloak/protocol/oid4vc/clientpolicy/CredentialClientPolicies.java
@@ -7,4 +7,10 @@ public interface CredentialClientPolicies {
      */
     PredicateCredentialClientPolicy VC_POLICY_CREDENTIAL_OFFER_REQUIRED = new PredicateCredentialClientPolicy(
             "oid4vci-offer-required", "vc.policy.offer.required", true, false);
+
+    /**
+     * Governs whether Credential Offers with `pre-authorized_code` grant are allowed
+     */
+    PredicateCredentialClientPolicy VC_POLICY_CREDENTIAL_OFFER_PREAUTH_ALLOWED = new PredicateCredentialClientPolicy(
+            "oid4vci-offer-preauth-allowed", "vc.policy.offer.preauth.allowed", true, true);
 }
--- services/src/main/java/org/keycloak/protocol/oid4vc/clientpolicy/CredentialClientPolicyExecutor.java
+++ services/src/main/java/org/keycloak/protocol/oid4vc/clientpolicy/CredentialClientPolicyExecutor.java
@@ -8,6 +8,7 @@ import org.keycloak.models.RealmModel;
 import org.keycloak.models.oid4vci.CredentialScopeModel;
 import org.keycloak.protocol.oid4vc.issuance.credentialoffer.CredentialOfferState;
 import org.keycloak.protocol.oid4vc.model.CredentialScopeRepresentation;
+import org.keycloak.protocol.oid4vc.model.PreAuthorizedCodeGrant;
 import org.keycloak.representations.idm.ClientPolicyExecutorConfigurationRepresentation;
 import org.keycloak.representations.idm.ClientPolicyRepresentation;
 import org.keycloak.services.clientpolicy.ClientPolicyContext;
@@ -17,6 +18,7 @@ import org.keycloak.services.clientpolicy.executor.ClientPolicyExecutorProvider;
 
 import org.jspecify.annotations.Nullable;
 
+import static org.keycloak.protocol.oid4vc.clientpolicy.CredentialClientPolicies.VC_POLICY_CREDENTIAL_OFFER_PREAUTH_ALLOWED;
 import static org.keycloak.protocol.oid4vc.clientpolicy.CredentialClientPolicies.VC_POLICY_CREDENTIAL_OFFER_REQUIRED;
 
 public class CredentialClientPolicyExecutor implements ClientPolicyExecutorProvider<ClientPolicyExecutorConfigurationRepresentation> {
@@ -48,6 +50,36 @@ public class CredentialClientPolicyExecutor implements ClientPolicyExecutorProvi
         CredentialScopeModel credScope = context.getCredentialScopeModel();
         if (credScope == null) {
             throw new ClientPolicyException(OAuthErrorException.INVALID_SCOPE, "No client scope");
+        }
+
+        // Check policy 'oid4vci-offer-preauth-allowed'
+        //
+        if (context.getEvent() == ClientPolicyEvent.CREDENTIAL_OFFER_CREATE) {
+
+            CredentialOfferState offerState = context.getCredentialOfferState();
+            if (offerState == null)
+                throw new ClientPolicyException(OAuthErrorException.UNSUPPORTED_GRANT_TYPE, "No credential offer state");
+
+            PredicateCredentialClientPolicy preAuthPolicy = VC_POLICY_CREDENTIAL_OFFER_PREAUTH_ALLOWED;
+            ClientPolicyRepresentation clientPolicy = findClientPolicyByName(session, preAuthPolicy.getName());
+
+            // Allowed by client when policy is not there or enabled
+            boolean allowedByClient = clientPolicy == null || clientPolicy.isEnabled();
+
+            for (String grantType : offerState.getCredentialsOffer().getGrants().keySet()) {
+                if (grantType.equals(PreAuthorizedCodeGrant.PRE_AUTH_GRANT_TYPE)) {
+
+                    // Allowed by scope when property is undefined or true
+                    // See default value on the policy definition
+                    boolean allowedByScope = preAuthPolicy.validate(new CredentialScopeRepresentation(credScope));
+
+                    if (!allowedByClient || !allowedByScope) {
+                        throw new ClientPolicyException(Errors.NOT_ALLOWED,
+                                "Pre-Authorized code grant rejected by policy " + preAuthPolicy.getName() + " for scope " + credScope.getName());
+                    }
+                }
+            }
+            context.setEvaluatedOnEvent(true);
         }
 
         // Check policy 'oid4vci-offer-required'
