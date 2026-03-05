5caf8ed16562a61c491998c6bf0254dd9316c5db
Martin Bartoš <mabartos@redhat.com>
2026-03-05 17:45:35 +0100

    New client service - DELETE - delete individual client
    Closes #46783
    
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>

--- rest/admin-v2/tests/src/test/java/org/keycloak/tests/admin/client/v2/ClientPoliciesV2Test.java
+++ rest/admin-v2/tests/src/test/java/org/keycloak/tests/admin/client/v2/ClientPoliciesV2Test.java
@@ -41,6 +41,7 @@ import org.keycloak.representations.idm.ClientPolicyRepresentation;
 import org.keycloak.representations.idm.ClientProfileRepresentation;
 import org.keycloak.representations.idm.ClientProfilesRepresentation;
 import org.keycloak.services.PatchTypeNames;
+import org.keycloak.services.client.ClientServiceHelper;
 import org.keycloak.services.clientpolicy.ClientPolicyEvent;
 import org.keycloak.services.clientpolicy.condition.AnyClientConditionFactory;
 import org.keycloak.services.clientpolicy.condition.ClientUpdaterContextConditionFactory;
@@ -514,8 +515,12 @@ public class ClientPoliciesV2Test extends AbstractClientApiV2Test {
         setupAlwaysAppliedTestPolicy();
         cleanupClient(rep.getClientId());
 
-        // for now, the VIEW is also present, but it is not required for delete
-        assertClientPolicyEventIsEmitted(ClientPolicyEvent.VIEW, ClientPolicyEvent.UNREGISTER);
+        if (ClientServiceHelper.isLegacyClientServiceEnabled()) {
+            // for now, the VIEW is also present, but it is not required for delete
+            assertClientPolicyEventIsEmitted(ClientPolicyEvent.VIEW, ClientPolicyEvent.UNREGISTER);
+        } else {
+            assertClientPolicyEventIsEmitted(ClientPolicyEvent.UNREGISTER);
+        }
     }
 
     private void assertClientPolicyEventIsEmitted(ClientPolicyEvent... events) {
