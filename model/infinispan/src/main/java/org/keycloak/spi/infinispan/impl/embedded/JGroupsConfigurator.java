17d18144fe3283ece008e3a1b3c0f8e8efa73884
Ryan Emerson <remerson@ibm.com>
2026-03-06 06:56:11 +0000

    JGroups bind port configuration ignored when --cache-embedded-network-bind-port set
    Closes #46663
    
    Signed-off-by: Ryan Emerson <remerson@ibm.com>

--- model/infinispan/src/main/java/org/keycloak/spi/infinispan/impl/embedded/JGroupsConfigurator.java
+++ model/infinispan/src/main/java/org/keycloak/spi/infinispan/impl/embedded/JGroupsConfigurator.java
@@ -459,8 +459,10 @@ public final class JGroupsConfigurator {
                 return;
             }
             checkPropertyAlreadySet(userConfig, property);
-            if (altProperty != null)
+            if (altProperty != null) {
                 checkPropertyAlreadySet(userConfig, altProperty);
+                System.setProperty(altProperty, userConfig);
+            }
             System.setProperty(property, userConfig);
         }
 
