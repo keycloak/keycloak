b2dbdd38665d8ed0149dcd1d10ecc0094511a33a
Thomas Diesler <tdiesler@ibm.com>
2026-03-09 08:27:32 +0100

    [OID4VCI] Migrate OID4VCCredentialOfferMatrixTest (#46946)
    closes #46971
    
    
    Signed-off-by: Thomas Diesler <tdiesler@ibm.com>

--- tests/utils-shared/src/main/java/org/keycloak/testsuite/util/oauth/LoginUrlBuilder.java
+++ tests/utils-shared/src/main/java/org/keycloak/testsuite/util/oauth/LoginUrlBuilder.java
@@ -1,10 +1,12 @@
 package org.keycloak.testsuite.util.oauth;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.keycloak.OAuth2Constants;
 import org.keycloak.models.Constants;
 import org.keycloak.protocol.oidc.OIDCLoginProtocol;
+import org.keycloak.representations.AuthorizationDetailsJSONRepresentation;
 import org.keycloak.representations.ClaimsRepresentation;
 
 public class LoginUrlBuilder extends AbstractUrlBuilder {
@@ -26,6 +28,16 @@ public class LoginUrlBuilder extends AbstractUrlBuilder {
     public LoginUrlBuilder scope(String... scopes) {
         String joinedScopes = String.join(" ", Arrays.asList(scopes));
         parameter(OAuth2Constants.SCOPE, joinedScopes);
+        return this;
+    }
+
+    public LoginUrlBuilder authorizationDetails(AuthorizationDetailsJSONRepresentation authDetail) {
+        parameter(OAuth2Constants.AUTHORIZATION_DETAILS, authDetail != null ? List.of(authDetail) : List.of());
+        return this;
+    }
+
+    public LoginUrlBuilder authorizationDetails(List<AuthorizationDetailsJSONRepresentation> authDetails) {
+        parameter(OAuth2Constants.AUTHORIZATION_DETAILS, authDetails);
         return this;
     }
 
@@ -103,8 +115,9 @@ public class LoginUrlBuilder extends AbstractUrlBuilder {
         parameter(OIDCLoginProtocol.RESPONSE_MODE_PARAM, client.config().getResponseMode());
         parameter(OAuth2Constants.CLIENT_ID, client.config().getClientId());
         parameter(OAuth2Constants.REDIRECT_URI, client.config().getRedirectUri());
-
-        parameter(OAuth2Constants.SCOPE, client.config().getScope());
+        if (!params.containsKey(OAuth2Constants.SCOPE)) {
+            parameter(OAuth2Constants.SCOPE, client.config().getScope());
+        }
     }
 
     public AuthorizationEndpointResponse doLogin(String username, String password) {
