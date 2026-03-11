6c9e7cb4d358a9645c774b41a5849a2951cc789f
Alexander Schwartz <alexander.schwartz@ibm.com>
2026-03-11 19:31:50 +0100

    First draft test
    Signed-off-by: Alexander Schwartz <alexander.schwartz@ibm.com>

--- test-framework/tests/src/test/java/org/keycloak/testframework/tests/EventsTest.java
+++ test-framework/tests/src/test/java/org/keycloak/testframework/tests/EventsTest.java
@@ -2,6 +2,8 @@ package org.keycloak.testframework.tests;
 
 import org.keycloak.events.EventType;
 import org.keycloak.representations.idm.EventRepresentation;
+import org.keycloak.services.scheduled.ClearExpiredAdminEvents;
+import org.keycloak.services.scheduled.ClearExpiredEvents;
 import org.keycloak.testframework.annotations.InjectEvents;
 import org.keycloak.testframework.annotations.InjectRealm;
 import org.keycloak.testframework.annotations.KeycloakIntegrationTest;
@@ -10,10 +12,14 @@ import org.keycloak.testframework.events.Events;
 import org.keycloak.testframework.oauth.OAuthClient;
 import org.keycloak.testframework.oauth.annotations.InjectOAuthClient;
 import org.keycloak.testframework.realm.ManagedRealm;
+import org.keycloak.testframework.remote.runonserver.InjectRunOnServer;
+import org.keycloak.testframework.remote.runonserver.RunOnServerClient;
 import org.keycloak.testframework.remote.timeoffset.InjectTimeOffSet;
 import org.keycloak.testframework.remote.timeoffset.TimeOffSet;
 
 import org.junit.jupiter.api.Test;
+
+import static org.keycloak.models.jpa.entities.RealmAttributes.ADMIN_EVENTS_EXPIRATION;
 
 @KeycloakIntegrationTest
 public class EventsTest {
@@ -29,6 +35,9 @@ public class EventsTest {
 
     @InjectTimeOffSet
     TimeOffSet timeOffSet;
+
+    @InjectRunOnServer
+    RunOnServerClient runOnServer;
 
     @Test
     public void testFailedLogin() {
@@ -57,6 +66,23 @@ public class EventsTest {
         oAuthClient.doClientCredentialsGrantAccessTokenRequest();
 
         EventAssertion.assertSuccess(events.poll()).type(EventType.CLIENT_LOGIN);
+    }
+
+    @Test
+    public void testExpireEvents() {
+        this.realm.updateWithCleanup(r -> r
+                .eventsEnabled(true)
+                .adminEventsEnabled(true)
+                .eventsExpiration(1)
+                // TODO: Create a new method in the builder
+                .update(r1 -> r1.getAttributes().put(ADMIN_EVENTS_EXPIRATION, "1")));
+        // TODO create a user and an admin event. Ensure that they are in the store
+        timeOffSet.set(10);
+        runOnServer.run(session -> {
+            new ClearExpiredAdminEvents().run(session);
+            new ClearExpiredEvents().run(session);
+        });
+        // TODO: Verify that those events have been removed from the database
     }
 
 }
