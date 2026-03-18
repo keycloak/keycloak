db3d704f357e0a5b71c00936870028c5c4e5098c
stianst <stianst@gmail.com>
2026-03-18 14:32:36 +0100

    Add check for missing test packages in base testsuite and add missing packages
    Signed-off-by: stianst <stianst@gmail.com>

---
+++ tests/base/check-base-suites.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+RESULT=0
+
+for i in $(ls src/test/java/org/keycloak/tests/ | grep -v "suites"); do
+  if (! cat src/test/java/org/keycloak/tests/suites/Base*TestSuite.java | grep $i >/dev/null); then
+    echo "Package '$i' not defined in any base testsuites"
+    RESULT=1
+  fi
+done
+
+exit $RESULT
