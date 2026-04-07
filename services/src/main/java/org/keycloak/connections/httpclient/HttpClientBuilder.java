0af91ce076c1639f21f8652db4f93537878d598d
Martin Bartoš <mabartos@redhat.com>
2026-04-07 08:37:53 +0200

    fix: reusing the apache builder to retain retry config
    closes: #47379
    
    
    (cherry picked from commit 0780d567301719efa51095817af7854551da39c4)
    
    Signed-off-by: Steve Hawkins <shawkins@redhat.com>

--- services/src/main/java/org/keycloak/connections/httpclient/HttpClientBuilder.java
+++ services/src/main/java/org/keycloak/connections/httpclient/HttpClientBuilder.java
@@ -98,6 +98,15 @@ public class HttpClientBuilder {
     protected boolean disableCookies = false;
     protected ProxyMappings proxyMappings;
     protected boolean expectContinueEnabled = false;
+    protected final org.apache.http.impl.client.HttpClientBuilder apacheBuilder;
+
+    public HttpClientBuilder() {
+        this(HttpClients.custom());
+    }
+
+    public HttpClientBuilder(org.apache.http.impl.client.HttpClientBuilder apacheBuilder) {
+        this.apacheBuilder = apacheBuilder;
+    }
 
     /**
      * Socket inactivity timeout
@@ -301,7 +310,7 @@ public class HttpClientBuilder {
     }
 
     protected org.apache.http.impl.client.HttpClientBuilder getApacheHttpClientBuilder() {
-        return HttpClients.custom();
+        return apacheBuilder;
     }
 
     private SSLContext createSslContext(
