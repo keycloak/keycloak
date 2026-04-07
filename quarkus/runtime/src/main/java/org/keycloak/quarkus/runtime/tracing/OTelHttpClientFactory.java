daf9e69a9ffba25c6aaf6ac49f6bd15814cfd5d2
Martin Bartoš <mabartos@redhat.com>
2026-04-07 10:13:54 +0200

    NPE when using HttpClient and enabled tracing (#47687)
    Closes #47379
    
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>

--- quarkus/runtime/src/main/java/org/keycloak/quarkus/runtime/tracing/OTelHttpClientFactory.java
+++ quarkus/runtime/src/main/java/org/keycloak/quarkus/runtime/tracing/OTelHttpClientFactory.java
@@ -32,13 +32,13 @@ import org.keycloak.provider.Provider;
 import org.keycloak.quarkus.runtime.configuration.Configuration;
 import org.keycloak.tracing.TracingProvider;
 
+import io.opentelemetry.instrumentation.apachehttpclient.v4_3.ApacheHttpClientTelemetry;
+
 /**
  * The traced {@link HttpClientFactory} for {@link HttpClientProvider HttpClientProvider's} used by Keycloak for outbound HTTP calls which are traced.
  */
 public class OTelHttpClientFactory extends DefaultHttpClientFactory implements EnvironmentDependentProviderFactory {
     public static final String PROVIDER_ID = "opentelemetry";
-
-    private static OTelHttpClientBuilder BUILDER_SINGLETON;
 
     @Override
     public String getId() {
@@ -46,16 +46,9 @@ public class OTelHttpClientFactory extends DefaultHttpClientFactory implements E
     }
 
     @Override
-    public HttpClientProvider create(KeycloakSession session) {
-        if (BUILDER_SINGLETON == null) {
-            BUILDER_SINGLETON = new OTelHttpClientBuilder((OTelTracingProvider) session.getProvider(TracingProvider.class));
-        }
-        return super.create(session);
-    }
-
-    @Override
-    protected HttpClientBuilder newHttpClientBuilder() {
-        return BUILDER_SINGLETON;
+    protected HttpClientBuilder newHttpClientBuilder(KeycloakSession session) {
+        var provider = (OTelTracingProvider) session.getProvider(TracingProvider.class);
+        return new HttpClientBuilder(ApacheHttpClientTelemetry.builder(provider.getOpenTelemetry()).build().newHttpClientBuilder());
     }
 
     @Override
