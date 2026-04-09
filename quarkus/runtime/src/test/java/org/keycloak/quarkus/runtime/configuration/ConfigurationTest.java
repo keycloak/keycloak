9741481a5137d47efc47e05ba5457a1b6ad0f601
Bapuji Koraganti <bapuk.2008@gmail.com>
2026-04-08 22:41:30 -0400

    fix: add top-level --http-cookie-prefix option with RFC 6265 validation and tests
    Signed-off-by: Bapuji Koraganti <bapuk.2008@gmail.com>

--- quarkus/runtime/src/test/java/org/keycloak/quarkus/runtime/configuration/ConfigurationTest.java
+++ quarkus/runtime/src/test/java/org/keycloak/quarkus/runtime/configuration/ConfigurationTest.java
@@ -1006,4 +1006,24 @@ public class ConfigurationTest extends AbstractConfigurationTest {
         assertTrue(DatabasePropertyMappers.isPostgresConnectTimeoutEnabled());
         assertEquals("PT1M", config.getConfigValue(DatabasePropertyMappers.JDBC_ACQUISITION_TIMEOUT).getValue());
     }
+
+    @Test
+    public void testHttpCookiePrefixMapsToSpiProperty() {
+        ConfigArgsConfigSource.setCliArgs("--http-cookie-prefix=KC26_");
+        SmallRyeConfig config = createConfig();
+        assertEquals("KC26_", config.getConfigValue("kc.spi-cookie--default--cookie-prefix").getValue());
+    }
+
+    @Test
+    public void testHttpCookiePrefixDefaultEmpty() {
+        ConfigArgsConfigSource.setCliArgs("");
+        SmallRyeConfig config = createConfig();
+        assertEquals("", config.getConfigValue("kc.spi-cookie--default--cookie-prefix").getValue());
+    }
+
+    @Test
+    public void testHttpCookiePrefixViaSpiArgument() {
+        ConfigArgsConfigSource.setCliArgs("--spi-cookie-default-cookie-prefix=BLUE_");
+        assertEquals("BLUE_", initConfig("cookie", "default").get("cookie-prefix"));
+    }
 }
