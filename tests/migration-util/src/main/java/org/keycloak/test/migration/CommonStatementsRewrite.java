a516b5ebf72e9e3380701dbd5cadb5e3fd72b095
Lukas Hanusovsky <lhanusov@redhat.com>
2026-04-10 11:48:32 +0200

    Migration Tool - Updating CommonStatements rewrite
    Signed-off-by: Lukas Hanusovsky <lhanusov@redhat.com>

--- tests/migration-util/src/main/java/org/keycloak/test/migration/CommonStatementsRewrite.java
+++ tests/migration-util/src/main/java/org/keycloak/test/migration/CommonStatementsRewrite.java
@@ -4,18 +4,27 @@ import java.util.Map;
 
 public class CommonStatementsRewrite extends TestRewrite {
 
-   private final Map<String, String> STATEMENTS = Map.of(
-           "testRealmResource()", "managedRealm.admin()"
-   );
+    private static final Map<String, String> STATEMENTS = Map.of(
+        "testRealm()", "managedRealm.admin()",
+        "realmsResouce().realm", "managedRealm.admin()"
+    );
+
+    private static final String REGEX_RESOURCE = "realmsResouce\\(\\)\\.realm\\(\"?[a-zA-Z_-]*\"?\\)"; //realmsResouce() is a typo in the AbstractKeycloakTest class
 
     @Override
     public void rewrite() {
-        for (int i = 0; i < content.size(); i++) {
+        int startingLine = findClassDeclaration();
+
+        for (int i = startingLine; i < content.size(); i++) {
             String l = content.get(i);
             for (Map.Entry<String, String> entry : STATEMENTS.entrySet()) {
                 if (l.contains(entry.getKey())) {
-                    replaceLine(i, l.replace(entry.getKey(), entry.getValue()));
-                    info(i, "Statement rewritten: '" + entry.getKey() + "' --> '" + entry.getValue() + "'");
+                    if (!entry.getKey().equals("realmsResouce().realm")) {
+                        replaceLine(i, l.replace(entry.getKey(), entry.getValue()));
+                    } else {
+                        replaceLine(i, l.replaceAll(REGEX_RESOURCE, entry.getValue()));
+                    }
+                    info(i + 1, "Data rewritten: '" + entry.getKey() + "' --> '" + entry.getValue() + "'");
                 }
             }
         }
