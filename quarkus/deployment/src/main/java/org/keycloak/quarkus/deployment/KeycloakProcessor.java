a5ad8e5a14ad5bef48ce5132821eb5d1ed4c107d
Pedro Igor <pigor.craveiro@gmail.com>
2026-04-13 17:06:09 -0300

    Make sure user-defined user profile config is set if provided
    Closes #48008
    
    Signed-off-by: Pedro Igor <pigor.craveiro@gmail.com>

--- quarkus/deployment/src/main/java/org/keycloak/quarkus/deployment/KeycloakProcessor.java
+++ quarkus/deployment/src/main/java/org/keycloak/quarkus/deployment/KeycloakProcessor.java
@@ -105,7 +105,6 @@ import org.keycloak.quarkus.runtime.storage.database.jpa.NamedJpaConnectionProvi
 import org.keycloak.quarkus.runtime.themes.FlatClasspathThemeResourceProviderFactory;
 import org.keycloak.representations.provider.ScriptProviderDescriptor;
 import org.keycloak.representations.provider.ScriptProviderMetadata;
-import org.keycloak.representations.userprofile.config.UPConfig;
 import org.keycloak.services.DefaultKeycloakSessionFactory;
 import org.keycloak.services.ServicesLogger;
 import org.keycloak.services.resources.LoadBalancerResource;
@@ -396,27 +395,6 @@ class KeycloakProcessor {
         // Ignore queued TRACE and DEBUG messages for not initialized log handlers
         InitialConfigurator.DELAYED_HANDLER.setBuildTimeHandlers(new Handler[]{});
         throw new ConfigurationException(msg);
-    }
-
-    /**
-     * Parse the default configuration for the User Profile provider
-     */
-    @BuildStep
-    @Produce(UserProfileBuildItem.class)
-    UserProfileBuildItem parseDefaultUserProfileConfig() {
-        UPConfig defaultConfig = UPConfigUtils.parseSystemDefaultConfig();
-        logger.debug("Parsing default configuration for the User Profile provider");
-        return new UserProfileBuildItem(defaultConfig);
-    }
-
-    /**
-     * Set the default configuration to the User Profile provider
-     */
-    @BuildStep
-    @Consume(ProfileBuildItem.class)
-    @Record(ExecutionTime.STATIC_INIT)
-    void setDefaultUserProfileConfig(KeycloakRecorder recorder, UserProfileBuildItem configuration) {
-        recorder.setDefaultUserProfileConfiguration(configuration.getDefaultConfig());
     }
 
     @BuildStep
@@ -711,6 +689,8 @@ class KeycloakProcessor {
                 configureThemeResourceProviders(factories, spi);
             }
         }
+
+        recorder.setDefaultUserProfileConfiguration(UPConfigUtils.parseSystemDefaultConfig());
 
         return SyntheticBeanBuildItem.configure(QuarkusKeycloakSessionFactory.class).scope(Singleton.class)
                 .unremovable()
