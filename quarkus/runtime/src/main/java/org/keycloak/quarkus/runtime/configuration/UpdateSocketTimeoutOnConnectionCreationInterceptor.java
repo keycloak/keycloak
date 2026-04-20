e90912538e91f1abf40eb49142db4b9e2e7bc138
Alexander Schwartz <alexander.schwartz@ibm.com>
2026-04-20 10:00:04 +0200

    POC for network timeout derived from transaction timeout
    Signed-off-by: Alexander Schwartz <alexander.schwartz@ibm.com>
    Signed-off-by: vsaranchuk <vsaranchuk3@gmail.com>

--- quarkus/runtime/src/main/java/org/keycloak/quarkus/runtime/configuration/UpdateSocketTimeoutOnConnectionCreationInterceptor.java
+++ quarkus/runtime/src/main/java/org/keycloak/quarkus/runtime/configuration/UpdateSocketTimeoutOnConnectionCreationInterceptor.java
@@ -2,10 +2,16 @@ package org.keycloak.quarkus.runtime.configuration;
 
 import io.agroal.api.AgroalPoolInterceptor;
 
+import io.quarkus.runtime.configuration.DurationConverter;
+
 import org.apache.commons.lang3.StringUtils;
+
+import org.keycloak.config.TransactionOptions;
+import org.keycloak.models.utils.KeycloakModelUtils;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -20,10 +26,54 @@ import java.util.concurrent.Executors;
  */
 public class UpdateSocketTimeoutOnConnectionCreationInterceptor extends SocketTimeoutInterceptor implements AgroalPoolInterceptor {
 
-    private final Executor executor;
+    private Executor executor;
+    private volatile boolean supported = true;
+    private volatile Integer networkTimeout;
 
     public UpdateSocketTimeoutOnConnectionCreationInterceptor() {
         this.executor = Executors.newCachedThreadPool();
+    }
+
+    private int getDefaultNetworkTimeout() {
+        if (networkTimeout == null) {
+            synchronized (this) {
+                networkTimeout = (int) DurationConverter.parseDuration(Configuration.getConfigValue(TransactionOptions.TRANSACTION_DEFAULT_TIMEOUT).getValue()).toMillis();
+            }
+        }
+        return networkTimeout;
+    }
+
+    @Override
+    public void onConnectionAcquire(Connection connection) {
+        if (supported) {
+            try {
+                int timeout = KeycloakModelUtils.getTransactionLimit();
+                if (timeout == 0) {
+                    timeout = getDefaultNetworkTimeout();
+                } else {
+                    timeout = timeout * 1000;
+                }
+                connection.setNetworkTimeout(executor, timeout);
+            } catch (SQLFeatureNotSupportedException e) {
+                supported = false;
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        AgroalPoolInterceptor.super.onConnectionAcquire(connection);
+    }
+
+    public void onConnectionReturn(Connection connection) {
+        if (supported) {
+            try {
+                connection.setNetworkTimeout(executor, getDefaultNetworkTimeout());
+            } catch (SQLFeatureNotSupportedException e) {
+                supported = false;
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        AgroalPoolInterceptor.super.onConnectionReturn(connection);
     }
 
     @Override
