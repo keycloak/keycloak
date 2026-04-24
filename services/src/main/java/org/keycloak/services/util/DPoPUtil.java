3587706b44c95a20be1d4421b0e2a4f768d6d370
Thomas Diesler <tdiesler@proton.me>
2026-04-24 07:44:53 +0200

    [OID4VCI-FAPI2] Pass fapi2-security-profile-final-dpop-negative-tests
    closes #48070
    
    Signed-off-by: Thomas Diesler <tdiesler@proton.me>

--- services/src/main/java/org/keycloak/services/util/DPoPUtil.java
+++ services/src/main/java/org/keycloak/services/util/DPoPUtil.java
@@ -104,7 +104,8 @@ public class DPoPUtil {
     }
 
     private static URI normalize(URI uri) {
-        return UriBuilder.fromUri(uri).replaceQuery("").build();
+        URI normalized = UriBuilder.fromUri(uri).fragment(null).replaceQuery("").build();
+        return normalized;
     }
 
     /**
@@ -183,8 +184,11 @@ public class DPoPUtil {
             throw new VerificationException("Invalid or missing type in DPoP header: " + header.getType());
         }
 
-        String algorithm = header.getAlgorithm().name();
+        if (header.getAlgorithm() == null) {
+            throw new VerificationException("Invalid or missing alg in DPoP header: " + header.getAlgorithm());
+        }
 
+        String algorithm = header.getAlgorithm().name();
         if (!getDPoPSupportedAlgorithms(session).contains(algorithm)) {
             throw new VerificationException("Unsupported DPoP algorithm: " + header.getAlgorithm());
         }
@@ -202,8 +206,9 @@ public class DPoPUtil {
             if (key.getPublicKey() == null) {
                 throw new VerificationException("No public key in DPoP header");
             }
-            if (key.getPrivateKey() != null) {
-                throw new VerificationException("Private key is present in DPoP header");
+            // [TODO >>>] JWKSUtils.getKeyWrapper never seems to extract the private key. Should that be changed?
+            if (key.getPrivateKey() != null || jwk.getOtherClaims().containsKey("d")) {
+                throw new VerificationException("Private key parameter 'd' must not be present in DPoP JWK");
             }
         }
 
