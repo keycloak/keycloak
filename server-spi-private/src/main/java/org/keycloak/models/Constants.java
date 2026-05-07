44e25edf89cc8927e6f62d3a401ce3e994dcaac6
Thomas Diesler <tdiesler@proton.me>
2026-05-07 11:57:42 +0200

    [OID4VCI-FAPI2] Pass fapi2-security-profile-final-par-ensure-reused-request-uri-prior-to-auth-completion-succeeds
    closes #48072
    
    Signed-off-by: Thomas Diesler <tdiesler@proton.me>
    
    * Stop consuming/removing PAR request objects when first parsing request_uri at the authorization endpoint
    * Defer PAR removal until authentication/authorization completes.
    * Persist the request_uri in the authentication session so it can be consumed later.
    * Add a new Details.REQUEST_URI constant for reuse as an auth-note key.

--- server-spi-private/src/main/java/org/keycloak/models/Constants.java
+++ server-spi-private/src/main/java/org/keycloak/models/Constants.java
@@ -253,6 +253,9 @@ public final class Constants {
     // Internal note for storing authorization details response in client session context
     public static final String AUTHORIZATION_DETAILS_RESPONSE = "authorization_details_response";
 
+    // Internal note for storing the authorization request uri
+    public static final String AUTHORIZATION_REQUEST_URI = "request_uri";
+
     // This attribute can be used in a realm import definition to signal that default client scopes should be created in addition to the client scopes defined by the realm import definition.
     // When this attribute is omitted or set to false, the default client scopes are not created if at least one other client scope is defined by the realm import definition.
     public static final String CREATE_DEFAULT_CLIENT_SCOPES = "CreateDefaultClientScopes";
