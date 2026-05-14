8e31feb3e35707c015f966974e0431ceaebe92ea
Peter Zaoral <pzaoral@redhat.com>
2026-05-14 16:39:18 +0200

    Remove unused RealmAdminResource from DefaultClientsApi and DefaultAdminApi
    Signed-off-by: Peter Zaoral <pzaoral@redhat.com>

--- rest/admin-v2/services/src/main/java/org/keycloak/rest/admin/api/DefaultAdminApi.java
+++ rest/admin-v2/services/src/main/java/org/keycloak/rest/admin/api/DefaultAdminApi.java
@@ -4,11 +4,8 @@ import org.keycloak.admin.api.AdminApi;
 import org.keycloak.admin.api.client.ClientsApi;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
-import org.keycloak.protocol.oidc.TokenManager;
 import org.keycloak.rest.admin.api.client.DefaultClientsApi;
 import org.keycloak.services.resources.admin.AdminRoot;
-import org.keycloak.services.resources.admin.RealmAdminResource;
-import org.keycloak.services.resources.admin.RealmsAdminResource;
 import org.keycloak.services.resources.admin.fgap.AdminPermissionEvaluator;
 import org.keycloak.services.resources.admin.fgap.AdminPermissions;
 
@@ -17,20 +14,15 @@ public class DefaultAdminApi implements AdminApi {
     private final RealmModel realm;
     private final AdminPermissionEvaluator permissions;
 
-    // v1 resources
-    private final RealmAdminResource realmAdminResource;
-
     public DefaultAdminApi(KeycloakSession session, String realmName) {
         this.session = session;
         var authInfo = AdminRoot.authenticateRealmAdminRequest(session);
         this.permissions = AdminPermissions.evaluator(session, authInfo.getRealm(), authInfo);
         this.realm = session.realms().getRealmByName(realmName);
-        // remove v1 resource once we are not attached to API v1
-        this.realmAdminResource = new RealmsAdminResource(session, authInfo, new TokenManager()).getRealmAdmin(realmName);
     }
 
     @Override
     public ClientsApi clientsV2() {
-        return new DefaultClientsApi(session, realm, permissions, realmAdminResource);
+        return new DefaultClientsApi(session, realm, permissions);
     }
 }
--- rest/admin-v2/services/src/main/java/org/keycloak/rest/admin/api/client/DefaultClientsApi.java
+++ rest/admin-v2/services/src/main/java/org/keycloak/rest/admin/api/client/DefaultClientsApi.java
@@ -19,7 +19,6 @@ import org.keycloak.representations.admin.v2.BaseClientRepresentation;
 import org.keycloak.services.client.ClientService;
 import org.keycloak.services.client.ClientService.ClientProjectionOptions;
 import org.keycloak.services.client.DefaultClientService;
-import org.keycloak.services.resources.admin.RealmAdminResource;
 import org.keycloak.services.resources.admin.fgap.AdminPermissionEvaluator;
 
 public class DefaultClientsApi implements ClientsApi {
@@ -29,18 +28,12 @@ public class DefaultClientsApi implements ClientsApi {
     private final RealmModel realm;
     private final ClientService clientService;
 
-    // v1 resources
-    private final RealmAdminResource realmAdminResource;
-
     public DefaultClientsApi(@Nonnull KeycloakSession session,
                              @Nonnull RealmModel realm,
-                             @Nonnull AdminPermissionEvaluator permissions,
-                             // remove v1 resource once we are not attached to API v1
-                             @Nonnull RealmAdminResource realmAdminResource) {
+                             @Nonnull AdminPermissionEvaluator permissions) {
         this.session = session;
         this.realm = realm;
         this.permissions = permissions;
-        this.realmAdminResource = realmAdminResource;
         this.clientService = new DefaultClientService(session, realm, permissions);
     }
     
