781e26cc5cfdad6de7690ca9d69d3fee47f76cee
Michal Vavřík <43821672+michalvavrik@users.noreply.github.com>
2026-05-19 18:40:08 +0200

    chore(quarkus): avoid transitive kotlinx-metadata-jvm deployment-only dep
    Closes #48952
    
    (cherry picked from commit fe319271c876689a1cd14c23cc0361b669a59519)
    
    Signed-off-by: Michal Vavřík <michal.vavrik@aol.com>

--- tests/base/src/test/java/org/keycloak/tests/oid4vc/OID4VCIssuerWellKnownProviderTest.java
+++ tests/base/src/test/java/org/keycloak/tests/oid4vc/OID4VCIssuerWellKnownProviderTest.java
@@ -95,10 +95,10 @@ import static org.keycloak.protocol.oid4vc.issuance.OID4VCIssuerWellKnownProvide
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.AssertionsKt.assertNotNull;
 
 
 @KeycloakIntegrationTest(config = OID4VCIssuerTestBase.VCTestServerConfig.class)
