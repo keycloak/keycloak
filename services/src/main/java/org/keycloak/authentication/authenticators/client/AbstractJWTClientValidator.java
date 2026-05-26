3e8a1310d92f13371597d51ff65c5c650153abdf
Sebastian Łaskawiec <sebastian.laskawiec@defenseunicorns.com>
2026-05-26 15:14:36 +0200

    Enable configurable `client_id` parameter validation for federated client assertions (#48026)
    Closes #48024
    
    
    Signed-off-by: Sebastian Łaskawiec <sebastian.laskawiec@defenseunicorns.com>

--- services/src/main/java/org/keycloak/authentication/authenticators/client/AbstractJWTClientValidator.java
+++ services/src/main/java/org/keycloak/authentication/authenticators/client/AbstractJWTClientValidator.java
@@ -101,12 +101,6 @@ public abstract class AbstractJWTClientValidator extends AbstractBaseJWTValidato
             return failure("Token sub claim is required");
         }
 
-        String clientIdParam = context.getHttpRequest().getDecodedFormParameters().getFirst(OAuth2Constants.CLIENT_ID);
-        if (clientIdParam != null && !clientIdParam.equals(clientId)) {
-            logger.debug("client_id parameter does not match JWT subject");
-            return failure("client_id parameter does not match sub claim");
-        }
-
         String expectedTokenIssuer = getExpectedTokenIssuer();
         if (expectedTokenIssuer != null && !expectedTokenIssuer.equals(token.getIssuer())) {
             return false;
@@ -116,10 +110,15 @@ public abstract class AbstractJWTClientValidator extends AbstractBaseJWTValidato
 
         if (client == null) {
             return failure(AuthenticationFlowError.CLIENT_NOT_FOUND);
-        } else {
-            context.getEvent().client(client.getClientId());
-            context.setClient(client);
         }
+
+        String clientIdParam = context.getHttpRequest().getDecodedFormParameters().getFirst(OAuth2Constants.CLIENT_ID);
+        if (clientIdParam != null && !clientIdParam.equals(client.getClientId())) {
+            return failure("client_id parameter does not match authenticated client");
+        }
+
+        context.getEvent().client(client.getClientId());
+        context.setClient(client);
 
         if (!client.isEnabled()) {
             return failure(AuthenticationFlowError.CLIENT_DISABLED);
