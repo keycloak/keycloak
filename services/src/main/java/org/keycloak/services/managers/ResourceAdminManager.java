00dd0dd716c4319d3bac3eb2f2ac22d2a94f79fd
Marie Daly <marie.daly1@ibm.com>
2026-06-04 14:47:12 +0100

    Added validation to client_session_host (#49682)
    Closes #47935
    
    
    (cherry picked from commit 4b1c114dc1b64a7f834cfbe28b8139d599cf8b27)
    
    Signed-off-by: Marie Daly <marie.daly1@ibm.com>

--- services/src/main/java/org/keycloak/services/managers/ResourceAdminManager.java
+++ services/src/main/java/org/keycloak/services/managers/ResourceAdminManager.java
@@ -46,12 +46,14 @@ import org.keycloak.models.UserModel;
 import org.keycloak.protocol.LoginProtocol;
 import org.keycloak.protocol.oidc.OIDCAdvancedConfigWrapper;
 import org.keycloak.protocol.oidc.OIDCLoginProtocol;
+import org.keycloak.protocol.oidc.utils.ClientHostUtils;
 import org.keycloak.representations.LogoutToken;
 import org.keycloak.representations.adapters.action.GlobalRequestResult;
 import org.keycloak.representations.adapters.action.LogoutAction;
 import org.keycloak.representations.adapters.action.TestAvailabilityAction;
 import org.keycloak.services.ServicesLogger;
 import org.keycloak.services.util.ResolveRelative;
+import org.keycloak.utils.StringUtil;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
@@ -175,6 +177,28 @@ public class ResourceAdminManager {
         // KEYCLOAK-748)
         if (backchannelLogoutUrl.contains(CLIENT_SESSION_HOST_PROPERTY)) {
             String host = clientSession.getNote(AdapterConstants.CLIENT_SESSION_HOST);
+            if (StringUtil.isNullOrEmpty(host)) {
+                // Host placeholder in backchannel logout URL cannot be resolved. Usually the client did not send
+                // both 'client_session_host' and 'client_session_state' on its token request.
+                String adapterSessionId = clientSession.getNote(AdapterConstants.CLIENT_SESSION_STATE);
+                throw new IllegalStateException(String.format(
+                        "Cannot resolve '%s' for backchannel-logout. " +
+                                "clientId='%s' clientSessionId='%s' client_session_host='%s' client_session_state='%s'",
+                        CLIENT_SESSION_HOST_PROPERTY, resource.getClientId(), clientSession.getId(), host, adapterSessionId));
+            }
+            logger.debugf("Attempting backchannel-logout for client with host from client session. " +
+                            "clientId='%s' clientSessionId='%s' host='%s' backchannelLogoutUrl='%s'",
+                    resource.getClientId(), clientSession.getId(), host, backchannelLogoutUrl);
+
+            if (StringUtil.isNullOrEmpty(host) || !ClientHostUtils.isHostAllowedForClient(host, resource, session)) {
+                // Host placeholder in backchannel logout URL cannot be resolved or is not allowed. Usually the client did not send
+                // both 'client_session_host' and 'client_session_state' on its token request.
+                String adapterSessionId = clientSession.getNote(AdapterConstants.CLIENT_SESSION_STATE);
+                throw new IllegalStateException(String.format(
+                        "Cannot resolve '%s' for backchannel-logout or it was not allowed by the client. " +
+                                "clientId='%s' clientSessionId='%s' client_session_host='%s' client_session_state='%s'",
+                        CLIENT_SESSION_HOST_PROPERTY, resource.getClientId(), clientSession.getId(), host, adapterSessionId));
+            }
             String currentHostMgmtUrl = backchannelLogoutUrl.replace(CLIENT_SESSION_HOST_PROPERTY, host);
             return sendBackChannelLogoutRequestToClientUri(resource, clientSession, currentHostMgmtUrl);
         } else {
