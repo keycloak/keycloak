c0cd926924d320f302746de85e1eb396005c5898
Pedro Igor <pigor.craveiro@gmail.com>
2026-06-09 09:36:57 -0300

    Delay user storage cluster sync event to after completion
    Closes #48666
    
    Signed-off-by: Pedro Igor <pigor.craveiro@gmail.com>
    Signed-off-by: Alexander Schwartz <alexander.schwartz@ibm.com>
    Co-authored-by: Alexander Schwartz <alexander.schwartz@ibm.com>

--- model/storage-private/src/main/java/org/keycloak/storage/UserStorageManager.java
+++ model/storage-private/src/main/java/org/keycloak/storage/UserStorageManager.java
@@ -1080,7 +1080,18 @@ public class UserStorageManager extends AbstractStorageManager<UserStorageProvid
         if (!component.getProviderType().equals(UserStorageProvider.class.getName())) return;
         localStorage().preRemove(realm, component);
         if (getFederatedStorage() != null) getFederatedStorage().preRemove(realm, component);
-        StoreSyncEvent.fire(session, realm, component, true);
+        // enlistAfterCompletion(..) as we need to ensure that the realm is updated with the final settings
+        session.getTransactionManager().enlistAfterCompletion(new AbstractKeycloakTransaction() {
+            @Override
+            protected void commitImpl() {
+                StoreSyncEvent.fire(session, realm, component, true);
+            }
+
+            @Override
+            protected void rollbackImpl() {
+                // NOOP
+            }
+        });
     }
 
     @Override
@@ -1130,7 +1141,18 @@ public class UserStorageManager extends AbstractStorageManager<UserStorageProvid
         UserStorageProviderModel actual= new UserStorageProviderModel(newModel);
 
         if (isSyncSettingsUpdated(previous, actual)) {
-            StoreSyncEvent.fire(session, realm, actual, false);
+            // enlistAfterCompletion(..) as we need to ensure that the realm is updated with the final settings
+            session.getTransactionManager().enlistAfterCompletion(new AbstractKeycloakTransaction() {
+                @Override
+                protected void commitImpl() {
+                    StoreSyncEvent.fire(session, realm, actual, false);
+                }
+
+                @Override
+                protected void rollbackImpl() {
+                    // NOOP
+                }
+            });
         }
     }
 
