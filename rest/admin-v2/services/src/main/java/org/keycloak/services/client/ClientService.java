f48ef0ba44d6b85746e49b5499fd4625afbb54e0
Erik Jan de Wit <erikjan.dewit@gmail.com>
2026-06-15 09:59:18 +0200

    limit must be greater then 0
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- rest/admin-v2/services/src/main/java/org/keycloak/services/client/ClientService.java
+++ rest/admin-v2/services/src/main/java/org/keycloak/services/client/ClientService.java
@@ -56,8 +56,8 @@ public interface ClientService extends Service {
         if (offset != null && offset < 0) {
             throw new ServiceException("offset must be greater than or equal to 0", Response.Status.BAD_REQUEST);
         }
-        if (limit != null && limit < 0) {
-            throw new ServiceException("limit must be greater than or equal to 0", Response.Status.BAD_REQUEST);
+        if (limit != null && limit < 1) {
+            throw new ServiceException("limit must be greater than or equal to 1", Response.Status.BAD_REQUEST);
         }
         int normalizedOffset = offset != null ? offset : 0;
         int normalizedLimit = limit != null ? limit : Constants.DEFAULT_MAX_RESULTS;
