8789ea57243a8303fd0820c50dcf71665bf7f107
mposolda <mposolda@gmail.com>
2026-06-15 10:52:28 +0200

    [OID4VCI] Send credential offer email: admin UI closes #49308
    Signed-off-by: mposolda <mposolda@gmail.com>

---
+++ js/libs/keycloak-admin-client/src/defs/credentialOfferActionConfigRepresentation.ts
@@ -0,0 +1,8 @@
+/**
+ * Represents a configuration of credential offer action
+ * */
+export default interface CredentialOfferActionConfigRepresentation {
+  credential_configuration_id?: string;
+  client_id?: string;
+  pre_authorized?: boolean;
+}
