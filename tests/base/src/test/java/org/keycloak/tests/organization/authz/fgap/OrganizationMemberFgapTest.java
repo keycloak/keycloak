c29a4f0939d7a22512290abf86f15c0e18a82077
Stefan Guilhen <sguilhen@redhat.com>
2026-06-23 12:40:23 -0300

    Adapt OrganizationMemberFgapTest for release/26.6 test framework API
    Signed-off-by: Stefan Guilhen <sguilhen@redhat.com>

--- tests/base/src/test/java/org/keycloak/tests/organization/authz/fgap/OrganizationMemberFgapTest.java
+++ tests/base/src/test/java/org/keycloak/tests/organization/authz/fgap/OrganizationMemberFgapTest.java
@@ -22,23 +22,28 @@ import java.util.Set;
 import jakarta.ws.rs.core.Response;
 
 import org.keycloak.admin.client.Keycloak;
-import org.keycloak.admin.client.resource.ClientResource;
+import org.keycloak.admin.client.resource.ScopePermissionsResource;
+import org.keycloak.models.AdminRoles;
 import org.keycloak.models.Constants;
 import org.keycloak.models.utils.KeycloakModelUtils;
 import org.keycloak.representations.idm.CredentialRepresentation;
 import org.keycloak.representations.idm.OrganizationDomainRepresentation;
 import org.keycloak.representations.idm.OrganizationRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
+import org.keycloak.representations.idm.authorization.AbstractPolicyRepresentation;
+import org.keycloak.representations.idm.authorization.ScopePermissionRepresentation;
 import org.keycloak.representations.idm.authorization.UserPolicyRepresentation;
 import org.keycloak.testframework.annotations.InjectAdminClient;
+import org.keycloak.testframework.annotations.InjectClient;
 import org.keycloak.testframework.annotations.InjectRealm;
 import org.keycloak.testframework.annotations.KeycloakIntegrationTest;
 import org.keycloak.testframework.injection.LifeCycle;
+import org.keycloak.testframework.realm.ManagedClient;
 import org.keycloak.testframework.realm.ManagedRealm;
-import org.keycloak.testframework.realm.UserBuilder;
+import org.keycloak.testframework.realm.RealmConfig;
+import org.keycloak.testframework.realm.RealmConfigBuilder;
+import org.keycloak.testframework.realm.UserConfigBuilder;
 import org.keycloak.testframework.util.ApiUtil;
-import org.keycloak.tests.admin.authz.fgap.PermissionTestUtils;
-import org.keycloak.tests.utils.admin.AdminApiUtil;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -47,6 +52,8 @@ import static org.keycloak.authorization.fgap.AdminPermissionsSchema.MANAGE;
 import static org.keycloak.authorization.fgap.AdminPermissionsSchema.USERS_RESOURCE_TYPE;
 import static org.keycloak.authorization.fgap.AdminPermissionsSchema.VIEW;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -60,17 +67,17 @@ public class OrganizationMemberFgapTest {
     @InjectRealm(config = OrganizationFgapConfig.class, lifecycle = LifeCycle.METHOD)
     ManagedRealm realm;
 
+    @InjectClient(attachTo = Constants.ADMIN_PERMISSIONS_CLIENT_ID)
+    ManagedClient client;
+
     @InjectAdminClient(mode = InjectAdminClient.Mode.MANAGED_REALM, client = "myclient", user = "myadmin")
     Keycloak realmAdminClient;
 
-    private ClientResource clientResource;
     private String orgId;
     private String memberUserId;
 
     @BeforeEach
     public void setup() {
-        clientResource = AdminApiUtil.findClientByClientId(realm.admin(), Constants.ADMIN_PERMISSIONS_CLIENT_ID);
-
         OrganizationRepresentation orgRep = new OrganizationRepresentation();
         orgRep.setName("testorg");
         orgRep.setAlias("testorg");
@@ -82,7 +89,7 @@ public class OrganizationMemberFgapTest {
             orgId = ApiUtil.getCreatedId(response);
         }
 
-        UserRepresentation member = UserBuilder.create()
+        UserRepresentation member = UserConfigBuilder.create()
                 .username("orgmember")
                 .email("orgmember@testorg.org")
                 .password("password")
@@ -99,9 +106,9 @@ public class OrganizationMemberFgapTest {
     @Test
     public void testGetOrganizationMemberById() {
         UserRepresentation myadmin = realm.admin().users().search("myadmin").get(0);
-        UserPolicyRepresentation policy = PermissionTestUtils.createUserPolicy(
-                realm, clientResource, "Allow Admin " + KeycloakModelUtils.generateId(), myadmin.getId());
-        PermissionTestUtils.createAllPermission(clientResource, USERS_RESOURCE_TYPE, policy, Set.of(VIEW));
+        UserPolicyRepresentation policy = createUserPolicy(
+                realm, client, "Allow Admin " + KeycloakModelUtils.generateId(), myadmin.getId());
+        createAllPermission(client, USERS_RESOURCE_TYPE, policy, Set.of(VIEW));
 
         UserRepresentation user = realmAdminClient.realm(realm.getName()).users().get(memberUserId).toRepresentation();
         assertNotNull(user);
@@ -111,9 +118,9 @@ public class OrganizationMemberFgapTest {
     @Test
     public void testResetPasswordOfOrganizationMember() {
         UserRepresentation myadmin = realm.admin().users().search("myadmin").get(0);
-        UserPolicyRepresentation policy = PermissionTestUtils.createUserPolicy(
-                realm, clientResource, "Allow Admin " + KeycloakModelUtils.generateId(), myadmin.getId());
-        PermissionTestUtils.createAllPermission(clientResource, USERS_RESOURCE_TYPE, policy, Set.of(VIEW, MANAGE));
+        UserPolicyRepresentation policy = createUserPolicy(
+                realm, client, "Allow Admin " + KeycloakModelUtils.generateId(), myadmin.getId());
+        createAllPermission(client, USERS_RESOURCE_TYPE, policy, Set.of(VIEW, MANAGE));
 
         CredentialRepresentation credential = new CredentialRepresentation();
         credential.setType(CredentialRepresentation.PASSWORD);
@@ -123,5 +130,57 @@ public class OrganizationMemberFgapTest {
 
         UserRepresentation user = realmAdminClient.realm(realm.getName()).users().get(memberUserId).toRepresentation();
         assertNotNull(user);
+    }
+
+    private UserPolicyRepresentation createUserPolicy(ManagedRealm realm, ManagedClient client, String name, String... userIds) {
+        UserPolicyRepresentation policy = new UserPolicyRepresentation();
+        policy.setName(name);
+        for (String userId : userIds) {
+            policy.addUser(userId);
+        }
+        try (Response response = client.admin().authorization().policies().user().create(policy)) {
+            assertThat(response.getStatus(), equalTo(Response.Status.CREATED.getStatusCode()));
+            realm.cleanup().add(r -> {
+                UserPolicyRepresentation userPolicy = r.clients().get(client.getId()).authorization().policies().user().findByName(name);
+                if (userPolicy != null) {
+                    r.clients().get(client.getId()).authorization().policies().user().findById(userPolicy.getId()).remove();
+                }
+            });
+        }
+        return policy;
+    }
+
+    private ScopePermissionRepresentation createAllPermission(ManagedClient client, String resourceType, AbstractPolicyRepresentation policy, Set<String> scopes) {
+        ScopePermissionRepresentation permission = new ScopePermissionRepresentation();
+        permission.setName(KeycloakModelUtils.generateId());
+        permission.setResourceType(resourceType);
+        permission.setScopes(scopes);
+        permission.addPolicy(policy.getName());
+
+        ScopePermissionsResource scopePermissions = client.admin().authorization().permissions().scope();
+        try (Response response = scopePermissions.create(permission)) {
+            assertThat(response.getStatus(), equalTo(Response.Status.CREATED.getStatusCode()));
+        }
+        return permission;
+    }
+
+    public static class OrganizationFgapConfig implements RealmConfig {
+        @Override
+        public RealmConfigBuilder configure(RealmConfigBuilder realm) {
+            realm.addUser("myadmin")
+                    .name("My", "Admin")
+                    .email("myadmin@localhost")
+                    .emailVerified(true)
+                    .password("password")
+                    .clientRoles(Constants.REALM_MANAGEMENT_CLIENT_ID,
+                            AdminRoles.QUERY_USERS,
+                            AdminRoles.QUERY_GROUPS,
+                            AdminRoles.QUERY_CLIENTS);
+            realm.addClient("myclient")
+                    .secret("mysecret")
+                    .directAccessGrantsEnabled(true);
+            return realm.adminPermissionsEnabled(true)
+                    .organizationsEnabled(true);
+        }
     }
 }
