6f4fd6bb6b4463b7211febd0c9552c5ce198821c
Alexander Schwartz <alexander.schwartz@ibm.com>
2026-06-23 08:14:17 +0200

    Optimize retrieving groups of a user when logging in for disabled user cache
    Closes #50236
    
    Signed-off-by: Alexander Schwartz <alexander.schwartz@ibm.com>

--- model/jpa/src/main/java/org/keycloak/models/jpa/UserAdapter.java
+++ model/jpa/src/main/java/org/keycloak/models/jpa/UserAdapter.java
@@ -75,6 +75,12 @@ public class UserAdapter implements UserModel, JpaModel<UserEntity> {
     protected RealmModel realm;
     private final KeycloakSession session;
 
+    /**
+     * Listing the groups of a user is called frequently within a login session if users are not cached.
+     * Cache if then it is first retrieved during a session and discard it when a group membership changes.
+     */
+    private List<String> allGroups = null;
+
     public UserAdapter(KeycloakSession session, RealmModel realm, EntityManager em, UserEntity user) {
         this.em = em;
         this.user = user;
@@ -418,7 +424,14 @@ public class UserAdapter implements UserModel, JpaModel<UserEntity> {
 
     @Override
     public Stream<GroupModel> getGroupsStream(String search, Integer first, Integer max) {
-        return session.groups().getGroupsStream(realm, closing(createGetGroupsQuery().getResultStream()), search, first, max);
+        if (first == null && max == null) {
+            if (allGroups == null) {
+                allGroups = createGetGroupsQuery().getResultList();
+            }
+            return session.groups().getGroupsStream(realm, allGroups.stream(), search, first, max);
+        } else {
+            return session.groups().getGroupsStream(realm, closing(createGetGroupsQuery().getResultStream()), search, first, max);
+        }
     }
 
     @Override
@@ -443,6 +456,7 @@ public class UserAdapter implements UserModel, JpaModel<UserEntity> {
     @Override
     public void joinGroup(GroupModel group) {
         joinGroup(group, null);
+        allGroups = null;
     }
 
     private boolean hasDirectGroup(GroupModel group) {
@@ -492,6 +506,9 @@ public class UserAdapter implements UserModel, JpaModel<UserEntity> {
             em.remove(entity);
         }
         em.flush();
+        if (allGroups != null) {
+            allGroups.remove(group.getId());
+        }
         GroupMemberLeaveEvent.fire(group, this, session);
     }
 
