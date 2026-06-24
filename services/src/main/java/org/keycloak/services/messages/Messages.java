bde68888ef5d4861159664a9cb3183592a0749db
Awambeng <114798938+Awambeng@users.noreply.github.com>
2026-06-24 19:38:07 +0100

    [OID4VCI] Enforce DID uniqueness and restrict DID updates to admins (#50112)
    Closes #46524
    
    
    Signed-off-by: Awambeng Rodrick <awambengrodrick@gmail.com>

--- services/src/main/java/org/keycloak/services/messages/Messages.java
+++ services/src/main/java/org/keycloak/services/messages/Messages.java
@@ -96,6 +96,8 @@ public class Messages {
     public static final String RECAPTCHA_NOT_CONFIGURED = "recaptchaNotConfigured";
 
     public static final String EMAIL_EXISTS = "emailExistsMessage";
+
+    public static final String DID_EXISTS = "didExistsMessage";
     public static final String EMAIL_VERIFICATION_PENDING = "emailVerificationPending";
 
     public static final String FEDERATED_IDENTITY_EXISTS = "federatedIdentityExistsMessage";
