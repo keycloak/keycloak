6dce91857226b260fa45d52feeab1e1e34a0261e
Erik Jan de Wit <erikjan.dewit@gmail.com>
2026-06-25 12:59:07 +0200

    removed deprecated annotations
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- rest/admin-v2/api/src/main/java/org/keycloak/representations/admin/v2/RepresentationWithUuid.java
+++
@@ -1,9 +0,0 @@
-package org.keycloak.representations.admin.v2;
-
-/**
- * @deprecated Use {@link org.keycloak.representations.admin.v2.validation.ServerManagedFieldUnmodified} instead.
- */
-@Deprecated
-public interface RepresentationWithUuid {
-    String getUuid();
-}
