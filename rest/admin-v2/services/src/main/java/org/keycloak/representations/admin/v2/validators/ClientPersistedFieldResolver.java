5ca351a685c475367aa9b4e7a704453cffcd9749
Erik Jan de Wit <erikjan.dewit@gmail.com>
2026-07-01 10:56:40 +0200

    use client model mapper instead
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- rest/admin-v2/services/src/main/java/org/keycloak/representations/admin/v2/validators/ClientPersistedFieldResolver.java
+++ rest/admin-v2/services/src/main/java/org/keycloak/representations/admin/v2/validators/ClientPersistedFieldResolver.java
@@ -1,16 +1,10 @@
 package org.keycloak.representations.admin.v2.validators;
 
-import java.lang.reflect.Field;
-import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 
-import org.keycloak.common.util.reflections.Reflections;
 import org.keycloak.models.ClientModel;
-import org.keycloak.models.utils.reflection.NamedPropertyCriteria;
-import org.keycloak.models.utils.reflection.Property;
-import org.keycloak.models.utils.reflection.PropertyQueries;
+import org.keycloak.models.mapper.ClientModelMappers;
 import org.keycloak.representations.admin.v2.BaseClientRepresentation;
 import org.keycloak.validation.jakarta.ValidationContext;
 
@@ -19,11 +13,7 @@ import org.keycloak.validation.jakarta.ValidationContext;
  */
 public class ClientPersistedFieldResolver implements PersistedFieldResolver {
 
-    private static final Map<String, String> GETTER_PROPERTY_ALIASES = Map.of(
-            "uuid", "id",
-            "updatedTimestamp", "lastModifiedTimestamp",
-            "displayName", "name",
-            "appUrl", "baseUrl");
+    private static final ClientModelMappers MAPPERS = new ClientModelMappers();
 
     @Override
     public boolean supports(Class<?> representationType) {
@@ -43,7 +33,8 @@ public class ClientPersistedFieldResolver implements PersistedFieldResolver {
         if (persistedClient == null) {
             return null;
         }
-        return fieldValue(fieldName, persistedClient);
+        var converted = MAPPERS.getMapper(client.getProtocol()).get().fromModel(persistedClient);
+        return fieldValue(fieldName, converted);
     }
 
     @Override
@@ -54,30 +45,8 @@ public class ClientPersistedFieldResolver implements PersistedFieldResolver {
         return false;
     }
 
-    private String fieldValue(String fieldName, Object target) throws AssertionError {
-        for (String propertyName : getterPropertyNames(fieldName)) {
-            Property<Object> property = PropertyQueries.createQuery(target.getClass())
-                    .addCriteria(new NamedPropertyCriteria(propertyName))
-                    .getFirstResult();
-            if (property != null) {
-                return toStringValue(property.getValue(target));
-            }
-        }
-
-        Field field = Reflections.findDeclaredField(target.getClass(), fieldName);
-        if (field != null) {
-            return toStringValue(Reflections.getFieldValue(field, target));
-        }
-
-        throw new AssertionError("Unsupported field:" + fieldName);
-    }
-
-    private static List<String> getterPropertyNames(String fieldName) {
-        String alias = GETTER_PROPERTY_ALIASES.get(fieldName);
-        if (alias == null) {
-            return List.of(fieldName);
-        }
-        return List.of(fieldName, alias);
+    private String fieldValue(String fieldName, BaseClientRepresentation target) throws AssertionError {
+        return toStringValue(MAPPERS.resolveFieldValue(fieldName, target));
     }
 
     private static String toStringValue(Object value) {
