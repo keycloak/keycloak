edcc05a5ee6516972296cec1da9ab64e8bfc5ceb
Erik Jan de Wit <erikjan.dewit@gmail.com>
2026-07-02 12:35:33 +0200

    removed dead code
    fixes: #50560
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- rest/admin-v2/api/src/main/java/org/keycloak/admin/api/SortOrder.java
+++ rest/admin-v2/api/src/main/java/org/keycloak/admin/api/SortOrder.java
@@ -1,25 +1,10 @@
 package org.keycloak.admin.api;
 
-import jakarta.ws.rs.WebApplicationException;
-import jakarta.ws.rs.core.Response;
-
 public enum SortOrder {
     ASC,
     DESC;
 
     public boolean isAscending() {
         return this == ASC;
-    }
-
-    public static SortOrder fromString(String value) {
-        if (value == null || value.isBlank()) {
-            return ASC;
-        }
-        for (SortOrder order : values()) {
-            if (order.name().equalsIgnoreCase(value)) {
-                return order;
-            }
-        }
-        throw new WebApplicationException("sort direction must be asc or desc", Response.Status.BAD_REQUEST);
     }
 }
