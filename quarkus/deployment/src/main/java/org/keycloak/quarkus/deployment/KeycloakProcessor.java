281dda21da99afd9717dc62433dad33676d5787b
Alexander Schwartz <alexander.schwartz@ibm.com>
2026-07-02 10:38:36 +0200

    Adding lb-check endpoint for cachless in multi-cluster v2
    Closes #50413
    
    Signed-off-by: Alexander Schwartz <alexander.schwartz@ibm.com>

--- quarkus/deployment/src/main/java/org/keycloak/quarkus/deployment/KeycloakProcessor.java
+++ quarkus/deployment/src/main/java/org/keycloak/quarkus/deployment/KeycloakProcessor.java
@@ -880,7 +880,7 @@ class KeycloakProcessor {
                     AdminRoot.class.getName())), false));
         }
 
-        if (!MultiSiteUtils.isMultiSiteEnabled()) {
+        if (!MultiSiteUtils.isMultiSiteEnabled() && !Profile.isFeatureEnabled(Profile.Feature.CACHELESS)) {
             buildTimeConditionBuildItemBuildProducer.produce(new BuildTimeConditionBuildItem(index.getIndex().getClassByName(DotName.createSimple(
                     LoadBalancerResource.class.getName())), false));
         }
