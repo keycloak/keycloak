1ecbb5d02f3cbdf41b9217d5c176509e892e83f3
Ruchika Jha <Ruchika.Jha1@ibm.com>
2026-07-13 17:38:58 +0100

    Imp for counting the successful and non successful LDAP requests in metrics
    Closes #50343
    Closes #50722
    
    Signed-off-by: Ruchika <ruchika.jha1@ibm.com>
    Signed-off-by: Alexander Schwartz <alexander.schwartz@ibm.com>
    Co-authored-by: Alexander Schwartz <alexander.schwartz@ibm.com>

--- federation/ldap/src/main/java/org/keycloak/storage/ldap/idm/store/ldap/LDAPIdentityStore.java
+++ federation/ldap/src/main/java/org/keycloak/storage/ldap/idm/store/ldap/LDAPIdentityStore.java
@@ -63,6 +63,8 @@ import org.keycloak.storage.ldap.idm.query.internal.LDAPQueryConditionsBuilder;
 import org.keycloak.storage.ldap.idm.store.IdentityStore;
 import org.keycloak.storage.ldap.mappers.LDAPOperationDecorator;
 
+import io.micrometer.core.instrument.Meter;
+import io.micrometer.core.instrument.Timer;
 import org.jboss.logging.Logger;
 
 /**
@@ -81,8 +83,12 @@ public class LDAPIdentityStore implements IdentityStore {
     private final LDAPOperationManager operationManager;
 
     public LDAPIdentityStore(KeycloakSession session, LDAPConfig config) {
+        this(session, config, null);
+    }
+
+    public LDAPIdentityStore(KeycloakSession session, LDAPConfig config, Meter.MeterProvider<Timer> requestTimer) {
         this.config = config;
-        this.operationManager = new LDAPOperationManager(session, config);
+        this.operationManager = new LDAPOperationManager(session, config, requestTimer);
     }
 
     @Override
