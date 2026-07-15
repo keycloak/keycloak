e45f273577b1a6c83bb9cdc6b17ffbb220ff8baa
Ruchika <Ruchika.Jha1@ibm.com>
2026-07-15 12:34:01 +0100

    Implementation for the ldap connect server metric.
    Closes #50840
    
    Signed-off-by: Ruchika <ruchika.jha1@ibm.com>

--- federation/ldap/src/main/java/org/keycloak/storage/ldap/idm/store/ldap/LDAPOperationManager.java
+++ federation/ldap/src/main/java/org/keycloak/storage/ldap/idm/store/ldap/LDAPOperationManager.java
@@ -774,6 +774,14 @@ public class LDAPOperationManager {
 
     private <R> R execute(LdapOperation<R> operation, LDAPOperationDecorator decorator) throws NamingException {
         try (LDAPContextManager ldapContextManager = LDAPContextManager.create(session, config)) {
+            long connectStartNanos = System.nanoTime();
+            boolean connectSuccess = false;
+            try {
+                ldapContextManager.getLdapContext();
+                connectSuccess = true;
+            } finally {
+                recordLdapRequest("connect", connectSuccess, connectStartNanos);
+            }
             return execute(operation, ldapContextManager.getLdapContext(), decorator);
         }
     }
