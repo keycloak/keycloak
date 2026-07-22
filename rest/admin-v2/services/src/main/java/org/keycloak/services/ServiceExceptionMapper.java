a2b43d9e214fa38338be352ffbd1904bfd592af1
Erik Jan de Wit <erikjan.dewit@gmail.com>
2026-07-22 13:51:42 +0200

    added sort to jpa
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- rest/admin-v2/services/src/main/java/org/keycloak/services/ServiceExceptionMapper.java
+++ rest/admin-v2/services/src/main/java/org/keycloak/services/ServiceExceptionMapper.java
@@ -21,14 +21,6 @@ public class ServiceExceptionMapper implements ExceptionMapper<ServiceException>
 
     @Override
     public Response toResponse(ServiceException exception) {
-        // #region agent log
-        try (var w = new java.io.FileWriter("/home/edewit/workspace/keycloak/keycloak/.cursor/debug-b669cc.log", true)) {
-            w.write("{\"sessionId\":\"b669cc\",\"hypothesisId\":\"A,E\",\"location\":\"ServiceExceptionMapper.toResponse\",\"message\":\"service exception\",\"data\":{\"error\":\""
-                    + String.valueOf(exception.getMessage()).replace("\"", "\\\"")
-                    + "\"},\"timestamp\":" + System.currentTimeMillis() + "}\n");
-        } catch (Exception ignored) {
-        }
-        // #endregion
         return KeycloakErrorHandler.getResponse(session, exception.toWebApplicationException());
     }
 }
