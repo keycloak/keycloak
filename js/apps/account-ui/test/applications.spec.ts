a6d7aa98c5956647ed0b68e410da03e8642f4deb
Himanshi Gupta <higupta@higupta-thinkpadt14gen5.rmtin.csb>
2026-07-28 19:59:22 +0530

    fix for issue 50948
    Signed-off-by: Himanshi Gupta <higupta@higupta-thinkpadt14gen5.rmtin.csb>

--- js/apps/account-ui/test/applications.spec.ts
+++ js/apps/account-ui/test/applications.spec.ts
@@ -19,39 +19,38 @@ test.describe("Applications", () => {
     );
   });
 
-test("sorts applications alphabetically", async ({ page }) => {
-    await using testBed = await createTestBed();
+  test("sorts applications alphabetically", async ({ page }) => {
+    await using testBed = await createTestBed();
 
-    await page.route("**/applications", async (route) => {
-      await route.fulfill({
-        status: 200,
-        contentType: "application/json",
-        body: JSON.stringify([
-          {
-            clientId: "zebra-client",
-            clientName: "Zebra Application",
-          },
-          {
-            clientId: "alpha-client",
-            clientName: "Alpha Application",
-          },
-          {
-            clientId: "beta-client",
-            clientName: "Beta Application",
-          },
-        ]),
-      });
-    });
+    await page.route("**/applications", async (route) => {
+      await route.fulfill({
+        status: 200,
+        contentType: "application/json",
+        body: JSON.stringify([
+          {
+            clientId: "zebra-client",
+            clientName: "Zebra Application",
+          },
+          {
+            clientId: "alpha-client",
+            clientName: "Alpha Application",
+          },
+          {
+            clientId: "beta-client",
+            clientName: "Beta Application",
+          },
+        ]),
+      });
+    });
 
-    await login(page, testBed.realm);
-    await page.getByTestId("applications").click();
+    await login(page, testBed.realm);
+    await page.getByTestId("applications").click();
 
-    const applications = page.getByTestId("applications-list-item");
+    const applications = page.getByTestId("applications-list-item");
 
-    await expect(applications).toHaveCount(3);
-    await expect(applications.nth(0)).toContainText("Alpha Application");
-    await expect(applications.nth(1)).toContainText("Beta Application");
-    await expect(applications.nth(2)).toContainText("Zebra Application");
-  });
+    await expect(applications).toHaveCount(3);
+    await expect(applications.nth(0)).toContainText("Alpha Application");
+    await expect(applications.nth(1)).toContainText("Beta Application");
+    await expect(applications.nth(2)).toContainText("Zebra Application");
+  });
 });
-
