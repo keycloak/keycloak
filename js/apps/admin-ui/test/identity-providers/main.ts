559cb94f7be06e218b8397be99981c403e1ec38c
Erik Jan de Wit <erikjan.dewit@gmail.com>
2026-07-30 11:47:28 +0200

    fix test
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- js/apps/admin-ui/test/identity-providers/main.ts
+++ js/apps/admin-ui/test/identity-providers/main.ts
@@ -93,7 +93,12 @@ export async function createDefaultTrustProvider(
   alias: string,
   jwksUrl: string,
 ) {
-  await clickProviderCard(page, "default-trust");
+  const realm =
+    (await page.getByTestId("currentRealm").textContent()) ?? "master";
+
+  await page.goto(
+    `${SERVER_URL}/admin/master/console/${realm}/identity-providers/default-trust/add`,
+  );
 
   await page.getByTestId("alias").fill(alias);
   await expect(page.getByTestId("config.useJwksUrl")).toBeChecked();
