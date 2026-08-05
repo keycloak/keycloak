ea976b432cbf8021f733547ab9e2181a35a32fc6
Alexander Schwartz <alexander.schwartz@gmx.net>
2026-08-05 15:34:27 +0900

    Moved test class to other user storage classes
    Signed-off-by: Alexander Schwartz <alexander.schwartz@gmx.net>

--- tests/base/src/test/java/org/keycloak/tests/admin/userstorage/ServiceAccountUserStorageTest.java
+++ tests/base/src/test/java/org/keycloak/tests/admin/userstorage/ServiceAccountUserStorageTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.keycloak.tests.federation.storage;
+package org.keycloak.tests.admin.userstorage;
 
 import jakarta.ws.rs.BadRequestException;
 import jakarta.ws.rs.core.Response;
