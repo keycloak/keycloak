1ead0594c73433d6d57b537e266c4cadabb581b8
Martin Bartoš <mabartos@redhat.com>
2026-08-06 10:28:40 +0200

    [26.6] Rotated client secret remains valid when the feature is disabled (#51303)
    * Rotated client secret remains valid when the feature is disabled (#50910)
    
    * Rotated client secret remains valid when the feature is disabled
    
    Closes #50855
    
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>
    
    * Simplify tests
    
    Co-authored-by: Ricardo Martin <rmartinc@redhat.com>
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>
    
    ---------
    
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>
    Co-authored-by: Ricardo Martin <rmartinc@redhat.com>
    (cherry picked from commit 12a37e4fc65acceb82177f3179a3ff4010ce6fab)
    
    * Initialize Profiles for ClientAuthPostMethodTest (#51092)
    
    Closes #51065
    Closes #51067
    
    Signed-off-by: rmartinc <rmartinc@redhat.com>
    
    ---------
    
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>
    Signed-off-by: rmartinc <rmartinc@redhat.com>
    Co-authored-by: Ricardo Martin <rmartinc@redhat.com>

--- services/src/main/java/org/keycloak/protocol/oidc/OIDCClientSecretConfigWrapper.java
+++ services/src/main/java/org/keycloak/protocol/oidc/OIDCClientSecretConfigWrapper.java
@@ -7,6 +7,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
+import org.keycloak.common.Profile;
+import org.keycloak.common.Profile.Feature;
 import org.keycloak.common.util.Time;
 import org.keycloak.models.ClientModel;
 import org.keycloak.models.ClientSecretConstants;
@@ -30,9 +32,11 @@ import static org.keycloak.models.ClientSecretConstants.CLIENT_SECRET_REMAINING_
  * @author <a href="mailto:masales@redhat.com">Marcelo Sales</a>
  */
 public class OIDCClientSecretConfigWrapper extends AbstractClientConfigWrapper {
+    private final boolean isRotationFeatureEnabled;
 
     private OIDCClientSecretConfigWrapper(ClientModel client, ClientRepresentation clientRep) {
         super(client, clientRep);
+        this.isRotationFeatureEnabled = Profile.isFeatureEnabled(Feature.CLIENT_SECRET_ROTATION);
     }
 
     public static OIDCClientSecretConfigWrapper fromClientModel(ClientModel client) {
@@ -82,7 +86,7 @@ public class OIDCClientSecretConfigWrapper extends AbstractClientConfigWrapper {
     }
 
     public void removeClientSecretRotated() {
-        if (hasRotatedSecret()) {
+        if (hasRotatedSecretAttributes()) {
             setAttribute(CLIENT_ROTATED_SECRET, null);
             setAttribute(CLIENT_ROTATED_SECRET_CREATION_TIME, null);
             setAttribute(CLIENT_ROTATED_SECRET_EXPIRATION_TIME, null);
@@ -99,7 +103,12 @@ public class OIDCClientSecretConfigWrapper extends AbstractClientConfigWrapper {
     }
 
     public boolean hasRotatedSecret() {
-        return StringUtil.isNotBlank(getAttribute(CLIENT_ROTATED_SECRET)) && StringUtil.isNotBlank(getAttribute(CLIENT_ROTATED_SECRET_CREATION_TIME));
+        return isRotationFeatureEnabled && hasRotatedSecretAttributes();
+    }
+
+    private boolean hasRotatedSecretAttributes() {
+        return StringUtil.isNotBlank(getAttribute(CLIENT_ROTATED_SECRET))
+                && StringUtil.isNotBlank(getAttribute(CLIENT_ROTATED_SECRET_CREATION_TIME));
     }
 
     /**
