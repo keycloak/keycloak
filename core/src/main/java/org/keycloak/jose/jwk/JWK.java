b78e2472a97017b9818593fa216dce05531b4709
Awambeng Rodrick <awambengrodrick@gmail.com>
2026-08-11 16:40:32 +0100

    Address review comments from @abstractj
    Signed-off-by: Awambeng Rodrick <awambengrodrick@gmail.com>

--- core/src/main/java/org/keycloak/jose/jwk/JWK.java
+++ core/src/main/java/org/keycloak/jose/jwk/JWK.java
@@ -183,14 +183,7 @@ public class JWK {
     @JsonIgnore
     public <T> T getOtherClaim(String claimName, Class<T> claimType) {
         Object o = getOtherClaims().get(claimName);
-        if (o == null) {
-            return null;
-        }
-        try {
-            return claimType.cast(o);
-        } catch (ClassCastException e) {
-            return null;
-        }
+        return claimType.isInstance(o) ? claimType.cast(o) : null;
     }
 
 }
