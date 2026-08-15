aee6974bb11c06bd47cd7d4f80cfa02e61163d6d
Bernhard Strähle <bernhard.straehle@gmail.com>
2026-08-15 10:14:24 +0000

    Allow realm config interceptors to target a single realm
    Previously, a RealmConfigInterceptor applied to every realm created within a test class, and it couldn't distinguish which realm it is decorating.
    
    AbstractInterceptorHelper contains now an applies(..) filter that defaults to true, and Interception exposes the interceptor annotation, which is available whether or not the interceptor has been deployed. An interception that does not apply is skipped before it is applied, logged, or registered as a dependent of the intercepted instance. RealmConfigInterceptor exposes this as appliesTo(..).
    
    Existing interceptors inherit the default and keep applying to every realm.
    
    Signed-off-by: Bernhard Strähle <bernhard.straehle@gmail.com>

--- test-framework/core/src/main/java/org/keycloak/testframework/realm/RealmConfigInterceptor.java
+++ test-framework/core/src/main/java/org/keycloak/testframework/realm/RealmConfigInterceptor.java
@@ -8,4 +8,17 @@ public interface RealmConfigInterceptor<T, S extends Annotation> {
 
     RealmBuilder intercept(RealmBuilder realm, InstanceContext<T, S> instanceContext);
 
+    /**
+     * Controls which realms this interceptor applies to. By default, an interceptor applies to every realm created
+     * within the test class. Override to only intercept a specific realm, for example the realm referenced by the
+     * <code>realmRef</code> of the interceptor's own annotation.
+     *
+     * @param annotation the annotation of the interceptor itself
+     * @param realmInstanceContext the instance context of the realm being created
+     * @return <code>true</code> if the realm should be intercepted
+     */
+    default boolean appliesTo(S annotation, InstanceContext<?, ?> realmInstanceContext) {
+        return true;
+    }
+
 }
