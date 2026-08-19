61f55fc3d7e47692284ff51eaa9717524d1c6a9c
Erik Jan de Wit <erikjan.dewit@gmail.com>
2026-08-19 15:11:42 +0200

    tests: fix spotless failure in account REST migration
    Remove an unused assertNull import from AccountRestServiceReadOnlyAttributesTest so Spotless passes and Keycloak CI can run the test matrix.
    
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- tests/base/src/test/java/org/keycloak/tests/account/AccountRestServiceReadOnlyAttributesTest.java
+++ tests/base/src/test/java/org/keycloak/tests/account/AccountRestServiceReadOnlyAttributesTest.java
@@ -52,7 +52,6 @@ import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.not;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
