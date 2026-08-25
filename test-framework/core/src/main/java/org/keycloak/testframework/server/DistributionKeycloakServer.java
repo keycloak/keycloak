de9e422918030abbb72047f5ecb3912d5479b3d7
Peter Zaoral <pzaoral@redhat.com>
2026-08-25 11:21:31 +0200

    Fix DistributionKeycloakServer hanging on Windows when server startup is slow
    Closes #51790
    
    Signed-off-by: Peter Zaoral <pzaoral@redhat.com>

--- test-framework/core/src/main/java/org/keycloak/testframework/server/DistributionKeycloakServer.java
+++ test-framework/core/src/main/java/org/keycloak/testframework/server/DistributionKeycloakServer.java
@@ -50,6 +50,7 @@ public class DistributionKeycloakServer implements KeycloakServer {
     private final boolean reuse;
     private final long startTimeout;
     private final StringBuilder errorOutput = new StringBuilder();
+    private Thread stderrDrainer;
     private boolean tlsEnabled = false;
 
     public DistributionKeycloakServer(boolean debug, boolean reuse, long startTimeout) {
@@ -102,6 +103,8 @@ public class DistributionKeycloakServer implements KeycloakServer {
             } catch (Exception e) {
                 ProcessUtils.killRunningProcess(keycloakProcess);
                 throw e;
+            } finally {
+                stderrDrainer.interrupt();
             }
 
             if (!Environment.isWindows()) {
@@ -163,12 +166,22 @@ public class DistributionKeycloakServer implements KeycloakServer {
             keycloakProcess = pb.start();
 
             errorOutput.setLength(0);
-            Thread stderrDrainer = new Thread(() -> {
-                try (BufferedReader br = new BufferedReader(
-                        new InputStreamReader(keycloakProcess.getErrorStream(), StandardCharsets.UTF_8))) {
-                    for (String line; (line = br.readLine()) != null; ) {
-                        errorOutput.append(line).append(System.lineSeparator());
+            // Uses ready()+read() polling instead of blocking readLine() because
+            // blocking readBytes() on process pipes is not interruptible via Thread.interrupt()
+            stderrDrainer = new Thread(() -> {
+                try (InputStreamReader isr = new InputStreamReader(keycloakProcess.getErrorStream(), StandardCharsets.UTF_8)) {
+                    char[] buf = new char[1024];
+                    while (!Thread.currentThread().isInterrupted()) {
+                        if (isr.ready()) {
+                            int n = isr.read(buf);
+                            if (n < 0) break;
+                            errorOutput.append(buf, 0, n);
+                        } else {
+                            Thread.sleep(100);
+                        }
                     }
+                } catch (InterruptedException e) {
+                    // Interrupted after startup
                 } catch (IOException e) {
                     // Ignored
                 }
@@ -299,6 +312,10 @@ public class DistributionKeycloakServer implements KeycloakServer {
         }
     }
 
+    private String getErrorOutput() {
+        return errorOutput.toString();
+    }
+
     private File getZipLastModifiedFile(File dir) {
         return new File(dir, "zip-last-modified");
     }
@@ -323,10 +340,6 @@ public class DistributionKeycloakServer implements KeycloakServer {
         ServerInfoRepresentation info = kc.serverInfo().getInfo();
         kc.close();
         return info;
-    }
-
-    private String getErrorOutput() {
-        return errorOutput.toString();
     }
 
     private static File resolveKeycloakDist() {
