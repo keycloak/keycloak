47794e6f1f25b2665124216233fa92ecb6acb22f
Giuseppe Graziano <g.graziano94@gmail.com>
2026-08-26 11:58:45 +0200

    Fix signed-JWT client authentication policy bypass with forged assertion
    Closes #50997
    
    Signed-off-by: Giuseppe Graziano <g.graziano94@gmail.com>
    (cherry picked from commit 0a48bb76e84f1fb910b12a5fa1abddd0a5602b97)

--- services/src/main/java/org/keycloak/authentication/authenticators/client/FederatedJWTClientAuthenticator.java
+++ services/src/main/java/org/keycloak/authentication/authenticators/client/FederatedJWTClientAuthenticator.java
@@ -62,6 +62,11 @@ public class FederatedJWTClientAuthenticator extends AbstractClientAuthenticator
     }
 
     @Override
+    public boolean supportsClientAssertion() {
+        return true;
+    }
+
+    @Override
     public void postInit(KeycloakSessionFactory factory) {
         factory.getProviderFactoriesStream(IdentityProvider.class)
                 .filter(ClientAssertionIdentityProviderFactory.class::isInstance)
