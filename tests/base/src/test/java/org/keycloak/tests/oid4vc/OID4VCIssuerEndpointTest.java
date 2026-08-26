48f8661ab6dd3fb387689089726e2694a4de48c3
mposolda <mposolda@gmail.com>
2026-08-26 10:44:07 +0200

    [CVE-2026-16089] Authorization codes can be retargeted to another client session closes #51003
    Signed-off-by: mposolda <mposolda@gmail.com>
    (cherry picked from commit 6eb0a6c4621267387b002056f08900531113f353)

--- tests/base/src/test/java/org/keycloak/tests/oid4vc/OID4VCIssuerEndpointTest.java
+++ tests/base/src/test/java/org/keycloak/tests/oid4vc/OID4VCIssuerEndpointTest.java
@@ -40,15 +40,11 @@ import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.resource.ComponentsResource;
 import org.keycloak.common.VerificationException;
 import org.keycloak.common.util.MultivaluedHashMap;
-import org.keycloak.common.util.SecretGenerator;
-import org.keycloak.common.util.Time;
 import org.keycloak.constants.OID4VCIConstants;
 import org.keycloak.jose.jws.JWSInput;
-import org.keycloak.models.AuthenticatedClientSessionModel;
 import org.keycloak.models.ClientScopeModel;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.UserModel;
-import org.keycloak.models.UserSessionModel;
 import org.keycloak.models.oid4vci.CredentialScopeModel;
 import org.keycloak.protocol.oid4vc.issuance.OID4VCIssuerEndpoint;
 import org.keycloak.protocol.oid4vc.issuance.TimeProvider;
@@ -63,7 +59,6 @@ import org.keycloak.protocol.oid4vc.model.OID4VCAuthorizationDetail;
 import org.keycloak.protocol.oid4vc.model.VerifiableCredential;
 import org.keycloak.protocol.oid4vc.userprofile.DuplicateDidValidator;
 import org.keycloak.protocol.oidc.utils.OAuth2Code;
-import org.keycloak.protocol.oidc.utils.OAuth2CodeParser;
 import org.keycloak.representations.JsonWebToken;
 import org.keycloak.representations.idm.ClientScopeRepresentation;
 import org.keycloak.representations.idm.ComponentRepresentation;
@@ -72,7 +67,6 @@ import org.keycloak.representations.userprofile.config.UPAttribute;
 import org.keycloak.representations.userprofile.config.UPAttributePermissions;
 import org.keycloak.representations.userprofile.config.UPConfig;
 import org.keycloak.services.managers.AppAuthManager;
-import org.keycloak.services.managers.AuthenticationManager;
 import org.keycloak.testframework.remote.providers.runonserver.RunOnServerException;
 import org.keycloak.testframework.util.ApiUtil;
 import org.keycloak.testsuite.util.oauth.oid4vc.CredentialIssuerMetadataResponse;
@@ -87,7 +81,6 @@ import org.jboss.logging.Logger;
 import static org.keycloak.OID4VCConstants.CLAIM_NAME_VC;
 import static org.keycloak.jose.jwe.JWEConstants.RSA_OAEP_256;
 import static org.keycloak.models.oid4vci.CredentialScopeModel.VC_CONFIGURATION_ID;
-import static org.keycloak.protocol.oid4vc.issuance.OID4VCIssuerEndpoint.CREDENTIAL_OFFER_URI_CODE_SCOPE;
 import static org.keycloak.userprofile.DeclarativeUserProfileProvider.UP_COMPONENT_CONFIG_KEY;
 import static org.keycloak.userprofile.config.UPConfigUtils.ROLE_ADMIN;
 import static org.keycloak.userprofile.config.UPConfigUtils.ROLE_USER;
@@ -475,29 +468,5 @@ public abstract class OID4VCIssuerEndpointTest extends OID4VCIssuerTestBase {
                 TIME_PROVIDER,
                 30
         );
-    }
-
-    static OAuth2CodeEntry prepareSessionCode(
-            KeycloakSession session,
-            AppAuthManager.BearerTokenAuthenticator authenticator,
-            String note) {
-
-        AuthenticationManager.AuthResult authResult = authenticator.authenticate();
-        UserSessionModel userSessionModel = authResult.session();
-        AuthenticatedClientSessionModel authenticatedClientSessionModel =
-                userSessionModel.getAuthenticatedClientSessionByClient(authResult.client().getId());
-
-        OAuth2Code oauth2Code = new OAuth2Code(
-                SecretGenerator.getInstance().randomString(),
-                Time.currentTime() + 6000,
-                SecretGenerator.getInstance().randomString(),
-                CREDENTIAL_OFFER_URI_CODE_SCOPE,
-                authenticatedClientSessionModel.getUserSession().getId()
-        );
-
-        String nonce = OAuth2CodeParser.persistCode(session, authenticatedClientSessionModel, oauth2Code);
-        authenticatedClientSessionModel.setNote(nonce, note);
-
-        return new OID4VCIssuerEndpointTest.OAuth2CodeEntry(nonce, oauth2Code);
     }
 }
