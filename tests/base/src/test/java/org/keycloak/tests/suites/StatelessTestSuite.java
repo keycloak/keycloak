121065654bd19edd1e06a97037f2f8b7d680016a
Alexander Schwartz <alexander.schwartz@ibm.com>
2026-08-28 18:01:08 +0200

    Adding this test as it triggers brute force detection
    Signed-off-by: Alexander Schwartz <alexander.schwartz@ibm.com>

--- tests/base/src/test/java/org/keycloak/tests/suites/StatelessTestSuite.java
+++ tests/base/src/test/java/org/keycloak/tests/suites/StatelessTestSuite.java
@@ -6,6 +6,7 @@ import org.keycloak.testframework.server.KeycloakServerConfig;
 import org.keycloak.testframework.server.KeycloakServerConfigBuilder;
 import org.keycloak.tests.admin.client.SessionTest;
 import org.keycloak.tests.admin.concurrency.ConcurrentLoginTest;
+import org.keycloak.tests.forms.RecoveryAuthnCodesAuthenticatorTest;
 import org.keycloak.tests.loginfailures.LoginFailureExpirationTest;
 import org.keycloak.tests.model.UserSessionProviderOfflineTest;
 import org.keycloak.tests.model.UserSessionProviderTest;
@@ -29,7 +30,8 @@ import org.junit.platform.suite.api.Suite;
         UserSessionLimitsTest.class,
         SessionTimeoutValidationTest.class,
         LoginFailureExpirationTest.class,
-        OrganizationMemberTest.class
+        OrganizationMemberTest.class,
+        RecoveryAuthnCodesAuthenticatorTest.class
 })
 public class StatelessTestSuite {
 
