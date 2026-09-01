b0320060696d7394f5b5e0466f66056b7d21b13b
Alexander Schwartz <aschwart@redhat.com>
2026-09-01 11:10:31 +0200

    Stabilize test PasskeysUsernameFormTest.passwordLoginWithNonDiscoverableCredential
    Closes #52074
    
    Signed-off-by: Alexander Schwartz <alexander.schwartz@ibm.com>

--- test-framework/ui/src/main/java/org/keycloak/testframework/ui/webdriver/WaitUtils.java
+++ test-framework/ui/src/main/java/org/keycloak/testframework/ui/webdriver/WaitUtils.java
@@ -7,9 +7,12 @@ import org.keycloak.OAuth2Constants;
 import org.keycloak.testframework.ui.page.AbstractPage;
 
 import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
 import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WaitUtils {
@@ -46,6 +49,13 @@ public class WaitUtils {
 
     public WaitUtils waitForTitle(String title) {
         createDefaultWait().until(d -> d.getTitle().equals(title));
+        return this;
+    }
+
+    public WaitUtils waitForPageReload(Runnable action) {
+        WebElement body = managed.findElement(By.tagName("body"));
+        action.run();
+        createDefaultWait().until(ExpectedConditions.stalenessOf(body));
         return this;
     }
 
