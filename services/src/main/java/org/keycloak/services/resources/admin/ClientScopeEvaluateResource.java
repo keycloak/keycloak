eda565c7c2c903cadd2fe6c9bb5f1ba6d5fcf6b7
iingawal <iingawal@redhat.com>
2026-09-02 18:39:45 +0530

    Revert "Fix parameterized logging arguments"
    Reverting extra changes made in 2 services/src/main/java/org/keycloak/protocol/saml/SamlService.java and services/src/main/java/org/keycloak/services/resources/admin/ClientScopeEvaluateResource.java files
    
    This reverts commit 4841d88b4edbc81a16b2613a0816aca457eae22f.
    
    Signed-off-by: iingawal <iingawal@redhat.com>

--- services/src/main/java/org/keycloak/services/resources/admin/ClientScopeEvaluateResource.java
+++ services/src/main/java/org/keycloak/services/resources/admin/ClientScopeEvaluateResource.java
@@ -238,7 +238,7 @@ public class ClientScopeEvaluateResource {
 
         UserModel user = getUserModel(userId);
 
-        logger.debugf("generateExampleIdToken invoked. User: %s, Scope param: %s, Target Audience: %s", user.getUsername(), scopeParam, audience);
+        logger.debugf("generateExampleIdToken invoked. User: %s, Scope param: %s, Target Audience: %s", user.getUsername(), scopeParam);
 
         return sessionAware(OIDCLoginProtocol.LOGIN_PROTOCOL, user, scopeParam, audience, (userSession, clientSessionCtx, audienceClients, authSession) ->
         {
