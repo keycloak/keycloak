be838901824da60146251b547b65a4de34ddccae
Giuseppe Graziano <g.graziano94@gmail.com>
2026-09-10 09:59:28 +0200

    Fix jwt claim in introspection response
    Closes #51284
    
    Signed-off-by: Giuseppe Graziano <g.graziano94@gmail.com>

--- testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/oauth/TokenIntrospectionTest.java
+++ testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/oauth/TokenIntrospectionTest.java
@@ -130,6 +130,7 @@ public class TokenIntrospectionTest extends AbstractTestRealmKeycloakTest {
         ));
         noScopeApp.setOptionalClientScopes(List.of());
         noScopeApp.setDefaultClientScopes(List.of());
+        noScopeApp.setAttributes(Map.of(Constants.SUPPORT_JWT_CLAIM_IN_INTROSPECTION_RESPONSE_ENABLED, "true"));
 
         UserRepresentation user = new UserRepresentation();
         user.setUsername("no-permissions");
@@ -884,5 +885,23 @@ public class TokenIntrospectionTest extends AbstractTestRealmKeycloakTest {
             clientRep.setNotBefore(originalClientNotBefore);
             ApiUtil.findClientByClientId(testRealm(), "test-app").update(clientRep);
         }
+    }
+
+    @Test
+    public void testIntrospectionAudienceCheckDoesNotReturnJwt() throws Exception {
+        // The inactive response must not leak a signed JWT representation of the token to a client not in the audience
+        oauth.doLogin("test-user@localhost", "password");
+        String code = oauth.parseLoginResponse().getCode();
+        AccessTokenResponse tokenResponse = oauth.doAccessTokenRequest(code);
+        events.clear();
+
+        JsonNode introspectionResponse = oauth.introspectionRequest(tokenResponse.getAccessToken())
+                .tokenTypeHint("access_token")
+                .client("no-scope", "password")
+                .jwtResponse()
+                .send().asJsonNode();
+
+        assertFalse(introspectionResponse.get("active").asBoolean());
+        assertFalse(introspectionResponse.has("jwt"));
     }
 }
