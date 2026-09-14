b9552ebca8c8c7babe377c1fd04285481531fd00
Peter Zaoral <pzaoral@redhat.com>
2026-09-14 18:12:03 +0200

    Parse pom.properties as data instead of sourcing it as shell
    Closes: #53024
    
    Signed-off-by: Peter Zaoral <pzaoral@redhat.com>

--- distribution/licenses-common/check-licenses.sh
+++ distribution/licenses-common/check-licenses.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-MODULES=`readlink -f $1`
-LICENSES=`readlink -f $2`
+MODULES=$(readlink -f "$1")
+LICENSES=$(readlink -f "$2")
 
-for i in `find $MODULES -name '*.jar'`; do
-	if ( unzip -l $i | grep pom.properties &>/dev/null ); then
-		unzip -p $i '*/pom.properties' > /tmp/pom.properties
-		source /tmp/pom.properties &>/dev/null
+for i in $(find "$MODULES" -name '*.jar'); do
+	if (unzip -l "$i" | grep -q pom.properties); then
+		props=$(unzip -p "$i" '*/pom.properties')
+		groupId=$(printf '%s\n'    "$props" | grep '^groupId='    | head -1 | cut -d= -f2- | tr -d '[:space:]')
+		artifactId=$(printf '%s\n' "$props" | grep '^artifactId=' | head -1 | cut -d= -f2- | tr -d '[:space:]')
+		version=$(printf '%s\n'    "$props" | grep '^version='    | head -1 | cut -d= -f2- | tr -d '[:space:]')
 
-		if [ ! -f $LICENSES/$groupId\,$artifactId\,$version,* ]; then	
+		if [ ! -f "$LICENSES/$groupId,$artifactId,$version,"* ]; then
 			echo "Missing: "
 			echo "group: $groupId"
 			echo "artifact: $artifactId"
@@ -19,6 +21,4 @@ for i in `find $MODULES -name '*.jar'`; do
 		echo "Not Maven: $i"
 		echo "---------------------"
 	fi
-
-
 done
