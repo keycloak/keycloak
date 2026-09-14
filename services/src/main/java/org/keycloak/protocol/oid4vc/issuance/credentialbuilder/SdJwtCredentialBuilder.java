83a2060465cd62e4a2a20a928ea5229993cbc357
Awambeng Rodrick <awambengrodrick@gmail.com>
2026-09-14 13:56:19 +0100

    fix: support selective disclosure for SD-JWT arrays
    - Fix array claims to disclose elements individually.
    - Allow holders to selectively disclose a single array element.
    - Update affected tests and add a regression test.
    
    Closes #52671
    
    Signed-off-by: Awambeng Rodrick <awambengrodrick@gmail.com>

--- services/src/main/java/org/keycloak/protocol/oid4vc/issuance/credentialbuilder/SdJwtCredentialBuilder.java
+++ services/src/main/java/org/keycloak/protocol/oid4vc/issuance/credentialbuilder/SdJwtCredentialBuilder.java
@@ -1,20 +1,3 @@
-/*
- * Copyright 2024 Red Hat, Inc. and/or its affiliates
- * and other contributors as indicated by the @author tags.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.keycloak.protocol.oid4vc.issuance.credentialbuilder;
 
 import java.net.URI;
@@ -90,11 +73,8 @@ public class SdJwtCredentialBuilder implements CredentialBuilder {
                 .stream()
                 .filter(entry -> !credentialBuildConfig.getSdJwtVisibleClaims().contains(entry.getKey()))
                 .forEach(entry -> {
-                    if (entry instanceof List<?> listValue) {
-                        // FIXME: Unreachable branch. The intent was probably to check `entry.getValue()`,
-                        //  but changing just that will expose the array field name and break many tests.
-                        //  Needs further discussion on the wanted behavior.
-
+                    if (entry.getValue() instanceof List<?> listValue) {
+                        // Disclose elements one by one, the claim name itself stays visible
                         IntStream.range(0, listValue.size())
                                 .forEach(i -> disclosureSpecBuilder
                                         .withUndisclosedArrayElt(entry.getKey(), i, SdJwtUtils.randomSalt())
