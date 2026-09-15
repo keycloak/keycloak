fe187e939a936fb50a6be4edeab03e35e266dbfc
Michal Vavřík <dev@michalvavrik.net>
2026-09-15 11:44:27 +0200

    feat: support system truststore reloading
    closes: https://github.com/keycloak/keycloak/issues/51680
    
    Signed-off-by: Michal Vavřík <dev@michalvavrik.net>

--- tests/base/src/test/java/org/keycloak/tests/suites/FipsStrictTestSuite.java
+++ tests/base/src/test/java/org/keycloak/tests/suites/FipsStrictTestSuite.java
@@ -20,6 +20,7 @@ import org.keycloak.tests.forms.LoginSSLTest;
 import org.keycloak.tests.forms.LoginTest;
 import org.keycloak.tests.keys.JavaKeystoreKeyProviderTest;
 import org.keycloak.tests.oid4vc.issuance.signing.OID4VCSdJwtIssuingEndpointTest;
+import org.keycloak.tests.truststore.TruststoreReloadTest;
 
 import org.junit.platform.suite.api.AfterSuite;
 import org.junit.platform.suite.api.BeforeSuite;
@@ -38,7 +39,8 @@ import org.junit.platform.suite.api.Suite;
         KcAdmTest.class,
         KcAdmCreateTest.class,
         KcRegTest.class,
-        KcRegCreateTest.class
+        KcRegCreateTest.class,
+        TruststoreReloadTest.class
 })
 public class FipsStrictTestSuite {
 
