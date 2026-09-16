c7bc1e612aea6556b62263a0d33818767334dc8e
Martin Kanis <martin-kanis@users.noreply.github.com>
2026-09-16 17:30:25 +0200

    Improve retrieval of large read-only resources via admin API (#52658)
    * Improve retrieval of large read-only resources via admin API
    
    Closes #52033
    
    Signed-off-by: Martin Kanis <mkanis@ibm.com>
    (cherry picked from commit 68ff31182d1fa7bad04e2c8c7069d560e63c6297)
    
    * Improve exception handling for retrieval of large read-only resources via admin API
    
    Closes #52571
    
    Signed-off-by: Martin Kanis <mkanis@ibm.com>
    (cherry picked from commit 531b4f0e7afc53d8f2ac9f7e2180ac0c7b261b7e)
    
    ---------
    
    Signed-off-by: Martin Kanis <mkanis@ibm.com>

--- services/src/main/java/org/keycloak/services/DefaultKeycloakSession.java
+++ services/src/main/java/org/keycloak/services/DefaultKeycloakSession.java
@@ -30,6 +30,7 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.keycloak.common.util.StackUtil;
 import org.keycloak.component.ComponentFactory;
@@ -86,6 +87,7 @@ public abstract class DefaultKeycloakSession implements KeycloakSession {
     private VaultTranscriber vaultTranscriber;
     private ClientPolicyManager clientPolicyManager;
     private boolean closed = false;
+    private boolean readOnly = false;
 
     public DefaultKeycloakSession(DefaultKeycloakSessionFactory factory) {
         this.factory = factory;
@@ -149,6 +151,33 @@ public abstract class DefaultKeycloakSession implements KeycloakSession {
     @Override
     public Map<String, Object> getAttributes() {
         return Collections.unmodifiableMap(attributes);
+    }
+
+    @Override
+    public boolean isReadOnly() {
+        return readOnly;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> T runAsReadOnly(Supplier<T> supplier) {
+        boolean previous = readOnly;
+        readOnly = true;
+        boolean restore = true;
+        try {
+            T result = supplier.get();
+            if (result instanceof Stream<?> stream) {
+                T closing = (T) stream.onClose(() -> readOnly = previous);
+                // the close handler is registered, so it takes over restoring the flag
+                restore = false;
+                return closing;
+            }
+            return result;
+        } finally {
+            if (restore) {
+                readOnly = previous;
+            }
+        }
     }
 
     @Override
