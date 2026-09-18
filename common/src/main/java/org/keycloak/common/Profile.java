ffe74ca39c39bd807e1d6cf3f3d148a45fdc11da
Awambeng Rodrick <awambengrodrick@gmail.com>
2026-09-18 10:57:36 +0100

    Promote OID4VCI from experimental to preview
    - Change OID4VCI from EXPERIMENTAL to PREVIEW in Profile.java and update the related documentation and release notes. Keep the OID4VCI sub-features experimental.
    
    Closes #52813
    
    Signed-off-by: Awambeng Rodrick <awambengrodrick@gmail.com>

--- common/src/main/java/org/keycloak/common/Profile.java
+++ common/src/main/java/org/keycloak/common/Profile.java
@@ -145,7 +145,7 @@ public class Profile {
 
         PERSISTENT_USER_SESSIONS("Persistent online user sessions across restarts and upgrades", Type.DEFAULT, FeatureUpdatePolicy.SHUTDOWN),
 
-        OID4VC_VCI("Support for the OID4VCI protocol as part of OID4VC.", Type.EXPERIMENTAL),
+        OID4VC_VCI("Support for the OID4VCI protocol as part of OID4VC.", Type.PREVIEW),
         OID4VC_VCI_PREAUTH_CODE("Support for credential offers with `pre-authorized_code` grant.", Type.EXPERIMENTAL, OID4VC_VCI),
         OID4VC_VCI_REST_CREDENTIAL_OFFER("Support for the REST endpoint to create credential offers.", Type.EXPERIMENTAL, OID4VC_VCI),
         OID4VC_MDOC("Support for OID4VC `mso_mdoc` credential type.", Type.EXPERIMENTAL), // Dependent on either VCI or VP, does nothing if neither is active
