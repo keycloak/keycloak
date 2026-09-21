1b0bd5a43cadaa1e38111ab008451af7a957c3f9
Himanshi Gupta <higupta@higupta-thinkpadt14gen5.rmtin.csb>
2026-09-21 19:48:46 +0530

    closes #53002
    Signed-off-by: Himanshi Gupta <higupta@higupta-thinkpadt14gen5.rmtin.csb>

--- js/apps/admin-ui/src/clients/scopes/EvaluateScopes.tsx
+++ js/apps/admin-ui/src/clients/scopes/EvaluateScopes.tsx
@@ -184,52 +184,52 @@ export const EvaluateScopes = ({ clientId, protocol }: EvaluateScopesProps) => {
     [],
   );
 
-useFetch(
-    async () => {
-      const scope = selected.join(" ");
+  useFetch(
+    async () => {
+      const scope = selected.join(" ");
 
-      const [realmRoles, clientRoles] = await Promise.all([
-        adminClient.clients.evaluatePermission({
-          id: clientId,
-          roleContainer: realm,
-          scope,
-          type: "granted",
-        }),
-        adminClient.clients.evaluatePermission({
-          id: clientId,
-          roleContainer: clientId,
-          scope,
-          type: "granted",
-        }),
-      ]);
+      const [realmRoles, clientRoles] = await Promise.all([
+        adminClient.clients.evaluatePermission({
+          id: clientId,
+          roleContainer: realm,
+          scope,
+          type: "granted",
+        }),
+        adminClient.clients.evaluatePermission({
+          id: clientId,
+          roleContainer: clientId,
+          scope,
+          type: "granted",
+        }),
+      ]);
 
-      const effectiveRoles = [...realmRoles, ...clientRoles];
+      const effectiveRoles = [...realmRoles, ...clientRoles];
 
-      const mapperList = (await adminClient.clients.evaluateListProtocolMapper({
-        id: clientId,
-        scope,
-      })) as ({
-        type: ProtocolMapperTypeRepresentation;
-      } & ProtocolMapperRepresentation)[];
+      const mapperList = (await adminClient.clients.evaluateListProtocolMapper({
+        id: clientId,
+        scope,
+      })) as ({
+        type: ProtocolMapperTypeRepresentation;
+      } & ProtocolMapperRepresentation)[];
 
-      return {
-        mapperList,
-        effectiveRoles,
-      };
-    },
-    ({ mapperList, effectiveRoles }) => {
-      setEffectiveRoles(effectiveRoles);
-      mapperList.map((mapper) => {
-        mapper.type = mapperTypes.find(
-          (type) => type.id === mapper.protocolMapper,
-        )!;
-      });
+      return {
+        mapperList,
+        effectiveRoles,
+      };
+    },
+    ({ mapperList, effectiveRoles }) => {
+      setEffectiveRoles(effectiveRoles);
+      mapperList.map((mapper) => {
+        mapper.type = mapperTypes.find(
+          (type) => type.id === mapper.protocolMapper,
+        )!;
+      });
 
-      setProtocolMappers(mapperList);
-      refresh();
-    },
-    [selected],
-  );
+      setProtocolMappers(mapperList);
+      refresh();
+    },
+    [selected],
+  );
 
   useFetch(
     async () => {
