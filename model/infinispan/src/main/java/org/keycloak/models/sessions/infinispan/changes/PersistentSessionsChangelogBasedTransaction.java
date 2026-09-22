44325ee0a019d4e9b03b28857082d99122df06d5
Alexander Schwartz <alexander.schwartz@ibm.com>
2026-09-22 18:09:30 +0200

    NPE check
    Signed-off-by: Alexander Schwartz <alexander.schwartz@ibm.com>

--- model/infinispan/src/main/java/org/keycloak/models/sessions/infinispan/changes/PersistentSessionsChangelogBasedTransaction.java
+++ model/infinispan/src/main/java/org/keycloak/models/sessions/infinispan/changes/PersistentSessionsChangelogBasedTransaction.java
@@ -58,7 +58,7 @@ abstract public class PersistentSessionsChangelogBasedTransaction<K, V extends S
         this.cacheName = cacheName;
         this.cacheHolder = cacheHolder;
         this.offlineCacheHolder = offlineCacheHolder;
-        this.tombstoneBackupCache = cacheHolder.cache() != null ? SessionTombstoneBackup.getBackupCache(session) : null;
+        this.tombstoneBackupCache = cacheHolder != null && cacheHolder.cache() != null ? SessionTombstoneBackup.getBackupCache(session) : null;
     }
 
     public Cache<K, SessionEntityWrapper<V>> getCache(boolean offline) {
