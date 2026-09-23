37900036f677a1097fbe216f5c689e573aad6187
Thomas Darimont <thomas.darimont@googlemail.com>
2026-09-23 14:38:33 +0200

    SSF: spotless
    Signed-off-by: Thomas Darimont <thomas.darimont@googlemail.com>

--- ssf/transmitter/src/test/java/org/keycloak/ssf/transmitter/event/SecurityEventTokenMapperTest.java
+++ ssf/transmitter/src/test/java/org/keycloak/ssf/transmitter/event/SecurityEventTokenMapperTest.java
@@ -1,10 +1,10 @@
 package org.keycloak.ssf.transmitter.event;
 
 import java.util.HashMap;
-import java.util.UUID;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.UUID;
 import java.util.stream.Stream;
 
 import org.keycloak.common.Profile;
@@ -33,12 +33,12 @@ import org.keycloak.ssf.transmitter.subject.PurgedUserSnapshot;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doAnswer;
