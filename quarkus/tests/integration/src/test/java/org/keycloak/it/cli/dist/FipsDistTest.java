3d4421b63a2bac3f9dad6251292cf587ab25310c
Stian Thorgersen <stianst@gmail.com>
2026-09-25 13:06:45 +0200

    Upgrade BouncyCastle FIPS (#53182)
    Closes #52974
    
    Closes #52972
    
    Signed-off-by: stianst <stianst@gmail.com>
    (cherry picked from commit 1bf99e3722ec78b9e285673c2ad6962d407a09bf)

--- quarkus/tests/integration/src/test/java/org/keycloak/it/cli/dist/FipsDistTest.java
+++ quarkus/tests/integration/src/test/java/org/keycloak/it/cli/dist/FipsDistTest.java
@@ -36,7 +36,7 @@ import org.junit.jupiter.api.Test;
 @Tag(DistributionTest.SLOW)
 public class FipsDistTest {
 
-    private static final String BCFIPS_VERSION = "BCFIPS version 2.0102";
+    private static final String BCFIPS_VERSION = "BCFIPS version 2.0103";
 
     @Test
     void testFipsNonApprovedMode(KeycloakRunner runner) {
