e3627bbd42a7d51bcaf3e4d432a7eba79e0527eb
Steve Hawkins <shawkins@redhat.com>
2026-09-25 11:11:22 -0400

    fix: removing the stripping of the signingCertificate
    closes: #53117
    
    Signed-off-by: Steve Hawkins <shawkins@redhat.com>

--- rest/admin-v2/services/src/main/java/org/keycloak/models/utils/StripSecretsUtilsV2.java
+++ rest/admin-v2/services/src/main/java/org/keycloak/models/utils/StripSecretsUtilsV2.java
@@ -7,7 +7,6 @@ import java.util.function.BiConsumer;
 
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.representations.admin.v2.OIDCClientRepresentation;
-import org.keycloak.representations.admin.v2.SAMLClientRepresentation;
 import org.keycloak.utils.StringUtil;
 
 /**
@@ -18,7 +17,6 @@ public class StripSecretsUtilsV2 extends StripSecretsUtils {
 
     static {
         REPRESENTATION_FORMATTER.put(OIDCClientRepresentation.class, (session, o) -> StripSecretsUtilsV2.stripOidcClient((OIDCClientRepresentation) o));
-        REPRESENTATION_FORMATTER.put(SAMLClientRepresentation.class, (session, o) -> StripSecretsUtilsV2.stripSamlClient((SAMLClientRepresentation) o));
     }
 
     public static <T> T stripSecrets(KeycloakSession session, T representation) {
@@ -30,14 +28,6 @@ public class StripSecretsUtilsV2 extends StripSecretsUtils {
                 .map(OIDCClientRepresentation.Auth::getSecret)
                 .filter(StringUtil::isNotBlank)
                 .ifPresent(secret -> rep.getAuth().setSecret(maskNonVaultValue(secret)));
-        return rep;
-    }
-
-    protected static SAMLClientRepresentation stripSamlClient(SAMLClientRepresentation rep) {
-        Optional.ofNullable(rep.getSigningCertificate())
-                .filter(StringUtil::isNotBlank)
-                .ifPresent(cert -> rep.setSigningCertificate(maskNonVaultValue(cert)));
-
         return rep;
     }
 
